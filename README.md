926f19f236683978e9c9ca3b10eae314ea576fd2
Paul Dechov <dechovsky@gmail.com>
2019-03-06 17:02:20 -0500

    Minimum WordPress plugin

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # WooCommerce Payments
+
+This is a feature plugin for accepting payments via a WooCommerce-branded payment gateway.
---
+++ woocommerce-payments.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Plugin Name: WooCommerce Payments
+ * Plugin URI: https://github.com/Automattic/woocommerce-payments
+ * Description: Feature plugin for accepting payments via a WooCommerce-branded payment gateway.
+ * Author: Automattic
+ * Author URI: https://woocommerce.com/
+ * Text Domain: woocommerce-payments
+ * Domain Path: /languages
+ * Version: 0.1.0
+ */
