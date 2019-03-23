84dc4cdf740246959ab44f22fee9b887bf2e78f5
Valerie <valerie@automattic.com>
2019-03-22 21:17:15 -0400

    Try vendor/bin/phpunit again

--- bin/phpunit.sh
+++ bin/phpunit.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
-phpunit -c phpunit.xml
+./vendor/bin/phpunit
+# phpunit -c phpunit.xml
 # WORKING_DIR="$PWD"
 # cd "$WP_CORE_DIR/wp-content/plugins/woocommerce-payments/"
 # phpunit --version
