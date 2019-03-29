c7495d275f4052351e18ce81f939c198ac8d1195
Valerie <valerie@automattic.com>
2019-03-28 23:55:09 -0400

    no composer install for 7.0, so use global phpunit

--- bin/phpunit.sh
+++ bin/phpunit.sh
@@ -2,7 +2,7 @@
 
 # ./vendor/bin/phpunit
 
-if [[ ${TRAVIS_PHP_VERSION:0:3} == "5.6" ]]; then
+if [[ ${TRAVIS_PHP_VERSION:0:3} == "5.6" ]] || [[ ${TRAVIS_PHP_VERSION:0:3} == "7.0" ]]; then
 	phpunit -c phpunit.xml
 else
   ./vendor/bin/phpunit
