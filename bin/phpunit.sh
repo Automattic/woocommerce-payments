9e0b8dd18247f7440b420fc4dbbe366e436256ef
Valerie <valerie@automattic.com>
2019-04-02 10:58:47 -0400

    Rename xml files to xml.dist for local override

--- bin/phpunit.sh
+++ bin/phpunit.sh
@@ -8,7 +8,7 @@ if [[ ${TRAVIS_PHP_VERSION:0:3} == "5.2" ]] ||
 	[[ ${TRAVIS_PHP_VERSION:0:3} == "5.3" ]] ||
 	[[ ${TRAVIS_PHP_VERSION:0:3} == "5.6" ]] ||
 	[[ ${TRAVIS_PHP_VERSION:0:3} == "7.0" ]]; then
-	phpunit -c phpunit.xml
+	phpunit -c phpunit.xml.dist
 else
   ./vendor/bin/phpunit
 fi
