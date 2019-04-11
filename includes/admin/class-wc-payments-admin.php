f8c2f0a79d21ecc5a368966252b8cf665af774b2
Valerie <valerie@automattic.com>
2019-04-11 12:23:47 -0400

    Add initial view to payments page

--- includes/admin/class-wc-payments-admin.php
+++ includes/admin/class-wc-payments-admin.php
@@ -17,6 +17,8 @@ class WC_Payments_Admin {
 	public function __construct() {
 		// Add menu items.
 		add_action( 'admin_menu', array( $this, 'add_payments_menu' ), 9 );
+		add_action( 'init', array( $this, 'register_payments_scripts' ) );
+		add_action( 'admin_enqueue_scripts', array( $this, 'enqueue_payments_scripts' ) );
 	}
 
 	/**
@@ -43,5 +45,37 @@ class WC_Payments_Admin {
 			<div id="woocommerce-payments__root"></div>
 		</div>
 		<?php
+	}
+
+	/**
+	 * Register the CSS and JS scripts
+	 */
+	public function register_payments_scripts() {
+		wp_register_script(
+			'WCPAY_DASH_APP',
+			plugins_url( 'dist/index.js', WCPAY_PLUGIN_FILE ),
+			array( 'wp-element' ),
+			filemtime( WCPAY_ABSPATH . 'dist/index.js' ),
+			true
+		);
+		wp_register_style(
+			'WCPAY_DASH_APP',
+			plugins_url( 'dist/index.css', WCPAY_PLUGIN_FILE ),
+			array(),
+			filemtime( WCPAY_ABSPATH . 'dist/index.css' )
+		);
+	}
+
+	/**
+	 * Load the assets
+	 *
+	 * @param string $hook fdfd.
+	 */
+	public function enqueue_payments_scripts( $hook ) {
+		if ( 'toplevel_page_wc-payments' !== $hook ) {
+			return;
+		}
+		wp_enqueue_script( 'WCPAY_DASH_APP' );
+		wp_enqueue_style( 'WCPAY_DASH_APP' );
 	}
 }
