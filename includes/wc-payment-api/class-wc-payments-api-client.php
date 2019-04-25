2b2b7bc18edcbe7af2558b0ed73719eeff3e02ed
James Rodger <james.r.rodger@gmail.com>
2019-04-25 10:25:19 +0100

    Add methods for creating and confirming payment intents

--- includes/wc-payment-api/class-wc-payments-api-client.php
+++ includes/wc-payment-api/class-wc-payments-api-client.php
@@ -17,13 +17,30 @@ class WC_Payments_API_Client {
 	const POST = 'POST';
 	const GET  = 'GET';
 
-	const CHARGES_API = 'charges';
-	const SOURCES_API = 'sources';
+	const CHARGES_API         = 'charges';
+	const SOURCES_API         = 'sources';
+	const PAYMENT_INTENTS_API = 'payment_intents';
+
+	/**
+	 * The ID of the Stripe account this client will be used for.
+	 *
+	 * @var string
+	 */
+	private $account_id;
+
+	/**
+	 * Set the account ID to use for requests to the API.
+	 *
+	 * @param string $account_id - The ID of the Stripe account this client will be used for.
+	 */
+	public function set_account_id( $account_id ) {
+		$this->account_id = $account_id;
+	}
 
 	/**
 	 * Create a charge
 	 *
-	 * @param int    $amount - Amount to charge.
+	 * @param int    $amount    - Amount to charge.
 	 * @param string $source_id - ID of the source to associate with charge.
 	 *
 	 * @return WC_Payments_API_Charge
@@ -41,6 +58,48 @@ class WC_Payments_API_Client {
 	}
 
 	/**
+	 * Create a payment intent
+	 *
+	 * @param int    $amount        - Amount to charge.
+	 * @param string $currency_code - Currency to charge in.
+	 *
+	 * @return WC_Payments_API_Payment_Intent
+	 * @throws Exception - Exception thrown on payment intent creation failure.
+	 */
+	public function create_payment_intent( $amount, $currency_code ) {
+		// TODO: There's scope to have amount and currency bundled up into an object.
+		$request             = array();
+		$request['amount']   = $amount;
+		$request['currency'] = $currency_code;
+
+		$response_array = $this->request( $request, self::PAYMENT_INTENTS_API, self::POST );
+
+		return $this->deserialize_payment_intent_object_from_array( $response_array );
+	}
+
+	/**
+	 * Confirm a payment intent
+	 *
+	 * @param WC_Payments_API_Payment_Intent $intent    - The payment intent to confirm.
+	 * @param string                         $source_id - ID of source to process charge with.
+	 *
+	 * @return WC_Payments_API_Payment_Intent
+	 * @throws Exception - Exception thrown on payment intent confirmation failure.
+	 */
+	public function confirm_payment_intent( WC_Payments_API_Payment_Intent $intent, $source_id ) {
+		$request           = array();
+		$request['source'] = $source_id;
+
+		$response_array = $this->request(
+			$request,
+			self::PAYMENT_INTENTS_API . '/' . $intent->get_id() . '/confirm',
+			self::POST
+		);
+
+		return $this->deserialize_payment_intent_object_from_array( $response_array );
+	}
+
+	/**
 	 * Send the request to the WooCommerce Payment API
 	 *
 	 * @param array  $request - Details of the request to make.
@@ -48,16 +107,25 @@ class WC_Payments_API_Client {
 	 * @param string $method  - The HTTP method to make the request with.
 	 *
 	 * @return array
+	 * @throws Exception - If the account ID hasn't been set.
 	 */
 	private function request( $request, $api, $method ) {
 
 		// TODO: Send the request to the API.
+		// Add account ID to the request.
+		if ( ! isset( $this->account_id ) ) {
+			throw new Exception( __( 'Account ID must be set', 'woocommerce-payments' ) );
+		}
+		$request['account_id'] = $this->account_id;
+
 		// Mock up a response for now.
 		$response            = array();
 		$response['id']      = wp_generate_uuid4();
 		$response['amount']  = $request['amount'];
 		$response['created'] = time();
+		$response['status']  = 'success';
 
+		// TODO: Handle error responses by throwing exceptions.
 		return $response;
 	}
 
@@ -84,5 +152,27 @@ class WC_Payments_API_Client {
 		}
 
 		return $charge;
+	}
+
+	/**
+	 * De-serialize a payment intent array into a payment intent object
+	 *
+	 * @param array $payment_intent_array - The payment intent array to de-serialize.
+	 *
+	 * @return WC_Payments_API_Payment_Intent
+	 * @throws Exception - Unable to deserialize payment intent array.
+	 */
+	private function deserialize_payment_intent_object_from_array( array $payment_intent_array ) {
+		// TODO: Throw an exception if the response array doesn't contain mandatory properties.
+		$created = new DateTime( '@' . $payment_intent_array['created'] );
+
+		$intent = new WC_Payments_API_Payment_Intent(
+			$payment_intent_array['id'],
+			$payment_intent_array['amount'],
+			$created,
+			$payment_intent_array['status']
+		);
+
+		return $intent;
 	}
 }
