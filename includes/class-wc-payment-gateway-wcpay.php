1f52593c25f544f9f1885da9dabc3954c6178205
James Rodger <james.r.rodger@gmail.com>
2019-05-03 12:50:02 +0100

    Create a Stripe account ID setting
    We'll need this until the actual authentication is implemented and then
    we can defer the fetching of the account ID to the payments API.
    
    It would be nicer to inject the account ID to use into the API client
    at the point the client is created. However, we don't have access to the
    setting at that point so we set it up here instead.

--- includes/class-wc-payment-gateway-wcpay.php
+++ includes/class-wc-payment-gateway-wcpay.php
@@ -95,6 +95,13 @@ class WC_Payment_Gateway_WCPay extends WC_Payment_Gateway_CC {
 				'default'     => __( 'Pay with your credit card via WooCommerce Payments.', 'woocommerce-payments' ),
 				'desc_tip'    => true,
 			),
+			'stripe_account_id'    => array(
+				'title'       => __( 'Stripe Account ID', 'woocommerce-payments' ),
+				'type'        => 'text',
+				'description' => __( 'Get your account ID from your Stripe account.', 'woocommerce-payments' ),
+				'default'     => '',
+				'desc_tip'    => true,
+			),
 			'testmode'             => array(
 				'title'       => __( 'Test mode', 'woocommerce-payments' ),
 				'label'       => __( 'Enable Test Mode', 'woocommerce-payments' ),
@@ -150,6 +157,11 @@ class WC_Payment_Gateway_WCPay extends WC_Payment_Gateway_CC {
 		}
 
 		add_action( 'woocommerce_update_options_payment_gateways_' . $this->id, array( $this, 'process_admin_options' ) );
+
+		// Add account ID to the payments.
+		$this->payments_api_client->set_account_id(
+			$this->get_option( 'stripe_account_id' )
+		);
 	}
 
 	/**
