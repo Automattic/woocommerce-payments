e155775c10192f02f766056a7f1707307d1fb154
James Rodger <james.r.rodger@gmail.com>
2019-05-18 16:06:25 -0400

    Add methods for creating and confirming payment intents

--- includes/wc-payment-api/class-wc-payments-api-client.php
+++ includes/wc-payment-api/class-wc-payments-api-client.php
@@ -17,8 +17,9 @@ class WC_Payments_API_Client {
 	const POST = 'POST';
 	const GET  = 'GET';
 
-	const CHARGES_API = 'charges';
-	const SOURCES_API = 'sources';
+	const CHARGES_API    = 'charges';
+	const SOURCES_API    = 'sources';
+	const INTENTIONS_API = 'intentions';
 
 	/**
 	 * The ID of the Stripe account this client will be used for.
@@ -54,6 +55,48 @@ class WC_Payments_API_Client {
 		$response_array = $this->request( $request, self::CHARGES_API, self::POST );
 
 		return $this->deserialize_charge_object_from_array( $response_array );
+	}
+
+	/**
+	 * Create an intention
+	 *
+	 * @param int    $amount        - Amount to charge.
+	 * @param string $currency_code - Currency to charge in.
+	 *
+	 * @return WC_Payments_API_Intention
+	 * @throws Exception - Exception thrown on intention creation failure.
+	 */
+	public function create_intention( $amount, $currency_code ) {
+		// TODO: There's scope to have amount and currency bundled up into an object.
+		$request             = array();
+		$request['amount']   = $amount;
+		$request['currency'] = $currency_code;
+
+		$response_array = $this->request( $request, self::INTENTIONS_API, self::POST );
+
+		return $this->deserialize_intention_object_from_array( $response_array );
+	}
+
+	/**
+	 * Confirm an intention
+	 *
+	 * @param WC_Payments_API_Intention $intent    - The intention to confirm.
+	 * @param string                    $source_id - ID of source to process charge with.
+	 *
+	 * @return WC_Payments_API_Intention
+	 * @throws Exception - Exception thrown on intention confirmation failure.
+	 */
+	public function confirm_intention( WC_Payments_API_Intention $intent, $source_id ) {
+		$request           = array();
+		$request['source'] = $source_id;
+
+		$response_array = $this->request(
+			$request,
+			self::INTENTIONS_API . '/' . $intent->get_id() . '/confirm',
+			self::POST
+		);
+
+		return $this->deserialize_intention_object_from_array( $response_array );
 	}
 
 	/**
@@ -100,5 +143,28 @@ class WC_Payments_API_Client {
 		}
 
 		return $charge;
+	}
+
+	/**
+	 * De-serialize an intention array into a intention object
+	 *
+	 * @param array $intention_array - The intention array to de-serialize.
+	 *
+	 * @return WC_Payments_API_Intention
+	 * @throws Exception - Unable to deserialize intention array.
+	 */
+	private function deserialize_intention_object_from_array( array $intention_array ) {
+		// TODO: Throw an exception if the response array doesn't contain mandatory properties.
+		$created = new DateTime();
+		$created->setTimestamp( $intention_array['created'] );
+
+		$intent = new WC_Payments_API_Intention(
+			$intention_array['id'],
+			$intention_array['amount'],
+			$created,
+			$intention_array['status']
+		);
+
+		return $intent;
 	}
 }
