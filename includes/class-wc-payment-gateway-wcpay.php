1c6e7e55a53156a52f3f63fd38dac80ec6817e3f
Daniel Rey López <daniel.rey.lopez@gmail.com>
2019-05-18 15:35:19 -0400

    Use "createSource" instead of "createToken"

--- includes/class-wc-payment-gateway-wcpay.php
+++ includes/class-wc-payment-gateway-wcpay.php
@@ -204,7 +204,7 @@ class WC_Payment_Gateway_WCPay extends WC_Payment_Gateway_CC {
 		<p><?php echo wp_kses_post( $this->get_description() ); ?></p>
 		<div id="wc-payment-card-element"></div>
 		<div id="wc-payment-errors" role="alert"></div>
-		<input id="wc-payment-token" type="hidden" name="wc-payment-token" />
+		<input id="wc-payment-source" type="hidden" name="wc-payment-source" />
 		<?php
 	}
 
@@ -224,15 +224,15 @@ class WC_Payment_Gateway_WCPay extends WC_Payment_Gateway_CC {
 			$transaction_id = '';
 
 			if ( $amount > 0 ) {
-				// Get the payment token from the request (generated when the user entered their card details).
-				$token = $this->get_token_from_request();
+				// Get the payment source from the request (generated when the user entered their card details).
+				$source = $this->get_source_from_request();
 
 				// Create a payment intent.
 				$intent = $this->payments_api_client->create_payment_intent( $amount, 'usd' );
 
 				// TODO: We could attempt to confirm the intent when creating it instead?
 				// Try to confirm the intent & capture the charge (if 3DS is not required).
-				$intent = $this->payments_api_client->confirm_payment_intent( $intent, $token );
+				$intent = $this->payments_api_client->confirm_payment_intent( $intent, $source );
 
 				// TODO: We're not handling *all* sorts of things here. For example, redirecting to a 3DS auth flow.
 				$transaction_id = $intent->get_id();
@@ -270,21 +270,21 @@ class WC_Payment_Gateway_WCPay extends WC_Payment_Gateway_CC {
 	}
 
 	/**
-	 * Extract the payment token from the request's POST variables
+	 * Extract the payment source from the request's POST variables
 	 *
 	 * @return string
-	 * @throws Exception - If no token is found.
+	 * @throws Exception - If no source is found.
 	 */
-	private function get_token_from_request() {
+	private function get_source_from_request() {
 		// phpcs:disable WordPress.Security.NonceVerification.NoNonceVerification
-		if ( ! isset( $_POST['wc-payment-token'] ) ) {
-			// If no payment token is set then stop here with an error.
-			throw new Exception( __( 'Payment token not found.', 'woocommerce-payments' ) );
+		if ( ! isset( $_POST['wc-payment-source'] ) ) {
+			// If no payment source is set then stop here with an error.
+			throw new Exception( __( 'Payment source not found.', 'woocommerce-payments' ) );
 		}
 
-		$token = wc_clean( $_POST['wc-payment-token'] ); //phpcs:ignore WordPress.Security.ValidatedSanitizedInput.MissingUnslash
+		$source = wc_clean( $_POST['wc-payment-source'] ); //phpcs:ignore WordPress.Security.ValidatedSanitizedInput.MissingUnslash
 		// phpcs:enable WordPress.Security.NonceVerification.NoNonceVerification
 
-		return $token;
+		return $source;
 	}
 }
