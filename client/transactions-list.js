09ba3c1451d5fd68e84dec2d8d7588b8fcff3f74
Paul Dechov <dechovsky@gmail.com>
2019-06-13 16:12:02 -0400

    Adapt table columns to be closer to design

--- client/transactions-list.js
+++ client/transactions-list.js
@@ -9,12 +9,12 @@ import { formatCurrency } from '@woocommerce/currency';
 import { TableCard } from '@woocommerce/components';
 
 const headers = [
-	{ key: 'type', label: 'Type', isLeftAligned: true },
-	{ key: 'status', label: 'Status', isLeftAligned: true },
+	{ key: 'created', label: 'Date / Time', isLeftAligned: true },
+	{ key: 'type', label: 'Type' },
+	{ key: 'status', label: 'Status', hiddenByDefault: true },
 	{ key: 'description', label: 'Description', hiddenByDefault: true, isLeftAligned: true },
 	{ key: 'amount', label: 'Amount' },
 	{ key: 'fee', label: 'Fee' },
-	{ key: 'created', label: 'Created on' },
 	{ key: 'available_on', label: 'Available on' },
 ];
 
@@ -26,12 +26,12 @@ export default () => {
 	}, [] );
 
 	const rows = response && response.data.map( ( { type, status, description, amount, fee, created, available_on } ) => [
+		{ value: created * 1000, display: dateI18n( 'Y-m-d H:i', moment( created * 1000 ) ) },
 		{ value: type, display: type[ 0 ].toUpperCase() + type.slice( 1 ) },
 		{ value: status, display: status[ 0 ].toUpperCase() + status.slice( 1 ) },
 		{ value: description, display: description },
 		{ value: amount / 100, display: formatCurrency( amount / 100 ) },
 		{ value: fee / 100, display: formatCurrency( fee / 100 ) },
-		{ value: created * 1000, display: dateI18n( 'Y-m-d H:i', moment( created * 1000 ) ) },
 		{ value: available_on * 1000, display: dateI18n( 'Y-m-d H:i', moment( available_on * 1000 ) ) },
 	] );
 
