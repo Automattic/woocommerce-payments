fe99cb956294147f3a5605ee79ccd62c9ca91380
Kristófer R <kristofer.thorlaksson@automattic.com>
2019-07-05 14:19:51 +0000

    Add wrapper for HTTP request calls through Jetpack
    This will allow us to mock the HTTP request API when unit and/or
    integration testing the Payments API Client.

---
+++ includes/wc-payment-api/class-wc-payments-http.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * WC_Payments_Http class.
+ *
+ * @package WooCommerce\Payments
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * A wrapper around Jetpack HTTP request library. Necessary to increase
+ * the testability of WC_Payments_API_Client, and allow dependency
+ * injection.
+ */
+class WC_Payments_Http {
+
+	/**
+	 * Sends a remote request through Jetpack (?).
+	 *
+	 * @param array  $args - The arguments to passed to Jetpack.
+	 * @param string $body - The body passed on to the HTTP request.
+	 *
+	 * @return WP_Error|array HTTP response on success.
+	 */
+	public function remote_request( $args, $body = null ) {
+		$args['blog_id'] = Jetpack_Options::get_option( 'id' );
+		$args['user_id'] = JETPACK_MASTER_USER;
+		return Jetpack_Client::remote_request( $args, $body );
+	}
+}
