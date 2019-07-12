f1a782f6560e8a6952a06efe8a30c6e60c679554
Kristófer R <kristofer.thorlaksson@automattic.com>
2019-07-12 21:00:11 +0000

    Retrieve data to display in TransactionsList using transactions selector
    By retrieving the relevant selectors we can access the data from the
    fresh-data data store, and use that to populate the transactions table.
    
    As far as I understand this (@coderkevin would be a better person to
    ask) the `withSelect` method basically injects data from the selector
    into the component's `this.props`, allowing us to use the data in the
    component.

--- client/transactions/index.js
+++ client/transactions/index.js
@@ -3,7 +3,11 @@
  */
 import { Component } from '@wordpress/element';
 import { compose } from '@wordpress/compose';
+import { dateI18n } from '@wordpress/date';
+import moment from 'moment';
+import { formatCurrency } from '@woocommerce/currency';
 import { TableCard } from '@woocommerce/components';
+import { capitalize } from 'lodash';
 
 /**
  * Internal dependencies.
@@ -27,6 +31,30 @@ const headers = [
 
 class TransactionsList extends Component {
 	render() {
+		const { transactions } = this.props;
+
+		const rows = transactions.map( ( txn ) => {
+			const charge = txn.source.object === 'charge' ? txn.source : null;
+			const order_url = txn.order ? <a href={ txn.order.url }>#{ txn.order.number }</a> : <span>&ndash;</span>;
+
+			const data = {
+				created: { value: txn.created * 1000, display: dateI18n( 'M j, Y / g:iA', moment( txn.created * 1000 ) ) },
+				type: { value: txn.type, display: capitalize( txn.type ) },
+				source: charge && { value: charge.payment_method_details.card.brand, display: <code>{ charge.payment_method_details.card.brand }</code> },
+				order: { value: txn.order, display: order_url },
+				customer: charge && { value: charge.billing_details.name, display: charge.billing_details.name },
+				email: charge && { value: charge.billing_details.email, display: charge.billing_details.email },
+				country: charge && { value: charge.billing_details.address.country, display: charge.billing_details.address.country },
+				amount: { value: txn.amount / 100, display: formatCurrency( txn.amount / 100 ) },
+				fee: { value: txn.fee / 100, display: formatCurrency( txn.fee / 100 ) },
+				net: { value: ( txn.amount - txn.fee ) / 100, display: formatCurrency( ( txn.amount - txn.fee ) / 100 ) },
+				// TODO deposit: { value: available_on * 1000, display: dateI18n( 'Y-m-d H:i', moment( available_on * 1000 ) ) },
+				risk_level: charge && { value: charge.outcome.risk_level, display: capitalize( charge.outcome.risk_level ) },
+			};
+
+			return headers.map( ( { key } ) => data[ key ] || { display: null } );
+		} );
+
 		return (
 			<TableCard
 				title="Transactions"
@@ -34,12 +62,17 @@ class TransactionsList extends Component {
 				rowsPerPage={ 10 }
 				totalRows={ 10 }
 				headers={ headers }
-				rows={ [] }
+				rows={ rows }
 			/>
 		);
 	}
 };
 
 export default compose(
-	withSelect( select => {} )
+	withSelect( select => {
+		const { getTransactions } = select( 'wc-payments-api' );
+		const transactions = getTransactions();
+
+		return { transactions };
+	} )
 )( TransactionsList );
