db7831ff1ccb66ebc92a57e4224636a5885e9681
Kristófer R <kristofer.thorlaksson@automattic.com>
2019-07-12 20:34:24 +0000

    Add function to initialize the fresh-data API client

---
+++ client/payments-api/payments-data-store/create-api-client.js
@@ -0,0 +1,51 @@
+
+/**
+ * External dependencies.
+ */
+import { ApiClient } from '@fresh-data/framework';
+
+/**
+ * Internal dependencies.
+ */
+import createStore from './create-store';
+
+function createDataHandlers( store ) {
+	return {
+		dataRequested: resourceNames => {
+			// This is a temporary fix until it can be resolved upstream in fresh-data.
+			// See: https://github.com/woocommerce/woocommerce-admin/pull/2387/files#r292355276
+			if ( document.hidden ) {
+				return;
+			}
+
+			store.dispatch( {
+				type: 'FRESH_DATA_REQUESTED',
+				resourceNames,
+				time: new Date(),
+			} );
+		},
+		dataReceived: resourceNames => {
+			store.dispatch( {
+				type: 'FRESH_DATA_RECEIVED',
+				resources,
+				time: new Date(),
+			} );
+		},
+	};
+}
+
+function createApiClient( name, apiSpec ) {
+	const store = createStore( name );
+	const dataHandlers = createDataHandlers( store );
+	const apiClient = new ApiClient( apiSpec );
+	apiClient.setDataHandlers( dataHandlers );
+
+	const storeChanged = () => {
+		apiClient.setState( store.getState() );
+	}
+	store.subscribe( storeChanged );
+
+	return apiClient;
+}
+
+export default createApiClient;
