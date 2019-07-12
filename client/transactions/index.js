dca7fd5c4c8bfa7bc32b1f33296d8d6b092b8078
Kristófer R <kristofer.thorlaksson@automattic.com>
2019-07-12 20:46:18 +0000

    Refactor TransactionsList to React Component class
    This is necessary to use the component with fresh-data. Currently the
    component always shows an empty table.

--- client/transactions/index.js
+++ client/transactions/index.js
@@ -1,13 +1,8 @@
 /**
  * External dependencies
  */
-import { useState, useEffect } from '@wordpress/element';
-import apiFetch from '@wordpress/api-fetch';
-import { dateI18n } from '@wordpress/date';
-import moment from 'moment';
-import { formatCurrency } from '@woocommerce/currency';
+import { Component } from '@wordpress/element';
 import { TableCard } from '@woocommerce/components';
-import { capitalize } from 'lodash';
 
 const headers = [
 	{ key: 'created', label: 'Date / Time', required: true, isLeftAligned: true, defaultSort: true, defaultOrder: 'desc' },
@@ -24,65 +19,19 @@ const headers = [
 	{ key: 'risk_level', label: 'Risk Level', hiddenByDefault: true },
 ];
 
-const TransactionsPage = () => {
-	const [ transactions, setTransactions ] = useState( [] );
-	const [ loading, setLoading ] = useState( false );
-
-	useEffect( () => {
-		setLoading( true );
-		apiFetch( { path: '/wc/v3/payments/transactions' } ).then( ( response ) => {
-			const { data } = response;
-			if ( data ) {
-				setTransactions( data );
-			} else {
-				console.error( response );
-			}
-
-			setLoading( false );
-		} );
-	}, [] );
-
-	const rows = transactions.map( ( txn ) => {
-		const charge = txn.source.object === 'charge' ? txn.source : null;
-		const order_url = txn.order ? <a href={ txn.order.url }>#{ txn.order.number }</a> : <span>&ndash;</span>;
-
-		// Extract nested properties from the charge.
-		const billing_details = charge ? charge.billing_details : null;
-		const outcome = charge ? charge.outcome : null;
-		const payment_method_details = charge ? charge.payment_method_details : null;
-
-		const address = billing_details ? billing_details.address : null;
-		const card = payment_method_details ?  payment_method_details.card : null;
-
-		// Map transaction into table row.
-		const data = {
-			created: { value: txn.created * 1000, display: dateI18n( 'M j, Y / g:iA', moment( txn.created * 1000 ) ) },
-			type: { value: txn.type, display: capitalize( txn.type ) },
-			source: card && { value: card.brand, display: <code>{ card.brand }</code> },
-			order: { value: txn.order, display: order_url },
-			customer: billing_details && { value: billing_details.name, display: billing_details.name },
-			email: billing_details && { value: billing_details.email, display: billing_details.email },
-			country: address && { value: address.country, display: address.country },
-			amount: { value: txn.amount / 100, display: formatCurrency( txn.amount / 100 ) },
-			fee: { value: txn.fee / 100, display: formatCurrency( txn.fee / 100 ) },
-			net: { value: ( txn.amount - txn.fee ) / 100, display: formatCurrency( ( txn.amount - txn.fee ) / 100 ) },
-			// TODO deposit: { value: available_on * 1000, display: dateI18n( 'Y-m-d H:i', moment( available_on * 1000 ) ) },
-			risk_level: outcome && { value: outcome.risk_level, display: capitalize( outcome.risk_level ) },
-		};
-
-		return headers.map( ( { key } ) => data[ key ] || { display: null } );
-	} );
-
-	return (
-		<TableCard
-			title="Transactions"
-			isLoading={ loading }
-			rowsPerPage={ 10 }
-			totalRows={ 10 }
-			headers={ headers }
-			rows={ rows }
-		/>
-	);
+class TransactionsList extends Component {
+	render() {
+		return (
+			<TableCard
+				title="Transactions"
+				isLoading={ false }
+				rowsPerPage={ 10 }
+				totalRows={ 10 }
+				headers={ headers }
+				rows={ [] }
+			/>
+		);
+	}
 };
 
-export default TransactionsPage;
+export default TransactionsList;
