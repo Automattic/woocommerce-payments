dad4eca6a7ec82ad4a2b780b5122953611af555a
Kristófer R <kristofer.thorlaksson@automattic.com>
2019-07-16 00:20:30 +0000

    Add transactions list selector
    The selector can be used to retrieve transactions list for components.
    This commit also adapts the code to fit the new selector better.

--- client/payments-api/api-spec/transactions/selectors.js
+++ client/payments-api/api-spec/transactions/selectors.js
@@ -7,19 +7,8 @@ import { DEFAULT_REQUIREMENT } from '../../constants';
 const getTransactions = ( getResource, requireResource ) => (
 	requirement = DEFAULT_REQUIREMENT
 ) => {
-	return [ {
-		created: new Date(),
-		type: '',
-		source: {},
-		order: { url: '#', number: 123 },
-		customer: {},
-		email: 'admin@test.com',
-		country: 'USA',
-		amount: 1234,
-		fee: 1,
-		net: 2,
-		risk_level: '',
-	} ];
+	const resourceName = 'transactions-list';
+	return requireResource( requirement, resourceName ).data || {};
 }
 
 export default {
