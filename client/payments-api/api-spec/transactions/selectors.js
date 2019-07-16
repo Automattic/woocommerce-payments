4cbe81c95beb696e3f2d08a3a26a37b4242e6507
Kristófer R <kristofer.thorlaksson@automattic.com>
2019-07-16 00:21:52 +0000

    Add code to determine if the table loading view should be displayed

--- client/payments-api/api-spec/transactions/selectors.js
+++ client/payments-api/api-spec/transactions/selectors.js
@@ -11,6 +11,21 @@ const getTransactions = ( getResource, requireResource ) => (
 	return requireResource( requirement, resourceName ).data || {};
 }
 
+const getTransactionsIsLoading = ( getResource, requireResource ) => (
+	requirement = DEFAULT_REQUIREMENT
+) => {
+	const resourceName = 'transactions-list';
+	const transactions = requireResource( requirement, resourceName ).data || {};
+
+	// If no transactions are available, assume the request is loading.
+	if ( ! transactions.data ) {
+		return true;
+	}
+
+	return transactions.lastRequested > transactions.lastReceived;
+}
+
 export default {
 	getTransactions,
+	getTransactionsIsLoading,
 };
