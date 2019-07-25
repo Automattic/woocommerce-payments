139ad9e6055de35d6fbd4cf804856877b1b01b95
Paul Dechov <dechovsky@gmail.com>
2019-07-25 13:31:43 -0400

    WP coding standards: use $ instead of jQuery

--- assets/js/wc-payment-checkout.js
+++ assets/js/wc-payment-checkout.js
@@ -1,5 +1,5 @@
 /* global jQuery, Stripe, wc_payment_config */
-jQuery( function() {
+jQuery( function( $ ) {
 	'use strict';
 
 	var stripe   = new Stripe( wc_payment_config.publishableKey, {
@@ -15,9 +15,9 @@ jQuery( function() {
 	// Only attempt to mount the card element once that section of the page has loaded. We can use the updated_checkout
 	// event for this. This part of the page can also reload based on changes to checkout details, so we call unmount
 	// first to ensure the card element is re-mounted correctly.
-	jQuery( document.body ).on( 'updated_checkout', function() {
+	$( document.body ).on( 'updated_checkout', function() {
 		// Don't re-mount if already mounted in DOM.
-		if ( jQuery( '#wc-payment-card-element' ).children().length ) {
+		if ( $( '#wc-payment-card-element' ).children().length ) {
 			return;
 		}
 
@@ -37,7 +37,7 @@ jQuery( function() {
 
 	// Create payment method on submission.
 	var paymentMethodGenerated;
-	jQuery( 'form.checkout' ).on( 'checkout_place_order_woocommerce_payments', function() {
+	$( 'form.checkout' ).on( 'checkout_place_order_woocommerce_payments', function() {
 		// We'll resubmit the form after populating our payment method, so if this is the second time this event
 		// is firing we should let the form submission happen.
 		if ( paymentMethodGenerated ) {
@@ -67,7 +67,7 @@ jQuery( function() {
 				paymentMethodInput.value = id;
 
 				// Re-submit the form.
-				jQuery( '.woocommerce-checkout' ).submit();
+				$( '.woocommerce-checkout' ).submit();
 			} );
 
 		// Prevent form submission so that we can fire it once a payment method has been generated.
