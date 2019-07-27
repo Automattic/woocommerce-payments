7b727cae675d4c7b85ecb4d56ef27d1ad54fca37
Kristófer R <kristofer.thorlaksson@automattic.com>
2019-07-27 19:36:45 +0000

    Remove transactions list dependency on React.Component

--- client/transactions/index.js
+++ client/transactions/index.js
@@ -1,7 +1,6 @@
 /**
  * External dependencies
  */
-import { Component } from '@wordpress/element';
 import { compose } from '@wordpress/compose';
 import { dateI18n } from '@wordpress/date';
 import moment from 'moment';
@@ -29,53 +28,51 @@ const headers = [
 	{ key: 'risk_level', label: 'Risk Level', hiddenByDefault: true },
 ];
 
-class TransactionsList extends Component {
-	render() {
-		const { transactions, isLoading } = this.props;
-		const transactionsData = transactions.data || [];
-		// Do not display table loading view if data is already available.
+const TransactionsList = ( props ) => {
+	const { transactions, isLoading } = props;
+	const transactionsData = transactions.data || [];
+	// Do not display table loading view if data is already available.
 
-		const rows = transactionsData.map( ( txn ) => {
-			const charge = txn.source.object === 'charge' ? txn.source : null;
-			const order_url = txn.order ? <a href={ txn.order.url }>#{ txn.order.number }</a> : <span>&ndash;</span>;
+	const rows = transactionsData.map( ( txn ) => {
+		const charge = txn.source.object === 'charge' ? txn.source : null;
+		const order_url = txn.order ? <a href={ txn.order.url }>#{ txn.order.number }</a> : <span>&ndash;</span>;
 
-			// Extract nested properties from the charge.
-			const billing_details = charge ? charge.billing_details : null;
-			const outcome = charge ? charge.outcome : null;
-			const payment_method_details = charge ? charge.payment_method_details : null;
-			const address = billing_details ? billing_details.address : null;
-			const card = payment_method_details ?  payment_method_details.card : null;
+		// Extract nested properties from the charge.
+		const billing_details = charge ? charge.billing_details : null;
+		const outcome = charge ? charge.outcome : null;
+		const payment_method_details = charge ? charge.payment_method_details : null;
+		const address = billing_details ? billing_details.address : null;
+		const card = payment_method_details ?  payment_method_details.card : null;
 
-			// Map transaction into table row.
-			const data = {
-				created: { value: txn.created * 1000, display: dateI18n( 'M j, Y / g:iA', moment( txn.created * 1000 ) ) },
-				type: { value: txn.type, display: capitalize( txn.type ) },
-				source: card && { value: card.brand, display: <code>{ card.brand }</code> },
-				order: { value: txn.order, display: order_url },
-				customer: billing_details && { value: billing_details.name, display: billing_details.name },
-				email: billing_details && { value: billing_details.email, display: billing_details.email },
-				country: address && { value: address.country, display: address.country },
-				amount: { value: txn.amount / 100, display: formatCurrency( txn.amount / 100 ) },
-				fee: { value: txn.fee / 100, display: formatCurrency( txn.fee / 100 ) },
-				net: { value: ( txn.amount - txn.fee ) / 100, display: formatCurrency( ( txn.amount - txn.fee ) / 100 ) },
-				// TODO deposit: { value: available_on * 1000, display: dateI18n( 'Y-m-d H:i', moment( available_on * 1000 ) ) },
-				risk_level: outcome && { value: outcome.risk_level, display: capitalize( outcome.risk_level ) },
-			};
+		// Map transaction into table row.
+		const data = {
+			created: { value: txn.created * 1000, display: dateI18n( 'M j, Y / g:iA', moment( txn.created * 1000 ) ) },
+			type: { value: txn.type, display: capitalize( txn.type ) },
+			source: card && { value: card.brand, display: <code>{ card.brand }</code> },
+			order: { value: txn.order, display: order_url },
+			customer: billing_details && { value: billing_details.name, display: billing_details.name },
+			email: billing_details && { value: billing_details.email, display: billing_details.email },
+			country: address && { value: address.country, display: address.country },
+			amount: { value: txn.amount / 100, display: formatCurrency( txn.amount / 100 ) },
+			fee: { value: txn.fee / 100, display: formatCurrency( txn.fee / 100 ) },
+			net: { value: ( txn.amount - txn.fee ) / 100, display: formatCurrency( ( txn.amount - txn.fee ) / 100 ) },
+			// TODO deposit: { value: available_on * 1000, display: dateI18n( 'Y-m-d H:i', moment( available_on * 1000 ) ) },
+			risk_level: outcome && { value: outcome.risk_level, display: capitalize( outcome.risk_level ) },
+		};
 
-			return headers.map( ( { key } ) => data[ key ] || { display: null } );
-		} );
+		return headers.map( ( { key } ) => data[ key ] || { display: null } );
+	} );
 
-		return (
-			<TableCard
-				title="Transactions"
-				isLoading={ isLoading }
-				rowsPerPage={ 10 }
-				totalRows={ 10 }
-				headers={ headers }
-				rows={ rows }
-			/>
-		);
-	}
+	return (
+		<TableCard
+			title="Transactions"
+			isLoading={ isLoading }
+			rowsPerPage={ 10 }
+			totalRows={ 10 }
+			headers={ headers }
+			rows={ rows }
+		/>
+	);
 };
 
 export default compose(
