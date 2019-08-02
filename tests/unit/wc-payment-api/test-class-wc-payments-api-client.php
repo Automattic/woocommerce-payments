316cef98c847fc663cd6a242246cfb9bc5dc8a4c
James Rodger <james.r.rodger@gmail.com>
2019-08-02 17:13:47 +0100

    Extend base TestCase` class instead of WP_UnitTestCase
    `WP_UnitTestCase` is from the WordPress develop distribution, so we
    don't want to use it if we're trying to remove that as a dependency.

--- tests/unit/wc-payment-api/test-class-wc-payments-api-client.php
+++ tests/unit/wc-payment-api/test-class-wc-payments-api-client.php
@@ -8,7 +8,7 @@
 /**
  * WC_Payments_API_Client unit tests.
  */
-class WC_Payments_API_Client_Test extends WP_UnitTestCase {
+class WC_Payments_API_Client_Test extends PHPUnit\Framework\TestCase {
 
 	/**
 	 * System under test
