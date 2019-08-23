384b4f2ea846d6ddd2c20d23d27cf7c48b281efa
Kristófer R <kristofer.thorlaksson@automattic.com>
2019-08-24 00:33:25 +0800

    Change functions in `render()` from local to class functions
    Cleaner code, woop woop!

--- client/transactions/index.js
+++ client/transactions/index.js
@@ -67,6 +67,35 @@ export class TransactionsList extends Component {
 		return headers.map( ( { key } ) => data[ key ] || { display: null } );
 	}
 
+	onPageChange = ( ...params ) => {
+		this.setState( {
+			...this.state,
+			currentPage: params[ 0 ],
+		} );
+	};
+
+	onQueryChange = ( ...params ) => {
+		if ( 'per_page' === params[ 0 ] ) {
+			return ( ...rowsPerPage ) => {
+				this.setState( {
+					...this.state,
+					rowsPerPage: rowsPerPage[ 0 ],
+				} );
+			};
+		}
+	};
+
+	summary = ( showPlaceholder, numberOfTransactions ) => {
+		if ( showPlaceholder ) {
+			return [];
+		}
+
+		return [ {
+			label: 'transactions',
+			value: numberOfTransactions,
+		} ];
+	};
+
 	render() {
 		const {
 			showPlaceholder,
@@ -78,52 +107,19 @@ export class TransactionsList extends Component {
 				getTransactionsPage( this.state.currentPage, this.state.rowsPerPage ).data || [];
 		const rows = transactionsData.map( this.transactionsToRows );
 
-		// onPageChange defined like this since `const func = () => {}` not supported
-		// for class functions.
-		const onPageChange = ( ...params ) => {
-			this.setState( {
-				...this.state,
-				currentPage: params[ 0 ],
-			} );
-		};
-
-		// onQueryChange defined like this since `const func = () => {}` not supported
-		// for class functions.
-		const onQueryChange = ( ...params ) => {
-			if ( 'per_page' === params[ 0 ] ) {
-				return ( ...rowsPerPage ) => {
-					this.setState( {
-						...this.state,
-						rowsPerPage: rowsPerPage[ 0 ],
-					} );
-				};
-			}
-		};
-
-		const summary = () => {
-			if ( showPlaceholder ) {
-				return [];
-			}
-
-			return [ {
-				label: 'transactions',
-				value: getNumberOfTransactions(),
-			} ];
-		};
-
 		return (
 			<TableCard
 				title="Transactions"
 				isLoading={ showPlaceholder }
 				query={ { paged: this.state.currentPage, per_page: this.state.rowsPerPage } }
-				onPageChange={ onPageChange }
-				onQueryChange={ onQueryChange }
+				onPageChange={ this.onPageChange }
+				onQueryChange={ this.onQueryChange }
 				rowsPerPage={ this.state.rowsPerPage }
 				totalRows={ getNumberOfTransactions() }
 				downloadable={ true }
 				headers={ headers }
 				rows={ rows }
-				summary={ summary() }
+				summary={ this.summary( showPlaceholder, getNumberOfTransactions() ) }
 			/>
 		);
 	}
