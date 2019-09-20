cd365607fbaf47977ce9bc15ad7696aa366cc828
Luiz Reis <luiz.reis@automattic.com>
2019-09-20 18:09:55 -0300

    Add transaction summary details
    Display basic information of a transaction, such as amount, fees, dispute, status, control buttons for resolving disputes or refunding, etc.

--- client/transaction-details/summary/index.js
+++ client/transaction-details/summary/index.js
@@ -7,20 +7,42 @@ import { Card } from '@woocommerce/components';
 import { dateI18n } from '@wordpress/date';
 import moment from 'moment';
 import { get } from 'lodash';
+import { formatCurrency } from '@woocommerce/currency';
 
 /**
  * Internal dependencies.
  */
+import PaymentStatusChip from '../../components/payment-status-chip';
 import OrderLink from '../../components/order-link';
 import CardSummary from '../../components/card-summary';
 import HorizontalList from '../../components/horizontal-list';
+import './style.scss';
 
 const TransactionSummaryDetails = ( props ) => {
 	const { transaction } = props;
 	// TODO: this is a placeholder card and does not require translation
 	return (
-		<Card title="Summary" action={ transaction.id }>
-			Summary details for transaction { transaction.id }.
+		<Card>
+			<div className="transaction-summary">
+				<div className="transaction-summary__section">
+					<h1 className="transaction-summary__amount">
+						{ formatCurrency( ( transaction.amount || 0 ) / 100 ) }
+						<span className="transaction-summary__amount-currency">{ ( transaction.currency || 'cur' ) }</span>
+						<PaymentStatusChip transaction={ transaction } style={ { marginLeft: '1em' } } />
+					</h1>
+					<div className="transaction-summary__breakdown">
+						{ transaction.dispute
+							? <p>Dispute: { formatCurrency( ( get( transaction, 'dispute.amount' ) || 0 ) / 100 ) }</p>
+							: '' }
+						<p>Fee: { formatCurrency( ( -transaction.fee || 0 ) / 100 ) }</p>
+						<p>Net: { formatCurrency( ( transaction.net || 0 ) / 100 ) }</p>
+					</div>
+				</div>
+				<div className="transaction-summary__section">
+					{ /* TODO: implement control buttons depending on the transaction status */ }
+					{ /* E.g. if transaction is under dispute display Accept Dispute and Respond to Dispute buttons */ }
+				</div>
+			</div>
 			<hr style={ { margin: '0 -16px' /* Accounting for woocommerce-card__body padding */ } } />
 			<HorizontalList items={ [
 				{ title: 'Date', description: dateI18n( 'M j, Y, g:ia', moment( ( transaction.created || 0 ) * 1000 ) ) },
