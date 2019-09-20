088f3508f2f75fca9150f4b606bd69f87f4f9622
Luiz Reis <luiz.reis@automattic.com>
2019-09-20 15:18:48 -0300

    Add Transaction details to TransactionDetailsSummary

--- client/transaction-details/summary/index.js
+++ client/transaction-details/summary/index.js
@@ -4,10 +4,16 @@
  * External dependencies
  */
 import { Card } from '@woocommerce/components';
+import { dateI18n } from '@wordpress/date';
+import moment from 'moment';
+import { get } from 'lodash';
 
 /**
  * Internal dependencies.
  */
+import OrderLink from '../../components/order-link';
+import CardSummary from '../../components/card-summary';
+import HorizontalList from '../../components/horizontal-list';
 
 const TransactionSummaryDetails = ( props ) => {
 	const { transaction } = props;
@@ -15,6 +21,14 @@ const TransactionSummaryDetails = ( props ) => {
 	return (
 		<Card title="Summary" action={ transaction.id }>
 			Summary details for transaction { transaction.id }.
+			<hr style={ { margin: '0 -16px' /* Accounting for woocommerce-card__body padding */ } } />
+			<HorizontalList items={ [
+				{ title: 'Date', description: dateI18n( 'M j, Y, g:ia', moment( ( transaction.created || 0 ) * 1000 ) ) },
+				{ title: 'Order No.', description: <OrderLink order={ transaction.order } /> },
+				{ title: 'Customer', description: get( transaction, 'billing_details.name' ) || 'Sample customer' },
+				{ title: 'Payment Method', description: <CardSummary card={ get( transaction, 'source.payment_method_details.card' ) } /> },
+				{ title: 'Risk Evaluation', description: get( transaction, 'source.outcome.risk_level' ) || '–' },
+			] } />
 		</Card>
 	);
 };
---
+++ client/transaction-details/summary/test/index.js
@@ -0,0 +1,49 @@
+/** @format */
+/**
+ * External dependencies
+ */
+import { shallow } from 'enzyme';
+
+/**
+ * Internal dependencies
+ */
+import TransactionSummaryDetails from '../';
+
+describe( 'TransactionSummaryDetails', () => {
+    test( 'correctly renders a transaction', () => {
+        const transactionSummaryDetails = renderTransaction( {
+			id: 'txn_38jdHA39KKA',
+			/* Stripe data comes in seconds, instead of the default Date miliseconds */
+			created: Date.parse( 'Sep 19, 2019, 5:24 pm' ) / 1000,
+			order: {
+				number: 45981,
+				url: 'https://automattic.com/',
+			},
+			billing_details: {
+				name: 'Customer Name',
+			},
+			source: {
+				payment_method_details: {
+					card: {
+						brand: 'visa',
+						last4: '4242',
+					},
+				},
+				outcome: {
+					risk_level: 'normal',
+				},
+			},
+		} );
+        expect( transactionSummaryDetails ).toMatchSnapshot();
+    } );
+
+    test( 'renders defaults if transaction object is empty', () => {
+        const transactionSummaryDetails = renderTransaction( {} );
+        expect( transactionSummaryDetails ).toMatchSnapshot();
+    } );
+
+    function renderTransaction( transaction ) {
+        return shallow( <TransactionSummaryDetails transaction={ transaction } /> );
+    }
+} );
+
