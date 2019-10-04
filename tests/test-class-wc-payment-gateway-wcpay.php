e4572bfbf73748ff3d7d1f85c74ae244b0652d38
Luiz Reis <luiz.reis@automattic.com>
2019-10-04 10:00:55 -0300

    Refactor tests helper method

--- tests/test-class-wc-payment-gateway-wcpay.php
+++ tests/test-class-wc-payment-gateway-wcpay.php
@@ -10,6 +10,9 @@
  */
 class WC_Payment_Gateway_WCPay_Test extends WP_UnitTestCase {
 
+	const NO_REQUIREMENTS      = false;
+	const PENDING_REQUIREMENTS = true;
+
 	/**
 	 * System under test.
 	 *
@@ -46,7 +49,9 @@ class WC_Payment_Gateway_WCPay_Test extends WP_UnitTestCase {
 	public function test_get_account_data_no_requirement_property() {
 		$account_with_no_requirement_property = array();
 
-		$this->expect_acccount_to_have_pending_requirements_equals( $account_with_no_requirement_property, false );
+		$pending_requirements = $this->get_pending_requirements( $account_with_no_requirement_property );
+
+		$this->assertEquals( $pending_requirements, self::NO_REQUIREMENTS );
 	}
 
 	/**
@@ -59,7 +64,9 @@ class WC_Payment_Gateway_WCPay_Test extends WP_UnitTestCase {
 			'requirements' => array(),
 		);
 
-		$this->expect_acccount_to_have_pending_requirements_equals( $account_with_empty_requirement_property, false );
+		$pending_requirements = $this->get_pending_requirements( $account_with_empty_requirement_property );
+
+		$this->assertEquals( $pending_requirements, self::NO_REQUIREMENTS );
 	}
 
 	/**
@@ -76,7 +83,9 @@ class WC_Payment_Gateway_WCPay_Test extends WP_UnitTestCase {
 			),
 		);
 
-		$this->expect_acccount_to_have_pending_requirements_equals( $account_with_no_requirements, false );
+		$pending_requirements = $this->get_pending_requirements( $account_with_no_requirements );
+
+		$this->assertEquals( $pending_requirements, self::NO_REQUIREMENTS );
 	}
 
 	/**
@@ -93,7 +102,9 @@ class WC_Payment_Gateway_WCPay_Test extends WP_UnitTestCase {
 			),
 		);
 
-		$this->expect_acccount_to_have_pending_requirements_equals( $account_with_currently_due_requirement, true );
+		$pending_requirements = $this->get_pending_requirements( $account_with_currently_due_requirement );
+
+		$this->assertEquals( $pending_requirements, self::PENDING_REQUIREMENTS );
 	}
 
 	/**
@@ -111,7 +122,9 @@ class WC_Payment_Gateway_WCPay_Test extends WP_UnitTestCase {
 			),
 		);
 
-		$this->expect_acccount_to_have_pending_requirements_equals( $account_with_past_due_requirements, true );
+		$pending_requirements = $this->get_pending_requirements( $account_with_past_due_requirements );
+
+		$this->assertEquals( $pending_requirements, self::PENDING_REQUIREMENTS );
 	}
 
 	/**
@@ -130,25 +143,23 @@ class WC_Payment_Gateway_WCPay_Test extends WP_UnitTestCase {
 			),
 		);
 
-		$this->expect_acccount_to_have_pending_requirements_equals( $account_with_eventually_due_requirements, true );
+		$pending_requirements = $this->get_pending_requirements( $account_with_eventually_due_requirements );
+
+		$this->assertEquals( $pending_requirements, self::PENDING_REQUIREMENTS );
 	}
 
 	/**
 	 * Helper method to wrap common code when setting account mock and pending requirements call.
 	 *
-	 * @param array   $account Account object to be used when testing.
-	 * @param boolean $expected_result Expected result from account_has_pending_requirements call.
+	 * @param array $account Account object to be used when testing.
 	 * @return boolean Result of pending requirements for account provided.
 	 */
-	private function expect_acccount_to_have_pending_requirements_equals( $account, $expected_result ) {
+	private function get_pending_requirements( $account ) {
 		$this->mock_api_client
 			->expects( $this->any() )
 			->method( 'get_account_data' )
 			->will( $this->returnValue( $account ) );
 
-		$pending_requirements = $this->wcpay_gateway->account_has_pending_requirements();
-		$this->assertEquals( $expected_result, $pending_requirements );
-
-		return $pending_requirements;
+		return $this->wcpay_gateway->account_has_pending_requirements( $account );
 	}
 }
