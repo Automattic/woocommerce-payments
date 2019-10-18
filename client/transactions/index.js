5dbdf3062df2a8af9afac5194e3b6f6cc37e780a
Dwain Maralack <dwain.maralack@gmail.com>
2019-10-18 14:29:13 +0200

    Update output to have fees show as negative ($-99.99). If a fee is reversed it will show as positive

--- client/transactions/index.js
+++ client/transactions/index.js
@@ -55,7 +55,8 @@ export const TransactionsList = ( props ) => {
 			email: billing_details && { value: billing_details.email, display: billing_details.email },
 			country: address && { value: address.country, display: address.country },
 			amount: { value: txn.amount / 100, display: formatCurrency( txn.amount / 100 ) },
-			fee: { value: txn.fee / 100, display: '-' + formatCurrency( Math.abs( txn.fee ) / 100 ) },
+			// fees should display as negative. The format $-9.99 is determined by WC-Admin
+			fee: { value: txn.fee / 100, display: formatCurrency( ( txn.fee / 100 ) * -1 ) },
 			net: { value: ( txn.amount - txn.fee ) / 100, display: formatCurrency( ( txn.amount - txn.fee ) / 100 ) },
 			// TODO deposit: { value: available_on * 1000, display: dateI18n( 'Y-m-d H:i', moment( available_on * 1000 ) ) },
 			risk_level: outcome && { value: outcome.risk_level, display: capitalize( outcome.risk_level ) },
