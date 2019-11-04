ab2d69d718b4e5cf11a5515d5c56055c028e842a
Luiz Reis <luiz.reis@automattic.com>
2019-11-04 14:38:55 -0300

    Cache account details call (#252)
    * Cache account details call
    
    Use transients to cache the account details call for 2 hours and clear the transient whenever a login or OAuth link is requested.

--- includes/wc-payment-api/class-wc-payments-api-client.php
+++ includes/wc-payment-api/class-wc-payments-api-client.php
@@ -16,6 +16,8 @@ class WC_Payments_API_Client {
 	const ENDPOINT_SITE_FRAGMENT = 'sites/%s';
 	const ENDPOINT_REST_BASE     = 'wcpay';
 
+	const STRIPE_ACCOUNT_TRANSIENT = 'wcpay_stripe_account';
+
 	const POST = 'POST';
 	const GET  = 'GET';
 
@@ -269,7 +271,17 @@ class WC_Payments_API_Client {
 	 * @return array An array describing an account object.
 	 */
 	public function get_account_data() {
-		return $this->request( array(), self::ACCOUNTS_API . '/' . $this->account_id, self::GET );
+		$account = get_transient( self::STRIPE_ACCOUNT_TRANSIENT );
+
+		if ( false === $account ) {
+			$account = $this->request( array(), self::ACCOUNTS_API . '/' . $this->account_id, self::GET );
+
+			if ( ! empty( $account ) && ! is_wp_error( $account ) ) {
+				set_transient( self::STRIPE_ACCOUNT_TRANSIENT, $account, 2 * HOUR_IN_SECONDS );
+			}
+		}
+
+		return $account;
 	}
 
 	/**
@@ -281,6 +293,9 @@ class WC_Payments_API_Client {
 	 * @return array An array containing the url and state fields
 	 */
 	public function get_oauth_data( $return_url, $business_data = array() ) {
+		// Clear account transient when generating Stripe's oauth data.
+		delete_transient( self::STRIPE_ACCOUNT_TRANSIENT );
+
 		return $this->request(
 			array(
 				'return_url'    => $return_url,
@@ -299,6 +314,9 @@ class WC_Payments_API_Client {
 	 * @return array An array containing the url field
 	 */
 	public function get_login_data( $redirect_url ) {
+		// Clear account transient when generating Stripe dashboard's login link.
+		delete_transient( self::STRIPE_ACCOUNT_TRANSIENT );
+
 		return $this->request(
 			array( 'redirect_url' => $redirect_url ),
 			self::ACCOUNTS_API . '/' . $this->account_id . '/login_links',
