57e718bdab8e19fe6a4c7aed05fdb61a617dfd8f
Daniel Rey López <daniel.rey.lopez@automattic.com>
2019-11-12 12:12:40 +0000

    Put Woocommerce Payments on top of the gateways list by default (#271)
    * Put Woocommerce Payments on top of the gateways list by default
    
    Fixes #263
    
    * Put WCPay on the top of the list even on a fresh install
    
    * Simplified the logic, just stick WCPay at the start of the list even if it ends up with an "-1" position

--- includes/class-wc-payments.php
+++ includes/class-wc-payments.php
@@ -61,6 +61,8 @@ class WC_Payments {
 		self::$api_client->set_account_id( self::$gateway->get_option( 'stripe_account_id' ) );
 
 		add_filter( 'woocommerce_payment_gateways', array( __CLASS__, 'register_gateway' ) );
+		add_filter( 'option_woocommerce_gateway_order', array( __CLASS__, 'set_gateway_top_of_list' ), 2 );
+		add_filter( 'default_option_woocommerce_gateway_order', array( __CLASS__, 'set_gateway_top_of_list' ), 3 );
 
 		// Add admin screens.
 		if ( is_admin() ) {
@@ -337,6 +339,24 @@ class WC_Payments {
 		$gateways[] = self::$gateway;
 
 		return $gateways;
+	}
+
+	/**
+	 * By default, new payment gateways are put at the bottom of the list on the admin "Payments" settings screen.
+	 * For visibility, we want WooCommerce Payments to be at the top of the list.
+	 *
+	 * @param array $ordering Existing ordering of the payment gateways.
+	 *
+	 * @return array Modified ordering.
+	 */
+	public static function set_gateway_top_of_list( $ordering ) {
+		$ordering = (array) $ordering;
+		$id       = self::$gateway->id;
+		// Only tweak the ordering if the list hasn't been reordered with WooCommerce Payments in it already.
+		if ( ! isset( $ordering[ $id ] ) || ! is_numeric( $ordering[ $id ] ) ) {
+			$ordering[ $id ] = empty( $ordering ) ? 0 : ( min( $ordering ) - 1 );
+		}
+		return $ordering;
 	}
 
 	/**
