cdff400b2d14703c2de3fb805a1683d0d0826072
Luiz Reis <luiz.reis@automattic.com>
2020-01-10 18:33:12 -0300

    Update transactions list test

--- client/transactions/test/index.js
+++ client/transactions/test/index.js
@@ -11,73 +11,71 @@ import { TransactionsList } from '../';
 
 describe( 'Transactions list', () => {
 	test( 'renders correctly', () => {
-		const transactions = {
-			data: [
-				{
-					id: 'txn_j23jda9JJa',
-					created: 1572590800,
-					type: 'refund',
-					source: {
-						object: 'refund',
-						charge: {
-							id: 'ch_j23w39dsajda',
-							object: 'charge',
-							// eslint-disable-next-line camelcase
-							payment_method_details: {
-								card: {
-									brand: 'visa',
-								},
-							},
-							// eslint-disable-next-line camelcase
-							billing_details: {
-								name: 'Another customer',
-								email: 'another@customer.com',
-								address: {
-									country: 'US',
-								},
-							},
-							outcome: {
-								// eslint-disable-next-line camelcase
-								risk_level: 'high',
-							},
-						},
-					},
-					amount: 1000,
-					fee: 50,
-					// available_on: 1573199200,
-				},
-				{
-					id: 'txn_oa9kaKaa8',
-					created: 1572580800,
-					type: 'charge',
-					source: {
-						id: 'ch_j239jda',
+		const transactions = [
+			{
+				id: 'txn_j23jda9JJa',
+				created: 1572590800,
+				type: 'refund',
+				source: {
+					object: 'refund',
+					charge: {
+						id: 'ch_j23w39dsajda',
 						object: 'charge',
 						// eslint-disable-next-line camelcase
 						payment_method_details: {
 							card: {
-								brand: 'mastercard',
+								brand: 'visa',
 							},
 						},
 						// eslint-disable-next-line camelcase
 						billing_details: {
-							name: 'My name',
-							email: 'a@b.com',
+							name: 'Another customer',
+							email: 'another@customer.com',
 							address: {
 								country: 'US',
 							},
 						},
 						outcome: {
 							// eslint-disable-next-line camelcase
-							risk_level: 'normal',
+							risk_level: 'high',
 						},
 					},
-					amount: 1500,
-					fee: 50,
-					// available_on: 1573189200,
 				},
-			],
-		};
+				amount: 1000,
+				fee: 50,
+				// available_on: 1573199200,
+			},
+			{
+				id: 'txn_oa9kaKaa8',
+				created: 1572580800,
+				type: 'charge',
+				source: {
+					id: 'ch_j239jda',
+					object: 'charge',
+					// eslint-disable-next-line camelcase
+					payment_method_details: {
+						card: {
+							brand: 'mastercard',
+						},
+					},
+					// eslint-disable-next-line camelcase
+					billing_details: {
+						name: 'My name',
+						email: 'a@b.com',
+						address: {
+							country: 'US',
+						},
+					},
+					outcome: {
+						// eslint-disable-next-line camelcase
+						risk_level: 'normal',
+					},
+				},
+				amount: 1500,
+				fee: 50,
+				// available_on: 1573189200,
+			},
+		];
 
 		const list = shallow(
 			<TransactionsList
