d313fcff894803241ec22d01046fcc95b4300223
Kristófer R <kristofer.thorlaksson@automattic.com>
2020-01-13 16:08:05 +0000

    Add comments to utility functions

--- client/util.js
+++ client/util.js
@@ -9,6 +9,13 @@ import { addQueryArgs } from '@wordpress/url';
  */
 import 'payments-api/payments-data-store'; // For wcpaySettings.
 
+/**
+ * Returns true if WooCommerce Payments is in test mode, false otherwise.
+ *
+ * @param {boolean} fallback Test mode fallback value in case test mode value can't be found.
+ *
+ * @returns {boolean} True if in test mode, false otherwise. Fallback value if test mode value can't be found.
+ */
 export const isInTestMode = ( fallback = false ) => {
 	if ( 'undefined' === typeof wcpaySettings ) {
 		return fallback;
@@ -16,6 +23,11 @@ export const isInTestMode = ( fallback = false ) => {
 	return '1' === wcpaySettings.testMode || fallback;
 };
 
+/**
+ * Returns the URL to the WooCommerce Payments settings.
+ *
+ * @returns {string} URL to the WooCommerce Payments settings menu.
+ */
 export const getPaymentSettingsUrl = () => {
 	return addQueryArgs(
 		'admin.php',
