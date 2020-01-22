7806311d752e3fa99ac244d99b7171a204cabbdc
Valerie <valerie@automattic.com>
2020-01-22 21:13:59 +0900

    Use Timeline component

--- client/payment-details/timeline/index.js
+++ client/payment-details/timeline/index.js
@@ -3,9 +3,18 @@
 /**
  * Internal dependencies.
  */
+import { Card } from '@woocommerce/components';
+import { Timeline } from '@woocommerce/components';
 
-const PaymentDetailsTimeline = () => {
-	return null; // TODO: not implemented yet
+const PaymentDetailsTimeline = ( props ) => {
+	const { charge } = props;
+	// TODO: this is a placeholder card and does not require translation
+	return (
+		<Card title="Timeline">
+			Timeline details for charge { charge.id }.
+			<Timeline />
+		</Card>
+	);
 };
 
 export default PaymentDetailsTimeline;
