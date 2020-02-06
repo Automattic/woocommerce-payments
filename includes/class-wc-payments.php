02c12c8b313fca890dc325e6c9c62515ce09d623
Vasily Belolapotkov <belolapotkov.v@gmail.com>
2020-02-06 16:31:57 +0300

    Fix alert message when Jetpack is installed but not connected

--- includes/class-wc-payments.php
+++ includes/class-wc-payments.php
@@ -239,7 +239,7 @@ class WC_Payments {
 				$set_up_url = wp_nonce_url( 'admin.php?page=jetpack' );
 				$message    = sprintf(
 					/* translators: %1: WooCommerce Payments version, %2: Jetpack setup url */
-					__( 'To use WooCommerce Payments %1$s you\'ll need to <a href="%2$s">set up</a> the Jetpack plugin. Don\'t worry this is temporary thing.', 'woocommerce-payments' ),
+					__( 'To use WooCommerce Payments %1$s you\'ll need to <a href="%2$s">set up</a> the Jetpack plugin.', 'woocommerce-payments' ),
 					WCPAY_VERSION_NUMBER,
 					$set_up_url
 				);
