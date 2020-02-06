5b9cd252848801e05c45030b219d140e71555437
Paul Dechov <dechovsky@gmail.com>
2020-02-05 20:19:25 -0500

    Map dispute status and reason to display strings

--- client/disputes/index.js
+++ client/disputes/index.js
@@ -11,7 +11,6 @@ import moment from 'moment';
 import Currency from '@woocommerce/currency';
 import { TableCard, Link } from '@woocommerce/components';
 import Gridicon from 'gridicons';
-import { capitalize } from 'lodash';
 
 /**
  * Internal dependencies.
@@ -28,6 +27,36 @@ const headers = [
 	{ key: 'created', label: 'Disputed On' },
 	{ key: 'dueBy', label: 'Respond By' },
 ];
+
+// Mapping of dispute status to display string.
+const displayStatus = {
+	warning_needs_response: __( 'Inquiry: Needs response', 'woocommerce-payments' ),
+	warning_under_review: __( 'Inquiry: Under review', 'woocommerce-payments' ),
+	warning_closed: __( 'Inquiry: Closed', 'woocommerce-payments' ),
+	needs_response: __( 'Needs response', 'woocommerce-payments' ),
+	under_review: __( 'Under review', 'woocommerce-payments' ),
+	charge_refunded: __( 'Charge refunded', 'woocommerce-payments' ),
+	won: __( 'Won', 'woocommerce-payments' ),
+	lost: __( 'Lost', 'woocommerce-payments' ),
+};
+
+// Mapping of dispute reason to display string.
+const displayReason = {
+	bank_cannot_process: __( 'Bank cannot process', 'woocommerce-payments' ),
+	check_returned: __( 'Check returned', 'woocommerce-payments' ),
+	credit_not_processed: __( 'Credit not processed', 'woocommerce-payments' ),
+	customer_initiated: __( 'Customer initiated', 'woocommerce-payments' ),
+	debit_not_authorized: __( 'Debit not authorized', 'woocommerce-payments' ),
+	duplicate: __( 'Duplicate', 'woocommerce-payments' ),
+	fraudulent: __( 'Fraudulent', 'woocommerce-payments' ),
+	general: __( 'General', 'woocommerce-payments' ),
+	incorrect_account_details: __( 'Incorrect account details', 'woocommerce-payments' ),
+	insufficient_funds: __( 'Insufficient funds', 'woocommerce-payments' ),
+	product_not_received: __( 'Product not received', 'woocommerce-payments' ),
+	product_unacceptable: __( 'Product unacceptable', 'woocommerce-payments' ),
+	subscription_canceled: __( 'Subscription canceled', 'woocommerce-payments' ),
+	unrecognized: __( 'Unrecognized', 'woocommerce-payments' ),
+};
 
 export const DisputesList = ( props ) => {
 	const { disputes, showPlaceholder } = props;
@@ -49,9 +78,9 @@ export const DisputesList = ( props ) => {
 			amount: { value: dispute.amount / 100, display: currency.formatCurrency( dispute.amount / 100 ) },
 			status: {
 				value: dispute.status,
-				display: <>{ evidenceLink } <code>{ capitalize( dispute.status.replace( /_/g, ' ' ) ) }</code></>,
+				display: <>{ evidenceLink } <code>{ displayStatus[ dispute.status ] }</code></>,
 			},
-			reason: { value: dispute.reason, display: capitalize( dispute.reason.replace( /_/g, ' ' ) ) },
+			reason: { value: dispute.reason, display: displayReason[ dispute.reason ] },
 			created: { value: dispute.created * 1000, display: dateI18n( 'M j, Y / g:iA', moment( dispute.created * 1000 ) ) },
 			dueBy: {
 				value: dispute.evidence_details.due_by * 1000,
