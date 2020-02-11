41293c6c810e6c95436a0d1b16d34eae8af251f5
Vasily Belolapotkov <belolapotkov.v@gmail.com>
2020-02-11 14:57:27 +0300

    Refactor evidence form tests
    - extract and reuse test disputes objects
    - change the order of test casees for submit confirmation

--- client/disputes/evidence/test/index.js
+++ client/disputes/evidence/test/index.js
@@ -9,30 +9,43 @@ import { shallow, mount } from 'enzyme';
  */
 import { DisputeEvidenceForm } from '../';
 
+/* eslint-disable camelcase */
+const disputeNeedsResponse = {
+	id: 'dp_asdfghjkl',
+	amount: 1000,
+	created: 1572590800,
+	evidence: {
+		customer_purchase_ip: '127.0.0.1',
+		uncategorized_text: '',
+	},
+	evidence_details: {
+		due_by: 1573199200,
+	},
+	reason: 'fraudulent',
+	status: 'needs_response',
+};
+
+const disputeNoNeedForResponse = {
+	id: 'dp_zxcvbnm',
+	amount: 1050,
+	created: 1572480800,
+	evidence: {
+		customer_purchase_ip: '127.0.0.1',
+		uncategorized_text: 'winning_evidence',
+	},
+	evidence_details: {
+		due_by: 1573099200,
+	},
+	reason: 'general',
+	status: 'under_review',
+};
+/* eslint-enable camelcase */
+
 describe( 'Dispute evidence form', () => {
 	test( 'needing response, renders correctly', () => {
-        const dispute = {
-            id: 'dp_asdfghjkl',
-            amount: 1000,
-            created: 1572590800,
-            evidence: {
-                // eslint-disable-next-line camelcase
-                customer_purchase_ip: '127.0.0.1',
-                // eslint-disable-next-line camelcase
-                uncategorized_text: '',
-            },
-            // eslint-disable-next-line camelcase
-            evidence_details: {
-                // eslint-disable-next-line camelcase
-                due_by: 1573199200,
-            },
-            reason: 'fraudulent',
-            status: 'needs_response',
-        };
-
 		const form = shallow(
 			<DisputeEvidenceForm
-                evidence={ dispute.evidence }
+                evidence={ disputeNeedsResponse.evidence }
                 showPlaceholder={ false }
                 readOnly={ false }
 			/>
@@ -41,30 +54,11 @@ describe( 'Dispute evidence form', () => {
     } );
 
 	test( 'not needing response, renders correctly', () => {
-        const dispute = {
-            id: 'dp_zxcvbnm',
-            amount: 1050,
-            created: 1572480800,
-            evidence: {
-                // eslint-disable-next-line camelcase
-                customer_purchase_ip: '127.0.0.1',
-                // eslint-disable-next-line camelcase
-                uncategorized_text: 'winning_evidence',
-            },
-            // eslint-disable-next-line camelcase
-            evidence_details: {
-                // eslint-disable-next-line camelcase
-                due_by: 1573099200,
-            },
-            reason: 'general',
-            status: 'under_review',
-        };
-
-		const form = shallow(
+        const form = shallow(
 			<DisputeEvidenceForm
-                evidence={ dispute.evidence }
-                showPlaceholder={ false }
-                readOnly={ false }
+				evidence={ disputeNoNeedForResponse.evidence }
+				showPlaceholder={ false }
+				readOnly={ false }
 			/>
 		);
 		expect( form ).toMatchSnapshot();
@@ -72,34 +66,17 @@ describe( 'Dispute evidence form', () => {
 
 	test( 'confirmation requested on submit', () => {
 		window.confirm = jest.fn();
-		const dispute = {
-			id: 'dp_asdfghjkl',
-			amount: 1000,
-			created: 1572590800,
-			evidence: {
-				// eslint-disable-next-line camelcase
-				customer_purchase_ip: '127.0.0.1',
-				// eslint-disable-next-line camelcase
-				uncategorized_text: '',
-			},
-			// eslint-disable-next-line camelcase
-			evidence_details: {
-				// eslint-disable-next-line camelcase
-				due_by: 1573199200,
-			},
-			reason: 'fraudulent',
-			status: 'needs_response',
-		};
 
 		// We have to mount component to select button for click.
 		const form = mount(
 			<DisputeEvidenceForm
-				evidence={ dispute.evidence }
+				evidence={ disputeNeedsResponse.evidence }
 				showPlaceholder={ false }
 				readOnly={ false }
 				onSave={ jest.fn() }
 			/>
 		);
+
 		const submitButton = form.find( 'button.is-primary' );
 		submitButton.simulate( 'click' );
 		expect( window.confirm ).toHaveBeenCalledTimes( 1 );
@@ -110,48 +87,29 @@ describe( 'Dispute evidence form', () => {
 	);
 
 	test( 'onSave called after confirmation only', () => {
-		const dispute = {
-			id: 'dp_asdfghjkl',
-			amount: 1000,
-			created: 1572590800,
-			evidence: {
-				// eslint-disable-next-line camelcase
-				customer_purchase_ip: '127.0.0.1',
-				// eslint-disable-next-line camelcase
-				uncategorized_text: '',
-			},
-			// eslint-disable-next-line camelcase
-			evidence_details: {
-				// eslint-disable-next-line camelcase
-				due_by: 1573199200,
-			},
-			reason: 'fraudulent',
-			status: 'needs_response',
-		};
-
 		const onSave = jest.fn();
 
 		// We have to mount component to select button for click.
 		const form = mount(
 			<DisputeEvidenceForm
-				evidence={ dispute.evidence }
-				showPlaceholder = { false }
-				readOnly = { false }
-				onSave = { onSave }
+				evidence={ disputeNeedsResponse.evidence }
+				showPlaceholder={ false }
+				readOnly={ false }
+				onSave={ onSave }
 			/>
 		);
 		const submitButton = form.find( 'button.is-primary' );
 
 		window.confirm = jest.fn();
 		window.confirm
-		.mockReturnValueOnce( true )
-		.mockReturnValueOnce( false );
+		.mockReturnValueOnce( false )
+		.mockReturnValueOnce( true );
+
+		// Test not confirmed case.
+		submitButton.simulate( 'click' );
+		expect( onSave ).toHaveBeenCalledTimes( 0 );
 
 		// Test confirmed case.
-		submitButton.simulate( 'click' );
-		expect( onSave ).toHaveBeenCalledTimes( 1 );
-
-		// Test cancelled case.
 		submitButton.simulate( 'click' );
 		expect( onSave ).toHaveBeenCalledTimes( 1 );
 	} );
