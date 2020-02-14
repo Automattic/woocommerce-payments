45afa0b6df02c544d626bbd01f747e1ee5bc1075
Kristófer R <kristofer.thorlaksson@automattic.com>
2020-02-14 22:37:02 +0000

    Move file upload control to its own file

---
+++ client/disputes/evidence/file-upload.js
@@ -0,0 +1,62 @@
+/**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+// This is all coming from a global '@wordpress/components' import, and will probably
+// cause issues down the line if the global scope is updated.
+// For example the 'IconButton' is deprecated in the latest version, and styles are
+// applied in a different way.
+import { BaseControl, FormFileUpload, IconButton } from '@wordpress/components';
+import Gridicon from 'gridicons';
+
+export const FileUploadControl = ( evidence, field, onFileChange, onFileRemove ) => {
+	const fileName = ( evidence.metadata && evidence.metadata[ field.key ] ) || '';
+	const isDone = 0 !== fileName.length;
+	const isLoading = evidence.isUploading && ( evidence.isUploading[ field.key ] || false );
+	const hasError = evidence.uploadingErrors && ( evidence.uploadingErrors[ field.key ] || false );
+
+	const getIcon = () => {
+		if ( isDone && ! hasError ) {
+			return <Gridicon icon="checkmark" size={ 18 } />;
+		}
+		return <Gridicon icon="add-outline" size={ 18 } />;
+	};
+
+	const message = hasError ? __( 'Upload failed.', 'woocommerce-payments' ) : fileName;
+	const messageClass = hasError ? 'is-destructive' : null;
+
+	return (
+		<BaseControl
+			key={ field.key }
+			id={ 'form-file-upload-base-control-' + field.key }
+			label={ field.display }
+			help={ field.description }
+		>
+			<div className={ 'file-upload' }>
+				<FormFileUpload
+					id={ 'form-file-upload-' + field.key }
+					className={ isDone && ! hasError ? 'is-success' : null }
+					isLarge
+					isPrimary
+					isDestructive={ hasError }
+					isBusy={ isLoading }
+					disabled={ isLoading }
+					icon={ getIcon() }
+					accept=".pdf, image/png, image/jpeg"
+					onChange={ ( event ) => onFileChange( field.key, event.target.files[ 0 ] ) }
+				>
+					{ __( 'Upload File', 'woocommerce-payments' ) }
+				</FormFileUpload>
+
+				<span className={ messageClass }>{ message }</span>
+
+				{ isDone
+					? <IconButton
+						className={ 'delete-uploaded-file-button' }
+						icon={ <Gridicon icon="trash" size={ 18 } /> }
+						onClick={ () => onFileRemove( field.key ) } />
+					: null }
+			</div>
+		</BaseControl>
+	);
+};
--- client/disputes/evidence/index.js
+++ client/disputes/evidence/index.js
@@ -10,9 +10,10 @@ import apiFetch from '@wordpress/api-fetch';
 // cause issues down the line if the global scope is updated.
 // For example the 'IconButton' is deprecated in the latest version, and styles are
 // applied in a different way.
-import { Button, TextControl, TextareaControl, BaseControl, FormFileUpload, IconButton } from '@wordpress/components';
+import { Button, TextControl, TextareaControl } from '@wordpress/components';
 import { Section, Card } from '@woocommerce/components';
-import Gridicon from 'gridicons';
+
+import { FileUploadControl } from './file-upload';
 
 /**
  * Internal dependencies.
@@ -21,58 +22,6 @@ import '../style.scss';
 import evidenceFields from './fields';
 import Page from '../../components/page';
 import CardFooter from '../../components/card-footer';
-
-const FileUploadControl = ( evidence, field, onFileChange, onFileRemove ) => {
-	const fileName = ( evidence.metadata && evidence.metadata[ field.key ] ) || '';
-	const isDone = 0 !== fileName.length;
-	const isLoading = evidence.isUploading && ( evidence.isUploading[ field.key ] || false );
-	const hasError = evidence.uploadingErrors && ( evidence.uploadingErrors[ field.key ] || false );
-
-	const getIcon = () => {
-		if ( isDone && ! hasError ) {
-			return <Gridicon icon="checkmark" size={ 18 } />;
-		}
-		return <Gridicon icon="add-outline" size={ 18 } />;
-	};
-
-	const message = hasError ? __( 'Upload failed.', 'woocommerce-payments' ) : fileName;
-	const messageClass = hasError ? 'is-destructive' : null;
-
-	return (
-		<BaseControl
-			key={ field.key }
-			id={ 'form-file-upload-base-control-' + field.key }
-			label={ field.display }
-			help={ field.description }
-		>
-			<div className={ 'file-upload' }>
-				<FormFileUpload
-					id={ 'form-file-upload-' + field.key }
-					className={ isDone && ! hasError ? 'is-success' : null }
-					isLarge
-					isPrimary
-					isDestructive={ hasError }
-					isBusy={ isLoading }
-					disabled={ isLoading }
-					icon={ getIcon() }
-					accept=".pdf, image/png, image/jpeg"
-					onChange={ ( event ) => onFileChange( field.key, event.target.files[ 0 ] ) }
-				>
-					{ __( 'Upload File', 'woocommerce-payments' ) }
-				</FormFileUpload>
-
-				<span className={ messageClass }>{ message }</span>
-
-				{ isDone
-					? <IconButton
-						className={ 'delete-uploaded-file-button' }
-						icon={ <Gridicon icon="trash" size={ 18 } /> }
-						onClick={ () => onFileRemove( field.key ) } />
-					: null }
-			</div>
-		</BaseControl>
-	);
-};
 
 export const DisputeEvidenceForm = props => {
 	const {
