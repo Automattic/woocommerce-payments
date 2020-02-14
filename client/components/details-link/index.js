13118c6cbc9b2ffb573c1811db4adf2d12d1bdb4
Paul Dechov <dechovsky@gmail.com>
2020-02-14 09:16:21 -0500

    Generalize DetailsLink and move to its own component module

--- client/components/details-link/index.js
+++ client/components/details-link/index.js
@@ -7,23 +7,23 @@ import Gridicon from 'gridicons';
 import { addQueryArgs } from '@wordpress/url';
 import { Link } from '@woocommerce/components';
 
-const DetailsLink = ( { chargeId } ) => {
+const DetailsLink = ( { id, parentSegment } ) => {
 	// TODO: come up with a link generator utility (woocommerce-payments#229)
 
 	const detailsUrl = addQueryArgs(
 		'admin.php',
 		{
 			page: 'wc-admin',
-			path: '/payments/transactions/details',
-			id: chargeId,
+			path: `/payments/${ parentSegment }/details`,
+			id,
 		}
 	);
 
-	return chargeId ? (
-		<Link className="transactions-list__details-button" href={ detailsUrl } >
+	return id ? (
+		<Link href={ detailsUrl } >
 			<Gridicon icon="info-outline" size={ 18 } />
 		</Link>
-	) : '';
+	) : null;
 };
 
 export default DetailsLink;
