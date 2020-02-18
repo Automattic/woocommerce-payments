234fe5a585f85d8abae1da4def0f94e9e4753565
Paul Dechov <dechovsky@gmail.com>
2020-02-17 21:49:21 -0500

    Add snapshot test for DepositsList

---
+++ client/deposits/test/index.js
@@ -0,0 +1,53 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+import { shallow } from 'enzyme';
+
+/**
+ * Internal dependencies
+ */
+import { DepositsList } from '../';
+import { useDeposits } from '../../data';
+
+jest.mock( '../../data', () => ( {
+	useDeposits: jest.fn(),
+} ) );
+
+const mockDeposits = [
+	{
+		id: 'po_mock1',
+		date: '2020-01-02 17:46:02',
+		type: 'deposit',
+		amount: 2000,
+		status: 'paid',
+		bankAccount: 'MOCK BANK •••• 1234 (USD)',
+	},
+	{
+		id: 'po_mock2',
+		date: '2020-01-03 17:46:02',
+		type: 'withdrawal',
+		amount: 3000,
+		status: 'pending',
+		bankAccount: 'MOCK BANK •••• 1234 (USD)',
+	}
+];
+
+describe( 'Deposits list', () => {
+	beforeEach( () => {
+		jest.clearAllMocks();
+	} );
+
+	test( 'renders correctly', () => {
+		useDeposits.mockReturnValue( {
+			deposits: mockDeposits,
+			isLoading: false,
+		} );
+
+		const list = shallow(
+			<DepositsList />
+		);
+		expect( list ).toMatchSnapshot();
+	} );
+} );
