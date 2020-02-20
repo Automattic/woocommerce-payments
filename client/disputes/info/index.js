a5a9cc108b6e544190dd0083897c338c20f1a3fc
Paul Dechov <dechovsky@gmail.com>
2020-02-20 13:43:59 -0500

    Add charge-related fields to dispute info

--- client/disputes/info/index.js
+++ client/disputes/info/index.js
@@ -7,11 +7,13 @@ import { __ } from '@wordpress/i18n';
 import { dateI18n } from '@wordpress/date';
 import moment from 'moment';
 import Currency from '@woocommerce/currency';
+import { Link } from '@woocommerce/components';
 
 /**
  * Internal dependencies.
  */
 import OrderLink from 'components/order-link';
+import { getDetailsURL } from 'components/details-link';
 import { reasons } from '../strings';
 import { formatStringValue } from '../../util';
 import './style.scss';
@@ -32,12 +34,19 @@ const Info = ( { dispute } ) => {
 	const reasonMapping = reasons[ dispute.reason ];
 	const reasonDisplay = reasonMapping ? reasonMapping.display : formatStringValue( dispute.reason );
 
+	const chargeId = typeof dispute.charge === 'object' ? dispute.charge.id : dispute.charge;
+	const transactionIdDisplay = (
+		<Link href={ getDetailsURL( chargeId, 'transactions' ) } >{ chargeId }</Link>
+	);
+
 	const data = {
 		created: dateI18n( 'M j, Y', moment( dispute.created * 1000 ) ),
 		amount: `${ currency.formatCurrency( dispute.amount / 100 ) } ${ currency.code }`,
 		dueBy: dateI18n( 'M j, Y - g:iA', moment( dispute.evidence_details.due_by * 1000 ) ),
 		reason: reasonDisplay,
 		order: dispute.order ? <OrderLink order={ dispute.order } /> : null,
+		customer: typeof dispute.charge === 'object' ? dispute.charge.billing_details.name : null,
+		transactionId: transactionIdDisplay,
 	};
 
 	return (
--- client/disputes/info/test/index.js
+++ client/disputes/info/test/index.js
@@ -15,7 +15,9 @@ describe( 'Dispute info', () => {
 		const dispute = {
 			amount: 1000,
 			created: 1572590800,
+			// eslint-disable-next-line camelcase
 			evidence_details: {
+				// eslint-disable-next-line camelcase
 				due_by: 1573199200,
 			},
 			reason: 'fraudulent',
@@ -23,6 +25,13 @@ describe( 'Dispute info', () => {
 			order: {
 				number: '1',
 				url: 'http://test.local/order/1',
+			},
+			charge: {
+				id: 'ch_mock',
+				// eslint-disable-next-line camelcase
+				billing_details: {
+					name: 'Mock Customer',
+				},
 			},
 		};
 
