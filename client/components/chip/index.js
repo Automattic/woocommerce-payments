019ec9401a2d5c4b4b3b2a7c036ee708058c1061
Paul Dechov <dechovsky@gmail.com>
2020-02-20 18:53:34 -0500

    Compact chip variant for list view

--- client/components/chip/index.js
+++ client/components/chip/index.js
@@ -5,11 +5,19 @@
  */
 import './style.scss';
 
+const types = [ 'primary', 'light', 'alert' ];
+
 const Chip = ( props ) => {
-	const { message, type } = props;
-	const types = [ 'primary', 'light', 'alert' ];
+	const { message, type, isCompact } = props;
+
+	const classNames = [
+		'chip',
+		`chip-${ types.find( t => t === type ) || 'primary' }`,
+		isCompact ? 'is-compact' : '',
+	];
+
 	return (
-		<span className={ `chip chip-${ types.find( t => t === type ) || 'primary' }` }>
+		<span className={ classNames.join( ' ' ).trim() }>
 			{ message }
 		</span>
 	);
