4c776468eb9ba31a9012b14b38b2d03d6312b174
Vasily Belolapotkov <belolapotkov.v@gmail.com>
2020-02-21 17:10:07 +0300

    Update evidence upload errors handling
    The WCPayments Server does not transfer reason of upload failure to
    WCPayments Client at the moment. Sending generic  error message to the
    client as a workaround.

--- includes/admin/class-wc-rest-payments-disputes-controller.php
+++ includes/admin/class-wc-rest-payments-disputes-controller.php
@@ -129,7 +129,12 @@ class WC_REST_Payments_Disputes_Controller extends WP_REST_Controller {
 			$response = $this->api_client->upload_evidence( $request );
 			return rest_ensure_response( $response );
 		} catch ( WC_Payments_API_Exception $e ) {
-			return new WP_Error( $e->get_http_code(), $e->getMessage() );
+			$http_code = $e->get_http_code();
+			// TODO: Update when more informative errors are sent by the server.
+			$message = 400 === $http_code
+				? $e->getMessage()
+				: __( 'Upload failed.', 'woocommerce-payments' );
+			return new WP_Error( $http_code, $message );
 		}
 	}
 
