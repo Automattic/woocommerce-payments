8cde1842c559ad8428cb85048a280744f7c641a5
Daniel Rey López <daniel.rey.lopez@gmail.com>
2020-02-24 21:54:12 +0000

    Remove the transaction timeline card since it's not implemented, set the transaction details cards to 1032 width

--- client/payment-details/index.js
+++ client/payment-details/index.js
@@ -9,12 +9,13 @@ import PaymentDetailsTimeline from './timeline';
 import PaymentDetailsPayment from './payment';
 import PaymentDetailsPaymentMethod from './payment-method';
 import PaymentDetailsSession from './session';
+import Page from 'components/page';
 
 const PaymentDetails = ( props ) => {
 	const chargeId = props.query.id;
 	const { charge } = useCharge( chargeId );
 	return (
-		<div className="wcpay-payment-details">
+		<Page maxWidth={ 1032 } className="wcpay-payment-details">
 			<PaymentDetailsSummary charge={ charge }></PaymentDetailsSummary>
 			<PaymentDetailsTimeline charge={ charge }></PaymentDetailsTimeline>
 			{ // Hidden for the beta.
@@ -22,7 +23,7 @@ const PaymentDetails = ( props ) => {
 			<PaymentDetailsPaymentMethod charge={ charge }></PaymentDetailsPaymentMethod>
 			{ // Hidden for the beta.
 				false && <PaymentDetailsSession charge={ charge }></PaymentDetailsSession> }
-		</div>
+		</Page>
 	);
 };
 
