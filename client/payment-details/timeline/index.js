5d723c3d760116826c4af15580778808d85ecaa1
Allen Snook <allen@allendav.com>
2020-03-16 16:59:58 -0700

    Add dummy data to validate prop shape with WIP component

--- client/payment-details/timeline/index.js
+++ client/payment-details/timeline/index.js
@@ -8,11 +8,39 @@ import { Timeline } from '@woocommerce/components';
 
 const PaymentDetailsTimeline = ( props ) => {
 	const { charge } = props;
+
+	// Dummy data for validating approach, work in progress
+	// TODO: Synthesize what timeline events we can from the charge object OR
+	// fetch them from an events endpoint
+	const items = [];
+	items.push(
+		{
+			datetime: 1603900800,
+			gridicon: 'checkmark',
+			headline: 'A payment of $97.28 was successfully charged',
+			body: [
+				'Fee: $3.12 (2.9% + $0.30)',
+				'Net deposit: $94.16',
+			],
+		},
+		{
+			datetime: 1603900800,
+			gridicon: 'plus-small',
+			headline: '$94.16 was added to your <a href="">October 29, 2019</a> deposit',
+			body: [],
+		},
+		{
+			datetime: 1603854540,
+			gridicon: 'checkmark',
+			headline: 'A payment of $97.28 was successfully authorized',
+			body: [],
+		},
+	);
+
 	// TODO: this is a placeholder card and does not require translation
 	return (
 		<Card title="Timeline">
-			Timeline details for charge { charge.id }.
-			<Timeline />
+			<Timeline items={ items }/>
 		</Card>
 	);
 };
