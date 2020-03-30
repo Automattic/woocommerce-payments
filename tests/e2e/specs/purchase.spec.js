d262631e45825fb9be717bd5d0a15896edce0897
Vasily Belolapotkov <belolapotkov.v@gmail.com>
2020-03-30 16:14:09 +0300

    Create successful purchase e2e test

---
+++ tests/e2e/specs/purchase.spec.js
@@ -0,0 +1,45 @@
+/* global page */
+/**
+ * External dependencies
+ */
+import config from 'config';
+
+/**
+ * Internal dependencies
+ */
+import { CustomerFlow, uiUnblocked } from '../utils';
+import { fillCardDetails } from '../utils/payments';
+
+const TIMEOUT = 100000;
+
+describe( 'Successfull purchase', () => {
+	beforeAll( async () => {
+		// Increase default value to avoid test failing due to timeouts.
+		page.setDefaultTimeout( 20000 );
+		await page.goto( config.get( 'url' ), { waitUntil: 'networkidle0' } );
+	} );
+
+	test( 'successfull purchase', async () => {
+		await CustomerFlow.goToShop();
+		await CustomerFlow.addToCartFromShopPage( 'Beanie' );
+		await CustomerFlow.goToCheckout();
+		await uiUnblocked();
+		await CustomerFlow.fillBillingDetails(
+			config.get( 'addresses.customer.billing' )
+		);
+		await uiUnblocked();
+		await expect( page ).toClick( '.wc_payment_method.payment_method_woocommerce_payments' );
+		const card = {
+			number: '4242424242424242',
+			expires: {
+				month: '02',
+				year: '24',
+			},
+			cvc: '424',
+		};
+		await fillCardDetails( page, card );
+		await CustomerFlow.placeOrder();
+		await expect( page ).toMatch( 'Order received' );
+	}, TIMEOUT );
+} );
+
