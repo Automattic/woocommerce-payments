8e663675902d465199243be239c04e2d1e48ece1
James Rodger <james.rodger@automattic.com>
2020-04-02 10:31:33 +0100

    Remove unused variable and fix comment typo

--- includes/admin/class-wc-payments-rest-controller.php
+++ includes/admin/class-wc-payments-rest-controller.php
@@ -45,7 +45,6 @@ class WC_Payments_REST_Controller extends WP_REST_Controller {
 	 * @return WP_Error|mixed - Method result of WP_Error in case of WC_Payments_API_Exception.
 	 */
 	public function forward_request( $api_method, $args, $err_code = '' ) {
-		$response;
 		try {
 			$response = call_user_func_array( [ $this->api_client, $api_method ], $args );
 		} catch ( WC_Payments_API_Exception $e ) {
@@ -59,7 +58,7 @@ class WC_Payments_REST_Controller extends WP_REST_Controller {
 	/**
 	 * Verify access.
 	 *
-	 * Override this method if custom perimissions required.
+	 * Override this method if custom permissions required.
 	 */
 	public function check_permission() {
 		return current_user_can( 'manage_woocommerce' );
