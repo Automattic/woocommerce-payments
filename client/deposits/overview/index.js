c7beb00813674602f2c57dad10d3c23ca44ff216
Luiz Reis <luiz.reis@automattic.com>
2020-04-06 16:00:13 -0300

    Use store data to fill DepositsOverview

--- client/deposits/overview/index.js
+++ client/deposits/overview/index.js
@@ -1,54 +1,107 @@
 /**
  * External dependencies
  */
-import { __ } from '@wordpress/i18n';
-import { SummaryList, SummaryNumber } from '@woocommerce/components';
+import { __, sprintf, _n } from '@wordpress/i18n';
+import { dateI18n } from '@wordpress/date';
+import moment from 'moment';
+import { SummaryListPlaceholder, SummaryList, SummaryNumber } from '@woocommerce/components';
 import Gridicon from 'gridicons';
+import Currency from '@woocommerce/currency';
 
 /**
  * Internal dependencies
  */
 import './style.scss';
+import { useDepositsOverview } from 'data';
+import Loadable from 'components/loadable';
+
+const currency = new Currency();
+const formatDate = ( date ) => dateI18n( 'F j, Y', moment.utc( date ) );
+const getAmount = ( obj ) => currency.formatCurrency( ( obj ? obj.amount : 0 ) / 100 );
+const getDepositDate = ( deposit ) => deposit ? formatDate( deposit.date ) : '–';
+const getBalanceDepositCount = ( balance ) =>
+	sprintf( _n( '%d deposit', '%d deposits', balance.deposits_count, 'woocommerce-payments' ), balance.deposits_count );
+const getNextDepositLabelFormatted = ( deposit ) => {
+	const baseLabel = ( deposit ) ? `${ __( 'Est.', 'woocommerce-payments' ) } ${ dateI18n( 'M j, Y', moment.utc( deposit.date ) ) }` : '';
+	if ( deposit && 'in_transit' === deposit.status ) {
+		return `${ baseLabel } - ${ __( 'In transit', 'woocommerce-payments' ) }`;
+	}
+	return baseLabel;
+};
+const getDepositScheduleFormatted = ( schedule ) => {
+	switch ( schedule.interval ) {
+		case 'manual':
+			return __( 'Manual', 'woocommerce-payments' );
+		case 'daily':
+			return __( 'Automatic, every business day', 'woocommerce-payments' );
+		case 'weekly':
+			return sprintf(
+				/** translators: %s day of the week e.g. Monday */
+				__( 'Automatic, every week on %s', 'woocommerce-payments' ),
+				// moment().day() is locale based when using strings. Since Stripe's response is in English,
+				// we need to temporarily set the locale to add the day before formatting
+				moment().locale( 'en' ).day( schedule.weekly_anchor ).locale( moment.locale() ).format( 'dddd' )
+			);
+		case 'monthly':
+			// If locale is set up as en_US or en_GB the ordinal will not show up
+			// More details can be found in https://github.com/WordPress/gutenberg/issues/15221/
+			// Using 'en' as the locale should be enough to workaround it
+			// TODO: Remove workaround when issue is resolved
+			const fixedLocale = moment.locale().startsWith( 'en' ) ? 'en' : moment.locale();
+			return sprintf(
+				/** translators: %s day of the month */
+				__( 'Automatic, every month on the %s', 'woocommerce-payments' ),
+				moment().locale( fixedLocale ).date( schedule.monthly_anchor ).format( 'Do' )
+			);
+	}
+};
 
 const DepositsOverview = () => {
+	const { overview, isLoading } = useDepositsOverview();
 	return (
 		<div className="wcpay-deposits-overview">
 			<p className="wcpay-deposits-overview__schedule">
 				<Gridicon icon="calendar" className="wcpay-deposits-overview__schedule-icon" />
 				<span className="wcpay-deposits-overview__schedule-label">{ __( 'Deposit Schedule:', 'woocommerce-payments' ) }</span>
 				{ ' ' }
-				<span className="wcpay-deposits-overview__schedule-value">Automatic, every business day</span>
+				<span className="wcpay-deposits-overview__schedule-value">
+					<Loadable isLoading={ isLoading } display="inline" placeholder="Deposit Schedule placeholder">
+						{ overview ? getDepositScheduleFormatted( overview.deposit_schedule ) : '' }
+					</Loadable>
+				</span>
 			</p>
-			<SummaryList label={ __( 'Deposits Overview', 'woocommerce-payments' ) }>
-				{ () => {
-					return [
-						<SummaryNumber
-							key="lastDeposit"
-							value={ '$65.74' }
-							label={ __( 'Last deposit', 'woocommerce-payments' ) }
-							prevLabel="April 18, 2019"
-						/>,
-						<SummaryNumber
-							key="nextDeposit"
-							value={ '$15.22' }
-							label={ __( 'Next deposit', 'woocommerce-payments' ) }
-							prevLabel="Est. April 18, 2019 - In transit"
-						/>,
-						<SummaryNumber
-							key="pendingBalance"
-							value={ '$48.66' }
-							label={ __( 'Pending balance', 'woocommerce-payments' ) }
-							prevLabel="2 deposits"
-						/>,
-						<SummaryNumber
-							key="availableBalance"
-							value={ '$0.00' }
-							label={ __( 'Available balance', 'woocommerce-payments' ) }
-							prevLabel=""
-						/>,
-					];
-				} }
-			</SummaryList>
+			{ isLoading
+				? <SummaryListPlaceholder numberOfItems={ 4 } />
+				: <SummaryList label={ __( 'Deposits Overview', 'woocommerce-payments' ) }>
+					{ () => {
+						return [
+							<SummaryNumber
+								key="lastDeposit"
+								label={ __( 'Last deposit', 'woocommerce-payments' ) }
+								value={ getAmount( overview.last_deposit ) }
+								prevLabel={ getDepositDate( overview.last_deposit ) }
+							/>,
+							<SummaryNumber
+								key="nextDeposit"
+								label={ __( 'Next deposit', 'woocommerce-payments' ) }
+								value={ getAmount( overview.next_deposit ) }
+								prevLabel={ getNextDepositLabelFormatted( overview.next_deposit ) }
+							/>,
+							<SummaryNumber
+								key="pendingBalance"
+								label={ __( 'Pending balance', 'woocommerce-payments' ) }
+								value={ getAmount( overview.balance.pending ) }
+								prevLabel={ getBalanceDepositCount( overview.balance.pending ) }
+							/>,
+							<SummaryNumber
+								key="availableBalance"
+								label={ __( 'Available balance', 'woocommerce-payments' ) }
+								value={ getAmount( overview.balance.available ) }
+								prevLabel=""
+							/>,
+						];
+					} }
+				</SummaryList> }
 		</div>
 	);
 };
--- client/deposits/overview/test/index.js
+++ client/deposits/overview/test/index.js
@@ -3,15 +3,128 @@
  */
 import React from 'react';
 import { render } from '@testing-library/react';
+import { merge } from 'lodash';
+import momentLib from 'moment';
 
 /**
  * Internal dependencies
  */
 import DepositsOverview from '../';
+import { useDepositsOverview } from 'data';
+
+jest.mock( 'data', () => ( { useDepositsOverview: jest.fn() } ) );
+const mockUseDepositsOverview = ( overview, isLoading = false ) =>
+	useDepositsOverview.mockReturnValue( {
+		overview: overview,
+		isLoading: isLoading,
+	} );
+
+const getMockedOverview = ( additionalData ) => merge(
+	{
+		// eslint-disable-next-line camelcase
+		last_deposit: {
+			amount: 6574,
+			date: new Date( '2019-04-18' ).getTime(),
+		},
+		// eslint-disable-next-line camelcase
+		next_deposit: {
+			amount: 1522,
+			date: new Date( '2019-04-22' ).getTime(),
+		},
+		balance: {
+			pending: {
+				amount: 4866,
+				// eslint-disable-next-line camelcase
+				deposits_count: 2,
+			},
+			available: {
+				amount: 0,
+			},
+		},
+		// eslint-disable-next-line camelcase
+		deposit_schedule: {
+			interval: 'daily',
+		},
+	},
+	additionalData
+);
 
 describe( 'Deposits Overview', () => {
+	afterEach( () => {
+		jest.clearAllMocks();
+	} );
+
 	test( 'renders correctly', () => {
+		mockUseDepositsOverview( getMockedOverview() );
 		const { container } = render( <DepositsOverview /> );
 		expect( container ).toMatchSnapshot();
 	} );
+
+	test( 'renders deposit schedule for manual interval', () => {
+		const depositSchedule = getDepositSchedule( { interval: 'manual' } );
+		expect( depositSchedule ).toEqual( 'Deposit Schedule: Manual' );
+	} );
+
+	test( 'renders deposit schedule for daily interval', () => {
+		const depositSchedule = getDepositSchedule( { interval: 'daily' } );
+		expect( depositSchedule ).toEqual( 'Deposit Schedule: Automatic, every business day' );
+	} );
+
+	test( 'renders deposit schedule for weekly interval', () => {
+		// eslint-disable-next-line camelcase
+		const depositSchedule = getDepositSchedule( { interval: 'weekly', weekly_anchor: 'monday' } );
+		expect( depositSchedule ).toEqual( 'Deposit Schedule: Automatic, every week on Monday' );
+	} );
+
+	test( 'renders deposit schedule for monthly interval', () => {
+		// eslint-disable-next-line camelcase
+		const depositSchedule = getDepositSchedule( { interval: 'monthly', monthly_anchor: 26 } );
+		expect( depositSchedule ).toEqual( 'Deposit Schedule: Automatic, every month on the 26th' );
+	} );
+
+	test( 'renders weekly anchor for non en locales', () => {
+		momentLib.locale( 'de' );
+		// eslint-disable-next-line camelcase
+		const depositSchedule = getDepositSchedule( { interval: 'weekly', weekly_anchor: 'tuesday' } );
+		// without resetting the locale to en the anchor monday would become Sonntag, instead of Dienstag
+		expect( depositSchedule ).toEqual( 'Deposit Schedule: Automatic, every week on Dienstag' );
+		// the default locale should not have changed
+		expect( momentLib.locale() ).toEqual( 'de' );
+	} );
+
+	test( 'renders in transit label to in_transit next deposits', () => {
+		// eslint-disable-next-line camelcase
+		const overview = getMockedOverview( { next_deposit: { status: 'in_transit' } } );
+		mockUseDepositsOverview( overview );
+		const { getByText } = render( <DepositsOverview /> );
+		const nextDepositDate = getByText( 'Est. Apr 22, 2019 - In transit' );
+		expect( nextDepositDate.parentElement.textContent ).toContain( 'Next deposit' );
+	} );
+
+	test( 'renders singular number of pending deposits', () => {
+		// eslint-disable-next-line camelcase
+		const overview = getMockedOverview( { balance: { pending: { deposits_count: 1 } } } );
+		mockUseDepositsOverview( overview );
+		const { getByText } = render( <DepositsOverview /> );
+		const pendingDeposits = getByText( '1 deposit' );
+		expect( pendingDeposits.parentElement.textContent ).toContain( 'Pending balance' );
+	} );
+
+	test( 'renders plural number of pending deposits', () => {
+		// eslint-disable-next-line camelcase
+		const overview = getMockedOverview( { balance: { pending: { deposits_count: 2 } } } );
+		mockUseDepositsOverview( overview );
+		const { getByText } = render( <DepositsOverview /> );
+		const pendingDeposits = getByText( '2 deposits' );
+		expect( pendingDeposits.parentElement.textContent ).toContain( 'Pending balance' );
+	} );
 } );
+
+function getDepositSchedule( schedule ) {
+	// eslint-disable-next-line camelcase
+	const overview = getMockedOverview( { deposit_schedule: schedule } );
+	mockUseDepositsOverview( overview );
+	const { getByText } = render( <DepositsOverview /> );
+	const depositSchedule = getByText( 'Deposit Schedule:' );
+	return depositSchedule.parentElement.textContent;
+}
