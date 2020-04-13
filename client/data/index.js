65d654f7f9bef17b225ea9db23edb21542caa86d
Vasily Belolapotkov <belolapotkov.v@gmail.com>
2020-04-13 13:22:16 +0300

    Refactor data store initialisation
    Extract data store initialisation into own module to allow using it in tests.

--- client/data/index.js
+++ client/data/index.js
@@ -1,47 +1,12 @@
 /** @format */
 
-/*
- * External dependencies
- */
-import { registerStore, combineReducers } from '@wordpress/data';
-import { controls } from '@wordpress/data-controls';
-
 /**
  * Internal dependencies
  */
 import { STORE_NAME } from './constants';
-import * as deposits from './deposits';
-import * as transactions from './transactions';
-import * as charges from './charges';
-import * as timeline from './timeline';
+import { initStore } from './store';
 
-registerStore( STORE_NAME, {
-	reducer: combineReducers( {
-		deposits: deposits.reducer,
-		transactions: transactions.reducer,
-		charges: charges.reducer,
-		timeline: timeline.reducer,
-	} ),
-	actions: {
-		...deposits.actions,
-		...transactions.actions,
-		...charges.actions,
-		...timeline.actions,
-	},
-	controls,
-	selectors: {
-		...deposits.selectors,
-		...transactions.selectors,
-		...charges.selectors,
-		...timeline.selectors,
-	},
-	resolvers: {
-		...deposits.resolvers,
-		...transactions.resolvers,
-		...charges.resolvers,
-		...timeline.resolvers,
-	},
-} );
+initStore();
 
 export const WCPAY_STORE_NAME = STORE_NAME;
 
