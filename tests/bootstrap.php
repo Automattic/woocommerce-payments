1c693d734354705ccf93883ea71067586fd645cb
James Rodger <james.rodger@automattic.com>
2020-04-17 17:09:01 +0100

    Update deprecated PHPCS whitelist notation
    Fixes error:
    
    ```
    Using the WPCS native whitelist comments is deprecated. Please use the
    PHPCS native "phpcs:ignore Standard.Category.SniffName.ErrorCode"
    annotations instead. Found: // WPCS: XSS ok.
    (WordPress.Security.EscapeOutput.DeprecatedWhitelistCommentFound)
    ```

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -12,7 +12,7 @@ if ( ! $_tests_dir ) {
 }
 
 if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
-	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL; // WPCS: XSS ok.
+	echo "Could not find $_tests_dir/includes/functions.php, have you run bin/install-wp-tests.sh ?" . PHP_EOL; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 	exit( 1 );
 }
 
