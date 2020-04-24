7f35cc966746c27321b880626af941b4c2649bbe
Allen Snook <allendav@automattic.com>
2020-04-24 10:42:31 -0700

    Support redirect to wc-admin task payments page after completing connection (#594)
    * Redirect to task payments page if that is where we started
    
    * Add return url in one more place
    
    * Passing admin.php to admin_url is required for a properly formed url
    
    * Pass GET param into init_oauth to avoid duplicated code
    
    * Remove unneeded blank comment line

--- includes/class-wc-payments-account.php
+++ includes/class-wc-payments-account.php
@@ -231,7 +231,8 @@ class WC_Payments_Account {
 
 		if ( isset( $_GET['wcpay-connect'] ) && check_admin_referer( 'wcpay-connect' ) ) {
 			try {
-				$this->init_oauth();
+				$wcpay_connect_param = sanitize_text_field( wp_unslash( $_GET['wcpay-connect'] ) );
+				$this->init_oauth( $wcpay_connect_param );
 			} catch ( Exception $e ) {
 				Logger::error( 'Init oauth flow failed. ' . $e );
 				$this->add_notice_to_settings_page(
@@ -334,27 +335,43 @@ class WC_Payments_Account {
 
 	/**
 	 * Initializes the OAuth flow by fetching the URL from the API and redirecting to it
+	 *
+	 * @param string $wcpay_connect_from - where the user should be returned to after connecting.
 	 */
-	private function init_oauth() {
+	private function init_oauth( $wcpay_connect_from ) {
 		// Clear account transient when generating Stripe's oauth data.
 		delete_transient( self::ACCOUNT_TRANSIENT );
 
 		$current_user = wp_get_current_user();
 
+		// Usually the return URL is the WCPay plugin settings page.
+		// But if connection originated on the WCADMIN payment task page, return there.
+		$return_url = WC_Payment_Gateway_WCPay::get_settings_url();
+		if ( strcmp( $wcpay_connect_from, 'WCADMIN_PAYMENT_TASK' ) === 0 ) {
+			$return_url = add_query_arg(
+				array(
+					'page' => 'wc-admin',
+					'task' => 'payments',
+				),
+				admin_url( 'admin.php' )
+			);
+		}
+
 		$oauth_data = $this->payments_api_client->get_oauth_data(
-			WC_Payment_Gateway_WCPay::get_settings_url(),
+			$return_url,
 			array(
 				'email'         => $current_user->user_email,
 				'business_name' => get_bloginfo( 'name' ),
 			)
 		);
 
+		// If an account already exists for this site, we're done.
 		if ( false === $oauth_data['url'] ) {
 			WC_Payments::get_gateway()->update_option( 'enabled', 'yes' );
 			wp_safe_redirect(
 				add_query_arg(
 					array( 'wcpay-connection-success' => '1' ),
-					WC_Payment_Gateway_WCPay::get_settings_url()
+					$return_url
 				)
 			);
 			exit;
