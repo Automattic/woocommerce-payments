d9197961af5badb706751eb267c6d3deb966d1aa
Luiz Reis <luiz.reis@automattic.com>
2020-04-24 14:38:10 -0300

    Add transaction filters component with date and type options

--- client/transactions/index.js
+++ client/transactions/index.js
@@ -2,11 +2,13 @@
  * Internal dependencies
  */
 import Page from 'components/page';
+import TransactionFilters from './filters';
 import TransactionsList from './list';
 
 export const TransactionsPage = () => {
 	return (
 		<Page>
+			<TransactionFilters />
 			<TransactionsList />
 		</Page>
 	);
