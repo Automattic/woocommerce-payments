43074484a2f47a8b8d18e1bdb40a07583d594afa
Allen Snook <allen@allendav.com>
2020-05-07 14:06:09 -0700

    Don't bother removing stripe after embedding it

--- client/hooks/include-stripe-js/index.js
+++ client/hooks/include-stripe-js/index.js
@@ -9,16 +9,17 @@ import { useEffect } from 'react';
 
 const includeStripeJS = () => {
 	useEffect( () => {
+		const scriptId = 'wcpaystripejs';
 		const script = document.createElement( 'script' );
-
 		script.src = 'https://js.stripe.com/v3';
 		script.async = true;
+		script.id = scriptId;
 
-		document.body.appendChild( script );
+		if ( ! document.getElementById( scriptId ) ) {
+			document.body.appendChild( script );
+		}
 
-		return () => {
-			document.body.removeChild( script );
-		};
+		return () => {};
 	}, [] );
 };
 
