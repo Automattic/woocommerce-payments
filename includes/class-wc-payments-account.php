0d59eccec3203c51ddb7f16e36c351ad7e31c74a
Paul Dechov <dechovsky@gmail.com>
2020-05-19 08:17:46 -0400

    Check that new param is available before relying on it

--- includes/class-wc-payments-account.php
+++ includes/class-wc-payments-account.php
@@ -147,7 +147,7 @@ class WC_Payments_Account {
 			'paymentsEnabled' => $account['payments_enabled'],
 			'depositsStatus'  => $account['deposits_status'],
 			'currentDeadline' => isset( $account['current_deadline'] ) ? $account['current_deadline'] : false,
-			'pastDue'         => $account['has_overdue_requirements'],
+			'pastDue'         => isset( $account['has_overdue_requirements'] ) ? $account['has_overdue_requirements'] : true,
 			'accountLink'     => $this->get_login_url(),
 		);
 	}
