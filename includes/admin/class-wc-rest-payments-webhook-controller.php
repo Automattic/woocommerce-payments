fdacd5d338c3999c696d06f95ab03b7692bca03c
James Rodger <james.rodger@automattic.com>
2020-06-09 18:08:44 +0100

    Fix typo - i before e except in thousands of exception cases

--- includes/admin/class-wc-rest-payments-webhook-controller.php
+++ includes/admin/class-wc-rest-payments-webhook-controller.php
@@ -101,7 +101,7 @@ class WC_REST_Payments_Webhook_Controller extends WC_Payments_REST_Controller {
 			// Extract information about the webhook event.
 			$event_type = $this->read_rest_property( $body, 'type' );
 
-			Logger::debug( 'Webhook recieved: ' . $event_type );
+			Logger::debug( 'Webhook received: ' . $event_type );
 			Logger::debug(
 				'Webhook body: '
 				. var_export( WC_Payments_Utils::redact_array( $body, WC_Payments_API_Client::API_KEYS_TO_REDACT ), true ) // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_var_export
