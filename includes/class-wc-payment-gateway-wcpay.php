de0a3b5647d27fa2852cb83868b04ff71ebce592
Daniel Rey López <daniel.rey.lopez@gmail.com>
2020-06-28 18:32:34 +0100

    Reshuffle the capture logic so order notes are added in the correct sequence

--- includes/class-wc-payment-gateway-wcpay.php
+++ includes/class-wc-payment-gateway-wcpay.php
@@ -670,7 +670,9 @@ class WC_Payment_Gateway_WCPay extends WC_Payment_Gateway_CC {
 	 * @param WC_Order $order - Order to capture charge on.
 	 */
 	public function capture_charge( $order ) {
-		$amount = $order->get_total();
+		$amount                   = $order->get_total();
+		$is_authorization_expired = false;
+		$status                   = null;
 
 		try {
 			$intent = $this->payments_api_client->capture_intention(
@@ -683,35 +685,39 @@ class WC_Payment_Gateway_WCPay extends WC_Payment_Gateway_CC {
 
 			$order->update_meta_data( '_intention_status', $status );
 			$order->save();
-
-			if ( 'succeeded' === $status ) {
-				$note = sprintf(
-					WC_Payments_Utils::esc_interpolated_html(
-					/* translators: %1: the successfully charged amount */
-						__( 'A payment of %1$s was <strong>successfully captured</strong> using WooCommerce Payments.', 'woocommerce-payments' ),
-						[ 'strong' => '<strong>' ]
-					),
-					wc_price( $amount )
-				);
-				$order->add_order_note( $note );
-				$order->payment_complete();
-			} else {
-				$note = sprintf(
-					WC_Payments_Utils::esc_interpolated_html(
-					/* translators: %1: the successfully charged amount */
-						__( 'A capture of %1$s <strong>failed</strong> to complete.', 'woocommerce-payments' ),
-						[ 'strong' => '<strong>' ]
-					),
-					wc_price( $amount )
-				);
-				$order->add_order_note( $note );
-			}
 		} catch ( WC_Payments_API_Exception $e ) {
 			// Fetch the Intent to check if it's already expired and the site missed the "charge.expired" webhook.
 			$intent = $this->payments_api_client->get_intent( $order->get_transaction_id() );
 			if ( 'canceled' === $intent->get_status() ) {
-				WC_Payments_Utils::mark_payment_expired( $order );
+				$is_authorization_expired = true;
 			}
+		}
+
+		if ( 'succeeded' === $status ) {
+			$note = sprintf(
+				WC_Payments_Utils::esc_interpolated_html(
+				/* translators: %1: the successfully charged amount */
+					__( 'A payment of %1$s was <strong>successfully captured</strong> using WooCommerce Payments.', 'woocommerce-payments' ),
+					[ 'strong' => '<strong>' ]
+				),
+				wc_price( $amount )
+			);
+			$order->add_order_note( $note );
+			$order->payment_complete();
+		} else {
+			$note = sprintf(
+				WC_Payments_Utils::esc_interpolated_html(
+				/* translators: %1: the successfully charged amount */
+					__( 'A capture of %1$s <strong>failed</strong> to complete.', 'woocommerce-payments' ),
+					[ 'strong' => '<strong>' ]
+				),
+				wc_price( $amount )
+			);
+			$order->add_order_note( $note );
+		}
+
+		if ( $is_authorization_expired ) {
+			WC_Payments_Utils::mark_payment_expired( $order );
 		}
 	}
 
