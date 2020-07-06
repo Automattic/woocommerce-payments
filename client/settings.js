62e2882a2cdc05b26a4dd20b30a4ba8b09c93f2e
Luiz Reis <luiz.reis@automattic.com>
2020-07-06 14:04:36 -0300

    Use string literal in __ call for i18n to work

--- client/settings.js
+++ client/settings.js
@@ -18,11 +18,9 @@ ReactDOM.render(
 if ( isWCPaySettingsPage() ) {
 	const saveButton = document.querySelector( 'button[name="save"]' );
 	const manualCaptureCheckbox = document.getElementById( 'woocommerce_woocommerce_payments_manual_capture' );
-	const manualCaptureConfirmMessage = 'When manual capture is enabled, you need to capture funds manually within 7 days of the order \
-being placed, otherwise the authorization will be canceled alongside the order. Are you sure you want to enable it?';
 
 	if ( saveButton && manualCaptureCheckbox && ! manualCaptureCheckbox.checked ) {
-		addCheckboxConfirmation( saveButton, manualCaptureCheckbox,	manualCaptureConfirmMessage	);
+		addCheckboxConfirmation( saveButton, manualCaptureCheckbox );
 	}
 }
 
@@ -31,10 +29,16 @@ function isWCPaySettingsPage() {
 	return 'wc-settings' === page && 'checkout' === tab && 'woocommerce_payments' === section;
 }
 
-function addCheckboxConfirmation( saveButton, checkbox, confirmMessage ) {
+function addCheckboxConfirmation( saveButton, checkbox ) {
 	saveButton.addEventListener( 'click', ( e ) => {
 		if ( checkbox.checked ) {
-			const hasUserConfirmed = confirm( __( confirmMessage, 'woocommerce-payments' ) );
+			const hasUserConfirmed = confirm(
+				__(
+					'When manual capture is enabled, you need to capture funds manually within 7 days of the order being placed, \
+otherwise the authorization will be canceled alongside the order. Are you sure you want to enable it?',
+					'woocommerce-payments'
+				)
+			);
 			if ( ! hasUserConfirmed ) {
 				e.preventDefault();
 			}
