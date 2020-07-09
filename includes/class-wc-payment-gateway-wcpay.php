2bec4242990d779a0d9ef8aa3ff52fe32a5f1b8c
Dwain Maralack <dwainm@users.noreply.github.com>
2020-07-09 17:50:27 +0200

    Update class-wc-payment-gateway-wcpay.php
    Move success tracking further up before we add a note as the note adding may fail and this doesn't mean the refund failed.

--- includes/class-wc-payment-gateway-wcpay.php
+++ includes/class-wc-payment-gateway-wcpay.php
@@ -530,6 +530,7 @@ class WC_Payment_Gateway_WCPay extends WC_Payment_Gateway_CC {
 			} else {
 				$refund = $this->payments_api_client->refund_charge( $charge_id, WC_Payments_Utils::prepare_amount( $amount, 'USD' ) );
 			}
+			Tracker::track_admin( 'wcpay_edit_order_refund_success' );
 		} catch ( Exception $e ) {
 
 			$note = sprintf(
@@ -562,7 +563,6 @@ class WC_Payment_Gateway_WCPay extends WC_Payment_Gateway_CC {
 		}
 
 		$order->add_order_note( $note );
-		Tracker::track_admin( 'wcpay_edit_order_refund_success' );
 
 		return true;
 	}
