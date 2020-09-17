c97f2ae92046063977f93f7a60d61f2f08503bc7
Kristófer R <kristofer.thorlaksson@automattic.com>
2020-09-17 19:44:25 +0800

    add enum for payment capture type

---
+++ includes/constants/class-payment-capture-type.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Class Capture_Type
+ *
+ * @package WooCommerce\Payments
+ */
+
+namespace WCPay\Constants;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+use MyCLabs\Enum\Enum;
+
+/**
+ * Enum used in WCPay\Payment_Information to determine whether a payment was
+ * initated by a merchant or a customer.
+ */
+class Payment_Capture_Type extends Enum {
+	const AUTOMATIC = 'automatic_capture';
+	const MANUAL    = 'manual_capture';
+};
