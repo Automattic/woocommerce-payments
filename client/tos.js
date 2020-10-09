474954abc565be3b41945c04d03eb623c86ef12b
Vasily Belolapotkov <belolapotkov.v@gmail.com>
2020-10-09 19:02:14 +0300

    Add basic TOS modal UI without behavior

--- client/tos.js
+++ client/tos.js
@@ -1,2 +1,17 @@
-// eslint-disable-next-line no-console
-console.log( 'wcpay-tos-script is loaded' );
+/**
+ * External dependencies
+ */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import TosModal from 'tos-modal';
+
+renderTosModal();
+
+function renderTosModal() {
+	const container = document.createElement( 'div' );
+	container.id = 'wcpay-tos-container';
+	const wpcontent = document.getElementById( 'wpcontent' );
+	wpcontent.appendChild( container );
+	ReactDOM.render( <TosModal />, container );
+}
