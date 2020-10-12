3a101f19f24749dbca26290114ddb4f1ffd1281e
Vasily Belolapotkov <belolapotkov.v@gmail.com>
2020-10-12 16:39:31 +0300

    Split tos modal into smaller components

--- client/tos-modal/index.js
+++ client/tos-modal/index.js
@@ -12,12 +12,96 @@ import { Link } from '@woocommerce/components';
  */
 import './style.scss';
 
+const TosLink = () => (
+	<Link href="https://wordpress.com/tos" target="_blank" type="external" />
+);
+
+const TosModalUI = ( { onAccept, onDecline } ) => {
+	const title = __(
+		'WooCommerce Payments: Terms of Service',
+		'woocommerce-payments'
+	);
+	const message = interpolateComponents( {
+		mixedString: __(
+			'To continue using WooCommerce Payments, please review and agree to our {{link}}Terms of Service{{/link}}.' +
+				' By clicking “Accept” you agree to the Terms of Service.',
+			'woocommerce-payments'
+		),
+		components: { link: <TosLink /> },
+	} );
+
+	return (
+		<Modal
+			title={ title }
+			isDismissible={ false }
+			shouldCloseOnClickOutside={ false }
+			shouldCloseOnEsc={ false }
+			onRequestClose={ onDecline }
+			className="woocommerce-payments__tos-modal"
+		>
+			<div className="woocommerce-payments__tos-wrapper">
+				<div className="woocommerce-payments__tos-modal-message">
+					{ message }
+				</div>
+				<div className="woocommerce-payments__tos-footer">
+					<Button isTertiary onClick={ onDecline }>
+						{ __( 'Decline', 'woocommerce-payments' ) }
+					</Button>
+
+					<Button isPrimary onClick={ onAccept }>
+						{ __( 'Accept', 'woocommerce-payments' ) }
+					</Button>
+				</div>
+			</div>
+		</Modal>
+	);
+};
+
+const DisableModalUI = ( { onDisable, onCancel } ) => {
+	const title = __( 'Disable WooCommerce Payments', 'woocommerce-payments' );
+	const message = interpolateComponents( {
+		mixedString: __(
+			'By declining our {{link}}Terms of Service{{/link}},' +
+				' you’ll no longer be able to capture credit card payments using WooCommerce Payments.' +
+				' Your previous transaction and deposit data will still be available.',
+			'woocommerce-payments'
+		),
+		components: { link: <TosLink /> },
+	} );
+
+	return (
+		<Modal
+			title={ title }
+			isDismissible={ false }
+			shouldCloseOnClickOutside={ false }
+			shouldCloseOnEsc={ false }
+			onRequestClose={ onDisable }
+			className="woocommerce-payments__tos-modal"
+		>
+			<div className="woocommerce-payments__tos-wrapper">
+				<div className="woocommerce-payments__tos-modal-message">
+					{ message }
+				</div>
+				<div className="woocommerce-payments__tos-footer">
+					<Button isTertiary onClick={ onCancel }>
+						{ __( 'Back', 'woocommerce-payments' ) }
+					</Button>
+
+					<Button isPrimary onClick={ onDisable }>
+						{ __( 'Disable', 'woocommerce-payments' ) }
+					</Button>
+				</div>
+			</div>
+		</Modal>
+	);
+};
+
 const TosModal = () => {
 	// TODO: detect initial state
-	const [ isOpen, setIsOpen ] = useState( true );
+	const [ isTosModalOpen, setIsTosModalOpen ] = useState( true );
 	const [ isDisableModalOpen, setIsDisableModalOpen ] = useState( false );
 
-	const closeModal = () => setIsOpen( false );
+	const closeModal = () => setIsTosModalOpen( false );
 	const closeDisableModal = () => setIsDisableModalOpen( false );
 
 	const declineTos = () => {
@@ -31,106 +115,20 @@ const TosModal = () => {
 
 	const cancelPluginDisable = () => {
 		closeDisableModal();
-		setIsOpen( true );
+		setIsTosModalOpen( true );
 	};
 
-	const title = __(
-		'WooCommerce Payments: Terms of Service',
-		'woocommerce-payments'
-	);
-	const trackingMessage = interpolateComponents( {
-		mixedString: __(
-			'To continue using WooCommerce Payments, please review and agree to our {{link}}Terms of Service{{/link}}.' +
-				' By clicking “Accept” you agree to the Terms of Service.',
-			'woocommerce-payments'
-		),
-		components: {
-			link: (
-				<Link
-					href="https://wordpress.com/tos"
-					target="_blank"
-					type="external"
-				/>
-			),
-		},
-	} );
-
 	if ( isDisableModalOpen ) {
-		const disableModalTitle = __(
-			'Disable WooCommerce Payments',
-			'woocommerce-payments'
-		);
-		const disableMessage = interpolateComponents( {
-			mixedString: __(
-				'By declining our {{link}}Terms of Service{{/link}},' +
-					' you’ll no longer be able to capture credit card payments using WooCommerce Payments.' +
-					' Your previous transaction and deposit data will still be available.',
-				'woocommerce-payments'
-			),
-			components: {
-				link: (
-					<Link
-						href="https://wordpress.com/tos"
-						target="_blank"
-						type="external"
-					/>
-				),
-			},
-		} );
-
 		return (
-			<Modal
-				title={ disableModalTitle }
-				isDismissible={ false }
-				shouldCloseOnClickOutside={ false }
-				shouldCloseOnEsc={ false }
-				onRequestClose={ disablePlugin }
-				className="woocommerce-payments__tos-modal"
-			>
-				<div className="woocommerce-payments__tos-wrapper">
-					<div className="woocommerce-payments__tos-modal-message">
-						{ disableMessage }
-					</div>
-					<div className="woocommerce-payments__tos-footer">
-						<Button isTertiary onClick={ cancelPluginDisable }>
-							{ __( 'Back', 'woocommerce-payments' ) }
-						</Button>
-
-						<Button isPrimary onClick={ disablePlugin }>
-							{ __( 'Disable', 'woocommerce-payments' ) }
-						</Button>
-					</div>
-				</div>
-			</Modal>
+			<DisableModalUI
+				onDisable={ disablePlugin }
+				onCancel={ cancelPluginDisable }
+			/>
 		);
 	}
 
-	if ( isOpen ) {
-		return (
-			<Modal
-				title={ title }
-				isDismissible={ false }
-				shouldCloseOnClickOutside={ false }
-				shouldCloseOnEsc={ false }
-				onRequestClose={ closeModal }
-				className="woocommerce-payments__tos-modal"
-			>
-				<div className="woocommerce-payments__tos-wrapper">
-					<div className="woocommerce-payments__tos-modal-message">
-						{ trackingMessage }
-					</div>
-					<div className="woocommerce-payments__tos-footer">
-						<Button isTertiary onClick={ declineTos }>
-							{ __( 'Decline', 'woocommerce-payments' ) }
-						</Button>
-
-						<Button isPrimary onClick={ acceptTos }>
-							{ __( 'Accept', 'woocommerce-payments' ) }
-						</Button>
-					</div>
-				</div>
-			</Modal>
-		);
+	if ( isTosModalOpen ) {
+		return <TosModalUI onAccept={ acceptTos } onDecline={ declineTos } />;
 	}
 
 	return null;
