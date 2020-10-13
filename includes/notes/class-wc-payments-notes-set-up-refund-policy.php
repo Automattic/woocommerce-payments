d14a3f6e27c78578b109146ec4147378e01394ce
Vladimir Reznichenko <kalessil@gmail.com>
2020-10-13 10:42:43 +0200

    WCPay: implemented set up refund policy note.

---
+++ includes/notes/class-wc-payments-notes-set-up-refund-policy.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Set up refund policy note for WooCommerce inbox.
+ *
+ * @package WooCommerce\Payments\Admin
+ */
+
+use Automattic\WooCommerce\Admin\Notes\NoteTraits;
+use Automattic\WooCommerce\Admin\Notes\WC_Admin_Note;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Class WC_Payments_Notes_Set_Up_Refund_Policy
+ */
+class WC_Payments_Notes_Set_Up_Refund_Policy {
+	use NoteTraits;
+
+	/**
+	 * Name of the note for use in the database.
+	 */
+	const NOTE_NAME = 'wc-payments-notes-set-up-refund-policy';
+
+	/**
+	 * Name of the note for use in the database.
+	 */
+	const NOTE_DOCUMENTATION_URL = 'https://docs.woocommerce.com/document/payments/disputes/';
+
+	/**
+	 * Handle activation scenario and create the note if it not yet created.
+	 */
+	public function on_wcpay_activation() {
+		self::possibly_add_note();
+	}
+
+	/**
+	 * Handle deactivation scenario and drop the note.
+	 */
+	public function on_wcpay_deactivation() {
+		self::possibly_delete_note();
+	}
+
+	/**
+	 * Get the note.
+	 */
+	public static function get_note() {
+		$note = new WC_Admin_Note();
+
+		$note->set_title( __( 'Set up refund policy', 'woocommerce-payments' ) );
+		$note->set_content( __( 'Set up and update orders emails pitch here.', 'woocommerce-payments' ) );
+		$note->set_content_data( (object) [] );
+		$note->set_type( WC_Admin_Note::E_WC_ADMIN_NOTE_INFORMATIONAL );
+		$note->set_name( self::NOTE_NAME );
+		$note->set_source( 'woocommerce-admin' );
+		$note->add_action(
+			self::NOTE_NAME,
+			__( 'Set up policy', 'woocommerce-payments' ),
+			self::NOTE_DOCUMENTATION_URL,
+			'unactioned',
+			true
+		);
+
+		return $note;
+	}
+}
