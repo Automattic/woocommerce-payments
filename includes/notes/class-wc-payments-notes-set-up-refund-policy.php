a35a8ccad52ab6c2dc33937f01d5d74173e1f80b
Vladimir Reznichenko <kalessil@gmail.com>
2020-10-19 16:08:43 +0200

    WCPay: Updated notification text and link.

--- includes/notes/class-wc-payments-notes-set-up-refund-policy.php
+++ includes/notes/class-wc-payments-notes-set-up-refund-policy.php
@@ -24,7 +24,7 @@ class WC_Payments_Notes_Set_Up_Refund_Policy {
 	/**
 	 * Name of the note for use in the database.
 	 */
-	const NOTE_DOCUMENTATION_URL = 'https://docs.woocommerce.com/document/payments/disputes/';
+	const NOTE_DOCUMENTATION_URL = 'https://docs.woocommerce.com/document/woocommerce-refunds#refund-policy-howto';
 
 	/**
 	 * Handle activation scenario and create the note if it not yet created.
@@ -46,15 +46,8 @@ class WC_Payments_Notes_Set_Up_Refund_Policy {
 	public static function get_note() {
 		$note = new WC_Admin_Note();
 
-		/*
-		 * For the documentation:
-		 * - Reference https://woocommerce.com/refund-policy/ as policy example
-		 * - Reference https://woocommerce.com/posts/how-to-customize-emails-in-woocommerce/
-		 * - Add "Managing Disputes with WooCommerce Payments" (-> https://docs.woocommerce.com/documentation/woocommerce-payments/)
-		 */
-
 		$note->set_title( __( 'Set up refund policy', 'woocommerce-payments' ) );
-		$note->set_content( __( 'Protect your merchant account by defining the policy and making it accessible to customers.', 'woocommerce-payments' ) );
+		$note->set_content( __( 'Protect your merchant account from fraudulent disputes by defining the policy and making it accessible to customers.', 'woocommerce-payments' ) );
 		$note->set_content_data( (object) [] );
 		$note->set_type( WC_Admin_Note::E_WC_ADMIN_NOTE_INFORMATIONAL );
 		$note->set_name( self::NOTE_NAME );
