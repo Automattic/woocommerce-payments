b838c10f97d6332ce20ec3075f020b74600d2ade
Vladimir Reznichenko <kalessil@gmail.com>
2020-10-19 09:56:39 +0200

    Reverted PHPUnit 8 usage attempt as WP breaks tests execution with version higher that PHPUnit 7.

--- bin/phpunit.sh
+++ bin/phpunit.sh
@@ -1,13 +1,7 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 if [[ $RUN_PHPCS == 1 || $SHOULD_DEPLOY == 1 ]]; then
 	exit
 fi
 
-PHP_VERSION=$(php -r 'echo PHP_VERSION;')
-if [[ ${PHP_VERSION:0:3} == '7.4' ]]; then
-  wget -O phpunit https://phar.phpunit.de/phpunit-8.phar && chmod +x phpunit
-  ./phpunit
-else
-  ./vendor/bin/phpunit
-fi
+./vendor/bin/phpunit
