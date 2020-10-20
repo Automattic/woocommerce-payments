782375826db7e489d33739405c3f0f9e9ba3d192
Vladimir Reznichenko <kalessil@gmail.com>
2020-10-20 10:09:24 +0200

    WCPay: added new test cases for refund policy note.

---
+++ tests/notes/test-class-wc-payments-notes-set-up-refund-policy.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * Class WC_Payments_Notes_Set_Up_Refund_Policy_Test
+ *
+ * @package WooCommerce\Payments\Tests
+ */
+
+/**
+ * Class WC_Payments_Notes_Set_Up_Refund_Policy tests.
+ */
+class WC_Payments_Notes_Set_Up_Refund_Policy_Test extends WP_UnitTestCase {
+	public function test_removes_note_on_extension_deactivation() {
+		// Trigger WCPay extension deactivation callback.
+		wcpay_deactivated();
+
+		$note_id = WC_Payments_Notes_Set_Up_Refund_Policy::NOTE_NAME;
+		$this->assertSame( [], ( WC_Data_Store::load( 'admin-note' ) )->get_notes_with_name( $note_id ) );
+	}
+
+	public function test_adds_note_on_extension_activation() {
+		// Trigger WCPay extension activation callback.
+		wcpay_activated();
+
+		$note_id = WC_Payments_Notes_Set_Up_Refund_Policy::NOTE_NAME;
+		$this->assertNotSame( [], ( WC_Data_Store::load( 'admin-note' ) )->get_notes_with_name( $note_id ) );
+	}
+}
