3fbe585f069ae7141b4ddea69c3c21eb8477c2fc
Daniel Rey López <daniel.rey.lopez@gmail.com>
2020-11-16 23:10:35 +0000

    Make the fee description compatible with "discount" fees that really are just custom base fees

--- client/account-fees/index.js
+++ client/account-fees/index.js
@@ -30,7 +30,7 @@ const ExpirationBar = ( { feeData } ) => {
 			/>
 			<p className="description">
 				{ sprintf(
-					/* translators: %1: the authorized amount, %2: transaction ID of the payment */
+					/* translators: %1: total payment volume until this promotion expires */
 					__(
 						'Discounted base fee expires after the first %1$s of total payment volume.',
 						'woocommerce-payments'
@@ -43,9 +43,11 @@ const ExpirationBar = ( { feeData } ) => {
 };
 
 const AccountFees = ( { accountFees } ) => {
-	let currentFee = accountFees.base;
+	const baseFee = accountFees.base;
+	let currentFee = baseFee;
 
 	let feeDescription = sprintf(
+		/* translators: %1: Percentage part of the fee. %2: Fixed part of the fee */
 		__( '%1$.1f%% + %2$s per transaction', 'woocommerce-payments' ),
 		currentFee.percentage_rate * 100,
 		currency.formatCurrency( currentFee.fixed_rate / 100 )
@@ -54,27 +56,43 @@ const AccountFees = ( { accountFees } ) => {
 	if ( accountFees.discount.length ) {
 		// TODO: Figure out how the UI should work if there are several "discount" fees stacked.
 		currentFee = accountFees.discount[ 0 ];
+		let percentage, fixed;
 
-		const percentage =
-			accountFees.base.percentage_rate *
-			100 *
-			( 1 - currentFee.discount );
-		const fixed =
-			( accountFees.base.fixed_rate / 100 ) * ( 1 - currentFee.discount );
-		feeDescription = createInterpolateElement(
-			sprintf(
-				/* translators: %s - formatted requirements current deadline, <a> - dashboard login URL */
-				__(
-					'<s>%1$s</s> %2$.1f%% + %3$s per transaction (%4$d%% discount)',
-					'woocommerce-payments'
-				),
-				feeDescription,
-				percentage,
-				currency.formatCurrency( fixed ),
-				currentFee.discount * 100
+		if ( currentFee.discount ) {
+			// Proper discount fee (XX% off)
+			percentage =
+				baseFee.percentage_rate * 100 * ( 1 - currentFee.discount );
+			fixed = ( baseFee.fixed_rate / 100 ) * ( 1 - currentFee.discount );
+		} else {
+			// Custom base fee (2% + $.20)
+			percentage = currentFee.percentage_rate;
+			fixed = currentFee.fixed_rate;
+		}
+
+		let descriptionString = sprintf(
+			/* translators: %1 Base fee (that don't apply to this account at this moment), %2 and %3: Current fee (e.g: 2.9% + $.30) */
+			__(
+				'<s>%1$s</s> %2$.1f%% + %3$s per transaction',
+				'woocommerce-payments'
 			),
-			{ s: <s /> }
+			feeDescription,
+			percentage,
+			currency.formatCurrency( fixed )
 		);
+
+		if ( currentFee.discount ) {
+			descriptionString +=
+				' ' +
+				sprintf(
+					/* translators: %d percentage discount to apply */
+					__( '(%$d%% discount)', 'woocommerce-payments' ),
+					currentFee.discount * 100
+				);
+		}
+
+		feeDescription = createInterpolateElement( descriptionString, {
+			s: <s />,
+		} );
 	}
 
 	return (
