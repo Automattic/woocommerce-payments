d22d55fe01de43021e44eb16e0fd597fda580175
Vladimir Reznichenko <kalessil@gmail.com>
2020-12-01 09:46:19 +0100

    GitHub: debugging

--- tests/e2e/env/setup.sh
+++ tests/e2e/env/setup.sh
@@ -51,13 +51,10 @@ if [[ -n $CI ]]; then
 fi
 
 step "Setting up SERVER containers"
-echo '> '
-#redirect_output $SERVER_PATH/local/bin/docker-setup.sh
 $SERVER_PATH/local/bin/docker-setup.sh
-echo ' <'
 
 step "Configuring server with stripe account"
-redirect_output $SERVER_PATH/local/bin/link-account.sh $BLOG_ID $E2E_WCPAY_STRIPE_ACCOUNT_ID test 1
+$SERVER_PATH/local/bin/link-account.sh $BLOG_ID $E2E_WCPAY_STRIPE_ACCOUNT_ID test 1
 
 cd $cwd
 
