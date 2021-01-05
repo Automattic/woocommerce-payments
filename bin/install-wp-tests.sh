5ac57f427bde00766bd16ceca73a735943e9dcf2
Vladimir Reznichenko <kalessil@gmail.com>
2021-01-05 10:58:46 +0100

    Currencies: installing admin doesn't help.

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -194,11 +194,6 @@ install_woocommerce() {
 			WC_INSTALL_EXTRA+=" --version=$WC_VERSION"
 		fi
 		wp plugin install woocommerce --activate$WC_INSTALL_EXTRA
-
-		# starting the mentioned version here, woocommerce-admin was not initializing, hence we introduced this
-		if $(dpkg --compare-versions $WC_VERSION "ge" "4.9.0"); then
-			wp plugin install woocommerce-admin --activate
-		fi
 	fi
 }
 
