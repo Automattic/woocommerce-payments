0f299b31448eab1389aa4d13735905e20847473c
Vladimir Reznichenko <kalessil@gmail.com>
2021-01-12 10:43:22 +0100

    CI: play around autoloading setup in tests

--- bin/run-ci-tests.bash
+++ bin/run-ci-tests.bash
@@ -7,7 +7,7 @@ IFS=$'\n\t'
 # set environment variables
 WCPAY_DIR="$GITHUB_WORKSPACE"
 
-composer self-update 2.0.6 && composer install --no-progress
+composer self-update 2.0.6 && composer install --no-progress --classmap-authoritative
 sudo systemctl start mysql.service
 bash bin/install-wp-tests.sh woocommerce_test root root localhost $WP_VERSION $WC_VERSION false
 bash bin/phpunit.sh
