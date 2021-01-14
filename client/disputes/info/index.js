048dd479c10b162ca79fec13365fe7d9fdd8f509
Paul Dechov <dechovsky@gmail.com>
2021-01-14 00:12:15 -0500

    Use dispute currency to format dispute amount

--- client/disputes/info/index.js
+++ client/disputes/info/index.js
@@ -6,7 +6,6 @@
 import { __ } from '@wordpress/i18n';
 import { dateI18n } from '@wordpress/date';
 import moment from 'moment';
-import Currency from '@woocommerce/currency';
 import { Link } from '@woocommerce/components';
 
 /**
@@ -16,10 +15,9 @@ import OrderLink from 'components/order-link';
 import { getDetailsURL } from 'components/details-link';
 import { reasons } from '../strings';
 import { formatStringValue } from 'utils';
+import { formatCurrency } from 'utils/currency';
 import './style.scss';
 import Loadable from 'components/loadable';
-
-const currency = new Currency();
 
 const fields = [
 	{ key: 'created', label: __( 'Dispute date', 'woocommerce-payments' ) },
@@ -67,8 +65,9 @@ const Info = ( { dispute, isLoading } ) => {
 					'M j, Y',
 					moment( dispute.created * 1000 ).toISOString()
 				),
-				amount: `${ currency.formatCurrency(
-					dispute.amount / 100
+				amount: `${ formatCurrency(
+					dispute.amount || 0,
+					dispute.currency || 'USD'
 				) } ${ dispute.currency.toUpperCase() }`,
 				dueBy: dateI18n(
 					'M j, Y - g:iA',
