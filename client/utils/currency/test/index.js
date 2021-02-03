dafc463e8e7098359e5d2db6861a99d9c39dc2d8
Vasily Belolapotkov <belolapotkov.v@gmail.com>
2021-02-03 12:13:37 +0300

    Create first tests for formatFX utility

--- client/utils/currency/test/index.js
+++ client/utils/currency/test/index.js
@@ -24,4 +24,16 @@ describe( 'Currency utilities', () => {
 		expect( utils.formatCurrency( 1000, 'AUD' ) ).toEqual( 'AUD 10.00' );
 		expect( utils.formatCurrency( 1000, 'JPY' ) ).toEqual( 'JPY 1000' );
 	} );
+
+	test.each`
+		source                                   | target                                 | expected
+		${ { currency: 'EUR', amount: 1242 } }   | ${ { currency: 'USD', amount: 1484 } } | ${ '€1,00 → $1.19488: $14.84' }
+		${ { currency: 'INR', amount: 131392 } } | ${ { currency: 'USD', amount: 1779 } } | ${ '₹1.00 → $0.013539: $17.79' }
+		${ { currency: 'RUB', amount: 136746 } } | ${ { currency: 'USD', amount: 1777 } } | ${ '1,00₽ → $0.012996: $17.77' }
+	`(
+		'format FX string $source.currency -> $target.currency',
+		( { source, target, expected } ) => {
+			expect( utils.formatFX( source, target ) ).toBe( expected );
+		}
+	);
 } );
