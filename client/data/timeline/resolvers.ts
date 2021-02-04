c3231f299c6515fe7b3fab742ff05299a6888044
Kristófer R <kristofer.thorlaksson@automattic.com>
2021-02-04 15:09:43 +0800

    Migrate rest of timeline data layer to TS

--- client/data/timeline/resolvers.ts
+++ client/data/timeline/resolvers.ts
@@ -10,13 +10,25 @@ import { apiFetch } from '@wordpress/data-controls';
  */
 import { NAMESPACE } from '../constants';
 import { updateTimeline, updateErrorForTimeline } from './actions';
+import { Timeline } from './types';
 
-export function* getTimeline( intentionId ) {
+const isTimeline = ( responseData: any ): responseData is Timeline => {
+	return (
+		typeof responseData === 'object' &&
+		undefined !== ( responseData as Timeline ).length
+	);
+};
+
+export function* getTimeline( intentionId: string ) {
 	try {
 		const results = yield apiFetch( {
 			path: `${ NAMESPACE }/timeline/${ intentionId }`,
 		} );
-		yield updateTimeline( intentionId, results.data );
+
+		const timeline = results.data;
+		if ( isTimeline( timeline ) ) {
+			yield updateTimeline( intentionId, timeline );
+		}
 	} catch ( error ) {
 		yield updateErrorForTimeline( intentionId, error );
 	}
