fa1efaccdab86cbc4ce39196f00e94c2e2cca5fc
Daniel Mallory <daniel.mallory@automattic.com>
2021-02-05 17:41:08 +0000

    updates to the create and update order processing logic to prevent multiple update actions being created.

--- includes/class-wc-payment-gateway-wcpay.php
+++ includes/class-wc-payment-gateway-wcpay.php
@@ -1682,10 +1682,7 @@ class WC_Payment_Gateway_WCPay extends WC_Payment_Gateway_CC {
 			$this->action_scheduler_service->schedule_job(
 				strtotime( '+10 seconds' ),
 				'wcpay_track_update_order',
-				[
-					'order_id'      => $order_id,
-					'date_modified' => $order->get_date_modified(),
-				],
+				[ 'order_id' => $order_id ],
 				self::GATEWAY_ID
 			);
 		}
