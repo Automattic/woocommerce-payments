a171a3a444267bac60d8074758cece53b542a22a
Jesse Pearson <jesse.pearson@automattic.com>
2021-02-16 08:43:11 -0400

    Added styling for instant deposit button and modal.

--- client/deposits/overview/instant-deposit-button.js
+++ client/deposits/overview/instant-deposit-button.js
@@ -6,21 +6,45 @@
 import { Button, Modal } from '@wordpress/components';
 import { __, sprintf } from '@wordpress/i18n';
 import { useState } from '@wordpress/element';
+import { __experimentalCreateInterpolateElement as createInterpolateElement } from 'wordpress-element';
 import Currency from '@woocommerce/currency';
 import apiFetch from '@wordpress/api-fetch';
 
 const currency = new Currency();
 
 const InstantDepositModal = ( { amount, fee, net, onClose, onSubmit, inProgress } ) => {
+	// TODO: add proper url for instant payout doc
+	const learnMoreHref = '';
+	const description = createInterpolateElement(
+		/* translators: <a> - instant payout doc URL */
+		__(
+			'Need cash in a hurry? Instant deposits are available within 30 minutes for a nominal 1% service fee. <a>Learn more</a>',
+			'woocommerce-payments'
+		),
+		// eslint-disable-next-line jsx-a11y/anchor-has-content
+		{ a: <a href={ learnMoreHref } /> }
+	);
+
 	return (
 		<Modal
 			title={ __( 'Instant deposit', 'woocommerce-payments' ) }
 			onRequestClose={ onClose }
+			className='wcpay-instant-deposits-modal'
 		>
+			<p>{ description }</p>
 			<ul>
-				<li>Total amount: { currency.formatCurrency( amount / 100 ) }</li>
-				<li>Fee: { currency.formatCurrency( fee / 100 ) }</li>
-				<li>Net: { currency.formatCurrency( net / 100 ) }</li>
+				<li className='wcpay-instant-deposits-modal__balance'>
+					{ __( 'Balance available for instant deposit: ', 'woocommerce-payments' ) }
+					<span>{ currency.formatCurrency( amount / 100 ) }</span>
+				</li>
+				<li className='wcpay-instant-deposits-modal__fee'>
+					{ __( '1% service fee: ', 'woocommerce-payments' ) }
+					<span>-{ currency.formatCurrency( fee / 100 ) }</span>
+				</li>
+				<li className='wcpay-instant-deposits-modal__net'>
+					{ __( 'Net deposit amount: ', 'woocommerce-payments' ) }
+					<span>{ currency.formatCurrency( net / 100 ) }</span>
+				</li>
 			</ul>
 			<Button isPrimary onClick={ onSubmit } isBusy={ inProgress }>
 				{ sprintf(
@@ -28,6 +52,9 @@ const InstantDepositModal = ( { amount, fee, net, onClose, onSubmit, inProgress 
 					__( 'Deposit %s now', 'woocommerce-payments' ),
 					currency.formatCurrency( net / 100 )
 				) }
+			</Button>
+			<Button isDefault onClick={ onClose }>
+				{ __('Cancel', 'woocommerce-payments') }
 			</Button>
 		</Modal>
 	)
