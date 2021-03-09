a31065950e1a19059bdf479c36374524db5a9da5
Vladimir Reznichenko <kalessil@gmail.com>
2021-03-09 12:36:43 +0100

    Deposits: fixed failing tests with mocked summary

--- client/deposits/list/test/index.js
+++ client/deposits/list/test/index.js
@@ -4,15 +4,17 @@
  * External dependencies
  */
 import { render } from '@testing-library/react';
+import { updateQueryString } from '@woocommerce/navigation';
 
 /**
  * Internal dependencies
  */
 import { DepositsList } from '../';
-import { useDeposits } from 'data';
+import { useDeposits, useDepositsSummary } from 'data';
 
 jest.mock( 'data', () => ( {
 	useDeposits: jest.fn(),
+	useDepositsSummary: jest.fn(),
 } ) );
 
 const mockDeposits = [
@@ -37,13 +39,33 @@ const mockDeposits = [
 describe( 'Deposits list', () => {
 	beforeEach( () => {
 		jest.clearAllMocks();
-		global.wcpaySettings = { zeroDecimalCurrencies: [] };
+
+		// the query string is preserved across tests, so we need to reset it
+		// eslint-disable-next-line camelcase
+		updateQueryString( { currency_is: 'usd' }, '/', {} );
+
+		global.wcpaySettings = {
+			currencies: {
+				default: 'usd',
+				supported: [ 'usd' ],
+				names: { usd: 'United States (US) dollar' },
+			},
+			zeroDecimalCurrencies: [],
+		};
 	} );
 
 	test( 'renders correctly', () => {
 		useDeposits.mockReturnValue( {
 			deposits: mockDeposits,
 			depositsCount: 2,
+			isLoading: false,
+		} );
+
+		useDepositsSummary.mockReturnValue( {
+			depositsSummary: {
+				count: 3,
+				total: 300,
+			},
 			isLoading: false,
 		} );
 
