eddef69c29960e0f713f5edf5db35656a94d9044
Ricardo Metring <ricardometring@gmail.com>
2021-03-10 18:55:52 -0300

    Improve live account notice wording

--- includes/class-wc-payments-apple-pay-registration.php
+++ includes/class-wc-payments-apple-pay-registration.php
@@ -344,8 +344,8 @@ class WC_Payments_Apple_Pay_Registration {
 		?>
 		<div class="notice notice-warning apple-pay-message">
 			<p>
-				<strong><?php echo esc_html_e( 'Payment Request Button:', 'woocommerce-payments' ); ?></strong>
-				<?php echo esc_html_e( 'To verify your domain with Apple Pay, you need a live Stripe account.', 'woocommerce-payments' ); ?>
+				<strong><?php echo esc_html( 'Apple Pay:' ); ?></strong>
+				<?php echo esc_html_e( 'Payment Request Buttons are enabled. To use Apple Pay, please use a live WooCommerce Payments account.', 'woocommerce-payments' ); ?>
 			</p>
 		</div>
 		<?php
