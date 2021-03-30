2712b311a4af4e41784cc0a793d3b612d07f6268
Harris Wong <harris.wong@automattic.com>
2021-03-30 11:52:20 -0600

    Add SEPA to storefront checkout as proof of concept

--- includes/class-wc-payments.php
+++ includes/class-wc-payments.php
@@ -23,6 +23,8 @@ class WC_Payments {
 	 */
 	private static $gateway;
 
+	private static $sepa_gateway;
+
 	/**
 	 * Instance of WC_Payments_API_Client, created in init function.
 	 *
@@ -132,6 +134,7 @@ class WC_Payments {
 		include_once __DIR__ . '/class-wc-payments-customer-service.php';
 		include_once __DIR__ . '/class-logger.php';
 		include_once __DIR__ . '/class-wc-payment-gateway-wcpay.php';
+		include_once __DIR__ . '/class-wc-payment-gateway-sepa.php';
 		include_once __DIR__ . '/class-wc-payments-token-service.php';
 		include_once __DIR__ . '/class-wc-payments-payment-request-button-handler.php';
 		include_once __DIR__ . '/class-wc-payments-apple-pay-registration.php';
@@ -159,6 +162,7 @@ class WC_Payments {
 		self::$fraud_service            = new WC_Payments_Fraud_Service( self::$api_client, self::$customer_service, self::$account );
 
 		$gateway_class = 'WC_Payment_Gateway_WCPay';
+		$sepa_class = WC_Payment_Gateway_Sepa::class;
 		// TODO: Remove admin payment method JS hack for Subscriptions <= 3.0.7 when we drop support for those versions.
 		if ( class_exists( 'WC_Subscriptions' ) && version_compare( WC_Subscriptions::$version, '2.2.0', '>=' ) ) {
 			include_once __DIR__ . '/compat/subscriptions/class-wc-payment-gateway-wcpay-subscriptions-compat.php';
@@ -166,6 +170,7 @@ class WC_Payments {
 		}
 
 		self::$gateway = new $gateway_class( self::$api_client, self::$account, self::$customer_service, self::$token_service, self::$action_scheduler_service );
+		self::$sepa_gateway = new $sepa_class( self::$api_client, self::$account, self::$customer_service, self::$token_service, self::$action_scheduler_service );
 
 		// Feature flag.
 		// TODO: Remove this check ahead of Apple Pay release.
@@ -400,6 +405,16 @@ class WC_Payments {
 	 */
 	public static function register_gateway( $gateways ) {
 		$gateways[] = self::$gateway;
+
+		// $gateways[] = self::$sepa_gateway;
+		if (
+			( empty( $_GET['page'] ) && empty( $_GET['tab'] ) )
+			||
+			( ! empty( $_GET['page'] ) && ! empty( $_GET['tab'] ) && $_GET['page'] === 'wc-settings' && $_GET['tab'] === 'checkout' && ! empty( $_GET['section'] ) )
+		) {
+			// $gateways[] = 'WC_Payment_Gateway_Giropay';
+			$gateways[] = self::$sepa_gateway;
+		}
 
 		return $gateways;
 	}
