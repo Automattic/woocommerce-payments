a94693ea6b0567967a3ff11358a51b5aab0e2a23
Brian Borman <brian.borman@automattic.com>
2021-04-16 12:13:48 -0400

    Combine Giropay JS into single checkout JS

--- includes/payment-method/class-giropay.php
+++ includes/payment-method/class-giropay.php
@@ -52,33 +52,6 @@ class Giropay extends WC_Payment_Gateway_WCPay {
 	}
 
 	/**
-	 * Registers all scripts, necessary for the gateway.
-	 */
-	public function register_scripts() {
-		// Register Stripe's JavaScript using the same ID as the Stripe Gateway plugin. This prevents this JS being
-		// loaded twice in the event a site has both plugins enabled. We still run the risk of different plugins
-		// loading different versions however. If Stripe release a v4 of their JavaScript, we could consider
-		// changing the ID to stripe_v4. This would allow older plugins to keep using v3 while we used any new
-		// feature in v4. Stripe have allowed loading of 2 different versions of stripe.js in the past (
-		// https://stripe.com/docs/stripe-js/elements/migrating).
-		wp_register_script(
-			'stripe',
-			'https://js.stripe.com/v3/',
-			[],
-			'3.0',
-			true
-		);
-
-		wp_register_script(
-			'wcpay-giropay-checkout',
-			plugins_url( 'dist/giropay_checkout.js', WCPAY_PLUGIN_FILE ),
-			[ 'stripe', 'wc-checkout' ],
-			WC_Payments::get_file_version( 'dist/giropay_checkout.js' ),
-			true
-		);
-	}
-
-	/**
 	 * Processes redirect payments.
 	 * This method is currently specific to the Giropay redirect payment method.
 	 *
