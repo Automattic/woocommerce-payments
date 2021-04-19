4c6539c23df7732a938514405842fcc77903a0ca
Francesco <frosso@users.noreply.github.com>
2021-04-19 17:15:16 -0500

    refactor: add Giropay to features class (#1620)

--- includes/class-wc-payments-features.php
+++ includes/class-wc-payments-features.php
@@ -30,4 +30,13 @@ class WC_Payments_Features {
 	public static function is_sepa_enabled() {
 		return '1' === get_option( '_wcpay_feature_sepa', '0' );
 	}
+
+	/**
+	 * Checks whether the Giropay gateway feature is enabled
+	 *
+	 * @return bool
+	 */
+	public static function is_giropay_enabled() {
+		return '1' === get_option( '_wcpay_feature_giropay', '0' );
+	}
 }
