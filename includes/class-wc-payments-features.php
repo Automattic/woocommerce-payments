e6d390f0a79b5347e74f075f96df28b06953e851
frosso <francesco.rosso@automattic.com>
2021-04-19 11:15:31 -0500

    dunno why this wasn't flagged

--- includes/class-wc-payments-features.php
+++ includes/class-wc-payments-features.php
@@ -19,6 +19,6 @@ class WC_Payments_Features {
 	 * @return bool
 	 */
 	public static function is_grouped_settings_enabled() {
-		return get_option( '_wcpay_feature_grouped_settings', '0' ) === '1';
+		return '1' === get_option( '_wcpay_feature_grouped_settings', '0' );
 	}
 }
