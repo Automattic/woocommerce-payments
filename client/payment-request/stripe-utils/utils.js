c55e59e677862698de77e1e5a1cf0284a18c49ba
Ricardo Metring <ricardometring@gmail.com>
2021-04-20 18:16:24 -0300

    Remove old constants

--- client/payment-request/stripe-utils/utils.js
+++ client/payment-request/stripe-utils/utils.js
@@ -6,8 +6,7 @@ import { __ } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
-import { normalizeLineItems } from './normalize';
-import { errorTypes, errorCodes } from './constants';
+import { normalizeLineItems } from '../utils';
 
 /**
  * @typedef {import('./type-defs').StripePaymentItem} StripePaymentItem
@@ -130,100 +129,100 @@ const canDoPaymentRequest = ( paymentRequest ) => {
 	} );
 };
 
-const isNonFriendlyError = ( type ) =>
-	[
-		errorTypes.INVALID_REQUEST,
-		errorTypes.API_CONNECTION,
-		errorTypes.API_ERROR,
-		errorTypes.AUTHENTICATION_ERROR,
-		errorTypes.RATE_LIMIT_ERROR,
-	].includes( type );
+// const isNonFriendlyError = ( type ) =>
+// 	[
+// 		errorTypes.INVALID_REQUEST,
+// 		errorTypes.API_CONNECTION,
+// 		errorTypes.API_ERROR,
+// 		errorTypes.AUTHENTICATION_ERROR,
+// 		errorTypes.RATE_LIMIT_ERROR,
+// 	].includes( type );
 
-const getErrorMessageForCode = ( code ) => {
-	const messages = {
-		[ errorCodes.INVALID_NUMBER ]: __(
-			'The card number is not a valid credit card number.',
-			'woocommerce-payments'
-		),
-		[ errorCodes.INVALID_EXPIRY_MONTH ]: __(
-			'The card expiration month is invalid.',
-			'woocommerce-payments'
-		),
-		[ errorCodes.INVALID_EXPIRY_YEAR ]: __(
-			'The card expiration year is invalid.',
-			'woocommerce-payments'
-		),
-		[ errorCodes.INVALID_CVC ]: __(
-			'The card security code is invalid.',
-			'woocommerce-payments'
-		),
-		[ errorCodes.INCORRECT_NUMBER ]: __(
-			'The card number is incorrect.',
-			'woocommerce-payments'
-		),
-		[ errorCodes.INCOMPLETE_NUMBER ]: __(
-			'The card number is incomplete.',
-			'woocommerce-payments'
-		),
-		[ errorCodes.INCOMPLETE_CVC ]: __(
-			'The card security code is incomplete.',
-			'woocommerce-payments'
-		),
-		[ errorCodes.INCOMPLETE_EXPIRY ]: __(
-			'The card expiration date is incomplete.',
-			'woocommerce-payments'
-		),
-		[ errorCodes.EXPIRED_CARD ]: __(
-			'The card has expired.',
-			'woocommerce-payments'
-		),
-		[ errorCodes.INCORRECT_CVC ]: __(
-			'The card security code is incorrect.',
-			'woocommerce-payments'
-		),
-		[ errorCodes.INCORRECT_ZIP ]: __(
-			'The card zip code failed validation.',
-			'woocommerce-payments'
-		),
-		[ errorCodes.INVALID_EXPIRY_YEAR_PAST ]: __(
-			'The card expiration year is in the past',
-			'woocommerce-payments'
-		),
-		[ errorCodes.CARD_DECLINED ]: __(
-			'The card was declined.',
-			'woocommerce-payments'
-		),
-		[ errorCodes.MISSING ]: __(
-			'There is no card on a customer that is being charged.',
-			'woocommerce-payments'
-		),
-		[ errorCodes.PROCESSING_ERROR ]: __(
-			'An error occurred while processing the card.',
-			'woocommerce-payments'
-		),
-	};
-	return messages[ code ] || null;
-};
+// const getErrorMessageForCode = ( code ) => {
+// 	const messages = {
+// 		[ errorCodes.INVALID_NUMBER ]: __(
+// 			'The card number is not a valid credit card number.',
+// 			'woocommerce-payments'
+// 		),
+// 		[ errorCodes.INVALID_EXPIRY_MONTH ]: __(
+// 			'The card expiration month is invalid.',
+// 			'woocommerce-payments'
+// 		),
+// 		[ errorCodes.INVALID_EXPIRY_YEAR ]: __(
+// 			'The card expiration year is invalid.',
+// 			'woocommerce-payments'
+// 		),
+// 		[ errorCodes.INVALID_CVC ]: __(
+// 			'The card security code is invalid.',
+// 			'woocommerce-payments'
+// 		),
+// 		[ errorCodes.INCORRECT_NUMBER ]: __(
+// 			'The card number is incorrect.',
+// 			'woocommerce-payments'
+// 		),
+// 		[ errorCodes.INCOMPLETE_NUMBER ]: __(
+// 			'The card number is incomplete.',
+// 			'woocommerce-payments'
+// 		),
+// 		[ errorCodes.INCOMPLETE_CVC ]: __(
+// 			'The card security code is incomplete.',
+// 			'woocommerce-payments'
+// 		),
+// 		[ errorCodes.INCOMPLETE_EXPIRY ]: __(
+// 			'The card expiration date is incomplete.',
+// 			'woocommerce-payments'
+// 		),
+// 		[ errorCodes.EXPIRED_CARD ]: __(
+// 			'The card has expired.',
+// 			'woocommerce-payments'
+// 		),
+// 		[ errorCodes.INCORRECT_CVC ]: __(
+// 			'The card security code is incorrect.',
+// 			'woocommerce-payments'
+// 		),
+// 		[ errorCodes.INCORRECT_ZIP ]: __(
+// 			'The card zip code failed validation.',
+// 			'woocommerce-payments'
+// 		),
+// 		[ errorCodes.INVALID_EXPIRY_YEAR_PAST ]: __(
+// 			'The card expiration year is in the past',
+// 			'woocommerce-payments'
+// 		),
+// 		[ errorCodes.CARD_DECLINED ]: __(
+// 			'The card was declined.',
+// 			'woocommerce-payments'
+// 		),
+// 		[ errorCodes.MISSING ]: __(
+// 			'There is no card on a customer that is being charged.',
+// 			'woocommerce-payments'
+// 		),
+// 		[ errorCodes.PROCESSING_ERROR ]: __(
+// 			'An error occurred while processing the card.',
+// 			'woocommerce-payments'
+// 		),
+// 	};
+// 	return messages[ code ] || null;
+// };
 
-const getErrorMessageForTypeAndCode = ( type, code = '' ) => {
-	switch ( type ) {
-		case errorTypes.INVALID_EMAIL:
-			return __(
-				'Invalid email address, please correct and try again.',
-				'woo-gutenberg-product-blocks'
-			);
-		case isNonFriendlyError( type ):
-			return __(
-				'Unable to process this payment, please try again or use alternative method.',
-				'woo-gutenberg-product-blocks'
-			);
-		case errorTypes.CARD_ERROR:
-			return getErrorMessageForCode( code );
-		case errorTypes.VALIDATION_ERROR:
-			return ''; // These are shown inline.
-	}
-	return null;
-};
+// const getErrorMessageForTypeAndCode = ( type, code = '' ) => {
+// 	switch ( type ) {
+// 		case errorTypes.INVALID_EMAIL:
+// 			return __(
+// 				'Invalid email address, please correct and try again.',
+// 				'woo-gutenberg-product-blocks'
+// 			);
+// 		case isNonFriendlyError( type ):
+// 			return __(
+// 				'Unable to process this payment, please try again or use alternative method.',
+// 				'woo-gutenberg-product-blocks'
+// 			);
+// 		case errorTypes.CARD_ERROR:
+// 			return getErrorMessageForCode( code );
+// 		case errorTypes.VALIDATION_ERROR:
+// 			return ''; // These are shown inline.
+// 	}
+// 	return null;
+// };
 
 /**
  * Get error messages from WooCommerce notice from server response.
@@ -245,19 +244,19 @@ const getErrorMessageFromNotice = ( notice ) => {
  *
  * @return {Object} pluckedAddress  An object containing shipping address that are needed to fetch an address.
  */
-const pluckAddress = ( { country, state, city, postcode } ) => ( {
-	country,
-	state,
-	city,
-	postcode: postcode.replace( ' ', '' ).toUpperCase(),
-} );
+// const pluckAddress = ( { country, state, city, postcode } ) => ( {
+// 	country,
+// 	state,
+// 	city,
+// 	postcode: postcode.replace( ' ', '' ).toUpperCase(),
+// } );
 
 export {
 	getTotalPaymentItem,
 	getPaymentRequest,
 	updatePaymentRequest,
 	canDoPaymentRequest,
-	getErrorMessageForTypeAndCode,
+	// getErrorMessageForTypeAndCode,
 	getErrorMessageFromNotice,
-	pluckAddress,
+	// pluckAddress,
 };
