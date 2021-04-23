c7ec5183446f1d57d0a3150f9e6df29d6b5ac8fe
Diego Curbelo <diego@curbelo.com>
2021-04-23 16:58:28 -0300

    Fix wrong __ translation domain

--- client/payment-details/timeline/map-events.js
+++ client/payment-details/timeline/map-events.js
@@ -194,7 +194,7 @@ const composeFeeString = ( event ) => {
 
 	return sprintf(
 		/* translators: %1$s is the total fee amount, %2$f%% is the fee percentage, and %3$s is the fixed fee amount. */
-		__( 'Fee (%2$.1f%% + %3$s): %1$s', 'woocommeerce-payments' ),
+		__( 'Fee (%2$.1f%% + %3$s): %1$s', 'woocommerce-payments' ),
 		formatCurrency( -feeAmount, feeCurrency ),
 		percentage * 100,
 		formatCurrency( fixed, fixedCurrency )
