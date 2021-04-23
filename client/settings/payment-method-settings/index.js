87802dd2c0792f8a58ab32a9d1036f8aa8f883a5
Waclaw Jacek <waclaw.jacek@automattic.com>
2021-04-23 14:46:37 +0200

    Add GiropaySettings with placeholder content and breadcrumbs

--- client/settings/payment-method-settings/index.js
+++ client/settings/payment-method-settings/index.js
@@ -1,9 +1,45 @@
 /** @format */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import './index.scss';
+import SettingsSection from '../settings-section';
+import { getPaymentSettingsUrl } from '../../utils';
+import GiropaySettings from './giropay-settings';
 
 const PaymentMethodSettings = ( { methodId } ) => {
+	const methods = [
+		{
+			id: 'woocommerce_payments_giropay',
+			title: 'giropay',
+			description: 'giropay description.',
+			controls: <GiropaySettings />,
+		},
+	];
+
+	const method = methods.find( ( { id } ) => id === methodId );
+
+	if ( ! method ) {
+		return (
+			<p>
+				{ __(
+					'Invalid payment method ID specified.',
+					'woocommerce-payments'
+				) }
+			</p>
+		);
+	}
+
 	return (
 		<div className="payment-method-settings">
-			Method &quot;{ methodId }&quot; settings placeholder.
+			<h2 className="payment-method-settings__breadcrumbs">
+				<a href={ getPaymentSettingsUrl() }>WooCommerce Payments</a>{ ' ' }
+				&gt; <span>{ method.title }</span>
+			</h2>
+
+			<SettingsSection { ...method }>{ method.controls }</SettingsSection>
 		</div>
 	);
 };
