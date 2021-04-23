e9cee31acab9467ae3cdcbc8930be950d4f66410
Ricardo Metring <ricardometring@gmail.com>
2021-04-23 17:19:36 -0300

    Removing paymentRequestAvailable, because: - The totalPrice check was wrong (minimum is 50), and for the future, it depends on currency being used + Stripe's own rules. - Those checks weren't performed in the shortcode version. - The credit card blocks checkout doesn't do these checks either.

--- client/payment-request/blocks/index.js
+++ client/payment-request/blocks/index.js
@@ -7,53 +7,11 @@ import { applePayImage } from './apple-pay-preview';
 import { loadStripe } from '../stripe-utils';
 
 import { getConfig } from '../../utils/checkout';
-import { getPaymentRequestData } from '../utils';
+// import { getPaymentRequestData } from '../utils';
 
 const ApplePayPreview = () => <img src={ applePayImage } alt="" />;
 
 const componentStripePromise = loadStripe();
-
-let isStripeInitialized = false,
-	canPay = false;
-
-// Initialise stripe API client and determine if payment method can be used
-// in current environment (e.g. geo + shopper has payment settings configured).
-function paymentRequestAvailable( { currencyCode, totalPrice } ) {
-	// Stripe only supports carts of greater value than 30 cents.
-	if ( 30 > totalPrice ) {
-		return false;
-	}
-
-	// If we've already initialised, return the cached results.
-	if ( isStripeInitialized ) {
-		return canPay;
-	}
-
-	return componentStripePromise.then( ( stripe ) => {
-		if ( null === stripe ) {
-			isStripeInitialized = true;
-			return canPay;
-		}
-		if ( stripe.error && stripe.error instanceof Error ) {
-			throw stripe.error;
-		}
-		// Do a test payment to confirm if payment method is available.
-		const paymentRequest = stripe.paymentRequest( {
-			total: {
-				label: 'Total',
-				amount: totalPrice,
-				pending: true,
-			},
-			country: getPaymentRequestData( 'checkout' )?.country_code,
-			currency: currencyCode,
-		} );
-		return paymentRequest.canMakePayment().then( ( result ) => {
-			canPay = !! result;
-			isStripeInitialized = true;
-			return canPay;
-		} );
-	} );
-}
 
 const paymentRequestPaymentMethod = ( api ) => ( {
 	name: PAYMENT_METHOD_NAME_PAYMENT_REQUEST,
@@ -61,11 +19,7 @@ const paymentRequestPaymentMethod = ( api ) => ( {
 		<PaymentRequestExpress api={ api } stripe={ componentStripePromise } />
 	),
 	edit: <ApplePayPreview />,
-	canMakePayment: ( cartData ) =>
-		paymentRequestAvailable( {
-			currencyCode: cartData?.cartTotals?.currency_code?.toLowerCase(),
-			totalPrice: parseInt( cartData?.cartTotals?.total_price || 0, 10 ),
-		} ),
+	canMakePayment: !! api.getStripe(),
 	paymentMethodId: PAYMENT_METHOD_NAME_PAYMENT_REQUEST,
 	supports: {
 		features: getConfig( 'features' ),
