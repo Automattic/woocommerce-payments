1e16cc5f26444cdb39e9ac4d78d462320bff74fe
Ricardo Metring <ricardometring@gmail.com>
2021-04-29 03:17:07 -0300

    Add optional chaining operator to all normalization props.

--- client/payment-request/utils/normalize.js
+++ client/payment-request/utils/normalize.js
@@ -28,16 +28,16 @@ export const normalizeLineItems = ( cartTotalItems, pending = false ) => {
  * @return {Object} Order object in the format WooCommerce expects.
  */
 export const normalizeOrderData = ( paymentData ) => {
-	const name = paymentData.paymentMethod.billing_details.name;
-	const email = paymentData.paymentMethod.billing_details.email;
-	const phone = paymentData.paymentMethod.billing_details.phone;
-	const billing = paymentData.paymentMethod.billing_details.address;
-	const shipping = paymentData.shippingAddress ?? {};
+	const name = paymentData?.paymentMethod?.billing_details?.name ?? '';
+	const email = paymentData?.paymentMethod?.billing_details?.email ?? '';
+	const phone = paymentData?.paymentMethod?.billing_details?.phone ?? '';
+	const billing = paymentData?.paymentMethod?.billing_details?.address ?? {};
+	const shipping = paymentData?.shippingAddress ?? {};
 
 	let paymentRequestType = 'payment_request_api';
-	if ( 'applePay' === paymentData.walletName ) {
+	if ( 'applePay' === paymentData?.walletName ) {
 		paymentRequestType = 'apple_pay';
-	} else if ( 'googlePay' === paymentData.walletName ) {
+	} else if ( 'googlePay' === paymentData?.walletName ) {
 		paymentRequestType = 'google_pay';
 	}
 
@@ -46,37 +46,33 @@ export const normalizeOrderData = ( paymentData ) => {
 		billing_first_name:
 			name?.split( ' ' )?.slice( 0, 1 )?.join( ' ' ) ?? '',
 		billing_last_name: name?.split( ' ' )?.slice( 1 )?.join( ' ' ) ?? '',
-		billing_company: billing.organization ?? '',
-		billing_email: email ?? paymentData.payerEmail,
+		billing_company: billing?.organization ?? '',
+		billing_email: email ?? paymentData?.payerEmail ?? '',
 		billing_phone:
-			phone ?? paymentData.payerPhone.replace( '/[() -]/g', '' ),
-		billing_country: billing.country ?? '',
-		billing_address_1: billing.line1 ?? '',
-		billing_address_2: billing.line2 ?? '',
-		billing_city: billing.city ?? '',
-		billing_state: billing.state ?? '',
-		billing_postcode: billing.postal_code ?? '',
+			phone ?? paymentData?.payerPhone?.replace( '/[() -]/g', '' ) ?? '',
+		billing_country: billing?.country ?? '',
+		billing_address_1: billing?.line1 ?? '',
+		billing_address_2: billing?.line2 ?? '',
+		billing_city: billing?.city ?? '',
+		billing_state: billing?.state ?? '',
+		billing_postcode: billing?.postal_code ?? '',
 		shipping_first_name:
 			shipping?.recipient?.split( ' ' )?.slice( 0, 1 )?.join( ' ' ) ?? '',
 		shipping_last_name:
 			shipping?.recipient?.split( ' ' )?.slice( 1 )?.join( ' ' ) ?? '',
-		shipping_company: shipping.organization ?? '',
-		shipping_country: shipping.country ?? '',
-		shipping_address_1: shipping.addressLine?.[ 0 ] ?? '',
-		shipping_address_2: shipping.addressLine?.[ 1 ] ?? '',
-		shipping_city: shipping.city ?? '',
-		shipping_state: shipping.region ?? '',
-		shipping_postcode: shipping.postalCode ?? '',
-		shipping_method: [
-			null === paymentData.shippingOption
-				? null
-				: paymentData.shippingOption.id,
-		],
+		shipping_company: shipping?.organization ?? '',
+		shipping_country: shipping?.country ?? '',
+		shipping_address_1: shipping?.addressLine?.[ 0 ] ?? '',
+		shipping_address_2: shipping?.addressLine?.[ 1 ] ?? '',
+		shipping_city: shipping?.city ?? '',
+		shipping_state: shipping?.region ?? '',
+		shipping_postcode: shipping?.postalCode ?? '',
+		shipping_method: [ paymentData?.shippingOption?.id ?? null ],
 		order_comments: '',
 		payment_method: 'woocommerce_payments',
 		ship_to_different_address: 1,
 		terms: 1,
-		'wcpay-payment-method': paymentData.paymentMethod.id,
+		'wcpay-payment-method': paymentData?.paymentMethod?.id,
 		payment_request_type: paymentRequestType,
 		/* eslint-enable camelcase */
 	};
@@ -94,20 +90,20 @@ export const normalizeShippingAddress = ( shippingAddress ) => {
 	return {
 		/* eslint-disable camelcase */
 		first_name:
-			shippingAddress.recipient
+			shippingAddress?.recipient
 				?.split( ' ' )
 				?.slice( 0, 1 )
 				?.join( ' ' ) ?? '',
 		last_name:
-			shippingAddress.recipient?.split( ' ' )?.slice( 1 )?.join( ' ' ) ??
+			shippingAddress?.recipient?.split( ' ' )?.slice( 1 )?.join( ' ' ) ??
 			'',
 		company: '',
-		address_1: shippingAddress.addressLine?.[ 0 ] ?? '',
-		address_2: shippingAddress.addressLine?.[ 1 ] ?? '',
-		city: shippingAddress.city ?? '',
-		state: shippingAddress.region ?? '',
-		country: shippingAddress.country ?? '',
-		postcode: shippingAddress.postalCode?.replace( ' ', '' ) ?? '',
+		address_1: shippingAddress?.addressLine?.[ 0 ] ?? '',
+		address_2: shippingAddress?.addressLine?.[ 1 ] ?? '',
+		city: shippingAddress?.city ?? '',
+		state: shippingAddress?.region ?? '',
+		country: shippingAddress?.country ?? '',
+		postcode: shippingAddress?.postalCode?.replace( ' ', '' ) ?? '',
 		/* eslint-enable camelcase */
 	};
 };
