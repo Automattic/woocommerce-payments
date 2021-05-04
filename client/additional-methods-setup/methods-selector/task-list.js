fdea6d615565f72c3e2ad2791ca3cbe2c86badae
frosso <francesco.rosso@automattic.com>
2021-05-03 20:10:47 -0500

    accessibility improvements

--- client/additional-methods-setup/methods-selector/task-list.js
+++ client/additional-methods-setup/methods-selector/task-list.js
@@ -1,20 +1,52 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { useLayoutEffect, useRef } from 'react';
 import classNames from 'classnames';
 
 /**
  * Internal dependencies
  */
 import { useChildTaskContext } from '../setup-tasks/child-context';
+import { useSetupTasksControllerContext } from '../setup-tasks/setup-context';
 import './task-list.scss';
 
-export const TaskList = ( { children } ) => (
-	<div className="woocommerce-timeline woocommerce-timeline__task-list">
-		<ul>{ children }</ul>
-	</div>
-);
+export const TaskList = ( { children } ) => {
+	const isFirstMount = useRef( true );
+	const wrapperRef = useRef( null );
+	const { activeTask } = useSetupTasksControllerContext();
+
+	useLayoutEffect( () => {
+		// set the focus on the next active heading.
+		// but need to set the focus only after the first mount, only when the active task changes.
+		if ( true === isFirstMount.current ) {
+			isFirstMount.current = false;
+			return;
+		}
+
+		if ( ! wrapperRef.current ) {
+			return;
+		}
+
+		const nextActiveTitle = wrapperRef.current.querySelector(
+			'.woocommerce-timeline-group.is-active .woocommerce-timeline-item__headline'
+		);
+		if ( ! nextActiveTitle ) {
+			return;
+		}
+
+		nextActiveTitle.focus();
+	}, [ activeTask ] );
+
+	return (
+		<div
+			className="woocommerce-timeline woocommerce-timeline__task-list"
+			ref={ wrapperRef }
+		>
+			<ul>{ children }</ul>
+		</div>
+	);
+};
 
 export const TaskItem = ( { children, title, index, className } ) => {
 	const { isCompleted, isActive } = useChildTaskContext();
@@ -30,7 +62,12 @@ export const TaskItem = ( { children, title, index, className } ) => {
 				<li className="woocommerce-timeline-item">
 					<div className="woocommerce-timeline-item__top-border"></div>
 					<div className="woocommerce-timeline-item__title">
-						<div className="woocommerce-timeline-item__headline">
+						<div
+							className="woocommerce-timeline-item__headline"
+							// tabindex with value `-1` is necessary to programmatically set the focus
+							// on an element that is not interactive.
+							tabIndex="-1"
+						>
 							{ index }
 							<span>{ title }</span>
 						</div>
