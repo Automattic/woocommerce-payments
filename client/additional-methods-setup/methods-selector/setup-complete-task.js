0cf2bd2e3d9b2a54998e0cc75c5e730f67e10ac6
frosso <francesco.rosso@automattic.com>
2021-05-03 20:20:23 -0500

    go big or go home

--- client/additional-methods-setup/methods-selector/setup-complete-task.js
+++ client/additional-methods-setup/methods-selector/setup-complete-task.js
@@ -1,9 +1,10 @@
 /**
  * External dependencies
  */
-import React from 'react';
+import React, { useCallback } from 'react';
 import { __ } from '@wordpress/i18n';
 import { Button } from '@wordpress/components';
+import { getHistory, getNewPath } from '@woocommerce/navigation';
 
 /**
  * Internal dependencies
@@ -13,38 +14,11 @@ import ChildTaskController from '../setup-tasks/child-provider';
 
 import { TaskItem } from './task-list';
 
-const Body = () => {
-	return (
-		<CollapsibleBody>
-			<p>
-				{ __(
-					"You're ready to begin accepting payments with the new methods!.",
-					'woocommerce-payments'
-				) }
-			</p>
-			<p>
-				{ __(
-					// eslint-disable-next-line max-len
-					'Enter your VAT account information and set up taxe to ensure smooth transactions if you plan to sell to customers in Europe.',
-					'woocommerce-payments'
-				) }
-			</p>
-			<p>
-				{ __(
-					'To manage other payment settings or update your payment information, visit the payment settings.',
-					'woocommerce-payments'
-				) }
-			</p>
-			<p>
-				<Button isPrimary>
-					{ __( 'Go to WooCommerce Home', 'woocommerce-payments' ) }
-				</Button>
-			</p>
-		</CollapsibleBody>
-	);
-};
-
 const SetupComplete = () => {
+	const handleGoHome = useCallback( () => {
+		getHistory().push( getNewPath( {}, '/', {} ) );
+	}, [] );
+
 	return (
 		<ChildTaskController id="setup-complete">
 			<TaskItem
@@ -52,7 +26,35 @@ const SetupComplete = () => {
 				title={ __( 'Setup complete', 'woocommerce-payments' ) }
 				index={ 2 }
 			>
-				<Body />
+				<CollapsibleBody>
+					<p>
+						{ __(
+							"You're ready to begin accepting payments with the new methods!.",
+							'woocommerce-payments'
+						) }
+					</p>
+					<p>
+						{ __(
+							// eslint-disable-next-line max-len
+							'Enter your VAT account information and set up taxe to ensure smooth transactions if you plan to sell to customers in Europe.',
+							'woocommerce-payments'
+						) }
+					</p>
+					<p>
+						{ __(
+							'To manage other payment settings or update your payment information, visit the payment settings.',
+							'woocommerce-payments'
+						) }
+					</p>
+					<p>
+						<Button onClick={ handleGoHome } isPrimary>
+							{ __(
+								'Go to WooCommerce Home',
+								'woocommerce-payments'
+							) }
+						</Button>
+					</p>
+				</CollapsibleBody>
 			</TaskItem>
 		</ChildTaskController>
 	);
