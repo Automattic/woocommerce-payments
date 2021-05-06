4dd975f9aacd589a8a28c60e2672e489cb51eded
Alfredo Sumaran <a.sumaran@gmail.com>
2021-05-06 14:02:33 -0500

    Use correct amount for Variable products if “Default form values” is set (#1642)
    * Use correct amount for Variable products if “Default form values” is set
    
    * Use variation value passed via GET otherwise get default value for given attribute
    
    * Sanitize attribute name to be used as key

--- includes/class-wc-payments-payment-request-button-handler.php
+++ includes/class-wc-payments-payment-request-button-handler.php
@@ -224,7 +224,17 @@ class WC_Payments_Payment_Request_Button_Handler {
 		$product = $this->get_product();
 
 		if ( 'variable' === $product->get_type() ) {
-			$attributes = wc_clean( wp_unslash( $_GET ) ); // phpcs:ignore WordPress.Security.NonceVerification
+			$variation_attributes = $product->get_variation_attributes();
+			$attributes           = [];
+
+			foreach ( $variation_attributes as $attribute_name => $attribute_values ) {
+				$attribute_key = 'attribute_' . sanitize_title( $attribute_name );
+
+				// Passed value via GET takes precedence. Otherwise get the default value for given attribute.
+				$attributes[ $attribute_key ] = isset( $_GET[ $attribute_key ] ) // phpcs:ignore WordPress.Security.NonceVerification
+					? wc_clean( wp_unslash( $_GET[ $attribute_key ] ) ) // phpcs:ignore WordPress.Security.NonceVerification
+					: $product->get_variation_default_attribute( $attribute_name );
+			}
 
 			$data_store   = WC_Data_Store::load( 'product' );
 			$variation_id = $data_store->find_matching_product_variation( $product, $attributes );
