78f2067b1c0eb1de53d9425e97da1596978397de
Valery Sukhomlinov <683297+dmvrtx@users.noreply.github.com>
2021-05-07 16:12:53 +0200

    Add TypeScript preset for babel to recognize TypeScript syntax

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,10 @@
 // :exploding_head: same case here: https://github.com/facebook/jest/issues/9292#issuecomment-625750534
 module.exports = {
 	ignore: [],
-	presets: [ '@wordpress/babel-preset-default' ],
+	presets: [
+		'@wordpress/babel-preset-default',
+		[ '@babel/preset-typescript', { allowNamespaces: true } ],
+	],
 	plugins: [
 		[ '@babel/transform-runtime', { corejs: 3 } ],
 		'@babel/plugin-proposal-optional-chaining',
