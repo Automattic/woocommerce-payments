2ff1ec7bc407eccef4b689d8b5e8ce7319e8c4cf
Valery Sukhomlinov <683297+dmvrtx@users.noreply.github.com>
2021-05-10 16:24:25 +0200

    Split AccountStatus into narrow-focused sub-components

--- client/components/account-status/index.js
+++ client/components/account-status/index.js
@@ -17,78 +17,8 @@ import DepositsStatus from 'components/deposits-status';
 import PaymentsStatus from 'components/payments-status';
 import StatusChip from './status-chip';
 
-const AccountStatus = ( props ) => {
-	const { accountStatus, accountFees } = props;
-	let cardHeader;
-	let cardBody;
-
-	if ( accountStatus.error ) {
-		cardHeader = (
-			<div className="title">
-				{ __( 'Account details', 'woocommerce-payments' ) }
-			</div>
-		);
-
-		cardBody = (
-			<div>
-				{ __(
-					'Error determining the connection status.',
-					'woocommerce-payments'
-				) }
-			</div>
-		);
-	} else {
-		cardHeader = (
-			<>
-				<div className="title">
-					{ __( 'Account details', 'woocommerce-payments' ) }
-				</div>
-				<div>
-					<StatusChip accountStatus={ accountStatus.status } />
-				</div>
-				<div className="woocommerce-account-status-header-controls">
-					<Button disabled isLink>
-						{ __( 'Edit details', 'woocommerce-payments' ) }
-					</Button>
-				</div>
-			</>
-		);
-
-		cardBody = (
-			<>
-				{ accountStatus.email && (
-					<AccountStatusItem
-						label={ __(
-							'Connected email:',
-							'woocommerce-payments'
-						) }
-						value={ accountStatus.email }
-					/>
-				) }
-				<AccountStatusItem
-					label={ __( 'Payments:', 'woocommerce-payments' ) }
-					value={
-						<PaymentsStatus
-							paymentsEnabled={ accountStatus.paymentsEnabled }
-						/>
-					}
-				/>
-				<AccountStatusItem
-					label={ __( 'Deposits:', 'woocommerce-payments' ) }
-					value={
-						<DepositsStatus
-							depositsStatus={ accountStatus.depositsStatus }
-						/>
-					}
-				/>
-				<AccountStatusItem
-					label={ __( 'BaseFee:', 'woocommerce-payments' ) }
-					value={ <AccountFees accountFees={ accountFees } /> }
-				/>
-			</>
-		);
-	}
-
+const AccountStatusCard = ( props ) => {
+	const { title, content } = props;
 	return (
 		<Card isSmall>
 			<CardHeader
@@ -96,10 +26,99 @@ const AccountStatus = ( props ) => {
 					'woocommerce-card__header woocommerce-account-status-header'
 				}
 			>
-				{ cardHeader }
+				{ title }
 			</CardHeader>
-			<CardBody>{ cardBody }</CardBody>
+			<CardBody>{ content }</CardBody>
 		</Card>
+	);
+};
+
+const AccountStatusError = () => {
+	return (
+		<AccountStatusCard
+			title={
+				<div className="title">
+					{ __( 'Account details', 'woocommerce-payments' ) }
+				</div>
+			}
+			content={
+				<div>
+					{ __(
+						'Error determining the connection status.',
+						'woocommerce-payments'
+					) }
+				</div>
+			}
+		/>
+	);
+};
+
+const AccountStatusDetails = ( props ) => {
+	const { accountStatus, accountFees } = props;
+
+	return (
+		<AccountStatusCard
+			title={
+				<>
+					<div className="title">
+						{ __( 'Account details', 'woocommerce-payments' ) }
+					</div>
+					<div>
+						<StatusChip accountStatus={ accountStatus.status } />
+					</div>
+					<div className="woocommerce-account-status-header-controls">
+						<Button disabled isLink>
+							{ __( 'Edit details', 'woocommerce-payments' ) }
+						</Button>
+					</div>
+				</>
+			}
+			content={
+				<>
+					{ accountStatus.email && (
+						<AccountStatusItem
+							label={ __(
+								'Connected email:',
+								'woocommerce-payments'
+							) }
+							value={ accountStatus.email }
+						/>
+					) }
+					<AccountStatusItem
+						label={ __( 'Payments:', 'woocommerce-payments' ) }
+						value={
+							<PaymentsStatus
+								paymentsEnabled={
+									accountStatus.paymentsEnabled
+								}
+							/>
+						}
+					/>
+					<AccountStatusItem
+						label={ __( 'Deposits:', 'woocommerce-payments' ) }
+						value={
+							<DepositsStatus
+								depositsStatus={ accountStatus.depositsStatus }
+							/>
+						}
+					/>
+					<AccountStatusItem
+						label={ __( 'BaseFee:', 'woocommerce-payments' ) }
+						value={ <AccountFees accountFees={ accountFees } /> }
+					/>
+				</>
+			}
+		/>
+	);
+};
+
+const AccountStatus = ( props ) => {
+	const { accountStatus } = props;
+
+	return accountStatus.error ? (
+		<AccountStatusError />
+	) : (
+		<AccountStatusDetails { ...props } />
 	);
 };
 
