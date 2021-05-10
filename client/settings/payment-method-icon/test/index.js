48ce08239422c7175e8e10abc6846c3a2dbc2328
Waclaw Jacek <waclaw.jacek@automattic.com>
2021-05-10 15:25:08 +0200

    Update PaymentMethodIcon tests to use the woocommerce_payments_ prefix

--- client/settings/payment-method-icon/test/index.js
+++ client/settings/payment-method-icon/test/index.js
@@ -12,36 +12,48 @@ import PaymentMethodIcon from '..';
 
 describe( 'PaymentMethodIcon', () => {
 	test( 'renders GiroPay payment method icon', () => {
-		const { container } = render( <PaymentMethodIcon name="giropay" /> );
+		const { container } = render(
+			<PaymentMethodIcon name="woocommerce_payments_giropay" />
+		);
 		expect( container.querySelector( 'svg' ) ).toBeInTheDocument();
 	} );
 
 	test( 'renders Sepa payment method icon', () => {
-		const { container } = render( <PaymentMethodIcon name="sepa" /> );
+		const { container } = render(
+			<PaymentMethodIcon name="woocommerce_payments_sepa" />
+		);
 		expect( container.querySelector( 'svg' ) ).toBeInTheDocument();
 	} );
 
 	test( 'renders GiroPay payment method icon', () => {
-		const { container } = render( <PaymentMethodIcon name="sofort" /> );
+		const { container } = render(
+			<PaymentMethodIcon name="woocommerce_payments_sofort" />
+		);
 		expect( container.querySelector( 'svg' ) ).toBeInTheDocument();
 	} );
 
 	test( 'renders GiroPay payment method icon and label', () => {
-		render( <PaymentMethodIcon name="giropay" showName /> );
+		render(
+			<PaymentMethodIcon name="woocommerce_payments_giropay" showName />
+		);
 
 		const label = screen.queryByText( 'GiroPay' );
 		expect( label ).toBeInTheDocument();
 	} );
 
 	test( 'renders Sepa payment method icon and label', () => {
-		render( <PaymentMethodIcon name="sepa" showName /> );
+		render(
+			<PaymentMethodIcon name="woocommerce_payments_sepa" showName />
+		);
 
 		const label = screen.queryByText( 'Direct Debit Payments' );
 		expect( label ).toBeInTheDocument();
 	} );
 
 	test( 'renders GiroPay payment method icon and label', () => {
-		render( <PaymentMethodIcon name="sofort" showName /> );
+		render(
+			<PaymentMethodIcon name="woocommerce_payments_sofort" showName />
+		);
 
 		const label = screen.queryByText( 'Sofort' );
 		expect( label ).toBeInTheDocument();
