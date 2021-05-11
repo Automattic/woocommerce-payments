a8c35a8271084723103a7ff6ffe6e64fed84d1cd
Jeff Stieler <jeff.m.stieler@gmail.com>
2021-05-11 13:49:42 -0400

    Add empty/placeholder task list to Overview.

---
+++ client/overview/task-list.js
@@ -0,0 +1,22 @@
+/** @format **/
+
+/**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import { CollapsibleList } from '@woocommerce/experimental';
+
+/**
+ * Internal dependencies.
+ */
+import './style.scss';
+
+export const TaskList = () => {
+	return (
+		<CollapsibleList
+			collapsed={ false }
+			collapseLabel={ __( 'Hide tasks', 'woocommerce-payments' ) }
+			expandLabel={ __( 'Show tasks', 'woocommerce-payments' ) }
+		/>
+	);
+};
