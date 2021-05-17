3c529f10d9418f1a9427da5ea3ec9d51d0108d8c
Valery Sukhomlinov <683297+dmvrtx@users.noreply.github.com>
2021-05-17 13:55:58 +0200

    JS: update tests and snapshots

--- client/overview/test/index.js
+++ client/overview/test/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 /** @format */
 
 /**
@@ -11,6 +12,25 @@ import { render } from '@testing-library/react';
 import OverviewPage from '../';
 
 describe( 'Overview page', () => {
+	beforeEach( () => {
+		global.wcpaySettings = {
+			zeroDecimalCurrencies: [],
+			accountStatus: {
+				status: 'complete',
+				paymentsEnabled: 1,
+				depositsStatus: 'weekly',
+			},
+			accountFees: {
+				base: {
+					currency: 'EUR',
+					percentage_rate: 0.029,
+					fixed_rate: 0.3,
+				},
+				discount: [],
+			},
+		};
+	} );
+
 	it( 'Renders the task list', () => {
 		const { container } = render( <OverviewPage /> );
 
