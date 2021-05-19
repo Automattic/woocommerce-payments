7919a1346e1bfbab338c190d990205f46309b53e
Ismael Martín Alabarce <info@ismaeld.com>
2021-05-19 11:18:50 +0200

    WIP initial legacy widget implementation

---
+++ includes/multi-currency/class-currency-switcher-widget.php
@@ -0,0 +1,167 @@
+<?php
+/**
+ * WooCommerce Payments Currency Switcher Widget
+ *
+ * @package WooCommerce\Payments
+ */
+
+namespace WCPay\Multi_Currency;
+
+use WP_Widget;
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * Currency Switcher Widget Class
+ */
+class Currency_Switcher_Widget extends WP_Widget {
+
+	const DEFAULT_SETTINGS = [
+		'title'  => '',
+		'symbol' => true,
+		'flag'   => false,
+	];
+
+	/**
+	 * Register widget with WordPress.
+	 */
+	public function __construct() {
+		parent::__construct(
+			'currency_switcher_widget',
+			__( 'Currency Switcher', 'woocommerce-payments' ),
+			[ 'description' => __( 'Let your customers switch between your enabled currencies', 'woocommerce-payments' ) ]
+		);
+	}
+
+	/**
+	 * Front-end display of widget.
+	 *
+	 * @param array $args     Widget arguments.
+	 * @param array $instance Saved values from database.
+	 */
+	public function widget( $args, $instance ) {
+		$instance = wp_parse_args(
+			$instance,
+			self::DEFAULT_SETTINGS
+		);
+
+		$title = apply_filters( 'widget_title', $instance['title'] );
+
+		echo $args['before_widget']; // phpcs:ignore WordPress.Security.EscapeOutput
+		if ( ! empty( $title ) ) {
+			echo $args['before_title'] . $title . $args['after_title']; // phpcs:ignore WordPress.Security.EscapeOutput
+		}
+
+		?>
+		<select>
+			<?php
+			foreach ( Multi_Currency::instance()->get_enabled_currencies() as $currency ) {
+				$this->display_currency_option( $currency, $instance['symbol'], $instance['flag'] );
+			}
+			?>
+		</select>
+		<?php
+
+		echo $args['after_widget']; // phpcs:ignore WordPress.Security.EscapeOutput
+	}
+
+	/**
+	 * Back-end widget form.
+	 *
+	 * @param array $instance Previously saved values from database.
+	 */
+	public function form( $instance ) {
+		$instance = wp_parse_args(
+			$instance,
+			self::DEFAULT_SETTINGS
+		);
+		?>
+		<p>
+			<label for="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>">
+				<?php esc_html_e( 'Title:', 'woocommerce-payments' ); ?>
+			</label>
+			<input
+				class="widefat"
+				id="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"
+				name="<?php echo esc_attr( $this->get_field_name( 'title' ) ); ?>"
+				type="text"
+				value="<?php echo esc_attr( $instance['title'] ); ?>"
+			/>
+		</p>
+		<p>
+			<input
+				class="checkbox"
+				id="<?php echo esc_attr( $this->get_field_id( 'symbol' ) ); ?>"
+				name="<?php echo esc_attr( $this->get_field_name( 'symbol' ) ); ?>"
+				type="checkbox"<?php checked( $instance['symbol'] ); ?>
+			/>
+			<label for="<?php echo esc_attr( $this->get_field_id( 'symbol' ) ); ?>">
+				<?php esc_html_e( 'Display curency symbols', 'woocommerce-payments' ); ?>
+			</label>
+			<br/>
+			<input
+				class="checkbox"
+				id="<?php echo esc_attr( $this->get_field_id( 'flag' ) ); ?>"
+				name="<?php echo esc_attr( $this->get_field_name( 'flag' ) ); ?>"
+				type="checkbox"<?php checked( $instance['flag'] ); ?>
+			/>
+			<label for="<?php echo esc_attr( $this->get_field_id( 'flag' ) ); ?>">
+				<?php esc_html_e( 'Display flags', 'woocommerce-payments' ); ?>
+			</label>
+		</p>
+		<?php
+	}
+
+	/**
+	 * Sanitize widget form values as they are saved.
+	 *
+	 * @param array $new_instance Values just sent to be saved.
+	 * @param array $old_instance Previously saved values from database.
+	 *
+	 * @return array Updated safe values to be saved.
+	 */
+	public function update( $new_instance, $old_instance ) {
+		$new_instance = wp_parse_args(
+			$new_instance,
+			self::DEFAULT_SETTINGS
+		);
+
+		$instance = [
+			'title'  => sanitize_text_field( $new_instance['title'] ),
+			'symbol' => $new_instance['symbol'] ? 1 : 0,
+			'flag'   => $new_instance['flag'] ? 1 : 0,
+		];
+
+		return $instance;
+	}
+
+	/**
+	 * Create an <option> element with provided currency. With symbol and flag if requested.
+	 *
+	 * @param Currency $currency    Currency to use for <option> element.
+	 * @param boolean  $with_symbol Whether to show the currency symbol.
+	 * @param boolean  $with_flag   Whether to show the currency flag.
+	 * @return void Displays HTML of currency <option>
+	 */
+	private function display_currency_option( Currency $currency, bool $with_symbol, bool $with_flag ) {
+		$text = $currency->get_code();
+		if ( $with_symbol ) {
+			$text = $currency->get_symbol() . ' ' . $text;
+		}
+		if ( $with_flag ) {
+			$text = $currency->get_flag() . ' ' . $text;
+		}
+		echo "<option>$text</option>"; // phpcs:ignore WordPress.Security.EscapeOutput
+	}
+}
+
+/**
+ * Widget registration
+ *
+ * @return void
+ */
+function currency_switcher_register_widget() {
+	register_widget( 'WCPay\Multi_Currency\Currency_Switcher_Widget' );
+}
+
+add_action( 'widgets_init', 'WCPay\Multi_Currency\currency_switcher_register_widget' );
