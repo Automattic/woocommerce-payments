971f3b8a0320124c655457bf93038cbdc1a8962a
Jesse Pearson <jessepearson@users.noreply.github.com>
2021-05-19 14:33:04 -0300

    Create data store for multi shopper currency feature (#1856)
    * Create data store for multi-currecy feature.
    
    * Fix copy/paste errors, set hook and selector return names/types correctly.

--- client/data/store.js
+++ client/data/store.js
@@ -13,6 +13,7 @@ import * as transactions from './transactions';
 import * as charges from './charges';
 import * as timeline from './timeline';
 import * as disputes from './disputes';
+import * as multiCurrency from './multi-currency';
 
 // Extracted into wrapper function to facilitate testing.
 export const initStore = () =>
@@ -23,6 +24,7 @@ export const initStore = () =>
 			charges: charges.reducer,
 			timeline: timeline.reducer,
 			disputes: disputes.reducer,
+			multiCurrency: multiCurrency.reducer,
 		} ),
 		actions: {
 			...deposits.actions,
@@ -30,6 +32,7 @@ export const initStore = () =>
 			...charges.actions,
 			...timeline.actions,
 			...disputes.actions,
+			...multiCurrency.actions,
 		},
 		controls,
 		selectors: {
@@ -38,6 +41,7 @@ export const initStore = () =>
 			...charges.selectors,
 			...timeline.selectors,
 			...disputes.selectors,
+			...multiCurrency.selectors,
 		},
 		resolvers: {
 			...deposits.resolvers,
@@ -45,5 +49,6 @@ export const initStore = () =>
 			...charges.resolvers,
 			...timeline.resolvers,
 			...disputes.resolvers,
+			...multiCurrency.resolvers,
 		},
 	} );
