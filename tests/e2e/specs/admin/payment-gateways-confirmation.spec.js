84dcf5b20f0b15ec49c012a20c18763335d3fcf6
frosso <francesco.rosso@automattic.com>
2021-05-20 17:31:59 -0500

    feat: disable WCPay confirmation dialog

---
+++ tests/e2e/specs/admin/payment-gateways-confirmation.spec.js
@@ -0,0 +1,158 @@
+/**
+ * External dependencies
+ */
+import config from 'config';
+import shell from 'shelljs';
+
+const { merchant } = require( '@woocommerce/e2e-utils' );
+
+const WCADMIN_GATEWAYS_LIST = `${ config.get(
+	'url'
+) }wp-admin/admin.php?page=wc-settings&tab=checkout&section`;
+const WP_CLI = `docker run --rm --user xfs --volumes-from wcp_e2e_wordpress --network container:wcp_e2e_wordpress wordpress:cli`;
+
+describe( 'payment gateways disable confirmation', () => {
+	beforeAll( async () => {
+		shell.exec( `${ WP_CLI } wp option _wcpay_feature_grouped_settings 1`, {
+			silent: true,
+		} );
+
+		await merchant.login();
+	} );
+
+	beforeEach( async () => {
+		await page.goto( WCADMIN_GATEWAYS_LIST, {
+			waitUntil: 'networkidle0',
+		} );
+	} );
+
+	afterAll( () => {
+		shell.exec(
+			`${ WP_CLI } wp option delete _wcpay_feature_grouped_settings`,
+			{
+				silent: true,
+			}
+		);
+	} );
+
+	it( 'should show the confirmation dialog when disabling WCPay', async () => {
+		await expect( page ).toMatch(
+			'The "WooCommerce Payments" payment method is currently enabled'
+		);
+
+		// Click the "Disable WCPay" toggle button
+		await expect( page ).toClick(
+			'tr[data-gateway_id="woocommerce_payments"] .wc-payment-gateway-method-toggle-enabled'
+		);
+
+		// Dialog should be displayed
+		await expect( page ).toMatch( 'Disable WooCommerce Payments' );
+
+		// Clicking "Cancel" should not disable WCPay
+		await expect( page ).toClick( 'button', {
+			text: 'Cancel',
+		} );
+
+		// After clicking "Cancel", the modal should close and WCPay should still be enabled, even after refresh
+		await expect( page ).not.toMatch( 'Disable WooCommerce Payments' );
+		await expect( page ).toClick( 'button', {
+			text: 'Save changes',
+		} );
+		await page.waitForNavigation( {
+			waitUntil: 'networkidle0',
+		} );
+		await expect( page ).toMatch(
+			'The "WooCommerce Payments" payment method is currently enabled'
+		);
+	} );
+
+	it( 'should allow disabling WCPay', async () => {
+		await expect( page ).toMatch(
+			'The "WooCommerce Payments" payment method is currently enabled'
+		);
+
+		// Click the "Disable WCPay" toggle button
+		await expect( page ).toClick(
+			'tr[data-gateway_id="woocommerce_payments"] .wc-payment-gateway-method-toggle-enabled'
+		);
+
+		// Dialog should be displayed
+		await expect( page ).toMatch( 'Disable WooCommerce Payments' );
+
+		// Clicking "Disable" should disable WCPay
+		await expect( page ).toClick( 'button', {
+			text: 'Disable',
+		} );
+
+		// After clicking "Disable", the modal should close
+		await expect( page ).not.toMatch( 'Disable WooCommerce Payments' );
+
+		// and refreshing the page should show WCPay become disabled
+		await expect( page ).toClick( 'button', {
+			text: 'Save changes',
+		} );
+		await page.waitForNavigation( {
+			waitUntil: 'networkidle0',
+		} );
+		await expect( page ).toMatch(
+			'The "WooCommerce Payments" payment method is currently disabled'
+		);
+
+		// now we can re-enable it with no issues
+		await expect( page ).toClick(
+			'tr[data-gateway_id="woocommerce_payments"] .wc-payment-gateway-method-toggle-enabled'
+		);
+		await expect( page ).toClick( 'button', {
+			text: 'Save changes',
+		} );
+		await page.waitForNavigation( {
+			waitUntil: 'networkidle0',
+		} );
+		await expect( page ).toMatch(
+			'The "WooCommerce Payments" payment method is currently enabled'
+		);
+	} );
+
+	it( 'should show the modal even after cancelling the action one time', async () => {
+		await expect( page ).toMatch(
+			'The "WooCommerce Payments" payment method is currently enabled'
+		);
+
+		// Click the "Disable WCPay" toggle button
+		await expect( page ).toClick(
+			'tr[data-gateway_id="woocommerce_payments"] .wc-payment-gateway-method-toggle-enabled'
+		);
+
+		// Dialog should be displayed
+		await expect( page ).toMatch( 'Disable WooCommerce Payments' );
+
+		// Clicking "Cancel" should not disable WCPay
+		await expect( page ).toClick( 'button', {
+			text: 'Cancel',
+		} );
+
+		// After clicking "Cancel", the modal should close and WCPay should still be enabled
+		await expect( page ).not.toMatch( 'Disable WooCommerce Payments' );
+		await expect( page ).toMatch(
+			'The "WooCommerce Payments" payment method is currently enabled'
+		);
+
+		// trying again to disable it - the modal should display again
+		await expect( page ).toClick(
+			'tr[data-gateway_id="woocommerce_payments"] .wc-payment-gateway-method-toggle-enabled'
+		);
+		await expect( page ).toMatch( 'Disable WooCommerce Payments' );
+		await expect( page ).toClick( 'button', {
+			text: 'Cancel',
+		} );
+		await expect( page ).not.toMatch( 'Disable WooCommerce Payments' );
+		await expect( page ).toClick(
+			'tr[data-gateway_id="woocommerce_payments"] .wc-payment-gateway-method-toggle-enabled'
+		);
+		await expect( page ).toMatch( 'Disable WooCommerce Payments' );
+		await expect( page ).toClick( 'button', {
+			text: 'Cancel',
+		} );
+		await expect( page ).not.toMatch( 'Disable WooCommerce Payments' );
+	} );
+} );
