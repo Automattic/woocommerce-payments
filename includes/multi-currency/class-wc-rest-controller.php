afb6fe67d31d6b3e10de7208410a2446cc92d669
Jesse Pearson <jesse.pearson@automattic.com>
2021-05-20 10:05:14 -0300

    Updated currencies to be returned via a single endpoint. Updated data returned to include necessary information for display.

--- includes/multi-currency/class-wc-rest-controller.php
+++ includes/multi-currency/class-wc-rest-controller.php
@@ -27,51 +27,39 @@ class WC_REST_Controller extends \WC_Payments_REST_Controller {
 	public function register_routes() {
 		register_rest_route(
 			$this->namespace,
-			'/' . $this->rest_base . '/available',
+			'/' . $this->rest_base . '/currencies',
 			[
 				'methods'             => \WP_REST_Server::READABLE,
-				'callback'            => [ $this, 'get_available_currencies' ],
-				'permission_callback' => [ $this, 'check_permission' ],
-			]
-		);
-		register_rest_route(
-			$this->namespace,
-			'/' . $this->rest_base . '/default',
-			[
-				'methods'             => \WP_REST_Server::READABLE,
-				'callback'            => [ $this, 'get_default_currency' ],
-				'permission_callback' => [ $this, 'check_permission' ],
-			]
-		);
-		register_rest_route(
-			$this->namespace,
-			'/' . $this->rest_base . '/enabled',
-			[
-				'methods'             => \WP_REST_Server::READABLE,
-				'callback'            => [ $this, 'get_enabled_currencies' ],
+				'callback'            => [ $this, 'get_store_currencies' ],
 				'permission_callback' => [ $this, 'check_permission' ],
 			]
 		);
 	}
 
 	/**
-	 * Retrieve available currencies for the store.
+	 * Retrieve currencies for the store.
 	 */
-	public function get_available_currencies() {
-		return WC_Payments_Multi_Currency()->get_available_currencies();
-	}
+	public function get_store_currencies() {
+		$default    = WC_Payments_Multi_Currency()->get_default_currency();
+		$currencies = [
+			'available' => WC_Payments_Multi_Currency()->get_available_currencies(),
+			'enabled'   => WC_Payments_Multi_Currency()->get_enabled_currencies(),
+			'default'   => [ $default->get_code() => $default ],
+		];
 
-	/**
-	 * Retrieve available currencies for the store.
-	 */
-	public function get_default_currency() {
-		return WC_Payments_Multi_Currency()->get_default_currency();
-	}
+		foreach ( $currencies as $grouping => $group ) {
+			foreach ( $group as $code => $currency ) {
+				$currencies[ $grouping ][ $code ] = [
+					'code'   => $code,
+					'rate'   => $currency->get_rate(),
+					'name'   => $currency->get_name(),
+					'id'     => $currency->get_id(),
+					'flag'   => $currency->get_flag(),
+					'symbol' => $currency->get_symbol(),
+				];
+			}
+		}
 
-	/**
-	 * Retrieve enabled currencies for the store.
-	 */
-	public function get_enabled_currencies() {
-		return WC_Payments_Multi_Currency()->get_enabled_currencies();
+		return $currencies;
 	}
 }
