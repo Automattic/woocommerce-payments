925d588175c0925c6ae06340c286f598855dc0ef
Luiz Reis <luiz.reis@automattic.com>
2021-05-21 10:37:43 -0300

    Update price convertion test comments

--- tests/unit/multi-currency/test-class-multi-currency.php
+++ tests/unit/multi-currency/test-class-multi-currency.php
@@ -72,7 +72,7 @@ class WCPay_Multi_Currency_Tests extends WP_UnitTestCase {
 		WC()->session->set( WCPay\Multi_Currency\Multi_Currency::CURRENCY_SESSION_KEY, 'GBP' );
 		add_filter( 'wcpay_multi_currency_apply_charm_only_to_products', '__return_true' );
 
-		// 0.708099 * 10 = 7,08099 -> rounded to 7 -> 7 - 0.1 = 6.9
+		// 0.708099 * 10 = 7,08099 -> ceiled to 8 -> 8 - 0.1 = 7.9
 		$this->assertSame( 7.9, $this->multi_currency->get_price( '10.0' ) );
 	}
 
@@ -80,7 +80,7 @@ class WCPay_Multi_Currency_Tests extends WP_UnitTestCase {
 		WC()->session->set( WCPay\Multi_Currency\Multi_Currency::CURRENCY_SESSION_KEY, 'GBP' );
 		add_filter( 'wcpay_multi_currency_apply_charm_only_to_products', '__return_false' );
 
-		// 0.708099 * 10 = 7,08099 -> rounded to 7 -> 7 - 0.1 = 6.9
+		// 0.708099 * 10 = 7,08099 -> ceiled to 8 -> 8 - 0.1 = 7.9
 		$this->assertSame( 7.9, $this->multi_currency->get_price( '10.0', false ) );
 	}
 
@@ -88,7 +88,7 @@ class WCPay_Multi_Currency_Tests extends WP_UnitTestCase {
 		WC()->session->set( WCPay\Multi_Currency\Multi_Currency::CURRENCY_SESSION_KEY, 'GBP' );
 		add_filter( 'wcpay_multi_currency_apply_charm_only_to_products', '__return_true' );
 
-		// 0.708099 * 10 = 7,08099 -> rounded to 7 -> 7 - 0.1 = 6.9
+		// 0.708099 * 10 = 7,08099 -> ceiled to 8 -> 8 + 0.0 = 8.0
 		$this->assertSame( 8.0, $this->multi_currency->get_price( '10.0', false ) );
 	}
 
