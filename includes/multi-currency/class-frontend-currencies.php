ce1fcda7d1a57a448b9df09a9b106978bf141dde
Luiz Reis <luiz.reis@automattic.com>
2021-05-21 14:14:21 -0300

    Add currency formatting to Frontend_Currencies
    This commit uses the locale data from WooCommerce core (/i18n/locale-info.php)
    and extracts the currency settings from it. It also adds the required methods
    to properly format a currency in the frontend, dealing with number of decimals,
    decimal and thousand separators and price formatting.

--- includes/multi-currency/class-frontend-currencies.php
+++ includes/multi-currency/class-frontend-currencies.php
@@ -21,6 +21,13 @@ class Frontend_Currencies {
 	protected $multi_currency;
 
 	/**
+	 * Multi-Currency currency formatting map.
+	 *
+	 * @var array
+	 */
+	protected $currency_settings = [];
+
+	/**
 	 * Constructor.
 	 *
 	 * @param Multi_Currency $multi_currency The Multi_Currency instance.
@@ -28,8 +35,14 @@ class Frontend_Currencies {
 	public function __construct( Multi_Currency $multi_currency ) {
 		$this->multi_currency = $multi_currency;
 
+		$this->load_locale_data();
+
 		// Currency hooks.
 		add_filter( 'woocommerce_currency', [ $this, 'get_current_currency_code' ] );
+		add_filter( 'wc_get_price_decimals', [ $this, 'get_current_currency_decimals' ] );
+		add_filter( 'wc_get_price_decimal_separator', [ $this, 'get_current_currency_decimal_separator' ] );
+		add_filter( 'wc_get_price_thousand_separator', [ $this, 'get_current_currency_thousand_separator' ] );
+		add_filter( 'woocommerce_price_format', [ $this, 'get_current_currency_format' ] );
 	}
 
 	/**
@@ -39,5 +52,104 @@ class Frontend_Currencies {
 	 */
 	public function get_current_currency_code() {
 		return $this->multi_currency->get_selected_currency()->get_code();
+	}
+
+	/**
+	 * Returns the number of decimals to be used by WooCommerce.
+	 *
+	 * @return int The number of decimals.
+	 */
+	public function get_current_currency_decimals() {
+		$currency_code = $this->multi_currency->get_selected_currency()->get_code();
+		return $this->get_currency_settings( $currency_code )['num_decimals'];
+	}
+
+	/**
+	 * Returns the decimal separator to be used by WooCommerce.
+	 *
+	 * @return int The decimal separator.
+	 */
+	public function get_current_currency_decimal_separator() {
+		$currency_code = $this->multi_currency->get_selected_currency()->get_code();
+		return $this->get_currency_settings( $currency_code )['decimal_sep'];
+	}
+
+	/**
+	 * Returns the thousand separator to be used by WooCommerce.
+	 *
+	 * @return int The thousand separator.
+	 */
+	public function get_current_currency_thousand_separator() {
+		$currency_code = $this->multi_currency->get_selected_currency()->get_code();
+		return $this->get_currency_settings( $currency_code )['thousand_sep'];
+	}
+
+	/**
+	 * Returns the currency format to be used by WooCommerce.
+	 *
+	 * @return int The currency format.
+	 */
+	public function get_current_currency_format() {
+		$currency_code = $this->multi_currency->get_selected_currency()->get_code();
+		$currency_pos  = $this->get_currency_settings( $currency_code )['currency_pos'];
+
+		switch ( $currency_pos ) {
+			case 'left':
+				return '%1$s%2$s';
+			case 'right':
+				return '%2$s%1$s';
+			case 'left_space':
+				return '%1$s&nbsp;%2$s';
+			case 'right_space':
+				return '%2$s&nbsp;%1$s';
+			default:
+				return '%1$s%2$s';
+		}
+	}
+
+	/**
+	 * Retrieves the currency's settings from mapped data.
+	 *
+	 * @param string $currency_code The currency code.
+	 *
+	 * @return array The currency's settings.
+	 */
+	private function get_currency_settings( $currency_code ) {
+		// Default to USD settings if mapping not found.
+		$currency_settings = $this->currency_settings[ $currency_code ] ?? [
+			'currency_pos' => 'left',
+			'thousand_sep' => ',',
+			'decimal_sep'  => '.',
+			'num_decimals' => 2,
+		];
+
+		return apply_filters( 'wcpay_multi_currency_currency_settings', $currency_settings, $currency_code );
+	}
+
+	/**
+	 * Loads locale data from WooCommerce core (/i18n/locale-info.php) and maps it
+	 * to be used by currency.
+	 */
+	private function load_locale_data() {
+		$locale_info = include WC()->plugin_path() . '/i18n/locale-info.php';
+
+		// Extract the currency formatting options from the locale info.
+		foreach ( $locale_info as $locale ) {
+			$currency_code = $locale['currency_code'];
+
+			// Convert Norwegian Krone symbol to its ISO 4217 currency code.
+			if ( 'Kr' === $currency_code ) {
+				$currency_code = 'NOK';
+			}
+
+			if ( empty( $this->currency_settings[ $currency_code ] ) ) {
+				$this->currency_settings[ $currency_code ] = [
+					'currency_pos' => $locale['currency_pos'],
+					'thousand_sep' => $locale['thousand_sep'],
+					'decimal_sep'  => $locale['decimal_sep'],
+					'num_decimals' => $locale['num_decimals'],
+				];
+			}
+		}
 	}
 }
--- tests/unit/multi-currency/test-class-frontend-currencies.php
+++ tests/unit/multi-currency/test-class-frontend-currencies.php
@@ -23,9 +23,6 @@ class WCPay_Multi_Currency_Frontend_Currencies_Tests extends WP_UnitTestCase {
 	 */
 	private $frontend_currencies;
 
-	/**
-	 * Pre-test setup
-	 */
 	public function setUp() {
 		parent::setUp();
 
@@ -34,10 +31,132 @@ class WCPay_Multi_Currency_Frontend_Currencies_Tests extends WP_UnitTestCase {
 		$this->frontend_currencies = new WCPay\Multi_Currency\Frontend_Currencies( $this->mock_multi_currency );
 	}
 
+	public function tearDown() {
+		remove_all_filters( 'wcpay_multi_currency_currency_settings' );
+
+		parent::tearDown();
+	}
+
 	public function test_get_currency_code() {
 		$current_currency = new WCPay\Multi_Currency\Currency( 'USD' );
 		$this->mock_multi_currency->method( 'get_selected_currency' )->willReturn( $current_currency );
 
 		$this->assertSame( 'USD', $this->frontend_currencies->get_current_currency_code() );
+	}
+
+	public function test_get_current_currency_decimals_returns_default_settings() {
+		$current_currency = new WCPay\Multi_Currency\Currency( 'RANDOM_CODE' );
+		$this->mock_multi_currency->method( 'get_selected_currency' )->willReturn( $current_currency );
+
+		$this->assertEquals( 2, $this->frontend_currencies->get_current_currency_decimals() );
+	}
+
+	public function test_get_current_currency_decimals_returns_currency_settings() {
+		$current_currency = new WCPay\Multi_Currency\Currency( 'JPY' );
+		$this->mock_multi_currency->method( 'get_selected_currency' )->willReturn( $current_currency );
+
+		$this->assertEquals( 0, $this->frontend_currencies->get_current_currency_decimals() );
+	}
+
+	public function test_get_current_currency_decimals_returns_filtered_settings() {
+		$current_currency = new WCPay\Multi_Currency\Currency( 'JPY' );
+		$this->mock_multi_currency->method( 'get_selected_currency' )->willReturn( $current_currency );
+		$this->mock_currency_settings( [ 'num_decimals' => 1 ] );
+
+		$this->assertEquals( 1, $this->frontend_currencies->get_current_currency_decimals() );
+	}
+
+	public function test_get_current_currency_decimal_separator_returns_default_settings() {
+		$current_currency = new WCPay\Multi_Currency\Currency( 'RANDOM_CODE' );
+		$this->mock_multi_currency->method( 'get_selected_currency' )->willReturn( $current_currency );
+
+		$this->assertEquals( '.', $this->frontend_currencies->get_current_currency_decimal_separator() );
+	}
+
+	public function test_get_current_currency_decimal_separator_returns_currency_settings() {
+		$current_currency = new WCPay\Multi_Currency\Currency( 'EUR' );
+		$this->mock_multi_currency->method( 'get_selected_currency' )->willReturn( $current_currency );
+
+		$this->assertEquals( ',', $this->frontend_currencies->get_current_currency_decimal_separator() );
+	}
+
+	public function test_get_current_currency_decimal_separator_returns_filtered_settings() {
+		$current_currency = new WCPay\Multi_Currency\Currency( 'EUR' );
+		$this->mock_multi_currency->method( 'get_selected_currency' )->willReturn( $current_currency );
+		$this->mock_currency_settings( [ 'decimal_sep' => '/' ] );
+
+		$this->assertEquals( '/', $this->frontend_currencies->get_current_currency_decimal_separator() );
+	}
+
+	public function test_get_current_currency_thousand_separator_returns_default_settings() {
+		$current_currency = new WCPay\Multi_Currency\Currency( 'RANDOM_CODE' );
+		$this->mock_multi_currency->method( 'get_selected_currency' )->willReturn( $current_currency );
+
+		$this->assertEquals( ',', $this->frontend_currencies->get_current_currency_thousand_separator() );
+	}
+
+	public function test_get_current_currency_thousand_separator_returns_currency_settings() {
+		$current_currency = new WCPay\Multi_Currency\Currency( 'EUR' );
+		$this->mock_multi_currency->method( 'get_selected_currency' )->willReturn( $current_currency );
+
+		$this->assertEquals( '.', $this->frontend_currencies->get_current_currency_thousand_separator() );
+	}
+
+	public function test_get_current_currency_thousand_separator_returns_filtered_settings() {
+		$current_currency = new WCPay\Multi_Currency\Currency( 'EUR' );
+		$this->mock_multi_currency->method( 'get_selected_currency' )->willReturn( $current_currency );
+		$this->mock_currency_settings( [ 'thousand_sep' => '/' ] );
+
+		$this->assertEquals( '/', $this->frontend_currencies->get_current_currency_thousand_separator() );
+	}
+
+	public function test_get_current_currency_format_returns_default_settings() {
+		$current_currency = new WCPay\Multi_Currency\Currency( 'RANDOM_CODE' );
+		$this->mock_multi_currency->method( 'get_selected_currency' )->willReturn( $current_currency );
+
+		$this->assertEquals( '%1$s%2$s', $this->frontend_currencies->get_current_currency_format() );
+	}
+
+	public function test_get_current_currency_format_returns_currency_settings() {
+		$current_currency = new WCPay\Multi_Currency\Currency( 'HUF' );
+		$this->mock_multi_currency->method( 'get_selected_currency' )->willReturn( $current_currency );
+
+		$this->assertEquals( '%2$s&nbsp;%1$s', $this->frontend_currencies->get_current_currency_format() );
+	}
+
+	public function test_get_current_currency_format_returns_filtered_settings() {
+		$current_currency = new WCPay\Multi_Currency\Currency( 'HUF' );
+		$this->mock_multi_currency->method( 'get_selected_currency' )->willReturn( $current_currency );
+		$this->mock_currency_settings( [ 'currency_pos' => 'left_space' ] );
+
+		$this->assertEquals( '%1$s&nbsp;%2$s', $this->frontend_currencies->get_current_currency_format() );
+	}
+
+	/**
+	 * @dataProvider currency_format_provider
+	 */
+	public function test_get_current_currency_format_outputs_right_format( $currency_pos, $expected_format ) {
+		$this->mock_currency_settings( [ 'currency_pos' => $currency_pos ] );
+		$this->assertEquals( $expected_format, $this->frontend_currencies->get_current_currency_format() );
+	}
+
+	public function currency_format_provider() {
+		return [
+			[ '', '%1$s%2$s' ],
+			[ 'random_value', '%1$s%2$s' ],
+			[ 'left', '%1$s%2$s' ],
+			[ 'right', '%2$s%1$s' ],
+			[ 'left_space', '%1$s&nbsp;%2$s' ],
+			[ 'right_space', '%2$s&nbsp;%1$s' ],
+		];
+	}
+
+	private function mock_currency_settings( $currency_settings ) {
+		add_filter(
+			'wcpay_multi_currency_currency_settings',
+			function () use ( $currency_settings ) {
+				return $currency_settings;
+			}
+		);
 	}
 }
