0c1e37d179075b62324458c9e4da0699ca464434
Jesse Pearson <jessepearson@users.noreply.github.com>
2021-06-01 13:02:12 -0300

    Set manual exchange rate field to hide if automatic rate is chosen. (#1977)
    * Set manual exchange rate field to hide if automatic rate is chosen.
    
    * Removed onload event and switched to use forEach to iterate through radio options.

--- client/multi-currency/index.js
+++ client/multi-currency/index.js
@@ -16,3 +16,22 @@ const currencyContainer = document.getElementById(
 if ( currencyContainer ) {
 	ReactDOM.render( <EnabledCurrencies />, currencyContainer );
 }
+
+const hideShowManualField = ( show ) => {
+	const manualRateField = document
+		.querySelector( '[id^=wcpay_multi_currency_manual_rate_]' )
+		.closest( 'tr' );
+	manualRateField.style.display = show ? 'table-row' : 'none';
+};
+
+const triggerHideShow = ( value, checked ) => {
+	hideShowManualField( 'manual' === value && true === checked );
+};
+
+document.querySelectorAll( '.exchange-rate-selector' ).forEach( ( radio ) => {
+	triggerHideShow( radio.value, radio.checked );
+
+	radio.addEventListener( 'change', ( event ) => {
+		triggerHideShow( event.target.value, event.target.checked );
+	} );
+} );
