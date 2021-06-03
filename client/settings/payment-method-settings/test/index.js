0c6a321e233ca08da9bd9860d2de82575144c106
frosso <francesco.rosso@automattic.com>
2021-06-03 12:03:40 -0500

    making the settings page work

--- client/settings/payment-method-settings/test/index.js
+++ client/settings/payment-method-settings/test/index.js
@@ -16,9 +16,7 @@ jest.mock( '../../../data', () => ( {
 
 describe( 'PaymentMethodSettings', () => {
 	test( 'renders title and description', () => {
-		render(
-			<PaymentMethodSettings methodId="woocommerce_payments_digital_wallets" />
-		);
+		render( <PaymentMethodSettings methodId="digital_wallets" /> );
 
 		const heading = screen.queryByRole( 'heading', {
 			name: 'Express checkouts',
@@ -27,17 +25,13 @@ describe( 'PaymentMethodSettings', () => {
 	} );
 
 	test( 'renders settings', () => {
-		render(
-			<PaymentMethodSettings methodId="woocommerce_payments_digital_wallets" />
-		);
+		render( <PaymentMethodSettings methodId="digital_wallets" /> );
 
 		expect( screen.queryByText( 'Call to action' ) ).toBeInTheDocument();
 	} );
 
 	test( 'renders breadcrumbs', () => {
-		render(
-			<PaymentMethodSettings methodId="woocommerce_payments_digital_wallets" />
-		);
+		render( <PaymentMethodSettings methodId="digital_wallets" /> );
 
 		const linkToPayments = screen.getByRole( 'link', {
 			name: 'WooCommerce Payments',
@@ -60,9 +54,7 @@ describe( 'PaymentMethodSettings', () => {
 	} );
 
 	test( 'renders digital wallets settings and confirm its h2 copy', () => {
-		render(
-			<PaymentMethodSettings methodId="woocommerce_payments_digital_wallets" />
-		);
+		render( <PaymentMethodSettings methodId="digital_wallets" /> );
 
 		const heading = screen.queryByRole( 'heading', {
 			name: 'Express checkouts',
@@ -71,9 +63,7 @@ describe( 'PaymentMethodSettings', () => {
 	} );
 
 	test( 'renders banner at the top', () => {
-		render(
-			<PaymentMethodSettings methodId="woocommerce_payments_digital_wallets" />
-		);
+		render( <PaymentMethodSettings methodId="digital_wallets" /> );
 
 		const banner = screen.queryByTitle( 'WooCommerce Payments' );
 		expect( banner ).toBeInTheDocument();
