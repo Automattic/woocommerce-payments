f46b830e5677c0b643be965465bf5bb8d381e225
frosso <francesco.rosso@automattic.com>
2021-06-03 12:44:05 -0500

    JS tests

--- client/settings/payment-method-settings/test/digital-wallets-settings.js
+++ client/settings/payment-method-settings/test/digital-wallets-settings.js
@@ -10,13 +10,24 @@ import { render, screen } from '@testing-library/react';
  */
 import DigitalWalletsSettings from '../digital-wallets-settings';
 
+jest.mock( '../../../data', () => ( {
+	useSettings: jest.fn().mockReturnValue( {} ),
+	useDigitalWalletsButtonActionType: jest.fn().mockReturnValue( [ 'buy' ] ),
+	useDigitalWalletsButtonSize: jest.fn().mockReturnValue( [ 'default' ] ),
+	useDigitalWalletsButtonTheme: jest.fn().mockReturnValue( [ 'dark' ] ),
+} ) );
+
 describe( 'DigitalWalletsSettings', () => {
 	test( 'renders settings with defaults', () => {
 		render( <DigitalWalletsSettings /> );
 
 		// confirm settings headings
-		expect( screen.queryByText( 'Call to action' ) ).toBeInTheDocument();
-		expect( screen.queryByText( 'Appearance' ) ).toBeInTheDocument();
+		expect(
+			screen.queryByRole( 'heading', { name: 'Call to action' } )
+		).toBeInTheDocument();
+		expect(
+			screen.queryByRole( 'heading', { name: 'Appearance' } )
+		).toBeInTheDocument();
 
 		// confirm radio button groups displayed
 		const [ ctaRadio, sizeRadio, themeRadio ] = screen.getAllByRole(
@@ -30,6 +41,6 @@ describe( 'DigitalWalletsSettings', () => {
 		// confirm default values
 		expect( screen.getByLabelText( 'Buy' ) ).toBeChecked();
 		expect( screen.getByLabelText( 'Default (40 px)' ) ).toBeChecked();
-		expect( screen.getByLabelText( 'Dark' ) ).toBeChecked();
+		expect( screen.getByLabelText( /Dark/ ) ).toBeChecked();
 	} );
 } );
