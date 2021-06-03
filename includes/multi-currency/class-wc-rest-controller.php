aac3270fecd3dd4f0ab829f74fd3767fdb7de36b
Jesse Pearson <jessepearson@users.noreply.github.com>
2021-06-03 10:32:33 -0300

    Initial start of settings for multi-currency (#1762)
    * Rebase settings branch to feature branch.
    
    * Create data store for multi-currecy feature.
    
    * Fix copy/paste errors, set hook and selector return names/types correctly.
    
    * Rebased to feature branch.
    
    * Fix rebasing error.
    
    * Basic functionality for customer multi currency settings.
    
    * Initial start of settings for multi-currency. Items added, but not saving, styled, or dynamic.
    
    * Initial start of settings for multi-currency. Items added, but not saving, styled, or dynamic.
    
    * Fix merge conflicts.
    
    * Fix merge errors after rebase
    
    * Fix merge errors after rebase
    
    * Minor text changes.
    
    * Fix failing test for multi currency json encode
    
    * Fix enabled currencies list so that it reloads on update. (#1956)
    
    * Fix styling for enabled currencies components, cleaned up classnames. (#1929)
    
    * Updated currency symbols to display correctly for enabled currencies. (#1930)
    
    * Fix styling for enabled currencies components, cleaned up classnames.
    
    * Updated currency symbols to display correctly for enabled currencies.
    
    * Better solution for displaying currency symbols.
    
    * Better solution for displaying currency symbols.
    
    * Fix lint errors after conflict error fix.
    
    * Set manual exchange rate field to hide if automatic rate is chosen. (#1977)
    
    * Set manual exchange rate field to hide if automatic rate is chosen.
    
    * Removed onload event and switched to use forEach to iterate through radio options.
    
    * Properly set manual rate if currency uses manual rate. (#1986)
    
    * Search functionality for customer multi currencies available modal (#1971)
    
    * WIP initial implementation of enabled currencies search
    
    * Create a basic search component
    
    That uses WC style plus a search icon, and forward all props to the input element
    
    * Use the new search component and minor refactor
    
    * Move symbol entity decode from JS to PHP on jsonSerialize
    To allow search by symbol
    
    * Style search & footer to include a full width separator
    
    * Fix extra whitespace
    
    * Remove TODO
    
    * Add instance of Multi_Currency class directly into Settings, define parameters before assignment. (#2007)
    
    * Separate initialization and getting of currency arrays. (#2006)
    
    * Separate initialization and getting of currency arrays.
    
    * Separate initialization and getting of currency arrays.
    
    * Set default currency to null if it's not found in available currencies.
    
    * Removed store settings and preview option in single currency, these will be addressed soon.
    
    * Fix errors and failing tests after conflict merge.
    
    Co-authored-by: Ismael Martín Alabarce <info@ismaeld.com>

--- includes/multi-currency/class-wc-rest-controller.php
+++ includes/multi-currency/class-wc-rest-controller.php
@@ -34,6 +34,16 @@ class WC_REST_Controller extends \WC_Payments_REST_Controller {
 				'permission_callback' => [ $this, 'check_permission' ],
 			]
 		);
+
+		register_rest_route(
+			$this->namespace,
+			'/' . $this->rest_base . '/update-enabled-currencies',
+			[
+				'methods'             => \WP_REST_Server::CREATABLE,
+				'callback'            => [ $this, 'update_enabled_currencies' ],
+				'permission_callback' => [ $this, 'check_permission' ],
+			]
+		);
 	}
 
 	/**
@@ -45,5 +55,16 @@ class WC_REST_Controller extends \WC_Payments_REST_Controller {
 			'enabled'   => WC_Payments_Multi_Currency()->get_enabled_currencies(),
 			'default'   => WC_Payments_Multi_Currency()->get_default_currency(),
 		];
+	}
+
+	/**
+	 * Update enabled currencies based on posted data.
+	 *
+	 * @param WP_REST_Request $request Full data about the request.
+	 */
+	public function update_enabled_currencies( $request ) {
+		$params = $request->get_params();
+		WC_Payments_Multi_Currency()->set_enabled_currencies( $params['enabled'] );
+		return $this->get_store_currencies();
 	}
 }
