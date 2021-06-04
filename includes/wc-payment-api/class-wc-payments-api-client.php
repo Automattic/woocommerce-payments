b344421edd2f9f00d67f01f9c8ba9822468d5527
Daniel Mallory <daniel.mallory@automattic.com>
2021-06-04 12:02:09 +0100

    Initial commit, set up functionality to fetch and cache rates.

--- includes/wc-payment-api/class-wc-payments-api-client.php
+++ includes/wc-payment-api/class-wc-payments-api-client.php
@@ -30,6 +30,7 @@ class WC_Payments_API_Client {
 	const CHARGES_API         = 'charges';
 	const CONN_TOKENS_API     = 'terminal/connection_tokens';
 	const CUSTOMERS_API       = 'customers';
+	const CURRENCY_API        = 'currency';
 	const INTENTIONS_API      = 'intentions';
 	const REFUNDS_API         = 'refunds';
 	const DEPOSITS_API        = 'deposits';
@@ -778,6 +779,25 @@ class WC_Payments_API_Client {
 	 */
 	public function get_timeline( $intention_id ) {
 		return $this->request( [], self::TIMELINE_API . '/' . $intention_id, self::GET );
+	}
+
+	/**
+	 * Get currency rates from the server.
+	 *
+	 * @param string $currency_from - The currency to convert from.
+	 * @param array  $currencies_to - An array of the currencies we want to convert into.
+	 *
+	 * @return array
+	 */
+	public function get_currency_rates( $currency_from, $currencies_to ) {
+		return $this->request(
+			[
+				'currency_from' => $currency_from,
+				'currencies_to' => $currencies_to,
+			],
+			self::CURRENCY_API . '/rates',
+			self::GET
+		);
 	}
 
 	/**
