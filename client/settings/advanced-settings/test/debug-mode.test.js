30c1271c0d623e859da0a2e447374c8b0e6684e7
Waclaw Jacek <waclaw.jacek@automattic.com>
2021-06-04 12:51:22 +0200

    Extract AdvancedSettings tests to added component tests

---
+++ client/settings/advanced-settings/test/debug-mode.test.js
@@ -0,0 +1,63 @@
+/** @format **/
+
+/**
+ * External dependencies
+ */
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+/**
+ * Internal dependencies
+ */
+import { useDebugLog, useDevMode } from 'data';
+import DebugMode from '../debug-mode';
+
+jest.mock( '../../../data', () => ( {
+	useDevMode: jest.fn().mockReturnValue( false ),
+	useDebugLog: jest.fn().mockReturnValue( [ false, jest.fn() ] ),
+} ) );
+
+describe( 'DebugMode', () => {
+	afterEach( () => {
+		jest.clearAllMocks();
+	} );
+
+	it( 'toggles the logging checkbox', () => {
+		const setDebugLogMock = jest.fn();
+		useDebugLog.mockReturnValue( [ false, setDebugLogMock ] );
+
+		render( <DebugMode /> );
+
+		const loggingCheckbox = screen.queryByRole( 'checkbox', {
+			name: 'Log error messages',
+		} );
+
+		expect(
+			screen.queryByText(
+				'Dev mode is active so logging is on by default.'
+			)
+		).not.toBeInTheDocument();
+		expect( loggingCheckbox ).not.toBeChecked();
+		expect( setDebugLogMock ).not.toHaveBeenCalled();
+
+		userEvent.click( loggingCheckbox );
+
+		expect( setDebugLogMock ).toHaveBeenCalledWith( true );
+	} );
+
+	it( 'prevents toggling the logging checkbox when dev mode is active', () => {
+		useDevMode.mockReturnValue( true );
+
+		render( <DebugMode /> );
+
+		const loggingCheckbox = screen.queryByRole( 'checkbox', {
+			name: 'Dev mode is active so logging is on by default.',
+		} );
+
+		expect(
+			screen.queryByText( 'Log error messages' )
+		).not.toBeInTheDocument();
+		expect( loggingCheckbox ).toBeChecked();
+		expect( loggingCheckbox ).toBeDisabled();
+	} );
+} );
--- client/settings/advanced-settings/test/index.test.js
+++ client/settings/advanced-settings/test/index.test.js
@@ -10,152 +10,49 @@ import userEvent from '@testing-library/user-event';
  * Internal dependencies
  */
 import AdvancedSettings from '..';
-import { useDebugLog, useDevMode } from '../../../data';
-
-jest.mock( '../../../data', () => ( {
-	useDevMode: jest.fn().mockReturnValue( false ),
-	useDebugLog: jest.fn().mockReturnValue( [ false, jest.fn() ] ),
-} ) );
+import WCPaySettingsContext from '../../wcpay-settings-context';
 
 describe( 'AdvancedSettings', () => {
-	afterEach( () => {
-		jest.clearAllMocks();
-	} );
-
 	it( 'toggles the advanced settings section', () => {
-		render( <AdvancedSettings /> );
+		const settingsContext = {
+			featureFlags: { upe: true },
+		};
+
+		render(
+			<WCPaySettingsContext.Provider value={ settingsContext }>
+				<AdvancedSettings />
+			</WCPaySettingsContext.Provider>
+		);
 
 		expect( screen.queryByText( 'Debug mode' ) ).not.toBeInTheDocument();
+		expect(
+			screen.queryByText( 'Block appearance' )
+		).not.toBeInTheDocument();
 
 		userEvent.click( screen.getByText( 'Advanced settings' ) );
 
 		expect( screen.queryByText( 'Debug mode' ) ).toBeInTheDocument();
-		expect( screen.getByText( 'Debug mode' ) ).toHaveFocus();
-	} );
-
-	it( 'toggles the logging checkbox', () => {
-		const setDebugLogMock = jest.fn();
-		useDebugLog.mockReturnValue( [ false, setDebugLogMock ] );
-
-		render( <AdvancedSettings /> );
-
-		userEvent.click( screen.getByText( 'Advanced settings' ) );
-
-		const loggingCheckbox = screen.queryByRole( 'checkbox', {
-			name: 'Log error messages',
-		} );
-
+		expect( screen.queryByText( 'Block appearance' ) ).toBeInTheDocument();
 		expect(
-			screen.queryByText(
-				'Dev mode is active so logging is on by default.'
-			)
-		).not.toBeInTheDocument();
-		expect( loggingCheckbox ).not.toBeChecked();
-		expect( setDebugLogMock ).not.toHaveBeenCalled();
-
-		userEvent.click( loggingCheckbox );
-
-		expect( setDebugLogMock ).toHaveBeenCalledWith( true );
-	} );
-
-	it( 'prevents toggling the logging checkbox when dev mode is active', () => {
-		useDevMode.mockReturnValue( true );
-
-		render( <AdvancedSettings /> );
-
-		userEvent.click( screen.getByText( 'Advanced settings' ) );
-
-		const loggingCheckbox = screen.queryByRole( 'checkbox', {
-			name: 'Dev mode is active so logging is on by default.',
-		} );
-
-		expect(
-			screen.queryByText( 'Log error messages' )
-		).not.toBeInTheDocument();
-		expect( loggingCheckbox ).toBeChecked();
-		expect( loggingCheckbox ).toBeDisabled();
-	} );
-
-	it( 'focuses on the "custom font" input when the checkbox is checked', () => {
-		render( <AdvancedSettings /> );
-
-		userEvent.click( screen.getByText( 'Advanced settings' ) );
-
-		expect( screen.getByText( 'Debug mode' ) ).toHaveFocus();
-
-		expect(
-			screen.queryByLabelText( 'Custom font URL' )
-		).not.toBeInTheDocument();
-
-		userEvent.click( screen.getByText( 'Use a custom font' ) );
-		userEvent.tab();
-
-		expect( screen.getByLabelText( 'Custom font URL' ) ).toHaveFocus();
-
-		userEvent.click( screen.getByText( 'Use a custom font' ) );
-
-		expect(
-			screen.queryByLabelText( 'Custom font URL' )
-		).not.toBeInTheDocument();
-	} );
-
-	it( 'focuses on the "Additional CSS styling" input when the checkbox is checked', () => {
-		render( <AdvancedSettings /> );
-
-		userEvent.click( screen.getByText( 'Advanced settings' ) );
-
-		expect( screen.getByText( 'Debug mode' ) ).toHaveFocus();
-
-		expect(
-			screen.queryByLabelText( 'Additional CSS styling' )
-		).not.toBeInTheDocument();
-
-		userEvent.click( screen.getByText( 'Add CSS styling' ) );
-		userEvent.tab();
-
-		expect(
-			screen.getByLabelText( 'Additional CSS styling' )
+			screen.getByText( 'Debug mode' ).closest( 'div' )
 		).toHaveFocus();
-
-		userEvent.click( screen.getByText( 'Add CSS styling' ) );
-
-		expect(
-			screen.queryByLabelText( 'Additional CSS styling' )
-		).not.toBeInTheDocument();
 	} );
 
-	it( 'persists the "Additional CSS styling" and "custom font" input values when the checkbox is toggled', () => {
-		render( <AdvancedSettings /> );
+	it( 'BlockAppearance is not displayed if UPE feature flag is disabled', () => {
+		const settingsContext = {
+			featureFlags: {},
+		};
+
+		render(
+			<WCPaySettingsContext.Provider value={ settingsContext }>
+				<AdvancedSettings />
+			</WCPaySettingsContext.Provider>
+		);
 
 		userEvent.click( screen.getByText( 'Advanced settings' ) );
 
-		// make the text inputs visible
-		userEvent.click( screen.getByText( 'Add CSS styling' ) );
-		userEvent.click( screen.getByText( 'Use a custom font' ) );
-
-		userEvent.type(
-			screen.getByLabelText( 'Custom font URL' ),
-			'https://fonts.googleapis.com/css?family=Source+Sans+Pro'
-		);
-		userEvent.type(
-			screen.getByLabelText( 'Additional CSS styling' ),
-			'.some-selector { color: blue; }'
-		);
-
-		// make the text inputs not visible
-		userEvent.click( screen.getByText( 'Add CSS styling' ) );
-		userEvent.click( screen.getByText( 'Use a custom font' ) );
-
-		// make the text inputs visible again
-		userEvent.click( screen.getByText( 'Add CSS styling' ) );
-		userEvent.click( screen.getByText( 'Use a custom font' ) );
-
-		// they should have some values entered
-		expect( screen.getByLabelText( 'Custom font URL' ).value ).toBe(
-			'https://fonts.googleapis.com/css?family=Source+Sans+Pro'
-		);
-		expect( screen.getByLabelText( 'Additional CSS styling' ).value ).toBe(
-			'.some-selector { color: blue; }'
-		);
+		expect(
+			screen.queryByText( 'Block appearance' )
+		).not.toBeInTheDocument();
 	} );
 } );
