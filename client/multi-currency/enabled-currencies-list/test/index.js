346ed969aefb61c66fc779cac5495df6647f3be9
Jesse Pearson <jesse.pearson@automattic.com>
2021-06-04 09:52:24 -0300

    Tests for enabled currencies list.

---
+++ client/multi-currency/enabled-currencies-list/test/index.js
@@ -0,0 +1,208 @@
+/* eslint-disable camelcase */
+/**
+ * External dependencies
+ */
+import React from 'react';
+import { fireEvent, render, screen } from '@testing-library/react';
+
+/**
+ * Internal dependencies
+ */
+import EnabledCurrencies from '../';
+import {
+	useCurrencies,
+	useAvailableCurrencies,
+	useDefaultCurrency,
+	useEnabledCurrencies,
+} from 'data';
+
+jest.mock( 'data', () => ( {
+	useCurrencies: jest.fn(),
+	useAvailableCurrencies: jest.fn(),
+	useDefaultCurrency: jest.fn(),
+	useEnabledCurrencies: jest.fn(),
+} ) );
+
+const availableCurrencies = {
+	USD: {
+		code: 'USD',
+		rate: 1,
+		name: 'United States (US) dollar',
+		id: 'usd',
+		is_default: true,
+		flag: '🇺🇸',
+		symbol: '$',
+	},
+	CAD: {
+		code: 'CAD',
+		rate: '1.206823',
+		name: 'Canadian dollar',
+		id: 'cad',
+		is_default: false,
+		flag: '🇨🇦',
+		symbol: '$',
+	},
+	GBP: {
+		code: 'GBP',
+		rate: '0.708099',
+		name: 'Pound sterling',
+		id: 'gbp',
+		is_default: false,
+		flag: '🇬🇧',
+		symbol: '£',
+	},
+	EUR: {
+		code: 'EUR',
+		rate: '0.826381',
+		name: 'Euro',
+		id: 'eur',
+		is_default: false,
+		flag: '🇪🇺',
+		symbol: '€',
+	},
+	AED: {
+		code: 'AED',
+		rate: '3.6732',
+		name: 'United Arab Emirates dirham',
+		id: 'aed',
+		is_default: false,
+		flag: '🇦🇪',
+		symbol: 'د.إ',
+	},
+	CDF: {
+		code: 'CDF',
+		rate: '2000',
+		name: 'Congolese franc',
+		id: 'cdf',
+		is_default: false,
+		flag: '🇨🇩',
+		symbol: 'Fr',
+	},
+	NZD: {
+		code: 'NZD',
+		rate: '1.387163',
+		name: 'New Zealand dollar',
+		id: 'nzd',
+		is_default: false,
+		flag: '🇳🇿',
+		symbol: '$',
+	},
+	DKK: {
+		code: 'DKK',
+		rate: '6.144615',
+		name: 'Danish krone',
+		id: 'dkk',
+		is_default: false,
+		flag: '🇩🇰',
+		symbol: 'DKK',
+	},
+	BIF: {
+		code: 'BIF',
+		rate: '1974',
+		name: 'Burundian franc',
+		id: 'bif',
+		is_default: false,
+		flag: '🇧🇮',
+		symbol: 'Fr',
+	},
+	CLP: {
+		code: 'CLP',
+		rate: '706.8',
+		name: 'Chilean peso',
+		id: 'clp',
+		is_default: false,
+		flag: '🇨🇱',
+		symbol: '$',
+	},
+};
+
+const enabledCurrencies = {
+	USD: {
+		code: 'USD',
+		rate: 1,
+		name: 'United States (US) dollar',
+		id: 'usd',
+		is_default: true,
+		flag: '🇺🇸',
+		symbol: '$',
+	},
+	CAD: {
+		code: 'CAD',
+		rate: '1.42',
+		name: 'Canadian dollar',
+		id: 'cad',
+		is_default: false,
+		flag: '🇨🇦',
+		symbol: '$',
+	},
+	GBP: {
+		code: 'GBP',
+		rate: '0.708099',
+		name: 'Pound sterling',
+		id: 'gbp',
+		is_default: false,
+		flag: '🇬🇧',
+		symbol: '£',
+	},
+	EUR: {
+		code: 'EUR',
+		rate: '0.826381',
+		name: 'Euro',
+		id: 'eur',
+		is_default: false,
+		flag: '🇪🇺',
+		symbol: '€',
+	},
+};
+
+const defaultCurrency = {
+	code: 'USD',
+	rate: 1,
+	name: 'United States (US) dollar',
+	id: 'usd',
+	is_default: true,
+	flag: '🇺🇸',
+	symbol: '$',
+};
+
+useCurrencies.mockReturnValue( {
+	available: availableCurrencies,
+	enabled: enabledCurrencies,
+	default: defaultCurrency,
+} );
+
+useAvailableCurrencies.mockReturnValue( availableCurrencies );
+
+useDefaultCurrency.mockReturnValue( defaultCurrency );
+
+useEnabledCurrencies.mockReturnValue( {
+	enabledCurrencies: enabledCurrencies,
+	submitEnabledCurrenciesUpdate: () => {},
+} );
+
+describe( 'Multi Currency enabled currencies list', () => {
+	beforeEach( () => {
+		jest.clearAllMocks();
+		global.wcpaySettings = { zeroDecimalCurrencies: [] };
+	} );
+
+	test( 'Enabled currencies list renders correctly', () => {
+		const { container } = render( <EnabledCurrencies /> );
+		expect( container ).toMatchSnapshot();
+	} );
+
+	test( 'Available currencies modal renders correctly', () => {
+		render( <EnabledCurrencies /> );
+		expect(
+			screen.queryByRole( 'dialog', { name: /add enabled currencies/i } )
+		).not.toBeInTheDocument();
+		fireEvent.click(
+			screen.getByRole( 'button', { name: /add currencies/i } )
+		);
+		const modal = screen.queryByRole( 'dialog', {
+			name: /add enabled currencies/i,
+		} );
+		expect( modal ).toBeInTheDocument();
+		expect( modal ).toMatchSnapshot();
+	} );
+} );
