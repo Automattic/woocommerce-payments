04b1487049158c7ede6c67dabca9b6c96f35f6b5
Jesse Pearson <jesse.pearson@automattic.com>
2021-06-07 16:18:44 -0300

    Add preview for Multi-Currency single currency settings page.

--- client/multi-currency/index.js
+++ client/multi-currency/index.js
@@ -17,11 +17,53 @@ if ( currencyContainer ) {
 	ReactDOM.render( <EnabledCurrencies />, currencyContainer );
 }
 
+let rateType = 'automatic';
+
+const automaticRate = document.querySelector(
+	'[name=wcpay_multi_currency_automatic_exchange_rate]'
+);
+
+const manualRate = document.querySelector(
+	'[name^=wcpay_multi_currency_manual_rate_]'
+);
+
+const rounding = document.querySelector(
+	'[name^=wcpay_multi_currency_price_rounding_]'
+);
+
+const charm = document.querySelector(
+	'[name^=wcpay_multi_currency_price_charm_]'
+);
+
+const previewAmount = document.querySelector(
+	'[name^=wcpay_multi_currency_preview_default_]'
+);
+
+const previewField = document.querySelector(
+	'[name^=wcpay_multi_currency_preview_converted_]'
+);
+
+function updatePreview() {
+	// Get needed field values and update field.
+	const rate = 'manual' === rateType ? manualRate.value : automaticRate.value;
+	let total = previewAmount.value * rate;
+
+	if ( 'none' !== rounding.value ) {
+		const precision = Math.pow( 10, rounding.value );
+		total = Math.ceil( total * precision ) / precision;
+	}
+
+	total += parseFloat( charm.value );
+	previewField.value = total.toFixed( 2 );
+}
+
 const hideShowManualField = ( show ) => {
 	const manualRateField = document
 		.querySelector( '[id^=wcpay_multi_currency_manual_rate_]' )
 		.closest( 'tr' );
 	manualRateField.style.display = show ? 'table-row' : 'none';
+	rateType = show ? 'manual' : 'automatic';
+	updatePreview();
 };
 
 const triggerHideShow = ( value, checked ) => {
@@ -34,4 +76,20 @@ document.querySelectorAll( '.exchange-rate-selector' ).forEach( ( radio ) => {
 	radio.addEventListener( 'change', ( event ) => {
 		triggerHideShow( event.target.value, event.target.checked );
 	} );
+} );
+
+manualRate.addEventListener( 'input', () => {
+	updatePreview();
+} );
+
+rounding.addEventListener( 'input', () => {
+	updatePreview();
+} );
+
+charm.addEventListener( 'input', () => {
+	updatePreview();
+} );
+
+previewAmount.addEventListener( 'input', () => {
+	updatePreview();
 } );
