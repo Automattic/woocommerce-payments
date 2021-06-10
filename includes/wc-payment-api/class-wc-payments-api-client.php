839092eaf010d2acf52292904cb994735131f49f
Taha Paksu <taha.paksu@automattic.com>
2021-06-10 15:05:11 +0300

    Add zero priced fee item when list is empty This prevents the error received from Stripe when the list item array is empty

--- includes/wc-payment-api/class-wc-payments-api-client.php
+++ includes/wc-payment-api/class-wc-payments-api-client.php
@@ -1181,8 +1181,22 @@ class WC_Payments_API_Client {
 	 */
 	private function request_with_level3_data( $params, $api, $method, $is_site_specific = true ) {
 		// If level3 data is not present for some reason, simply proceed normally.
-		if ( ! isset( $params['level3'] ) ) {
+		if ( ! isset( $params['level3'] ) || ! is_array( $params['level3'] ) ) {
 			return $this->request( $params, $api, $method, $is_site_specific );
+		}
+
+		// If level3 data doesn't contain any items, add a zero priced fee to meet Stripe's requirement
+		if ( !isset( $params['level3']['line_items'] ) || !is_array( $params['level3']['line_items'] ) || 0 === count( $params['level3']['line_items'] ) ) {
+			$params['level3']['line_items'] = [
+				[
+					'discount_amount' 		=> 0,
+					'product_code' 			=> '0-00-fee',
+					'product_description' 	=> '$0.00 fee',
+					'quantity' 				=> 1,
+					'tax_amount' 			=> 0,
+					'unit_cost' 			=> 0,
+				],
+			];
 		}
 
 		try {
