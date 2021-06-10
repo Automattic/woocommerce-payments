6b27d0ff2b3fbab2354993e9e41bd2d88abd41ee
Jesse Pearson <jessepearson@users.noreply.github.com>
2021-06-10 08:52:20 -0300

    Added admin note about multi-currency being available. (#2103)
    * Added admin note about multi-currency being available.
    
    * Updated register_deactivation_hook call for removing multi-currency notes.

--- includes/multi-currency/class-multi-currency.php
+++ includes/multi-currency/class-multi-currency.php
@@ -115,6 +115,7 @@ class Multi_Currency {
 
 		if ( is_admin() ) {
 			add_filter( 'woocommerce_get_settings_pages', [ $this, 'init_settings_pages' ] );
+			add_action( 'admin_init', [ __CLASS__, 'add_woo_admin_notes' ] );
 		}
 	}
 
@@ -494,5 +495,25 @@ class Multi_Currency {
 		include_once WCPAY_ABSPATH . 'includes/multi-currency/class-frontend-prices.php';
 		include_once WCPAY_ABSPATH . 'includes/multi-currency/class-frontend-currencies.php';
 		include_once WCPAY_ABSPATH . 'includes/multi-currency/class-user-settings.php';
+	}
+
+	/**
+	 * Adds Multi-Currency notes to the WC-Admin inbox.
+	 */
+	public static function add_woo_admin_notes() {
+		if ( defined( 'WC_VERSION' ) && version_compare( WC_VERSION, '4.4.0', '>=' ) ) {
+			require_once WCPAY_ABSPATH . 'includes/multi-currency/notes/class-note-multi-currency-available.php';
+			Note_Multi_Currency_Available::possibly_add_note();
+		}
+	}
+
+	/**
+	 * Removes Multi-Currency notes from the WC-Admin inbox.
+	 */
+	public static function remove_woo_admin_notes() {
+		if ( defined( 'WC_VERSION' ) && version_compare( WC_VERSION, '4.4.0', '>=' ) ) {
+			require_once WCPAY_ABSPATH . 'includes/multi-currency/notes/class-note-multi-currency-available.php';
+			Note_Multi_Currency_Available::possibly_delete_note();
+		}
 	}
 }
