b5b54c90319b10ff00e72859a4595fbd21dc957d
Veljko <veljano@yahoo.com>
2021-06-18 11:49:40 +0200

    Test structure part II

--- tests/e2e/specs/subscriptions/merchant-renew-action-scheduler.spec.js
+++ tests/e2e/specs/subscriptions/merchant-renew-action-scheduler.spec.js
@@ -11,19 +11,25 @@ const {
 	uiUnblocked,
 } = require( '@woocommerce/e2e-utils' );
 
-import { RUN_SUBSCRIPTIONS_TESTS, describeif, merchantWCP } from '../../utils';
+import {
+	RUN_SUBSCRIPTIONS_TESTS,
+	RUN_ACTION_SCHEDULER_TESTS,
+	describeif,
+	merchantWCP,
+} from '../../utils';
 
 import { fillCardDetails, setupCheckout } from '../../utils/payments';
 
 const productName = 'Subscription for systems renewal';
 const productSlug = 'subscription-for-systems-renewal';
+const actionSchedulerHook = 'woocommerce_scheduled_subscription_payment';
 
 const customerBilling = config.get( 'addresses.customer.billing' );
 
-let subscriptionId;
+let orderId;
 
-describeif( RUN_SUBSCRIPTIONS_TESTS )(
-	'Subscriptions > Renew a subscription as a merchant',
+describeif( RUN_SUBSCRIPTIONS_TESTS, RUN_ACTION_SCHEDULER_TESTS )(
+	'Subscriptions > Renew a subscription via Action Scheduler',
 	() => {
 		beforeAll( async () => {
 			await merchant.login();
@@ -47,25 +53,47 @@ describeif( RUN_SUBSCRIPTIONS_TESTS )(
 			await shopper.placeOrder();
 			await expect( page ).toMatch( 'Order received' );
 
-			// Get the subscription ID
-			const subscriptionIdField = await page.$(
-				'.woocommerce-orders-table__cell-subscription-id > a'
+			// Get the order ID so we can open it in the merchant view
+			const orderIdField = await page.$(
+				'.woocommerce-order-overview__order.order > strong'
 			);
-			subscriptionId = await subscriptionIdField.evaluate(
-				( el ) => el.innerText
-			);
+			orderId = await orderIdField.evaluate( ( el ) => el.innerText );
 
 			await merchant.login();
 		} );
 
 		afterAll( async () => {
 			// Delete the user created with the subscription
-			await withRestApi.deleteCustomerByEmail( customerBilling.email );
+			//await withRestApi.deleteCustomerByEmail( customerBilling.email );
 			await merchant.logout();
 		} );
 
-		it( 'should be able to renew a subscription as a merchant', async () => {
-			 
+		it( 'should be able to renew a subscription via Action Scheduler', async () => {
+			 // Go to Action Scheduler
+			 await merchantWCP.openActionScheduler();
+
+			 // Filter results by pending
+			 await page.click( 'ul.subsubsub > .pending > a' );
+
+			 // Search by pending subscriptions
+			 await expect( page ).toFill( 'input#plugin-search-input', {
+				 text: actionSchedulerHook
+			} );
+			await expect( page ).toClick( 'input#search-submit.button' );
+			await page.waitForNavigation( { waitUntil: 'networkidle0' } );
+
+			// Run the Action Scheduler task to renew a subscription
+			await expect( page ).toClick( 'div.row-actions > span.run > a' );
+			await page.waitForNavigation( { waitUntil: 'networkidle0' } );
+			await expect( page ).toMatchElement( 'div#message.updated > p > strong', {
+				text: actionSchedulerHook
+			} );
+		} );
+
+		it( 'should verify that the subscription has been renewed', async () => {
+			// Go to Subscriptions and verify the subscription renewal
+			await merchantWCP.openSubscriptions();
+			await expect( page ).toMatchElement( 'tbody#the-list > tr#post-' + orderId + ' > td.orders.column-orders > a', {text: '2'} );
 		} );
 	}
 );
