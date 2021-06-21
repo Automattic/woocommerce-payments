dc1c520425ff9407f0b2b6a7744fc3f4478828bb
Paul Dechov <dechovsky@gmail.com>
2021-06-21 12:29:57 -0400

    Adjust manual refund button after 'Refund' button is clicked
    This addresses cases when the refund buttons are initialized or (re-initialized) after page load.

--- client/order/index.js
+++ client/order/index.js
@@ -9,25 +9,25 @@ jQuery( function ( $ ) {
 	const disableManualRefunds = getConfig( 'disableManualRefunds' ) ?? false;
 	const manualRefundsTip = getConfig( 'manualRefundsTip' ) ?? '';
 
-	/**
-	 * The script is included in the footer, so all the DOM must already be in place.
-	 * This allows us to modify the tip before it gets used on document.ready.
-	 */
-	$( '.do-manual-refund' ).each( function () {
-		const $refundButton = $( this );
+	$( '#woocommerce-order-items' ).on(
+		'click',
+		'button.refund-items',
+		function () {
+			const $refundButton = $( '.do-manual-refund' );
 
-		if ( disableManualRefunds ) {
-			$refundButton.hide();
-		} else {
-			// Adjust the messaging on the manual refund button.
-			$refundButton
-				.attr( {
-					// Tips are readable through $.data(), but jQuery.tipTip use the title attribute to generate
-					// the tooltip.
-					title: manualRefundsTip,
-				} )
-				// Regenerate the tipTip tooltip.
-				.tipTip();
+			if ( disableManualRefunds ) {
+				$refundButton.hide();
+			} else {
+				// Adjust the messaging on the manual refund button.
+				$refundButton
+					.attr( {
+						// Tips are readable through $.data(), but jQuery.tipTip use the title attribute to generate
+						// the tooltip.
+						title: manualRefundsTip,
+					} )
+					// Regenerate the tipTip tooltip.
+					.tipTip();
+			}
 		}
-	} );
+	);
 } );
