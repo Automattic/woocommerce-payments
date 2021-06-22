f25cd99bc0d5121de20d01fe2a2582af750a5b69
Naman Malhotra <naman03malhotra@gmail.com>
2021-06-22 13:27:22 +0530

    Fix - Finish Setup button just reloads the setup page during onboarding

--- client/connect-account-page/index.js
+++ client/connect-account-page/index.js
@@ -97,11 +97,8 @@ const ConnectPageOnboardingDisabled = () => (
 
 const ConnectPageOnboarding = () => {
 	const [ isSubmitted, setSubmitted ] = useState( false );
-	const {
-		availableCountries,
-		country,
-		url: connectUrl,
-	} = wcpaySettings.connect;
+	const { url: connectUrl } = wcpaySettings;
+	const { availableCountries, country } = wcpaySettings.connect;
 
 	const handleLocationCheck = () => {
 		// Reset the 'Set up' button state if merchant decided to stop
@@ -110,8 +107,7 @@ const ConnectPageOnboarding = () => {
 		};
 		// Redirect the merchant if merchant decided to continue
 		const handleModalConfirmed = () => {
-			// The raw URL value has ampersands escaped and we need to unescape them for redirect to happen
-			window.location = connectUrl.replaceAll( '&amp;', '&' );
+			window.location = connectUrl;
 		};
 
 		// Populate translated list of supported countries we want to render in the modal window.
