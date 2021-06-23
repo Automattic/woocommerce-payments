1cecfd22efb458ff7a3b7b9f6a6c2f4a5a40900b
Harris Wong <harris.wong@automattic.com>
2021-06-23 10:26:38 -0600

    Fix test since we removed learn more button

--- tests/unit/notes/test-class-wc-payments-notes-additional-payment-methods.php
+++ tests/unit/notes/test-class-wc-payments-notes-additional-payment-methods.php
@@ -35,15 +35,10 @@ class WC_Payments_Notes_Additional_Payment_Methods_Test extends WP_UnitTestCase 
 		$this->assertSame( 'wc-payments-notes-additional-payment-methods', $note->get_name() );
 		$this->assertSame( 'woocommerce-payments', $note->get_source() );
 
-		[$enable_upe_action, $learn_more_action] = $note->get_actions();
+		[$enable_upe_action] = $note->get_actions();
 		$this->assertSame( 'wc-payments-notes-additional-payment-methods', $enable_upe_action->name );
 		$this->assertSame( 'Enable on your store', $enable_upe_action->label );
 		$this->assertSame( 'http://example.org/wp-admin/admin.php?page=wc-settings&tab=checkout&section=woocommerce_payments&action=enable-upe', $enable_upe_action->query );
 		$this->assertSame( true, $enable_upe_action->primary );
-
-		$this->assertSame( 'learn-more', $learn_more_action->name );
-		$this->assertSame( 'Learn more', $learn_more_action->label );
-		$this->assertSame( 'https://docs.woocommerce.com/document/payments/', $learn_more_action->query );
-		$this->assertSame( false, $learn_more_action->primary );
 	}
 }
