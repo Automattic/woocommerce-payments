41109d7972852011d6046473b1dcd59c3fc2ec43
Luiz Reis <luiz.reis@automattic.com>
2021-06-23 17:44:59 -0300

    Fix get_customer_country tests

--- tests/unit/multi-currency/test-class-utils.php
+++ tests/unit/multi-currency/test-class-utils.php
@@ -17,12 +17,27 @@ class WCPay_Multi_Currency_Utils_Tests extends WP_UnitTestCase {
 	private $utils;
 
 	/**
+	 * Store options to be restored between tests.
+	 *
+	 * @var array
+	 */
+	private $wc_options;
+
+	/**
 	 * Pre-test setup
 	 */
 	public function setUp() {
 		parent::setUp();
 
 		$this->utils = new WCPay\Multi_Currency\Utils();
+
+		$this->wc_options['woocommerce_default_customer_address'] = get_option( 'woocommerce_default_customer_address' );
+		$this->wc_options['woocommerce_default_country']          = get_option( 'woocommerce_default_country' );
+	}
+
+	public function tearDown() {
+		update_option( 'woocommerce_default_customer_address', $this->wc_options['woocommerce_default_customer_address'] );
+		update_option( 'woocommerce_default_country', $this->wc_options['woocommerce_default_country'] );
 	}
 
 	public function test_is_call_in_backtrace_return_false() {
@@ -35,14 +50,14 @@ class WCPay_Multi_Currency_Utils_Tests extends WP_UnitTestCase {
 
 	public function test_get_customer_country_returns_wc_default_location() {
 		update_option( 'woocommerce_default_customer_address', 'base' );
-		update_option( 'woocommerce_default_country', 'GB' );
+		update_option( 'woocommerce_default_country', 'US:CA' );
 
-		$this->assertSame( 'GB', $this->utils->get_customer_country() );
+		$this->assertSame( 'US', $this->utils->get_customer_country() );
 	}
 
 	public function test_get_customer_country_returns_wc_customer_country() {
 		update_option( 'woocommerce_default_customer_address', 'base' );
-		update_option( 'woocommerce_default_country', 'GB' );
+		update_option( 'woocommerce_default_country', 'US:CA' );
 
 		WC()->customer->set_billing_country( 'BR' );
 
