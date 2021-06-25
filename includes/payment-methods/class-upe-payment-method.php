8daf8018fb108d823eb82be256200218823ba753
Samir Merchant <samir.merchant@automattic.com>
2021-06-24 20:55:06 -0400

    fixes class namespacing error

--- includes/payment-methods/class-upe-payment-method.php
+++ includes/payment-methods/class-upe-payment-method.php
@@ -80,8 +80,8 @@ abstract class UPE_Payment_Method {
 	 * @return bool
 	 */
 	public function is_enabled_at_checkout() {
-		if ( class_exists( 'WC_Subscriptions' ) && version_compare( WC_Subscriptions::$version, '2.2.0', '>=' ) ) {
-			if ( WC_Subscriptions_Cart::cart_contains_subscription() ) {
+		if ( class_exists( 'WC_Subscriptions' ) && version_compare( \WC_Subscriptions::$version, '2.2.0', '>=' ) ) {
+			if ( \WC_Subscriptions_Cart::cart_contains_subscription() ) {
 				return $this->is_payment_method_reusable();
 			}
 		}
