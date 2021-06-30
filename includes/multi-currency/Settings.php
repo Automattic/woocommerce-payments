c6ff2816a99359a814bb3f3cdf6d0c5385b1fc50
Luiz Reis <luiz.reis@automattic.com>
2021-06-30 11:05:24 -0300

    Do not render decimals for zero-decimal currencies preview

--- includes/multi-currency/Settings.php
+++ includes/multi-currency/Settings.php
@@ -242,6 +242,10 @@ class Settings extends \WC_Settings_Page {
 					name="<?php echo esc_attr( $this->id . '_automatic_exchange_rate' ); ?>"
 					value="<?php echo esc_attr( $available_currencies[ $currency->get_code() ]->get_rate() ); ?>"
 				/>
+				<input type="hidden"
+					name="<?php echo esc_attr( $this->id . '_num_decimals' ); ?>"
+					value="<?php echo esc_attr( $currency->get_is_zero_decimal() ? 0 : 2 ); ?>"
+				/>
 			</td>
 		</tr>
 		<?php
