4e2cff5e324f85c246abf72d21581887d9b50d95
Vladimir Reznichenko <kalessil@gmail.com>
2021-06-30 23:03:32 +0200

    Meetup: enforce client coverage

--- bin/phpunit.sh
+++ bin/phpunit.sh
@@ -1,7 +1,11 @@
-#!/usr/bin/env bash
+#!/bin/bash
+
+# set strict mode for bash
+set -euo pipefail
+IFS=$'\n\t'
 
 if [[ $RUN_PHPCS == 1 || $SHOULD_DEPLOY == 1 ]]; then
 	exit
 fi
 
-if [ -f "phpunit.phar" ]; then php phpunit.phar -c phpunit.xml.dist; else ./vendor/bin/phpunit; fi;
+if [ -f "phpunit.phar" ]; then php phpunit.phar -c phpunit.xml.dist $; else ./vendor/bin/phpunit $; fi;
--- bin/run-ci-tests.bash
+++ bin/run-ci-tests.bash
@@ -10,5 +10,7 @@ WCPAY_DIR="$GITHUB_WORKSPACE"
 composer self-update 2.0.6 && composer install --no-progress
 sudo systemctl start mysql.service
 bash bin/install-wp-tests.sh woocommerce_test root root localhost $WP_VERSION $WC_VERSION false
-bash bin/phpunit.sh
+echo 'Running the tests...'
+bash bin/phpunit.sh --coverage-clover /tmp/clover.xml
+vendor/bin/coverage-check /tmp/clover.xml ${MIN_COVERAGE}
 
