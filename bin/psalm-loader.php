438c3dac2fd8e2e04edea42128dc87efe2b5d417
Daniel Mallory <daniel.mallory@automattic.com>
2021-06-30 12:38:52 +0100

    Setting up Psalm

--- bin/psalm-loader.php
+++
@@ -1,6 +0,0 @@
-<?php
-// phpcs:ignoreFile - This is related to the Psalm static code analysis, and not part of the plugin.
-
-require_once __DIR__ . '/../vendor/autoload.php';
-require_once __DIR__ . '/../vendor/php-stubs/wordpress-stubs/wordpress-stubs.php';
-require_once __DIR__ . '/../vendor/php-stubs/woocommerce-stubs/woocommerce-stubs.php';
---
+++ psalm-loader.php
@@ -0,0 +1,8 @@
+<?php
+// phpcs:ignoreFile - This is related to the Psalm static code analysis, and not part of the plugin.
+
+require_once __DIR__ . '/vendor/php-stubs/wordpress-stubs/wordpress-stubs.php';
+require_once __DIR__ . '/vendor/php-stubs/woocommerce-stubs/woocommerce-stubs.php';
+require_once __DIR__ . '/vendor/php-stubs/woocommerce-stubs/woocommerce-packages-stubs.php';
+
+require_once __DIR__ . '/vendor/autoload.php';
