7b2a5650b2e3723b693535a7f3e1870faeabd60f
Kristófer R <kristofer.thorlaksson@automattic.com>
2021-07-08 06:49:41 +0000

    fix 3DS payments with saved cards

--- client/checkout/blocks/index.js
+++ client/checkout/blocks/index.js
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
+import { useEffect } from '@wordpress/element';
 
 // Handled as an external dependency: see '/webpack.config.js:83'
 import {
@@ -20,6 +21,7 @@ import WCPayFields from './fields.js';
 import request from './request.js';
 import enqueueFraudScripts from 'fraud-scripts';
 import paymentRequestPaymentMethod from '../../payment-request/blocks';
+import confirmCardPayment from './confirm-card-payment.js';
 
 // Create an API object, which will be used throughout the checkout.
 const api = new WCPayAPI(
@@ -32,10 +34,31 @@ const api = new WCPayAPI(
 	request
 );
 
+const IntentConfirmationHandler = ( {
+	stripe,
+	elements,
+	eventRegistration: { onCheckoutAfterProcessingWithSuccess },
+	emitResponse,
+} ) => {
+	// Once the server has completed payment processing, confirm the intent of necessary.
+	useEffect(
+		() =>
+			onCheckoutAfterProcessingWithSuccess(
+				( { processingResponse: { paymentDetails } } ) =>
+					confirmCardPayment( api, paymentDetails, emitResponse )
+			),
+		// not sure if we need to disable this, but kept it as-is to ensure nothing breaks. Please consider passing all the deps.
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+		[ elements, stripe, api ]
+	);
+	return <></>;
+};
+
 registerPaymentMethod( {
 	name: PAYMENT_METHOD_NAME_CARD,
 	content: <WCPayFields api={ api } />,
 	edit: <WCPayFields api={ api } />,
+	savedTokenComponent: <IntentConfirmationHandler api={ api } />,
 	canMakePayment: () => !! api.getStripe(),
 	paymentMethodId: PAYMENT_METHOD_NAME_CARD,
 	label: __( 'Credit Card', 'woocommerce-payments' ),
