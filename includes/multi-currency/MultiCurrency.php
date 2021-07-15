9bdb1070aeda15e2bad79d413e3d205dfcfe2523
frosso <francesco.rosso@automattic.com>
2021-07-15 14:39:23 -0500

    feat: add euro when euro payments enabled

--- includes/multi-currency/MultiCurrency.php
+++ includes/multi-currency/MultiCurrency.php
@@ -162,6 +162,7 @@ class MultiCurrency {
 			$this->update_manual_rate_currencies_notice_option();
 		}
 
+		new PaymentMethodsCompatibility( $this, WC_Payments::get_gateway() );
 		new AdminNotices();
 		new UserSettings( $this );
 
