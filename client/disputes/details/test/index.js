e00f9d7baf6f29759b60fbea503cdbdb9c0981a8
David Quiñones <daquinons@users.noreply.github.com>
2021-07-15 11:43:12 +0200

    Adds Dispute Status Chip into the header of the Dispute Details page
    The status needs to appear in the Details page as the design states
    
    Improved tests for testing all combinations of dispute reasons and statuses

--- client/disputes/details/test/index.js
+++ client/disputes/details/test/index.js
@@ -38,28 +38,51 @@ describe( 'Dispute details screen', () => {
 		'unrecognized',
 	];
 
-	test.each( reasons )( 'renders correctly for %s dispute', ( reason ) => {
-		const dispute = {
-			id: 'dp_asdfghjkl',
-			amount: 1000,
-			currency: 'usd',
-			created: 1572590800,
-			evidence_details: {
-				due_by: 1573199200,
-			},
-			reason,
-			status: 'needs_response',
-			order: {
-				number: '1',
-				url: 'http://test.local/order/1',
-			},
-		};
+	const statuses = [
+		'warning_needs_response',
+		'warning_under_review',
+		'warning_closed',
+		'needs_response',
+		'under_review',
+		'charge_refunded',
+		'won',
+		'lost',
+	];
 
-		useDispute.mockReturnValue( { dispute, isLoading: false } );
-
-		const { container } = render(
-			<DisputeDetails query={ { id: 'dp_asdfghjkl' } } />
-		);
-		expect( container ).toMatchSnapshot();
+	// Adding together all reasons and statuses combinations
+	const testCases = [];
+	reasons.forEach( ( reason ) => {
+		statuses.forEach( ( status ) => {
+			testCases.push( [ reason, status ].join( ',' ) );
+		} );
 	} );
+
+	test.each( testCases )(
+		'renders correctly for %s dispute',
+		( testCase ) => {
+			const [ reason, status ] = testCase.split( ',' );
+			const dispute = {
+				id: 'dp_asdfghjkl',
+				amount: 1000,
+				currency: 'usd',
+				created: 1572590800,
+				evidence_details: {
+					due_by: 1573199200,
+				},
+				reason,
+				status,
+				order: {
+					number: '1',
+					url: 'http://test.local/order/1',
+				},
+			};
+
+			useDispute.mockReturnValue( { dispute, isLoading: false } );
+
+			const { container } = render(
+				<DisputeDetails query={ { id: 'dp_asdfghjkl' } } />
+			);
+			expect( container ).toMatchSnapshot();
+		}
+	);
 } );
