613b829e48bcaacbe8df3cc711530dbedd125a63
David Quiñones <daquinons@users.noreply.github.com>
2021-07-16 10:37:44 +0200

    Add Dispute Status Chip into the header of the Dispute Details page (#2436)
    * Adds Dispute Status Chip into the header of the Dispute Details page
    
    The status needs to appear in the Details page as the design states
    
    Improved tests for testing all combinations of dispute reasons and statuses

--- client/disputes/details/test/index.js
+++ client/disputes/details/test/index.js
@@ -38,6 +38,17 @@ describe( 'Dispute details screen', () => {
 		'unrecognized',
 	];
 
+	const statuses = [
+		'warning_needs_response',
+		'warning_under_review',
+		'warning_closed',
+		'needs_response',
+		'under_review',
+		'charge_refunded',
+		'won',
+		'lost',
+	];
+
 	test.each( reasons )( 'renders correctly for %s dispute', ( reason ) => {
 		const dispute = {
 			id: 'dp_asdfghjkl',
@@ -62,4 +73,32 @@ describe( 'Dispute details screen', () => {
 		);
 		expect( container ).toMatchSnapshot();
 	} );
+
+	test.each( statuses )(
+		'renders correctly for %s dispute status',
+		( status ) => {
+			const dispute = {
+				id: 'dp_asdfghjkl',
+				amount: 1000,
+				currency: 'usd',
+				created: 1572590800,
+				evidence_details: {
+					due_by: 1573199200,
+				},
+				reason: 'fraudulent',
+				status,
+				order: {
+					number: '1',
+					url: 'http://test.local/order/1',
+				},
+			};
+
+			useDispute.mockReturnValue( { dispute, isLoading: false } );
+
+			const { container } = render(
+				<DisputeDetails query={ { id: 'dp_asdfghjkl' } } />
+			);
+			expect( container ).toMatchSnapshot();
+		}
+	);
 } );
