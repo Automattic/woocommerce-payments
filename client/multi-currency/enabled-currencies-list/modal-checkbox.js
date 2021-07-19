ef52184b6fd7050a77f546a768f29895d6b3fba6
Luiz Reis <luiz.reis@automattic.com>
2021-07-19 12:03:32 -0300

    Add currency code as flag when flag is empty

--- client/multi-currency/enabled-currencies-list/modal-checkbox.js
+++ client/multi-currency/enabled-currencies-list/modal-checkbox.js
@@ -30,7 +30,13 @@ const EnabledCurrenciesModalCheckbox = ( {
 					components: {
 						flag: (
 							<span className="enabled-currency-checkbox__flag">
-								{ flag }
+								{ '' !== flag ? (
+									flag
+								) : (
+									<div className="enabled-currency-checkbox__flag-text">
+										{ code }
+									</div>
+								) }
 							</span>
 						),
 						name: <span>{ name }</span>,
