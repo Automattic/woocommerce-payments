f3ae03a186910a50c569987bdd1f968de3f0293c
Ismael Martín Alabarce <info@ismaeld.com>
2021-07-20 12:42:41 +0200

    Simplify add widget and refactor affected code
    Use breadcrumb defaults to add the widget markup. To avoid the need of css code for each storefront child theme, making it easier to maintain.
    Remove unneeded code, move the setting checkbox back to Settings class and update description to mention that the widget will be added to breadcrumb section.

--- includes/multi-currency/StorefrontIntegration.php
+++ includes/multi-currency/StorefrontIntegration.php
@@ -28,13 +28,6 @@ class StorefrontIntegration {
 	protected $multi_currency;
 
 	/**
-	 * Themes where we need to add a wrapper around the breadcrumbs before adding the widget.
-	 *
-	 * @var array
-	 */
-	protected $wrapper_themes;
-
-	/**
 	 * Constructor.
 	 *
 	 * @param MultiCurrency $multi_currency The MultiCurrency instance.
@@ -42,7 +35,6 @@ class StorefrontIntegration {
 	public function __construct( MultiCurrency $multi_currency ) {
 		$this->multi_currency = $multi_currency;
 		$this->id             = $this->multi_currency->id;
-		$this->wrapper_themes = [ 'arcade', 'boutique', 'homestore', 'stationery' ];
 		$this->init_actions_and_filters();
 	}
 
@@ -52,234 +44,65 @@ class StorefrontIntegration {
 	 * @return void
 	 */
 	public function add_inline_css() {
+		$css = '
+			#woocommerce-payments-multi-currency-storefront-widget {
+				float: right;
+			}
+			#woocommerce-payments-multi-currency-storefront-widget form {
+				margin: 0;
+			}
+		';
+
 		wp_add_inline_style(
 			'storefront-style',
-			apply_filters( $this->id . '_storefront_widget_css', $this->get_css() )
+			apply_filters( $this->id . '_storefront_widget_css', $css )
 		);
 	}
 
 	/**
-	 * Adds the switcher widget.
+	 * Generates the switcher widget markup.
 	 *
-	 * @return void
+	 * @return string The widget markup.
 	 */
-	public function add_switcher_widget() {
+	public function switcher_widget_markup(): string {
 		/**
 		 * The spl_object_hash function is used here due to we register the widget with an instance of the widget and
 		 * not the class name of the widget. WordPress core takes the instance and passes it through spl_object_hash
 		 * to get a hash and adds that as the widget's name in the $wp_widget_factory->widgets[] array. In order to
 		 * call the_widget, you need to have the name of the widget, so we get the instance and hash to use.
 		 */
+		ob_start();
 		the_widget(
 			spl_object_hash( $this->multi_currency->get_currency_switcher_widget() ),
-			apply_filters(
-				$this->id . '_storefront_widget_options',
-				[
-					'symbol' => true,
-					'flag'   => true,
-				]
-			),
+			null,
 			apply_filters(
 				$this->id . '_storefront_widget_args',
 				[
-					'before_widget' => '<div id="woocommerce-payments-multi-currency-storefront-widget">',
+					'before_widget' => '<div id="woocommerce-payments-multi-currency-storefront-widget" class="woocommerce-breadcrumb">',
 					'after_widget'  => '</div>',
 				]
 			)
 		);
+		return ob_get_clean();
 	}
 
-	/**
-	 * This closes the div tags opened in modify_breadcrumb_wrapper.
-	 *
-	 * @return void
-	 */
-	public function close_breadcrumb_wrapper() {
-		if ( $this->is_wrapper_theme() ) {
-			echo '</div>';
-		} else {
-			echo '</div></div>';
-		}
-	}
+
 
 	/**
-	 * Adds the checkbox to the store settings to toggle the currency switched in the Storefront header.
+	 * This modifies the breadcrumb defaults for us to be able to place the widget.
 	 *
-	 * @param array $settings The settings from the Settings class.
+	 * @param array $defaults The defaults breadcrumb properties.
 	 *
-	 * @return array The modified settings array.
+	 * @return array The modified defaults properties.
 	 */
-	public function filter_store_settings( array $settings ): array {
-		foreach ( $settings as $setting ) {
-			if ( isset( $setting['id'] ) && $this->id . '_enable_auto_currency' === $setting['id'] ) {
-				// Add the checkboxgroup prop to the store settings section, then add the setting to the new array.
-				$setting['checkboxgroup'] = 'start';
-				$new_settings[]           = $setting;
-
-				// Add the checkbox to be able to toggle the widget.
-				$new_settings[] = [
-					'desc'          => __( 'Add a currency switcher to the Storefront theme. ', 'woocommerce-payments' ),
-					'desc_tip'      => sprintf(
-						/* translators: %s: url to the widgets page */
-						__( 'A currency switcher is also available in your widgets. <a href="%s">Configure now</a>', 'woocommerce-payments' ),
-						'widgets.php'
-					),
-					'id'            => $this->id . '_enable_storefront_switcher',
-					'default'       => 'yes',
-					'type'          => 'checkbox',
-					'checkboxgroup' => 'end',
-				];
-
-			} else {
-				// Otherwise just pass the setting to the new array.
-				$new_settings[] = $setting;
-			}
-		}
-		return $new_settings;
-	}
-
-	/**
-	 * This modifies the wrapper(s) around the breadcrumb for us to be able to place the widget in the wrapper.
-	 *
-	 * @param array $defaults The default breadcrumb wrapper elements.
-	 *
-	 * @return array The modified wrapper elements.
-	 */
-	public function modify_breadcrumb_wrapper( array $defaults ): array {
-		if ( $this->is_wrapper_theme() ) {
-			// This adds the opening div tag so we can make use of it for these themes.
-			$defaults['wrap_before'] = '<div class="storefront-breadcrumb-wrapper"><nav class="woocommerce-breadcrumb">';
-		} else {
-			// This removes the closing div tags for all other Storefront themes.
-			$defaults['wrap_after'] = '</nav>';
-		}
+	public function modify_breadcrumb_defaults( array $defaults ): array {
+		/**
+		 * Some storefront child themes uses different wrappers and styles. We need to place the widget before
+		 * the <nav> to display it properly.
+		 */
+		$defaults['wrap_before'] = str_replace( '<nav', $this->switcher_widget_markup() . '<nav', $defaults['wrap_before'] );
 
 		return $defaults;
-	}
-
-	/**
-	 * Gets the css for the current Storefront theme.
-	 *
-	 * @return string The css to be added to the header.
-	 */
-	private function get_css(): string {
-		$flex_rules = '
-			display: flex;
-			flex-wrap: wrap;
-			justify-content: space-between;
-			align-items: center;
-		';
-
-		$without_wrapper = '
-			div.storefront-breadcrumb div.col-full {
-				' . $flex_rules . '
-			}
-
-			div.storefront-breadcrumb div.col-full:before,
-			div.storefront-breadcrumb div.col-full:after {
-				display: none;
-			}
-		';
-
-		switch ( $this->multi_currency->get_theme_stylesheet() ) {
-			case 'arcade':
-			case 'boutique':
-				$css = '
-					div.storefront-breadcrumb-wrapper {
-						' . $flex_rules . '
-						margin: 0 0 1.618em;
-					}
-
-					div.storefront-breadcrumb-wrapper nav.woocommerce-breadcrumb {
-						margin: 0;
-					}
-				';
-				break;
-			case 'bistro':
-				$css = $without_wrapper . '
-					div.storefront-breadcrumb div.col-full {
-						margin-top: 4.236em;
-					}
-
-					div.storefront-breadcrumb nav.woocommerce-breadcrumb {
-						margin: 0;
-					}
-				';
-				break;
-			case 'deli':
-				$css = $without_wrapper . '
-					div.storefront-breadcrumb div.col-full {
-						margin-bottom: 0;
-					}
-
-					div.storefront-breadcrumb nav.woocommerce-breadcrumb {
-						margin: 0;
-					}
-				';
-				break;
-			case 'galleria':
-			case 'outlet':
-				$css = $without_wrapper . '
-					#woocommerce-payments-multi-currency-storefront-widget {
-						font-size: .8rem;
-					}
-				';
-				break;
-			case 'homestore':
-				$css = '
-					div.storefront-breadcrumb-wrapper {
-						' . $flex_rules . '
-						font-size: 0.9em;
-						border-bottom: 3px solid #151515;
-						color: #a2a1a1;
-						margin: -3em 0 3.24rem;
-						padding: 0 0 .6rem;
-					}
-
-					div.storefront-breadcrumb-wrapper nav.woocommerce-breadcrumb {
-						border: none;
-						margin: 0;
-						padding: 0;
-					}
-				';
-				break;
-			case 'stationery':
-				$css = '
-					div.storefront-breadcrumb-wrapper {
-						' . $flex_rules . '
-						margin: 0 0 1.861em;
-						padding: 1.861em 0;
-					}
-
-					div.storefront-breadcrumb-wrapper nav.woocommerce-breadcrumb {
-						margin: 0;
-						padding: 0;
-					}
-				';
-				break;
-			case 'toyshop':
-				$css = $without_wrapper . '
-					div.storefront-breadcrumb div.col-full {
-						margin-bottom: 2rem;
-					}
-
-					div.storefront-breadcrumb nav.woocommerce-breadcrumb {
-						margin: 0;
-					}
-				';
-				break;
-			default:
-				$css = $without_wrapper;
-				break;
-		}
-
-		$css .= '
-			#woocommerce-payments-multi-currency-storefront-widget form {
-				margin: 0;
-			}
-		';
-
-		return $css;
 	}
 
 	/**
@@ -288,25 +111,10 @@ class StorefrontIntegration {
 	 * @return void
 	 */
 	private function init_actions_and_filters() {
-		add_filter( $this->id . '_enabled_currencies_settings', [ $this, 'filter_store_settings' ] );
-
 		// We specifically look for 'no', because we want it enabled by default.
 		if ( 'no' !== get_option( $this->id . '_enable_storefront_switcher' ) ) {
-			add_filter( 'woocommerce_breadcrumb_defaults', [ $this, 'modify_breadcrumb_wrapper' ], 9999 );
+			add_filter( 'woocommerce_breadcrumb_defaults', [ $this, 'modify_breadcrumb_defaults' ], 9999 );
 			add_action( 'wp_enqueue_scripts', [ $this, 'add_inline_css' ], 50 );
-
-			$breadcrumb_action = $this->is_wrapper_theme() ? 'storefront_content_top' : 'storefront_before_content';
-			add_action( $breadcrumb_action, [ $this, 'add_switcher_widget' ], 11 );
-			add_action( $breadcrumb_action, [ $this, 'close_breadcrumb_wrapper' ], 12 );
 		}
-	}
-
-	/**
-	 * Returns if a wrapper theme is in use or not.
-	 *
-	 * @return bool
-	 */
-	private function is_wrapper_theme(): bool {
-		return in_array( $this->multi_currency->get_theme_stylesheet(), $this->wrapper_themes, true );
 	}
 }
--- tests/unit/multi-currency/test-class-storefront-integration.php
+++ tests/unit/multi-currency/test-class-storefront-integration.php
@@ -29,94 +29,81 @@ class WCPay_Multi_Currency_Storefront_Integration_Tests extends WP_UnitTestCase 
 		$this->storefront_integration = new StorefrontIntegration( $this->mock_multi_currency );
 	}
 
-	public function test_registers_settings_filter() {
-		$this->assertSame(
-			10,
-			has_filter(
-				'wcpay_multi_currency_enabled_currencies_settings',
-				[ $this->storefront_integration, 'filter_store_settings' ]
-			)
-		);
+	public function tearDown() {
+		remove_all_filters( 'stylesheet' );
+		remove_all_filters( 'option_wcpay_multi_currency_enable_storefront_switcher' );
+
+		parent::tearDown();
 	}
 
-	public function test_does_not_register_actions_when_switcher_disabled() {
-		$this->mock_multi_currency->method( 'get_theme_stylesheet' )->willReturn( 'storefront' );
-
-		// Update the option, and then recreate the instance before testing.
-		update_option( 'wcpay_multi_currency_enable_storefront_switcher', 'no' );
+	/**
+	 * @dataProvider switcher_filter_provider
+	 */
+	public function test_does_not_register_actions_when_switcher_disabled( $filter, $function_name ) {
+		$this->mock_theme( 'storefront' );
+		$this->mock_option( 'no' );
+		// Reinit class to re-evaluate conditional hooks.
 		$this->storefront_integration = new StorefrontIntegration( $this->mock_multi_currency );
 
 		$this->assertFalse(
-			has_filter( 'woocommerce_breadcrumb_defaults', [ $this->storefront_integration, 'modify_breadcrumb_wrapper' ] ),
-			"The filter 'woocommerce_breadcrumb_defaults' with function 'modify_breadcrumb_wrapper' was found."
-		);
-		$this->assertFalse(
-			has_action( 'wp_enqueue_scripts', [ $this->storefront_integration, 'add_inline_css' ] ),
-			"The action 'wp_enqueue_scripts' with function 'add_inline_css' was found."
-		);
-		$this->assertFalse(
-			has_action( 'storefront_before_content', [ $this->storefront_integration, 'add_switcher_widget' ] ),
-			"The action 'storefront_before_content' with function 'add_switcher_widget' was found."
-		);
-		$this->assertFalse(
-			has_action( 'storefront_before_content', [ $this->storefront_integration, 'close_breadcrumb_wrapper' ] ),
-			"The action 'storefront_before_content' with function 'close_breadcrumb_wrapper' was found."
+			has_filter( $filter, [ $this->storefront_integration, $function_name ] ),
+			"The filter '$filter' with function '$function_name' was found."
 		);
 	}
 
-	public function test_registers_default_actions_when_switcher_enabled() {
-		$this->mock_multi_currency->method( 'get_theme_stylesheet' )->willReturn( 'storefront' );
 
-		// Due to we are testing conditional actions/filters, we need to reinit the class.
+	/**
+	 * @dataProvider switcher_filter_provider
+	 */
+	public function test_registers_default_actions_when_switcher_enabled( $filter, $function_name ) {
+		$this->mock_theme( 'storefront' );
+		// Reinit class to re-evaluate conditional hooks.
 		$this->storefront_integration = new StorefrontIntegration( $this->mock_multi_currency );
 
 		$this->assertGreaterThan(
 			10,
-			has_filter( 'woocommerce_breadcrumb_defaults', [ $this->storefront_integration, 'modify_breadcrumb_wrapper' ] ),
-			"The filter 'woocommerce_breadcrumb_defaults' with function 'modify_breadcrumb_wrapper' was not registered with a priority above 10."
-		);
-		$this->assertGreaterThan(
-			10,
-			has_action( 'wp_enqueue_scripts', [ $this->storefront_integration, 'add_inline_css' ] ),
-			"The action 'wp_enqueue_scripts' with function 'add_inline_css' was not registered with a priority above 10."
-		);
-		$this->assertGreaterThan(
-			10,
-			has_action( 'storefront_before_content', [ $this->storefront_integration, 'add_switcher_widget' ] ),
-			"The action 'storefront_before_content' with function 'add_switcher_widget' was not registered with a priority above 10."
-		);
-		$this->assertGreaterThan(
-			10,
-			has_action( 'storefront_before_content', [ $this->storefront_integration, 'close_breadcrumb_wrapper' ] ),
-			"The action 'storefront_before_content' with function 'close_breadcrumb_wrapper' was not registered with a priority above 10."
+			has_filter( $filter, [ $this->storefront_integration, $function_name ] ),
+			"The filter '$filter' with function '$function_name' was not registered with a priority above 10."
 		);
 	}
 
-	public function test_registers_wrapper_theme_actions_when_switcher_enabled_and_wrapper_theme_found() {
-		$this->mock_multi_currency->method( 'get_theme_stylesheet' )->willReturn( 'arcade' );
-
-		// Due to we are testing conditional actions/filters, we need to reinit the class.
+	/**
+	 * @dataProvider switcher_filter_provider
+	 */
+	public function test_registers_actions_when_switcher_enabled_and_storefront_theme_found( $filter, $function_name ) {
+		$this->mock_theme( 'storefront' );
+		// Reinit class to re-evaluate conditional hooks.
 		$this->storefront_integration = new StorefrontIntegration( $this->mock_multi_currency );
 
 		$this->assertGreaterThan(
 			10,
-			has_filter( 'woocommerce_breadcrumb_defaults', [ $this->storefront_integration, 'modify_breadcrumb_wrapper' ] ),
-			"The filter 'woocommerce_breadcrumb_defaults' with function 'modify_breadcrumb_wrapper' was not registered with a priority above 10."
+			has_filter( $filter, [ $this->storefront_integration, $function_name ] ),
+			"The filter '$filter' with function '$function_name' was not registered with a priority above 10."
 		);
-		$this->assertGreaterThan(
-			10,
-			has_action( 'wp_enqueue_scripts', [ $this->storefront_integration, 'add_inline_css' ] ),
-			"The action 'wp_enqueue_scripts' with function 'add_inline_css' was not registered with a priority above 10."
+	}
+
+	public function switcher_filter_provider() {
+		return [
+			[ 'woocommerce_breadcrumb_defaults', 'modify_breadcrumb_defaults' ],
+			[ 'wp_enqueue_scripts', 'add_inline_css' ],
+		];
+	}
+
+	private function mock_theme( $theme ) {
+		add_filter(
+			'stylesheet',
+			function() use ( $theme ) {
+				return $theme;
+			}
 		);
-		$this->assertGreaterThan(
-			10,
-			has_action( 'storefront_content_top', [ $this->storefront_integration, 'add_switcher_widget' ] ),
-			"The action 'storefront_content_top' with function 'add_switcher_widget' was not registered with a priority above 10."
-		);
-		$this->assertGreaterThan(
-			10,
-			has_action( 'storefront_content_top', [ $this->storefront_integration, 'close_breadcrumb_wrapper' ] ),
-			"The action 'storefront_content_top' with function 'close_breadcrumb_wrapper' was not registered with a priority above 10."
+	}
+
+	private function mock_option( $value ) {
+		add_filter(
+			'pre_option_wcpay_multi_currency_enable_storefront_switcher',
+			function() use ( $value ) {
+				return $value;
+			}
 		);
 	}
 }
