fc6d357dda6dbc61cc7c1b16046bf3f040fb38bb
David Quiñones <daquinons@users.noreply.github.com>
2021-07-21 10:03:36 +0200

    Disable payment gateway when not in test mode and not using https or ssl checkout enforcement (#2480)
    * Adds new rules to is_available method in gateway
    * Adds a task in Payments Overview when out of testing mode and not using https or ssl checkout

--- includes/admin/class-wc-payments-admin.php
+++ includes/admin/class-wc-payments-admin.php
@@ -279,6 +279,7 @@ class WC_Payments_Admin {
 				'isUpeSettingsPreviewEnabled' => WC_Payments_Features::is_upe_settings_preview_enabled(),
 				'isUpeEnabled'                => WC_Payments_Features::is_upe_enabled(),
 			],
+			'needsHttpsSetup'        => $this->wcpay_gateway->needs_https_setup(),
 		];
 
 		wp_localize_script(
