b93d254641c27286c338f8a4d109d8f0edee3f00
Rodel <rodel.calasagsag@automattic.com>
2021-07-22 22:08:11 +0800

    Test for partial refunds

--- tests/e2e/specs/merchant/merchant-orders-partial-refund.spec.js
+++ tests/e2e/specs/merchant/merchant-orders-partial-refund.spec.js
@@ -11,124 +11,188 @@ const { merchant, shopper, uiUnblocked } = require( '@woocommerce/e2e-utils' );
 import { merchantWCP, takeScreenshot } from '../../utils';
 import { fillCardDetails, setupProductCheckout } from '../../utils/payments';
 
-const orders = [];
+const card = config.get( 'cards.basic' );
+const product1 = config.get( 'products.simple.name' );
+const product2 = 'Belt';
+const product3 = 'Hoodie';
 
-describe( 'Order > Partial refund', () => {
-	beforeAll( async () => {
-		// mytodo use api to create the orders
-		await page.goto( config.get( 'url' ), { waitUntil: 'networkidle0' } );
+/**
+ * Elements:
+ * - test title
+ * - object containing the items to be ordered, and the quantities and amounts to be refunded
+ */
+const dataTable = [
+	[
+		'a single line item',
+		{
+			lineItems: [
+				[ product1, 1 ],
+				[ product2, 1 ],
+			],
+			refundInputs: [ { refundQty: 0, refundAmount: 5 } ],
+		},
+	],
+	[
+		'several line items',
+		{
+			lineItems: [
+				[ product1, 1 ],
+				[ product2, 2 ],
+				[ product3, 1 ],
+			],
+			refundInputs: [
+				{ refundQty: 1, refundAmount: 18 },
+				{ refundQty: 1, refundAmount: 55 },
+			],
+		},
+	],
+];
 
-		await setupProductCheckout(
-			config.get( 'addresses.customer.billing' )
-		);
-		const card = config.get( 'cards.basic' );
-		await fillCardDetails( page, card );
-		await shopper.placeOrder();
-		await expect( page ).toMatch( 'Order received' );
+describe.each( dataTable )(
+	'Order > Partial refund',
+	( testName, { lineItems, refundInputs } ) => {
+		let orderId;
+		let orderTotal;
 
-		// Get the order ID so we can open it in the merchant view
-		const orderIdField = await page.$(
-			'.woocommerce-order-overview__order.order > strong'
-		);
-		const orderTotalField = await page.$(
-			'.woocommerce-order-overview__total strong'
-		);
-		const orderId = await orderIdField.evaluate( ( el ) => el.innerText );
-		const orderTotal = await orderTotalField.evaluate(
-			( el ) => el.innerText
-		);
-		orders.push( {
-			orderId: orderId,
-			orderTotal: Number( orderTotal.replace( '$', '' ) ),
+		beforeAll( async () => {
+			// Set up the test order
+			await setupProductCheckout(
+				config.get( 'addresses.customer.billing' ),
+				lineItems
+			);
+			await fillCardDetails( page, card );
+			await shopper.placeOrder();
+			await expect( page ).toMatch( 'Order received' );
+
+			// Remember the order ID and order total. We will need them later.
+			orderId = await page.$eval(
+				'.woocommerce-order-overview__order.order > strong',
+				( el ) => el.innerText
+			);
+			orderTotal = await page.$eval(
+				'.woocommerce-order-overview__total strong',
+				( el ) => Number( el.innerText.replace( '$', '' ) )
+			);
+
+			// Login as merchant
+			await merchant.login();
 		} );
 
-		// Login as merchant
-		await merchant.login();
-	} );
-
-	afterAll( async () => {
-		await takeScreenshot( 'my-wip' ); // mytodo remove later
-		await merchant.logout();
-	} );
-
-	it( 'should refund a partial amount', async () => {
-		const orderIndex = 0;
-		const { orderId, orderTotal } = orders[ orderIndex ];
-		const refundAmount = 5;
-		const refundAmountString = refundAmount.toFixed( 2 );
-		const refundReason = `Partial refund of $${ refundAmountString }`;
-		const netPayment = ( orderTotal - refundAmount ).toFixed( 2 );
-
-		await merchant.goToOrder( orderId );
-
-		// We need to remove any listeners on the `dialog` event otherwise we can't catch the dialog below
-		await page.removeAllListeners( 'dialog' );
-
-		// Click the Refund button
-		await expect( page ).toClick( 'button.refund-items' );
-
-		// Do a partial refund of $5
-		await expect( page ).toFill( '.refund_line_total', refundAmountString );
-		await expect( page ).toFill( '#refund_reason', refundReason );
-		await expect( page ).toMatchElement( '.do-api-refund', {
-			text: `Refund $${ refundAmountString } via WooCommerce Payments`,
-		} );
-		const refundDialog = await expect( page ).toDisplayDialog( async () => {
-			await expect( page ).toClick( 'button.do-api-refund' );
-		} );
-		await refundDialog.accept();
-		await uiUnblocked();
-		await page.waitForNavigation( { waitUntil: 'networkidle0' } );
-
-		// Verify the product line item shows the refunded amount
-		await expect( page ).toMatchElement( '.line_cost .refunded .amount', {
-			text: `$${ refundAmountString }`,
+		afterAll( async () => {
+			await takeScreenshot( `afterAll - ${ testName }` ); // mytodo remove later
+			await merchant.logout();
 		} );
 
-		// Verify the refund shows in the list with the amount
-		await expect( page ).toMatchElement( '.refund > .line_cost', {
-			text: `-$${ refundAmountString }`,
-		} );
+		it( `should refund ${ testName }`, async () => {
+			const refundReason = `Refunding ${ testName }`;
+			const refundTotal = refundInputs
+				.map( ( { refundAmount } ) => refundAmount )
+				.reduce( ( acc, cur ) => acc + cur );
+			const refundTotalString = refundTotal.toFixed( 2 );
+			const netPayment = ( orderTotal - refundTotal ).toFixed( 2 );
 
-		// Verify system note was added
-		await expect( page ).toMatchElement( '.system-note', {
-			text: `A refund of $${ refundAmountString } was successfully processed using WooCommerce Payments.`,
-		} );
-		await expect( page ).toMatchElement( '.system-note', {
-			text: `Reason: ${ refundReason }`,
-		} );
+			await merchant.goToOrder( orderId );
 
-		// Verify "Refunded" and "Net Payment" values in Order Totals section
-		await expect( page ).toMatchElement(
-			'.wc-order-totals .total.refunded-total',
-			{
-				text: `-$${ refundAmountString }`,
+			// We need to remove any listeners on the `dialog` event otherwise we can't catch the dialog below
+			await page.removeAllListeners( 'dialog' );
+
+			// Click the Refund button
+			await expect( page ).toClick( 'button.refund-items' );
+
+			// Fill up the quantity and/or amount to be refunded per line item
+			const rows = await page.$$( '#order_line_items tr.item' );
+			for ( let i = 0; i < refundInputs.length; i++ ) {
+				const { refundQty, refundAmount } = refundInputs[ i ];
+				const row = rows[ i ];
+
+				if ( refundQty ) {
+					await expect( row ).toFill(
+						'.refund_order_item_qty',
+						`${ refundQty }`
+					);
+				} else {
+					await expect( row ).toFill(
+						'.refund_line_total',
+						`${ refundAmount }`
+					);
+				}
 			}
-		);
-		await expect( page ).toMatchElement( '.wc-order-totals .total', {
-			text: `$${ netPayment }`,
+
+			// Fill up the rest of the form and complete the refund flow
+			await expect( page ).toFill( '#refund_reason', refundReason );
+			await expect( page ).toMatchElement( '.do-api-refund', {
+				text: `Refund $${ refundTotalString } via WooCommerce Payments`,
+			} );
+			const refundDialog = await expect( page ).toDisplayDialog(
+				async () => {
+					await expect( page ).toClick( 'button.do-api-refund' );
+				}
+			);
+			await refundDialog.accept();
+			await uiUnblocked();
+			await page.waitForNavigation( { waitUntil: 'networkidle0' } );
+
+			// Verify each line item shows the refunded quantity and/or amount
+			const updatedRows = await page.$$( '#order_line_items tr.item' );
+			for ( let i = 0; i < refundInputs.length; i++ ) {
+				const { refundQty, refundAmount } = refundInputs[ i ];
+				const row = updatedRows[ i ];
+
+				if ( refundQty ) {
+					await expect( row ).toMatchElement( '.quantity .refunded', {
+						text: `-${ refundQty }`,
+					} );
+				}
+
+				await expect( row ).toMatchElement( '.line_cost .refunded', {
+					text: `-$${ refundAmount.toFixed( 2 ) }`,
+				} );
+			}
+
+			// Verify the refund shows in the list with the amount
+			await expect( page ).toMatchElement( '.refund > .line_cost', {
+				text: `-$${ refundTotalString }`,
+			} );
+
+			// Verify system note was added
+			await expect( page ).toMatchElement( '.system-note', {
+				text: `A refund of $${ refundTotalString } was successfully processed using WooCommerce Payments.`,
+			} );
+			await expect( page ).toMatchElement( '.system-note', {
+				text: `Reason: ${ refundReason }`,
+			} );
+
+			// Verify "Refunded" and "Net Payment" values in Order Totals section
+			await expect( page ).toMatchElement(
+				'.wc-order-totals .total.refunded-total',
+				{
+					text: `-$${ refundTotalString }`,
+				}
+			);
+			await expect( page ).toMatchElement( '.wc-order-totals .total', {
+				text: `$${ netPayment }`,
+			} );
+
+			// Pull out and follow the link to avoid working in multiple tabs
+			const paymentDetailsLink = await page.$eval(
+				'p.order_number > a',
+				( anchor ) => anchor.getAttribute( 'href' )
+			);
+
+			await merchantWCP.openPaymentDetails( paymentDetailsLink );
+
+			// Verify the transaction timeline reflects the refund events
+			await Promise.all( [
+				expect( page ).toMatchElement( 'li.woocommerce-timeline-item', {
+					text: `A payment of $${ refundTotalString } was successfully refunded.`,
+				} ),
+				expect( page ).toMatchElement( 'li.woocommerce-timeline-item', {
+					text: `$${ refundTotalString } will be deducted from a future deposit.`,
+				} ),
+				expect( page ).toMatchElement( 'li.woocommerce-timeline-item', {
+					text: 'Payment status changed to Partial Refund.',
+				} ),
+			] );
 		} );
-
-		// Pull out and follow the link to avoid working in multiple tabs
-		const paymentDetailsLink = await page.$eval(
-			'p.order_number > a',
-			( anchor ) => anchor.getAttribute( 'href' )
-		);
-
-		await merchantWCP.openPaymentDetails( paymentDetailsLink );
-		await takeScreenshot( 'merchant-orders-full-refund_payment-details' );
-
-		// Verify the transaction timeline reflects the refund events
-		await Promise.all( [
-			expect( page ).toMatchElement( 'li.woocommerce-timeline-item', {
-				text: `A payment of $${ refundAmountString } was successfully refunded.`,
-			} ),
-			expect( page ).toMatchElement( 'li.woocommerce-timeline-item', {
-				text: `$${ refundAmountString } will be deducted from a future deposit.`,
-			} ),
-			expect( page ).toMatchElement( 'li.woocommerce-timeline-item', {
-				text: 'Payment status changed to Partial Refund.',
-			} ),
-		] );
-	} );
-} );
+	}
+);
