e4e9fa6dec670dc5dac27b7ee5da1df13bf2a888
frosso <francesco.rosso@automattic.com>
2021-08-05 10:43:19 -0500

    I guess more test cases never hurt anyone, right? ...right?

--- tests/unit/notes/test-class-wc-payments-notes-additional-payment-methods.php
+++ tests/unit/notes/test-class-wc-payments-notes-additional-payment-methods.php
@@ -12,6 +12,7 @@ class WC_Payments_Notes_Additional_Payment_Methods_Test extends WP_UnitTestCase 
 	public function setUp() {
 		parent::setUp();
 
+		wp_set_current_user( 1 );
 		update_option( '_wcpay_feature_upe_settings_preview', '1' );
 		update_option( '_wcpay_feature_upe', '0' );
 	}
@@ -61,5 +62,18 @@ class WC_Payments_Notes_Additional_Payment_Methods_Test extends WP_UnitTestCase 
 		$note = WC_Payments_Notes_Additional_Payment_Methods::get_note();
 
 		$this->assertSame( 'Boost your sales by accepting new payment methods', $note->get_title() );
+	}
+
+	public function test_maybe_enable_feature_flag_redirects_to_onboarding_when_account_not_connected() {
+		$account_mock = $this->getMockBuilder( \WC_Payments_Account::class )->disableOriginalConstructor()->setMethods( [ 'is_stripe_connected', 'redirect_to_onboarding_page' ] )->getMock();
+		$account_mock->expects( $this->atLeastOnce() )->method( 'is_stripe_connected' )->will( $this->returnValue( false ) );
+		$account_mock->expects( $this->once() )->method( 'redirect_to_onboarding_page' );
+		WC_Payments_Notes_Additional_Payment_Methods::set_account( $account_mock );
+		$_GET['page']   = 'wc-settings';
+		$_GET['action'] = 'enable-upe';
+
+		WC_Payments_Notes_Additional_Payment_Methods::maybe_enable_upe_feature_flag();
+
+		$this->assertSame( '0', get_option( '_wcpay_feature_upe' ) );
 	}
 }
