cf20623e6ff6c1135836de05788a1e8e87d87cdb
Brian Borman <brian.borman@automattic.com>
2021-08-09 13:00:18 -0400

    Route any non-UPE payments through parent gateway process payment

--- includes/payment-methods/class-upe-payment-gateway.php
+++ includes/payment-methods/class-upe-payment-gateway.php
@@ -302,13 +302,13 @@ class UPE_Payment_Gateway extends WC_Payment_Gateway_WCPay {
 	}
 
 	/**
-	 * Create and confirm payment intent. Saved payment methods do not follow UPE workflow.
+	 * Create and confirm payment intent. Function used to route any payments that do not use the UPE flow through the parent process payment.
 	 *
 	 * @param int $order_id Order ID to process the payment for.
 	 *
 	 * @return array|null An array with result of payment and redirect URL, or nothing.
 	 */
-	public function process_payment_using_saved_method( $order_id ) {
+	public function parent_process_payment( $order_id ) {
 		return parent::process_payment( $order_id );
 	}
 
@@ -320,9 +320,6 @@ class UPE_Payment_Gateway extends WC_Payment_Gateway_WCPay {
 	 * @return array|null An array with result of payment and redirect URL, or nothing.
 	 */
 	public function process_payment( $order_id ) {
-		if ( isset( $_POST['payment_request_type'] ) && ( 'google_pay' === $_POST['payment_request_type'] || 'apple_pay' === $_POST['payment_request_type'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification.Missing
-			return parent::process_payment( $order_id );
-		}
 		$payment_intent_id         = isset( $_POST['wc_payment_intent_id'] ) ? wc_clean( wp_unslash( $_POST['wc_payment_intent_id'] ) ) : ''; // phpcs:ignore WordPress.Security.NonceVerification.Missing
 		$order                     = wc_get_order( $order_id );
 		$amount                    = $order->get_total();
@@ -347,8 +344,8 @@ class UPE_Payment_Gateway extends WC_Payment_Gateway_WCPay {
 					$selected_upe_payment_type
 				);
 			}
-		} elseif ( $token ) {
-			return $this->process_payment_using_saved_method( $order_id );
+		} else {
+			return $this->parent_process_payment( $order_id );
 		}
 
 		return [
