dc139cb54fe456636fac11fce152336958c80b2c
Veljko <veljano@yahoo.com>
2021-08-13 18:02:43 +0200

    Try with only onboarding flow

--- tests/e2e/specs/merchant/merchant-a-onboarding-setup.spec.js
+++ tests/e2e/specs/merchant/merchant-a-onboarding-setup.spec.js
@@ -24,49 +24,4 @@ describe( 'Onboarding > WooCommerce Setup Wizard & Task List', () => {
 	it( 'can complete onboarding when visiting the first time', async () => {
 		await completeOnboardingWizard();
 	} );
-
-	it( 'can dismiss tax setup', async () => {
-		await merchant.runSetupWizard();
-		await page.waitForSelector( 'div.woocommerce-homescreen' );
-
-		// Click on "Set up tax" task to move to the next step
-		const [ setUpTax ] = await page.$x(
-			'//div[contains(text(),"Set up tax")]'
-		);
-		await setUpTax.click();
-
-		// Click to dismiss setting up taxes
-		await expect( page ).toClick( 'button.components-button.is-tertiary', {
-			text: "I don't charge sales tax",
-		} );
-		await page.waitForNavigation( {
-			waitUntil: 'networkidle0',
-		} );
-	} );
-
-	it( 'can setup shipping', async () => {
-		await merchant.runSetupWizard();
-		await page.waitForSelector( 'div.woocommerce-homescreen' );
-
-		// Click on "Set up shipping" task to move to the next step
-		const [ setUpShipping ] = await page.$x(
-			'//div[contains(text(),"Set up shipping")]'
-		);
-		await setUpShipping.click();
-
-		// Wait for "Proceed" button to become active
-		await page.waitForSelector( 'button.is-primary:not(:disabled)' );
-
-		// Click on "Proceed" button to save shipping settings
-		await page.click( 'button.is-primary' );
-
-		// Verify success message from the response
-		await expect( page ).toMatchElement(
-			'div.components-snackbar__content',
-			'Your shipping rates have been updated'
-		);
-
-		// Click "No, thanks" to get back to the task list
-		await expect( page ).toClick( 'button.components-button.is-tertiary' );
-	} );
 } );
