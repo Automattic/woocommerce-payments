74e15e0caf81277c2eb5f46ff5ac77421ec1ee8f
Rasmy Nguyen <raz@automattic.com>
2021-08-16 10:55:26 +0900

    Adds subscription product service (#2669)
    * Add product service
    
    This adds the product service which handles syncing WC subscription
    products with Stripe.
    
    * Fix archive price issue and add archive unit tests
    
    This fixes an issue where old price ids weren't being archived on
    product update.
    
    This also adds some unit tests for archiving products and prices.
    
    * Add Subscriptions Product mock and fix tests
    
    This adds a Subscriptions Product mock class with which to run product
    service tests.
    
    * Add Subscriptions Product methods to fix tests
    
    This adds the get_trial_length and get_sign_up_fee static methods to the
    WC_Subscriptions_Product test helper to fix some broken tests that
    ignored these if the class didn't exist.
    
    * Move loading of stripe billing into its own class
    
    This moves the loading of Stripe Billing into WC_Payments_Subscriptions
    class.
    
    This also resolves some psalm errors related to missing class
    definitions which are expected.
    
    * Remove update actions before updating product meta
    
    This removes actions from woocommerce_update_product/_variation hooks to
    avoid retriggering on meta update.

--- includes/class-wc-payments.php
+++ includes/class-wc-payments.php
@@ -295,6 +295,10 @@ class WC_Payments {
 			}
 		}
 
+		// Load subscriptions (Stripe Billing).
+		include_once WCPAY_ABSPATH . '/includes/subscriptions/class-wc-payments-subscriptions.php';
+		WC_Payments_Subscriptions::init( self::$api_client );
+
 		add_action( 'rest_api_init', [ __CLASS__, 'init_rest_api' ] );
 		add_action( 'woocommerce_woocommerce_payments_updated', [ __CLASS__, 'set_plugin_activation_timestamp' ] );
 	}
