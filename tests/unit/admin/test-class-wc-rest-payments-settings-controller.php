6b42e656a2d6e07ecd4d8884bd60423a1309b8f2
Jesse Pearson <jessepearson@users.noreply.github.com>
2021-08-19 15:46:41 -0300

    Disable Bancontact, iDEAL, and P24 as UPE payment methods. (#2770)

--- tests/unit/admin/test-class-wc-rest-payments-settings-controller.php
+++ tests/unit/admin/test-class-wc-rest-payments-settings-controller.php
@@ -131,7 +131,7 @@ class WC_REST_Payments_Settings_Controller_Test extends WP_UnitTestCase {
 		$enabled_method_ids = $response->get_data()['available_payment_method_ids'];
 
 		$this->assertEquals(
-			[ 'card', 'bancontact', 'giropay', 'ideal', 'p24', 'sofort' ],
+			[ 'card', 'giropay', 'sofort' ],
 			$enabled_method_ids
 		);
 	}
