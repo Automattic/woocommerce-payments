1efb1852249e5e0a26a96856027aa2a58ef8a6af
Anurag Bhandari <anurag@automattic.com>
2021-08-27 15:18:33 +0530

    Add a new component - DownloadButton
    It is a simple WP button with a hardset icon and some styling.
    
    For now it will be used on TransactionsList and DepositsList components.
    But there is really no restriction to use it anywhere else.

---
+++ client/components/download-button/index.js
@@ -0,0 +1,30 @@
+/** @format **/
+
+/**
+ * External dependencies
+ */
+import React from 'react';
+import { __ } from '@wordpress/i18n';
+import { Button } from '@wordpress/components';
+import Gridicon from 'gridicons';
+
+/**
+ * Internal dependencies
+ */
+import './style.scss';
+
+const DownloadButton = ( { isDisabled, onClick } ) => (
+	<Button
+		key="download"
+		className="woocommerce-table__download-button"
+		disabled={ isDisabled }
+		onClick={ onClick }
+	>
+		<Gridicon icon={ 'cloud-download' } />
+		<span className="woocommerce-table__download-button__label">
+			{ __( 'Download', 'woocommerce-payments' ) }
+		</span>
+	</Button>
+);
+
+export default DownloadButton;
