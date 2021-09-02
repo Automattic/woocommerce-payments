4b7aba77a2048a0fa7ccaef7c1a70a92ec625b9e
Cesar Tardaguila <2722505+ctarda@users.noreply.github.com>
2021-09-02 09:12:37 -0400

    Attempt to register and endpoint for locations

---
+++ includes/admin/class-wc-rest-payments-locations-controller.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Class WC_REST_Payments_Locations_Controller
+ *
+ * @package WooCommerce\Payments\Admin
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+/**
+ * REST controller for account details and status.
+ */
+class WC_REST_Payments_Locations_Controller extends WC_Payments_REST_Controller {
+
+	/**
+	 * Endpoint path.
+	 *
+	 * @var string
+	 */
+	protected $rest_base = 'payments/location';
+
+	/**
+	 * Configure REST API routes.
+	 */
+	public function register_routes() {
+		register_rest_route(
+			$this->namespace,
+			$this->rest_base,
+			[
+				'methods'             => WP_REST_Server::READABLE,
+				'callback'            => [ $this, 'get_location_data' ],
+				'permission_callback' => [ $this, 'check_permission' ],
+			]
+		);
+	}
+
+	/**
+	 * Get account details via API.
+	 *
+	 * @param WP_REST_Request $request Request object.
+	 *
+	 * @return WP_REST_Response|WP_Error
+	 */
+	public function get_location_data( $request ) {
+		$location = [
+					'id' => 'tml_ESCNWwpwlfv5JB',
+					'object'     => 'terminal.location',
+					'display_name'=> 'Hotel Plaza',
+					];
+
+		return rest_ensure_response( $location );
+	}
+}
