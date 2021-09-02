f1a72d12299dcfe3687ac2c276e0e15c0766b4dd
Radoslav Georgiev <rageorgiev@gmail.com>
2021-09-02 12:32:21 +0300

    Handling exceptions from the server.
    Mostly in the context of eligibility for card present.

--- includes/admin/class-wc-rest-payments-terminal-locations-controller.php
+++ includes/admin/class-wc-rest-payments-terminal-locations-controller.php
@@ -7,6 +7,7 @@
 
 defined( 'ABSPATH' ) || exit;
 
+use WCPay\Exceptions\API_Exception;
 /**
  * REST controller for account details and status.
  */
@@ -44,39 +45,43 @@ class WC_REST_Payments_Terminal_Locations_Controller extends WC_Payments_REST_Co
 	 * @return WP_REST_Response|WP_Error
 	 */
 	public function get_store_location( $request ) {
-		$name    = get_bloginfo();
-		$address = [
-			'city'        => WC()->countries->get_base_city(),
-			'country'     => WC()->countries->get_base_country(),
-			'line1'       => WC()->countries->get_base_address(),
-			'line2'       => WC()->countries->get_base_address_2(),
-			'postal_code' => WC()->countries->get_base_postcode(),
-			'state'       => WC()->countries->get_base_state(),
-		];
+		try {
+			$name    = get_bloginfo();
+			$address = [
+				'city'        => WC()->countries->get_base_city(),
+				'country'     => WC()->countries->get_base_country(),
+				'line1'       => WC()->countries->get_base_address(),
+				'line2'       => WC()->countries->get_base_address_2(),
+				'postal_code' => WC()->countries->get_base_postcode(),
+				'state'       => WC()->countries->get_base_state(),
+			];
 
-		// Load the cached locations or retrieve them from the API.
-		$locations = get_transient( static::STORE_LOCATIONS_TRANSIENT_KEY );
-		if ( ! $locations ) {
-			$locations = $this->api_client->get_terminal_locations();
-			set_transient( static::STORE_LOCATIONS_TRANSIENT_KEY, $locations, DAY_IN_SECONDS );
-		}
-
-		// Check the existing locations to see if one of them matches the store.
-		foreach ( $locations as $location ) {
-			if (
-				$location['display_name'] === $name
-				&& count( array_intersect( $location['address'], $address ) ) === count( $address )
-			) {
-				return $this->format_location_response( $location );
+			// Load the cached locations or retrieve them from the API.
+			$locations = get_transient( static::STORE_LOCATIONS_TRANSIENT_KEY );
+			if ( ! $locations ) {
+				$locations = $this->api_client->get_terminal_locations();
+				set_transient( static::STORE_LOCATIONS_TRANSIENT_KEY, $locations, DAY_IN_SECONDS );
 			}
+
+			// Check the existing locations to see if one of them matches the store.
+			foreach ( $locations as $location ) {
+				if (
+					$location['display_name'] === $name
+					&& count( array_intersect( $location['address'], $address ) ) === count( $address )
+				) {
+					return $this->format_location_response( $location );
+				}
+			}
+
+			// Create a new location, and add it to the cached list.
+			$location    = $this->api_client->create_terminal_location( $name, $address );
+			$locations[] = $location;
+			set_transient( static::STORE_LOCATIONS_TRANSIENT_KEY, $locations, DAY_IN_SECONDS );
+
+			return $this->format_location_response( $location );
+		} catch ( API_Exception $e ) {
+			return rest_ensure_response( new WP_Error( $e->get_error_code(), $e->getMessage() ) );
 		}
-
-		// Create a new location, and add it to the cached list.
-		$location    = $this->api_client->create_terminal_location( $name, $address );
-		$locations[] = $location;
-		set_transient( static::STORE_LOCATIONS_TRANSIENT_KEY, $locations, DAY_IN_SECONDS );
-
-		return $this->format_location_response( $location );
 	}
 
 	/**
