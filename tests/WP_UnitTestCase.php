103fabc96ceceeadf4e9127cd8ed006c5965b50f
Dat Hoang <htdat@users.noreply.github.com>
2021-09-03 13:05:44 +0700

    Add stub WP_UnitTestCase

---
+++ tests/WP_UnitTestCase.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Class WP_UnitTestCase
+ *
+ * @package WooCommerce\Payments\Tests
+ */
+
+/**
+ * This stub assists IDE in recognizing PHPUnit tests.
+ *
+ * Class WP_UnitTestCase
+ */
+class WP_UnitTestCase extends \PHPUnit\Framework\TestCase {
+
+}
