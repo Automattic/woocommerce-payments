f9f5262bb08987ac80a81e38fdb1f6bee213ada7
Daniel Mallory <daniel.mallory@automattic.com>
2021-09-10 10:07:24 +0100

    Working on the structure of the currency widget

--- client/multi-currency/blocks/currency-switcher.js
+++ client/multi-currency/blocks/currency-switcher.js
@@ -1,69 +1,138 @@
 /**
  * External dependencies
  */
-// import { sprintf, __ } from '@wordpress/i18n';
-import { registerBlockType } from '@wordpress/blocks';
-import { createElement } from '@wordpress/element';
 import React from 'react';
+import { __ } from '@wordpress/i18n';
+import {
+	CheckboxControl,
+	SelectControl,
+	TextControl,
+} from '@wordpress/components';
+import { registerBlockType } from '@wordpress/blocks';
+import { useBlockProps } from '@wordpress/block-editor';
+import { createElement, useState } from '@wordpress/element';
 // import ReactDOM from 'react-dom';
+
+/**
+ * Internal dependencies
+ */
+import {
+	useCurrencies,
+	useDefaultCurrency,
+	useEnabledCurrencies,
+} from 'wcpay/data';
 
 registerBlockType( 'woocommerce-payments/multi-currency-switcher', {
 	apiVersion: 2,
-	title: 'Currency Switcher',
+	title: __( 'Currency Switcher', 'woocommerce-payments' ),
+	description: __(
+		'Let your customers switch between your enabled currencies',
+		'woocommerce-payments'
+	),
 	icon: 'smiley',
 	category: 'widgets',
 	attributes: {
-		symbol: { type: 'boolean' },
-		flag: { type: 'boolean' },
+		title: {
+			type: 'array',
+		},
+		symbol: {
+			type: 'boolean',
+		},
+		flag: {
+			type: 'boolean',
+		},
 	},
-	edit( props ) {
-		function updateSymbol( event ) {
-			props.setAttributes( { symbol: event.target.value } );
-		}
-		function updateFlag( event ) {
-			props.setAttributes( { flag: event.target.value } );
-		}
+	edit: ( props ) => {
+		const {
+			attributes: { title, symbol, flag },
+			setAttributes,
+			className,
+		} = props;
+
+		const blockProps = useBlockProps();
+
+		const onChangeTitle = ( title ) => {
+			setAttributes( { title: title } );
+		};
+
+		const [ isFlagChecked, onChangeFlag ] = useState( flag );
+		const [ isSymbolChecked, onChangeSymbol ] = useState( symbol );
+
 		return (
-			<div>
-				<p>
-					<label htmlFor="title">Title</label>
-					<input id="title" className="widefat" type="text" value />
-				</p>
-				<p>
-					<input
-						id="symbol"
-						className="checkbox"
-						type="checkbox"
-						value={ props.attributes.symbol }
-						onChange={ updateSymbol }
-						checked={ props.attributes.symbol ? 'checked' : '' }
-					/>
-					<label htmlFor="symbol">Display currency symbols</label>
-				</p>
-				<p>
-					<input
-						id="flag"
-						className="checkbox"
-						type="checkbox"
-						value={ props.attributes.flag }
-						onChange={ updateFlag }
-						checked={ props.attributes.flag ? 'checked' : '' }
-					/>
-					<label htmlFor="flag">
-						Display flags on supported devices
-					</label>
-				</p>
+			<div { ...blockProps }>
+				<h3>{ __( 'Currency Switcher', 'woocommerce-payments' ) }</h3>
+
+				<TextControl
+					label={ __( 'Title', 'woocommerce-payments' ) }
+					// help={ __(
+					// 	'The title that will appear above the currency switcher. Leave blank for no title.',
+					// 	'woocommerce-payments'
+					// ) }
+					onChange={ onChangeTitle }
+					value={ title }
+				/>
+				<CheckboxControl
+					label={ __(
+						'Display currency symbols',
+						'woocommerce-payments'
+					) }
+					checked={ isSymbolChecked }
+					onChange={ onChangeSymbol }
+				/>
+				<CheckboxControl
+					label={ __(
+						'Display flags on supported devices',
+						'woocommerce-payments'
+					) }
+					checked={ isFlagChecked }
+					onChange={ onChangeFlag }
+				/>
 			</div>
 		);
 	},
-	save( props ) {
-		return createElement(
-			'h3',
-			{ style: { border: '3px solid red' } },
-			'The value of symbol is ' +
-				props.attributes.symbol +
-				' and the value of flag is ' +
-				props.attributes.flag
+	save: ( props ) => {
+		// const defaultCurrency = useDefaultCurrency();
+		// const enabledCurrencies = useEnabledCurrencies();
+
+		// console.log( defaultCurrency );
+		// console.log( enabledCurrencies );
+
+		const onChangeCurrency = ( currency ) => {
+			// TODO: All this would need to do is submit the form so maybe could be inline.
+			print( currency );
+		};
+
+		const blockProps = useBlockProps.save();
+
+		return (
+			<div { ...blockProps }>
+				<span className="widget-title">{ props.attributes.title }</span>
+				<form>
+					<select
+						name="currency"
+						aria-label={ props.attributes.title }
+						onBlur="this.form.submit()"
+					>
+						<option value="USD">USD</option>
+						<option value="EUR">EUR</option>
+						<option value="JPY">JPY</option>
+					</select>
+				</form>
+			</div>
+			// <div { ...blockProps }>
+			// 	<h3>This is the Currency Switcher Widget</h3>
+			// </div>
+			// <SelectControl
+			// 	{ ...blockProps }
+			// 	label={ props.attributes.title }
+			// 	value={ [ 'a', 'b', 'c' ] }
+			// 	onChange={ onChangeCurrency }
+			// 	options={ [
+			// 		{ value: 'jpy', label: 'JPY' },
+			// 		{ value: 'eur', label: 'EUR' },
+			// 		{ value: 'usd', label: 'USD' },
+			// 	] }
+			// />
 		);
 	},
 } );
