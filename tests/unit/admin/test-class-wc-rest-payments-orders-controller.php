a1950e08e67b758f6ba5a1e068df12a24588009c
Anurag Bhandari <anurag@automattic.com>
2021-09-14 14:48:13 +0530

    Make minor adjustments to terminal payment invalid args test
    * Rename the test function to a more generic name
    * Make a stricter assertion with assertSame for HTTP code
    * Add an assertion for error code
    * Split the error message string into multiple lines

--- tests/unit/admin/test-class-wc-rest-payments-orders-controller.php
+++ tests/unit/admin/test-class-wc-rest-payments-orders-controller.php
@@ -223,7 +223,7 @@ class WC_REST_Payments_Orders_Controller_Test extends WP_UnitTestCase {
 		$this->assertEquals( 'Payment capture failed to complete with the following message: Test error', $response->get_error_message() );
 	}
 
-	public function test_capture_terminal_payment_error_capture_amount_greater_than_authorized_amount() {
+	public function test_capture_terminal_payment_error_invalid_arguments() {
 		$order = $this->create_mock_order();
 
 		$mock_intent = $this->createMock( WC_Payments_API_Intention::class );
@@ -245,6 +245,7 @@ class WC_REST_Payments_Orders_Controller_Test extends WP_UnitTestCase {
 			->expects( $this->once() )
 			->method( 'capture_charge' )
 			->willReturn(
+				// See https://stripe.com/docs/error-codes#amount-too-large.
 				[
 					'status'    => 'failed',
 					'message'   => 'Error: The payment could not be captured because the requested capture amount is greater than the authorized amount.',
@@ -265,8 +266,13 @@ class WC_REST_Payments_Orders_Controller_Test extends WP_UnitTestCase {
 		$this->assertInstanceOf( 'WP_Error', $response );
 		$data = $response->get_error_data();
 		$this->assertArrayHasKey( 'status', $data );
-		$this->assertEquals( 400, $data['status'] );
-		$this->assertEquals( 'Payment capture failed to complete with the following message: Error: The payment could not be captured because the requested capture amount is greater than the authorized amount.', $response->get_error_message() );
+		$this->assertSame( 400, $data['status'] );
+		$this->assertSame( 'wcpay_capture_error', $response->get_error_code() );
+		$this->assertEquals(
+			'Payment capture failed to complete with the following message: ' .
+			'Error: The payment could not be captured because the requested capture amount is greater than the authorized amount.',
+			$response->get_error_message()
+		);
 	}
 
 	public function test_capture_terminal_payment_handles_exceptions() {
