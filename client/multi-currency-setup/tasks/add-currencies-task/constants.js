a592b44ad61b759b197158d6e3df55456f3755c5
Taha Paksu <taha.paksu@automattic.com>
2021-09-14 15:38:15 +0300

    Translate `numberWords`

--- client/multi-currency-setup/tasks/add-currencies-task/constants.js
+++ client/multi-currency-setup/tasks/add-currencies-task/constants.js
@@ -1,14 +1,19 @@
+/**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
 export const numberWords = [
-	'any',
-	'one',
-	'two',
-	'three',
-	'four',
-	'five',
-	'six',
-	'seven',
-	'eight',
-	'nine',
+	__( 'any', 'woocommerce-payments' ),
+	__( 'one', 'woocommerce-payments' ),
+	__( 'two', 'woocommerce-payments' ),
+	__( 'three', 'woocommerce-payments' ),
+	__( 'four', 'woocommerce-payments' ),
+	__( 'five', 'woocommerce-payments' ),
+	__( 'six', 'woocommerce-payments' ),
+	__( 'seven', 'woocommerce-payments' ),
+	__( 'eight', 'woocommerce-payments' ),
+	__( 'nine', 'woocommerce-payments' ),
 ];
 
 export const recommendedCurrencyCodes = [
