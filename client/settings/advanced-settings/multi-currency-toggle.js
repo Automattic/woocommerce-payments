258efaac213e307def74b11ebd64219d4e409e96
Taha Paksu <taha.paksu@automattic.com>
2021-09-15 18:58:40 +0300

    Update regarding design specs

--- client/settings/advanced-settings/multi-currency-toggle.js
+++ client/settings/advanced-settings/multi-currency-toggle.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { CheckboxControl } from '@wordpress/components';
+import { CheckboxControl, ExternalLink } from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import { useEffect, useRef } from '@wordpress/element';
 
@@ -9,6 +9,7 @@ import { useEffect, useRef } from '@wordpress/element';
  * Internal dependencies
  */
 import { useMultiCurrency } from 'wcpay/data';
+import interpolateComponents from 'interpolate-components';
 
 const MultiCurrencyToggle = () => {
 	const [
@@ -29,37 +30,23 @@ const MultiCurrencyToggle = () => {
 	};
 
 	return (
-		<>
-			<h4 ref={ headingRef } tabIndex="-1">
-				{ __(
-					'Enable Customer Multi Currency',
+		<CheckboxControl
+			label={ __( 'Enable multi-currency', 'woocommerce-payments' ) }
+			help={ interpolateComponents( {
+				mixedString: __(
+					'Allow customers to shop and pay in multiple currencies. {{learnMoreLink}}Learn more{{/learnMoreLink}}',
 					'woocommerce-payments'
-				) }
-			</h4>
-			<CheckboxControl
-				label={ __(
-					'Allow your customers to shop and pay in their local currency.',
-					'woocommerce-payments'
-				) }
-				help={
-					! isMultiCurrencyEnabled
-						? __(
-								'By enabling this, a new tab, "Multi-currency" will appear on the ' +
-									'WooCommerce Settings page, and you will be able to configure this ' +
-									'feature to suit your needs.',
-								'woocommerce-payments'
-						  )
-						: __(
-								'By disabling this, the "Multi-currency" tab, added by our feature, ' +
-									"will be hidden from your settings page, and you won't be able to use " +
-									'this feature in your store.',
-								'woocommerce-payments'
-						  )
-				}
-				checked={ isMultiCurrencyEnabled }
-				onChange={ handleMultiCurrencyStatusChange }
-			/>
-		</>
+				),
+				components: {
+					learnMoreLink: (
+						// eslint-disable-next-line max-len
+						<ExternalLink href="https://docs.woocommerce.com/document/payments/currencies/multi-currency-setup" />
+					),
+				},
+			} ) }
+			checked={ isMultiCurrencyEnabled }
+			onChange={ handleMultiCurrencyStatusChange }
+		/>
 	);
 };
 
