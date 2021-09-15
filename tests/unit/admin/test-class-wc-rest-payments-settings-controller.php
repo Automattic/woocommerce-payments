b88df2c01a8814cad9b6990bf5e0dd3a83f81424
Naman Malhotra <naman03malhotra@gmail.com>
2021-09-15 14:46:56 +0530

    fix test

--- tests/unit/admin/test-class-wc-rest-payments-settings-controller.php
+++ tests/unit/admin/test-class-wc-rest-payments-settings-controller.php
@@ -134,7 +134,7 @@ class WC_REST_Payments_Settings_Controller_Test extends WP_UnitTestCase {
 		$enabled_method_ids = $response->get_data()['available_payment_method_ids'];
 
 		$this->assertEquals(
-			[ 'card', 'sepa_debit', 'giropay', 'sofort' ],
+			[ 'card', 'giropay', 'sofort' ],
 			$enabled_method_ids
 		);
 	}
