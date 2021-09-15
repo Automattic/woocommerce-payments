0482400f139a5e990f4f59c340fba78f446fef71
Taha Paksu <taha.paksu@automattic.com>
2021-09-15 22:19:15 +0300

    Extract preview pane into its own component

---
+++ client/multi-currency/preview-modal/index.js
@@ -0,0 +1,46 @@
+/**
+ * External dependencies
+ */
+import { Modal } from '@wordpress/components';
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies
+ */
+import './index.scss';
+
+const PreviewModal = ( {
+	isPreviewModalOpen,
+	isStorefrontSwitcherEnabledValue,
+	isAutomaticSwitchEnabledValue,
+	setPreviewModalOpen,
+} ) => {
+	const handlePreviewModalCloseClick = () => {
+		setPreviewModalOpen( false );
+	};
+
+	return (
+		isPreviewModalOpen && (
+			<Modal
+				title={ __( 'Preview', 'woocommerce-payments' ) }
+				isDismissible={ true }
+				className="multi-currency-store-settings-preview-modal"
+				shouldCloseOnClickOutside={ false }
+				onRequestClose={ handlePreviewModalCloseClick }
+			>
+				<iframe
+					title={ __( 'Preview', 'woocommerce-payments' ) }
+					className={ 'multi-currency-store-settings-preview-iframe' }
+					src={
+						'/?is_mc_onboarding_simulation=1&enable_storefront_switcher=' +
+						isStorefrontSwitcherEnabledValue +
+						'&enable_auto_currency=' +
+						isAutomaticSwitchEnabledValue
+					}
+				></iframe>
+			</Modal>
+		)
+	);
+};
+
+export default PreviewModal;
