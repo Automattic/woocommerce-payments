b3fb87afb3da572a9dcb4a74b20461a9399f2019
Taha Paksu <taha.paksu@automattic.com>
2021-09-16 11:16:59 +0300

    Refactor and add tests

--- client/multi-currency-setup/tasks/add-currencies-task/utils.js
+++ client/multi-currency-setup/tasks/add-currencies-task/utils.js
@@ -10,12 +10,15 @@ import { sprintf, __ } from '@wordpress/i18n';
  * @return {string} The string representation of the currency object.
  */
 export const StringRepresentationOfCurrency = ( currency ) => {
-	const name = currency.name;
-	const hintText =
-		currency.code === currency.symbol
-			? currency.code
-			: sprintf( '%s %s', currency.symbol, currency.code );
-	return sprintf( '%s (%s)', name, hintText );
+	if ( currency && currency.name && currency.symbol && currency.code ) {
+		const name = currency.name;
+		const hintText =
+			currency.code === currency.symbol
+				? currency.code
+				: sprintf( '%s %s', currency.symbol, currency.code );
+		return sprintf( '%s (%s)', name, hintText );
+	}
+	return '';
 };
 
 /**
@@ -31,27 +34,19 @@ export const ConcatenateCurrencyStrings = (
 	except,
 	currenciesData
 ) => {
-	const currenciesToList = currencies.filter( ( code ) => code !== except );
-	if ( 0 === currenciesToList.length ) return '';
-	if ( 1 === currenciesToList.length )
-		return StringRepresentationOfCurrency(
-			currenciesData[ currenciesToList[ 0 ] ]
-		);
-	if ( 2 === currenciesToList.length )
-		return currenciesToList
-			.map( ( d ) => currenciesData[ d ] )
-			.map( StringRepresentationOfCurrency )
-			.join( __( ' and ' ) );
-
-	const lastCurrency = currenciesData[ currenciesToList.pop() ];
-	return (
-		currenciesToList
-			.map( ( d ) => currenciesData[ d ] )
-			.map( StringRepresentationOfCurrency )
-			.join( ', ' ) +
-		', ' +
-		__( 'and', 'woocommerce-payments' ) +
-		' ' +
-		StringRepresentationOfCurrency( lastCurrency )
+	const filteredCurrencies = currencies.filter(
+		( code ) => code !== except && currenciesData[ code ]
 	);
+	const __and = __( 'and', 'woocommerce-payments' );
+	return filteredCurrencies
+		.map( ( code ) =>
+			StringRepresentationOfCurrency( currenciesData[ code ] )
+		)
+		.join( ', ' )
+		.replace(
+			/, ([^,]+)$/,
+			2 === filteredCurrencies.length
+				? ' ' + __and + ' $1'
+				: ', ' + __and + ' $1'
+		);
 };
