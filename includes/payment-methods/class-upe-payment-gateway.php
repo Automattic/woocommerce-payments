ef9c111ce6157a108cf4b1cadd5ff2ee24118cd9
Chris Aprea <chris.aprea@automattic.com>
2021-09-20 13:39:05 +1000

    Trigger an event to init the tokenization-form.js script (#2933)
    Co-authored-by: Chris <chris.aprea@automattic.comchris.aprea@automattic.comchris.aprea@automattic.com>

--- includes/payment-methods/class-upe-payment-gateway.php
+++ includes/payment-methods/class-upe-payment-gateway.php
@@ -111,7 +111,7 @@ class UPE_Payment_Gateway extends WC_Payment_Gateway_WCPay {
 		wp_register_script(
 			'wcpay-upe-checkout',
 			plugins_url( 'dist/upe_checkout.js', WCPAY_PLUGIN_FILE ),
-			[ 'stripe', 'wc-checkout' ],
+			[ 'stripe', 'wc-checkout', 'woocommerce-tokenization-form' ],
 			WC_Payments::get_file_version( 'dist/upe_checkout.js' ),
 			true
 		);
