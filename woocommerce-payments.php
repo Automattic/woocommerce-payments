d2b9e5fcb3f9112bdf17a711b86d298743244036
Dat Hoang <htdat@users.noreply.github.com>
2021-09-22 16:31:31 +0700

    Remove most of var_dump

--- woocommerce-payments.php
+++ woocommerce-payments.php
@@ -55,17 +55,10 @@ register_activation_hook( __FILE__, 'wcpay_activated' );
 register_deactivation_hook( __FILE__, 'wcpay_deactivated' );
 
 // The JetPack autoloader might not catch up yet when activating the plugin. If so, we'll stop here to avoid JetPack connection failures.
-var_dump( __FILE__ . ' - file and line - ' . __LINE__ ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_var_dump
-var_dump( class_exists( Automattic\Jetpack\Connection\Rest_Authentication::class ) ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_var_dump
-var_dump( class_exists( MyCLabs\Enum\Enum::class ) ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_var_dump
-
 $is_autoloading_ready = class_exists( Automattic\Jetpack\Connection\Rest_Authentication::class ) && class_exists( MyCLabs\Enum\Enum::class );
 if ( ! $is_autoloading_ready ) {
 	return;
 }
-
-var_dump( __FILE__ . ' - file and line - ' . __LINE__ ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_var_dump
-
 
 // Subscribe to automated translations.
 add_filter( 'woocommerce_translations_updates_for_woocommerce-payments', '__return_true' );
@@ -75,7 +68,6 @@ add_filter( 'woocommerce_translations_updates_for_woocommerce-payments', '__retu
  */
 function wcpay_jetpack_init() {
 	if ( ! wcpay_check_old_jetpack_version() ) {
-		var_dump( __FILE__ . ' - file and line - ' . __LINE__ ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_var_dump
 		return;
 	}
 	$jetpack_config = new Automattic\Jetpack\Config();
@@ -99,15 +91,12 @@ add_action( 'plugins_loaded', 'wcpay_jetpack_init', 1 );
  */
 function wcpay_init() {
 	require_once WCPAY_ABSPATH . '/includes/class-wc-payments.php';
-	// TODO Just for testing.
-	var_dump( __FILE__ . ' - file and line - ' . __LINE__ ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_var_dump
 	WC_Payments::init();
 }
 
 // Make sure this is run *after* WooCommerce has a chance to initialize its packages (wc-admin, etc). That is run with priority 10.
 // If you change the priority of this action, you'll need to change it in the wcpay_check_old_jetpack_version function too.
 add_action( 'plugins_loaded', 'wcpay_init', 11 );
-var_dump( __FILE__ . ' - file and line - ' . __LINE__ ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_var_dump
 
 /**
  * Check if WCPay is installed alongside an old version of Jetpack (8.1 or earlier). Due to the autoloader code in those old
@@ -117,13 +106,10 @@ var_dump( __FILE__ . ' - file and line - ' . __LINE__ ); // phpcs:ignore WordPre
  * @return bool True if the plugin can keep initializing itself, false otherwise.
  */
 function wcpay_check_old_jetpack_version() {
-	var_dump( __FILE__ . ' - file and line - ' . __LINE__ ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_var_dump
-
 	if ( defined( 'JETPACK__VERSION' ) && version_compare( JETPACK__VERSION, '8.2', '<' ) && JETPACK__VERSION !== 'wpcom' ) {
 		add_filter( 'admin_notices', 'wcpay_show_old_jetpack_notice' );
 		// Prevent the rest of the plugin from initializing.
 		remove_action( 'plugins_loaded', 'wcpay_init', 11 );
-		var_dump( __FILE__ . ' - file and line - ' . __LINE__ ); // phpcs:ignore WordPress.PHP.DevelopmentFunctions.error_log_var_dump
 		return false;
 	}
 	return true;
