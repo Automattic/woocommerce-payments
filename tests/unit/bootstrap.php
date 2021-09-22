97128b04df26c110f2a948693023e31a5a0fbaa9
Dat Hoang <htdat@users.noreply.github.com>
2021-09-22 16:40:00 +0700

    Revert yoast/phpunit-polyfills

--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -74,9 +74,6 @@ function _manually_load_plugin() {
 	require_once $_plugin_dir . 'includes/admin/tracks/class-tracker.php';
 	require_once $_plugin_dir . 'includes/notes/class-wc-payments-notes-additional-payment-methods.php';
 }
-// TODO - just for test. Try loading phpunit-polyfills.
-$_plugin_dir = dirname( __FILE__ ) . '/../../';
-require_once $_plugin_dir . '/vendor/yoast/phpunit-polyfills/phpunitpolyfills-autoload.php';
 
 tests_add_filter( 'muplugins_loaded', '_manually_load_plugin' );
 
