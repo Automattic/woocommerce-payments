0dc381fb9372e0ccfcd573901d459f82ae73347a
César Costa <costa.cesar.a@hotmail.com>
2021-09-27 11:34:43 -0300

    Set default capture method to automatic on create intention

--- includes/wc-payment-api/class-wc-payments-api-client.php
+++ includes/wc-payment-api/class-wc-payments-api-client.php
@@ -231,7 +231,7 @@ class WC_Payments_API_Client {
 	 * @param string $currency_code   - Currency to charge in.
 	 * @param array  $payment_methods - Payment methods to include.
 	 * @param int    $order_id        - The order ID.
-	 * @param string $capture_method  - Capture method (either `automatic` or `manual`).
+	 * @param string $capture_method  - optional capture method (either `automatic` or `manual`).
 	 *
 	 * @return WC_Payments_API_Intention
 	 * @throws API_Exception - Exception thrown on intention creation failure.
@@ -241,7 +241,7 @@ class WC_Payments_API_Client {
 		$currency_code,
 		$payment_methods,
 		$order_id,
-		$capture_method
+		$capture_method = 'automatic'
 	) {
 		$request                         = [];
 		$request['amount']               = $amount;
