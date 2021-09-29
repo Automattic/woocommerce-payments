0624022e999e00aab076fc8b2aa36d633d0296e7
Danae Millan <danae.millan@automattic.com>
2021-09-29 13:05:16 -0300

    Remove redundant check avoid tracking UPE in new installs
    UPE track will be skipped because the UPE option won't be set in new installs, so this isn't needed.

--- includes/migrations/class-track-upe-status.php
+++ includes/migrations/class-track-upe-status.php
@@ -31,13 +31,6 @@ class Track_Upe_Status {
 			return;
 		}
 
-		// No need to track on new installs. We want the data
-		// from existing ones. New installs will have the events built-in.
-		if ( ! get_option( 'woocommerce_woocommerce_payments_version' ) ) {
-			self::mark_as_tracked();
-			return;
-		}
-
 		$upe_value = get_option( \WC_Payments_Features::UPE_FLAG_NAME, 'not-set' );
 
 		// Don't trigger the track event when the flag isn't set.
