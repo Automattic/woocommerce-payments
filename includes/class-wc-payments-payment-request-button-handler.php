d5e0a40d6dad8f10a173af623718a821260d3e18
Malith Senaweera <malith.senaweera@automattic.com>
2021-09-30 12:43:13 -0500

    Adjust product page Payment Request shipping options
    When checking out through Google Pay button in cart page, it checks
    whether there is a shipping method before showing shipping options in
    payments popup. However, the same check is absent when checking out from
    product page. This change adds the same checks to product checkout to make
    the two checkouts consistent.

--- includes/class-wc-payments-payment-request-button-handler.php
+++ includes/class-wc-payments-payment-request-button-handler.php
@@ -283,7 +283,7 @@ class WC_Payments_Payment_Request_Button_Handler {
 			];
 		}
 
-		if ( wc_shipping_enabled() && $product->needs_shipping() ) {
+		if ( wc_shipping_enabled() && 0 !== wc_get_shipping_method_count( true ) && $product->needs_shipping() ) {
 			$items[] = [
 				'label'   => __( 'Shipping', 'woocommerce-payments' ),
 				'amount'  => 0,
@@ -305,7 +305,7 @@ class WC_Payments_Payment_Request_Button_Handler {
 			'pending' => true,
 		];
 
-		$data['needs_shipping'] = ( wc_shipping_enabled() && $product->needs_shipping() );
+		$data['needs_shipping'] = ( wc_shipping_enabled() && 0 !== wc_get_shipping_method_count( true ) && $product->needs_shipping() );
 		$data['currency']       = strtolower( get_woocommerce_currency() );
 		$data['country_code']   = substr( get_option( 'woocommerce_default_country' ), 0, 2 );
 
