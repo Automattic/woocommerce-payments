d2b62517bc88413a350810bddb22c55e2535835b
Dan Paun <dan.paun@automattic.com>
2021-09-30 16:57:20 +0300

    move is connected check outside try block

--- includes/class-wc-payment-gateway-wcpay.php
+++ includes/class-wc-payment-gateway-wcpay.php
@@ -499,11 +499,12 @@ class WC_Payment_Gateway_WCPay extends WC_Payment_Gateway_CC {
 	 * @return bool
 	 */
 	public function is_card_present_eligible( $empty_value = false ) {
-		try {
-			if ( ! $this->is_connected() ) {
-				return $empty_value;
-			}
 
+		if ( ! $this->is_connected() ) {
+			return $empty_value;
+		}
+
+		try {
 			return $this->account->is_card_present_eligible();
 		} catch ( Exception $e ) {
 			Logger::error( 'Failed to get account card present eligible .' . $e );
