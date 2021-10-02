536cb0f7e0803f896a049806c7932b8f8a1a964e
Danae Millan <danae.millan@automattic.com>
2021-10-02 14:40:20 -0300

    Create an ErrorBoundary component
    First iteration of an ErrorBoundary component to avoid unmounting the whole application when something goes wrong.

---
+++ client/components/error-boundary/index.js
@@ -0,0 +1,45 @@
+/**
+ * WordPress dependencies
+ */
+import { Component } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+import { Notice } from '@wordpress/components';
+
+class ErrorBoundary extends Component {
+	constructor() {
+		super( ...arguments );
+
+		this.state = {
+			error: null,
+		};
+	}
+
+	static getDerivedStateFromError( error ) {
+		return { error };
+	}
+
+	componentDidCatch( error, info ) {
+		if ( this.props.onError ) {
+			this.props.onError( error, info );
+		}
+	}
+
+	render() {
+		if ( ! this.state.error ) {
+			return this.props.children;
+		}
+
+		return (
+			<Notice status="error" isDismissible={ false }>
+				{ __(
+					'There was an error rendering the component, please try again. Contact support for assistance if the problem persists.',
+					'woocommerce-payments'
+				) }
+				<br />
+				{ this.state.error.toString() }
+			</Notice>
+		);
+	}
+}
+
+export default ErrorBoundary;
