c70b9fa541012f9eeb9e956b96c6f5c226c356c5
Rasmy Nguyen <raz@automattic.com>
2021-10-06 07:50:15 +0900

    Load tokenization script on add-payment-method page (#3043)

--- includes/payment-methods/class-upe-payment-gateway.php
+++ includes/payment-methods/class-upe-payment-gateway.php
@@ -688,7 +688,7 @@ class UPE_Payment_Gateway extends WC_Payment_Gateway_WCPay {
 	 */
 	public function payment_fields() {
 		try {
-			$display_tokenization = $this->supports( 'tokenization' ) && is_checkout();
+			$display_tokenization = $this->supports( 'tokenization' ) && ( is_checkout() || is_add_payment_method_page() );
 
 			$payment_fields = $this->get_payment_fields_js_config();
 			wp_localize_script( 'wcpay-upe-checkout', 'wcpay_config', $payment_fields );
