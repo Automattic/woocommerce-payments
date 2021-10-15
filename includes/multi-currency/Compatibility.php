fc3936cd7be0c6a5563fa8b41032a4570cef1bf9
Jesse Pearson <jesse.pearson@automattic.com>
2021-10-15 15:06:26 -0300

    Add compatibility for Multi-Currency and UPS plugin.

--- includes/multi-currency/Compatibility.php
+++ includes/multi-currency/Compatibility.php
@@ -9,6 +9,7 @@ namespace WCPay\MultiCurrency;
 
 use WC_Order;
 use WC_Order_Refund;
+use WCPay\MultiCurrency\Compatibility\WooCommerceUPS;
 
 defined( 'ABSPATH' ) || exit;
 
@@ -16,6 +17,9 @@ defined( 'ABSPATH' ) || exit;
  * Class that controls Multi-Currency Compatibility.
  */
 class Compatibility {
+
+	const FILTER_PREFIX = 'wcpay_multi_currency_';
+
 	/**
 	 * Subscription switch cart item.
 	 *
@@ -57,6 +61,8 @@ class Compatibility {
 		if ( defined( 'DOING_CRON' ) ) {
 			add_filter( 'woocommerce_admin_sales_record_milestone_enabled', [ $this, 'attach_order_modifier' ] );
 		}
+
+		$compatibility_classes[] = new WooCommerceUPS( $multi_currency, $utils );
 	}
 
 	/**
@@ -258,6 +264,15 @@ class Compatibility {
 		}
 
 		return true;
+	}
+
+	/**
+	 * Determines if the store currency should be returned or not.
+	 *
+	 * @return bool
+	 */
+	public function should_return_store_currency(): bool {
+		return apply_filters( self::FILTER_PREFIX . 'should_return_store_currency', false );
 	}
 
 	/**
