034089f5fcb775766f1a756bd07a0a85ce4b011f
David Quiñones <daquinons@users.noreply.github.com>
2021-10-21 16:26:57 +0200

    Enable P24 UPE payment method

--- includes/payment-methods/class-upe-payment-gateway.php
+++ includes/payment-methods/class-upe-payment-gateway.php
@@ -777,6 +777,7 @@ class UPE_Payment_Gateway extends WC_Payment_Gateway_WCPay {
 
 		$methods[] = 'giropay';
 		$methods[] = 'sofort';
+		$methods[] = 'p24';
 
 		return array_values(
 			apply_filters(
