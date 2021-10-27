e351d41b438b8ea8c4fe306bbd95c878af95311c
Radoslav Georgiev <rageorgiev@gmail.com>
2021-10-27 12:08:37 +0300

    Raise WC tested up and Tested up to

--- woocommerce-payments.php
+++ woocommerce-payments.php
@@ -9,7 +9,7 @@
  * Text Domain: woocommerce-payments
  * Domain Path: /languages
  * WC requires at least: 4.4
- * WC tested up to: 5.7.1
+ * WC tested up to: 5.8.0
  * Requires WP: 5.6
  * Version: 3.1.0
  *
