67c4ffdbfa4ddb0f08cc5f21ad6fe0e0a1ef87e8
Leonardo Lopes de Albuquerque <leonardo.albuquerque@automattic.com>
2021-10-28 14:30:19 -0300

    changed as much as possible from jquery to pure js #3101

--- client/admin/admin.js
+++ client/admin/admin.js
@@ -11,44 +11,50 @@ jQuery( function ( $ ) {
 	const ajaxUrl = woocommerce_admin.ajax_url;
 
 	$( '#woocommerce-subscription-data #customer_user' ).change( function () {
-		const data = {
-			action: 'wcs_get_saved_credit_cards',
-			customer: $( this ).val(),
-			nonce: wcpay_admin.get_cards_tokens_nonce,
-		};
+		const data = new FormData();
 
-		$.ajax( {
-			url: ajaxUrl,
-			data: data,
-			dataType: 'json',
-			type: 'POST',
-			success: function ( cards ) {
-				const cardsSelect = $(
+		data.append( 'action', 'wcs_get_saved_credit_cards' );
+		data.append( 'customer', this.options[ this.selectedIndex ].value );
+		data.append( 'nonce', wcpay_admin.get_cards_tokens_nonce );
+
+		fetch( ajaxUrl, {
+			method: 'POST',
+			credentials: 'same-origin',
+			body: data,
+		} )
+			.then( ( response ) => response.json() )
+			.then( ( cards ) => {
+				const cardsSelect = document.querySelector(
 					'#' +
-						$.escapeSelector(
+						CSS.escape(
 							'_payment_method_meta[woocommerce_payments][wc_order_tokens][token]'
 						)
-				).html( '' );
-
-				cardsSelect.append(
-					$( '<option>', {
-						value: 0,
-						text: __(
-							'Please select a payment method',
-							'woocommerce-payments'
-						),
-					} )
 				);
 
-				$.each( cards, function ( i, card ) {
-					cardsSelect.append(
-						$( '<option>', {
-							value: card.tokenId,
-							text: card.displayName,
-						} )
-					);
-				} );
-			},
-		} );
+				let html = '';
+
+				html +=
+					'<option value="">' +
+					__(
+						'Please select a payment method',
+						'woocommerce-payments'
+					) +
+					'</option>';
+
+				for ( const card of cards ) {
+					html +=
+						'<option value="' +
+						card.tokenId +
+						'">' +
+						card.displayName +
+						'</option>';
+				}
+
+				cardsSelect.innerHTML = html;
+			} )
+			.catch( ( error ) => {
+				console.log( '[WP Pageviews Plugin]' );
+				console.error( error );
+			} );
 	} );
 } );
