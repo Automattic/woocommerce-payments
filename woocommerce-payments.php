0698f63b839a2877ee4c9914ed3d6869655254c6
mattallan <matt.allan@automattic.com>
2021-11-04 16:26:42 +1000

    Check for the WooCommerce class

--- woocommerce-payments.php
+++ woocommerce-payments.php
@@ -106,7 +106,7 @@ if ( ! function_exists( 'wcpay_init_subscriptions_core' ) ) {
 	 * Initialise subscriptions-core if WC Subscriptions (the plugin) isn't loaded
 	 */
 	function wcpay_init_subscriptions_core() {
-		if ( ! WC_Payments_Features::is_wcpay_subscriptions_enabled() ) {
+		if ( ! class_exists( 'WooCommerce' ) || ! WC_Payments_Features::is_wcpay_subscriptions_enabled() ) {
 			return;
 		}
 
@@ -135,7 +135,7 @@ if ( ! function_exists( 'wcpay_init_subscriptions_core' ) ) {
 					include_once ABSPATH . 'wp-admin/includes/plugin.php';
 				}
 
-				return is_plugin_active( $plugin_slug . '/' . $plugin_slug . '.php' );
+				return is_plugin_active( $plugin_slug );
 			}
 		};
 
