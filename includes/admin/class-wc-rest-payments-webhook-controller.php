06adbe03bc150bb1a24bacbc3726d477ad1329dc
James Allan <james.allan@automattic.com>
2021-11-10 15:00:12 +1000

    Fix race conditions leading to duplicate order status transitions and invoice already paid errors (#3287)
    * Only attempt to set the payment intent on invoice paid webhooks if the intent exists
    
    * Catch and handle "Invoice is already paid" errors
    
    * Avoid race conditions when processing payments via the payment_intent.succeeded webhook and UPE redirect via the order received page
    
    * Add changelog entries
    
    * Rather than get a brand new instance of the order, just read the properties again
    
    * Fix unit test
    
    Co-authored-by: Chris <chris.aprea@automattic.comchris.aprea@automattic.comchris.aprea@automattic.com>

--- includes/admin/class-wc-rest-payments-webhook-controller.php
+++ includes/admin/class-wc-rest-payments-webhook-controller.php
@@ -287,6 +287,9 @@ class WC_REST_Payments_Webhook_Controller extends WC_Payments_REST_Controller {
 			);
 		}
 
+		// Get an updated set of order properties to avoid race conditions when the server sends the paid webhook before we've finished processing the original payment request.
+		$order->get_data_store()->read( $order );
+
 		if ( ! $order->has_status( [ 'processing', 'completed' ] ) ) {
 			$order->payment_complete();
 		}
