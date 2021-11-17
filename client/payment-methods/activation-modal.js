7b1d56385445cabaab46409d280e3981257745e1
Taha Paksu <3295+tpaksu@users.noreply.github.com>
2021-11-17 19:36:42 +0300

    Fix activation modal list

--- client/payment-methods/activation-modal.js
+++ client/payment-methods/activation-modal.js
@@ -14,6 +14,7 @@ import PaymentConfirmIllustration from '../components/payment-confirm-illustrati
 import ConfirmationModal from '../components/confirmation-modal';
 import PaymentMethodsMap from '../payment-methods-map';
 import RequirementsMap from '../requirements-map';
+import './activation-modal.scss';
 
 const ConfirmPaymentMethodActivationModal = ( {
 	paymentMethod,
