9f5ed72df7281d36c1a1950b4caa358bcfb9229d
James Allan <james.allan@automattic.com>
2021-11-23 09:10:07 +1000

    Use WC Subscriptions helper function to generate URL for adding subscription products

--- includes/subscriptions/class-wc-payments-subscriptions-empty-state-manager.php
+++ includes/subscriptions/class-wc-payments-subscriptions-empty-state-manager.php
@@ -53,8 +53,8 @@ class WC_Payments_Subscriptions_Empty_State_Manager {
 		$script_asset      = file_exists( $script_asset_path ) ? require_once $script_asset_path : [ 'dependencies' => [] ];
 		$wcpay_settings    = [
 			'connectUrl'    => WC_Payments_Account::get_connect_url(),
-			'isConnected'   => $this->account->is_stripe_connected(),
-			'newProductUrl' => esc_url( admin_url( 'post-new.php?post_type=product' ) ),
+			'isConnected'   => ! $this->account->is_stripe_connected(),
+			'newProductUrl' => WC_Subscriptions_Admin::add_subscription_url(),
 		];
 
 		wp_register_script(
