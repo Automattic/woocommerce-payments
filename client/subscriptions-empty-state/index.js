50dc8422d2f8992cb1bdf2d0b3f3380a9eddf4fa
James Allan <james.allan@automattic.com>
2021-11-22 15:43:46 +1000

    Add empty state screens

---
+++ client/subscriptions-empty-state/index.js
@@ -0,0 +1,98 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import { __ } from '@wordpress/i18n';
+import ReactDOM from 'react-dom';
+
+import { __experimentalCreateInterpolateElement as createInterpolateElement } from 'wordpress-element';
+import { Button } from '@wordpress/components';
+
+import connectedImage from '../../assets/images/subscriptions-empty-state-connected.svg';
+import unconnectedImage from '../../assets/images/subscriptions-empty-state-unconnected.svg';
+
+import './style.scss';
+
+const { wcpay } = window;
+
+const renderImage = ( isConnected ) => {
+	if ( isConnected ) {
+		return <img src={ connectedImage } alt="" />;
+	}
+
+	return <img src={ unconnectedImage } alt="" />;
+};
+
+const renderDescription = ( isConnected ) => {
+	let description = '';
+
+	if ( isConnected ) {
+		description = __(
+			'This is where you’ll see and manage all subscriptions in your store. Create a ' +
+				'subscription product to turn one-time purchases into a steady income.',
+			'woocommerce-payments'
+		);
+	} else {
+		description = __(
+			'Track recurring revenue and manage active subscriptions directly from your store’s ' +
+				'dashboard — powered by WooCommerce Payments.',
+			'woocommerce-payments'
+		);
+	}
+
+	return (
+		<p className="wcpay-empty-subscriptions__description">
+			{ description }
+		</p>
+	);
+};
+
+const renderTOS = () => {
+	return (
+		<p className="wcpay-empty-subscriptions__tos">
+			{ createInterpolateElement(
+				__(
+					'By clicking "Finish setup", you agree to the <a>Terms of Service</a>',
+					'woocommerce-payments'
+				),
+				{
+					a: (
+						// eslint-disable-next-line jsx-a11y/anchor-has-content
+						<a
+							href="https://wordpress.com/tos/"
+							target="_blank"
+							rel="noreferrer"
+						/>
+					),
+				}
+			) }
+		</p>
+	);
+};
+
+const renderButtons = ( isConnected ) => {
+	return (
+		<div className="wcpay-empty-subscriptions__button_container">
+			{ isConnected ? (
+				''
+			) : (
+				<Button isPrimary href={ wcpay.connectUrl }>
+					{ __( 'Finish setup', 'woocommerce-payments' ) }
+				</Button>
+			) }
+			<Button isSecondary href={ wcpay.newProductUrl }>
+				{ __( 'Create subscription product', 'woocommerce-payments' ) }
+			</Button>
+		</div>
+	);
+};
+
+ReactDOM.render(
+	<div className="wcpay-empty-subscriptions__container">
+		{ renderImage( wcpay.isConnected ) }
+		{ renderDescription( wcpay.isConnected ) }
+		{ wcpay.isConnected ? '' : renderTOS() }
+		{ renderButtons( wcpay.isConnected ) }
+	</div>,
+	document.querySelector( '#wcpay_subscriptions_empty_state' )
+);
---
+++ includes/subscriptions/class-wc-payments-subscriptions-empty-state-manager.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * Class WC_Payments_Subscriptions_Empty_State.
+ *
+ * @package WooCommerce\Payments
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Class for loading WooCommerce Payments Subscription empty state screen.
+ */
+class WC_Payments_Subscriptions_Empty_State_Manager {
+
+	/**
+	 * WC_Payments_Account instance to get information about the account.
+	 *
+	 * @var WC_Payments_Account
+	 */
+	private $account;
+
+	/**
+	 * WC_Payments_Subscriptions_Empty_State Constructor
+	 *
+	 * @param WC_Payments_Account $account Account class instance.
+	 */
+	public function __construct( WC_Payments_Account $account ) {
+		$this->account = $account;
+
+		add_action( 'admin_enqueue_scripts', [ $this, 'enqueue_scripts_and_styles' ] );
+		add_filter( 'woocommerce_subscriptions_not_found_label', [ $this, 'replace_subscriptions_empty_state' ] );
+	}
+
+	/**
+	 * Enqueues the WCPay Subscription empty state scripts and styles.
+	 */
+	public function enqueue_scripts_and_styles() {
+		$screen = get_current_screen();
+
+		// Only enqueue the scripts on the admin subscriptions screen.
+		if ( ! $screen || 'edit-shop_subscription' !== $screen->id ) {
+			return;
+		}
+
+		$script_src_url    = plugins_url( 'dist/subscriptions-empty-state.js', WCPAY_PLUGIN_FILE );
+		$script_asset_path = WCPAY_ABSPATH . 'dist/subscriptions-empty-state.asset.php';
+		$script_asset      = file_exists( $script_asset_path ) ? require_once $script_asset_path : [ 'dependencies' => [] ];
+		$wcpay_settings    = [
+			'connectUrl'    => WC_Payments_Account::get_connect_url(),
+			'isConnected'   => $this->account->is_stripe_connected(),
+			'newProductUrl' => esc_url( admin_url( 'post-new.php?post_type=product' ) ),
+		];
+
+		wp_register_script(
+			'WCPAY_SUBSCRIPTIONS_EMPTY_STATE',
+			$script_src_url,
+			$script_asset['dependencies'],
+			WC_Payments::get_file_version( 'dist/subscriptions-empty-state.js' ),
+			true
+		);
+
+		wp_localize_script(
+			'WCPAY_SUBSCRIPTIONS_EMPTY_STATE',
+			'wcpay',
+			$wcpay_settings
+		);
+
+		wp_register_style(
+			'WCPAY_SUBSCRIPTIONS_EMPTY_STATE',
+			plugins_url( 'dist/subscriptions-empty-state.css', WCPAY_PLUGIN_FILE ),
+			[],
+			WC_Payments::get_file_version( 'dist/subscriptions-empty-state.css' )
+		);
+
+		wp_enqueue_script( 'WCPAY_SUBSCRIPTIONS_EMPTY_STATE' );
+		wp_enqueue_style( 'WCPAY_SUBSCRIPTIONS_EMPTY_STATE' );
+	}
+
+	/**
+	 * Replaces the default empty subscriptions state HTML with a wrapper for our content to be placed into.
+	 *
+	 * @return string The empty subscriptions sate wrapper.
+	 */
+	public function replace_subscriptions_empty_state() {
+		return '<div id="wcpay_subscriptions_empty_state"></div>';
+	}
+}
