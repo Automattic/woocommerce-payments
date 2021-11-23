94744a10cc5bf29f6611428040a84a56e967919d
Chris Aprea <chris.aprea@automattic.com>
2021-11-23 15:41:20 +1100

    WCPay Subscriptions empty state tweaks (#3414)
    * Tweaks
    
    * Use the lobotomized owl selector to fix margin issue

--- client/subscriptions-empty-state/index.js
+++ client/subscriptions-empty-state/index.js
@@ -3,7 +3,7 @@
  */
 import React from 'react';
 import { __ } from '@wordpress/i18n';
-import ReactDOM from 'react-dom';
+import { render, useState } from '@wordpress/element';
 
 import { __experimentalCreateInterpolateElement as createInterpolateElement } from 'wordpress-element';
 import { Button } from '@wordpress/components';
@@ -13,76 +13,87 @@ import unconnectedImage from '../../assets/images/subscriptions-empty-state-unco
 
 import './style.scss';
 
-const { wcpay } = window;
+const {
+	wcpay: { connectUrl, isConnected, newProductUrl },
+} = window;
 
-const renderImage = ( isConnected ) => {
-	return (
-		<img src={ isConnected ? connectedImage : unconnectedImage } alt="" />
-	);
-};
+const Image = () => (
+	<img src={ isConnected ? connectedImage : unconnectedImage } alt="" />
+);
 
-const renderDescription = ( isConnected ) => {
-	return (
-		<p className="wcpay-empty-subscriptions__description">
-			{ isConnected
-				? __(
-						'This is where you’ll see and manage all subscriptions in your store. Create a ' +
-							'subscription product to turn one-time purchases into a steady income.',
-						'woocommerce-payments'
-				  )
-				: __(
-						'Track recurring revenue and manage active subscriptions directly from your store’s ' +
-							'dashboard — powered by WooCommerce Payments.',
-						'woocommerce-payments'
-				  ) }
-		</p>
-	);
-};
-
-const renderTOS = () => {
-	return (
-		<p className="wcpay-empty-subscriptions__tos">
-			{ createInterpolateElement(
-				__(
-					'By clicking "Finish setup", you agree to the <a>Terms of Service</a>',
+const Description = () => (
+	<p className="wcpay-empty-subscriptions__description">
+		{ isConnected
+			? __(
+					'This is where you’ll see and manage all subscriptions in your store. Create a ' +
+						'subscription product to turn one-time purchases into a steady income.',
 					'woocommerce-payments'
-				),
-				{
-					a: (
-						// eslint-disable-next-line jsx-a11y/anchor-has-content
-						<a
-							href="https://wordpress.com/tos/"
-							target="_blank"
-							rel="noreferrer"
-						/>
-					),
-				}
-			) }
-		</p>
-	);
-};
+			  )
+			: __(
+					'Track recurring revenue and manage active subscriptions directly from your store’s ' +
+						'dashboard — powered by WooCommerce Payments.',
+					'woocommerce-payments'
+			  ) }
+	</p>
+);
 
-const renderButtons = ( isConnected ) => {
+const TOS = () => (
+	<p className="wcpay-empty-subscriptions__tos">
+		{ createInterpolateElement(
+			__(
+				'By clicking "Finish setup", you agree to the <a>Terms of Service</a>',
+				'woocommerce-payments'
+			),
+			{
+				a: (
+					// eslint-disable-next-line jsx-a11y/anchor-has-content
+					<a
+						href="https://wordpress.com/tos/"
+						target="_blank"
+						rel="noreferrer"
+					/>
+				),
+			}
+		) }
+	</p>
+);
+
+const ActionButtons = () => {
+	const [ isFinishingSetup, setIsFinishingSetup ] = useState( false );
+	const [ isCreatingProduct, setIsCreatingProduct ] = useState( false );
+
 	return (
 		<div className="wcpay-empty-subscriptions__button_container">
 			{ ! isConnected && (
-				<Button isPrimary href={ wcpay.connectUrl }>
+				<Button
+					disabled={ isFinishingSetup }
+					href={ connectUrl }
+					isBusy={ isFinishingSetup }
+					isPrimary
+					onClick={ () => setIsFinishingSetup( true ) }
+				>
 					{ __( 'Finish setup', 'woocommerce-payments' ) }
 				</Button>
 			) }
-			<Button isSecondary href={ wcpay.newProductUrl }>
+			<Button
+				disabled={ isCreatingProduct }
+				href={ newProductUrl }
+				isBusy={ isCreatingProduct }
+				isSecondary
+				onClick={ () => setIsCreatingProduct( true ) }
+			>
 				{ __( 'Create subscription product', 'woocommerce-payments' ) }
 			</Button>
 		</div>
 	);
 };
 
-ReactDOM.render(
+render(
 	<div className="wcpay-empty-subscriptions__container">
-		{ renderImage( wcpay.isConnected ) }
-		{ renderDescription( wcpay.isConnected ) }
-		{ ! wcpay.isConnected && renderTOS() }
-		{ renderButtons( wcpay.isConnected ) }
+		<Image />
+		<Description />
+		{ ! isConnected && <TOS /> }
+		<ActionButtons />
 	</div>,
 	document.querySelector( '#wcpay_subscriptions_empty_state' )
 );
