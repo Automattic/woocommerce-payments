77158b1adbb76271afdd9ce6191d870c49d8da1f
Danae Millan <danae.millan@automattic.com>
2021-11-25 16:27:28 -0300

    Adjust the additional-methods-setup test to expect a page and not a task

--- client/additional-methods-setup/test/task.test.js
+++ client/additional-methods-setup/test/task.test.js
@@ -6,7 +6,6 @@
 import { render, screen } from '@testing-library/react';
 import MethodSelector from '../methods-selector';
 import UpePreviewMethodSelector from '../upe-preview-methods-selector';
-import { getPath, updateQueryString } from '@woocommerce/navigation';
 
 /**
  * Internal dependencies
@@ -32,25 +31,16 @@ describe( 'AdditionalMethodsPage', () => {
 		jest.restoreAllMocks();
 	} );
 
-	it( 'returns task', () => {
-		const result = AdditionalMethodsPage( {} );
-
-		expect( result.key ).toEqual(
-			'woocommerce-payments--additional-payment-methods'
-		);
-	} );
-
 	describe( 'if UPE settings preview is disabled', () => {
-		it( 'renders "Set up additional payment methods" task', () => {
-			const result = AdditionalMethodsPage( {
-				isUpeSettingsPreviewEnabled: false,
-			} );
+		it( 'renders "Set up additional payment methods" page', () => {
+			global.wcpaySettings = {
+				additionalMethodsSetup: {
+					isUpeSettingsPreviewEnabled: false,
+					isUpeEnabled: false,
+				},
+			};
 
-			render( result.container );
-
-			expect(
-				screen.queryByText( 'Set up additional payment methods' )
-			).toBeInTheDocument();
+			render( <AdditionalMethodsPage /> );
 
 			expect(
 				screen.queryByText( 'Non-UPE method selector' )
@@ -59,43 +49,19 @@ describe( 'AdditionalMethodsPage', () => {
 	} );
 
 	describe( 'if UPE settings preview is enabled', () => {
-		it( 'renders "Boost your sales by accepting new payment methods" task', () => {
-			const result = AdditionalMethodsPage( {
-				isUpeSettingsPreviewEnabled: true,
-			} );
+		it( 'renders "Boost your sales by accepting new payment methods" page', () => {
+			global.wcpaySettings = {
+				additionalMethodsSetup: {
+					isUpeSettingsPreviewEnabled: true,
+					isUpeEnabled: false,
+				},
+			};
 
-			render( result.container );
-
-			expect(
-				screen.queryByText(
-					'Enable the new WooCommerce Payments checkout experience'
-				)
-			).toBeInTheDocument();
+			render( <AdditionalMethodsPage /> );
 
 			expect(
 				screen.queryByText( 'UPE preview method selector' )
 			).toBeInTheDocument();
 		} );
-	} );
-
-	it( 'adds onClick redirect on WCPay > Overview page', () => {
-		getPath.mockReturnValue( '/payments/overview' );
-
-		const result = AdditionalMethodsPage( {} );
-
-		expect( result ).toHaveProperty( 'onClick' );
-		result.onClick();
-		expect( updateQueryString ).toHaveBeenCalledWith(
-			{ task: 'woocommerce-payments--additional-payment-methods' },
-			''
-		);
-	} );
-
-	it( 'does not add onClick redirect on WC > Home page', () => {
-		getPath.mockReturnValue( '/' );
-
-		const result = AdditionalMethodsPage( {} );
-
-		expect( result ).not.toHaveProperty( 'onClick' );
 	} );
 } );
