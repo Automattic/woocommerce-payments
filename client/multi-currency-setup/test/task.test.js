12372205cffdb76929595feaf5b10913f5941759
Danae Millan <danae.millan@automattic.com>
2021-11-27 11:37:42 -0300

    Remove the tests related to the removed multi-currency task
    The multi-currency setup is now a page, the tests for the task will now fail thus removing it. We can probably remove this set of tests unless we add some others as we aren't really testing much here.

--- client/multi-currency-setup/test/task.test.js
+++ client/multi-currency-setup/test/task.test.js
@@ -5,20 +5,15 @@
  */
 import { render, screen } from '@testing-library/react';
 import MultiCurrencySetup from '../tasks/multi-currency-setup';
-import { getPath, updateQueryString } from '@woocommerce/navigation';
 
 /**
  * Internal dependencies
  */
-import createMultiCurrencySetupTask from '../task';
+import MultiCurrencySetupPage from '../';
 
 jest.mock( '../tasks/multi-currency-setup', () => jest.fn() );
-jest.mock( '@woocommerce/navigation', () => ( {
-	getPath: jest.fn(),
-	updateQueryString: jest.fn(),
-} ) );
 
-describe( 'createMultiCurrencySetupTask()', () => {
+describe( 'MultiCurrencySetupPage()', () => {
 	beforeEach( () => {
 		MultiCurrencySetup.mockReturnValue( <p>Multi-Currency setup task</p> );
 	} );
@@ -27,58 +22,17 @@ describe( 'createMultiCurrencySetupTask()', () => {
 		jest.restoreAllMocks();
 	} );
 
-	it( 'returns task', () => {
-		const result = createMultiCurrencySetupTask( {} );
-
-		expect( result.key ).toEqual(
-			'woocommerce-payments--multi-currency-setup'
-		);
-	} );
-
-	it.each( [ [ 'yes' ], [ 'no' ], [ '' ], [ false ] ] )(
-		'marks task completed if isSetupCompleted equals "yes" (testing: %s)',
-		( isSetupCompleted ) => {
-			const result = createMultiCurrencySetupTask( {
-				isSetupCompleted,
-			} );
-
-			expect( result.completed ).toEqual( 'yes' === isSetupCompleted );
-		}
-	);
-
 	it( 'renders "Sell worldwide in multiple currencies" task', () => {
-		const result = createMultiCurrencySetupTask( {
-			isSetupCompleted: false,
-		} );
+		global.wcpaySettings = {
+			multiCurrencySetup: {
+				isSetupCompleted: true,
+			},
+		};
 
-		render( result.container );
+		render( <MultiCurrencySetupPage /> );
 
-		expect( result.title ).toEqual(
-			'Sell worldwide in multiple currencies'
-		);
 		expect(
 			screen.queryByText( 'Multi-Currency setup task' )
 		).toBeInTheDocument();
-	} );
-
-	it( 'adds onClick redirect on WCPay > Overview page', () => {
-		getPath.mockReturnValue( '/payments/overview' );
-
-		const result = createMultiCurrencySetupTask( {} );
-
-		expect( result ).toHaveProperty( 'onClick' );
-		result.onClick();
-		expect( updateQueryString ).toHaveBeenCalledWith(
-			{ task: 'woocommerce-payments--multi-currency-setup' },
-			''
-		);
-	} );
-
-	it( 'does not add onClick redirect on WC > Home page', () => {
-		getPath.mockReturnValue( '/' );
-
-		const result = createMultiCurrencySetupTask( {} );
-
-		expect( result ).not.toHaveProperty( 'onClick' );
 	} );
 } );
