e1e4be06bde456ef15ff7eb25628937c79814e53
mordeth <cvetan.cvetanov@automattic.com>
2021-11-29 14:00:56 +0200

    Add logo and icon upload settings

--- client/card-readers/settings/sections/branding-details.js
+++ client/card-readers/settings/sections/branding-details.js
@@ -5,6 +5,7 @@
 import React from 'react';
 import { __ } from '@wordpress/i18n';
 import { ColorPaletteControl } from '@wordpress/block-editor';
+import BrandingFileUpload from '../file-upload';
 
 /**
  * Internal dependencies
@@ -12,6 +13,8 @@ import { ColorPaletteControl } from '@wordpress/block-editor';
 import {
 	useAccountBrandingPrimaryColor,
 	useAccountBrandingSecondaryColor,
+	useAccountBrandingLogo,
+	useAccountBrandingIcon,
 } from '../../../data';
 
 const BrandingDetailsSection = () => {
@@ -25,9 +28,48 @@ const BrandingDetailsSection = () => {
 		setAccountBrandingSecondaryColor,
 	] = useAccountBrandingSecondaryColor();
 
+	const [
+		getAccountBrandingLogo,
+		setAccountBrandingLogo,
+	] = useAccountBrandingLogo();
+
+	const [
+		getAccountBrandingIcon,
+		setAccountBrandingIcon,
+	] = useAccountBrandingIcon();
+
 	return (
 		<>
 			<h4>{ __( 'Branding', 'woocommerce-payments' ) }</h4>
+
+			<BrandingFileUpload
+				fieldKey="branding-logo"
+				label={ __( 'Logo', 'woocommerce-payments' ) }
+				accept="image/png, image/jpeg"
+				disabled={ false }
+				help={ __(
+					'Upload a .png or .jpg file.',
+					'woocommerce-payments'
+				) }
+				purpose="business_logo"
+				fileID={ getAccountBrandingLogo }
+				updateFileID={ setAccountBrandingLogo }
+			/>
+
+			<BrandingFileUpload
+				fieldKey="branding-icon"
+				label={ __( 'Icon', 'woocommerce-payments' ) }
+				accept="image/png, image/jpeg"
+				disabled={ false }
+				help={ __(
+					'Upload a .png or .jpg file. For best results upload a 100px x 100px .png file.',
+					'woocommerce-payments'
+				) }
+				purpose="business_icon"
+				value={ getAccountBrandingIcon }
+				updateFileID={ setAccountBrandingIcon }
+			/>
+
 			<ColorPaletteControl
 				onChange={ setAccountBrandingPrimaryColor }
 				disableCustomColors={ false }
