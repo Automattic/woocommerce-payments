c504f03a53053768e191b684a30c453d584aa923
Achyuth Ajoy <achyuthajoy@gmail.com>
2021-11-30 17:52:37 +0530

    Add logic to split tests by group & branch

--- tests/e2e/config/jest.config.js
+++ tests/e2e/config/jest.config.js
@@ -5,13 +5,33 @@ const { jestConfig } = require( '@automattic/puppeteer-utils' );
 config( { path: path.resolve( __dirname, '.env' ) } );
 config( { path: path.resolve( __dirname, 'local.env' ) } );
 
+// Define paths to look for E2E tests.
+const e2ePaths = {
+	wcpay: path.resolve( __dirname, '../specs/wcpay' ),
+	subscriptions: path.resolve( __dirname, '../specs/subscriptions' ),
+	blocks: path.resolve( __dirname, '../specs/blocks' ),
+};
+
+// Allow E2E tests to run specific tests - wcpay, subscriptions, blocks, all (default).
+const allowedPaths = [];
+if ( process.env.E2E_GROUP ) {
+	if ( process.env.E2E_BRANCH ) {
+		allowedPaths.push(
+			e2ePaths[ process.env.E2E_GROUP ] + '/' + process.env.E2E_BRANCH
+		);
+	} else {
+		allowedPaths.push( e2ePaths[ process.env.E2E_GROUP ] );
+	}
+} else {
+	Object.values( e2ePaths ).forEach( ( testPath ) => {
+		allowedPaths.push( testPath );
+	} );
+}
+
 module.exports = {
 	...jestConfig,
 	rootDir: path.resolve( __dirname, '../../../' ),
-	roots: [
-		path.resolve( __dirname, '../specs/merchant' ),
-		path.resolve( __dirname, '../specs/shopper' ),
-	],
+	roots: allowedPaths,
 	setupFilesAfterEnv: [
 		path.resolve( __dirname, '../setup/jest-setup.js' ),
 		...jestConfig.setupFilesAfterEnv,
