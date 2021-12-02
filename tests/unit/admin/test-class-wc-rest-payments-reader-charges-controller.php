8c2dc26c4dd327c535c6021a6b23009c5ca96600
mordeth <cvetan.cvetanov@automattic.com>
2021-12-02 21:16:12 +0200

    Add readers endpoint

--- tests/unit/admin/test-class-wc-rest-payments-reader-charges-controller.php
+++ tests/unit/admin/test-class-wc-rest-payments-reader-charges-controller.php
@@ -7,6 +7,7 @@
 
 use PHPUnit\Framework\MockObject\MockObject;
 use WCPay\Exceptions\Rest_Request_Exception;
+use WC_REST_Payments_Reader_Controller as Controller;
 
 /**
  * WC_REST_Payments_Reader_Controller_Test unit tests.
@@ -29,6 +30,24 @@ class WC_REST_Payments_Reader_Controller_Test extends WP_UnitTestCase {
 
 		$this->mock_api_client = $this->createMock( WC_Payments_API_Client::class );
 		$this->controller      = new WC_REST_Payments_Reader_Controller( $this->mock_api_client );
+		$this->reader          = [
+			'id'          => 'tmr_P400-123-456-789',
+			'device_type' => 'verifone_P400',
+			'label'       => 'Blue Rabbit',
+			'livemode'    => false,
+			'location'    => null,
+			'metadata'    => [],
+			'status'      => 'online',
+			'is_active'   => true,
+		];
+	}
+
+	/**
+	 * Post test cleanup
+	 */
+	public function tearDown() {
+		parent::tearDown();
+		delete_transient( Controller::STORE_READERS_TRANSIENT_KEY );
 	}
 
 	public function test_get_summary_no_transaction() {
@@ -115,5 +134,27 @@ class WC_REST_Payments_Reader_Controller_Test extends WP_UnitTestCase {
 		$request->set_param( 'transaction_id', 1 );
 		$response = $this->controller->get_summary( $request );
 		$this->assertSame( $readers, $response->get_data() );
+	}
+
+	public function test_getting_all_readers_uses_cache_for_existing_readers() {
+		set_transient( Controller::STORE_READERS_TRANSIENT_KEY, [ $this->reader ] );
+
+		$this->mock_api_client
+			->expects( $this->never() )
+			->method( 'get_terminal_readers' );
+
+		$this->mock_api_client
+			->expects( $this->never() )
+			->method( 'get_readers_charge_summary' );
+
+		// Setup the request.
+		$request = new WP_REST_Request(
+			'GET',
+			'/wc/v3/payments/readers'
+		);
+		$request->set_header( 'Content-Type', 'application/json' );
+		$result = $this->controller->get_all_readers( $request );
+
+		$this->assertEquals( [ $this->reader ], $result->get_data() );
 	}
 }
