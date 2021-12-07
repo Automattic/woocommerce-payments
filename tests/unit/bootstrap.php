7a0d31e7c4266f17db17d2ec2c941c2b77b55a55
Vladimir Reznichenko <kalessil@gmail.com>
2021-12-07 10:34:38 +0100

    HACK week: introduce a patch for PHPUnit 7 to further evaluate PHP 8.1 support

--- tests/unit/bootstrap.php
+++ tests/unit/bootstrap.php
@@ -17,11 +17,15 @@ if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
 }
 
 if ( PHP_VERSION_ID >= 80000 && file_exists( $_tests_dir . '/includes/phpunit7/MockObject' ) ) {
-	// WP Core test library includes patches for PHPUnit 7 to make it compatible with PHP8.
+	// WP Core test library includes patches for PHPUnit 7 to make it compatible with PHP8.0.
 	require_once $_tests_dir . '/includes/phpunit7/MockObject/Builder/NamespaceMatch.php';
 	require_once $_tests_dir . '/includes/phpunit7/MockObject/Builder/ParametersMatch.php';
 	require_once $_tests_dir . '/includes/phpunit7/MockObject/InvocationMocker.php';
 	require_once $_tests_dir . '/includes/phpunit7/MockObject/MockMethod.php';
+}
+if ( PHP_VERSION_ID >= 81000 ) {
+	// Our patches for PHPUnit 7 to make it compatible with PHP8.1.
+	require_once $_tests_dir . '/patches/PHPUnit7/Util/Configuration.php';
 }
 
 // Give access to tests_add_filter() function.
