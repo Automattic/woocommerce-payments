8d8a45cfc10d9c3ac13b3bd6f1088496ed0b9be0
Vladimir Reznichenko <kalessil@gmail.com>
2021-12-09 13:59:39 +0100

    HACK week: migrate empty state table to TS

--- client/globals.d.ts
+++ client/globals.d.ts
@@ -1,9 +1,11 @@
 declare const wcpaySettings: {
+	connectUrl: string;
 	isSubscriptionsActive: boolean;
 	featureFlags: {
 		customSearch: boolean;
 	};
 	fraudServices: unknown[];
+	isJetpackConnected: boolean;
 	accountStatus: {
 		status: string;
 	};
