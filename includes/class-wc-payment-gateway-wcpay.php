e09915e595f5e1491f78a2970d801380729a0745
Vladimir Reznichenko <kalessil@gmail.com>
2021-12-09 11:03:24 +0100

    HACK week: add some more types declarations

--- includes/class-wc-payment-gateway-wcpay.php
+++ includes/class-wc-payment-gateway-wcpay.php
@@ -1615,7 +1615,7 @@ class WC_Payment_Gateway_WCPay extends WC_Payment_Gateway_CC {
 	 *
 	 * @return string Statement descriptor of default value.
 	 */
-	protected function get_account_statement_descriptor( string $empty_value = '' ) {
+	protected function get_account_statement_descriptor( string $empty_value = '' ): string {
 		try {
 			if ( $this->is_connected() ) {
 				return $this->account->get_statement_descriptor();
