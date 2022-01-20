45d8853d2c8a45232b931e17b50cca78d39bab99
Mayisha <mayisha007@gmail.com>
2022-01-20 15:25:47 +0600

    load css

--- includes/platform-checkout-user/class-platform-checkout-save-user.php
+++ includes/platform-checkout-user/class-platform-checkout-save-user.php
@@ -30,8 +30,16 @@ class Platform_Checkout_Save_User {
 	 */
 	public function register_checkout_page_scripts() {
 		$script_src_url    = plugins_url( 'dist/platform-checkout.js', WCPAY_PLUGIN_FILE );
+		$style_url         = plugins_url( 'dist/platform-checkout.css', WCPAY_PLUGIN_FILE );
 		$script_asset_path = WCPAY_ABSPATH . 'dist/platform-checkout.asset.php';
 		$script_asset      = file_exists( $script_asset_path ) ? require_once $script_asset_path : [ 'dependencies' => [] ];
+
+		wp_register_style(
+			'WCPAY_PLATFORM_CHECKOUT',
+			$style_url,
+			[],
+			\WC_Payments::get_file_version( 'dist/platform-checkout.css' )
+		);
 
 		wp_register_script(
 			'WCPAY_PLATFORM_CHECKOUT',
@@ -40,6 +48,8 @@ class Platform_Checkout_Save_User {
 			\WC_Payments::get_file_version( 'dist/platform-checkout.js' ),
 			true
 		);
+
+		wp_enqueue_style( 'WCPAY_PLATFORM_CHECKOUT' );
 		wp_enqueue_script( 'WCPAY_PLATFORM_CHECKOUT' );
 	}
 }
