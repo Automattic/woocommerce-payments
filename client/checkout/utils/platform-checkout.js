0f5bc16837d1d6ef8d0dd12dd323f31a4927d127
Mike Moore <michael.moore@automattic.com>
2022-01-22 00:56:22 -0500

    Remove some comments

--- client/checkout/utils/platform-checkout.js
+++ client/checkout/utils/platform-checkout.js
@@ -1,31 +1,3 @@
-// Check whether user is a valid Platform Checkout user
-/* export const handlePlatformCheckoutEmailInput = ( container, field ) => {
-	let timer;
-	const waitTime = 500;
-	const platformCheckoutEmailContainer = document.querySelector( container );
-
-	const platformCheckoutLocateUser = () => {
-		platformCheckoutEmailContainer.classList.add( 'is-loading' );
-
-		// Placeholder to simulate request. Replace with request to Platform Checkout email verification endpoint.
-		setTimeout( () => {
-			platformCheckoutEmailContainer.classList.remove( 'is-loading' );
-		}, 3000 );
-	};
-
-	const platformCheckoutEmailInput = document.querySelector( field );
-
-	platformCheckoutEmailInput.addEventListener( 'input', ( e ) => {
-		const input = e.currentTarget.value;
-
-		clearTimeout( timer );
-		platformCheckoutEmailContainer.classList.remove( 'is-loading' );
-
-		timer = setTimeout( () => {
-			platformCheckoutLocateUser( input );
-		}, waitTime );
-	} );
-}; */
 export const handlePlatformCheckoutEmailInput = ( field ) => {
 	let timer;
 	const waitTime = 500;
