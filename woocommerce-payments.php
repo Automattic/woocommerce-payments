02098e411cf5ee4593acfef4b7367ffcf8c1b4b3
deepakpathania <deepakpathania789@gmail.com>
2022-01-27 14:54:47 +0530

    Bump minimum supported and tested up to version for WordPress

--- woocommerce-payments.php
+++ woocommerce-payments.php
@@ -10,7 +10,7 @@
  * Domain Path: /languages
  * WC requires at least: 4.6
  * WC tested up to: 6.1.1
- * Requires at least: 5.6
+ * Requires at least: 5.7
  * Requires PHP: 7.0
  * Version: 3.6.0
  *
