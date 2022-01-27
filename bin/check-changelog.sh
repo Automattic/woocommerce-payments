6d527fcf502ba0d2932002aa834042d98380b830
Dat Hoang <htdat@users.noreply.github.com>
2022-01-27 15:35:25 +0700

    Update error message.

--- bin/check-changelog.sh
+++ bin/check-changelog.sh
@@ -6,12 +6,13 @@ HEAD=${2-HEAD}
 # Get only added files from git diff.
 CHANGELOG_FILES=$(git diff --name-only --diff-filter=A "$BASE" "$HEAD"  | grep '^changelog\/')
 
-if [[ "$CHANGELOG_FILES" != "" ]]; then
+if [[ -n "$CHANGELOG_FILES" ]]; then
 	echo "Found changelog file(s):"
 	echo "$CHANGELOG_FILES"
 else
-	echo -e "\033[31m ERROR: no changelog found. \033[00m"
-	echo "Add add at least one changelog file for your PR by running: ./vendor/bin/changelogger add"
+	echo ":error::No changelog found."
+	echo "Add at least one changelog file for your PR by running: ./vendor/bin/changelogger add"
+	echo "Choose *patch* to leave it empty if the change is not significant. You can add multiple changelog files in one PR by running this command a few times."
 	exit 1
 fi
 
