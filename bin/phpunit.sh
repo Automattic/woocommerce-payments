9d5f1f01fb7349d95b210117ddc0b83f180b19fe
Zvonimir Maglica <zvonimirmaglica4@gmail.com>
2022-01-28 14:53:32 +0100

    Refactored CI PHPUnit script

--- bin/phpunit.sh
+++ bin/phpunit.sh
@@ -4,4 +4,14 @@ if [[ $RUN_PHPCS == 1 || $SHOULD_DEPLOY == 1 ]]; then
 	exit
 fi
 
-if [ -f "phpunit.phar" ]; then php phpunit.phar -c phpunit.xml.dist $@; else ./vendor/bin/phpunit $@; fi;
+CURRENT_PHP_MAJOR_VERSION=$(php -r 'echo PHP_MAJOR_VERSION;')
+
+if [ $CURRENT_PHP_MAJOR_VERSION -gt 7 ]; then
+	wget -O phpunit https://phar.phpunit.de/phpunit-9.phar
+	chmod +x phpunit
+	phpunit -c phpunit.xml.dist $@
+
+else
+	./vendor/bin/phpunit $@;
+fi
+
