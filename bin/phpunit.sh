3615c28cd8407aa3b07c65e0219b0636ac229f4f
Zvonimir Maglica <zvonimirmaglica4@gmail.com>
2022-01-28 14:57:31 +0100

    Fixed typo in script

--- bin/phpunit.sh
+++ bin/phpunit.sh
@@ -9,7 +9,7 @@ CURRENT_PHP_MAJOR_VERSION=$(php -r 'echo PHP_MAJOR_VERSION;')
 if [ $CURRENT_PHP_MAJOR_VERSION -gt 7 ]; then
 	wget -O phpunit https://phar.phpunit.de/phpunit-9.phar
 	chmod +x phpunit
-	phpunit -c phpunit.xml.dist $@
+	./phpunit -c phpunit.xml.dist $@
 
 else
 	./vendor/bin/phpunit $@;
