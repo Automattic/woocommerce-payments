ae9a42a4dcf2d90241c013dfa708fba9c976cde3
David Stone <david@nnucomputerwhiz.com>
2022-02-03 15:29:36 +0200

    use yado conditions you must

--- client/checkout/utils/platform-checkout.js
+++ client/checkout/utils/platform-checkout.js
@@ -1,8 +1,8 @@
 /**
  * External dependencies
  */
-import {__} from "@wordpress/i18n";
-import { getConfig } from "wcpay/utils/checkout";
+import { __ } from '@wordpress/i18n';
+import { getConfig } from 'wcpay/utils/checkout';
 
 export const handlePlatformCheckoutEmailInput = ( field ) => {
 	let timer;
@@ -12,26 +12,52 @@ export const handlePlatformCheckoutEmailInput = ( field ) => {
 	const parentDiv = platformCheckoutEmailInput.parentNode;
 	spinner.classList.add( 'wc-block-components-spinner' );
 
+	const iframeWrapper = document.createElement( 'div' );
+	iframeWrapper.setAttribute( 'role', 'dialog' );
+	iframeWrapper.setAttribute( 'aria-modal', 'true' );
+	iframeWrapper.classList.add( 'platform-checkout-sms-otp-iframe-wrapper' );
+	const iframe = document.createElement( 'iframe' );
+	iframe.title = __(
+		'Platform checkout SMS code verification',
+		'woocommerce-payments'
+	);
+	iframe.classList.add( 'platform-checkout-sms-otp-iframe' );
+	iframeWrapper.insertBefore( iframe, null );
+
+	const closeIframe = () => {
+		iframeWrapper.remove();
+		platformCheckoutEmailInput.focus();
+	};
+	iframeWrapper.addEventListener( 'click', closeIframe );
+
+	const openIframe = ( email ) => {
+		iframe.src =
+			getConfig( 'platformCheckoutHost' ) + '/sms-otp/?email=' + email;
+		parentDiv.insertBefore( iframeWrapper, null );
+		iframe.focus();
+	};
+
+	document.addEventListener( 'keyup', ( event ) => {
+		const key = event.which || event.keyCode;
+
+		if ( 27 === key && closeIframe() ) {
+			event.stopPropagation();
+		}
+	} );
+
 	const platformCheckoutLocateUser = ( email ) => {
 		parentDiv.insertBefore( spinner, platformCheckoutEmailInput );
 
 		fetch(
-			getConfig( 'platformCheckoutHost' ) + '/wp-json/platform-checkout/v1/user/exists?email='+email)
-			.then(response => response.json())
+			getConfig( 'platformCheckoutHost' ) +
+				'/wp-json/platform-checkout/v1/user/exists?email=' +
+				email
+		)
+			.then( ( response ) => response.json() )
 			.then( ( data ) => {
-				if ( data['user-exists'] ) {
-					const iframeWrapper = document.createElement( 'div' );
-					iframeWrapper.classList.add( 'platform-checkout-sms-otp-iframe-wrapper' );
-					const iframe = document.createElement( 'iframe' );
-					iframe.title = __(
-						'Platform checkout SMS code verification',
-						'woocommerce-payments'
-					);
-					iframe.classList.add( 'platform-checkout-sms-otp-iframe' );
-					iframe.src = getConfig( 'platformCheckoutHost' ) + '/sms-otp/?email='+email;
-					iframeWrapper.insertBefore( iframe, null );
-					parentDiv.insertBefore( iframeWrapper, null );
- 				}
+				if ( data[ 'user-exists' ] ) {
+					openIframe( email );
+				}
 			} )
 			.finally( () => {
 				spinner.remove();
@@ -48,14 +74,14 @@ export const handlePlatformCheckoutEmailInput = ( field ) => {
 	};
 
 	platformCheckoutEmailInput.addEventListener( 'input', ( e ) => {
-		const input = e.currentTarget.value;
+		const email = e.currentTarget.value;
 
 		clearTimeout( timer );
 		spinner.remove();
 
 		timer = setTimeout( () => {
-			if ( validateEmail( input ) ) {
-				platformCheckoutLocateUser( input );
+			if ( validateEmail( email ) ) {
+				platformCheckoutLocateUser( email );
 			}
 		}, waitTime );
 	} );
