9386c57c70f89280af276d166b39b0c48a406534
Brett Shumaker <brettshumaker@gmail.com>
2022-02-03 10:09:47 -0500

    Update iframe header state based on window size.

--- client/checkout/utils/platform-checkout.js
+++ client/checkout/utils/platform-checkout.js
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
+import { debounce } from 'lodash';
 import { getConfig } from 'wcpay/utils/checkout';
 
 export const handlePlatformCheckoutEmailInput = ( field ) => {
@@ -89,4 +90,37 @@ export const handlePlatformCheckoutEmailInput = ( field ) => {
 			}
 		}, waitTime );
 	} );
+
+	// Track the current state of the header. This default
+	// value should match the default state on the platform.
+	let iframeHeaderValue = true;
+	const getWindowSize = () => {
+		if ( 768 >= window.innerWidth && ! iframeHeaderValue ) {
+			iframeHeaderValue = ! iframeHeaderValue;
+			iframe.contentWindow.postMessage(
+				{
+					action: 'setHeader',
+					value: true,
+				},
+				getConfig( 'platformCheckoutHost' )
+			);
+		}
+
+		if ( 768 < window.innerWidth && iframeHeaderValue ) {
+			iframeHeaderValue = ! iframeHeaderValue;
+			iframe.contentWindow.postMessage(
+				{
+					action: 'setHeader',
+					value: false,
+				},
+				getConfig( 'platformCheckoutHost' )
+			);
+		}
+	};
+	// Set the initial value.
+	getWindowSize();
+
+	// Do this on debounced resize.
+	const debouncedGetWindowSize = debounce( getWindowSize, 100 );
+	window.addEventListener( 'resize', debouncedGetWindowSize );
 };
