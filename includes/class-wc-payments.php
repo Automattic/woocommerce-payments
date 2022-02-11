3e1a1000cf7bd0e8adcb691a75f6ee6205b405e9
Daniel Rey López <daniel.rey.lopez@gmail.com>
2022-02-11 12:36:08 +0100

    Add a summary card for the currently active loan (#3750)
    * Add a summary card for the currently active loan
    
    * Added tests
    
    * Lint
    
    * Fix typo (deposists), add typing info to a "catch" block
    
    * Removed TS "unknown" annotation
    
    Co-authored-by: Taha Paksu <3295+tpaksu@users.noreply.github.com>

--- includes/class-wc-payments.php
+++ includes/class-wc-payments.php
@@ -616,6 +616,10 @@ class WC_Payments {
 		$charges_controller = new WC_REST_Payments_Reader_Controller( self::$api_client, self::$card_gateway, self::$in_person_payments_receipts_service );
 		$charges_controller->register_routes();
 
+		include_once WCPAY_ABSPATH . 'includes/admin/class-wc-rest-payments-capital-controller.php';
+		$capital_controller = new WC_REST_Payments_Capital_Controller( self::$api_client );
+		$capital_controller->register_routes();
+
 		if ( WC_Payments_Features::is_upe_settings_preview_enabled() ) {
 			include_once WCPAY_ABSPATH . 'includes/admin/class-wc-rest-upe-flag-toggle-controller.php';
 			$upe_flag_toggle_controller = new WC_REST_UPE_Flag_Toggle_Controller( self::get_gateway() );
