524958e89322f2996d304713348d2d85b01f39d1
Dat Hoang <htdat@users.noreply.github.com>
2022-02-15 14:52:40 +0700

    Remove redundant changelog entries and update GitHub workflow texts. (#3797)

--- bin/check-changelog.sh
+++ bin/check-changelog.sh
@@ -10,9 +10,10 @@ if [[ -n "$CHANGELOG_FILES" ]]; then
 	echo "Found changelog file(s):"
 	echo "$CHANGELOG_FILES"
 else
-	echo ":error::No changelog found."
+	echo "::error::No changelog found."
 	echo "Add at least one changelog file for your PR by running: npm run changelog"
 	echo "Choose *patch* to leave it empty if the change is not significant. You can add multiple changelog files in one PR by running this command a few times."
+	echo "Remove changelog in readme.txt and changelog.txt if you have already added them in your PR."
 	exit 1
 fi
 
