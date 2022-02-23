1e6c3f2d1353aad1b9e7882f6a6f9ff30eefdcbc
Zvonimir Maglica <zvonimirmaglica4@gmail.com>
2022-02-23 13:47:33 +0100

    Fixed shell script versioning

--- bin/phpunit.sh
+++ bin/phpunit.sh
@@ -6,15 +6,19 @@ fi
 
 CURRENT_PHP_MAJOR_VERSION=$(php -r 'echo PHP_MAJOR_VERSION;')
 CURRENT_PHP_MINOR_VERSION=$(php -r 'echo PHP_MINOR_VERSION;')
+echo $CURRENT_PHP_MINOR_VERSION
 # The PHPUnit version inside composer.json is not compatible with PHP 7.2 and above
 # Update this constant if you wish to bump supported PHP major version
 SUPPORTED_PHP_MAJOR_VERSION_FOR_PHPUNIT_INSTALLED_VIA_COMPOSER_JSON=7
 SUPPORTED_PHP_MINOR_VERSION_FOR_PHPUNIT_INSTALLED_VIA_COMPOSER_JSON=3
 
-if [ $CURRENT_PHP_MAJOR_VERSION -gte $SUPPORTED_PHP_MAJOR_VERSION_FOR_PHPUNIT_INSTALLED_VIA_COMPOSER_JSON -a $CURRENT_PHP_MINOR_VERSION -gte $SUPPORTED_PHP_MINOR_VERSION_FOR_PHPUNIT_INSTALLED_VIA_COMPOSER_JSON ]; then
+if [ $CURRENT_PHP_MAJOR_VERSION -gt $SUPPORTED_PHP_MAJOR_VERSION_FOR_PHPUNIT_INSTALLED_VIA_COMPOSER_JSON ]; then
 	./vendor/bin/phpunit $@;
 else
-	chmod +x ./bin/phpunit6
-    ./bin/phpunit6 -c phpunit.xml.dist $@
+	if [ $CURRENT_PHP_MINOR_VERSION -gte $SUPPORTED_PHP_MINOR_VERSION_FOR_PHPUNIT_INSTALLED_VIA_COMPOSER_JSON ]; then
+		./bin/phpunit6 -c phpunit.xml.dist $@
+	else
+    	chmod +x ./bin/phpunit6
+    fi
 fi
 
