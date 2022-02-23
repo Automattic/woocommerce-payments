fb58259800a83043fb7bcd3c0d5a1772cda04a3f
Zvonimir Maglica <zvonimirmaglica4@gmail.com>
2022-02-23 13:29:11 +0100

    Added PHPUnit 9

--- bin/phpunit.sh
+++ bin/phpunit.sh
@@ -5,17 +5,16 @@ if [[ $RUN_PHPCS == 1 || $SHOULD_DEPLOY == 1 ]]; then
 fi
 
 CURRENT_PHP_MAJOR_VERSION=$(php -r 'echo PHP_MAJOR_VERSION;')
-# The PHPUnit version inside composer.json is not compatible with PHP 8
+CURRENT_PHP_MINOR_VERSION=$(php -r 'echo PHP_MINOR_VERSION;')
+# The PHPUnit version inside composer.json is not compatible with PHP 7.2 and above
 # Update this constant if you wish to bump supported PHP major version
-# Consider this bump when PHPUnit inside composer.json is bumped
 SUPPORTED_PHP_MAJOR_VERSION_FOR_PHPUNIT_INSTALLED_VIA_COMPOSER_JSON=7
+SUPPORTED_PHP_MINOR_VERSION_FOR_PHPUNIT_INSTALLED_VIA_COMPOSER_JSON=3
 
-if [ $CURRENT_PHP_MAJOR_VERSION -gt $SUPPORTED_PHP_MAJOR_VERSION_FOR_PHPUNIT_INSTALLED_VIA_COMPOSER_JSON ]; then
-	wget -O phpunit https://phar.phpunit.de/phpunit-9.phar
-	chmod +x phpunit
-	./phpunit -c phpunit.xml.dist $@
-
-else
+if [ $CURRENT_PHP_MAJOR_VERSION -gte $SUPPORTED_PHP_MAJOR_VERSION_FOR_PHPUNIT_INSTALLED_VIA_COMPOSER_JSON -a $CURRENT_PHP_MINOR_VERSION -gte $SUPPORTED_PHP_MINOR_VERSION_FOR_PHPUNIT_INSTALLED_VIA_COMPOSER_JSON ]; then
 	./vendor/bin/phpunit $@;
+else
+	chmod +x phpunit6
+    ./phpunit6 -c phpunit.xml.dist $@
 fi
 
