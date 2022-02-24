eaf5fe22a0b7fa66d03b660cc88223ad916fa6e6
Brett Shumaker <brettshumaker@gmail.com>
2022-02-24 06:31:22 -0500

    Fix platform checkout iframe positioning (#3832)

--- client/checkout/platform-checkout/email-input-iframe.js
+++ client/checkout/platform-checkout/email-input-iframe.js
@@ -2,7 +2,6 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { debounce } from 'lodash';
 import { getConfig } from 'wcpay/utils/checkout';
 
 export const handlePlatformCheckoutEmailInput = ( field, api ) => {
@@ -13,10 +12,13 @@ export const handlePlatformCheckoutEmailInput = ( field, api ) => {
 	const parentDiv = platformCheckoutEmailInput.parentNode;
 	spinner.classList.add( 'wc-block-components-spinner' );
 
+	// Make the iframe wrapper.
 	const iframeWrapper = document.createElement( 'div' );
 	iframeWrapper.setAttribute( 'role', 'dialog' );
 	iframeWrapper.setAttribute( 'aria-modal', 'true' );
 	iframeWrapper.classList.add( 'platform-checkout-sms-otp-iframe-wrapper' );
+
+	// Make the iframe.
 	const iframe = document.createElement( 'iframe' );
 	iframe.title = __(
 		'Platform checkout SMS code verification',
@@ -24,13 +26,23 @@ export const handlePlatformCheckoutEmailInput = ( field, api ) => {
 	);
 	iframe.classList.add( 'platform-checkout-sms-otp-iframe' );
 
+	// Make the iframe arrow.
+	const iframeArrow = document.createElement( 'span' );
+	iframeArrow.setAttribute( 'aria-hidden', 'true' );
+	iframeArrow.classList.add( 'arrow' );
+
+	// Maybe we could make this a configurable option defined in PHP so it could be filtered by merchants.
+	const fullScreenModalBreakpoint = 768;
+
 	// Track the current state of the header. This default
 	// value should match the default state on the platform.
 	let iframeHeaderValue = true;
 	const getWindowSize = () => {
 		if (
-			( 768 <= window.innerWidth && iframeHeaderValue ) ||
-			( 768 > window.innerWidth && ! iframeHeaderValue )
+			( fullScreenModalBreakpoint <= window.innerWidth &&
+				iframeHeaderValue ) ||
+			( fullScreenModalBreakpoint > window.innerWidth &&
+				! iframeHeaderValue )
 		) {
 			iframeHeaderValue = ! iframeHeaderValue;
 			iframe.contentWindow.postMessage(
@@ -41,39 +53,128 @@ export const handlePlatformCheckoutEmailInput = ( field, api ) => {
 				getConfig( 'platformCheckoutHost' )
 			);
 		}
-		if ( iframeHeaderValue ) {
-			document.body.style.overflow = 'hidden';
-			iframe.style.height = '';
-			iframe.style.top = '';
+
+		// Prevent scrolling when the iframe is open.
+		document.body.style.overflow = 'hidden';
+	};
+
+	/**
+	 * Handles setting the iframe popover position based on the input field.
+	 * It tries to be positioned at the right of the input field unless the
+	 * window is too narrow, then it sticks 50px from the right edge of the
+	 * screen.
+	 */
+	const setPopoverPosition = () => {
+		// If for some reason the iframe is not loaded, just return.
+		if ( ! iframe ) {
+			return;
+		}
+
+		// If the window width is less than the breakpoint, reset the styles and return.
+		if ( fullScreenModalBreakpoint >= window.innerWidth ) {
+			iframe.style.left = '0';
+			iframe.style.right = '';
+			return;
+		}
+
+		// Check if the iframe is off the top of the screen and scroll back into view.
+		if ( 0 >= iframe.getBoundingClientRect().top ) {
+			const topOffset = 50;
+			const scrollTop =
+				document.documentElement.scrollTop +
+				platformCheckoutEmailInput.getBoundingClientRect().top -
+				iframe.getBoundingClientRect().height / 2 -
+				topOffset;
+			window.scrollTo( {
+				top: scrollTop,
+			} );
+		}
+
+		// Get references to the iframe and input field bounding rects.
+		const anchorRect = platformCheckoutEmailInput.getBoundingClientRect();
+		const iframeRect = iframe.getBoundingClientRect();
+
+		// Set the iframe top.
+		iframe.style.top =
+			Math.floor( anchorRect.top - iframeRect.height / 2 ) + 'px';
+
+		// Set the arrow top.
+		iframeArrow.style.top =
+			Math.floor(
+				anchorRect.top +
+					anchorRect.height / 2 -
+					parseFloat(
+						window.getComputedStyle( iframeArrow )[
+							'border-right-width'
+						]
+					)
+			) + 'px';
+
+		// Check if the iframe is off the right edge of the screen. If so, stick it to the right edge of the window.
+		if (
+			50 >=
+			window.innerWidth - ( anchorRect.right + iframeRect.width )
+		) {
+			iframe.style.left = 'auto';
+			iframeArrow.style.left = 'auto';
+			iframe.style.right = '50px';
+			iframeArrow.style.right = `${ iframeRect.width + 50 }px`;
 		} else {
-			document.body.style.overflow = '';
+			iframe.style.left = `${ anchorRect.right + 5 }px`;
+			iframe.style.right = '';
+			iframeArrow.style.left = `${ anchorRect.right - 10 }px`;
+			iframeArrow.style.right = '';
 		}
 	};
-	// Do this on debounced resize.
-	const debouncedGetWindowSize = debounce( getWindowSize, 100 );
 
 	iframe.addEventListener( 'load', () => {
 		// Set the initial value.
+		iframeHeaderValue = true;
+
 		getWindowSize();
-		window.addEventListener( 'resize', debouncedGetWindowSize );
+		window.addEventListener( 'resize', getWindowSize );
+
+		setPopoverPosition();
+		window.addEventListener( 'resize', setPopoverPosition );
+
 		iframe.classList.add( 'open' );
 	} );
+
+	// Add the iframe and iframe arrow to the wrapper.
+	iframeWrapper.insertBefore( iframeArrow, null );
 	iframeWrapper.insertBefore( iframe, null );
 
 	const closeIframe = () => {
-		debouncedGetWindowSize.cancel();
-		window.removeEventListener( 'resize', debouncedGetWindowSize );
+		window.removeEventListener( 'resize', getWindowSize );
+		window.removeEventListener( 'resize', setPopoverPosition );
+
 		iframeWrapper.remove();
 		iframe.classList.remove( 'open' );
 		platformCheckoutEmailInput.focus();
+
 		document.body.style.overflow = '';
 	};
+
 	iframeWrapper.addEventListener( 'click', closeIframe );
 
 	const openIframe = ( email ) => {
-		iframe.src =
-			getConfig( 'platformCheckoutHost' ) + '/sms-otp/?email=' + email;
+		const urlParams = new URLSearchParams();
+		urlParams.append( 'email', email );
+		urlParams.append(
+			'needsHeader',
+			fullScreenModalBreakpoint > window.innerWidth
+		);
+
+		iframe.src = `${ getConfig(
+			'platformCheckoutHost'
+		) }/sms-otp/?${ urlParams.toString() }`;
+
+		// Insert the wrapper into the DOM.
 		parentDiv.insertBefore( iframeWrapper, null );
+
+		setPopoverPosition();
+
+		// Focus the iframe.
 		iframe.focus();
 	};
 
@@ -86,10 +187,13 @@ export const handlePlatformCheckoutEmailInput = ( field, api ) => {
 	const platformCheckoutLocateUser = ( email ) => {
 		parentDiv.insertBefore( spinner, platformCheckoutEmailInput );
 
+		const emailParam = new URLSearchParams();
+		emailParam.append( 'email', email );
+
 		fetch(
-			getConfig( 'platformCheckoutHost' ) +
-				'/wp-json/platform-checkout/v1/user/exists?email=' +
-				email
+			`${ getConfig(
+				'platformCheckoutHost'
+			) }/wp-json/platform-checkout/v1/user/exists?${ emailParam.toString() }`
 		)
 			.then( ( response ) => response.json() )
 			.then( ( data ) => {
@@ -150,12 +254,33 @@ export const handlePlatformCheckoutEmailInput = ( field, api ) => {
 				closeIframe();
 				break;
 			case 'iframe_height':
-				if ( 768 < window.innerWidth ) {
-					iframe.style.height = e.data.height + 'px';
-					iframe.style.top = Math.floor( e.data.height / -2 ) + 'px';
-				} else {
-					iframe.style.height = '';
-					iframe.style.top = '';
+				if ( 300 < e.data.height ) {
+					if ( fullScreenModalBreakpoint <= window.innerWidth ) {
+						// attach iframe to right side of platformCheckoutEmailInput.
+
+						iframe.style.height = e.data.height + 'px';
+
+						const inputRect = platformCheckoutEmailInput.getBoundingClientRect();
+
+						// iframe top is the input top minus the iframe height.
+						iframe.style.top =
+							Math.floor( inputRect.top - e.data.height / 2 ) +
+							'px';
+						// Arrow top is the input top plus half the input height minus the border width.
+						iframeArrow.style.top =
+							Math.floor(
+								inputRect.top +
+									inputRect.height / 2 -
+									parseFloat(
+										window.getComputedStyle( iframeArrow )[
+											'border-right-width'
+										]
+									)
+							) + 'px';
+					} else {
+						iframe.style.height = '';
+						iframe.style.top = '';
+					}
 				}
 				break;
 			default:
