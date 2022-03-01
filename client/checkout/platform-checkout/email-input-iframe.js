ef58ec7e5639e2283ad249faf0be2c89e7b3c4ea
Brett Shumaker <brettshumaker@gmail.com>
2022-03-01 07:33:58 -0500

    Update email validation that we use before checking if the email exists in platform checkout.
    Checks that the email address has at least a 2 character TLD. 1 character TLDs are technically valid but aren't used in practice. They also fail `is_email()` from WordPress which is what we use to validate emails on the platform checkout.
    
    Also needed to disable eslint with the `/* eslint-disable */` because the linter was trying to format the line to fit in max length (even with `// eslint-disable-line max-len`) and I couldn't commit the change. If someone knows how I can better solve this, let me know.

--- client/checkout/platform-checkout/email-input-iframe.js
+++ client/checkout/platform-checkout/email-input-iframe.js
@@ -224,12 +224,13 @@ export const handlePlatformCheckoutEmailInput = ( field, api ) => {
 	};
 
 	const validateEmail = ( value ) => {
-		const input = document.createElement( 'input' );
-		input.type = 'email';
-		input.required = true;
-		input.value = value;
-
-		return input.checkValidity() || false;
+		/* Borrowed from WooCommerce checkout.js with a slight tweak to add `{2,}` to the end and make the TLD at least 2 characters. */
+		/* eslint-disable */
+		const pattern = new RegExp(
+			/^([a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+(\.[a-z\d!#$%&'*+\-\/=?^_`{|}~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]+)*|"((([ \t]*\r\n)?[ \t]+)?([\x01-\x08\x0b\x0c\x0e-\x1f\x7f\x21\x23-\x5b\x5d-\x7e\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|\\[\x01-\x09\x0b\x0c\x0d-\x7f\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))*(([ \t]*\r\n)?[ \t]+)?")@(([a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[a-z\d\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])\.)+([a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]|[a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF][a-z\d\-._~\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]*[0-9a-z\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]){2,}\.?$/i
+		);
+		/* eslint-enable */
+		return pattern.test( value );
 	};
 
 	platformCheckoutEmailInput.addEventListener( 'input', ( e ) => {
