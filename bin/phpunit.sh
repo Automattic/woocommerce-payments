9243a32f4b77171cae97e6b584c6599fd3e0b003
Zvonimir Maglica <zvonimirmaglica4@gmail.com>
2022-03-01 09:16:33 +0100

    Code review fixes

--- bin/phpunit.sh
+++ bin/phpunit.sh
@@ -13,12 +13,13 @@ SUPPORTED_PHP_MAJOR_VERSION_FOR_PHPUNIT_INSTALLED_VIA_COMPOSER_JSON=7
 SUPPORTED_PHP_MINOR_VERSION_FOR_PHPUNIT_INSTALLED_VIA_COMPOSER_JSON=3
 
 if [ $CURRENT_PHP_MAJOR_VERSION -gt $SUPPORTED_PHP_MAJOR_VERSION_FOR_PHPUNIT_INSTALLED_VIA_COMPOSER_JSON ]; then
-	./vendor/bin/phpunit $@;
+	./vendor/bin/phpunit -c phpunit.xml.dist "$@";
 else
 	if [ $CURRENT_PHP_MINOR_VERSION -ge $SUPPORTED_PHP_MINOR_VERSION_FOR_PHPUNIT_INSTALLED_VIA_COMPOSER_JSON ]; then
-		./bin/phpunit6 -c phpunit.xml.dist $@
+		./vendor/bin/phpunit -c phpunit.xml.dist "$@";
 	else
     	chmod +x ./bin/phpunit6
+    	./bin/phpunit6 -c phpunit.xml.dist "$@";
     fi
 fi
 
