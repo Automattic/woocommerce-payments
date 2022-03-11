c5d26238e9905a58c83c5ec36c4998d9ef77dc8f
Waclaw Jacek <waclaw.jacek@automattic.com>
2022-03-11 19:02:33 +0100

    Add notices when redirecting to platform checkout is in progress/fails

--- client/components/platform-checkout/index.tsx
+++ client/components/platform-checkout/index.tsx
@@ -4,15 +4,18 @@
 import React from 'react';
 import { useState } from '@wordpress/element';
 import { __ } from '@wordpress/i18n';
+import { Notice } from '@wordpress/components';
 import interpolateComponents from 'interpolate-components';
 
 /**
  * Internal dependencies
  */
 import { getConfig } from 'utils/checkout';
+import './style.scss';
 
 interface apiResponse {
 	url: Location;
+	result?: string;
 }
 
 interface wcpayApi {
@@ -24,8 +27,39 @@ interface platformCheckoutButtonProps {
 	api: wcpayApi;
 }
 
+const SuccessNotice = () => {
+	return (
+		<Notice
+			status="success"
+			isDismissible={ false }
+			className="init-platform-checkout-notice"
+		>
+			{ __(
+				'Redirecting to platform checkout. Please wait…',
+				'woocommerce-payments'
+			) }
+		</Notice>
+	);
+};
+
+const ErrorNotice = () => {
+	return (
+		<Notice
+			status="error"
+			isDismissible={ false }
+			className="init-platform-checkout-notice"
+		>
+			{ __(
+				'An error occurred while redirecting to platform checkout. Please try again.',
+				'woocommerce-payments'
+			) }
+		</Notice>
+	);
+};
+
 const PlatformCheckout = ( { isStatic, api }: platformCheckoutButtonProps ) => {
 	const [ isLoading, setIsLoading ] = useState( false );
+	const [ responseStatus, setResponseStatus ] = useState( '' );
 
 	const buttonContent = (
 		<span>
@@ -46,13 +80,20 @@ const PlatformCheckout = ( { isStatic, api }: platformCheckoutButtonProps ) => {
 	const onClick = () => {
 		setIsLoading( true );
 		api.initPlatformCheckout().then( ( response ) => {
-			window.location = response.url;
+			if ( response?.result === 'success' ) {
+				window.location = response.url;
+				setResponseStatus( 'success' );
+			} else {
+				setResponseStatus( 'error' );
+			}
 			setIsLoading( false );
 		} );
 	};
 
 	return (
 		<>
+			{ responseStatus === 'success' && <SuccessNotice /> }
+			{ responseStatus === 'error' && <ErrorNotice /> }
 			<button
 				onClick={ onClick }
 				type="button"
