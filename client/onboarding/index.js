bf78c0e56f9fff575dcc0295b4d027420f39d525
Daniel Mallory <daniel.mallory@automattic.com>
2022-03-11 15:29:17 +0000

    Adding onboarding page

---
+++ client/onboarding/index.js
@@ -0,0 +1,20 @@
+/** @format */
+/**
+ * External dependencies
+ */
+import React from 'react';
+
+/**
+ * Internal dependencies
+ */
+import strings from './strings';
+
+const OnboardingPage = () => {
+	return (
+		<>
+			<h1>{ strings.onboarding.heading }</h1>
+		</>
+	);
+};
+
+export default OnboardingPage;
---
+++ client/onboarding/strings.js
@@ -0,0 +1,17 @@
+/* eslint-disable max-len */
+/**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+export default {
+	button: __( 'Finish setup', 'woocommerce-payments' ),
+
+	heading: __( 'WooCommerce Payments', 'woocommerce-payments' ),
+
+	learnMore: __( 'Learn more', 'woocommerce-payments' ),
+
+	onboarding: {
+		heading: __( 'Hello from the onboarding page', 'woocommerce-payments' ),
+	},
+};
