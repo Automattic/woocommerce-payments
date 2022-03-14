6b31603ed382ec3dc58de3de69d0697e04e15c11
Daniel Mallory <daniel.mallory@automattic.com>
2022-03-14 13:27:17 +0000

    Adding webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -31,6 +31,7 @@ const webpackConfig = {
 		'multi-currency-analytics':
 			'./client/multi-currency-analytics/index.js',
 		order: './client/order/index.js',
+		onboarding: './client/onboarding/index.js',
 		'subscriptions-empty-state':
 			'./client/subscriptions-empty-state/index.js',
 		'subscription-product-onboarding-modal':
