2664a43b2f5195dbf20210eb2af4de348a3b352d
Daniel Mallory <daniel.mallory@automattic.com>
2022-03-25 13:36:02 +0000

    Update to the tests for the add business info task

--- client/onboarding/tasks/add-business-info-task/test/index.js
+++ client/onboarding/tasks/add-business-info-task/test/index.js
@@ -3,79 +3,168 @@
 /**
  * External dependencies
  */
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import user from '@testing-library/user-event';
 import React from 'react';
 
 /**
  * Internal dependencies
  */
 import AddBusinessInfoTask from '../';
-import { useBusinessTypes } from '../../../../data';
+import { useBusinessTypes } from 'wcpay/data/onboarding';
 
-jest.mock( '../../../../data', () => ( {
+jest.mock( 'wcpay/data/onboarding', () => ( {
 	useBusinessTypes: jest.fn(),
 } ) );
 
+const businessTypes = [
+	{
+		key: 'US',
+		name: 'United States',
+		types: [
+			{
+				key: 'individual',
+				name: 'Individual',
+				structures: [],
+			},
+			{
+				key: 'company',
+				name: 'Company',
+				structures: [
+					{
+						key: 'sole_propietorship',
+						name: 'Sole propietorship',
+					},
+					{
+						key: 'single_member_llc',
+						name: 'Single member llc',
+					},
+				],
+			},
+		],
+	},
+	{
+		key: 'FR',
+		name: 'France',
+		types: [
+			{
+				key: 'individual',
+				name: 'Individual',
+				structures: [],
+			},
+			{
+				key: 'company',
+				name: 'Company',
+				structures: [],
+			},
+			{
+				key: 'non_profit',
+				name: 'Non-profit',
+				structures: [],
+			},
+		],
+	},
+];
+
 describe( 'AddBusinessInfoTask', () => {
 	beforeEach( () => {
+		jest.clearAllMocks();
 		global.wcpaySettings = {
 			connect: {
 				country: 'US',
 			},
 		};
-
-		useBusinessTypes.mockReturnValue( [
-			{
-				key: 'US',
-				name: 'United States',
-				types: [
-					{
-						key: 'individual',
-						name: 'Individual',
-						structures: [],
-					},
-					{
-						key: 'company',
-						name: 'Company',
-						structures: [
-							{
-								key: 'sole_propietorship',
-								name: 'Sole propietorship',
-							},
-							{
-								key: 'single_member_llc',
-								name: 'Single member llc',
-							},
-						],
-					},
-				],
-			},
-			{
-				key: 'FR',
-				name: 'France',
-				types: [
-					{
-						key: 'individual',
-						name: 'Individual',
-						structures: [],
-					},
-					{
-						key: 'company',
-						name: 'Company',
-						structures: [],
-					},
-					{
-						key: 'non_profit',
-						name: 'Non-profit',
-						structures: [],
-					},
-				],
-			},
-		] );
 	} );
 
-	it( 'renders page', () => {
+	it( 'shows a loadable block', () => {
+		useBusinessTypes.mockReturnValue( {
+			businessTypes: [],
+			isLoading: true,
+		} );
+
 		const { container: task } = render( <AddBusinessInfoTask /> );
 		expect( task ).toMatchSnapshot();
+	} );
+
+	it( 'shows the form', () => {
+		useBusinessTypes.mockReturnValue( {
+			businessTypes,
+			isLoading: false,
+		} );
+
+		const { container: task } = render( <AddBusinessInfoTask /> );
+		expect( task ).toMatchSnapshot();
+	} );
+
+	it( 'shows business type and structure from a selected country', () => {
+		global.wcpaySettings = {
+			connect: {
+				country: 'FR',
+			},
+		};
+		useBusinessTypes.mockReturnValue( {
+			businessTypes,
+			isLoading: false,
+		} );
+
+		const { container: task } = render( <AddBusinessInfoTask /> );
+
+		user.click( screen.getByRole( 'button', { name: /country/i } ) );
+		user.click(
+			screen.getByRole( 'option', {
+				name: /united states/i,
+			} )
+		);
+
+		user.click( screen.getByRole( 'button', { name: /business type/i } ) );
+		user.click(
+			screen.getByRole( 'option', {
+				name: /company/i,
+			} )
+		);
+
+		user.click(
+			screen.getByRole( 'button', { name: /business structure/i } )
+		);
+		user.click(
+			screen.getByRole( 'option', {
+				name: /single member llc/i,
+			} )
+		);
+
+		expect( task ).toMatchSnapshot();
+	} );
+
+	it( 'hides the structure when no structures are available', () => {
+		global.wcpaySettings = {
+			connect: {
+				country: 'FR',
+			},
+		};
+		useBusinessTypes.mockReturnValue( {
+			businessTypes,
+			isLoading: false,
+		} );
+
+		const { container: task } = render( <AddBusinessInfoTask /> );
+
+		user.click( screen.getByRole( 'button', { name: /country/i } ) );
+		user.click(
+			screen.getByRole( 'option', {
+				name: /united states/i,
+			} )
+		);
+
+		user.click( screen.getByRole( 'button', { name: /business type/i } ) );
+		user.click(
+			screen.getByRole( 'option', {
+				name: /individual/i,
+			} )
+		);
+
+		expect( task ).toMatchSnapshot();
+		expect(
+			screen.queryByRole( 'button', { name: /business structure/i } )
+		).not.toBeInTheDocument();
 	} );
 } );
