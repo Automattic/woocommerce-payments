9eec216856212f4dbce595eae0c10a99adc97274
Achyuth Ajoy <achyuthajoy@gmail.com>
2022-03-29 12:13:35 +0530

    PSALM lint fixes

--- includes/class-wc-payments-utils.php
+++ includes/class-wc-payments-utils.php
@@ -625,7 +625,7 @@ class WC_Payments_Utils {
 	 *
 	 * @param string $order_id WC Order ID.
 	 *
-	 * @return bool|WC_Order_Refund
+	 * @return bool|WC_Order|WC_Order_Refund
 	 */
 	public static function get_last_refund_from_order_id( $order_id ) {
 		$wc_refunds = wc_get_orders(
