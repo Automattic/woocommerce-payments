397538898c47dc8b38892ad6a0d09988e16bf6ed
Ahmed <ahmed.el.azzabi@automattic.com>
2022-04-01 12:33:00 +0000

    Transform Buyer_Fingerprinting_Service to a singleton

--- includes/fraud-prevention/class-buyer-fingerprinting-service.php
+++ includes/fraud-prevention/class-buyer-fingerprinting-service.php
@@ -12,23 +12,45 @@ namespace WCPay\Fraud_Prevention;
  */
 class Buyer_Fingerprinting_Service {
 	/**
+	 * Fraud prevention service instance.
+	 *
+	 * @var Fraud_Prevention_Service
+	 */
+	private $fraud_prevention_service;
+
+
+	/**
+	 * Buyer_Fingerprinting_Service constructor.
+	 *
+	 * @param Fraud_Prevention_Service $fraud_prevention_service Fraud Prevention instance.
+	 */
+	public function __construct( Fraud_Prevention_Service $fraud_prevention_service ) {
+		$this->fraud_prevention_service = $fraud_prevention_service;
+	}
+
+	/**
+	 * Returns singleton instance.
+	 *
+	 * @param null $fraud_prevention_service Fraud Prevention Service instance.
+	 * @return Buyer_Fingerprinting_Service
+	 */
+	public static function get_instance( $fraud_prevention_service = null ): self {
+		if ( null === self::$fraud_prevention_service ) {
+			self::$fraud_prevention_service = new self( $fraud_prevention_service ?? Fraud_Prevention_Service::get_instance() );
+		}
+
+		return self::$fraud_prevention_service;
+	}
+
+	/**
 	 * Hashes customer data for the fraud prevention.
 	 *
 	 * @param string $data The data you want to hash.
 	 *
 	 * @return string Hashed data.
 	 */
-	public static function hash_data_for_fraud_prevention( $data ) {
+	public function hash_data_for_fraud_prevention( $data ) {
 		return hash( 'sha512', $data );
-	}
-
-	/**
-	 * Checks if fraud prevention is enabled for this store.
-	 *
-	 * @return bool True if store has fraud prevention enabled.
-	 */
-	public static function is_fraud_prevention_enabled_for_store() {
-		return Fraud_Prevention_Service::get_instance()->is_enabled();
 	}
 
 	/**
@@ -36,17 +58,14 @@ class Buyer_Fingerprinting_Service {
 	 *
 	 * @param string $order_id The WC order id.
 	 *
-	 * @return [string] An array of hashed data for an order.
+	 * @return string[] An array of hashed data for an order.
 	 */
-	public static function get_hashed_data_for_order( $order_id ) {
-		$order        = wc_get_order( $order_id );
-		$email_domain = explode( '@', $order->get_billing_email(), 2 )[1];
+	public function get_hashed_data_for_order( $order_id ) {
+		$order = wc_get_order( $order_id );
 
 		return [
 			'shopper_ip_hash'        => self::hash_data_for_fraud_prevention( $order->get_customer_ip_address() ),
 			'shopper_useragent_hash' => self::hash_data_for_fraud_prevention( strtolower( $order->get_customer_user_agent() ) ),
-			'shopper_email_hash'     => self::hash_data_for_fraud_prevention( strtolower( $order->get_billing_email() ) ),
-			'shopper_email_domain'   => self::hash_data_for_fraud_prevention( strtolower( $email_domain ) ),
 		];
 	}
 }
