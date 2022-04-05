8ff6ba97e2cc006684775da76599718bab2c7d3a
Ricardo Metring <ricardometring@gmail.com>
2022-04-05 12:22:03 -0300

    Use `should_show_payment_request_button` for `set_session` and clarify purpose with additional comment

--- includes/class-wc-payments-payment-request-button-handler.php
+++ includes/class-wc-payments-payment-request-button-handler.php
@@ -150,8 +150,10 @@ class WC_Payments_Payment_Request_Button_Handler {
 	 * @return void
 	 */
 	public function set_session() {
+		// Don't set session cookies on product pages to allow for caching when payment request
+		// buttons are disabled. But keep cookies if there is already an active WC session in place.
 		if (
-			! ( $this->is_product() && $this->is_available_at( 'product' ) )
+			! ( $this->is_product() && $this->should_show_payment_request_button() )
 			|| ( isset( WC()->session ) && WC()->session->has_session() )
 		) {
 			return;
