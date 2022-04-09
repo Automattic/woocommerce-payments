d13a8a7927e984197dc4b6cfb678539d17a6eda6
Kristófer R <kristofer.thorlaksson@automattic.com>
2022-04-09 01:24:55 -0400

    Add tests for nonce checks when not in dev mode

--- tests/unit/test-class-wc-payments.php
+++ tests/unit/test-class-wc-payments.php
@@ -42,10 +42,30 @@ class WC_Payments_Test extends WP_UnitTestCase {
 	}
 
 	public function test_it_registers_platform_checkout_hooks_if_feature_flag_is_enabled() {
+		// Enable dev mode so nonce check is disabled.
+		add_filter(
+			'wcpay_dev_mode',
+			function () {
+				return true;
+			}
+		);
+
 		$this->set_platform_checkout_enabled( true );
 
 		foreach ( self::EXPECTED_PLATFORM_CHECKOUT_HOOKS as $hook => $callback ) {
 			$this->assertEquals( 10, has_filter( $hook, $callback ) );
+		}
+	}
+
+	public function test_it_registers_platform_checkout_hooks_except_nonce_check_if_feature_flag_is_enabled_but_not_in_dev_mode() {
+		$this->set_platform_checkout_enabled( true );
+
+		foreach ( self::EXPECTED_PLATFORM_CHECKOUT_HOOKS as $hook => $callback ) {
+			if ( 'woocommerce_store_api_disable_nonce_check' === $hook ) {
+				$this->assertFalse( has_filter( $hook, $callback ) );
+			} else {
+				$this->assertEquals( 10, has_filter( $hook, $callback ) );
+			}
 		}
 	}
 
@@ -68,7 +88,25 @@ class WC_Payments_Test extends WP_UnitTestCase {
 		$this->assertEquals( 'woocommerce_rest_missing_nonce', $response->get_data()['code'] );
 	}
 
+	public function test_rest_endpoints_validate_nonce_if_platform_checkout_feature_flag_is_enabled_but_not_in_dev_mode() {
+		$this->set_platform_checkout_feature_flag_enabled( true );
+		$request = new WP_REST_Request( 'GET', '/wc/store/checkout' );
+
+		$response = rest_do_request( $request );
+
+		$this->assertEquals( 401, $response->get_status() );
+		$this->assertEquals( 'woocommerce_rest_missing_nonce', $response->get_data()['code'] );
+	}
+
 	public function test_rest_endpoints_do_not_validate_nonce_if_platform_checkout_feature_flag_is_enabled() {
+		// Enable dev mode so nonce check is disabled.
+		add_filter(
+			'wcpay_dev_mode',
+			function () {
+				return true;
+			}
+		);
+
 		$this->set_platform_checkout_feature_flag_enabled( true );
 
 		$request = new WP_REST_Request( 'GET', '/wc/store/checkout' );
@@ -90,7 +128,26 @@ class WC_Payments_Test extends WP_UnitTestCase {
 		$this->assertEquals( 'woocommerce_rest_missing_nonce', $response->get_data()['code'] );
 	}
 
+	public function test_rest_endpoints_validate_nonce_if_platform_checkout_is_enabled_but_not_in_dev_mode() {
+		$this->set_platform_checkout_enabled( true );
+
+		$request = new WP_REST_Request( 'GET', '/wc/store/checkout' );
+
+		$response = rest_do_request( $request );
+
+		$this->assertEquals( 401, $response->get_status() );
+		$this->assertEquals( 'woocommerce_rest_missing_nonce', $response->get_data()['code'] );
+	}
+
 	public function test_rest_endpoints_do_not_validate_nonce_if_platform_checkout_is_enabled() {
+		// Enable dev mode so nonce check is disabled.
+		add_filter(
+			'wcpay_dev_mode',
+			function () {
+				return true;
+			}
+		);
+
 		$this->set_platform_checkout_enabled( true );
 
 		$request = new WP_REST_Request( 'GET', '/wc/store/checkout' );
@@ -110,23 +167,13 @@ class WC_Payments_Test extends WP_UnitTestCase {
 			remove_filter( $hook, $callback );
 		}
 
-		if ( $is_enabled ) {
-			// Make sure dev mode is on so the right filters get enabled.
-			add_filter(
-				'wcpay_dev_mode',
-				function () {
-					return true;
-				}
-			);
-		}
-
 		add_option( WC_Payments_Account::ACCOUNT_OPTION, [ 'account' => [ 'platform_checkout_eligible' => $is_enabled ] ] );
 		// Testing feature flag, so platform_checkout setting should always be on.
 		WC_Payments::get_gateway()->update_option( 'platform_checkout', 'yes' );
 
 		WC_Payments::maybe_register_platform_checkout_hooks();
 
-		// Trigger the addition of the disable nonce filter.
+		// Trigger the addition of the disable nonce filter when appropriate.
 		apply_filters( 'rest_request_before_callbacks', [], [], null );
 	}
 
@@ -136,23 +183,13 @@ class WC_Payments_Test extends WP_UnitTestCase {
 			remove_filter( $hook, $callback );
 		}
 
-		if ( $is_enabled ) {
-			// Make sure dev mode is on so the right filters get enabled.
-			add_filter(
-				'wcpay_dev_mode',
-				function () {
-					return true;
-				}
-			);
-		}
-
 		// Testing platform_checkout, so feature flag should always be on.
 		add_option( WC_Payments_Account::ACCOUNT_OPTION, [ 'account' => [ 'platform_checkout_eligible' => true ] ] );
 		WC_Payments::get_gateway()->update_option( 'platform_checkout', $is_enabled ? 'yes' : 'no' );
 
 		WC_Payments::maybe_register_platform_checkout_hooks();
 
-		// Trigger the addition of the disable nonce filter.
+		// Trigger the addition of the disable nonce filter when appropriate.
 		apply_filters( 'rest_request_before_callbacks', [], [], null );
 	}
 }
