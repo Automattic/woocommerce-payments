e9eaf3592d2ce7e3ea46a02d31cf20dbad1db3a3
Jessy P <jessy.pappachan@automattic.com>
2022-04-13 12:04:49 +0530

    If _charge_id does not exist, update it. This happens on some sites where maybe_process_upe_redirect does not run, but payment has completed correctly.

--- includes/class-wc-payments-webhook-processing-service.php
+++ includes/class-wc-payments-webhook-processing-service.php
@@ -330,6 +330,11 @@ class WC_Payments_Webhook_Processing_Service {
 		$charges_data  = $this->read_webhook_property( $event_charges, 'data' );
 		$charge_id     = $this->read_webhook_property( $charges_data[0], 'id' );
 
+		// update _charge_id meta if it doesn't exist - happens when maybe_process_upe_redirect fails sometimes.
+		if ( ! $order->get_meta( '_charge_id' ) ) {
+			$order->update_meta_data( '_charge_id', $charge_id );
+		}
+
 		$this->order_service->mark_payment_completed( $order, $intent_id, $intent_status, $charge_id );
 	}
 
