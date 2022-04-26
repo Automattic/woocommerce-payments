dfa6c4813a517f2211f8628331943990cf5c8b86
Achyuth Ajoy <achyuthajoy@gmail.com>
2022-04-26 11:10:04 +0530

    Add additional param to saveUPEAppearance

--- client/checkout/api/index.js
+++ client/checkout/api/index.js
@@ -442,12 +442,18 @@ export default class WCPayAPI {
 	 * Saves the calculated UPE appearance values in a transient.
 	 *
 	 * @param {Object} appearance The UPE appearance object with style values
+	 * @param {boolean} isBlocksCheckout True if save request is for Blocks Checkout. Default false.
 	 *
 	 * @return {Promise} The final promise for the request to the server.
 	 */
-	saveUPEAppearance( appearance ) {
-		return this.request( getConfig( 'ajaxUrl' ), {
+	saveUPEAppearance( appearance, isBlocksCheckout = false ) {
+		const appearanceData = {
+			is_blocks_checkout: isBlocksCheckout,
 			appearance,
+		};
+
+		return this.request( getConfig( 'ajaxUrl' ), {
+			appearanceData,
 			action: 'save_upe_appearance',
 			// eslint-disable-next-line camelcase
 			_ajax_nonce: getConfig( 'saveUPEAppearanceNonce' ),
