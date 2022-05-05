24d6c96d036a4fc31581ad2df9739986ed727e55
Shendy Kurnia <shendy.kurnia@a8c.com>
2022-05-05 18:12:13 +0700

    Bump `WC tested up to` to `6.4.1` and `Tested up to` to `5.9.3`.

--- woocommerce-payments.php
+++ woocommerce-payments.php
@@ -9,7 +9,7 @@
  * Text Domain: woocommerce-payments
  * Domain Path: /languages
  * WC requires at least: 5.2
- * WC tested up to: 6.3.1
+ * WC tested up to: 6.4.1
  * Requires at least: 5.7
  * Requires PHP: 7.0
  * Version: 4.0.2
