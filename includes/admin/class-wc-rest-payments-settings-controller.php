ec2a2ef21617fd722c94e8a6230f85f4144be6f6
Dan Paun <dan.paun@automattic.com>
2022-05-09 15:15:09 +0300

    remove backend validation

--- includes/admin/class-wc-rest-payments-settings-controller.php
+++ includes/admin/class-wc-rest-payments-settings-controller.php
@@ -25,12 +25,6 @@ class WC_REST_Payments_Settings_Controller extends WC_Payments_REST_Controller {
 		'account_branding_secondary_color',
 	];
 
-
-	const ACCOUNT_FIELDS_CANNOT_BE_EMPTY = [
-		'account_business_support_email',
-		'account_business_support_phone',
-	];
-
 	/**
 	 * Endpoint path.
 	 *
@@ -556,11 +550,8 @@ class WC_REST_Payments_Settings_Controller extends WC_Payments_REST_Controller {
 	 */
 	private function update_account( WP_REST_Request $request ) {
 		$updated_fields_callback = function ( $value, string $key ) {
-			if ( ! ( in_array( $key, static::ACCOUNT_FIELDS_TO_UPDATE, true ) &&
-				$this->wcpay_gateway->get_option( $key ) !== $value ) ) {
-				return false;
-			}
-			return ! in_array( $key, static::ACCOUNT_FIELDS_CANNOT_BE_EMPTY, true ) || ! empty( $value );
+			return in_array( $key, static::ACCOUNT_FIELDS_TO_UPDATE, true ) &&
+				$this->wcpay_gateway->get_option( $key ) !== $value;
 		};
 		$updated_fields          = array_filter( $request->get_params(), $updated_fields_callback, ARRAY_FILTER_USE_BOTH );
 
