96bfe5fc8a7f94785448268f27c91c2da3c5428f
Malith Senaweera <6216000+malithsen@users.noreply.github.com>
2022-05-12 12:36:20 -0500

    Update KYC tracks properties (#4249)
    * Update KYC tracks properties

--- includes/class-wc-payments-account.php
+++ includes/class-wc-payments-account.php
@@ -529,9 +529,22 @@ class WC_Payments_Account {
 		$redirect_param = sanitize_text_field( wp_unslash( $_GET['wcpay-connect-redirect'] ) );
 
 		// Let's record in Tracks merchants returning via the KYC reminder email.
+		if ( 'initial' === $redirect_param ) {
+			$offset      = 1;
+			$description = 'initial';
+		} elseif ( 'second' === $redirect_param ) {
+			$offset      = 3;
+			$description = 'second';
+		} else {
+			$follow_number = in_array( $redirect_param, [ '1', '2', '3', '4' ], true ) ? $redirect_param : '0';
+			// offset is recorded in days, $follow_number maps to the week number.
+			$offset      = (int) $follow_number * 7;
+			$description = 'weekly-' . $follow_number;
+		}
+
 		$track_props = [
-			'type'          => 'initial' === $redirect_param ? 'initial' : 'follow',
-			'follow_number' => in_array( $redirect_param, [ '1', '2', '3', '4' ], true ) ? $redirect_param : '0',
+			'offset'      => $offset,
+			'description' => $description,
 		];
 		wc_admin_record_tracks_event( 'wcpay_kyc_reminder_merchant_returned', $track_props );
 
