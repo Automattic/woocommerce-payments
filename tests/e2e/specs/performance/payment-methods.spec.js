9a85b2961f0bc250b64bda964e6c44d0fa24103c
Harris Wong <harris.wong@automattic.com>
2022-06-13 16:46:51 -0600

    Add performance test when UPE is enabled

--- tests/e2e/specs/performance/payment-methods.spec.js
+++ tests/e2e/specs/performance/payment-methods.spec.js
@@ -2,13 +2,17 @@
  * External dependencies
  */
 import config from 'config';
+const { merchant } = require( '@woocommerce/e2e-utils' );
 
 /**
  * Internal dependencies
  */
 import { setupProductCheckout } from '../../utils/payments';
-import { shopperWCP } from '../../utils';
+import { shopperWCP, merchantWCP } from '../../utils';
 import { getLoadingDurations } from '../../utils/performance';
+
+// The total number of times we should run our tests for averaging.
+const TOTAL_TRIALS = 3;
 
 const averageMetrics = ( metrics, i ) => {
 	const results = {};
@@ -18,7 +22,51 @@ const averageMetrics = ( metrics, i ) => {
 	return results;
 };
 
-describe( 'Checkout page performance', () => {
+/**
+ * This helper function goes to checkout page *i* times. Wait
+ * for the given card selector to load, retrieve all the metrics
+ * and find the average.
+ *
+ * @param {string} selector CSS selector.
+ * @return {mixed} The averaged results.
+ */
+const measureCheckoutMetrics = async ( selector ) => {
+	await expect( page ).toMatch( 'Checkout' );
+
+	// Run performance tests a few times, then take the average.
+	const results = {
+		serverResponse: [],
+		firstPaint: [],
+		domContentLoaded: [],
+		loaded: [],
+		firstContentfulPaint: [],
+		firstBlock: [],
+	};
+
+	let i = TOTAL_TRIALS;
+	while ( i-- ) {
+		await page.reload();
+		await page.waitForSelector( selector );
+		const {
+			serverResponse,
+			firstPaint,
+			domContentLoaded,
+			loaded,
+			firstContentfulPaint,
+			firstBlock,
+		} = await getLoadingDurations();
+
+		results.serverResponse.push( serverResponse );
+		results.firstPaint.push( firstPaint );
+		results.domContentLoaded.push( domContentLoaded );
+		results.loaded.push( loaded );
+		results.firstContentfulPaint.push( firstContentfulPaint );
+		results.firstBlock.push( firstBlock );
+	}
+	return results;
+};
+
+describe( 'Stripe element Checkout page performance', () => {
 	beforeEach( async () => {
 		await setupProductCheckout(
 			config.get( 'addresses.customer.billing' )
@@ -31,42 +79,48 @@ describe( 'Checkout page performance', () => {
 	} );
 
 	it( 'measures on page load', async () => {
-		const totalTrials = 3;
-		await expect( page ).toMatch( 'Checkout' );
+		const results = await measureCheckoutMetrics(
+			'#wcpay-card-element iframe'
+		);
+		console.log( 'Stripe element: All the trial results', results );
+		console.log(
+			'Stripe element: Average',
+			averageMetrics( results, TOTAL_TRIALS )
+		);
+	} );
+} );
 
-		// Run performance tests a few times, then take the average.
-		const results = {
-			serverResponse: [],
-			firstPaint: [],
-			domContentLoaded: [],
-			loaded: [],
-			firstContentfulPaint: [],
-			firstBlock: [],
-		};
+describe( 'UPE Checkout page performance', () => {
+	beforeEach( async () => {
+		// Activate UPE
+		await merchant.login();
+		await merchantWCP.activateUpe();
+		await merchant.logout();
 
-		let i = totalTrials;
-		while ( i-- ) {
-			await page.reload();
-			await page.waitForSelector(
-				'#payment_method_woocommerce_payments'
-			);
-			const {
-				serverResponse,
-				firstPaint,
-				domContentLoaded,
-				loaded,
-				firstContentfulPaint,
-				firstBlock,
-			} = await getLoadingDurations();
+		// Setup cart
+		await setupProductCheckout(
+			config.get( 'addresses.customer.billing' )
+		);
+	} );
 
-			results.serverResponse.push( serverResponse );
-			results.firstPaint.push( firstPaint );
-			results.domContentLoaded.push( domContentLoaded );
-			results.loaded.push( loaded );
-			results.firstContentfulPaint.push( firstContentfulPaint );
-			results.firstBlock.push( firstBlock );
-		}
-		console.log( 'All the trial results', results );
-		console.log( 'Average', averageMetrics( results, totalTrials ) );
+	afterAll( async () => {
+		// Clear the cart at the end so it's ready for another test
+		await shopperWCP.emptyCart();
+
+		// Deactivate UPE
+		await merchant.login();
+		await merchantWCP.deactivateUpe();
+		await merchant.logout();
+	} );
+
+	it( 'measures on page load', async () => {
+		const results = await measureCheckoutMetrics(
+			'#wcpay-upe-element iframe'
+		);
+		console.log( 'Stripe UPE: All the trial results', results );
+		console.log(
+			'Stripe UPE: Average',
+			averageMetrics( results, TOTAL_TRIALS )
+		);
 	} );
 } );
