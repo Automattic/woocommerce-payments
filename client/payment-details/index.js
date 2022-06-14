8f24be575cdf3a4bffe4084bf44569a4370b8c38
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2022-06-14 15:19:34 -0300

    Update page to follow the payment intent fallback flow

--- client/payment-details/index.js
+++ client/payment-details/index.js
@@ -1,13 +1,13 @@
 /**
  * External dependencies
  */
+import { useEffect } from 'react';
 import { __ } from '@wordpress/i18n';
 import { Card, CardBody } from '@wordpress/components';
 
 /**
  * Internal dependencies
  */
-import { useCharge } from '../data';
 import PaymentDetailsSummary from './summary';
 import PaymentDetailsTimeline from './timeline';
 import PaymentDetailsPaymentMethod from './payment-method';
@@ -15,6 +15,7 @@ import Page from 'components/page';
 import ErrorBoundary from 'components/error-boundary';
 import { TestModeNotice, topics } from 'components/test-mode-notice';
 import PaymentCardReaderChargeDetails from './readers';
+import { usePaymentIntentFallback } from 'wcpay/data/payment-intents';
 
 const PaymentDetails = ( props ) => {
 	if ( 'card_reader_fee' === props.query.transaction_type ) {
@@ -26,16 +27,19 @@ const PaymentDetails = ( props ) => {
 		);
 	}
 
-	return <PaymentChargeDetails chargeId={ props.query.id } />;
+	return <PaymentChargeDetails id={ props.query.id } />;
 };
 
-const PaymentChargeDetails = ( props ) => {
-	const chargeId = props.chargeId;
-	const { charge, isLoading, chargeError } = useCharge( chargeId );
+const PaymentChargeDetails = ( { id } ) => {
+	const { data, isLoading, error, redirect } = usePaymentIntentFallback( id );
 	const testModeNotice = <TestModeNotice topic={ topics.paymentDetails } />;
 
+	useEffect( () => {
+		if ( redirect ) window.location.href = redirect.url;
+	}, [ data.payment_intent, redirect ] );
+
 	// Check instance of chargeError because its default value is empty object
-	if ( ! isLoading && chargeError instanceof Error ) {
+	if ( ! isLoading && error instanceof Error ) {
 		return (
 			<Page maxWidth={ 1032 } className="wcpay-payment-details">
 				{ testModeNotice }
@@ -56,18 +60,18 @@ const PaymentChargeDetails = ( props ) => {
 			{ testModeNotice }
 			<ErrorBoundary>
 				<PaymentDetailsSummary
-					charge={ charge }
+					charge={ data }
 					isLoading={ isLoading }
 				/>
 			</ErrorBoundary>
 			{ wcpaySettings.featureFlags.paymentTimeline && (
 				<ErrorBoundary>
-					<PaymentDetailsTimeline chargeId={ chargeId } />
+					<PaymentDetailsTimeline chargeId={ id } />
 				</ErrorBoundary>
 			) }
 			<ErrorBoundary>
 				<PaymentDetailsPaymentMethod
-					charge={ charge }
+					charge={ data }
 					isLoading={ isLoading }
 				/>
 			</ErrorBoundary>
