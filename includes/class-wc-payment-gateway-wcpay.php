698f449ef398786fa34f9cf501d609efe873e930
Danae Millan <danae.millan@automattic.com>
2022-06-17 14:49:55 -0300

    Pass the WCPay version when checking the user exists and sending the OTP
    This is, when making requests to the 'user/exists' and 'sms-otp' endpoints of the platform checkout.

--- includes/class-wc-payment-gateway-wcpay.php
+++ includes/class-wc-payment-gateway-wcpay.php
@@ -672,6 +672,7 @@ class WC_Payment_Gateway_WCPay extends WC_Payment_Gateway_CC {
 			'platformCheckoutHost'           => defined( 'PLATFORM_CHECKOUT_FRONTEND_HOST' ) ? PLATFORM_CHECKOUT_FRONTEND_HOST : 'https://pay.woo.com',
 			'platformTrackerNonce'           => wp_create_nonce( 'platform_tracks_nonce' ),
 			'accountIdForIntentConfirmation' => apply_filters( 'wc_payments_account_id_for_intent_confirmation', '' ),
+			'wcpayVersionNumber'             => WCPAY_VERSION_NUMBER,
 		];
 	}
 
