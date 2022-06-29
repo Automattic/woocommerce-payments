a661754d6def0fbcf9be9113adcbe8eb66852075
James Allan <james.allan@automattic.com>
2022-06-29 15:57:26 +1000

    Add a notification badge to disputes admin menu item indicating disputes needing response  (#4326)
    * Add disputes needing response badge to disputes admin menu item
    
    * Replace disputes_summary class with the existing WCPay\Database_Cache class
    
    * Update function doc comment
    
    * Take into account disputes with the "warning_needs_response" status when displaying notification
    
    * Update status count array key inline with changes to server
    ref: https://github.com/Automattic/woocommerce-payments-server/pull/2078/commits/e0e5cd230e15f3012a4a17773fc9f2d698ede014
    
    * Add changelog entry
    
    * Fix unit test failures
    
    * Add test to test that the dispute notification is added when there are disputes needing a response
    
    * Return early if there is no dispute status count breakdown
    
    * Make sure the admin menu is created before attempting to get it
    
    * debugging failing test
    
    * Test that no notification badge is shown when there is no dispute needing a response
    
    * Use the new status count endpoint rather than the summary to get status counts
    
    * Update unit tests to match the new endpoint return format
    
    * Update cache key variable and value to match the new data it stores
    
    * Update cache delete comment reference to summary
    
    * Introduce public function to get the number of disputes needing a response
    
    * Fix linting issue because array sum can return a float
    
    * Direct the user to the disputes which need a response by default
    
    * Add doc comments and make dispute count function private
    
    * Update includes/wc-payment-api/class-wc-payments-api-client.php
    
    Co-authored-by: Eric Jinks <ericjinks@gmail.com>
    
    * Update unit tests to expect the dispute menu to include the awaiting response filter
    
    * Fix unit test
    
    * Fix unit test
    
    * After accepting a dispute, redirect to disputes needing a response
    
    Co-authored-by: Eric Jinks <ericjinks@gmail.com>

--- includes/class-wc-payments.php
+++ includes/class-wc-payments.php
@@ -328,7 +328,7 @@ class WC_Payments {
 			self::$card_gateway = new $card_class( self::$api_client, self::$account, self::$customer_service, self::$token_service, self::$action_scheduler_service, self::$failed_transaction_rate_limiter, self::$order_service );
 		}
 
-		self::$webhook_processing_service  = new WC_Payments_Webhook_Processing_Service( self::$api_client, self::$db_helper, self::$account, self::$remote_note_service, self::$order_service, self::$in_person_payments_receipts_service, self::$card_gateway, self::$customer_service );
+		self::$webhook_processing_service  = new WC_Payments_Webhook_Processing_Service( self::$api_client, self::$db_helper, self::$account, self::$remote_note_service, self::$order_service, self::$in_person_payments_receipts_service, self::$card_gateway, self::$customer_service, self::$database_cache );
 		self::$webhook_reliability_service = new WC_Payments_Webhook_Reliability_Service( self::$api_client, self::$action_scheduler_service, self::$webhook_processing_service );
 
 		self::maybe_register_platform_checkout_hooks();
@@ -365,7 +365,7 @@ class WC_Payments {
 		// Add admin screens.
 		if ( is_admin() && current_user_can( 'manage_woocommerce' ) ) {
 			include_once WCPAY_ABSPATH . 'includes/admin/class-wc-payments-admin.php';
-			new WC_Payments_Admin( self::$api_client, self::$card_gateway, self::$account );
+			new WC_Payments_Admin( self::$api_client, self::$card_gateway, self::$account, self::$database_cache );
 
 			// Use tracks loader only in admin screens because it relies on WC_Tracks loaded by WC_Admin.
 			include_once WCPAY_ABSPATH . 'includes/admin/tracks/tracks-loader.php';
