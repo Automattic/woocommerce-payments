98402d18974f272667dcc6ac8798f18ae4e69d3b
Daniel Mallory <daniel.mallory@automattic.com>
2022-07-07 16:15:19 +0100

    Updates to the logic to make the filter re-render

--- client/transactions/filters/index.tsx
+++ client/transactions/filters/index.tsx
@@ -8,11 +8,7 @@ import { getQuery } from '@woocommerce/navigation';
 /**
  * Internal dependencies
  */
-import {
-	getFilters,
-	getAdvancedFilters,
-	TransactionsFilterType,
-} from './config';
+import { getFilters, getAdvancedFilters } from './config';
 import { formatCurrencyName } from '../../utils/currency';
 import './style.scss';
 import { __ } from '@wordpress/i18n/build-types';
@@ -40,29 +36,6 @@ export const TransactionsFilters = ( {
 			value: currencyCode,
 		} )
 	);
-
-	// const populateDepositCurrencies = (
-	// 	filtersConfiguration: TransactionsFilterType[]
-	// ) => {
-	// 	filtersConfiguration.forEach( ( filter ) => {
-	// 		if ( 'store_currency_is' === filter.param ) {
-	// 			const currencies = storeCurrencies || [];
-	// 			// Generate select options: pick the first one (default) and add provided currencies
-	// 			filter.filters = [
-	// 				filter.filters[ 0 ],
-	// 				...currencies.map( ( currencyCode: string ) => ( {
-	// 					label: formatCurrencyName( currencyCode ),
-	// 					value: currencyCode,
-	// 				} ) ),
-	// 			];
-	// 			// Show the select when several currencies are available.
-	// 			if ( 2 < filter.filters.length ) {
-	// 				filter.showFilters = () => true;
-	// 			}
-	// 		}
-	// 	} );
-	// 	return filtersConfiguration;
-	// };
 
 	return (
 		<div className="woocommerce-filters-transactions">
