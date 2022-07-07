4247314fdc3ff9bd7c705c480c59ee1d8c49e0e0
James Allan <james.allan@automattic.com>
2022-07-07 13:49:42 +1000

    Move tasks folder and add method to fetch dispute counts

--- includes/class-wc-payments-tasks.php
+++ includes/class-wc-payments-tasks.php
@@ -21,17 +21,16 @@ class WC_Payments_Tasks {
 	 * WC_Payments_Admin_Tasks constructor.
 	 */
 	public static function init() {
-		include_once WCPAY_ABSPATH . 'includes/tasks/class-wc-payments-task-disputes.php';
+		include_once WCPAY_ABSPATH . 'includes/admin/tasks/class-wc-payments-task-disputes.php';
 
 		add_action( 'init', [ __CLASS__, 'add_task_disputes_need_response' ] );
 	}
 
 	/**
-	 * Adds a task to WC 'Things to do next' task list the if disputes awaiting response.
+	 * Adds a task to the WC 'Things to do next' task list the if disputes awaiting response.
 	 */
 	public static function add_task_disputes_need_response() {
-		$task = new WC_Payments_Task_Disputes();
-		// 'extended' = 'Things to do next' task list.
-		TaskLists::add_task( 'extended', $task );
+		// 'extended' = 'Things to do next' task list on WooCommerce > Home.
+		TaskLists::add_task( 'extended', new WC_Payments_Task_Disputes() );
 	}
 }
