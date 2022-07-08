096e5a51594988889d3fbc217f1eb6e325d9b125
James Allan <james.allan@automattic.com>
2022-07-08 13:16:22 +1000

    Add a disputes task to the "Things to do next" list on WC > Home (#4418)
    * Create Disputes task and add to WC 'extended' task list
    
    * Move tasks folder and add method to fetch dispute counts
    
    * Prevent fatal error when WooCommerce isn't active
    
    * Add change log entry
    
    * Override the task::get_parent_id() function to support versions of WC pre 6.4.0
    
    * Fix psalm errors caused by the TaskLists::add_task() second param is listed as an array but needs to be a task object
    
    * Remove task estimated time
    
    * Update changelog/add-wc-home-dispute-task
    
    Co-authored-by: Eric Jinks <ericjinks@gmail.com>
    
    * Add class comment to highlight this task is separate to the one on the Payments > Overview screen
    
    Co-authored-by: Eric Jinks <ericjinks@gmail.com>

---
+++ includes/class-wc-payments-tasks.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * WC_Payments_Tasks class
+ *
+ * @package WooCommerce\Payments\Tasks
+ */
+
+use Automattic\WooCommerce\Admin\Features\OnboardingTasks\TaskLists;
+use WooCommerce\Payments\Tasks\WC_Payments_Task_Disputes;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Hooks into WC TaskLists to display WCPay tasks.
+ */
+class WC_Payments_Tasks {
+
+	/**
+	 * WC_Payments_Admin_Tasks constructor.
+	 */
+	public static function init() {
+		include_once WCPAY_ABSPATH . 'includes/admin/tasks/class-wc-payments-task-disputes.php';
+
+		add_action( 'init', [ __CLASS__, 'add_task_disputes_need_response' ] );
+	}
+
+	/**
+	 * Adds a task to the WC 'Things to do next' task list the if disputes awaiting response.
+	 */
+	public static function add_task_disputes_need_response() {
+		// 'extended' = 'Things to do next' task list on WooCommerce > Home.
+		TaskLists::add_task( 'extended', new WC_Payments_Task_Disputes() );
+	}
+}
