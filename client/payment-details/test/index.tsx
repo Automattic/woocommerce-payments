a89f95e14b6fd7ecb98b6bc9f824236e7d57ed4e
Eduardo Pieretti Umpierre <eduardoumpierre@hotmail.com>
2022-07-18 14:18:24 -0300

    Redirect "ch_" charge id format to "pi_" payment intent id on Transaction Detail screens (#4348)
    * Register payment intents REST controller
    
    * Payment intents reducer added to store
    
    * Add payment intents fallback hook
    
    It should take different paths based on the id passed as param.
    In case of a charge id "ch_", it should fetch the charge and return
    an object with a redirect URL. Otherwise, it fetches the payment intent
    and the charge related, the return value should be the charge.
    
    * Add jsonSerialize method to define how the classes should be serialized to JSON
    
    * Refactor timeline hook to receive the payment intent id
    
    * Update page to follow the payment intent fallback flow
    
    * Add payment intent resolvers unit tests
    
    * Fix destructuring naming to represent the correct intent
    
    * Fix useEffect dependency array
    
    Remove unnecessary variable from useEffect's dependency array
    
    * Add unit tests to payment details page
    
    * Entry added on changelog file
    
    * Update changelog entry to be more descriptive
    
    * Rename mock object to reflect its content
    
    * Rename reducer name to fix plural error
    
    * Remove unnecessary data from wcpaySettings mock on Payment Details unit tests
    
    * Add snapshot test to validate Payment Details page structure
    
    It should render the charge data when receiving a payment intent id as
    query param.
    
    * Fix variable name on comment
    
    * Remove unnecessary text from test description
    
    * Move ApiError interface to a more specific file
    
    It's been moved to a more broad folder because it will be used by
    multiple files.
    
    * Update WC_Payments_API_Intention json serialize method to match Charge response
    
    * Add Typescript to new Payment Intent data files
    
    New State interface created to hold the store state.
    
    * Rename PaymentIntent hook to be more descriptive
    
    * Refactor fallback payment intent hook
    
    Move redirect logic to the component and reuse getCharge logic
    from the charge hook. Remove unnecessary conditional from the
    timeline hook.
    
    * Add unit tests for usePaymentIntentWithChargeFallback hook
    
    * Add unit tests for payment intents selectors
    
    * Fix PaymentChargeDetails useEffect dependency array
    
    * Update variable name to be more descriptive
    
    * Remove payment intent request dependency from timeline hook
    
    Remove payment intent dependency as the hook receives a payment
    intent id as an argument
    
    * Remove unnecessary variable memoization
    
    * Update Payment Detail snapshot

---
+++ client/payment-details/test/index.tsx
@@ -0,0 +1,172 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+import { render } from '@testing-library/react';
+import { useSelect } from '@wordpress/data';
+import React from 'react';
+
+/**
+ * Internal dependencies
+ */
+import PaymentDetailsPage from '../';
+
+declare const global: {
+	wcSettings: { countries: Record< string, string > };
+	wcpaySettings: {
+		zeroDecimalCurrencies: string[];
+		featureFlags: Record< string, boolean >;
+		connect: {
+			country: string;
+		};
+	};
+};
+
+// Workaround for mocking @wordpress/data.
+// See https://github.com/WordPress/gutenberg/issues/15031
+jest.mock( '@wordpress/data', () => ( {
+	createRegistryControl: jest.fn(),
+	dispatch: jest.fn( () => ( { setIsMatching: jest.fn() } ) ),
+	registerStore: jest.fn(),
+	select: jest.fn(),
+	combineReducers: jest.fn(),
+	useDispatch: jest.fn( () => ( { createNotice: jest.fn() } ) ),
+	withDispatch: jest.fn( () => jest.fn() ),
+	withSelect: jest.fn( () => jest.fn() ),
+	useSelect: jest.fn(),
+} ) );
+
+( useSelect as jest.Mock ).mockImplementation( ( cb ) =>
+	cb(
+		jest.fn().mockReturnValue( {
+			getCharge: jest.fn().mockReturnValue( {
+				id: 'ch_mock',
+				amount: 1500,
+				application_fee_amount: 74,
+				balance_transaction: {
+					amount: 1500,
+					currency: 'usd',
+					fee: 74,
+				},
+				billing_details: {
+					address: {
+						city: 'City',
+						country: 'US',
+						line1: 'Line 1 Street',
+						line2: 'Line 2',
+						postal_code: '99999',
+						state: 'CA',
+					},
+					email: 'email@example.com',
+					name: 'First Name',
+					phone: '1-000-000-0000',
+					formatted_address:
+						'Line 1 Street<br/>Line 2<br/>City, CA 99999',
+				},
+				captured: true,
+				created: 1655921807,
+				currency: 'usd',
+				customer: 'cus_mock',
+				dispute: null,
+				disputed: false,
+				paid: true,
+				payment_intent: 'pi_mock',
+				payment_method: 'pm_mock',
+				payment_method_details: {
+					card: {
+						brand: 'visa',
+						checks: {
+							address_line1_check: 'pass',
+							address_postal_code_check: 'pass',
+							cvc_check: 'pass',
+						},
+						country: 'US',
+						exp_month: 12,
+						exp_year: 2099,
+						fingerprint: 'fingerprint',
+						funding: 'credit',
+						last4: '4242',
+						network: 'visa',
+					},
+					type: 'card',
+				},
+				refunded: false,
+				refunds: {},
+				status: 'succeeded',
+			} ),
+			isResolving: jest.fn().mockReturnValue( false ),
+			getChargeError: jest.fn().mockReturnValue( null ),
+			getPaymentIntent: jest.fn().mockReturnValue( {
+				id: 'pi_mock',
+				charge: {
+					id: 'ch_mock',
+				},
+			} ),
+			getPaymentIntentError: jest.fn().mockReturnValue( null ),
+			getTimeline: jest.fn().mockReturnValue( {} ),
+			getTimelineError: jest.fn().mockReturnValue( null ),
+		} )
+	)
+);
+
+global.wcSettings = {
+	countries: {
+		US: 'United States of America',
+	},
+};
+
+global.wcpaySettings = {
+	featureFlags: { paymentTimeline: true },
+	zeroDecimalCurrencies: [ 'usd' ],
+	connect: { country: 'US' },
+};
+
+describe( 'Payment details page', () => {
+	const { location } = window;
+
+	const chargeQuery = { id: 'ch_mock' };
+	const paymentIntentQuery = { id: 'pi_mock' };
+	const redirectUrl =
+		'admin.php?page=wc-admin&path=%2Fpayments%2Ftransactions%2Fdetails&id=pi_mock';
+
+	beforeEach( () => {
+		Object.defineProperty( window, 'location', {
+			value: { href: 'http://example.com' },
+		} );
+	} );
+
+	afterAll( () => {
+		window.location = location;
+	} );
+
+	it( 'should match the snapshot - Payment Intent query param', () => {
+		const { container } = render(
+			<PaymentDetailsPage query={ paymentIntentQuery } />
+		);
+
+		expect( container ).toMatchSnapshot();
+	} );
+
+	it( 'should match the snapshot - Charge query param', () => {
+		const { container } = render(
+			<PaymentDetailsPage query={ chargeQuery } />
+		);
+
+		expect( container ).toMatchSnapshot();
+	} );
+
+	it( 'should redirect from ch_mock to pi_mock', () => {
+		render( <PaymentDetailsPage query={ chargeQuery } /> );
+
+		expect( window.location.href ).toEqual( redirectUrl );
+	} );
+
+	it( 'should not redirect with a payment intent ID as query param', () => {
+		const { href } = window.location;
+
+		render( <PaymentDetailsPage query={ paymentIntentQuery } /> );
+
+		expect( window.location.href ).toEqual( href );
+	} );
+} );
