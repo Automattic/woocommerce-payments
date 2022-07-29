f5c83e25f12778df07a1ad636e96cffeb8eb4a97
Ismael Martín Alabarce <info@ismaeld.com>
2022-07-29 12:59:25 +0200

    Add HMR webpack configuration
    Conditionally based on WEBPACK_SERVE from env

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,25 @@
 /* eslint-disable */
-const { merge } = require( 'webpack-merge' );
+const { mergeWithRules } = require( 'webpack-merge' );
 
 const shared = require( './webpack/shared' );
 const production = require( './webpack/production' );
 const development = require( './webpack/development' );
+const hmr = require( './webpack/hmr' );
 
 const mode = process.env.NODE_ENV || 'development';
 
-module.exports = () => {
+const merge = mergeWithRules( {
+	module: {
+		rules: {
+			test: 'match',
+			use: 'replace',
+		},
+	},
+} );
+
+module.exports = ( { WEBPACK_SERVE } ) => {
 	if ( mode === 'development' ) {
-		return merge( { mode }, shared, development );
+		return merge( { mode }, shared, development, !! WEBPACK_SERVE && hmr );
 	}
 
 	return merge( { mode }, shared, production );
