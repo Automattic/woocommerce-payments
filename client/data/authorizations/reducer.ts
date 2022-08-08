ae418995ef267bc902a064b26fefcc6271ea2b21
Ahmed <ahmed.el.azzabi@automattic.com>
2022-08-08 15:19:48 +0100

    add reducer

---
+++ client/data/authorizations/reducer.ts
@@ -0,0 +1,63 @@
+/** @format */
+
+/**
+ * Internal dependencies
+ */
+import TYPES from './action-types';
+import { getResourceId } from 'utils/data';
+import { Query } from '@woocommerce/navigation';
+import { Authorization } from './hooks';
+
+const defaultState = { summary: {} };
+
+const receiveAuthorizations = (
+	state = defaultState,
+	{
+		type,
+		query = {},
+		data = [],
+		error,
+	}: {
+		type: string;
+		query: Query;
+		data: Authorization[];
+		error: string;
+	}
+) => {
+	const index = getResourceId( query );
+
+	switch ( type ) {
+		case TYPES.SET_AUTHORIZATIONS:
+			return {
+				...state,
+				authorizations: data,
+			};
+		case TYPES.SET_ERROR_FOR_AUTHORIZATIONS:
+			return {
+				...state,
+				[ index ]: {
+					error: error,
+				},
+			};
+		case TYPES.SET_AUTHORIZATIONS_SUMMARY:
+			return {
+				...state,
+				summary: Object.assign( {}, state.summary, data ),
+			};
+		case TYPES.SET_ERROR_FOR_AUTHORIZATIONS_SUMMARY:
+			return {
+				...state,
+				summary: {
+					...state.summary,
+					[ index ]: {
+						error: error,
+					},
+				},
+			};
+	}
+
+	// Fallback to returning the same state.
+	return state;
+};
+
+export default receiveAuthorizations;
