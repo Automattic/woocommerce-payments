429dcf3da0757c23dbbefe21dcae863dbdbdf596
jbordonado <jbordonado@users.noreply.github.com>
2022-08-12 16:40:02 +0200

    Silence psalm erros regarding types

--- includes/class-wc-payments-db.php
+++ includes/class-wc-payments-db.php
@@ -19,7 +19,7 @@ class WC_Payments_DB {
 	 *
 	 * @param string $charge_id Charge ID corresponding to an order ID.
 	 *
-	 * @return boolean|WC_Order|WC_Order_Refund
+	 * @return boolean|WC_Order
 	 */
 	public function order_from_charge_id( string $charge_id ) {
 		return $this->order_from_meta_key_value( self::META_KEY_CHARGE_ID, $charge_id );
@@ -30,7 +30,7 @@ class WC_Payments_DB {
 	 *
 	 * @param array $charge_ids List of charge IDs corresponding to an order ID.
 	 *
-	 * @return array|WC_Order[]|WC_Order_Refund[]
+	 * @return stdClass|WC_Order[]
 	 */
 	public function orders_from_charge_ids( array $charge_ids ): array {
 		return $this->orders_from_meta_key_values( self::META_KEY_CHARGE_ID, $charge_ids );
@@ -41,7 +41,7 @@ class WC_Payments_DB {
 	 *
 	 * @param string $intent_id Intent ID corresponding to an order ID.
 	 *
-	 * @return boolean|WC_Order|WC_Order_Refund
+	 * @return boolean|WC_Order
 	 */
 	public function order_from_intent_id( $intent_id ) {
 		return $this->order_from_meta_key_value( self::META_KEY_INTENT_ID, $intent_id );
@@ -53,7 +53,7 @@ class WC_Payments_DB {
 	 * @param string $meta_key   Either '_intent_id' or '_charge_id'.
 	 * @param string $meta_value Value for the meta key.
 	 *
-	 * @return boolean|WC_Order|WC_Order_Refund
+	 * @return boolean|WC_Order
 	 */
 	private function order_from_meta_key_value( string $meta_key, string $meta_value ) {
 		$custom_query_var_handler = function( $query, $query_vars ) use ( $meta_key ) {
@@ -76,6 +76,11 @@ class WC_Payments_DB {
 		);
 		remove_filter( 'woocommerce_order_data_store_cpt_get_orders_query', $custom_query_var_handler, 10 );
 
+		/**
+		 * Suppress psalm error
+		 *
+		 * @psalm-suppress UndefinedMethod
+		 */
 		return $orders[0] ?? false;
 	}
 
@@ -85,7 +90,7 @@ class WC_Payments_DB {
 	 * @param string $meta_key   Either '_intent_id' or '_charge_id'.
 	 * @param array  $meta_value Value for the meta key.
 	 *
-	 * @return array|WC_Order[]|WC_Order_Refund[]
+	 * @return stdClass|WC_Order[]
 	 */
 	private function orders_from_meta_key_values( string $meta_key, array $meta_value ): array {
 		$custom_query_var_handler = function( $query, $query_vars ) use ( $meta_key ) {
@@ -109,6 +114,11 @@ class WC_Payments_DB {
 		);
 		remove_filter( 'woocommerce_order_data_store_cpt_get_orders_query', $custom_query_var_handler, 10 );
 
+		/**
+		 * Suppress psalm error
+		 *
+		 * @psalm-suppress InvalidReturnStatement
+		 */
 		return $orders ?? [];
 	}
 
