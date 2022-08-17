acf6692e4f314cf29ade54da4342766c179341c9
Jessy P <jessy.pappachan@automattic.com>
2022-08-17 21:17:17 +0530

    added checks

--- includes/class-wc-payments-db.php
+++ includes/class-wc-payments-db.php
@@ -95,7 +95,10 @@ class WC_Payments_DB {
 				'meta_value' => $meta_value, //phpcs:ignore WordPress.DB.SlowDBQuery.slow_db_query_meta_value
 			]
 		);
-		return $orders[0];
+		if ( $orders && ! empty( $orders ) ) {
+			return $orders[0];
+		}
+		return null;
 	}
 
 	/**
