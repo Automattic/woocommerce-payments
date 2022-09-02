f9aaeeea1d868f81e48b222f8bbee26cef214b6f
Achyuth Ajoy <achyuthajoy@gmail.com>
2022-09-02 16:19:08 +0530

    Use defined selectors

--- tests/e2e/specs/subscriptions/shopper/shopper-subscriptions-purchase-free-trial.spec.js
+++ tests/e2e/specs/subscriptions/shopper/shopper-subscriptions-purchase-free-trial.spec.js
@@ -32,12 +32,30 @@ const customerBilling = config.get(
 );
 let orderId;
 
+const testSelectors = {
+	productSubscriptionDetails: '.subscription-details',
+	cartSubscriptionFirstPaymentDate: '.first-payment-date',
+	cartOrderTotal: '.order-total',
+	checkoutSubscriptionDetails: '.subscription-details',
+	checkoutSubscriptionFirstPaymentDate: '.first-payment-date',
+	checkoutPlaceOrderButton: '#place_order',
+	checkoutOrderId: '.woocommerce-order-overview__order.order > strong',
+	wcOrderPaymentId: '.woocommerce-order-data__meta',
+	subscriptionStatus: '.subscription-status',
+	subscriptionProductName: '.order-item',
+	subscriptionRecurringTotal: '.recurring_total',
+	subscriptionTrialEnd: '.trial_end_date',
+};
+
 describeif( RUN_SUBSCRIPTIONS_TESTS )(
 	'Subscriptions > Purchase subscription with free trial',
 	() => {
 		beforeAll( async () => {
 			// Delete the user, if present
 			await withRestApi.deleteCustomerByEmail( customerBilling.email );
+		} );
+		afterAll( async () => {
+			await merchant.logout();
 		} );
 
 		it( 'should be able to purchase a subscription with free trial', async () => {
@@ -46,42 +64,58 @@ describeif( RUN_SUBSCRIPTIONS_TESTS )(
 			await page.goto( config.get( 'url' ) + `product/${ productSlug }`, {
 				waitUntil: 'networkidle0',
 			} );
-			await expect( page ).toMatchElement( '.subscription-details', {
-				text: '/ month with a 14-day free trial',
-			} );
+			await expect( page ).toMatchElement(
+				testSelectors.productSubscriptionDetails,
+				{
+					text: '/ month with a 14-day free trial',
+				}
+			);
 
 			// Add it to the cart and verify that the cart page shows the free trial details
-			await expect( page ).toClick( '.single_add_to_cart_button' );
-			await page.waitForNavigation( { waitUntil: 'networkidle0' } );
+			await shopper.addToCart();
 			await shopper.goToCart();
-			await expect( page ).toMatchElement( '.subscription-details', {
-				text: '/ month with a 14-day free trial',
-			} );
+			await expect( page ).toMatchElement(
+				testSelectors.productSubscriptionDetails,
+				{
+					text: '/ month with a 14-day free trial',
+				}
+			);
 
 			// Also verify that the first renewal is 14 days from now
-			await expect( page ).toMatchElement( '.first-payment-date', {
-				text: `First renewal: ${ renewalDateFormatted }`,
-			} );
+			await expect( page ).toMatchElement(
+				testSelectors.cartSubscriptionFirstPaymentDate,
+				{
+					text: `First renewal: ${ renewalDateFormatted }`,
+				}
+			);
 
 			// Verify that the order total is $0.00
-			await expect( page ).toMatchElement( '.order-total', {
+			await expect( page ).toMatchElement( testSelectors.cartOrderTotal, {
 				text: '$0.00',
 			} );
 
 			// Proceed to the checkout page and verify that the 14-day free trial is shown in the product line item,
 			// and that the first renewal date is 14 days from now.
 			await setupCheckout( customerBilling );
-			await expect( page ).toMatchElement( '.subscription-details', {
-				text: '/ month with a 14-day free trial',
-			} );
-			await expect( page ).toMatchElement( '.first-payment-date', {
-				text: `First renewal: ${ renewalDateFormatted }`,
-			} );
+			await expect( page ).toMatchElement(
+				testSelectors.checkoutSubscriptionDetails,
+				{
+					text: '/ month with a 14-day free trial',
+				}
+			);
+			await expect( page ).toMatchElement(
+				testSelectors.cartSubscriptionFirstPaymentDate,
+				{
+					text: `First renewal: ${ renewalDateFormatted }`,
+				}
+			);
 
 			// Pay using a 3DS card
 			const card = config.get( 'cards.3dsOTP' );
 			await fillCardDetails( page, card );
-			await expect( page ).toClick( '#place_order' );
+			await expect( page ).toClick(
+				testSelectors.checkoutPlaceOrderButton
+			);
 			await confirmCardAuthentication( page, '3DS', true );
 			await page.waitForNavigation( {
 				waitUntil: 'networkidle0',
@@ -89,9 +123,7 @@ describeif( RUN_SUBSCRIPTIONS_TESTS )(
 			await expect( page ).toMatch( 'Order received' );
 
 			// Get the order ID so we can open it in the merchant view
-			const orderIdField = await page.$(
-				'.woocommerce-order-overview__order.order > strong'
-			);
+			const orderIdField = await page.$( testSelectors.checkoutOrderId );
 			orderId = await orderIdField.evaluate( ( el ) => el.innerText );
 
 			await shopper.logout();
@@ -102,7 +134,7 @@ describeif( RUN_SUBSCRIPTIONS_TESTS )(
 
 			await merchant.goToOrder( orderId );
 			await expect( page ).toMatchElement(
-				'.woocommerce-order-data__meta',
+				testSelectors.wcOrderPaymentId,
 				{
 					text: /\(seti_.*\)/,
 				}
@@ -111,20 +143,30 @@ describeif( RUN_SUBSCRIPTIONS_TESTS )(
 			await merchantWCP.openSubscriptions();
 
 			// Verify we have an active subscription for the product
-			await expect( page ).toMatchElement( '.subscription-status', {
-				text: 'Active',
-			} );
-			await expect( page ).toMatchElement( '.order-item', {
-				text: productName,
-			} );
-			await expect( page ).toMatchElement( '.recurring_total', {
-				text: '$9.99 / month',
-			} );
-			await expect( page ).toMatchElement( '.trial_end_date', {
-				text: renewalDateFormatted,
-			} );
-
-			await merchant.logout();
+			await expect( page ).toMatchElement(
+				testSelectors.subscriptionStatus,
+				{
+					text: 'Active',
+				}
+			);
+			await expect( page ).toMatchElement(
+				testSelectors.subscriptionProductName,
+				{
+					text: productName,
+				}
+			);
+			await expect( page ).toMatchElement(
+				testSelectors.subscriptionRecurringTotal,
+				{
+					text: '$9.99 / month',
+				}
+			);
+			await expect( page ).toMatchElement(
+				testSelectors.subscriptionTrialEnd,
+				{
+					text: renewalDateFormatted,
+				}
+			);
 		} );
 	}
 );
