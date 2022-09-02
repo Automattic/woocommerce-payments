850a1c08384aeaf388d04c5026a8ae9291ffac4e
Achyuth Ajoy <achyuthajoy@gmail.com>
2022-09-02 19:31:12 +0530

    Add helper to run WP-CLI commands

--- tests/e2e/utils/helpers.js
+++ tests/e2e/utils/helpers.js
@@ -1,4 +1,11 @@
 /**
+ * External dependencies
+ */
+import shell from 'shelljs';
+const WP_CONTAINER = 'wcp_e2e_wordpress';
+const WP_CLI = `docker run --rm --user xfs --volumes-from ${ WP_CONTAINER } --network container:${ WP_CONTAINER } wordpress:cli`;
+
+/**
  * Wait for UI placeholders to finish and UI content is loaded.
  *
  */
@@ -17,5 +24,21 @@ export const takeScreenshot = ( name ) => {
 	return page.screenshot( {
 		path: `./screenshots/${ name }.png`,
 		fullPage: true,
+	} );
+};
+
+// Helper to run WP-CLI commands
+export const runWPCLI = ( command ) => {
+	return new Promise( ( resolve, reject ) => {
+		shell.exec(
+			`${ WP_CLI + ' ' + command }`,
+			{ silent: true },
+			( err ) => {
+				if ( err ) {
+					reject( err );
+				}
+				resolve();
+			}
+		);
 	} );
 };
