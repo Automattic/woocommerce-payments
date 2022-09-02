7cbb90ca118279122d3873ad8ba212c4a9ec9fc4
Achyuth Ajoy <achyuthajoy@gmail.com>
2022-09-02 16:31:15 +0530

    Use defined selectors & update teardown

--- tests/e2e/specs/subscriptions/shopper/shopper-subscriptions-purchase-multiple-subscriptions.spec.js
+++ tests/e2e/specs/subscriptions/shopper/shopper-subscriptions-purchase-multiple-subscriptions.spec.js
@@ -19,6 +19,17 @@ const customerBilling = config.get(
 );
 const card = config.get( 'cards.basic' );
 
+const testSelectors = {
+	myAccountSubscriptionsId: 'td.subscription-id',
+	myAccountSubscriptionsViewButton: 'td.subscription-actions a',
+	myAccountSubscriptionOrderItems:
+		'.shop_table order_details tbody tr.order_item',
+	myAccountSubscriptionProductName: '.product-name',
+	myAccountSubscriptionProductTotal: '.product-total',
+	myAccountSubscriptionRecurringTotal: '.shop_table tfoot tr:last-child td',
+	myAccountSubscriptionOrderTotal: 'td.order-total',
+};
+
 describeif( RUN_SUBSCRIPTIONS_TESTS )(
 	'Subscriptions > Purchase multiple subscriptions',
 	() => {
@@ -26,18 +37,15 @@ describeif( RUN_SUBSCRIPTIONS_TESTS )(
 			// Delete the user, if present
 			await withRestApi.deleteCustomerByEmail( customerBilling.email );
 		} );
+		afterAll( async () => {
+			await shopper.logout();
+		} );
 
 		it( 'should be able to purchase multiple subscriptions', async () => {
 			// As a Shopper, purchase the subscription products.
 			const productSlugs = Object.values( products );
 			for ( const productSlug of productSlugs ) {
-				await page.goto(
-					config.get( 'url' ) + `product/${ productSlug }`,
-					{
-						waitUntil: 'networkidle0',
-					}
-				);
-				await shopper.addToCart();
+				await shopperWCP.addToCartBySlug( productSlug );
 			}
 
 			await setupCheckout( customerBilling );
@@ -49,44 +57,54 @@ describeif( RUN_SUBSCRIPTIONS_TESTS )(
 
 			// Ensure the listing contains only one new entry
 			const entriesCount = await page.$$eval(
-				'td.subscription-id',
+				testSelectors.myAccountSubscriptionsId,
 				( els ) => els.length
 			);
 			expect( entriesCount ).toEqual( 1 );
 
 			// Click the 'View' button on the right side of the entry
-			await expect( page ).toClick( 'td.subscription-actions a', {
-				text: 'View',
-			} );
+			await expect( page ).toClick(
+				testSelectors.myAccountSubscriptionsViewButton,
+				{
+					text: 'View',
+				}
+			);
 			await page.waitForNavigation( { waitUntil: 'networkidle0' } );
 
 			// Ensure 'Subscription totals' section lists the subscription products with the correct price.
 			const subTotalsRows = await page.$$(
-				'.shop_table order_details tbody tr.order_item'
+				testSelectors.myAccountSubscriptionOrderItems
 			);
 			for ( let i = 0; i < subTotalsRows.length; i++ ) {
 				const row = subTotalsRows[ i ];
 
-				await expect( row ).toMatchElement( '.product-name', {
-					text: Object.keys( products )[ i ],
-				} );
-				await expect( row ).toMatchElement( '.product-total', {
-					text: '$9.99 / month',
-				} );
+				await expect( row ).toMatchElement(
+					testSelectors.myAccountSubscriptionProductName,
+					{
+						text: Object.keys( products )[ i ],
+					}
+				);
+				await expect( row ).toMatchElement(
+					testSelectors.myAccountSubscriptionProductTotal,
+					{
+						text: '$9.99 / month',
+					}
+				);
 			}
 			await expect( page ).toMatchElement(
-				'.shop_table tfoot tr:last-child td',
+				testSelectors.myAccountSubscriptionRecurringTotal,
 				{
 					text: '$19.98 / month',
 				}
 			);
 
 			// Confirm related order total matches payment
-			await expect( page ).toMatchElement( 'td.order-total', {
-				text: '$21.97 for 2 items',
-			} );
-
-			await shopper.logout();
+			await expect( page ).toMatchElement(
+				testSelectors.myAccountSubscriptionOrderTotal,
+				{
+					text: '$21.97 for 2 items',
+				}
+			);
 		} );
 	}
 );
