f2a1437c69dcfa7760a98e653b34d6ddfdab6739
bruce aldridge <bruce.aldridge@gmail.com>
2022-09-06 17:01:25 +1200

    Deprecate rather than remove deposits_status for account status data (#4702)
    * Deprecate rather than remove depositsStatus for account status data
    
    * Fix for incorrect key

--- includes/class-wc-payments-account.php
+++ includes/class-wc-payments-account.php
@@ -193,6 +193,7 @@ class WC_Payments_Account {
 			'status'              => $account['status'],
 			'paymentsEnabled'     => $account['payments_enabled'],
 			'deposits'            => $account['deposits'] ?? [],
+			'depositsStatus'      => $account['deposits']['status'] ?? $account['deposits_status'] ?? '',
 			'currentDeadline'     => isset( $account['current_deadline'] ) ? $account['current_deadline'] : false,
 			'pastDue'             => isset( $account['has_overdue_requirements'] ) ? $account['has_overdue_requirements'] : false,
 			'accountLink'         => $this->get_login_url(),
