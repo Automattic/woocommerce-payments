2746ff5982a7047f07b3020e4d1de3e02d1e6541
Dan Paun <dan.paun@automattic.com>
2022-09-07 18:16:42 +0300

    fix unit tests

--- tests/unit/admin/test-class-wc-rest-payments-settings-controller.php
+++ tests/unit/admin/test-class-wc-rest-payments-settings-controller.php
@@ -216,17 +216,12 @@ class WC_REST_Payments_Settings_Controller_Test extends WCPAY_UnitTestCase {
 	}
 
 
-	public function test_get_settings_non_us_returns_available_payment_method_ids() {
+	public function test_get_settings_link_inactive_returns_available_payment_method_ids() {
 
 		$this->mock_wcpay_account
 			->expects( $this->any() )
 			->method( 'get_cached_account_data' )
-			->willReturn( [ 'capabilities' => [ 'link_payments' => 'active' ] ] );
-
-		$this->mock_wcpay_account
-			->expects( $this->any() )
-			->method( 'get_account_country' )
-			->willReturn( 'CA' );
+			->willReturn( [ 'capabilities' => [ 'link_payments' => 'inactive' ] ] );
 
 		$response           = $this->upe_controller->get_settings();
 		$enabled_method_ids = $response->get_data()['available_payment_method_ids'];
