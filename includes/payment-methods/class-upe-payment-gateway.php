7ee712557e4c1abd36e889e112d997392a6761e0
Dan Paun <dan.paun@automattic.com>
2022-09-07 22:01:50 +0300

    fix payment method statuses check

--- includes/payment-methods/class-upe-payment-gateway.php
+++ includes/payment-methods/class-upe-payment-gateway.php
@@ -985,13 +985,11 @@ class UPE_Payment_Gateway extends WC_Payment_Gateway_WCPay {
 		$methods[] = Sepa_Payment_Method::PAYMENT_METHOD_STRIPE_ID;
 		$methods[] = P24_Payment_Method::PAYMENT_METHOD_STRIPE_ID;
 
-		$capability_key_map      = $this->get_payment_method_capability_key_map();
-		$payment_method_statuses = $this->get_upe_enabled_payment_method_statuses();
+		$capabilities             = $this->get_payment_method_capability_key_map();
+		$payment_methods_statuses = $this->get_upe_enabled_payment_method_statuses();
 
-		$capabilities = $this->get_payment_method_capability_key_map();
-		if ( isset( $capabilities[ Link_Payment_Method::PAYMENT_METHOD_STRIPE_ID ] ) ) {
-			$payment_methods_statuses = $this->get_upe_enabled_payment_method_statuses();
-			$link_status              = $payment_methods_statuses[ $capabilities[ Link_Payment_Method::PAYMENT_METHOD_STRIPE_ID ] ]['status'];
+		if ( isset( $payment_methods_statuses[ $capabilities[ Link_Payment_Method::PAYMENT_METHOD_STRIPE_ID ] ] ) ) {
+			$link_status = $payment_methods_statuses[ $capabilities[ Link_Payment_Method::PAYMENT_METHOD_STRIPE_ID ] ]['status'];
 			if ( 'inactive' !== $link_status ) {
 				$methods[] = Link_Payment_Method::PAYMENT_METHOD_STRIPE_ID;
 			}
