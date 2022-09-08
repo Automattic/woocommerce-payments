5810dc1504f704cb3c4beedb1b350a55b5f1df96
Eric Jinks <ericjinks@gmail.com>
2022-09-08 21:07:29 +1000

    Update amount currency format to match design

--- client/components/deposit-now-button/modal.tsx
+++ client/components/deposit-now-button/modal.tsx
@@ -8,7 +8,7 @@ import { __, sprintf } from '@wordpress/i18n';
 /**
  * Internal dependencies
  */
-import { formatExplicitCurrency } from 'utils/currency';
+import { formatCurrency, formatExplicitCurrency } from 'utils/currency';
 import './style.scss';
 
 interface DepositNowModalProps {
@@ -26,23 +26,22 @@ const DepositNowModal = ( {
 	onSubmit,
 	inProgress,
 }: DepositNowModalProps ): JSX.Element => {
-	const depositAmount = formatExplicitCurrency(
-		availableBalance.amount,
-		availableBalance.currency
-	);
 	/* translators: %s: amount representing the available monetary balance, %s: number of business days */
 	const description = sprintf(
 		__(
 			'Ready to get paid? Your available balance of %s will be deposited to your bank account within %s business days.',
 			'woocommerce-payments'
 		),
-		depositAmount,
+		formatExplicitCurrency(
+			availableBalance.amount,
+			availableBalance.currency
+		),
 		depositDelayDays
 	);
 
 	return (
 		<Modal
-			title={ __( 'Deposit Now', 'woocommerce-payments' ) }
+			title={ __( 'Deposit now', 'woocommerce-payments' ) }
 			onRequestClose={ onClose }
 			className="wcpay-deposit-now-modal"
 		>
@@ -58,7 +57,7 @@ const DepositNowModal = ( {
 					{ sprintf(
 						/* translators: %s: Monetary amount to deposit */
 						__( 'Deposit %s', 'woocommerce-payments' ),
-						depositAmount
+						formatCurrency( availableBalance.amount )
 					) }
 				</Button>
 				<Button isTertiary onClick={ onClose }>
