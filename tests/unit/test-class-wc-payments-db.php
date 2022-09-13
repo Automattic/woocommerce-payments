Automattic/woocommerce-payments

commit c6a78a1ea9a51e6b5e2cdc55ea77f7a3c6b1dc6f

Refactoring Direct DB query in orders_with_charge_id_from_charge_ids  (#4598)
* changed DB query to wc_get_orders

Co-authored-by: Jessy P <jessy.pappachan@automattic.com>

includes/class-wc-payments-db.php
@@ -38,28 +38,26 @@ class WC_Payments_DB {
 	 * @return array[]
 	 */
 	public function orders_with_charge_id_from_charge_ids( array $charge_ids ): array {
-		global $wpdb;
-
-		$charge_id_placeholder = implode( ',', array_fill( 0, count( $charge_ids ), '%s' ) );
 
 		// The order ID is saved to DB in `WC_Payment_Gateway_WCPay::process_payment()`.
-		$results = $wpdb->get_results(
-			$wpdb->prepare(
-				// phpcs:ignore WordPress.DB.PreparedSQL.InterpolatedNotPrepared,WordPress.DB.PreparedSQLPlaceholders.UnfinishedPrepare
-				"SELECT DISTINCT ID as order_id, meta.meta_value as charge_id FROM $wpdb->posts as posts LEFT JOIN $wpdb->postmeta as meta ON posts.ID = meta.post_id WHERE meta.meta_key = '_charge_id' AND meta.meta_value IN ($charge_id_placeholder)",
-				$charge_ids
-			)
+		$orders = wc_get_orders(
+			[
+				'meta_key'     => self::META_KEY_CHARGE_ID, //phpcs:ignore WordPress.DB.SlowDBQuery.slow_db_query_meta_key
+				'meta_value'   => $charge_ids, //phpcs:ignore WordPress.DB.SlowDBQuery.slow_db_query_meta_value
+				'meta_compare' => 'IN',
+			]
 		);
 
 		return array_map(
-			function ( stdClass $row ) : array {
+			function ( WC_Order $order ) : array {
 				return [
-					'order'     => $this->order_from_order_id( $row->order_id ),
-					'charge_id' => $row->charge_id,
+					'order'     => $order,
+					'charge_id' => $order->get_meta( self::META_KEY_CHARGE_ID ),
 				];
 			},
-			$results
+			$orders
 		);
+
 	}
 
 	/**

tests/unit/test-class-wc-payments-db.php
@@ -45,9 +45,9 @@ class WC_Payments_DB_Test extends WCPAY_UnitTestCase {
 
 		$this->assertCount( 2, $orders_with_charge_ids );
 		$this->assertIsArray( $orders_with_charge_ids[0] );
-		$this->assertSame( 'ch_1', $orders_with_charge_ids[0]['charge_id'] );
+		$this->assertTrue( in_array( $orders_with_charge_ids[0]['charge_id'], $existing_charge_ids, true ) );
 		$this->assertIsArray( $orders_with_charge_ids[1] );
-		$this->assertSame( 'ch_2', $orders_with_charge_ids[1]['charge_id'] );
+		$this->assertTrue( in_array( $orders_with_charge_ids[1]['charge_id'], $existing_charge_ids, true ) );
 	}
 
 }

commit 29f17c334df0adbc7a633e0b764bbd412b1b60e2

Fix Order ID being displayed N/A (#4752)
The Order # column in Payments > Transactions returns N/A even when an
order ID is present. This issue was most likely introduced in PR #4598.

When replacing the direct DB query with wc_get_orders, no limit arg
was passed. When no limit argument is passed wc_get_order function
will get the default posts_per_page as the limit. This default
limit causes the orders_with_charge_id_from_charge_ids method to
return incorrect number of orders.

includes/class-wc-payments-db.php
@@ -42,6 +42,7 @@ class WC_Payments_DB {
 		// The order ID is saved to DB in `WC_Payment_Gateway_WCPay::process_payment()`.
 		$orders = wc_get_orders(
 			[
+				'limit'        => -1,
 				'meta_key'     => self::META_KEY_CHARGE_ID, //phpcs:ignore WordPress.DB.SlowDBQuery.slow_db_query_meta_key
 				'meta_value'   => $charge_ids, //phpcs:ignore WordPress.DB.SlowDBQuery.slow_db_query_meta_value
 				'meta_compare' => 'IN',

tests/unit/test-class-wc-payments-db.php
@@ -29,10 +29,19 @@ class WC_Payments_DB_Test extends WCPAY_UnitTestCase {
 		$existing_charge_ids = [
 			'ch_1',
 			'ch_2',
+			'ch_3',
+			'ch_4',
+			'ch_5',
+			'ch_6',
+			'ch_7',
+			'ch_8',
+			'ch_9',
+			'ch_10',
+			'ch_11',
 		];
 
 		$non_existing_charge_ids = [
-			'ch_3',
+			'ch_12',
 		];
 
 		foreach ( $existing_charge_ids as $charge_id ) {
@@ -43,7 +52,7 @@ class WC_Payments_DB_Test extends WCPAY_UnitTestCase {
 
 		$orders_with_charge_ids = $this->wcpay_db->orders_with_charge_id_from_charge_ids( array_merge( $existing_charge_ids, $non_existing_charge_ids ) );
 
-		$this->assertCount( 2, $orders_with_charge_ids );
+		$this->assertCount( 11, $orders_with_charge_ids );
 		$this->assertIsArray( $orders_with_charge_ids[0] );
 		$this->assertTrue( in_array( $orders_with_charge_ids[0]['charge_id'], $existing_charge_ids, true ) );
 		$this->assertIsArray( $orders_with_charge_ids[1] );