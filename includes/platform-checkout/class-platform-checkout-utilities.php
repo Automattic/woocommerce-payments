df21c003d49f687ad3f79c2310c8f3d805408009
Brian Yu <brian@brianyu.me>
2022-09-22 16:47:33 -0400

    Allow subscription processing via WooPay

--- includes/platform-checkout/class-platform-checkout-utilities.php
+++ includes/platform-checkout/class-platform-checkout-utilities.php
@@ -25,8 +25,7 @@ class Platform_Checkout_Utilities {
 	public function should_enable_platform_checkout( $gateway ) {
 		$is_platform_checkout_eligible = WC_Payments_Features::is_platform_checkout_eligible(); // Feature flag.
 		$is_platform_checkout_enabled  = 'yes' === $gateway->get_option( 'platform_checkout', 'no' );
-		$disable_for_subscription      = ! WC_Payments_Features::is_platform_checkout_subscriptions_enabled() && $this->is_subscription_item_in_cart();
 
-		return $is_platform_checkout_eligible && $is_platform_checkout_enabled && ! $disable_for_subscription;
+		return $is_platform_checkout_eligible && $is_platform_checkout_enabled;
 	}
 }
