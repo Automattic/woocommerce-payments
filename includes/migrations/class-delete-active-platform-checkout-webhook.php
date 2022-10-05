f5e813acef0222365462aed3d81fe5467d1dd768
hsingyuc <hsingyuc.7@gmail.com>
2022-10-05 15:04:04 -0700

    Sanitize class name and update the version to next release version

--- includes/migrations/class-delete-active-platform-checkout-webhook.php
+++ includes/migrations/class-delete-active-platform-checkout-webhook.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Class Delete_Active_Webhook
+ * Class Delete_Active_Platform_Checkout_Webhook
  *
  * @package WooCommerce\Payments
  */
@@ -10,18 +10,18 @@ namespace WCPay\Migrations;
 defined( 'ABSPATH' ) || exit;
 
 /**
- * Class Delete_Active_Webhook
+ * Class Delete_Active_Platform_Checkout_Webhook
  *
- * Fires an event on plugin upgrade to delete the avtive webhook.
+ * Fires an event on plugin upgrade to delete the active webhook.
  * Runs only once. We want to know whether existing install had it
  * enabled before the current version.
  */
-class Delete_Active_Webhook {
+class Delete_Active_Platform_Checkout_Webhook {
 	/**
 	 * Checks whether we should trigger the event.
 	 */
 	public static function maybe_delete() {
-		if ( version_compare( get_option( 'woocommerce_woocommerce_payments_version' ), '4.8.0', '>' ) ) {
+		if ( version_compare( get_option( 'woocommerce_woocommerce_payments_version' ), '4.9.0', '>=' ) ) {
 			return;
 		}
 
