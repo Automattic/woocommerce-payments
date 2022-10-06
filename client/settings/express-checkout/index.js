5a6bf323ac80e1e3c7a4af12261ef9ddb693ace5
Dan Paun <dan.paun@automattic.com>
2022-10-06 11:08:04 +0300

    adjust texts and links in WC admin express checkout section

--- client/settings/express-checkout/index.js
+++ client/settings/express-checkout/index.js
@@ -164,7 +164,8 @@ const ExpressCheckout = () => {
 									/* eslint-disable jsx-a11y/anchor-has-content */
 									interpolateComponents( {
 										mixedString: __(
-											'By enabling this feature, you agree to {{stripeLink}}Stripe{{/stripeLink}}, ' +
+											'Boost sales by offering a fast, simple, and secure checkout experience.' +
+												'By enabling this feature, you agree to {{stripeLink}}Stripe{{/stripeLink}}, ' +
 												"{{appleLink}}Apple{{/appleLink}}, and {{googleLink}}Google{{/googleLink}}'s terms of use.",
 											'woocommerce-payments'
 										),
@@ -210,7 +211,7 @@ const ExpressCheckout = () => {
 						<li className="express-checkout has-icon-border">
 							<div className="express-checkout__checkbox loadable-checkbox label-hidden">
 								<CheckboxControl
-									label={ 'Stripe Link' }
+									label={ 'Link by Stripe' }
 									checked={ isStripeLinkEnabled }
 									onChange={ updateStripeLinkCheckout }
 								/>
@@ -220,7 +221,7 @@ const ExpressCheckout = () => {
 								<div className="express-checkout__label-container">
 									<div className="express-checkout__label">
 										{ __(
-											'Stripe Link',
+											'Link by Stripe',
 											'woocommerce-payments'
 										) }
 									</div>
@@ -229,9 +230,11 @@ const ExpressCheckout = () => {
 											/* eslint-disable jsx-a11y/anchor-has-content */
 											interpolateComponents( {
 												mixedString: __(
-													'By enabling this feature, you agree to the ' +
-														'{{stripeLinkTerms}}Link Terms{{/stripeLinkTerms}}, ' +
-														'and {{privacyPolicy}}Privacy Policy{{/privacyPolicy}}',
+													'Link autofills your customers’ payment and shipping details to ' +
+														'deliver an easy and seamless checkout experience. ' +
+														'By enabling this feature, you agree to the ' +
+														'{{stripeLinkTerms}}Link by Stripe terms{{/stripeLinkTerms}}, ' +
+														'and {{privacyPolicy}}Privacy Policy{{/privacyPolicy}}.',
 													'woocommerce-payments'
 												),
 												components: {
@@ -247,6 +250,27 @@ const ExpressCheckout = () => {
 															target="_blank"
 															rel="noreferrer"
 															href="https://link.co/privacy"
+														/>
+													),
+												},
+											} )
+											/* eslint-enable jsx-a11y/anchor-has-content */
+										}
+										<br />
+										{
+											/* eslint-disable jsx-a11y/anchor-has-content */
+											interpolateComponents( {
+												mixedString: __(
+													'{{linkDocs}}Link docs support article.{{/linkDocs}}',
+													'woocommerce-payments'
+												),
+												components: {
+													linkDocs: (
+														<a
+															target="_blank"
+															rel="noreferrer"
+															/* eslint-disable-next-line max-len */
+															href="https://woocommerce.com/document/payments/woocommerce-payments-stripe-link/"
 														/>
 													),
 												},
