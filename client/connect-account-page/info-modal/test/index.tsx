084c28f5a8bea69d3d296d301b2532959b326cfa
Daniel Mallory <daniel.mallory@automattic.com>
2022-10-14 11:26:29 +0100

    Remove unused variables

--- client/connect-account-page/info-modal/test/index.tsx
+++ client/connect-account-page/info-modal/test/index.tsx
@@ -2,8 +2,7 @@
  * External dependencies
  */
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import user from '@testing-library/user-event';
+import { render } from '@testing-library/react';
 
 /**
  * Internal dependencies
