1cc303e839b309d3f40644fc144d6d31ec4abfd0
Ahmed <ahmed.el.azzabi@automattic.com>
2022-10-18 11:42:08 +0100

    pass paged, per_page, order_by, and order to server side authorizations query

--- client/data/authorizations/hooks.ts
+++ client/data/authorizations/hooks.ts
@@ -14,22 +14,41 @@ import {
 	Authorizations,
 } from 'wcpay/types/authorizations';
 
-export const useAuthorizations = ( {}: Query ): Authorizations =>
+export const useAuthorizations = ( {
+		paged,
+		per_page,
+		orderby,
+		order,
+	}: Query, ): Authorizations =>
 	useSelect( ( select ) => {
 		const {
 			getAuthorizations,
 			getAuthorizationsError,
 			isResolving,
-		} = select( STORE_NAME );
-
-		const query = {};
+		} = select(STORE_NAME);
+		
+		const query = {
+			paged: Number.isNaN( parseInt( paged ?? '', 10 ) )
+				? '1'
+				: paged,
+			per_page: Number.isNaN( parseInt( per_page ?? '', 10 ) )
+				? '25'
+				: per_page,
+			orderby: orderby || 'created',
+			order: order || 'desc',
+		};
 
 		return {
 			authorizations: getAuthorizations( query ),
 			authorizationsError: getAuthorizationsError( query ),
 			isLoading: isResolving( 'getAuthorizations', [ query ] ),
 		};
-	}, [] );
+	}, [
+		paged,
+		per_page,
+		orderby,
+		order
+	]);
 
 export const useAuthorizationsSummary = ( {}: Query ): {
 	authorizationsSummary: AuthorizationsSummary;
