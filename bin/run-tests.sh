187d9671161eb7f7ec10138d7af72d02985b1936
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2022-10-17 22:41:32 -0300

    Add watch flag to run-tests script
    phpunit-watcher is used instead of regular phpunit
    in case of passing -w flag as a parameter.
    
    New test:php-watch npm script added to package.json

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -2,15 +2,37 @@
 
 set -e
 
+WATCH_FLAG=false
+
+while getopts ':w' OPTION; do
+	case $OPTION in
+		w)
+		WATCH_FLAG=true
+		;;
+	esac
+done
+
+shift "$(($OPTIND -1))"
+
 echo "Installing the test environment..."
 
 docker-compose exec -u www-data wordpress \
 	/var/www/html/wp-content/plugins/woocommerce-payments/bin/install-wp-tests.sh
 
-echo "Running the tests..."
+if $WATCH_FLAG; then
+	echo "Running the tests..."
 
-docker-compose exec -u www-data wordpress \
-	php -d xdebug.remote_autostart=on \
-	/var/www/html/wp-content/plugins/woocommerce-payments/vendor/bin/phpunit \
-	--configuration /var/www/html/wp-content/plugins/woocommerce-payments/phpunit.xml.dist \
-	$*
+	docker-compose exec -u www-data wordpress \
+		php -d xdebug.remote_autostart=on \
+		/var/www/html/wp-content/plugins/woocommerce-payments/vendor/bin/phpunit \
+		--configuration /var/www/html/wp-content/plugins/woocommerce-payments/phpunit.xml.dist \
+		$*
+else
+	echo "Running the tests on watch mode..."
+
+	docker-compose exec -u www-data wordpress \
+		php -d xdebug.remote_autostart=on \
+		/var/www/html/wp-content/plugins/woocommerce-payments/vendor/bin/phpunit-watcher watch \
+		--configuration /var/www/html/wp-content/plugins/woocommerce-payments/phpunit.xml.dist \
+		$*
+fi
