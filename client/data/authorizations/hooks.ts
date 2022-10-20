a6ba3197a161ad2880342caa5f1161d13a73b45c
Ahmed <ahmed.el.azzabi@automattic.com>
2022-10-20 09:12:38 +0100

    Adjust useAuthorizations hook to take advantage of filters
    This will push the function to retry whenever a filter changes

--- client/data/authorizations/hooks.ts
+++ client/data/authorizations/hooks.ts
@@ -14,22 +14,40 @@ import {
 	Authorizations,
 } from 'wcpay/types/authorizations';
 
-export const useAuthorizations = ( {}: Query ): Authorizations =>
-	useSelect( ( select ) => {
-		const {
-			getAuthorizations,
-			getAuthorizationsError,
-			isResolving,
-		} = select( STORE_NAME );
+export const useAuthorizations = ( {
+	paged,
+	// eslint-disable-next-line @typescript-eslint/naming-convention
+	per_page,
+	orderby,
+	order,
+}: Query ): Authorizations =>
+	useSelect(
+		( select ) => {
+			const {
+				getAuthorizations,
+				getAuthorizationsError,
+				isResolving,
+			} = select( STORE_NAME );
 
-		const query = {};
+			const query = {
+				paged: Number.isNaN( parseInt( paged ?? '', 10 ) )
+					? '1'
+					: paged,
+				per_page: Number.isNaN( parseInt( per_page ?? '', 10 ) )
+					? '25'
+					: per_page,
+				orderby: orderby || 'created',
+				order: order || 'desc',
+			};
 
-		return {
-			authorizations: getAuthorizations( query ),
-			authorizationsError: getAuthorizationsError( query ),
-			isLoading: isResolving( 'getAuthorizations', [ query ] ),
-		};
-	}, [] );
+			return {
+				authorizations: getAuthorizations( query ),
+				authorizationsError: getAuthorizationsError( query ),
+				isLoading: isResolving( 'getAuthorizations', [ query ] ),
+			};
+		},
+		[ paged, per_page, orderby, order ]
+	);
 
 export const useAuthorizationsSummary = ( {}: Query ): {
 	authorizationsSummary: AuthorizationsSummary;
