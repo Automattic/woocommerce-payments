dffdaf56dc07e1a3f160a80eebbf81d40adc5c67
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2022-10-20 21:33:31 -0300

    Fix PHP unit tests watcher script
    Update tests documentation to include new npm script.

--- bin/run-tests.sh
+++ bin/run-tests.sh
@@ -8,11 +8,10 @@ while getopts ':w' OPTION; do
 	case $OPTION in
 		w)
 		WATCH_FLAG=true
+		shift
 		;;
 	esac
 done
-
-shift "$(($OPTIND -1))"
 
 echo "Installing the test environment..."
 
@@ -20,19 +19,19 @@ docker-compose exec -u www-data wordpress \
 	/var/www/html/wp-content/plugins/woocommerce-payments/bin/install-wp-tests.sh
 
 if $WATCH_FLAG; then
+	echo "Running the tests on watch mode..."
+
+	# Change directory to WooCommerce Payments' root in order to have access to .phpunit-watcher.yml
+	docker-compose exec -u www-data wordpress bash -c \
+		"cd /var/www/html/wp-content/plugins/woocommerce-payments && \
+		php -d xdebug.remote_autostart=on \
+		./vendor/bin/phpunit-watcher watch --configuration ./phpunit.xml.dist $*"
+else
 	echo "Running the tests..."
 
 	docker-compose exec -u www-data wordpress \
 		php -d xdebug.remote_autostart=on \
 		/var/www/html/wp-content/plugins/woocommerce-payments/vendor/bin/phpunit \
-		--configuration /var/www/html/wp-content/plugins/woocommerce-payments/phpunit.xml.dist \
-		$*
-else
-	echo "Running the tests on watch mode..."
-
-	docker-compose exec -u www-data wordpress \
-		php -d xdebug.remote_autostart=on \
-		/var/www/html/wp-content/plugins/woocommerce-payments/vendor/bin/phpunit-watcher watch \
 		--configuration /var/www/html/wp-content/plugins/woocommerce-payments/phpunit.xml.dist \
 		$*
 fi
