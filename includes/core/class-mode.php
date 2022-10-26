aeb5a1910c21e05d6c1916a2c029c7e0d2359021
Radoslav Georgiev <rageorgiev@gmail.com>
2022-10-26 10:15:30 +0300

    Adding PHPDocs

--- includes/core/class-mode.php
+++ includes/core/class-mode.php
@@ -63,6 +63,38 @@ class Mode {
 	}
 
 	/**
+	 * Initializes the working mode of WooCommerce Payments.
+	 */
+	private function init() {
+		$dev_mode = (
+			// Plugin-specific dev mode.
+			$this->is_wcpay_dev_mode_defined()
+
+			// WordPress Dev Environment.
+			|| in_array( $this->get_wp_environment_type(), self::DEV_MODE_ENVIRONMENTS, true )
+		);
+
+		/**
+		 * Allows WooCommerce to enter dev mode.
+		 *
+		 * @see https://woocommerce.com/document/payments/testing/dev-mode/
+		 * @param bool $dev_mode The pre-determined dev mode.
+		 */
+		$this->dev_mode = (bool) apply_filters( 'wcpay_dev_mode', $dev_mode );
+
+		$test_mode_setting = 'yes' === $this->gateway->get_option( 'test_mode' );
+		$test_mode         = $this->dev_mode || $test_mode_setting;
+
+		/**
+		 * Allows WooCommerce to enter test mode.
+		 *
+		 * @see https://woocommerce.com/document/payments/testing/
+		 * @param bool $test_mode The pre-determined test mode.
+		 */
+		$this->test_mode = (bool) apply_filters( 'wcpay_test_mode', $test_mode );
+	}
+
+	/**
 	 * Checks whether WCPay is in the given mode.
 	 *
 	 * @param  string $property The mode to check for. Either `live`, `test`, or `dev`.
@@ -120,7 +152,7 @@ class Mode {
 	 *
 	 * @return bool Whether `WCPAY_DEV_MODE` is defined and true.
 	 */
-	protected function is_wcpay_dev_mode_defined() {
+	protected function is_wcpay_dev_mode_defined() : bool {
 		return(
 			defined( 'WCPAY_DEV_MODE' )
 			&& WCPAY_DEV_MODE
@@ -130,30 +162,11 @@ class Mode {
 	/**
 	 * Returns the current WP environment type.
 	 *
-	 * @return string
+	 * @return string|null
 	 */
 	protected function get_wp_environment_type() {
 		return function_exists( 'wp_get_environment_type' )
 			? wp_get_environment_type()
-			: false;
-	}
-
-	/**
-	 * Initializes the working mode of WooCommerce Payments.
-	 */
-	private function init() {
-		$dev_mode = (
-			// Plugin-specific dev mode.
-			$this->is_wcpay_dev_mode_defined()
-
-			// WordPress Dev Environment.
-			|| in_array( $this->get_wp_environment_type(), self::DEV_MODE_ENVIRONMENTS, true )
-		);
-
-		$this->dev_mode = apply_filters( 'wcpay_dev_mode', $dev_mode );
-
-		$gateway_test_mode = 'yes' === $this->gateway->get_option( 'test_mode' );
-		$test_mode         = $this->dev_mode || $gateway_test_mode;
-		$this->test_mode   = apply_filters( 'wcpay_test_mode', $test_mode );
+			: null;
 	}
 }
