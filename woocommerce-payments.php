3a0730e7906ae19967da06308fd6fca587493a89
jbordonado <jbordonado@users.noreply.github.com>
2022-10-26 12:12:28 +0200

    Bump  WC  and WP versions for 5.0.0 release

--- woocommerce-payments.php
+++ woocommerce-payments.php
@@ -8,9 +8,9 @@
  * Woo: 5278104:bf3cf30871604e15eec560c962593c1f
  * Text Domain: woocommerce-payments
  * Domain Path: /languages
- * WC requires at least: 7.2
- * WC tested up to: 7.4.0
- * Requires at least: 6.4
+ * WC requires at least: 7.3
+ * WC tested up to: 7.5.0
+ * Requires at least: 6.5
  * Requires PHP: 7.0
  * Version: 4.9.0
  *
