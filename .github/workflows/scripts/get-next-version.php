ec99ed8909f444722039970a6ca04a2d44171ec6
jbordonado <jbordonado@users.noreply.github.com>
2022-10-27 15:16:26 +0200

    Use a PHP script to get the next version

--- .github/workflows/scripts/get-next-version.php
+++ .github/workflows/scripts/get-next-version.php
@@ -6,6 +6,7 @@
  */
 
 // phpcs:disable WordPress.Security.EscapeOutput.OutputNotEscaped
+// phpcs:disable WordPress.NamingConventions.ValidVariableName.InterpolatedVariableNotSnakeCase
 
 $latest_version_with_release = getenv( 'RELEASE_VERSION' );
 if ( empty( $latest_version_with_release ) ) {
@@ -17,5 +18,5 @@ if ( empty( $latest_version_with_release ) ) {
 $latest_version_as_float = (float) $latest_version_with_release;
 $branch_major_minor      = number_format( $latest_version_as_float + 0.1, 1 );
 $next_version            = $branch_major_minor . '0';
-echo "Next release version: $next_version" >> '$GITHUB_STEP_SUMMARY';
+echo "Next release version: $next_version" >> "$GITHUB_STEP_SUMMARY";
 echo "::set-output name=NEXT_RELEASE_VERSION::$next_version" . PHP_EOL;
