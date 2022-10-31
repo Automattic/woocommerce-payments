7a3b676180274301f0aee8d9e6db1ab959514dd5
oaratovskyi <oleksandr.aratovskyi@a8c.com>
2022-10-31 15:44:16 +0200

    Fix php failing test

--- tests/unit/wc-payment-api/test-class-wc-payments-api-client.php
+++ tests/unit/wc-payment-api/test-class-wc-payments-api-client.php
@@ -734,25 +734,26 @@ class WC_Payments_API_Client_Test extends WCPAY_UnitTestCase {
 				),
 				wp_json_encode(
 					[
-						'test_mode'           => false,
-						'return_url'          => 'http://localhost',
-						'business_data'       => [
+						'test_mode'            => false,
+						'return_url'           => 'http://localhost',
+						'business_data'        => [
 							'a' => 1,
 							'b' => 2,
 							'c' => 3,
 						],
-						'site_data'           => [
+						'site_data'            => [
 							'site_username' => 'admin',
 							'site_locale'   => 'en_US',
 						],
-						'create_live_account' => true,
-						'actioned_notes'      => [
+						'create_live_account'  => true,
+						'actioned_notes'       => [
 							'd' => 4,
 							'e' => 5,
 							'f' => 6,
 						],
-						'progressive'         => false,
-						'account_data'        => [],
+						'progressive'          => false,
+						'progressive_complete' => false,
+						'account_data'         => [],
 					]
 				),
 				true,
