dbaf7f9eda10f6de6aec4722e067101da5b8d0b2
Ahmed <ahmed.el.azzabi@automattic.com>
2022-11-08 17:05:12 +0100

    Expose isAuthAndCaptureEnabled from WP

--- includes/class-wc-payments-features.php
+++ includes/class-wc-payments-features.php
@@ -155,6 +155,7 @@ class WC_Payments_Features {
 				'platformCheckout'        => self::is_platform_checkout_eligible(),
 				'documents'               => self::is_documents_section_enabled(),
 				'customDepositSchedules'  => self::is_custom_deposit_schedules_enabled(),
+				'isAuthAndCaptureEnabled' => self::is_auth_and_capture_enabled(),
 			]
 		);
 	}
