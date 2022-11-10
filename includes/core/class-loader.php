ccb41813703ec2029060badd94afbe525120de04
Zvonimir Maglica <zvonimirmaglica4@gmail.com>
2022-11-10 13:30:38 +0100

    Applied WPCS to folders

--- includes/core/class-loader.php
+++ includes/core/class-loader.php
@@ -7,17 +7,17 @@
 
 /** Load contracts. */
 
-require_once __DIR__ . '/Contracts/Server/Request/class-base-request-interface.php';
-require_once __DIR__ . '/Contracts/Server/Response/class-base-response-interface.php';
+require_once __DIR__ . '/contracts/server/request/class-base-request-interface.php';
+require_once __DIR__ . '/contracts/server/response/class-base-response-interface.php';
 
 
 /** Load enums. */
 
-require_once __DIR__ . '/Enums/class-endpoints.php';
+require_once __DIR__ . '/enums/class-endpoints.php';
 
 /** Load Core Server files */
 
-require_once __DIR__ . '/Server/Request/class-base-request.php';
-require_once __DIR__ . '/Server/Request/class-create-charge.php';
-require_once __DIR__ . '/Server/Request/class-create-intention.php';
-require_once __DIR__ . '/Server/Response/class-response.php';
+require_once __DIR__ . '/server/request/class-base-request.php';
+require_once __DIR__ . '/server/request/class-create-charge.php';
+require_once __DIR__ . '/server/request/class-create-intention.php';
+require_once __DIR__ . '/server/response/class-response.php';
