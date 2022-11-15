b5a6637bf6808273133b4f6d2207cbb044800328
Jessy P <jessy.pappachan@automattic.com>
2022-11-15 13:20:34 +0530

    Tests modified

--- tests/unit/core/test-class-wc-payments-customer-service-api.php
+++ tests/unit/core/test-class-wc-payments-customer-service-api.php
@@ -31,35 +31,29 @@ class WC_Payments_Customer_Service_API_Test extends WCPAY_UnitTestCase {
 	private $customer_service;
 
 	/**
-	 * Mock WC_Payments_API_Client.
+	 * Mock WC_Payments_Http.
 	 *
-	 * @var WC_Payments_API_Client|MockObject
+	 * @var WC_Payments_Http|MockObject
 	 */
-	private $mock_api_client;
-
-	/**
-	 * Mock WC_Payments_Account.
-	 *
-	 * @var WC_Payments_Account|MockObject
-	 */
-	private $mock_account;
-
-	/**
-	 * Mock Database_Cache.
-	 *
-	 * @var Database_Cache|MockObject
-	 */
-	private $mock_db_cache;
+	private $mock_http_client;
 
 
 	/**
 	 * Pre-test setup
 	 */
 	public function set_up() {
-		$this->mock_api_client      = $this->createMock( WC_Payments_API_Client::class );
-		$this->mock_account         = $this->createMock( WC_Payments_Account::class );
-		$this->mock_db_cache        = $this->createMock( Database_Cache::class );
-		$this->customer_service     = new WC_Payments_Customer_Service( $this->mock_api_client, $this->mock_account, $this->mock_db_cache );
+		$this->mock_http_client = $this
+			->getMockBuilder( 'WC_Payments_Http' )
+			->disableOriginalConstructor()
+			->setMethods( [ 'get_blog_id', 'is_connected', 'remote_request' ] )
+			->getMock();
+		add_filter(
+			'wc_payments_http',
+			function() {
+				return $this->mock_http_client;
+			}
+		);
+		$this->customer_service     = new WC_Payments_Customer_Service( WC_Payments::create_api_client(), WC_Payments::get_account_service(), WC_Payments::get_database_cache() );
 		$this->customer_service_api = new WC_Payments_Customer_Service_API( $this->customer_service );
 		parent::set_up();
 	}
@@ -76,62 +70,69 @@ class WC_Payments_Customer_Service_API_Test extends WCPAY_UnitTestCase {
 	 */
 	public function test_get_customer_id_by_user_id() {
 		update_user_option( 1, self::CUSTOMER_LIVE_META_KEY, 'cus_test12345' );
-
 		$customer_id = $this->customer_service_api->get_customer_id_by_user_id( 1 );
-
 		$this->assertEquals( 'cus_test12345', $customer_id );
 	}
 
+	public function test_get_customer_id_by_user_id_with_null_user_id() {
+		$customer_id = $this->customer_service_api->get_customer_id_by_user_id( null );
+		$this->assertEquals( null, $customer_id );
+	}
+
 		/**
-		 * Test create customer for user.
+		 * Test create customer for user for live mode.
 		 *
 		 * @throws API_Exception
 		 */
 	public function test_create_customer_for_user() {
 		$user             = new WP_User( 1 );
 		$user->user_login = 'testUser';
+		$this->mock_http_client->expects( $this->once() )
+			->method( 'remote_request' )
+			->willReturn(
+				[
+					'body'     =>
+					json_encode(
+						[
+							'id'   => 'cus_test12345',
+							'type' => 'customer',
+						]
+					),
+					'response' => [
+						'code'    => 200,
+						'message' => 'OK',
+					],
+				]
+			);
 
-		$mock_customer_data = [
-			'name'        => 'Test Name',
-			'description' => 'Name: Test Name, Guest',
-			'email'       => 'test@customer.email',
-			'phone'       => '123456',
-			'address'     => [
-				'line1'       => '1 Street St',
-				'line2'       => '',
-				'postal_code' => '09876',
-				'city'        => 'City',
-				'state'       => 'State',
-				'country'     => 'US',
-			],
-			'shipping'    => [
-				'name'    => 'Shipping Ship',
-				'address' => [
-					'line1'       => '2 Street St',
+		$customer_id = $this->customer_service->create_customer_for_user(
+			$user,
+			[
+				'name'        => 'Test Name',
+				'description' => 'Name: Test Name, Guest',
+				'email'       => 'test@customer.email',
+				'phone'       => '123456',
+				'address'     => [
+					'line1'       => '1 Street St',
 					'line2'       => '',
-					'postal_code' => '76543',
-					'city'        => 'City2',
-					'state'       => 'State2',
+					'postal_code' => '09876',
+					'city'        => 'City',
+					'state'       => 'State',
 					'country'     => 'US',
 				],
-			],
-		];
-
-		$this->mock_account->expects( $this->once() )
-			->method( 'get_fraud_services_config' )
-			->willReturn( [ 'sift' => [ 'session_id' => 'woo_session_id' ] ] );
-
-		$this->mock_api_client->expects( $this->once() )
-			->method( 'create_customer' )
-			->with(
-				array_merge(
-					$mock_customer_data,
-					[ 'session_id' => 'woo_session_id' ]
-				)
-			)
-			->willReturn( 'cus_test12345' );
-
-		$customer_id = $this->customer_service_api->create_customer_for_user( $user, $mock_customer_data );
+				'shipping'    => [
+					'name'    => 'Shipping Ship',
+					'address' => [
+						'line1'       => '2 Street St',
+						'line2'       => '',
+						'postal_code' => '76543',
+						'city'        => 'City2',
+						'state'       => 'State2',
+						'country'     => 'US',
+					],
+				],
+			]
+		);
 
 		$this->assertEquals( 'cus_test12345', $customer_id );
 		$this->assertEquals( 'cus_test12345', get_user_option( self::CUSTOMER_LIVE_META_KEY, $user->ID ) );
