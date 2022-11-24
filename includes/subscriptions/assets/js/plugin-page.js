7896b5a48698ed615c031337ae91b0b8dfb76900
Rua Haszard <rua@automattic.com>
2022-11-24 13:00:42 +1300

    minor refactor to rename various functions to scope to WCS: - WCPay-specific warning message coming soon to this file - Also added comments to clarify

--- includes/subscriptions/assets/js/plugin-page.js
+++ includes/subscriptions/assets/js/plugin-page.js
@@ -3,27 +3,35 @@ jQuery( function ( $ ) {
 
 	var wc_payments_plugin = {
 		init: function () {
+			this.init_deactivate_wc_subscriptions_warning();
+		},
+
+		// Initialise handlers for WC Subscriptions deactivate warning.
+		init_deactivate_wc_subscriptions_warning() {
+			// Intercept click on WCS deactivate link to show modal.
 			$( '#deactivate-woocommerce-subscriptions' ).on(
 				'click',
-				this.display_modal
+				this.display_wcs_warning
 			);
+			// Resume deactivate when user confirms modal.
 			$( document ).on(
 				'click',
 				'#wcpay-subscriptions-plugin-deactivation-submit',
-				this.deactivate_subscriptions
+				this.redirect_deactivate_wc_subscriptions
 			);
 		},
-		display_modal: function ( event ) {
+		// Show a modal to warn merchant that disabling WC Subscriptions plugin will switch to WCPay.
+		display_wcs_warning: function ( event ) {
 			event.preventDefault();
 
-			// Display modal
 			$( this ).WCBackboneModal( {
 				template: 'wcpay-subscriptions-plugin-warning',
 			} );
 
 			return false;
 		},
-		deactivate_subscriptions: function ( event ) {
+		// Trigger deactivate flow for WC Subscriptions.
+		redirect_deactivate_wc_subscriptions: function ( event ) {
 			$( '#wcpay-subscriptions-plugin-deactivation-submit' ).addClass(
 				'busy'
 			);
