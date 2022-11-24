9c47842cb6d1b634ce4db7de0a2d7a432d20a085
Jessy P <jessy.pappachan@automattic.com>
2022-11-24 13:25:59 +0530

    Added tests

--- tests/unit/core/test-class-wc-payments-order.php
+++ tests/unit/core/test-class-wc-payments-order.php
@@ -12,8 +12,40 @@ use WCPay\Core\WC_Payments_Order;
  */
 class WC_Payments_Order_Test extends WCPAY_UnitTestCase {
 
+	public function test_set_payment_intent_id() {
+		$wcpay_order = new WC_Payments_Order( WC_Helper_Order::create_order() );
+		$intent_id   = 'pi_mock_123';
+		$wcpay_order->set_payment_intent_id( $intent_id );
+		$this->assertEquals( $wcpay_order->get_wc_order()->get_meta( '_intent_id', true ), $intent_id );
+		$this->assertSame( 1, did_action( 'wcpay_payment_intent_id_updated' ) );
+		$this->assertSame( 0, did_action( 'wcpay_payment_method_id_updated' ) );
+	}
+
+	public function test_get_payment_intent_id() {
+		$intent_id   = 'pi_mock';
+		$wcpay_order = new WC_Payments_Order( WC_Helper_Order::create_order() );
+		$wcpay_order->get_wc_order()->update_meta_data( '_intent_id', $intent_id );
+		$this->assertEquals( $wcpay_order->get_payment_intent_id(), $intent_id );
+	}
+
+	public function test_set_payment_method_id() {
+		$wcpay_order    = new WC_Payments_Order( WC_Helper_Order::create_order() );
+		$payment_method = 'pm_mock';
+		$wcpay_order->set_payment_method_id( $payment_method );
+		$this->assertEquals( $wcpay_order->get_wc_order()->get_meta( '_payment_method_id', true ), $payment_method );
+		$this->assertSame( 0, did_action( 'wcpay_payment_intent_id_updated' ) );
+		$this->assertSame( 1, did_action( 'wcpay_payment_method_id_updated' ) );
+	}
+
+	public function test_get_payment_method_id() {
+		$payment_method_id = 'pm_mock_123';
+		$wcpay_order       = new WC_Payments_Order( WC_Helper_Order::create_order() );
+		$wcpay_order->get_wc_order()->update_meta_data( '_payment_method_id', $payment_method_id );
+		$this->assertEquals( $wcpay_order->get_payment_method_id(), $payment_method_id );
+	}
+
 	public function test_attach_intent_info_to_order() {
-		$order = WC_Helper_Order::create_order();
+		$order          = WC_Helper_Order::create_order();
 		$wcpay_order    = new WC_Payments_Order( $order );
 		$intent_id      = 'pi_mock';
 		$intent_status  = 'succeeded';
