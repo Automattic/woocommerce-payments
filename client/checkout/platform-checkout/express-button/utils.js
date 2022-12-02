8acbebbdf189bfa1604a720ae5893393d5307489
Brian Borman <brian.borman@automattic.com>
2022-12-02 14:13:15 -0500

    Check for undefined params before using

--- client/checkout/platform-checkout/express-button/utils.js
+++ client/checkout/platform-checkout/express-button/utils.js
@@ -7,5 +7,9 @@
  * @return {*}         The value of the parameter of null.
  */
 export const getWooPayExpressData = ( name ) => {
+	if ( 'undefined' === typeof wcpayWooPayExpressParams ) {
+		return undefined;
+	}
+
 	return wcpayWooPayExpressParams?.[ name ];
 };
