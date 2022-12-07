df598f82738a922afc43a9911fbe0e485d620d55
Miguel Gasca <miguel.gasca@automattic.com>
2022-12-07 20:06:33 +0100

    PR feedback

--- client/payment-details/summary/index.tsx
+++ client/payment-details/summary/index.tsx
@@ -114,9 +114,12 @@ const PaymentDetailsSummary = ( {
 	} = useContext( WCPaySettingsContext );
 
 	// We should only fetch the authorization data if the payment is marked for manual capture and it is not already captured.
+	// We also need to exclude failed payments and payments that have been refunded, because capture === false in those cases, even
+	// if the capture is automatic.
 	const shouldFetchAuthorization =
-		'captured' in charge && // captured key is only present for manual capture payments. See https://stripe.com/docs/api/charges/object#charge_object-captured
 		! charge.captured &&
+		charge.status !== 'failed' &&
+		charge.amount_refunded === 0 &&
 		isAuthAndCaptureEnabled;
 
 	const { authorization } = useAuthorization(
