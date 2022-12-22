67a1dc245ea5f68bf891e7e7885dbd70020940d9
Nagesh Pai <nagesh.pai@automattic.com>
2022-12-22 17:14:50 +0530

    Change path in WC_Payments class file

--- includes/class-wc-payments.php
+++ includes/class-wc-payments.php
@@ -282,7 +282,7 @@ class WC_Payments {
 		include_once __DIR__ . '/exceptions/class-invalid-webhook-data-exception.php';
 		include_once __DIR__ . '/exceptions/class-invalid-price-exception.php';
 		include_once __DIR__ . '/compat/class-wc-payment-woo-compat-utils.php';
-		include_once __DIR__ . '/constants/class-order-statuses.php';
+		include_once __DIR__ . '/constants/class-order-status.php';
 		include_once __DIR__ . '/constants/class-payment-type.php';
 		include_once __DIR__ . '/constants/class-payment-initiated-by.php';
 		include_once __DIR__ . '/constants/class-payment-capture-type.php';
