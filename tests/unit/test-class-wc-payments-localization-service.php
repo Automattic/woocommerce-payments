c6489b24e855dc845ec7827ab6711ece4e567e54
Allie Mims <allie.mims@automattic.com>
2022-12-22 09:27:53 -0500

    Update mock time to address failing unit tests

--- tests/unit/test-class-wc-payments-localization-service.php
+++ tests/unit/test-class-wc-payments-localization-service.php
@@ -179,7 +179,7 @@ class WC_Payments_Localization_Service_Test extends WCPAY_UnitTestCase {
 		add_filter(
 			'wcpay_mock_time',
 			function() {
-				return strtotime( '2022-12-31 23:59:59' );
+				return strtotime( '2022-12-31 23:49:49' );
 			}
 		);
 		$this->localization_service = new WC_Payments_Localization_Service();
