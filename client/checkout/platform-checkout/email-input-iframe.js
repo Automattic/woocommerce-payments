ad50d63d7b4d2a2b820ec91763666a1c417b798c
Mayisha <mayisha007@gmail.com>
2023-01-24 19:30:02 +0600

    remove duplicate functions to use `getTargetElement` fron util

--- client/checkout/platform-checkout/email-input-iframe.js
+++ client/checkout/platform-checkout/email-input-iframe.js
@@ -7,35 +7,7 @@ import wcpayTracks from 'tracks';
 import request from '../utils/request';
 import showErrorCheckout from '../utils/show-error-checkout';
 import { buildAjaxURL } from '../../payment-request/utils';
-
-// Waits for the element to exist as in the Blocks checkout, sometimes the field is not immediately available.
-const waitForElement = ( selector ) => {
-	return new Promise( ( resolve ) => {
-		if ( document.querySelector( selector ) ) {
-			return resolve( document.querySelector( selector ) );
-		}
-
-		const checkoutBlock = document.querySelector(
-			'[data-block-name="woocommerce/checkout"]'
-		);
-
-		if ( ! checkoutBlock ) {
-			return resolve( null );
-		}
-
-		const observer = new MutationObserver( ( mutationList, obs ) => {
-			if ( document.querySelector( selector ) ) {
-				resolve( document.querySelector( selector ) );
-				obs.disconnect();
-			}
-		} );
-
-		observer.observe( checkoutBlock, {
-			childList: true,
-			subtree: true,
-		} );
-	} );
-};
+import { getTargetElement } from './utils';
 
 export const handlePlatformCheckoutEmailInput = async (
 	field,
@@ -44,7 +16,7 @@ export const handlePlatformCheckoutEmailInput = async (
 ) => {
 	let timer;
 	const waitTime = 500;
-	const platformCheckoutEmailInput = await waitForElement( field );
+	const platformCheckoutEmailInput = await getTargetElement( field );
 	let hasCheckedLoginSession = false;
 
 	// If we can't find the input, return.
@@ -303,7 +275,7 @@ export const handlePlatformCheckoutEmailInput = async (
 	} );
 
 	if ( isBlocksCheckout ) {
-		const formSubmitButton = await waitForElement(
+		const formSubmitButton = await getTargetElement(
 			'button.wc-block-components-checkout-place-order-button'
 		);
 		formSubmitButton.addEventListener( 'click', () => {
