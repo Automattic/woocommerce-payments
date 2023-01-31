c85a1d2d65f66ef96972b82bbabf2e31d24e8599
Allie Mims <allie.mims@automattic.com>
2023-01-31 09:39:46 -0500

    Consolidate notice components

--- client/settings/fraud-protection/index.js
+++ client/settings/fraud-protection/index.js
@@ -71,7 +71,27 @@ const ExceedsDollarAmountRuleNotUSDStore = ( props ) => {
 	);
 };
 
-const StandardNotice = () => {
+const ProtectionLevelModalNotice = ( props ) => {
+	if ( 'high' === props.level ) {
+		return (
+			<Notice
+				className="component-notice--is-info"
+				status="info"
+				isDismissible={ false }
+			>
+				<div className="component-notice__content--flex">
+					<TipIcon className="component-notice__icon" />
+					<p>
+						{ __(
+							'Offers the highest level of filtering for stores, but may catch some legitimate transactions',
+							'woocommerce-payments'
+						) }
+					</p>
+				</div>
+			</Notice>
+		);
+	}
+
 	return (
 		<Notice
 			className="component-notice--is-info"
@@ -83,26 +103,6 @@ const StandardNotice = () => {
 				<p>
 					{ __(
 						"Provides a standard level of filtering that's suitable for most business.",
-						'woocommerce-payments'
-					) }
-				</p>
-			</div>
-		</Notice>
-	);
-};
-
-const HighNotice = () => {
-	return (
-		<Notice
-			className="component-notice--is-info"
-			status="info"
-			isDismissible={ false }
-		>
-			<div className="component-notice__content--flex">
-				<TipIcon className="component-notice__icon" />
-				<p>
-					{ __(
-						'Offers the highest level of filtering for stores, but may catch some legitimate transactions',
 						'woocommerce-payments'
 					) }
 				</p>
@@ -163,6 +163,7 @@ const ProtectionLevels = () => {
 								</p>
 								<HelpOutlineIcon
 									size={ 18 }
+									title="Standard level help icon"
 									onClick={ () =>
 										setStandardModalOpen( true )
 									}
@@ -182,7 +183,7 @@ const ProtectionLevels = () => {
 										className="fraud-protection-level-modal"
 									>
 										<div className="components-modal__body--fraud-protection">
-											<StandardNotice />
+											<ProtectionLevelModalNotice level="standard" />
 											<p>
 												{ __(
 													'Payments will be ',
@@ -312,6 +313,7 @@ const ProtectionLevels = () => {
 								</p>
 								<HelpOutlineIcon
 									size={ 18 }
+									title="High level help icon"
 									onClick={ () => setHighModalOpen( true ) }
 								/>
 								{ isHighModalOpen && (
@@ -329,7 +331,7 @@ const ProtectionLevels = () => {
 										className="fraud-protection-level-modal"
 									>
 										<div className="components-modal__body--fraud-protection">
-											<HighNotice />
+											<ProtectionLevelModalNotice level="high" />
 											<p>
 												{ __(
 													'Payments will be ',
