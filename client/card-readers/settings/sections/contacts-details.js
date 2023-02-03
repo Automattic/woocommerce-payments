404fde21f38f129b3db6226a442168809e38020a
Dat Hoang <htdat@users.noreply.github.com>
2023-02-03 20:33:28 +0700

    Update Card Reader settings components

--- client/card-readers/settings/sections/contacts-details.js
+++ client/card-readers/settings/sections/contacts-details.js
@@ -9,15 +9,23 @@ import { __ } from '@wordpress/i18n';
  */
 import SupportPhoneInput from 'wcpay/settings/support-phone-input';
 import SupportEmailInput from 'wcpay/settings/support-email-input';
+import React, { useEffect, useState } from 'react';
 
-const ContactDetailsSection = ( { setSaveDisabled } ) => {
+const ContactDetailsSection = ( { setInputsValid } ) => {
+	const [ isEmailInputValid, setEmailInputValid ] = useState( true );
+	const [ isPhoneInputValid, setPhoneInputValid ] = useState( true );
+
+	useEffect( () => {
+		setInputsValid( isEmailInputValid && isPhoneInputValid );
+	}, [ isEmailInputValid, isPhoneInputValid, setInputsValid ] );
+
 	return (
 		<>
 			<h4>
 				{ __( 'Customer support contacts', 'woocommerce-payments' ) }
 			</h4>
-			<SupportEmailInput onErrorMessage={ setSaveDisabled } />
-			<SupportPhoneInput onErrorMessage={ setSaveDisabled } />
+			<SupportEmailInput setInputVallid={ setEmailInputValid } />
+			<SupportPhoneInput setInputVallid={ setPhoneInputValid } />
 		</>
 	);
 };
