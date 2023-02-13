78ae41fd10d55b5b3d64176422b4904214195820
Allie Mims <allie.mims@automattic.com>
2023-02-13 11:04:35 -0500

    Convert to snapshot tests
    Commit snapshots

--- client/settings/fraud-protection/components/exceeds-dollar-amount-rule/index.test.js
+++ client/settings/fraud-protection/components/exceeds-dollar-amount-rule/index.test.js
@@ -1,7 +1,7 @@
 /**
  * External dependencies
  */
-import { render, screen } from '@testing-library/react';
+import { render } from '@testing-library/react';
 
 /**
  * Internal dependencies
@@ -12,65 +12,49 @@ describe( 'ExceedsDollarAmountRule', () => {
 	it( 'renders the high level dollar amount rule when USD is store currency', () => {
 		const storeCurrency = { name: 'US Dollar', symbol: '$', code: 'USD' };
 
-		render(
+		const { container: highUSDRule } = render(
 			<ExceedsDollarAmountRule
 				level="high"
 				storeCurrency={ storeCurrency }
 			/>
 		);
 
-		const highUSDRule = screen.getByText( /exceeds/ );
-		expect( highUSDRule ).toBeInTheDocument();
-		expect( highUSDRule ).toHaveTextContent(
-			'An order exceeds $1,000.00.'
-		);
+		expect( highUSDRule ).toMatchSnapshot();
 	} );
 	it( 'renders the standard level dollar amount rule when USD is store currency', () => {
 		const storeCurrency = { name: 'US Dollar', symbol: '$', code: 'USD' };
 
-		render(
+		const { container: standardUSDRule } = render(
 			<ExceedsDollarAmountRule
 				level="standard"
 				storeCurrency={ storeCurrency }
 			/>
 		);
 
-		const standardUSDRule = screen.getByText( /exceeds/ );
-		expect( standardUSDRule ).toBeInTheDocument();
-		expect( standardUSDRule ).toHaveTextContent(
-			'An order exceeds $1,000.00 or 10 items.'
-		);
+		expect( standardUSDRule ).toMatchSnapshot();
 	} );
 	it( 'renders the high level dollar amount rule equivalent when USD is not store currency', () => {
 		const storeCurrency = { name: 'Euro', symbol: '€', code: 'EUR' };
 
-		render(
+		const { container: highNonUSDRule } = render(
 			<ExceedsDollarAmountRule
 				level="high"
 				storeCurrency={ storeCurrency }
 			/>
 		);
 
-		const highNonUSDRule = screen.getByText( /exceeds/ );
-		expect( highNonUSDRule ).toBeInTheDocument();
-		expect( highNonUSDRule ).toHaveTextContent(
-			'An order exceeds the equivalent of $1,000.00 USD in Euro.'
-		);
+		expect( highNonUSDRule ).toMatchSnapshot();
 	} );
 	it( 'renders the standard level dollar amount rule equivalent when USD is not store currency', () => {
 		const storeCurrency = { name: 'Euro', symbol: '€', code: 'EUR' };
 
-		render(
+		const { container: standardNonUSDRule } = render(
 			<ExceedsDollarAmountRule
 				level="standard"
 				storeCurrency={ storeCurrency }
 			/>
 		);
 
-		const standardNonUSDRule = screen.getByText( /exceeds/ );
-		expect( standardNonUSDRule ).toBeInTheDocument();
-		expect( standardNonUSDRule ).toHaveTextContent(
-			'An order exceeds the equivalent of $1,000.00 USD in Euro or 10 items.'
-		);
+		expect( standardNonUSDRule ).toMatchSnapshot();
 	} );
 } );
