6be8b5ad0655e25d555f162825f584463f3a33b0
Allie Mims <allie.mims@automattic.com>
2023-02-17 16:01:37 -0500

    Put fraud protection settings behind feature flag Add necessary unit tests

--- client/settings/settings-manager/test/index.test.js
+++ client/settings/settings-manager/test/index.test.js
@@ -13,6 +13,7 @@ import WCPaySettingsContext from '../../wcpay-settings-context';
 describe( 'SettingsManager', () => {
 	it( 'renders the PaymentMethods section if the UPE feature flag is enabled', () => {
 		const context = { featureFlags: { upeSettingsPreview: true } };
+		global.wcpaySettings = {};
 
 		render(
 			<WCPaySettingsContext.Provider value={ context }>
@@ -27,6 +28,7 @@ describe( 'SettingsManager', () => {
 
 	it( 'does not render the PaymentMethods section if the UPE feature flag is disabled', () => {
 		const context = { featureFlags: {} };
+		global.wcpaySettings = {};
 		render(
 			<WCPaySettingsContext.Provider value={ context }>
 				<SettingsManager />
@@ -35,6 +37,38 @@ describe( 'SettingsManager', () => {
 
 		expect(
 			screen.queryByText( 'Payments accepted on checkout' )
+		).not.toBeInTheDocument();
+	} );
+
+	it( 'renders the Fraud Protection settings section if the option flag is enabled', () => {
+		const context = { featureFlags: {} };
+		global.wcpaySettings = {
+			isFraudProtectionSettingsEnabled: true,
+		};
+
+		render(
+			<WCPaySettingsContext.Provider value={ context }>
+				<SettingsManager />
+			</WCPaySettingsContext.Provider>
+		);
+
+		expect( screen.queryByText( 'Fraud Protection' ) ).toBeInTheDocument();
+	} );
+
+	it( 'does not render the Fraud Protection settings section if the option flag is disabled', () => {
+		const context = { featureFlags: {} };
+		global.wcpaySettings = {
+			isFraudProtectionSettingsEnabled: false,
+		};
+
+		render(
+			<WCPaySettingsContext.Provider value={ context }>
+				<SettingsManager />
+			</WCPaySettingsContext.Provider>
+		);
+
+		expect(
+			screen.queryByText( 'Fraud Protection' )
 		).not.toBeInTheDocument();
 	} );
 } );
