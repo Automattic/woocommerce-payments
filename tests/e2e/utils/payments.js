532dd6f8dc79a89bc0678542fd051df30026ccfb
frosso <francesco.rosso@automattic.com>
2023-02-17 17:43:27 -0800

    I think this is it

--- tests/e2e/utils/payments.js
+++ tests/e2e/utils/payments.js
@@ -7,9 +7,13 @@ const { shopper, uiUnblocked } = require( '@woocommerce/e2e-utils' );
 
 // WooCommerce Checkout
 export async function fillCardDetails( page, card ) {
-	if ( await page.$( '#payment .wcpay-upe-element' ) ) {
+	if (
+		await page.$(
+			'#payment .payment_method_woocommerce_payments .wcpay-upe-element'
+		)
+	) {
 		const frameHandle = await page.waitForSelector(
-			'#payment #wcpay-upe-element iframe'
+			'#payment .payment_method_woocommerce_payments .wcpay-upe-element iframe'
 		);
 
 		const stripeFrame = await frameHandle.contentFrame();
