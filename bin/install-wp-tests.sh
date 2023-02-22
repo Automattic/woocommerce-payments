266724b81d7acad4981a1103eb0a936b3a4a377f
Achyuth Ajoy <achyuthajoy@gmail.com>
2023-02-22 09:42:54 +0530

    Enable WP Debug for testing

--- bin/install-wp-tests.sh
+++ bin/install-wp-tests.sh
@@ -130,6 +130,9 @@ configure_wp() {
 
 	if [[ ! -f "$WP_CORE_DIR/wp-config.php" ]]; then
 		wp config create --dbname=$DB_NAME --dbuser=$DB_USER --dbpass=$DB_PASS --dbhost=$DB_HOST --dbprefix=wptests_
+		# Enable WP Debug
+		wp config set WP_DEBUG true
+		wp config set WP_DEBUG_DISPLAY true
 	fi
 
 	# Add db-error.php for detailed logging
