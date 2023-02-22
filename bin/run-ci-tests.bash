cf56e102b4f382fd293b12503e896794786de81b
Achyuth Ajoy <achyuthajoy@gmail.com>
2023-02-22 08:31:31 +0530

    Revert "Replace localhost with 127.0.0.1 for testing"
    This reverts commit 70a4dbe4c46b180bdfe791f1c67443de2df55f7a.

--- bin/run-ci-tests.bash
+++ bin/run-ci-tests.bash
@@ -9,6 +9,6 @@ WCPAY_DIR="$GITHUB_WORKSPACE"
 
 composer self-update 2.0.6 && composer install --no-progress
 sudo systemctl start mysql.service
-bash bin/install-wp-tests.sh woocommerce_test root root 127.0.0.1 $WP_VERSION $WC_VERSION false $GUTENBERG_VERSION
+bash bin/install-wp-tests.sh woocommerce_test root root localhost $WP_VERSION $WC_VERSION false $GUTENBERG_VERSION
 echo 'Running the tests...'
 bash bin/phpunit.sh
