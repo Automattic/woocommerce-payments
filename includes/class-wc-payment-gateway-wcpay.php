277125ea1a8cb72b9ca2afb9ddf60546173b7e2d
Jessy P <jessy.pappachan@automattic.com>
2023-03-03 12:27:11 +0530

    Fixed linting

--- includes/class-wc-payment-gateway-wcpay.php
+++ includes/class-wc-payment-gateway-wcpay.php
@@ -635,12 +635,12 @@ class WC_Payment_Gateway_WCPay extends WC_Payment_Gateway_CC {
 		) {
 			WC_Payments::get_gateway()->tokenization_script();
 			$script_handle = 'WCPAY_CHECKOUT';
-			$js_object = 'wcpayConfig';
+			$js_object     = 'wcpayConfig';
 			if ( WC_Payments_Features::is_upe_split_enabled() ) {
-				$script_handle = 'wcpay-upe-checkout';
-				$js_object = 'wcpay_upe_config';
-			}  elseif ( WC_Payments_Features::is_upe_legacy_enabled() ) {
-				$script_handle = 'wcpay-upe-checkout';
+				  $script_handle = 'wcpay-upe-checkout';
+				  $js_object     = 'wcpay_upe_config';
+			} elseif ( WC_Payments_Features::is_upe_legacy_enabled() ) {
+				 $script_handle = 'wcpay-upe-checkout';
 			}
 			wp_localize_script( $script_handle, $js_object, WC_Payments::get_wc_payments_checkout()->get_payment_fields_js_config() );
 			wp_enqueue_script( $script_handle );
