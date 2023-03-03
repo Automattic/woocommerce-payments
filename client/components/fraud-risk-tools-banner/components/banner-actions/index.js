f572f476d73d298b7b54be06bd436f875e55c7ae
Allie Mims <allie.mims@automattic.com>
2023-03-03 15:09:25 -0500

    Debug stale state issue

--- client/components/fraud-risk-tools-banner/components/banner-actions/index.js
+++ client/components/fraud-risk-tools-banner/components/banner-actions/index.js
@@ -4,14 +4,24 @@
 import { __ } from '@wordpress/i18n';
 import { Button } from '@wordpress/components';
 
-const BannerActions = () => {
+const BannerActions = ( {
+	remindMeCount,
+	handleRemindOnClick,
+	handleDontShowAgainOnClick,
+} ) => {
 	return (
 		<div className="discoverability-card__actions">
 			<Button href="#" isPrimary>
 				{ __( 'Learn More', 'woocommerce-payments' ) }
 			</Button>
-			<p>{ __( 'Remind me later', 'woocommerce-payments' ) }</p>
-			<p>{ __( "Don't show me this again", 'woocommerce-payments' ) }</p>
+			<Button isTertiary onClick={ handleRemindOnClick }>
+				{ __( 'Remind me later', 'woocommerce-payments' ) }
+			</Button>
+			{ 3 === remindMeCount && (
+				<Button isTertiary onClick={ handleDontShowAgainOnClick }>
+					{ __( "Don't show me this again", 'woocommerce-payments' ) }
+				</Button>
+			) }
 		</div>
 	);
 };
--- client/components/fraud-risk-tools-banner/index.js
+++ client/components/fraud-risk-tools-banner/index.js
@@ -9,22 +9,85 @@ import { Card } from '@wordpress/components';
  */
 import { BannerBody, NewPill, BannerActions } from './components';
 import './style.scss';
+import { useEffect, useState } from 'react';
+import { useDispatch } from '@wordpress/data';
 
 const FRTDiscoverabilityBanner = () => {
+	const { frtDiscoverBannerSettings } = wcpaySettings;
+	const { updateOptions } = useDispatch( 'wc/admin/options' );
+	const [ settings, setSettings ] = useState( () => {
+		try {
+			return JSON.parse( frtDiscoverBannerSettings );
+		} catch ( e ) {
+			return { remindMeCount: 0, remindMeAt: null, dontShowAgain: false };
+		}
+	} );
+
+	const showBanner =
+		null === settings.remindMeAt || Date.now() > settings.remindMeAt
+			? true
+			: false;
+
+	const setReminder = () => {
+		// const DAY_IN_MS = 24 * 60 * 60 * 1000;
+		const MIN_IN_MS = 60 * 1000; // Remove after testing. Also switch all references to DAY_IN_MS.
+		const nowTimestamp = Date.now();
+		setSettings( ( prevSettings ) => {
+			return {
+				...prevSettings,
+				remindMeCount: prevSettings.remindMeCount + 1,
+				remindMeAt: nowTimestamp + MIN_IN_MS, // Change to DAY_IN_MS.
+			};
+		} );
+	};
+
+	const setDontShowAgain = () => {
+		setSettings( ( prevSettings ) => {
+			return {
+				...prevSettings,
+				dontShowAgain: true,
+			};
+		} );
+	};
+
+	useEffect( () => {
+		updateOptions( {
+			wcpay_frt_discover_banner_settings: JSON.stringify( settings ),
+		} );
+	}, [ settings, updateOptions ] );
+
+	const handleRemindOnClick = () => {
+		setReminder();
+	};
+
+	const handleDontShowAgainOnClick = () => {
+		setDontShowAgain();
+	};
+
 	return (
-		<Card>
-			<div className="discoverability-card">
-				<NewPill />
-				<h3 className="discoverability-card__header">
-					{ __(
-						'Enhanced fraud protection for your store',
-						'woocommerce-payments'
-					) }
-				</h3>
-				<BannerBody />
-				<BannerActions />
-			</div>
-		</Card>
+		<>
+			{ showBanner && (
+				<Card>
+					<div className="discoverability-card">
+						<NewPill />
+						<h3 className="discoverability-card__header">
+							{ __(
+								'Enhanced fraud protection for your store',
+								'woocommerce-payments'
+							) }
+						</h3>
+						<BannerBody />
+						<BannerActions
+							remindMeCount={ settings.remindMeCount }
+							handleRemindOnClick={ handleRemindOnClick }
+							handleDontShowAgainOnClick={
+								handleDontShowAgainOnClick
+							}
+						/>
+					</div>
+				</Card>
+			) }
+		</>
 	);
 };
 
