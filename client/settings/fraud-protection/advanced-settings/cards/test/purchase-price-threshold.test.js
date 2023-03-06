5fa886ff7e30d89ea5f08b82c02add72db273891
Taha Paksu <3295+tpaksu@users.noreply.github.com>
2023-03-06 13:52:38 +0300

    Fix purchase price threshold rule test

--- client/settings/fraud-protection/advanced-settings/cards/test/purchase-price-threshold.test.js
+++ client/settings/fraud-protection/advanced-settings/cards/test/purchase-price-threshold.test.js
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
 /**
  * Internal dependencies
  */
@@ -14,6 +14,8 @@ describe( 'Purchase price threshold card', () => {
 		purchase_price_threshold: {
 			enabled: false,
 			block: false,
+			min_amount: null,
+			max_amount: null,
 		},
 	};
 	const setSettings = jest.fn();
@@ -57,5 +59,88 @@ describe( 'Purchase price threshold card', () => {
 			</FraudPreventionSettingsContext.Provider>
 		);
 		expect( container ).toMatchSnapshot();
+	} );
+	test( 'renders warning when both fields are empty', () => {
+		settings.purchase_price_threshold.enabled = true;
+		settings.purchase_price_threshold.block = true;
+		render(
+			<FraudPreventionSettingsContext.Provider value={ contextValue }>
+				<PurchasePriceThresholdRuleCard />
+			</FraudPreventionSettingsContext.Provider>
+		);
+		expect(
+			screen.queryByText(
+				'A price range must be set for this filter to take effect.'
+			)
+		).toBeInTheDocument();
+	} );
+	test( "doesn't render warning when only min items field is filled", () => {
+		settings.purchase_price_threshold.enabled = true;
+		settings.purchase_price_threshold.block = true;
+		settings.purchase_price_threshold.min_amount = 1;
+		settings.purchase_price_threshold.max_amount = null;
+		render(
+			<FraudPreventionSettingsContext.Provider value={ contextValue }>
+				<PurchasePriceThresholdRuleCard />
+			</FraudPreventionSettingsContext.Provider>
+		);
+		expect(
+			screen.queryByText(
+				'A price range must be set for this filter to take effect.'
+			)
+		).not.toBeInTheDocument();
+	} );
+	test( "doesn't render warning when only max items field is filled", () => {
+		settings.purchase_price_threshold.enabled = true;
+		settings.purchase_price_threshold.block = true;
+		settings.purchase_price_threshold.min_amount = null;
+		settings.purchase_price_threshold.max_amount = 1;
+		render(
+			<FraudPreventionSettingsContext.Provider value={ contextValue }>
+				<PurchasePriceThresholdRuleCard />
+			</FraudPreventionSettingsContext.Provider>
+		);
+		expect(
+			screen.queryByText(
+				'A price range must be set for this filter to take effect.'
+			)
+		).not.toBeInTheDocument();
+	} );
+	test( "doesn't render warning when both fields are filled", () => {
+		settings.purchase_price_threshold.enabled = true;
+		settings.purchase_price_threshold.block = true;
+		settings.purchase_price_threshold.min_amount = 1;
+		settings.purchase_price_threshold.max_amount = 2;
+		render(
+			<FraudPreventionSettingsContext.Provider value={ contextValue }>
+				<PurchasePriceThresholdRuleCard />
+			</FraudPreventionSettingsContext.Provider>
+		);
+		expect(
+			screen.queryByText(
+				'A price range must be set for this filter to take effect.'
+			)
+		).not.toBeInTheDocument();
+	} );
+	test( 'renders error when min items is greater than max items', () => {
+		settings.purchase_price_threshold.enabled = true;
+		settings.purchase_price_threshold.block = true;
+		settings.purchase_price_threshold.min_amount = 2;
+		settings.purchase_price_threshold.max_amount = 1;
+		render(
+			<FraudPreventionSettingsContext.Provider value={ contextValue }>
+				<PurchasePriceThresholdRuleCard />
+			</FraudPreventionSettingsContext.Provider>
+		);
+		expect(
+			screen.queryByText(
+				'A price range must be set for this filter to take effect.'
+			)
+		).not.toBeInTheDocument();
+		expect(
+			screen.queryByText(
+				'Maximum purchase price must be greater than the minimum purchase price.'
+			)
+		).toBeInTheDocument();
 	} );
 } );
