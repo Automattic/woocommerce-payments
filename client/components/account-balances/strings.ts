c45f185e53a0235cbfacaf6be2c57a682b410f88
Eric Jinks <3147296+Jinksi@users.noreply.github.com>
2023-03-13 20:49:04 +1000

    Add file to hold Account Balance timeOfDay strings

---
+++ client/components/account-balances/strings.ts
@@ -0,0 +1,10 @@
+/**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+export const timeOfDayStrings = {
+	morning: __( 'morning', 'woocommerce-payments' ),
+	afternoon: __( 'afternoon', 'woocommerce-payments' ),
+	evening: __( 'evening', 'woocommerce-payments' ),
+};
