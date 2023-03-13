1e569d5e348642cdbd56b6f286041bb8c79ad03b
mattallan <matt.allan@automattic.com>
2023-03-13 19:49:26 +1000

    Load the new deposits overview components if the feature flag is enabled

--- client/overview/index.js
+++ client/overview/index.js
@@ -16,6 +16,7 @@ import { TestModeNotice, topics } from 'components/test-mode-notice';
 import AccountStatus from 'components/account-status';
 import ActiveLoanSummary from 'components/active-loan-summary';
 import DepositsInformation from 'components/deposits-information';
+import DepositsOverview from 'components/deposits-overview';
 import ErrorBoundary from 'components/error-boundary';
 import TaskList from './task-list';
 import { getTasks, taskSort } from './task-list/tasks';
@@ -131,7 +132,10 @@ const OverviewPage = () => {
 
 			{ ! accountRejected && (
 				<ErrorBoundary>
-					<DepositsInformation />
+					{ window.wcpaySettings?.featureFlags?.isNewDepositsOverview
+						? <DepositsOverview />
+						: <DepositsInformation />
+					}
 				</ErrorBoundary>
 			) }
 
