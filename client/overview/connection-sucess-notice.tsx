f61304b494a35eb7d394030fb3a43fc8d3c0118d
Ismael Martín Alabarce <info@ismaeld.com>
2023-03-13 15:00:38 +0100

    Show a different copy after PO and full KYC

--- client/overview/connection-sucess-notice.tsx
+++ client/overview/connection-sucess-notice.tsx
@@ -11,15 +11,38 @@ import ConfettiImage from '../../assets/images/confetti.svg';
 import { Card } from '@wordpress/components';
 
 const ConnectionSuccessNotice: React.FC = () => {
+	const {
+		accountStatus: {
+			progressiveOnboarding: { isComplete, isEnabled },
+		},
+	} = wcpaySettings;
+
 	return (
 		<Card className="wcpay-connection-success">
 			<img src={ ConfettiImage } alt="confetti" />
-			<h2>
-				{ __(
-					"Congratulations, you're ready to accept payments. Happy selling!",
-					'woocommerce-payments'
-				) }
-			</h2>
+			{ isEnabled && ! isComplete ? (
+				<>
+					<h2>
+						{ __(
+							'Your store is ready to start selling!',
+							'woocommerce-payments'
+						) }
+					</h2>
+					<p>
+						{ __(
+							'Congratulations! Take a moment to celebrate and look out for the first sale.',
+							'woocommerce-payments'
+						) }
+					</p>
+				</>
+			) : (
+				<h2>
+					{ __(
+						'Your store has been fully verified!',
+						'woocommerce-payments'
+					) }
+				</h2>
+			) }
 		</Card>
 	);
 };
