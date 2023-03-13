0734276871183380b41bd3033bfe412e9554e1db
Shendy Kurnia <shendy.kurnia@a8c.com>
2023-03-14 06:48:08 +0700

    Add feature flag for simplify deposits UI.

--- includes/class-wc-payments-features.php
+++ includes/class-wc-payments-features.php
@@ -19,6 +19,7 @@ class WC_Payments_Features {
 	const WOOPAY_EXPRESS_CHECKOUT_FLAG_NAME = '_wcpay_feature_woopay_express_checkout';
 	const AUTH_AND_CAPTURE_FLAG_NAME        = '_wcpay_feature_auth_and_capture';
 	const PROGRESSIVE_ONBOARDING_FLAG_NAME  = '_wcpay_feature_progressive_onboarding';
+	const SIMPLIFY_DEPOSITS_UI_FLAG_NAME    = '_wcpay_feature_simplify_deposits_ui';
 
 	/**
 	 * Checks whether any UPE gateway is enabled.
@@ -236,6 +237,15 @@ class WC_Payments_Features {
 	}
 
 	/**
+	 * Checks whether Simplify Deposits UI is enabled.
+	 *
+	 * @return bool
+	 */
+	public static function is_simplify_deposits_ui_enabled(): bool {
+		return '1' === get_option( self::SIMPLIFY_DEPOSITS_UI_FLAG_NAME, '0' );
+	}
+
+	/**
 	 * Returns feature flags as an array suitable for display on the front-end.
 	 *
 	 * @return bool[]
@@ -254,6 +264,7 @@ class WC_Payments_Features {
 				'woopayExpressCheckout'   => self::is_woopay_express_checkout_enabled(),
 				'isAuthAndCaptureEnabled' => self::is_auth_and_capture_enabled(),
 				'progressiveOnboarding'   => self::is_progressive_onboarding_enabled(),
+				'simplifyDepoitsUi'       => self::is_simplify_deposits_ui_enabled(),
 			]
 		);
 	}
