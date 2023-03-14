b4c21729b2d85b14110e9a3d9227cf922f2e34d9
Matt Allan <matt.allan@automattic.com>
2023-03-14 16:31:46 +1000

    New Deposits Overview UI Foundation Component (#5759)
    Co-authored-by: Eric Jinks <3147296+Jinksi@users.noreply.github.com>

---
+++ client/components/deposits-overview/index.tsx
@@ -0,0 +1,72 @@
+/**
+ * External dependencies
+ */
+import * as React from 'react';
+import { Card, CardHeader } from '@wordpress/components';
+
+/**
+ * Internal dependencies.
+ */
+import { useAllDepositsOverviews } from 'wcpay/data';
+import strings from './strings';
+
+interface OverviewProps {
+	overview: AccountOverview.Overview | null | undefined;
+	account: AccountOverview.Account | null | undefined;
+	isLoading: boolean;
+}
+
+/**
+ * Renders a deposits overview
+ *
+ * @param {OverviewProps} props Deposits overview
+ * @return {JSX.Element} Rendered element with deposits overview
+ */
+const DepositsOverviewDetails: React.FunctionComponent< OverviewProps > = (
+	props
+) => {
+	return (
+		<Card>
+			<CardHeader>{ strings.heading }</CardHeader>
+
+			<p>Next Deposits Section Goes here</p>
+
+			<p>Deposits History Section Goes here</p>
+
+			<p>Deposits Card Footer/Action Goes here</p>
+		</Card>
+	);
+};
+
+const DepositsOverview = (): JSX.Element => {
+	const {
+		overviews,
+		isLoading,
+	} = useAllDepositsOverviews() as AccountOverview.OverviewsResponse;
+
+	const { currencies, account } = overviews;
+
+	if ( isLoading ) {
+		return (
+			<DepositsOverviewDetails
+				overview={ null }
+				account={ null }
+				isLoading={ isLoading }
+			/>
+		);
+	}
+
+	const overview = currencies[ 0 ]; // TODO: To handle multiple currencies we'll need to fetch the currently selected currency.
+
+	return (
+		<>
+			<DepositsOverviewDetails
+				account={ account }
+				overview={ overview }
+				isLoading={ isLoading }
+			/>
+		</>
+	);
+};
+
+export default DepositsOverview;
