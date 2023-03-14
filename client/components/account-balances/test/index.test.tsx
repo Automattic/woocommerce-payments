b2c68626f3abb7a6465d91efceec39199497fda3
Eric Jinks <3147296+Jinksi@users.noreply.github.com>
2023-03-14 20:31:16 +1000

    Add AccountBalancesHeader test

--- client/components/account-balances/test/index.test.tsx
+++ client/components/account-balances/test/index.test.tsx
@@ -8,22 +8,48 @@ import { render } from '@testing-library/react';
  * Internal dependencies
  */
 import AccountBalances from '../';
+import AccountBalancesHeader from '../header';
 import { getGreeting } from '../utils';
+import { useCurrentWpUser } from '../hooks';
 
 jest.mock( '../utils', () => ( {
+	getTimeOfDayString: jest.fn(),
 	getGreeting: jest.fn(),
+} ) );
+
+jest.mock( '../hooks', () => ( {
+	useCurrentWpUser: jest.fn(),
 } ) );
 
 const mockGetGreeting = getGreeting as jest.MockedFunction<
 	typeof getGreeting
 >;
+const mockUseCurrentWpUser = useCurrentWpUser as jest.MockedFunction<
+	typeof useCurrentWpUser
+>;
 
 describe( 'AccountBalances', () => {
-	// Adding an initial test to be expanded upon.
-	mockGetGreeting.mockReturnValue( 'Good afternoon!' );
-
 	test( 'renders', () => {
+		const expectedGreeting = 'Good afternoon, Tester 👋';
+		mockGetGreeting.mockReturnValue( expectedGreeting );
+		mockUseCurrentWpUser.mockReturnValue( {
+			displayName: 'Tester',
+			isLoading: false,
+		} );
 		const { container } = render( <AccountBalances /> );
 		expect( container ).toMatchSnapshot();
+	} );
+} );
+
+describe( 'AccountBalancesHeader', () => {
+	test( 'renders the correct greeting in the header', () => {
+		const expectedGreeting = 'Good afternoon, Tester 👋';
+		mockGetGreeting.mockReturnValue( expectedGreeting );
+		mockUseCurrentWpUser.mockReturnValue( {
+			displayName: 'Tester',
+			isLoading: false,
+		} );
+		const { getByText } = render( <AccountBalancesHeader /> );
+		getByText( expectedGreeting );
 	} );
 } );
