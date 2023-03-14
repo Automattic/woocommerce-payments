1f669d440276f728288740820e2f347342850b9a
Jessy <jessy.pappachan@automattic.com>
2023-03-14 23:11:12 +0530

    Improved E2E test

--- tests/e2e/specs/wcpay/merchant/merchant-orders-status-change.spec.js
+++ tests/e2e/specs/wcpay/merchant/merchant-orders-status-change.spec.js
@@ -10,6 +10,11 @@ const { merchant, shopper } = require( '@woocommerce/e2e-utils' );
  */
 import { fillCardDetails, setupProductCheckout } from '../../../utils/payments';
 
+const orderIdSelector = '.woocommerce-order-overview__order.order > strong';
+const orderStatusDropdownSelector = 'select[name="order_status"]';
+const cancelModalSelector = 'div.cancel-confirmation-modal';
+const selectedOrderStatusSelector = '.wc-order-status > span';
+
 let orderId;
 
 describe( 'Order > Status Change', () => {
@@ -24,9 +29,7 @@ describe( 'Order > Status Change', () => {
 		await expect( page ).toMatch( 'Order received' );
 
 		// Get the order ID so we can open it in the merchant view
-		const orderIdField = await page.$(
-			'.woocommerce-order-overview__order.order > strong'
-		);
+		const orderIdField = await page.$( orderIdSelector );
 		orderId = await orderIdField.evaluate( ( el ) => el.innerText );
 
 		// Login and open the order
@@ -38,15 +41,15 @@ describe( 'Order > Status Change', () => {
 		await merchant.logout();
 	} );
 
-	it( 'should show Cancel Confirmation modal on status change and reset on selecting Do Nothing', async () => {
+	it( 'should show Cancel Confirmation modal on status change, and reset the dropdown when Do Nothing is clicked', async () => {
 		// Select cancel from the order status dropdown.
 		await expect( page ).toSelect(
-			'select[name="order_status"]',
+			orderStatusDropdownSelector,
 			'Cancelled'
 		);
 
 		// Verify the confirmation modal shows.
-		await page.waitForSelector( 'div.cancel-confirmation-modal', {
+		await page.waitForSelector( cancelModalSelector, {
 			visible: true,
 		} );
 
@@ -54,21 +57,21 @@ describe( 'Order > Status Change', () => {
 		await expect( page ).toClick( 'button', { text: 'Do Nothing' } );
 
 		// Verify the order status is set to processing.
-		const selectedOrderStatus = await page.$( '.wc-order-status > span' );
+		const selectedOrderStatus = await page.$( selectedOrderStatusSelector );
 		await expect(
 			selectedOrderStatus.evaluate( ( el ) => el.innerText )
 		).resolves.toBe( 'Processing' );
 	} );
 
-	it( 'should show Cancel Confirmation modal on status change and Cancel on confirmation', async () => {
+	it( 'should show Cancel Confirmation modal on status change, and set the status to Cancel on confirmation', async () => {
 		// Select cancel from the order status dropdown.
 		await expect( page ).toSelect(
-			'select[name="order_status"]',
+			orderStatusDropdownSelector,
 			'Cancelled'
 		);
 
 		// Verify the confirmation modal shows.
-		await page.waitForSelector( 'div.cancel-confirmation-modal', {
+		await page.waitForSelector( cancelModalSelector, {
 			visible: true,
 		} );
 
@@ -76,7 +79,7 @@ describe( 'Order > Status Change', () => {
 		await expect( page ).toClick( 'button', { text: 'Cancel order' } );
 
 		// Verify the order status is set to cancel.
-		const selectedOrderStatus = await page.$( '.wc-order-status > span' );
+		const selectedOrderStatus = await page.$( selectedOrderStatusSelector );
 		await expect(
 			selectedOrderStatus.evaluate( ( el ) => el.innerText )
 		).resolves.toBe( 'Cancelled' );
