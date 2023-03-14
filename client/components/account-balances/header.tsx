89aa1463334ac246a637a18932ea8c01339b5858
Eric Jinks <3147296+Jinksi@users.noreply.github.com>
2023-03-14 21:18:49 +1000

    Add Woo Payments logo to AccountBalancer Header

--- client/components/account-balances/header.tsx
+++ client/components/account-balances/header.tsx
@@ -2,13 +2,15 @@
  * External dependencies
  */
 import React from 'react';
-import { CardHeader } from '@wordpress/components';
+import { CardHeader, Flex, FlexItem } from '@wordpress/components';
 
 /**
  * Internal dependencies
  */
 import { getGreeting } from './utils';
 import { useCurrentWpUser } from './hooks';
+import wooPaymentsLogo from '../../../assets/images/woopayments.svg';
+import './style.scss';
 
 /**
  * Renders the header of the account's balances card.
@@ -19,7 +21,22 @@ const AccountBalancesHeader: React.FC = () => {
 	const { displayName } = useCurrentWpUser();
 	const greeting = getGreeting( displayName );
 
-	return <CardHeader>{ greeting }</CardHeader>;
+	return (
+		<CardHeader>
+			<Flex
+				align="center"
+				justify="space-between"
+				className="wcpay-account-balances__header"
+			>
+				<FlexItem className="wcpay-account-balances__header__greeting">
+					{ greeting }
+				</FlexItem>
+				<FlexItem>
+					<img src={ wooPaymentsLogo } alt="Woo Payments logo" />
+				</FlexItem>
+			</Flex>
+		</CardHeader>
+	);
 };
 
 export default AccountBalancesHeader;
