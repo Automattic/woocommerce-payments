bcaa22f65e8e8b4a873eb30dd331ba30a7f52763
mattallan <matt.allan@automattic.com>
2023-03-15 14:54:33 +1000

    Add strings for Next Deposits heading section

--- client/components/deposits-overview/strings.ts
+++ client/components/deposits-overview/strings.ts
@@ -5,4 +5,11 @@ import { __ } from '@wordpress/i18n';
 
 export default {
 	heading: __( 'Deposits', 'woocommerce-payments' ),
+	next_deposits: {
+		title: __( 'Next deposits', 'woocommerce-payments' ),
+		description: __(
+			'The amount may change while payments are still accumulating',
+			'woocommerce-payments'
+		),
+	},
 };
