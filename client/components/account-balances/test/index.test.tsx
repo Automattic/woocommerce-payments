1f3765722f94c0314f8861803a4dbfd6dc24e499
James Allan <james.allan@automattic.com>
2023-03-15 15:06:58 +1000

    Fix js tests

--- client/components/account-balances/test/index.test.tsx
+++ client/components/account-balances/test/index.test.tsx
@@ -9,7 +9,38 @@ import { render } from '@testing-library/react';
  */
 import AccountBalances from '../';
 
+declare const global: {
+	wcpaySettings: {
+		accountDefaultCurrency: 'USD';
+		zeroDecimalCurrencies: string[];
+		currencyData: Record< string, any >;
+		connect: {
+			country: string;
+		};
+	};
+};
+
 describe( 'AccountBalances', () => {
+	beforeEach( () => {
+		global.wcpaySettings = {
+			accountDefaultCurrency: 'USD',
+			zeroDecimalCurrencies: [],
+			connect: {
+				country: 'US',
+			},
+			currencyData: {
+				US: {
+					code: 'USD',
+					symbol: '$',
+					symbolPosition: 'left',
+					thousandSeparator: ',',
+					decimalSeparator: '.',
+					precision: 2,
+				},
+			},
+		};
+	} );
+
 	// Adding an initial test to be expanded upon.
 	test( 'renders', () => {
 		const { container } = render( <AccountBalances /> );
