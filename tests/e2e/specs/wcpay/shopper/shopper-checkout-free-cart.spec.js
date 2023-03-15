5509e91b7f257e4d36aecdf059ceaba0f47ddac0
Jessy <jessy.pappachan@automattic.com>
2023-03-15 14:26:39 +0530

    Outer describe block

--- tests/e2e/specs/wcpay/shopper/shopper-checkout-free-cart.spec.js
+++ tests/e2e/specs/wcpay/shopper/shopper-checkout-free-cart.spec.js
@@ -30,89 +30,91 @@ const blocksPageEnabledSelector =
 	'.wc-block-components-main button:not(:disabled)';
 
 describe( 'Checkout with free coupon & after updating cart on Checkout page', () => {
-	beforeAll( async () => {
-		try {
-			await checkPageExists( 'checkout-wcb' );
-		} catch ( error ) {
-			await merchant.login();
-			await merchantWCP.addNewPageCheckoutWCB();
-			await merchant.logout();
-		}
-	} );
+	describe( 'Checkout with free coupon & after updating cart on Checkout page', () => {
+		beforeAll( async () => {
+			try {
+				await checkPageExists( 'checkout-wcb' );
+			} catch ( error ) {
+				await merchant.login();
+				await merchantWCP.addNewPageCheckoutWCB();
+				await merchant.logout();
+			}
+		} );
 
-	beforeEach( async () => {
-		await shopper.goToShop();
-		await shopper.addToCartFromShopPage( productName );
-		await shopper.goToCart();
-		await clearAndFillInput( couponInputSelector, 'free' );
-		await expect( page ).toClick( applyCouponSelector );
-	} );
+		beforeEach( async () => {
+			await shopper.goToShop();
+			await shopper.addToCartFromShopPage( productName );
+			await shopper.goToCart();
+			await clearAndFillInput( couponInputSelector, 'free' );
+			await expect( page ).toClick( applyCouponSelector );
+		} );
 
-	afterAll( async () => {
-		// Clear the cart at the end so it's ready for another test
-		await shopperWCP.emptyCart();
-	} );
+		afterAll( async () => {
+			// Clear the cart at the end so it's ready for another test
+			await shopperWCP.emptyCart();
+		} );
 
-	it( 'Checkout with a free coupon', async () => {
-		await shopper.goToCheckout();
-		await shopper.fillBillingDetails( billingDetails );
-		await page.waitFor( 1000 );
-		await uiUnblocked();
-		await shopper.placeOrder();
-		await expect( page ).toMatch( 'Order received' );
-	} );
+		it( 'Checkout with a free coupon', async () => {
+			await shopper.goToCheckout();
+			await shopper.fillBillingDetails( billingDetails );
+			await page.waitFor( 1000 );
+			await uiUnblocked();
+			await shopper.placeOrder();
+			await expect( page ).toMatch( 'Order received' );
+		} );
 
-	it( 'Remove free coupon, then checkout with Classic Checkout', async () => {
-		await shopper.goToCheckout();
-		await page.click( removeCouponSelector );
-		await uiUnblocked();
-		await setupCheckout( billingDetails );
-		await fillCardDetails( page, card );
-		await shopper.placeOrder();
-		await expect( page ).toMatch( 'Order received' );
-	} );
+		it( 'Remove free coupon, then checkout with Classic Checkout', async () => {
+			await shopper.goToCheckout();
+			await page.click( removeCouponSelector );
+			await uiUnblocked();
+			await setupCheckout( billingDetails );
+			await fillCardDetails( page, card );
+			await shopper.placeOrder();
+			await expect( page ).toMatch( 'Order received' );
+		} );
 
-	it( 'Remove free coupon, then checkout with WooCommerce Blocks', async () => {
-		await shopperWCP.openCheckoutWCB();
-		await shopperWCP.fillBillingDetailsWCB( billingDetails );
-		await fillCardDetailsWCB( page, card );
-		await page.waitForSelector( blocksPageEnabledSelector );
-		await expect( page ).toClick( 'button', { text: 'Place Order' } );
-		await page.waitForSelector( 'div.woocommerce-order' );
-		await expect( page ).toMatch( 'p', {
-			text: 'Thank you. Your order has been received.',
+		it( 'Remove free coupon, then checkout with WooCommerce Blocks', async () => {
+			await shopperWCP.openCheckoutWCB();
+			await shopperWCP.fillBillingDetailsWCB( billingDetails );
+			await fillCardDetailsWCB( page, card );
+			await page.waitForSelector( blocksPageEnabledSelector );
+			await expect( page ).toClick( 'button', { text: 'Place Order' } );
+			await page.waitForSelector( 'div.woocommerce-order' );
+			await expect( page ).toMatch( 'p', {
+				text: 'Thank you. Your order has been received.',
+			} );
 		} );
 	} );
-} );
 
-describe( 'Checkout with free coupon & after updating cart on Checkout page: UPE', () => {
-	beforeAll( async () => {
-		await merchant.login();
-		await merchantWCP.activateSplitUpe();
-		await merchant.logout();
-	} );
+	describe( 'Checkout with free coupon & after updating cart on Checkout page with UPE', () => {
+		beforeAll( async () => {
+			await merchant.login();
+			await merchantWCP.activateSplitUpe();
+			await merchant.logout();
+		} );
 
-	afterAll( async () => {
-		await merchant.login();
-		await merchantWCP.deactivateSplitUpe();
-		await merchant.logout();
-	} );
+		afterAll( async () => {
+			await merchant.login();
+			await merchantWCP.deactivateSplitUpe();
+			await merchant.logout();
+		} );
 
-	beforeEach( async () => {
-		await shopper.goToShop();
-		await shopper.addToCartFromShopPage( productName );
-		await shopper.goToCart();
-		await clearAndFillInput( couponInputSelector, 'free' );
-		await expect( page ).toClick( applyCouponSelector );
-	} );
+		beforeEach( async () => {
+			await shopper.goToShop();
+			await shopper.addToCartFromShopPage( productName );
+			await shopper.goToCart();
+			await clearAndFillInput( couponInputSelector, 'free' );
+			await expect( page ).toClick( applyCouponSelector );
+		} );
 
-	it( 'Remove free coupon, then checkout with UPE', async () => {
-		await shopper.goToCheckout();
-		await page.click( removeCouponSelector );
-		await uiUnblocked();
-		await setupCheckout( billingDetails );
-		await fillCardDetails( page, card );
-		await shopper.placeOrder();
-		await expect( page ).toMatch( 'Order received' );
+		it( 'Remove free coupon, then checkout with UPE', async () => {
+			await shopper.goToCheckout();
+			await page.click( removeCouponSelector );
+			await uiUnblocked();
+			await setupCheckout( billingDetails );
+			await fillCardDetails( page, card );
+			await shopper.placeOrder();
+			await expect( page ).toMatch( 'Order received' );
+		} );
 	} );
 } );
