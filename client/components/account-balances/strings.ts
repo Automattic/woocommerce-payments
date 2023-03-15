e09c5b3b0a162fdf5a28597f0437b6510a0349f4
James Allan <james.allan@automattic.com>
2023-03-15 10:17:32 +1000

    Remove translations copied from a different branch

--- client/components/account-balances/strings.ts
+++ client/components/account-balances/strings.ts
@@ -3,12 +3,6 @@
  */
 import { __ } from '@wordpress/i18n';
 
-export const timeOfDayStrings = {
-	morning: __( 'morning', 'woocommerce-payments' ),
-	afternoon: __( 'afternoon', 'woocommerce-payments' ),
-	evening: __( 'evening', 'woocommerce-payments' ),
-};
-
 export const fundLabelStrings = {
 	available: __( 'Available funds', 'woocommerce-payments' ),
 	pending: __( 'Pending funds', 'woocommerce-payments' ),
