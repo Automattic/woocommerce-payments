96339f3ed22e35639e7c6a3ed8937db8fcb0e565
James Allan <james.allan@automattic.com>
2023-03-16 10:12:05 +1000

    Add function and interface def comments

--- client/components/account-balances/balance-block.tsx
+++ client/components/account-balances/balance-block.tsx
@@ -5,6 +5,16 @@ import * as React from 'react';
 import { formatCurrency } from 'wcpay/utils/currency';
 import Loadable from 'components/loadable';
 
+/**
+ * BalanceBlockProps
+ *
+ * @typedef {Object} BalanceBlockProps
+ *
+ * @property {string} title        Title of the balance block. eg "Available Funds".
+ * @property {string} currencyCode Currency code of the balance block.
+ * @property {number} [amount]     Optional. The balance amount.
+ * @property {boolean} [isLoading] Optional. Whether the balance block is loading.
+ */
 interface BalanceBlockProps {
 	title: string;
 	currencyCode: string;
@@ -12,6 +22,17 @@ interface BalanceBlockProps {
 	isLoading?: boolean;
 }
 
+/**
+ * Renders a balance block including a title and amount.
+ *
+ * @param {BalanceBlockProps} props   Balance block props. See `BalanceBlockProps` interface.
+ * @param {string} props.title        Title of the balance block. eg "Available Funds".
+ * @param {string} props.currencyCode Currency code of the balance block.
+ * @param {number} [props.amount]     Optional. The balance amount.
+ * @param {boolean} [props.isLoading] Optional. Whether the balance block is loading.
+ *
+ * @return {JSX.Element} Rendered balance element.
+ */
 const BalanceBlock: React.FC< BalanceBlockProps > = ( {
 	title,
 	currencyCode,
