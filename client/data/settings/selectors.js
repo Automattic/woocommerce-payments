8829c89e7854ad5ad54d33170e0c783c416decfe
Taha Paksu <3295+tpaksu@users.noreply.github.com>
2023-03-17 12:45:04 +0300

    Remove duplicate selector caused by merge

--- client/data/settings/selectors.js
+++ client/data/settings/selectors.js
@@ -216,7 +216,3 @@ export const getCurrentProtectionLevel = ( state ) => {
 export const getAdvancedFraudProtectionSettings = ( state ) => {
 	return getSettings( state ).advanced_fraud_protection_settings || EMPTY_OBJ;
 };
-
-export const getAdvancedFraudProtectionSettings = ( state ) => {
-	return getSettings( state ).advanced_fraud_protection_settings || EMPTY_OBJ;
-};
