26c0a2333261c0bc192b715aa4e7bb20ac289437
Eric Jinks <3147296+Jinksi@users.noreply.github.com>
2023-03-17 22:44:13 +1000

    Add balance tooltip strings

--- client/components/account-balances/strings.ts
+++ client/components/account-balances/strings.ts
@@ -25,5 +25,35 @@ export const fundLabelStrings = {
 	reserved: __( 'Reserved funds', 'woocommerce-payments' ),
 };
 
+export const learnMoreString = __( 'Learn more.', 'woocommerce-payments' );
+
+export const fundTooltipStrings = {
+	available: __(
+		'The amount of funds available to be deposited.',
+		'woocommerce-payments'
+	),
+	availableNegativeBalance: __(
+		'Learn more about why your account balance may be negative.',
+		'woocommerce-payments'
+	),
+	pending: __(
+		'The amount of funds still in the %d day pending period.',
+		'woocommerce-payments'
+	),
+	reserved: __(
+		'The amount of funds being held in reserve.',
+		'woocommerce-payments'
+	),
+};
+
+export const documentationUrls = {
+	depositSchedule:
+		'https://woocommerce.com/document/woocommerce-payments/deposits/deposit-schedule',
+	reservedFunds:
+		'https://woocommerce.com/document/woocommerce-payments/our-policies/reserves',
+	negativeBalance:
+		'https://woocommerce.com/document/woocommerce-payments/fees-and-debits/account-showing-negative-balance',
+};
+
 /** translators: %s is the currency code, e.g. USD. */
 export const currencyBalanceString = __( '%s Balance', 'woocommerce-payments' );
