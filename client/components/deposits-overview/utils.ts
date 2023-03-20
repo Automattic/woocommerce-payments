5e9eb0d58c039d9d4a4d2482ff2c2608846bf180
mattallan <matt.allan@automattic.com>
2023-03-20 14:50:51 +1000

    Clean up the utils function

--- client/components/deposits-overview/utils.ts
+++ client/components/deposits-overview/utils.ts
@@ -3,39 +3,37 @@
  */
 import { formatCurrency } from 'utils/currency';
 
-/**
- * Formats the next deposit data from the overview object into an object to be used in the Next Deposits table.
- *
- * @param {AccountOverview.Overview} overview The overview object.
- * @return {Object} The next deposit data { date, status, amount}.
- */
-export const getNextDeposit = (
-	overview?: AccountOverview.Overview
-): {
+type NextDepositTableData = {
 	date: number;
 	status: string;
 	amount: string;
-} => {
-	// Set default values for the next deposit.
-	const nextDeposit: {
-		date: number;
-		status: string;
-		amount: string;
-	} = {
-		date: 0,
-		status: '',
-		amount: '0',
-	};
+};
 
+/**
+ * Formats the next deposit data from the overview object into an object that can be used in the Next Deposits table.
+ *
+ * @param {AccountOverview.Overview} overview - The overview object containing information about the next scheduled deposit.
+ * @return {NextDepositTableData} An object containing the formatted next deposit data, with the following properties:
+ * - date: A Unix timestamp representing the date of the next scheduled deposit.
+ * - status: A string representing the status of the next scheduled deposit. If no status is provided, defaults to 'estimated.
+ * - amount: A formatted string representing the amount of the next scheduled deposit in the currency specified in the overview object.
+ */
+export const getNextDeposit = (
+	overview?: AccountOverview.Overview
+): NextDepositTableData => {
 	if ( ! overview ) {
-		return nextDeposit;
+		return {
+			date: 0,
+			status: 'estimated',
+			amount: formatCurrency( 0, 'USD' ),
+		};
 	}
 
 	const { currency, nextScheduled } = overview;
 
-	nextDeposit.date = nextScheduled.date;
-	nextDeposit.status = nextScheduled.status ?? 'estimated';
-	nextDeposit.amount = formatCurrency( nextScheduled.amount ?? 0, currency );
-
-	return nextDeposit;
+	return {
+		date: nextScheduled.date ?? 0,
+		status: nextScheduled.status ?? 'estimated',
+		amount: formatCurrency( nextScheduled.amount ?? 0, currency ),
+	};
 };
