b9e2776b8e7f7e68b82d7d1dad70171cee746650
James Allan <james.allan@automattic.com>
2023-03-21 10:48:00 +1000

    Replace info svg with existing icon

--- client/settings/fraud-protection/advanced-settings/rule-card-notice.js
+++ client/settings/fraud-protection/advanced-settings/rule-card-notice.js
@@ -9,43 +9,7 @@ import NoticeOutlineIcon from 'gridicons/dist/notice-outline';
  */
 import './../style.scss';
 import BannerNotice from '../../../components/banner-notice';
-
-const getInfoSVGIcon = () => {
-	return (
-		<svg
-			width="24"
-			height="24"
-			viewBox="0 0 24 24"
-			fill="none"
-			xmlns="http://www.w3.org/2000/svg"
-			data-testid="rule-card-notice-info-icon-svg"
-		>
-			<circle
-				cx="12"
-				cy="9"
-				r="5"
-				stroke="#007CBA"
-				strokeWidth="1.42857"
-			/>
-			<line
-				x1="9"
-				y1="16.8818"
-				x2="15"
-				y2="16.8818"
-				stroke="#007CBA"
-				strokeWidth="1.5"
-			/>
-			<line
-				x1="10"
-				y1="19.25"
-				x2="14"
-				y2="19.25"
-				stroke="#007CBA"
-				strokeWidth="1.5"
-			/>
-		</svg>
-	);
-};
+import TipIcon from 'wcpay/icons';
 
 const FraudProtectionRuleCardNotice = ( { type, children } ) => {
 	const supportedTypes = [ 'error', 'warning', 'info' ];
@@ -59,7 +23,7 @@ const FraudProtectionRuleCardNotice = ( { type, children } ) => {
 
 	// If the type is info, we want to use the info icon instead.
 	if ( 'info' === type ) {
-		icon = getInfoSVGIcon();
+		icon = <TipIcon />;
 	}
 
 	return (
