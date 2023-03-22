91e0dd595b6349d2cf2be89fd5a933b98606a774
mattallan <matt.allan@automattic.com>
2023-03-22 17:06:29 +1000

    Remove redundant DepositsOverviewDetails const, can just use DepositOverview

--- client/components/deposits-overview/index.tsx
+++ client/components/deposits-overview/index.tsx
@@ -11,22 +11,16 @@ import { useAllDepositsOverviews } from 'wcpay/data';
 import strings from './strings';
 import NextDepositDetails from './next-deposit';
 
-interface OverviewProps {
-	overview?: AccountOverview.Overview;
-	account?: AccountOverview.Account;
-	isLoading: boolean;
-}
+const DepositsOverview = (): JSX.Element => {
+	const {
+		overviews,
+		isLoading,
+	} = useAllDepositsOverviews() as AccountOverview.OverviewsResponse;
 
-/**
- * Renders a deposits overview
- *
- * @param {OverviewProps} props Deposits overview
- * @return {JSX.Element} Rendered element with deposits overview
- */
-const DepositsOverviewDetails: React.FunctionComponent< OverviewProps > = ( {
-	isLoading,
-	overview,
-} ) => {
+	const { currencies, account } = overviews;
+
+	const overview = currencies[ 0 ]; // TODO: To handle multiple currencies we'll need to fetch the currently selected currency.
+
 	return (
 		<Card>
 			<CardHeader>{ strings.heading }</CardHeader>
@@ -36,31 +30,6 @@ const DepositsOverviewDetails: React.FunctionComponent< OverviewProps > = ( {
 
 			<p>Deposits Card Footer/Action Goes here</p>
 		</Card>
-	);
-};
-
-const DepositsOverview = (): JSX.Element => {
-	const {
-		overviews,
-		isLoading,
-	} = useAllDepositsOverviews() as AccountOverview.OverviewsResponse;
-
-	const { currencies, account } = overviews;
-
-	if ( isLoading ) {
-		return <DepositsOverviewDetails isLoading={ isLoading } />;
-	}
-
-	const overview = currencies[ 0 ]; // TODO: To handle multiple currencies we'll need to fetch the currently selected currency.
-
-	return (
-		<>
-			<DepositsOverviewDetails
-				account={ account }
-				overview={ overview }
-				isLoading={ isLoading }
-			/>
-		</>
 	);
 };
 
