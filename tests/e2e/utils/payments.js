926875d744021c941311ecde7713e14bcc82242e
jbordonado <jbordonado@users.noreply.github.com>
2023-03-22 11:54:08 +0100

    Improve tests

--- tests/e2e/utils/payments.js
+++ tests/e2e/utils/payments.js
@@ -53,20 +53,25 @@ export async function fillCardDetails( page, card ) {
 			'[name="cardnumber"]',
 			{ timeout: 30000 }
 		);
-		await cardNumberInput.type( card.number, { delay: 100 } );
+		await cardNumberInput.type( card.number, { delay: 20 } );
+		await page.waitFor( 1000 );
 
 		const cardDateInput = await stripeFrame.waitForSelector(
-			'[name="exp-date"]'
+			'[name="exp-date"]',
+			{ timeout: 30000 }
 		);
 
 		await cardDateInput.type( card.expires.month + card.expires.year, {
-			delay: 100,
+			delay: 20,
 		} );
+		await page.waitFor( 1000 );
 
 		const cardCvcInput = await stripeFrame.waitForSelector(
-			'[name="cvc"]'
+			'[name="cvc"]',
+			{ timeout: 30000 }
 		);
-		await cardCvcInput.type( card.cvc, { delay: 100 } );
+		await cardCvcInput.type( card.cvc, { delay: 20 } );
+		await page.waitFor( 1000 );
 	}
 }
 
@@ -76,23 +81,23 @@ export async function clearCardDetails() {
 		'#payment #wcpay-card-element iframe[name^="__privateStripeFrame"]'
 	);
 	const stripeFrame = await frameHandle.contentFrame();
+
 	const cardNumberInput = await stripeFrame.waitForSelector(
 		'[name="cardnumber"]'
 	);
-	const cardDateInput = await stripeFrame.waitForSelector(
-		'[name="exp-date"]'
-	);
-	const cardCvcInput = await stripeFrame.waitForSelector( '[name="cvc"]' );
-
 	await cardNumberInput.click();
 	await page.waitFor( 1000 );
 	await cardNumberInput.click( { clickCount: 3 } );
 	await page.keyboard.press( 'Backspace' );
 
+	const cardDateInput = await stripeFrame.waitForSelector(
+		'[name="exp-date"]'
+	);
 	await page.waitFor( 1000 );
 	await cardDateInput.click( { clickCount: 3 } );
 	await page.keyboard.press( 'Backspace' );
 
+	const cardCvcInput = await stripeFrame.waitForSelector( '[name="cvc"]' );
 	await page.waitFor( 1000 );
 	await cardCvcInput.click( { clickCount: 3 } );
 	await page.keyboard.press( 'Backspace' );
