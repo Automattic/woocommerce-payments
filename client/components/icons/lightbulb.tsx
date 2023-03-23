a18e5c181dcd172e4ec4c3691fc5708f7dcef0d5
Ismael Martín Alabarce <info@ismaeld.com>
2023-03-23 12:23:55 +0100

    Adjust TipBox style to match Woo Banner notices design

--- client/components/icons/lightbulb.tsx
+++ client/components/icons/lightbulb.tsx
@@ -6,9 +6,9 @@ import React from 'react';
 export default (): JSX.Element => (
 	<svg
 		xmlns="http://www.w3.org/2000/svg"
-		width="16"
-		height="16"
-		viewBox="0 0 16 16"
+		width="24"
+		height="24"
+		viewBox="-4 -4 24 24"
 	>
 		<path
 			fillRule="evenodd"
