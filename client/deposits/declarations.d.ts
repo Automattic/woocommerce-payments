0d9631bdd9bc233429ce4b26ecf8faf079b6edfc
Eric Jinks <3147296+Jinksi@users.noreply.github.com>
2023-03-24 10:08:19 +1000

    Add comment to explain external module type declaration

--- client/deposits/declarations.d.ts
+++ client/deposits/declarations.d.ts
@@ -1,9 +1,7 @@
 /**
- * External dependencies
- */
-
-/**
- * Internal dependencies
+ * The types for the `@woocommerce/components` package are being declared here
+ * because it does not have its own types (as of version 5.1.2).
+ * We should remove this file once we've updated to a version of `@woocommerce/components` with type defs.
  */
 declare module '@woocommerce/components' {
 	const SummaryListPlaceholder: ( props: {
