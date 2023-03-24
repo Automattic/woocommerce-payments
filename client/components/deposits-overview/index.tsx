8884edeb0c2c533f9e7a3261e267d25e41cb9ef2
Matt Allan <matt.allan@automattic.com>
2023-03-24 11:53:37 +1000

    [Simplifying Deposits UI] Adds the next deposits table (#5799)
    Co-authored-by: Eric Jinks <3147296+Jinksi@users.noreply.github.com>
    Co-authored-by: Shendy <73803630+shendy-a8c@users.noreply.github.com>
    Co-authored-by: bruce aldridge <bruce.aldridge@gmail.com>

--- client/components/deposits-overview/index.tsx
+++ client/components/deposits-overview/index.tsx
@@ -11,33 +11,6 @@ import { useAllDepositsOverviews } from 'wcpay/data';
 import strings from './strings';
 import NextDepositDetails from './next-deposit';
 
-interface OverviewProps {
-	overview?: AccountOverview.Overview;
-	account?: AccountOverview.Account;
-	isLoading: boolean;
-}
-
-/**
- * Renders a deposits overview
- *
- * @param {OverviewProps} props Deposits overview
- * @return {JSX.Element} Rendered element with deposits overview
- */
-const DepositsOverviewDetails: React.FunctionComponent< OverviewProps > = ( {
-	isLoading,
-} ) => {
-	return (
-		<Card>
-			<CardHeader>{ strings.heading }</CardHeader>
-			<NextDepositDetails isLoading={ isLoading } />
-
-			<p>Deposits History Section Goes here</p>
-
-			<p>Deposits Card Footer/Action Goes here</p>
-		</Card>
-	);
-};
-
 const DepositsOverview = (): JSX.Element => {
 	const {
 		overviews,
@@ -46,20 +19,17 @@ const DepositsOverview = (): JSX.Element => {
 
 	const { currencies, account } = overviews;
 
-	if ( isLoading ) {
-		return <DepositsOverviewDetails isLoading={ isLoading } />;
-	}
-
 	const overview = currencies[ 0 ]; // TODO: To handle multiple currencies we'll need to fetch the currently selected currency.
 
 	return (
-		<>
-			<DepositsOverviewDetails
-				account={ account }
-				overview={ overview }
-				isLoading={ isLoading }
-			/>
-		</>
+		<Card>
+			<CardHeader>{ strings.heading }</CardHeader>
+			<NextDepositDetails isLoading={ isLoading } overview={ overview } />
+
+			<p>Deposits History Section Goes here</p>
+
+			<p>Deposits Card Footer/Action Goes here</p>
+		</Card>
 	);
 };
 
