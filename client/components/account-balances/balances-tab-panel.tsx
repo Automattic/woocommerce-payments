205bc60c7e678018bfdea2f5378d49e368234cff
James Allan <james.allan@automattic.com>
2023-03-27 17:36:59 +1000

    Remove the "Reserve" funds balance block

--- client/components/account-balances/balances-tab-panel.tsx
+++ client/components/account-balances/balances-tab-panel.tsx
@@ -29,7 +29,6 @@ type BalanceTab = {
 	currencyCode: string;
 	availableFunds: number;
 	pendingFunds: number;
-	reservedFunds: number;
 };
 
 /**
@@ -51,7 +50,6 @@ const AccountBalancesTabPanel: React.FC = () => {
 			currencyCode: wcpaySettings.accountDefaultCurrency,
 			availableFunds: 0,
 			pendingFunds: 0,
-			reservedFunds: 0,
 		},
 	];
 
@@ -65,7 +63,6 @@ const AccountBalancesTabPanel: React.FC = () => {
 				currencyCode: overview.currency,
 				availableFunds: overview.available.amount,
 				pendingFunds: overview.pending.amount,
-				reservedFunds: 0, // TODO: Add reserve funds to the overview object.
 			} )
 		);
 	}
@@ -83,12 +80,6 @@ const AccountBalancesTabPanel: React.FC = () => {
 					<BalanceBlock
 						type="pending"
 						amount={ tab.pendingFunds }
-						currencyCode={ tab.currencyCode }
-						isLoading={ isLoading }
-					/>
-					<BalanceBlock
-						type="reserved"
-						amount={ tab.reservedFunds }
 						currencyCode={ tab.currencyCode }
 						isLoading={ isLoading }
 					/>
