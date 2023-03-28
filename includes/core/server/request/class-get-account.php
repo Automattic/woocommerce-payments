aedc754328649ba64acc384be48df3ec5ac018bd
Dat Hoang <htdat@users.noreply.github.com>
2023-03-28 18:04:00 +0700

    Migrate the get_account API.

---
+++ includes/core/server/request/class-get-account.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Class file for WCPay\Core\Server\Request\Get_Account.
+ *
+ * @package WooCommerce Payments
+ */
+
+namespace WCPay\Core\Server\Request;
+
+use WCPay\Core\Server\Request;
+use WC_Payments_API_Client;
+
+/**
+ * Request class for getting intents.
+ */
+class Get_Account extends Request {
+
+
+	/**
+	 * Returns the request's API.
+	 *
+	 * @return string
+	 */
+	public function get_api(): string {
+		return WC_Payments_API_Client::ACCOUNTS_API;
+	}
+
+	/**
+	 * Returns the request's HTTP method.
+	 */
+	public function get_method(): string {
+		return 'GET';
+	}
+
+	/**
+	 * Formats the response from the server.
+	 *
+	 * @param  mixed $response The response from `WC_Payments_API_Client::request`.
+	 * @return mixed           The same response from REST API.
+	 */
+	public function format_response( $response ) {
+		return $response;
+	}
+}
--- includes/wc-payment-api/class-wc-payments-api-client.php
+++ includes/wc-payment-api/class-wc-payments-api-client.php
@@ -1317,23 +1317,6 @@ class WC_Payments_API_Client {
 	}
 
 	/**
-	 * Get current account data
-	 *
-	 * @return array An array describing an account object.
-	 *
-	 * @throws API_Exception - Error contacting the API.
-	 */
-	public function get_account_data() {
-		return $this->request(
-			[
-				'test_mode' => WC_Payments::mode()->is_dev(), // only send a test mode request if in dev mode.
-			],
-			self::ACCOUNTS_API,
-			self::GET
-		);
-	}
-
-	/**
 	 * Get current platform checkout eligibility
 	 *
 	 * @return array An array describing platform checkout eligibility.
