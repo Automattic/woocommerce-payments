6372e1331b554af4d2b0f0a28b84ebb03194dc1f
Zvonimir Maglica <zvonimirmaglica4@gmail.com>
2023-03-28 10:21:44 +0200

    Removed unused list transactions function

--- includes/wc-payment-api/class-wc-payments-api-client.php
+++ includes/wc-payment-api/class-wc-payments-api-client.php
@@ -360,60 +360,6 @@ class WC_Payments_API_Client {
 	}
 
 	/**
-	 * List transactions
-	 *
-	 * @param int    $page       The requested page.
-	 * @param int    $page_size  The size of the requested page.
-	 * @param string $sort       The column to be used for sorting.
-	 * @param string $direction  The sorting direction.
-	 * @param array  $filters    The filters to be used in the query.
-	 * @param string $deposit_id The deposit to filter on.
-	 *
-	 * @return array
-	 * @throws API_Exception - Exception thrown on request failure.
-	 */
-	public function list_transactions( $page = 0, $page_size = 25, $sort = 'date', $direction = 'desc', $filters = [], $deposit_id = null ) {
-		// Map Order # terms to the actual charge id to be used in the server.
-		if ( ! empty( $filters['search'] ) ) {
-			$filters['search'] = WC_Payments_Utils::map_search_orders_to_charge_ids( $filters['search'] );
-		}
-
-		$query = array_merge(
-			$filters,
-			[
-				'page'       => $page,
-				'pagesize'   => $page_size,
-				'sort'       => $sort,
-				'direction'  => $direction,
-				'deposit_id' => $deposit_id,
-			]
-		);
-
-		$transactions = $this->request( $query, self::TRANSACTIONS_API, self::GET );
-
-		$charge_ids             = array_column( $transactions['data'], 'charge_id' );
-		$orders_with_charge_ids = count( $charge_ids ) ? $this->wcpay_db->orders_with_charge_id_from_charge_ids( $charge_ids ) : [];
-
-		// Add order information to each transaction available.
-		// TODO: Throw exception when `$transactions` or `$transaction` don't have the fields expected?
-		if ( isset( $transactions['data'] ) ) {
-			foreach ( $transactions['data'] as &$transaction ) {
-				foreach ( $orders_with_charge_ids as $order_with_charge_id ) {
-					if ( $order_with_charge_id['charge_id'] === $transaction['charge_id'] && ! empty( $transaction['charge_id'] ) ) {
-						$order                            = $order_with_charge_id['order'];
-						$transaction['order']             = $this->build_order_info( $order );
-						$transaction['payment_intent_id'] = $order->get_meta( '_intent_id' );
-					}
-				}
-			}
-			// Securing future changes from modifying reference content.
-			unset( $transaction );
-		}
-
-		return $transactions;
-	}
-
-	/**
 	 * Retrieves transaction list for a given fraud outcome status.
 	 *
 	 * @param List_Fraud_Outcome_Transactions $request Fraud outcome transactions request.
