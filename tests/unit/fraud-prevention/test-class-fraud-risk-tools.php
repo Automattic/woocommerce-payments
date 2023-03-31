ae1aa102d09b1f25abf1259c81448f545bdd6159
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2023-03-31 00:26:10 -0300

    Fix unit tests

--- tests/unit/fraud-prevention/test-class-fraud-risk-tools.php
+++ tests/unit/fraud-prevention/test-class-fraud-risk-tools.php
@@ -98,7 +98,7 @@ class Fraud_Risk_Tools_Test extends WCPAY_UnitTestCase {
 			'check'   => [
 				'key'      => 'order_total',
 				'operator' => 'greater_than',
-				'value'    => 100000,
+				'value'    => '100000|usd',
 			],
 		],
 	];
@@ -133,7 +133,7 @@ class Fraud_Risk_Tools_Test extends WCPAY_UnitTestCase {
 			'check'   => [
 				'key'      => 'order_total',
 				'operator' => 'greater_than',
-				'value'    => 100000,
+				'value'    => '100000|usd',
 			],
 		],
 	];
