b98779df4ac1d650cf4cfca609203c56c1bea7e1
Taha Paksu <3295+tpaksu@users.noreply.github.com>
2023-04-03 13:09:26 +0300

    Remove fraud and risk tools feature flag (#5941)

--- client/overview/test/index.js
+++ client/overview/test/index.js
@@ -93,7 +93,6 @@ describe( 'Overview page', () => {
 				accountOverviewTaskList: true,
 			},
 			accountLoans: {},
-			isFraudProtectionSettingsEnabled: true,
 			frtDiscoverBannerSettings: JSON.stringify( {
 				remindMeCount: 0,
 				remindMeAt: null,
@@ -293,20 +292,7 @@ describe( 'Overview page', () => {
 		} );
 	} );
 
-	it( 'does not render FRTDiscoverabilityBanner if feature flag option is false', () => {
-		global.wcpaySettings = {
-			...global.wcpaySettings,
-			isFraudProtectionSettingsEnabled: false,
-		};
-
-		render( <OverviewPage /> );
-
-		expect(
-			screen.queryByText( 'Enhanced fraud protection for your store' )
-		).not.toBeInTheDocument();
-	} );
-
-	it( 'renders FRTDiscoverabilityBanner if feature flag option is true', () => {
+	it( 'renders FRTDiscoverabilityBanner', () => {
 		render( <OverviewPage /> );
 
 		expect(
--- tests/e2e/utils/flows.js
+++ tests/e2e/utils/flows.js
@@ -399,6 +399,14 @@ export const merchantWCP = {
 			waitUntil: 'networkidle0',
 		} );
 
+		// Skip fraud protection tools tour.
+		const tourKitDismissButton = await page.$(
+			`.woocommerce-tour-kit button`
+		);
+		if ( tourKitDismissButton ) {
+			await tourKitDismissButton.click();
+		}
+
 		await page.$eval( paymentMethod, ( method ) => method.click() );
 		await new Promise( ( resolve ) => setTimeout( resolve, 2000 ) );
 		await expect( page ).toClick( 'button', {
@@ -410,6 +418,14 @@ export const merchantWCP = {
 		await page.goto( WCPAY_PAYMENT_SETTINGS, {
 			waitUntil: 'networkidle0',
 		} );
+
+		// Skip fraud protection tools tour.
+		const tourKitDismissButton = await page.$(
+			`.woocommerce-tour-kit button`
+		);
+		if ( tourKitDismissButton ) {
+			await tourKitDismissButton.click();
+		}
 
 		await page.$eval( paymentMethod, ( method ) => method.click() );
 		await expect( page ).toClick( 'button', {
@@ -535,6 +551,15 @@ export const merchantWCP = {
 
 	openWCPSettings: async () => {
 		await merchant.openSettings( 'checkout', 'woocommerce_payments' );
+	},
+
+	skipFraudProtectionTour: async () => {
+		const tourKitDismissButton = await page.$(
+			`.woocommerce-tour-kit button`
+		);
+		if ( tourKitDismissButton ) {
+			await tourKitDismissButton.click();
+		}
 	},
 
 	wcpSettingsSaveChanges: async () => {
