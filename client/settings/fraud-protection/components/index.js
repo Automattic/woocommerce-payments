48b658598929c5bcb4e2449e8e91c4839e955507
Dwain Maralack <dwain.maralack@gmail.com>
2023-04-03 08:41:51 +0200

    Remove dollar-amount related code

--- client/settings/fraud-protection/components/index.js
+++ client/settings/fraud-protection/components/index.js
@@ -1,14 +1,12 @@
 /**
  * Internal dependencies
  */
-import ExceedsDollarAmountRule from './exceeds-dollar-amount-rule';
 import ProtectionLevelModalNotice from './protection-level-modal-notice';
 import FraudProtectionHelpText from './fp-help-text';
 import ProtectionLevels from './protection-levels';
 import { BasicFraudProtectionModal } from './fp-modal';
 
 export {
-	ExceedsDollarAmountRule,
 	ProtectionLevelModalNotice,
 	FraudProtectionHelpText,
 	ProtectionLevels,
