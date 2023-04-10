4276eb4ae94f7b05067ce0b4136cd63a9a6ca0d0
Dat Hoang <htdat@users.noreply.github.com>
2023-04-10 14:37:45 +0700

    Remove constructor in trait Use_Test_Mode_Only_When_Dev_Mode, and use REQUIRED_PARAMS

--- includes/core/server/request/trait-use-test-mode-only-when-dev-mode.php
+++ includes/core/server/request/trait-use-test-mode-only-when-dev-mode.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Trait file for WCPay\Core\Server\Request\Request_Test_Mode_Only_For_Dev_Mode.
+ * Trait file for WCPay\Core\Server\Request\Use_Test_Mode_Only_When_Dev_Mode.
  *
  * @package WooCommerce Payments
  */
@@ -8,24 +8,17 @@
 namespace WCPay\Core\Server\Request;
 
 use WC_Payments;
-use WC_Payments_API_Client;
-use WC_Payments_Http_Interface;
-use WCPay\Core\Exceptions\Server\Request\Invalid_Request_Parameter_Exception;
 
 /**
  * Trait for requests that we only send a test mode request if in dev mode.
  */
 trait Use_Test_Mode_Only_When_Dev_Mode {
 	/**
-	 * Class constructor.
+	 * Set the test_mode parameter to true only if in dev mode.
 	 *
-	 * @param  WC_Payments_API_Client     $api_client  Api client.
-	 * @param  WC_Payments_Http_Interface $http_interface  Http interface.
-	 *
-	 * @throws Invalid_Request_Parameter_Exception
+	 * @return void
 	 */
-	public function __construct( WC_Payments_API_Client $api_client, WC_Payments_Http_Interface $http_interface ) {
-		parent::__construct( $api_client, $http_interface );
+	public function set_test_mode_only_when_dev_mode() {
 		$this->set_param( 'test_mode', WC_Payments::mode()->is_dev() );
 	}
 }
