edcebf069472f64f9afd67cec92bfe29a2b44c77
Allie Mims <allie.mims@automattic.com>
2023-04-12 15:42:28 -0400

    Convert JS files to TSX. Co-authored-by: Eduardo Pieretti Umpierre <eduardoumpierre@users.noreply.github.com>

--- client/globals.d.ts
+++ client/globals.d.ts
@@ -67,6 +67,7 @@ declare const wcpaySettings: {
 	};
 	accountDefaultCurrency: string;
 	isFraudProtectionSettingsEnabled: boolean;
+	frtDiscoverBannerSettings: string;
 };
 
 declare const wcTracks: any;
