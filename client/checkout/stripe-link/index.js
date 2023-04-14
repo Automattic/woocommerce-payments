f9aef6bc2f4d8d4a5fae673b4371ae27ed7b2f42
Timur Karimov <timurkarimov@timurs-macbook-pro.home>
2023-04-14 14:06:06 +0200

    Expand Stripe Link test coverage

--- client/checkout/stripe-link/index.js
+++ client/checkout/stripe-link/index.js
@@ -23,6 +23,91 @@ const showLinkButton = ( linkAutofill ) => {
 	} );
 };
 
+export const autofill = ( event, options ) => {
+	const { billingAddress, shippingAddress } = event.value;
+	const fillWith = options.fill_field_method
+		? options.fill_field_method
+		: ( address, nodeId, key ) => {
+				if ( null !== document.getElementById( nodeId ) ) {
+					document.getElementById( nodeId ).value =
+						address.address[ key ];
+				}
+		  };
+
+	if ( options.complete_shipping() ) {
+		const shippingNames = shippingAddress.name.split( / (.*)/s, 2 );
+		shippingAddress.address.last_name = shippingNames[ 1 ];
+		shippingAddress.address.first_name = shippingNames[ 0 ];
+
+		fillWith( shippingAddress, options.shipping_fields.line1, 'line1' );
+		fillWith( shippingAddress, options.shipping_fields.line2, 'line2' );
+		fillWith( shippingAddress, options.shipping_fields.city, 'city' );
+		fillWith( shippingAddress, options.shipping_fields.country, 'country' );
+		fillWith(
+			shippingAddress,
+			options.shipping_fields.first_name,
+			'first_name'
+		);
+		fillWith(
+			shippingAddress,
+			options.shipping_fields.last_name,
+			'last_name'
+		);
+		const billingCountryStateSelects = document.querySelectorAll(
+			'#billing_country, #billing_state, #shipping_country, #shipping_state'
+		);
+		billingCountryStateSelects.forEach( ( select ) =>
+			select.dispatchEvent( new Event( 'change' ) )
+		);
+		fillWith( shippingAddress, options.shipping_fields.state, 'state' );
+		fillWith(
+			shippingAddress,
+			options.shipping_fields.postal_code,
+			'postal_code'
+		);
+	}
+
+	if ( options.complete_billing() ) {
+		const billingNames = billingAddress.name.split( / (.*)/s, 2 );
+		billingAddress.address.last_name = billingNames[ 1 ];
+		billingAddress.address.first_name = billingNames[ 0 ];
+
+		fillWith( billingAddress, options.billing_fields.line1, 'line1' );
+		fillWith( billingAddress, options.billing_fields.line2, 'line2' );
+		fillWith( billingAddress, options.billing_fields.city, 'city' );
+		fillWith( billingAddress, options.billing_fields.country, 'country' );
+		fillWith(
+			billingAddress,
+			options.billing_fields.first_name,
+			'first_name'
+		);
+		fillWith(
+			billingAddress,
+			options.billing_fields.last_name,
+			'last_name'
+		);
+
+		const billingCountryStateSelects = document.querySelectorAll(
+			'#billing_country, #billing_state, #shipping_country, #shipping_state'
+		);
+		billingCountryStateSelects.forEach( ( select ) =>
+			select.dispatchEvent( new Event( 'change' ) )
+		);
+		fillWith( billingAddress, options.billing_fields.state, 'state' );
+		fillWith(
+			billingAddress,
+			options.billing_fields.postal_code,
+			'postal_code'
+		);
+	}
+	const billingCountryStateSelects = document.querySelectorAll(
+		'#billing_country, #billing_state, #shipping_country, #shipping_state'
+	);
+	billingCountryStateSelects.forEach( ( select ) =>
+		select.dispatchEvent( new Event( 'change' ) )
+	);
+};
+
 const enableStripeLinkPaymentMethod = ( options ) => {
 	if ( ! document.getElementById( options.emailId ) ) {
 		return;
@@ -42,96 +127,7 @@ const enableStripeLinkPaymentMethod = ( options ) => {
 	showButton( linkAutofill );
 
 	linkAutofill.on( 'autofill', ( event ) => {
-		const { billingAddress, shippingAddress } = event.value;
-		const fillWith = options.fill_field_method
-			? options.fill_field_method
-			: ( address, nodeId, key ) => {
-					if ( null !== document.getElementById( nodeId ) ) {
-						document.getElementById( nodeId ).value =
-							address.address[ key ];
-					}
-			  };
-
-		if ( options.complete_shipping() ) {
-			const shippingNames = shippingAddress.name.split( / (.*)/s, 2 );
-			shippingAddress.address.last_name = shippingNames[ 1 ];
-			shippingAddress.address.first_name = shippingNames[ 0 ];
-
-			fillWith( shippingAddress, options.shipping_fields.line1, 'line1' );
-			fillWith( shippingAddress, options.shipping_fields.line2, 'line2' );
-			fillWith( shippingAddress, options.shipping_fields.city, 'city' );
-			fillWith(
-				shippingAddress,
-				options.shipping_fields.country,
-				'country'
-			);
-			fillWith(
-				shippingAddress,
-				options.shipping_fields.first_name,
-				'first_name'
-			);
-			fillWith(
-				shippingAddress,
-				options.shipping_fields.last_name,
-				'last_name'
-			);
-			const billingCountryStateSelects = document.querySelectorAll(
-				'#billing_country, #billing_state, #shipping_country, #shipping_state'
-			);
-			billingCountryStateSelects.forEach( ( select ) =>
-				select.dispatchEvent( new Event( 'change' ) )
-			);
-			fillWith( shippingAddress, options.shipping_fields.state, 'state' );
-			fillWith(
-				shippingAddress,
-				options.shipping_fields.postal_code,
-				'postal_code'
-			);
-		}
-
-		if ( options.complete_billing() ) {
-			const billingNames = billingAddress.name.split( / (.*)/s, 2 );
-			billingAddress.address.last_name = billingNames[ 1 ];
-			billingAddress.address.first_name = billingNames[ 0 ];
-
-			fillWith( billingAddress, options.billing_fields.line1, 'line1' );
-			fillWith( billingAddress, options.billing_fields.line2, 'line2' );
-			fillWith( billingAddress, options.billing_fields.city, 'city' );
-			fillWith(
-				billingAddress,
-				options.billing_fields.country,
-				'country'
-			);
-			fillWith(
-				billingAddress,
-				options.billing_fields.first_name,
-				'first_name'
-			);
-			fillWith(
-				billingAddress,
-				options.billing_fields.last_name,
-				'last_name'
-			);
-
-			const billingCountryStateSelects = document.querySelectorAll(
-				'#billing_country, #billing_state, #shipping_country, #shipping_state'
-			);
-			billingCountryStateSelects.forEach( ( select ) =>
-				select.dispatchEvent( new Event( 'change' ) )
-			);
-			fillWith( billingAddress, options.billing_fields.state, 'state' );
-			fillWith(
-				billingAddress,
-				options.billing_fields.postal_code,
-				'postal_code'
-			);
-		}
-		const billingCountryStateSelects = document.querySelectorAll(
-			'#billing_country, #billing_state, #shipping_country, #shipping_state'
-		);
-		billingCountryStateSelects.forEach( ( select ) =>
-			select.dispatchEvent( new Event( 'change' ) )
-		);
+		autofill( event, options );
 	} );
 };
 
