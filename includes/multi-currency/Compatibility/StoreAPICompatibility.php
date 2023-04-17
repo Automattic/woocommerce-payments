f10df13068a7b521161349f0f9d166e13bb99b04
Taha Paksu <3295+tpaksu@users.noreply.github.com>
2023-04-18 01:50:45 +0300

    Limit the GET param modification to only product query

--- includes/multi-currency/Compatibility/StoreAPICompatibility.php
+++ includes/multi-currency/Compatibility/StoreAPICompatibility.php
@@ -27,33 +27,27 @@ class StoreAPICompatibility extends BaseCompatibility {
 		$formatters = $container->get( Formatters::class );
 		$formatters->register( 'currency', Helpers\MultiCurrencyFormatter::class );
 		// Convert GET parameter prices before it is applied to posts query.
-		add_filter( 'posts_clauses', [ $this, 'convert_prices_if_min_max_price' ], 9, 2 );
+		add_filter( 'pre_get_posts', [ $this, 'modify_query_vars_currency_for_price_filter' ] );
 	}
 
 	/**
 	 * Converts the min_price and max_price filters back to the store currency.
 	 *
-	 * @param   array    $args      Arguments array.
-	 * @param   WP_Query $wp_query  The query object to modify args.
+	 * @param   WP_Query $wp_query  The WP query vars to modify.
 	 *
 	 * @return  array
 	 */
-	public function convert_prices_if_min_max_price( $args, $wp_query ) {
-		/**
-		 * Context:
-		 * The `price_filter_post_clauses` method checks the $_GET['min_price'] or $_GET['max_price'] and limits the
-		 * product display to those limits by directly applying the parameters to the DB query, which is always saved
-		 * in the store currency, and when MC is involved, the prices on the querystring are sent as the customer currency.
-		 * To match the price limits on the querystring to the saved values on the DB, the filters need to be converted
-		 * back to the store currency.
-		 *
-		 * Permalink: https://github.com/woocommerce/woocommerce/blob/trunk/plugins/woocommerce/includes/class-wc-query.php#L670-L673
-		 */
-
+	public function modify_query_vars_currency_for_price_filter( $wp_query ) {
 		// phpcs:disable WordPress.Security.NonceVerification.Recommended
-		if ( ! $wp_query->is_main_query() || isset( $_GET['are_prices_converted'] ) || ( ! isset( $_GET['max_price'] ) && ! isset( $_GET['min_price'] ) ) ) {
-			return $args;
+		if (
+			! isset( $wp_query->query_vars['wc_query'] )
+			|| 'product_query' !== $wp_query->query_vars['wc_query']
+			|| isset( $_GET['are_prices_converted'] )
+			|| ( ! isset( $_GET['max_price'] ) && ! isset( $_GET['min_price'] ) )
+		) {
+			return $wp_query;
 		}
+
 		$currency_rate = $this->multi_currency->get_selected_currency()->get_rate();
 		if ( isset( $_GET['min_price'] ) ) {
 			$_GET['min_price'] = floatval( wp_unslash( $_GET['min_price'] ) ) / $currency_rate;
@@ -63,7 +57,6 @@ class StoreAPICompatibility extends BaseCompatibility {
 		}
 		$_GET['are_prices_converted'] = true;
 		// phpcs:enable WordPress.Security.NonceVerification.Recommended
-
-		return $args;
+		return $wp_query;
 	}
 }
