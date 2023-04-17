69af1de4e09ae1cadfdd72d1fd00cd36690f0fdd
Taha Paksu <3295+tpaksu@users.noreply.github.com>
2023-04-18 00:42:57 +0300

    Limit StoreApi formatter modification only for ProductCollectionSchema

--- includes/multi-currency/Compatibility/Helpers/MultiCurrencyFormatter.php
+++ includes/multi-currency/Compatibility/Helpers/MultiCurrencyFormatter.php
@@ -9,11 +9,29 @@ namespace WCPay\MultiCurrency\Compatibility\Helpers;
 
 use Automattic\WooCommerce\StoreApi\Formatters\CurrencyFormatter;
 use WCPay\MultiCurrency\MultiCurrency;
+use WCPay\MultiCurrency\Utils;
 
 /**
  * Class that controls currency conversion in StoreAPI.
  */
 class MultiCurrencyFormatter {
+
+	/**
+	 * Multicurrency Utility class.
+	 *
+	 * @var WCPay\MultiCurrency\Utils
+	 */
+	protected $utils;
+
+	/**
+	 * Constructor.
+	 *
+	 * @return  void
+	 */
+	public function __construct() {
+		$this->utils = new Utils();
+	}
+
 	/**
 	 * Format a given value and return the result.
 	 *
@@ -22,11 +40,14 @@ class MultiCurrencyFormatter {
 	 * @return array
 	 */
 	public function format( $value, array $options = [] ) {
-		$original_formatter = new CurrencyFormatter();
-		if ( is_array( $value ) ) {
-			foreach ( $value as $key => $original ) {
-				if ( is_string( $original ) ) {
-					$value[ $key ] = (string) MultiCurrency::instance()->get_price( $original, 'product' );
+		$is_call_for_price_filter = $this->utils->is_call_in_backtrace( [ 'Automattic\WooCommerce\StoreApi\Schemas\V1\ProductCollectionDataSchema->get_item_response' ] );
+		$original_formatter       = new CurrencyFormatter();
+		if ( $is_call_for_price_filter ) {
+			if ( is_array( $value ) ) {
+				foreach ( $value as $key => $original ) {
+					if ( is_string( $original ) ) {
+						$value[ $key ] = (string) MultiCurrency::instance()->get_price( $original, 'product' );
+					}
 				}
 			}
 		}
