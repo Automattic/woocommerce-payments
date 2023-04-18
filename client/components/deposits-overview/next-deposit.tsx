0767eccac54794e80047ba2a6477baf831c8951c
Rua Haszard <rua.haszard@automattic.com>
2023-04-18 18:17:54 +1200

    Display a deposits paused notice when the account has a negative available balance (#5957)
    Co-authored-by: Rua Haszard <rua@automattic.com>
    Co-authored-by: James Allan <james.allan@automattic.com>

--- client/components/deposits-overview/next-deposit.tsx
+++ client/components/deposits-overview/next-deposit.tsx
@@ -18,18 +18,100 @@ import { __ } from '@wordpress/i18n';
 /**
  * Internal dependencies.
  */
-import strings from './strings';
 import Loadable from 'components/loadable';
 import { getNextDeposit } from './utils';
 import DepositStatusPill from 'components/deposit-status-pill';
 import { getDepositDate } from 'deposits/utils';
-import { useDepositIncludesLoan } from 'wcpay/data';
+import { useDepositIncludesLoan, useAllDepositsOverviews } from 'wcpay/data';
 import BannerNotice from 'wcpay/components/banner-notice';
+import { useSelectedCurrency } from 'wcpay/overview/hooks';
 
 type NextDepositProps = {
 	isLoading: boolean;
 	overview?: AccountOverview.Overview;
 };
+
+const DepositIncludesLoanPayoutNotice = () => (
+	<BannerNotice
+		status="warning"
+		icon={ <InfoOutlineIcon /> }
+		isDismissible={ false }
+	>
+		{ interpolateComponents( {
+			mixedString: __(
+				'This deposit will include funds from your WooCommerce Capital loan. {{learnMoreLink}}Learn more{{/learnMoreLink}}',
+				'woocommerce-payments'
+			),
+			components: {
+				learnMoreLink: (
+					// Link content is in the format string above. Consider disabling jsx-a11y/anchor-has-content.
+					// eslint-disable-next-line jsx-a11y/anchor-has-content
+					<a
+						href={
+							'https://woocommerce.com/document/woocommerce-payments/stripe-capital/overview'
+						}
+						target="_blank"
+						rel="noreferrer"
+					/>
+				),
+			},
+		} ) }
+	</BannerNotice>
+);
+
+const NewAccountWaitingPeriodNotice = () => (
+	<BannerNotice
+		status="warning"
+		icon={ <NoticeOutlineIcon /> }
+		className="new-account-waiting-period-notice"
+		isDismissible={ false }
+	>
+		{ interpolateComponents( {
+			mixedString: __(
+				'Your first deposit is held for seven business days. {{whyLink}}Why?{{/whyLink}}',
+				'woocommerce-payments'
+			),
+			components: {
+				whyLink: (
+					// Link content is in the format string above. Consider disabling jsx-a11y/anchor-has-content.
+					// eslint-disable-next-line jsx-a11y/anchor-has-content
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						href="https://woocommerce.com/document/woocommerce-payments/deposits/deposit-schedule/#section-1"
+					/>
+				),
+			},
+		} ) }
+	</BannerNotice>
+);
+
+const NegativeBalanceDepositsPausedNotice = () => (
+	<BannerNotice
+		status="warning"
+		icon={ <NoticeOutlineIcon /> }
+		className="negative-balance-deposits-paused-notice"
+		isDismissible={ false }
+	>
+		{ interpolateComponents( {
+			mixedString: __(
+				'Deposits may be interrupted while your WooCommerce Payments balance remains negative. {{whyLink}}Why?{{/whyLink}}',
+				'woocommerce-payments'
+			),
+			components: {
+				whyLink: (
+					// Link content is in the format string above. Consider disabling jsx-a11y/anchor-has-content.
+					// eslint-disable-next-line jsx-a11y/anchor-has-content
+					<a
+						target="_blank"
+						rel="noopener noreferrer"
+						href="https://woocommerce.com/document/woocommerce-payments/fees-and-debits/account-showing-negative-balance/"
+					/>
+				),
+			},
+		} ) }
+	</BannerNotice>
+);
 
 /**
  * Renders the Next Deposit details component.
@@ -49,12 +131,25 @@ const NextDepositDetails: React.FC< NextDepositProps > = ( {
 		nextDeposit.date > 0 ? nextDeposit : null
 	);
 
-	// Next deposit related notices logic.
 	const { includesFinancingPayout } = useDepositIncludesLoan(
 		nextDeposit.id
 	);
 	const completedWaitingPeriod =
 		wcpaySettings.accountStatus.deposits?.completed_waiting_period;
+
+	const {
+		overviews,
+	} = useAllDepositsOverviews() as AccountOverview.OverviewsResponse;
+	const { selectedCurrency } = useSelectedCurrency();
+	const displayedCurrency =
+		selectedCurrency ?? wcpaySettings.accountDefaultCurrency;
+
+	const availableBalance = overviews?.currencies.find(
+		( currencyOverview ) => displayedCurrency === currencyOverview.currency
+	)?.available;
+
+	const negativeBalanceDepositsPaused =
+		availableBalance && availableBalance.amount < 0;
 
 	return (
 		<>
@@ -64,19 +159,22 @@ const NextDepositDetails: React.FC< NextDepositProps > = ( {
 					<FlexItem className={ `${ tableClass }__cell` }>
 						<Loadable
 							isLoading={ isLoading }
-							value={ strings.tableHeaders.nextDepositDate }
+							value={ __(
+								'Estimated dispatch date',
+								'woocommerce-payments'
+							) }
 						/>
 					</FlexItem>
 					<FlexItem className={ `${ tableClass }__cell` }>
 						<Loadable
 							isLoading={ isLoading }
-							value={ strings.tableHeaders.status }
+							value={ __( 'Status', 'woocommerce-payments' ) }
 						/>
 					</FlexItem>
 					<FlexItem className={ `${ tableClass }__cell` }>
 						<Loadable
 							isLoading={ isLoading }
-							value={ strings.tableHeaders.amount }
+							value={ __( 'Amount', 'woocommerce-payments' ) }
 						/>
 					</FlexItem>
 				</Flex>
@@ -119,61 +217,14 @@ const NextDepositDetails: React.FC< NextDepositProps > = ( {
 				<CardBody
 					className={ 'wcpay-deposits-overview__notices__container' }
 				>
-					{ /* Deposit includes capital funds notice */ }
 					{ includesFinancingPayout && (
-						<BannerNotice
-							status="warning"
-							icon={ <InfoOutlineIcon /> }
-							isDismissible={ false }
-						>
-							{ interpolateComponents( {
-								mixedString:
-									strings.notices.depositIncludesLoan +
-									__(
-										' {{learnMoreLink}}Learn more{{/learnMoreLink}}',
-										'woocommerce-payments'
-									),
-								components: {
-									learnMoreLink: (
-										// eslint-disable-next-line jsx-a11y/anchor-has-content
-										<a
-											href={
-												strings.documentationUrls
-													.capital
-											}
-											target="_blank"
-											rel="noreferrer"
-										/>
-									),
-								},
-							} ) }
-						</BannerNotice>
+						<DepositIncludesLoanPayoutNotice />
 					) }
-					{ /* New account waiting period notice */ }
 					{ ! completedWaitingPeriod && (
-						<BannerNotice
-							status="warning"
-							icon={ <NoticeOutlineIcon /> }
-							className="new-account-waiting-period-notice"
-							isDismissible={ false }
-						>
-							{ interpolateComponents( {
-								mixedString: __(
-									'Your first deposit is held for seven business days. {{whyLink}}Why?{{/whyLink}}',
-									'woocommerce-payments'
-								),
-								components: {
-									whyLink: (
-										// eslint-disable-next-line jsx-a11y/anchor-has-content
-										<a
-											target="_blank"
-											rel="noopener noreferrer"
-											href="https://woocommerce.com/document/woocommerce-payments/deposits/deposit-schedule/#section-1"
-										/>
-									),
-								},
-							} ) }
-						</BannerNotice>
+						<NewAccountWaitingPeriodNotice />
+					) }
+					{ negativeBalanceDepositsPaused && (
+						<NegativeBalanceDepositsPausedNotice />
 					) }
 				</CardBody>
 			) }
