1c6a629b5837be3750fcdfb62fdc15560edb8d9d
Mike Moore <michael.moore@automattic.com>
2023-04-19 12:29:51 -0400

    Update variable names

--- includes/class-wc-payments-express-checkout-button-display-handler.php
+++ includes/class-wc-payments-express-checkout-button-display-handler.php
@@ -50,14 +50,15 @@ class WC_Payments_Express_Checkout_Button_Display_Handler {
 		$this->platform_checkout_button_handler->init();
 		$this->payment_request_button_handler->init();
 
-		$is_woopay_express_checkout_enabled = WC_Payments_Features::is_woopay_express_checkout_enabled();
+		$is_woopay_express_checkout_enabled = $this->platform_checkout_button_handler->is_woopay_enabled();
+		$is_payment_request_enabled         = 'yes' === $this->gateway->get_option( 'payment_request' );
 
-		if ( $is_woopay_express_checkout_enabled || 'yes' === $this->gateway->get_option( 'payment_request' ) ) {
+		if ( $is_woopay_express_checkout_enabled || $is_payment_request_enabled ) {
 			add_action( 'woocommerce_after_add_to_cart_quantity', [ $this, 'display_express_checkout_buttons' ], 1 );
 			add_action( 'woocommerce_proceed_to_checkout', [ $this, 'display_express_checkout_buttons' ], 1 );
 			add_action( 'woocommerce_checkout_before_customer_details', [ $this, 'display_express_checkout_buttons' ], 1 );
 
-			if ( 'yes' === $this->gateway->get_option( 'payment_request' ) ) {
+			if ( $is_payment_request_enabled ) {
 				// Load separator on the Pay for Order page.
 				add_action( 'before_woocommerce_pay_form', [ $this, 'display_express_checkout_buttons' ], 1 );
 			}
@@ -70,10 +71,10 @@ class WC_Payments_Express_Checkout_Button_Display_Handler {
 	 * @return void
 	 */
 	public function display_express_checkout_separator_if_necessary() {
-		$woopay          = $this->platform_checkout_button_handler->is_woopay_enabled() && $this->platform_checkout_button_handler->should_show_platform_checkout_button();
-		$payment_request = $this->payment_request_button_handler->should_show_payment_request_button();
-		$should_hide     = $payment_request && ! $woopay;
-		if ( $woopay || $payment_request ) {
+		$should_show_woopay          = $this->platform_checkout_button_handler->should_show_platform_checkout_button();
+		$should_show_payment_request = $this->payment_request_button_handler->should_show_payment_request_button();
+		$should_hide                 = $should_show_payment_request && ! $should_show_woopay;
+		if ( $should_show_woopay || $should_show_payment_request ) {
 			?>
 			<p id="wcpay-payment-request-button-separator" style="margin-top:1.5em;text-align:center;<?php echo $should_hide ? 'display:none;' : ''; ?>">&mdash; <?php esc_html_e( 'OR', 'woocommerce-payments' ); ?> &mdash;</p>
 			<?php
