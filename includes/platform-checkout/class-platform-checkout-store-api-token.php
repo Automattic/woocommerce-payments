02c005bf56f85580ba76ed8c69b2e85ec479e0c1
Ricardo Metring <ricardometring@gmail.com>
2023-04-25 14:40:51 -0300

    Fix class `AbstractCartRoute` not found (#6146)

--- includes/platform-checkout/class-platform-checkout-store-api-token.php
+++ includes/platform-checkout/class-platform-checkout-store-api-token.php
@@ -9,51 +9,53 @@ namespace WCPay\Platform_Checkout;
 
 use Automattic\WooCommerce\StoreApi\Routes\V1\AbstractCartRoute;
 
-/**
- * This class is used to get the cart token from the cart route.
- */
-class Platform_Checkout_Store_Api_Token extends AbstractCartRoute {
-
+if ( class_exists( AbstractCartRoute::class ) ) {
 	/**
-	 * Helper method to get the instance of the class.
-	 *
-	 * @return Platform_Checkout_Store_Api_Token The instance of the class.
-	 *
-	 * @psalm-suppress InvalidArgument Psalm thinks namespace is incorrect.
+	 * This class is used to get the cart token from the cart route.
 	 */
-	public static function init() {
-		$formatters        = new \Automattic\WooCommerce\StoreApi\Formatters();
-		$extend_schema     = new \Automattic\WooCommerce\StoreApi\Schemas\ExtendSchema( $formatters );
-		$schema_controller = new \Automattic\WooCommerce\StoreApi\SchemaController( $extend_schema );
-		return new self( $schema_controller, $schema_controller->get( 'cart' ) );
-	}
+	class Platform_Checkout_Store_Api_Token extends AbstractCartRoute {
 
-	/**
-	 * Get the path of this REST route.
-	 *
-	 * @throws \Exception Throws exception because this method is not meant to be implemented in this utility class.
-	 */
-	public function get_path() {
-		throw new \Exception( 'Not implemented' );
-	}
+		/**
+		 * Helper method to get the instance of the class.
+		 *
+		 * @return Platform_Checkout_Store_Api_Token The instance of the class.
+		 *
+		 * @psalm-suppress InvalidArgument Psalm thinks namespace is incorrect.
+		 */
+		public static function init() {
+			$formatters        = new \Automattic\WooCommerce\StoreApi\Formatters();
+			$extend_schema     = new \Automattic\WooCommerce\StoreApi\Schemas\ExtendSchema( $formatters );
+			$schema_controller = new \Automattic\WooCommerce\StoreApi\SchemaController( $extend_schema );
+			return new self( $schema_controller, $schema_controller->get( 'cart' ) );
+		}
 
-	/**
-	 * Get arguments for this REST route.
-	 *
-	 * @throws \Exception Throws exception because this method is not meant to be implemented in this utility class.
-	 */
-	public function get_args() {
-		throw new \Exception( 'Not implemented' );
-	}
+		/**
+		 * Get the path of this REST route.
+		 *
+		 * @throws \Exception Throws exception because this method is not meant to be implemented in this utility class.
+		 */
+		public function get_path() {
+			throw new \Exception( 'Not implemented' );
+		}
 
-    //phpcs:disable Generic.CodeAnalysis.UselessOverridingMethod
-	/**
-	 * This function is used to get the cart token from the cart route.
-	 *
-	 * @return string The cart token.
-	 * @psalm-suppress UndefinedMethod
-	 */
-	public function get_cart_token() {
-		return parent::get_cart_token();
+		/**
+		 * Get arguments for this REST route.
+		 *
+		 * @throws \Exception Throws exception because this method is not meant to be implemented in this utility class.
+		 */
+		public function get_args() {
+			throw new \Exception( 'Not implemented' );
+		}
+
+		//phpcs:disable Generic.CodeAnalysis.UselessOverridingMethod
+		/**
+		 * This function is used to get the cart token from the cart route.
+		 *
+		 * @return string The cart token.
+		 * @psalm-suppress UndefinedMethod
+		 */
+		public function get_cart_token() {
+			return parent::get_cart_token();
+		}
 	}
 }
