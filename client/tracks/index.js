6ec9c3735e749f1fbd15a667917c26a992f96d84
Rua Haszard <rua@automattic.com>
2023-05-03 08:29:43 +1200

    fix jsdoc indentation - use spaces

--- client/tracks/index.js
+++ client/tracks/index.js
@@ -17,8 +17,8 @@ function isEnabled() {
 /**
  * Records site event.
  *
- * @param {string}  eventName       	Name of the event.
- * @param {Object} [eventProperties]	Event properties (optional).
+ * @param {string}  eventName        Name of the event.
+ * @param {Object} [eventProperties] Event properties (optional).
  */
 function recordEvent( eventName, eventProperties ) {
 	// Wc-admin track script is enqueued after ours, wrap in domReady
@@ -33,7 +33,7 @@ function recordEvent( eventName, eventProperties ) {
 /**
  * Records events from buyers (aka shoppers).
  *
- * @param {string}  eventName       	Name of the event.
+ * @param {string}  eventName         Name of the event.
  * @param {Object}  [eventProperties] Event properties (optional).
  */
 function recordUserEvent( eventName, eventProperties ) {
