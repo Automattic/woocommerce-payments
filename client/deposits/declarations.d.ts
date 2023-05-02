05e1ef0cb594f567981429b7f76a187eea478cfe
Rua Haszard <rua@automattic.com>
2023-05-02 15:35:49 +1200

    extend our Link props typedef to allow onClick: - this is supported by the woo component - see https://github.com/woocommerce/woocommerce/blob/trunk/packages/js/components/src/link/index.tsx

--- client/deposits/declarations.d.ts
+++ client/deposits/declarations.d.ts
@@ -32,6 +32,7 @@ declare module '@woocommerce/components' {
 		href: string;
 		children?: React.ReactNode;
 		type?: string;
+		onClick?: ( ...props: any ) => any;
 	};
 	const Link: ( props: LinkParams ) => JSX.Element;
 
