cd2562eb487eb6a8e02dced3c3cc9686b8f80e3a
Guilherme Pressutto <gpressutto5@gmail.com>
2023-05-11 16:27:22 -0300

    Fixed auto redirection on checkout page

--- client/checkout/woopay/email-input-iframe.js
+++ client/checkout/woopay/email-input-iframe.js
@@ -459,12 +459,6 @@ export const handleWooPayEmailInput = async (
 				hasCheckedLoginSession = true;
 				api.initWooPay( '', e.data.platformCheckoutUserSession )
 					.then( ( response ) => {
-						// Do nothing if the iframe has been closed.
-						if (
-							! document.querySelector( '.woopay-otp-iframe' )
-						) {
-							return;
-						}
 						if ( 'success' === response.result ) {
 							loginSessionIframeWrapper.classList.add(
 								'woopay-login-session-iframe-wrapper'
@@ -474,6 +468,14 @@ export const handleWooPayEmailInput = async (
 								wcpayTracks.events.WOOPAY_AUTO_REDIRECT
 							);
 							spinner.remove();
+							// Do nothing if the iframe has been closed.
+							if (
+								! document.querySelector(
+									'.woopay-login-session-iframe'
+								)
+							) {
+								return;
+							}
 							window.location = response.url;
 						} else {
 							closeLoginSessionIframe();
