bf15bf29097216876e73ef931aea686c04e6a7b8
Eric Jinks <3147296+Jinksi@users.noreply.github.com>
2023-05-12 11:13:10 +1000

    Refactor deposits/utils/index.js to TS to improve code quality

--- client/deposits/utils/index.ts
+++ client/deposits/utils/index.ts
@@ -5,20 +5,27 @@ import { __ } from '@wordpress/i18n';
 import { dateI18n } from '@wordpress/date';
 import moment from 'moment';
 
-const formatDate = ( format, date ) =>
+const formatDate = ( format: string, date: number | string ) =>
 	dateI18n(
 		format,
 		moment.utc( date ).toISOString(),
 		true // TODO Change call to gmdateI18n and remove this deprecated param once WP 5.4 support ends.
 	);
 
-export const getDepositDate = ( deposit ) =>
-	deposit ? formatDate( 'F j, Y', deposit.date ) : '—';
+interface DepositObject {
+	date: number | string;
+}
+export const getDepositDate = ( deposit: DepositObject | null ): string =>
+	deposit ? formatDate( 'F j, Y', deposit?.date ) : '—';
 
+interface GetDepositMonthlyAnchorLabelProps {
+	monthlyAnchor: number;
+	capitalize?: boolean;
+}
 export const getDepositMonthlyAnchorLabel = ( {
 	monthlyAnchor,
 	capitalize = true,
-} ) => {
+}: GetDepositMonthlyAnchorLabelProps ): string => {
 	// If locale is set up as en_US or en_GB the ordinal will not show up
 	// More details can be found in https://github.com/WordPress/gutenberg/issues/15221/
 	// Using 'en' as the locale should be enough to workaround it
