e8791490eea407500bc183b5e90b1baed3d645f3
Samir Merchant <samir.merchant@automattic.com>
2023-05-16 17:30:14 -0400

    moves cancel_authorization hook to checkout class

--- includes/class-wc-payments-checkout.php
+++ includes/class-wc-payments-checkout.php
@@ -75,6 +75,7 @@ class WC_Payments_Checkout {
 
 		add_action( 'wc_payments_add_payment_fields', [ $this, 'payment_fields' ] );
 		add_action( 'woocommerce_order_action_capture_charge', [ $this->gateway, 'capture_charge' ] );
+		add_action( 'woocommerce_order_action_cancel_authorization', [ $this->gateway, 'cancel_authorization' ] );
 	}
 
 	/**
