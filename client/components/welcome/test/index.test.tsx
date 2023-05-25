09bec96db9f108df7fc43fe23826dba1bc849f99
Eric Jinks <3147296+Jinksi@users.noreply.github.com>
2023-05-26 06:25:30 +1000

    Improve Welcome tests to focus on main cases

--- client/components/welcome/test/index.test.tsx
+++ client/components/welcome/test/index.test.tsx
@@ -8,39 +8,49 @@ import { render } from '@testing-library/react';
  * Internal dependencies
  */
 import Welcome from '..';
-import { getGreeting } from '../utils';
 import { useCurrentWpUser } from '../hooks';
-
-jest.mock( '../utils', () => ( {
-	getGreeting: jest.fn(),
-} ) );
 
 jest.mock( '../hooks', () => ( {
 	useCurrentWpUser: jest.fn(),
 } ) );
 
-const mockGetGreeting = getGreeting as jest.MockedFunction<
-	typeof getGreeting
->;
 const mockUseCurrentWpUser = useCurrentWpUser as jest.MockedFunction<
 	typeof useCurrentWpUser
 >;
 
-const mockUser = {
-	id: 123,
-	first_name: 'Tester',
-	username: 'admin',
-	name: 'admin',
-	nickname: 'Tester-nickname',
-	last_name: 'Tester-lastname',
-	email: 'tester@test.com',
-	locale: 'en',
-};
-
 describe( 'Welcome', () => {
-	test( 'renders the correct greeting in the header', () => {
-		const expectedGreeting = 'Good afternoon, Tester 👋';
-		mockGetGreeting.mockReturnValue( expectedGreeting );
+	test( 'renders the correct greeting when the user first name exists', () => {
+		const mockUser = {
+			id: 123,
+			first_name: 'Tester',
+			username: 'admin',
+			name: 'admin',
+			nickname: 'Tester-nickname',
+			last_name: 'Tester-lastname',
+			email: 'tester@test.com',
+			locale: 'en',
+		};
+		const expectedGreeting = /Good (morning|afternoon|evening), Tester 👋/;
+		mockUseCurrentWpUser.mockReturnValue( {
+			user: mockUser,
+			isLoading: false,
+		} );
+		const { getByText } = render( <Welcome /> );
+		getByText( expectedGreeting );
+	} );
+
+	test( 'renders the correct greeting when the user first name is empty', () => {
+		const mockUser = {
+			id: 123,
+			first_name: '',
+			username: 'admin',
+			name: 'admin',
+			nickname: 'Tester-nickname',
+			last_name: 'Tester-lastname',
+			email: 'tester@test.com',
+			locale: 'en',
+		};
+		const expectedGreeting = /Good (morning|afternoon|evening) 👋/;
 		mockUseCurrentWpUser.mockReturnValue( {
 			user: mockUser,
 			isLoading: false,
