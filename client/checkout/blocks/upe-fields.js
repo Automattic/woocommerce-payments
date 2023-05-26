033986fad9929e647adf69cf6652721241e93819
Ahmed <ahmed.el.azzabi@automattic.com>
2023-05-26 15:36:25 +0100

    fix i18n issues

--- client/checkout/blocks/upe-fields.js
+++ client/checkout/blocks/upe-fields.js
@@ -232,8 +232,10 @@ const WCPayUPEFields = ( {
 				) {
 					return {
 						type: 'error',
-						message:
+						message: __(
 							'This payment method can not be saved for future use.',
+							'woocommerce-payments'
+						),
 					};
 				}
 
