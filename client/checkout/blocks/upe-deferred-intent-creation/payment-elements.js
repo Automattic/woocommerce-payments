0fc981a30440c397c0c1d963c48d4390898c1cdb
Timur Karimov <timurkarimov@timurs-macbook-pro.home>
2023-06-16 17:03:59 +0200

    Rename scripts and re-group functions

--- client/checkout/blocks/upe-deferred-intent-creation/payment-elements.js
+++ client/checkout/blocks/upe-deferred-intent-creation/payment-elements.js
@@ -3,219 +3,13 @@
  */
 import { getAppearance } from 'wcpay/checkout/upe-styles';
 import { getUPEConfig } from 'wcpay/utils/checkout';
-import { useFingerprint, usePaymentCompleteHandler } from '../hooks';
+import { useFingerprint } from '../hooks';
 import { LoadableBlock } from 'wcpay/components/loadable';
-import {
-	Elements,
-	PaymentElement,
-	useElements,
-	useStripe,
-} from '@stripe/react-stripe-js';
-import {
-	getPaymentMethods,
-	// eslint-disable-next-line import/no-unresolved
-} from '@woocommerce/blocks-registry';
+import { Elements } from '@stripe/react-stripe-js';
 import { useEffect, useState } from 'react';
-import {
-	getStripeElementOptions,
-	useCustomerData,
-} from 'wcpay/checkout/utils/upe';
-import { __ } from '@wordpress/i18n';
+import PaymentProcessor from './payment-processor';
 
-const getBillingDetails = ( billingData ) => {
-	return {
-		name: ( billingData.first_name + ' ' + billingData.last_name ).trim(),
-		email: billingData.email,
-		phone: billingData.phone,
-		address: {
-			city: billingData.city,
-			country: billingData.country,
-
-			line1: billingData.address_1,
-			line2: billingData.address_2,
-			postal_code: billingData.postcode,
-			state: billingData.state,
-		},
-	};
-};
-
-const getFraudPreventionToken = () => {
-	return document
-		.querySelector( '#wcpay-fraud-prevention-token' )
-		?.getAttribute( 'value' );
-};
-
-/**
- * Validates the Stripe elements by submitting them and handling any errors that occur during submission.
- * If an error occurs, the function removes loading effect from the provided jQuery form and thus unblocks it,
- * and shows an error message in the checkout.
- *
- * @param {Object} elements The Stripe elements object to be validated.
- * @return {Promise} Promise for the checkout submission.
- */
-const validateElements = ( elements ) => {
-	return elements.submit().then( ( result ) => {
-		if ( result.error ) {
-			throw new Error( result.error.message );
-		}
-	} );
-};
-
-const WCPayUPEFields = ( {
-	api,
-	activePaymentMethod,
-	testingInstructions,
-	eventRegistration: { onPaymentSetup, onCheckoutAfterProcessingWithSuccess },
-	emitResponse,
-	paymentMethodId,
-	upeMethods,
-	errorMessage,
-	shouldSavePayment,
-	fingerprint,
-} ) => {
-	const stripe = useStripe();
-	const elements = useElements();
-	const [ isUPEComplete, setIsUPEComplete ] = useState( false );
-	const [ selectedUPEPaymentType, setSelectedUPEPaymentType ] = useState(
-		''
-	);
-
-	const paymentMethodsConfig = getUPEConfig( 'paymentMethodsConfig' );
-	const isTestMode = getUPEConfig( 'testMode' );
-	const testingInstructionsIfAppropriate = isTestMode
-		? testingInstructions
-		: '';
-	const gatewayConfig = getPaymentMethods()[ upeMethods[ paymentMethodId ] ];
-	const billingData = useCustomerData().billingAddress;
-
-	useEffect(
-		() =>
-			onPaymentSetup( () => {
-				async function handlePaymentProcessing() {
-					if (
-						upeMethods[ paymentMethodId ] !== activePaymentMethod
-					) {
-						return;
-					}
-
-					if ( ! isUPEComplete ) {
-						return {
-							type: 'error',
-							message: __(
-								'Your payment information is incomplete.',
-								'woocommerce-payments'
-							),
-						};
-					}
-
-					if ( errorMessage ) {
-						return {
-							type: 'error',
-							message: errorMessage,
-						};
-					}
-
-					if (
-						gatewayConfig.supports.showSaveOption &&
-						shouldSavePayment &&
-						! paymentMethodsConfig[ selectedUPEPaymentType ]
-							.isReusable
-					) {
-						return {
-							type: 'error',
-							message:
-								'This payment method cannot be saved for future use.',
-						};
-					}
-
-					await validateElements( elements );
-
-					const paymentMethodObject = await api
-						.getStripe()
-						.createPaymentMethod( {
-							elements,
-							params: {
-								billing_details: getBillingDetails(
-									billingData
-								),
-							},
-						} );
-
-					return {
-						type: 'success',
-						meta: {
-							paymentMethodData: {
-								payment_method: paymentMethodId,
-								'wcpay-payment-method':
-									paymentMethodObject.paymentMethod.id,
-								'wcpay-fraud-prevention-token': getFraudPreventionToken(),
-								'wcpay-fingerprint': fingerprint,
-							},
-						},
-					};
-				}
-				return handlePaymentProcessing();
-			} ),
-		[
-			activePaymentMethod,
-			api,
-			elements,
-			fingerprint,
-			gatewayConfig,
-			paymentMethodId,
-			paymentMethodsConfig,
-			selectedUPEPaymentType,
-			shouldSavePayment,
-			upeMethods,
-			errorMessage,
-			onPaymentSetup,
-			billingData,
-			isUPEComplete,
-		]
-	);
-
-	usePaymentCompleteHandler(
-		api,
-		stripe,
-		elements,
-		onCheckoutAfterProcessingWithSuccess,
-		emitResponse,
-		shouldSavePayment
-	);
-
-	// Checks whether there are errors within a field, and saves them for later reporting.
-	const upeOnChange = ( event ) => {
-		// Update WC Blocks gateway config based on selected UPE payment method.
-		const paymentType =
-			'link' !== event.value.type ? event.value.type : 'card';
-		gatewayConfig.supports.showSaveOption =
-			paymentMethodsConfig[ paymentType ].showSaveOption;
-
-		setIsUPEComplete( event.complete );
-		setSelectedUPEPaymentType( paymentType );
-	};
-
-	return (
-		<>
-			<p
-				className="content"
-				dangerouslySetInnerHTML={ {
-					__html: testingInstructionsIfAppropriate,
-				} }
-			></p>
-			<PaymentElement
-				options={ getStripeElementOptions(
-					shouldSavePayment,
-					paymentMethodsConfig
-				) }
-				onChange={ upeOnChange }
-				className="wcpay-payment-element"
-			/>
-		</>
-	);
-};
-
-const ConsumableWCPayFields = ( { api, ...props } ) => {
+const PaymentElements = ( { api, ...props } ) => {
 	const stripe = api.getStripe();
 	const [ errorMessage, setErrorMessage ] = useState( null );
 	const [ appearance, setAppearance ] = useState(
@@ -261,7 +55,7 @@ const ConsumableWCPayFields = ( { api, ...props } ) => {
 					appearance: appearance,
 				} }
 			>
-				<WCPayUPEFields
+				<PaymentProcessor
 					api={ api }
 					errorMessage={ errorMessage }
 					fingerprint={ fingerprint }
@@ -279,7 +73,7 @@ export const getDeferredIntentCreationUPEFields = (
 	testingInstructions
 ) => {
 	return (
-		<ConsumableWCPayFields
+		<PaymentElements
 			paymentMethodId={ upeName }
 			upeMethods={ upeMethods }
 			api={ api }
