46686a8a43464a083e47ed89a1c9ec0b1fcde13e
Vladimir Reznichenko <kalessil@gmail.com>
2023-06-16 10:15:16 +0200

    Affirm&Afterpay: addressed code review feedback.

--- includes/payment-methods/class-affirm-payment-method.php
+++ includes/payment-methods/class-affirm-payment-method.php
@@ -17,18 +17,6 @@ class Affirm_Payment_Method extends UPE_Payment_Method {
 
 	const PAYMENT_METHOD_STRIPE_ID = 'affirm';
 
-	const PAYMENT_METHOD_TOTAL_LIMIT = [
-		// See https://stripe.com/docs/payments/buy-now-pay-later#product-support.
-		'CAD' => [
-			'min' => 5000,
-			'max' => 3000000,
-		], // Represents CAD 50 - 30,000 CAD.
-		'USD' => [
-			'min' => 5000,
-			'max' => 3000000,
-		], // Represents USD 50 - 30,000 USD.
-	];
-
 	/**
 	 * Constructor for link payment method
 	 *
@@ -36,29 +24,21 @@ class Affirm_Payment_Method extends UPE_Payment_Method {
 	 */
 	public function __construct( $token_service ) {
 		parent::__construct( $token_service );
-		$this->stripe_id   = self::PAYMENT_METHOD_STRIPE_ID;
-		$this->title       = __( 'Affirm', 'woocommerce-payments' );
-		$this->is_reusable = false;
-		$this->currencies  = [ 'USD', 'CAD' ];
-		$this->icon_url    = plugins_url( 'assets/images/payment-methods/woo.svg', WCPAY_PLUGIN_FILE );
-	}
-
-	/**
-	 * Returns boolean dependent on whether payment method can be used at checkout.
-	 *
-	 * @return bool
-	 */
-	public function is_enabled_at_checkout() {
-		$is_enabled = parent::is_enabled_at_checkout();
-		if ( $is_enabled && isset( WC()->cart ) ) {
-			$currency = get_woocommerce_currency();
-			$amount   = WC_Payments_Utils::prepare_amount( WC()->cart->get_total( '' ), $currency );
-			if ( $amount > 0 ) {
-				$range = self::PAYMENT_METHOD_TOTAL_LIMIT[ $currency ];
-				return $amount >= $range['min'] && $amount <= $range['max'];
-			}
-		}
-		return $is_enabled;
+		$this->stripe_id           = self::PAYMENT_METHOD_STRIPE_ID;
+		$this->title               = __( 'Affirm', 'woocommerce-payments' );
+		$this->is_reusable         = false;
+		$this->icon_url            = plugins_url( 'assets/images/payment-methods/woo.svg', WCPAY_PLUGIN_FILE );
+		$this->currencies          = [ 'USD', 'CAD' ];
+		$this->limits_per_currency = [
+			'CAD' => [
+				'min' => 5000,
+				'max' => 3000000,
+			], // Represents CAD 50 - 30,000 CAD.
+			'USD' => [
+				'min' => 5000,
+				'max' => 3000000,
+			], // Represents USD 50 - 30,000 USD.
+		];
 	}
 
 	/**
--- includes/payment-methods/class-afterpay-payment-method.php
+++ includes/payment-methods/class-afterpay-payment-method.php
@@ -17,34 +17,6 @@ class Afterpay_Payment_Method extends UPE_Payment_Method {
 
 	const PAYMENT_METHOD_STRIPE_ID = 'afterpay_clearpay';
 
-	const PAYMENT_METHOD_TOTAL_LIMIT = [
-		// See https://stripe.com/docs/payments/afterpay-clearpay#collection-schedule.
-		'AUD' => [
-			'min' => 100,
-			'max' => 200000,
-		], // Represents AUD 1 - 2,000 AUD.
-		'CAD' => [
-			'min' => 100,
-			'max' => 200000,
-		], // Represents CAD 1 - 2,000 CAD.
-		'NZD' => [
-			'min' => 100,
-			'max' => 200000,
-		], // Represents NZD 1 - 2,000 NZD.
-		'GBP' => [
-			'min' => 100,
-			'max' => 100000,
-		], // Represents GBP 1 - 1,000 GBP.
-		'USD' => [
-			'min' => 100,
-			'max' => 400000,
-		], // Represents USD 1 - 4,000 USD.
-		'EUR' => [
-			'min' => 100,
-			'max' => 100000,
-		], // Represents EUR 1 - 1,000 EUR.
-	];
-
 	/**
 	 * Constructor for link payment method
 	 *
@@ -52,29 +24,37 @@ class Afterpay_Payment_Method extends UPE_Payment_Method {
 	 */
 	public function __construct( $token_service ) {
 		parent::__construct( $token_service );
-		$this->stripe_id   = self::PAYMENT_METHOD_STRIPE_ID;
-		$this->title       = __( 'Afterpay', 'woocommerce-payments' );
-		$this->is_reusable = false;
-		$this->currencies  = [ 'USD', 'CAD', 'AUD', 'NZD', 'GBP', 'EUR' ];
-		$this->icon_url    = plugins_url( 'assets/images/payment-methods/afterpay.svg', WCPAY_PLUGIN_FILE );
-	}
-
-	/**
-	 * Returns boolean dependent on whether payment method can be used at checkout.
-	 *
-	 * @return bool
-	 */
-	public function is_enabled_at_checkout() {
-		$is_enabled = parent::is_enabled_at_checkout();
-		if ( $is_enabled && isset( WC()->cart ) ) {
-			$currency = get_woocommerce_currency();
-			$amount   = WC_Payments_Utils::prepare_amount( WC()->cart->get_total( '' ), $currency );
-			if ( $amount > 0 ) {
-				$range = self::PAYMENT_METHOD_TOTAL_LIMIT[ $currency ];
-				return $amount >= $range['min'] && $amount <= $range['max'];
-			}
-		}
-		return $is_enabled;
+		$this->stripe_id           = self::PAYMENT_METHOD_STRIPE_ID;
+		$this->title               = __( 'Afterpay', 'woocommerce-payments' );
+		$this->is_reusable         = false;
+		$this->icon_url            = plugins_url( 'assets/images/payment-methods/afterpay.svg', WCPAY_PLUGIN_FILE );
+		$this->currencies          = [ 'USD', 'CAD', 'AUD', 'NZD', 'GBP', 'EUR' ];
+		$this->limits_per_currency = [
+			'AUD' => [
+				'min' => 100,
+				'max' => 200000,
+			], // Represents AUD 1 - 2,000 AUD.
+			'CAD' => [
+				'min' => 100,
+				'max' => 200000,
+			], // Represents CAD 1 - 2,000 CAD.
+			'NZD' => [
+				'min' => 100,
+				'max' => 200000,
+			], // Represents NZD 1 - 2,000 NZD.
+			'GBP' => [
+				'min' => 100,
+				'max' => 100000,
+			], // Represents GBP 1 - 1,000 GBP.
+			'USD' => [
+				'min' => 100,
+				'max' => 400000,
+			], // Represents USD 1 - 4,000 USD.
+			'EUR' => [
+				'min' => 100,
+				'max' => 100000,
+			], // Represents EUR 1 - 1,000 EUR.
+		];
 	}
 
 	/**
