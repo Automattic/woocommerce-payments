4eb2958da7f5400c79c3a77b4aaee1787b7f33fd
Shendy Kurnia <shendy.kurnia@a8c.com>
2023-06-28 10:51:41 +0700

    Handle when `get_disputes_needing_response()` returns non-array.

--- includes/admin/tasks/class-wc-payments-task-disputes.php
+++ includes/admin/tasks/class-wc-payments-task-disputes.php
@@ -102,6 +102,10 @@ class WC_Payments_Task_Disputes extends Task {
 		}
 
 		$active_disputes = $this->get_disputes_needing_response();
+		if ( ! is_array( $active_disputes ) || count( $active_disputes ) === 0 ) {
+			return '';
+		}
+
 		$currencies_map  = [];
 		foreach ( $active_disputes as $dispute ) {
 			if ( ! isset( $currencies_map[ $dispute['currency'] ] ) ) {
