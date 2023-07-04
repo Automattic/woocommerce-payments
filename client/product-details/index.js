344aa40c7329a7cfdc30fdd4e7dd7598540f7c9d
Nagesh Pai <nagesh.pai@automattic.com>
2023-07-04 14:09:07 +0530

    Handle reset/ clear button click

--- client/product-details/index.js
+++ client/product-details/index.js
@@ -7,7 +7,6 @@ import { initializeBnplSiteMessaging } from './bnpl-site-messaging';
 const bnplPaymentMessageElement = initializeBnplSiteMessaging();
 
 jQuery( function ( $ ) {
-	// Update BNPL payment message when quantity is changed.
 	$( '.quantity input' ).on( 'change', function ( event ) {
 		const newQuantity = event.target.value;
 		const price = window.wcpayStripeSiteMessaging.price;
@@ -16,7 +15,7 @@ jQuery( function ( $ ) {
 		} );
 	} );
 
-	// Update BNPL payment message when variation is changed in a variable product.
+	// Handle BNPL payment message changes for product variation.
 	$( '.single_variation_wrap' ).on( 'show_variation', function (
 		event,
 		variation
@@ -25,6 +24,12 @@ jQuery( function ( $ ) {
 		window.wcpayStripeSiteMessaging.price = variation.display_price * 100;
 		bnplPaymentMessageElement.update( {
 			amount: parseInt( variation.display_price * quantity * 100, 10 ),
+		} );
+	} );
+
+	$( '.reset_variations' ).on( 'click', function () {
+		bnplPaymentMessageElement.update( {
+			amount: 0,
 		} );
 	} );
 } );
