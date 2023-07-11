eadcc0371536a55c957dcf82f7667e107edf771c
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2023-07-11 17:44:09 -0300

    Clear merchant and shopper carts before interacting with UI

--- tests/e2e/specs/wcpay/merchant/merchant-fraud-protection.spec.js
+++ tests/e2e/specs/wcpay/merchant/merchant-fraud-protection.spec.js
@@ -15,113 +15,87 @@ describe( 'Fraud protection', () => {
 		beforeAll( async () => {
 			await merchant.login();
 			await shopperWCP.emptyCart();
-		} );
-
-		afterEach( async () => {
-			await shopperWCP.emptyCart();
+			await merchant.logout();
 		} );
 
 		afterAll( async () => {
+			await merchant.login();
 			await merchantWCP.openWCPSettings();
 			await merchantWCP.skipFraudProtectionTour();
 			await expect( page ).toClick( '#fraud-protection__basic-level' );
 			await merchantWCP.wcpSettingsSaveChanges();
-
 			await merchant.logout();
 		} );
 
 		describe( 'Purchase price threshold', () => {
 			it( 'Blocks the checkout when hitting the maximum limit', async () => {
+				await merchant.login();
 				await merchantWCP.disableAllFraudProtectionRules();
 				await merchantWCP.toggleFraudProtectionRule(
 					'purchase-price-threshold',
 					{
 						'#fraud-protection-purchase-price-minimum': '',
-						'#fraud-protection-purchase-price-maximum': '500',
+						'#fraud-protection-purchase-price-maximum': '50',
 					}
 				);
 				await merchantWCP.saveAdvancedFraudProtectionSettings();
+				await merchant.logout();
 
+				await shopper.login();
+				await shopperWCP.emptyCart();
 				await setupProductCheckout(
 					config.get( 'addresses.customer.billing' ),
-					[ [ config.get( 'products.simple.name' ), 105 ] ]
+					[ [ config.get( 'products.simple.name' ), 10 ] ]
 				);
 				await fillCardDetails( page, config.get( 'cards.basic' ) );
 				await expect( page ).toClick( '#place_order' );
 				await shopperWCP.validateCheckoutError();
+				await shopperWCP.emptyCart();
+				await shopper.logout();
 
+				await merchant.login();
 				await merchantWCP.openLatestBlockedOrder();
 				await merchantWCP.checkTransactionStatus( 'Payment blocked' );
 				await merchantWCP.checkFraudOutcomeEntry(
 					'Block if the purchase price is not in your defined range'
 				);
-			} );
-
-			it( 'Allows the checkout if the price is in between the limits', async () => {
-				await merchantWCP.disableAllFraudProtectionRules();
-				await merchantWCP.toggleFraudProtectionRule(
-					'purchase-price-threshold',
-					{
-						'#fraud-protection-purchase-price-minimum': '1',
-						'#fraud-protection-purchase-price-maximum': '50',
-					}
-				);
-				await merchantWCP.saveAdvancedFraudProtectionSettings();
-
-				await setupProductCheckout(
-					config.get( 'addresses.customer.billing' )
-				);
-				await fillCardDetails( page, config.get( 'cards.basic' ) );
-				await shopper.placeOrder();
-				await expect( page ).toMatch( 'Order received' );
+				await merchant.logout();
 			} );
 		} );
 
 		describe( 'Order items threshold', () => {
 			it( 'Blocks the checkout when hitting the maximum limit', async () => {
+				await merchant.login();
 				await merchantWCP.disableAllFraudProtectionRules();
 				await merchantWCP.toggleFraudProtectionRule(
 					'order-items-threshold',
 					{
 						'#fraud-protection-order-items-minimum': '',
-						'#fraud-protection-order-items-maximum': '100',
+						'#fraud-protection-order-items-maximum': '10',
 					}
 				);
 				await merchantWCP.saveAdvancedFraudProtectionSettings();
+				await merchant.logout();
 
+				await shopper.login();
+				await shopperWCP.emptyCart();
 				await setupProductCheckout(
 					config.get( 'addresses.customer.billing' ),
-					[ [ config.get( 'products.simple.name' ), 105 ] ]
+					[ [ config.get( 'products.simple.name' ), 11 ] ]
 				);
 				await fillCardDetails( page, config.get( 'cards.basic' ) );
 				await expect( page ).toClick( '#place_order' );
 				await shopperWCP.validateCheckoutError();
+				await shopperWCP.emptyCart();
+				await shopper.logout();
 
+				await merchant.login();
 				await merchantWCP.openLatestBlockedOrder();
 				await merchantWCP.checkTransactionStatus( 'Payment blocked' );
 				await merchantWCP.checkFraudOutcomeEntry(
 					'Block if the items count is not in your defined range'
 				);
-			} );
-
-			it( 'Allows the checkout if the price is in between the limits', async () => {
-				await merchantWCP.disableAllFraudProtectionRules();
-				await merchantWCP.toggleFraudProtectionRule(
-					'order-items-threshold',
-					{
-						'#fraud-protection-order-items-minimum': '1',
-						'#fraud-protection-order-items-maximum': '100',
-					}
-				);
-				await merchantWCP.saveAdvancedFraudProtectionSettings();
-
-				await setupProductCheckout(
-					config.get( 'addresses.customer.billing' ),
-					[ [ config.get( 'products.simple.name' ), 3 ] ]
-				);
-				await fillCardDetails( page, config.get( 'cards.basic' ) );
-				await shopper.placeOrder();
-				await expect( page ).toMatch( 'Order received' );
+				await merchant.logout();
 			} );
 		} );
 	} );
