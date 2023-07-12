d4b43d88dc70ac9282a156de82c204f6edf0b281
Eric Jinks <3147296+Jinksi@users.noreply.github.com>
2023-07-12 19:41:40 +1000

    Improve docblock for isDueWithin function

--- client/disputes/utils.ts
+++ client/disputes/utils.ts
@@ -9,20 +9,20 @@ import moment from 'moment';
  */
 import { CachedDispute, EvidenceDetails } from 'wcpay/types/disputes';
 
+interface IsDueWithinProps {
+	dueBy: CachedDispute[ 'due_by' ] | EvidenceDetails[ 'due_by' ] | undefined;
+	days: number;
+}
 /**
  * Returns true if a dispute due_by date is within the specified number of days.
  * Returns false if the dispute due_by date is not within the specified number of days
  * or if the due_by value is not a valid date.
  *
- * @param {Object} dispute - The dispute to check. See {@link CachedDispute} or {@link EvidenceDetails}.
+ * @param {IsDueWithinProps} dispute - An object containing the dispute due_by date, either in unix timestamp (number) or ISO format (string).
  * @param {number} days - The number of days to check.
  *
  * @return {boolean} True if the dispute is due within the specified number of days.
  */
-interface IsDueWithinProps {
-	dueBy: CachedDispute[ 'due_by' ] | EvidenceDetails[ 'due_by' ];
-	days: number;
-}
 export const isDueWithin = ( { dueBy, days }: IsDueWithinProps ): boolean => {
 	if ( ! dueBy ) {
 		return false;
