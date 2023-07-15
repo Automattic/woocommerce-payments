cefd5cb9d0992cc612c924f5b7c0b14d5dc29abc
Alefe Souza <contact@alefesouza.com>
2023-07-14 23:21:16 -0300

    Add unit tests

--- includes/woopay/class-woopay-adapted-extensions.php
+++ includes/woopay/class-woopay-adapted-extensions.php
@@ -7,6 +7,8 @@
 
 namespace WCPay\WooPay;
 
+use Automattic\WooCommerce\Blocks\Integrations\IntegrationInterface;
+
 /**
  * WooPay
  */
@@ -15,11 +17,25 @@ class WooPay_Adapted_Extensions {
 	const POINTS_AND_REWARDS_API    = 'points-and-rewards';
 
 	/**
+	 * WC Blocks registered integrations, as `$name => $instance` pairs.
+	 *
+	 * @var IntegrationInterface[]
+	 */
+	protected $registered_integrations = [];
+
+	/**
+	 * Initializa WC Blocks regitered integrations.
+	 */
+	public function __construct() {
+		do_action( 'woocommerce_blocks_checkout_block_registration', $this );
+	}
+
+	/**
 	 * Get WooPay adapted extensions settings and extra data needed on WooPay.
 	 *
 	 * @param string $email The user email the data will be loaded.
 	 */
-	public static function get_adapted_extensions_data( $email ) {
+	public function get_adapted_extensions_data( $email ) {
 		$enabled_adapted_extensions = get_option( WooPay_Scheduler::ENABLED_ADAPTED_EXTENSIONS_OPTION_NAME, [] );
 		$extension_settings         = [];
 
@@ -28,18 +44,17 @@ class WooPay_Adapted_Extensions {
 		}
 
 		$user = wp_get_current_user();
-		// WooPay will only ask to verify email if it's a guest user or doesn't match logged in user.
-		$email_and_merchant_login_match = $email === $user->user_email;
 
-		if ( ! $email_and_merchant_login_match ) {
-			// If the user is guest and has a merchant account, load data from there.
+		if ( ! is_user_logged_in() ) {
+			// If the user is guest and has a merchant account, load data from there to know if need
+			// to verify their email on WooPay.
 			$user_by_email = get_user_by( 'email', $email );
 			$user          = $user_by_email ?? $user;
 		}
 
 		// Points and Rewards.
 		if ( in_array( self::POINTS_AND_REWARDS_PLUGIN, $enabled_adapted_extensions, true ) ) {
-			$extension_settings[ self::POINTS_AND_REWARDS_API ] = self::get_points_and_rewards_data( $user, $email_and_merchant_login_match );
+			$extension_settings[ self::POINTS_AND_REWARDS_API ] = self::get_points_and_rewards_data( $user );
 		}
 
 		return $extension_settings;
@@ -48,21 +63,19 @@ class WooPay_Adapted_Extensions {
 	/**
 	 * Get Points and Rewards settings for WooPay.
 	 *
-	 * @param WP_User $user The user the data will be loaded.
-	 * @param bool    $email_and_merchant_login_match If the email and current logged in user match.
+	 * @psalm-suppress UndefinedClass
+	 * @param \WP_User $user The user the data will be loaded.
 	 */
-	private static function get_points_and_rewards_data( $user, $email_and_merchant_login_match ) {
+	public function get_points_and_rewards_data( $user ) {
 		if (
+			! empty( $this->registered_integrations[ self::POINTS_AND_REWARDS_API ] ) &&
 			! class_exists( 'WC_Points_Rewards_Manager' ) ||
-			! class_exists( 'WC_Points_Rewards_Integration' ) ||
-			! method_exists( 'WC_Points_Rewards_Manager', 'get_users_points' ) ||
-			! method_exists( 'WC_Points_Rewards_Integration', 'get_script_data' )
+			! method_exists( 'WC_Points_Rewards_Manager', 'get_users_points' )
 		) {
 			return null;
 		}
 
-		$points_and_rewards_integration = new \WC_Points_Rewards_Integration();
-		$points_and_rewards_script_data = $points_and_rewards_integration->get_script_data();
+		$points_and_rewards_script_data = $this->registered_integrations[ self::POINTS_AND_REWARDS_API ]->get_script_data();
 
 		list( $points, $monetary_value ) = explode( ':', get_option( 'wc_points_rewards_redeem_points_ratio', '' ) );
 
@@ -74,15 +87,29 @@ class WooPay_Adapted_Extensions {
 			'monetary_value' => $monetary_value,
 		];
 
-		// This data will only be loaded on WooPay if the email is verified.
+		// Check if the user has points to show the verify email alert.
 		$available_points_for_user = \WC_Points_Rewards_Manager::get_users_points( $user->ID );
 
 		if ( $available_points_for_user > 0 && $available_points_for_user > $points_and_rewards_script_data['minimum_points_amount'] ) {
 			// Only ask the user to verify email if they have available points.
-			$points_and_rewards_script_data['should_verify_email'] = ! $email_and_merchant_login_match;
+			$points_and_rewards_script_data['should_verify_email'] = ! is_user_logged_in();
 			$points_and_rewards_script_data['points_available']    = $available_points_for_user;
 		}
 
 		return $points_and_rewards_script_data;
+	}
+
+	/**
+	 * Get WC Blocks registered integrations.
+	 *
+	 * @param IntegrationInterface $integration An instance of IntegrationInterface.
+	 *
+	 * @return boolean True means registered successfully.
+	 */
+	public function register( IntegrationInterface $integration ) {
+		$name = $integration->get_name();
+
+		$this->registered_integrations[ $name ] = $integration;
+		return true;
 	}
 }
--- tests/unit/helpers/class-wc-helper-points-and-rewards.php
+++ tests/unit/helpers/class-wc-helper-points-and-rewards.php
@@ -5,6 +5,10 @@
  * @package WooCommerce\Payments\Tests
  */
 
+// phpcs:disable Generic.Files.OneObjectStructurePerFile.MultipleFound
+
+use Automattic\WooCommerce\Blocks\Integrations\IntegrationInterface;
+
 /**
  * Class WC_Points_Rewards.
  *
@@ -12,4 +16,46 @@
  */
 class WC_Points_Rewards {
 
+}
+
+/**
+ * Class WC_Points_Rewards_Integration.
+ *
+ * Mock class for test usage.
+ */
+class WC_Points_Rewards_Integration implements IntegrationInterface {
+	public function get_name() {
+		return 'points-and-rewards';
+	}
+
+	public function initialize() {
+	}
+
+	public function get_script_handles() {
+	}
+
+	public function get_script_data() {
+		return [
+			'woocommerce-points-and-rewards-blocks' => 'active',
+			'points_available'                      => 0,
+			'minimum_points_amount'                 => 0,
+			'partial_redemption_enabled'            => true,
+			'points_label_singular'                 => 'Point',
+			'points_label_plural'                   => 'Points',
+		];
+	}
+
+	public function get_editor_script_handles() {
+	}
+}
+
+/**
+ * Class WC_Points_Rewards_Manager.
+ *
+ * Mock class for test usage.
+ */
+class WC_Points_Rewards_Manager {
+	public function get_users_points( $user_id ) {
+		return $user_id;
+	}
 }
