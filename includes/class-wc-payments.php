6587c80b9d19d9479c94e3dddc73f7b9a4c43c90
Alefe Souza <contact@alefesouza.com>
2023-07-20 18:21:02 -0300

    Send Store API nonce for email verified users

--- includes/class-wc-payments.php
+++ includes/class-wc-payments.php
@@ -1535,6 +1535,21 @@ class WC_Payments {
 			WC()->customer->save();
 
 			$body['adapted_extensions'] = ( new WooPay_Adapted_Extensions() )->get_adapted_extensions_data( $email );
+
+			$store_user_email_registered = get_user_by( 'email', $email );
+
+			if ( $store_user_email_registered ) {
+				// Create a nonce for email verified WooPay users use on the Store API request.
+				$store_api_nonce_for_woopay_verified_email = function ( $uid, $action ) use ( $store_user_email_registered ) {
+					return 'wc_store_api' === $action ? $store_user_email_registered->ID : $uid;
+				};
+
+				add_filter( 'nonce_user_logged_out', $store_api_nonce_for_woopay_verified_email, 10, 2 );
+
+				$body['email_verified_session_nonce'] = wp_create_nonce( 'wc_store_api' );
+
+				remove_filter( 'nonce_user_logged_out', $store_api_nonce_for_woopay_verified_email );
+			}
 		}
 
 		$args = [
