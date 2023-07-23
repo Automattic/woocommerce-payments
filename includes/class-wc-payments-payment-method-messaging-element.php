98412b1724686bdb4199e354a49dc8db5c27b2d0
Nagesh Pai <nagesh.pai@automattic.com>
2023-07-23 13:51:59 +0530

    Hook the messaging element only if BNPL PM active

--- includes/class-wc-payments-payment-method-messaging-element.php
+++ includes/class-wc-payments-payment-method-messaging-element.php
@@ -48,16 +48,6 @@ class WC_Payments_Payment_Method_Messaging_Element {
 	 */
 	public function init(): string {
 		global $product;
-
-		$enabled_upe_payment_methods = $this->gateway->get_payment_method_ids_enabled_at_checkout();
-		// Filter non BNPL out of the list of payment methods.
-		$bnpl_payment_methods = array_intersect( $enabled_upe_payment_methods, [ Payment_Method::AFFIRM, Payment_Method::AFTERPAY ] );
-
-		// Do not render the messaging element if there are no BNPL payment methods enabled.
-		if ( empty( $bnpl_payment_methods ) ) {
-			return '';
-		}
-
 		$currency_code      = get_woocommerce_currency();
 		$store_country      = WC()->countries->get_base_country();
 		$billing_country    = WC()->customer->get_billing_country();
@@ -77,6 +67,10 @@ class WC_Payments_Payment_Method_Messaging_Element {
 				];
 			}
 		}
+
+		$enabled_upe_payment_methods = $this->gateway->get_payment_method_ids_enabled_at_checkout();
+		// Filter non BNPL out of the list of payment methods.
+		$bnpl_payment_methods = array_intersect( $enabled_upe_payment_methods, Payment_Method::BNPL_METHODS );
 
 		// register the script.
 		WC_Payments::register_script_with_dependencies( 'WCPAY_PRODUCT_DETAILS', 'dist/product-details', [ 'stripe' ] );
