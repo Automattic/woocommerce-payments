da5c88e539c5ca456a4bb13d1b4b78dc98594f30
Dat Hoang <htdat@users.noreply.github.com>
2023-08-01 21:48:44 +0700

    RPP: Create a payment service class (#6805)
    Co-authored-by: Radoslav Georgiev <rageorgiev@gmail.com>

---
+++ src/Internal/Service/PaymentProcessingService.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Class PaymentProcessingService
+ *
+ * @package WooCommerce\Payments
+ */
+
+namespace WCPay\Internal\Service;
+
+/**
+ *  Payment Processing Service.
+ */
+class PaymentProcessingService {
+
+}
