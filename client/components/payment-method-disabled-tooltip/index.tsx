bf7ef5860273bd235271e8f98cb298236fa39136
Miguel Gasca <miguel.gasca@automattic.com>
2023-08-01 17:05:42 +0200

    Additional refactor after review comments

--- client/components/payment-method-disabled-tooltip/index.tsx
+++ client/components/payment-method-disabled-tooltip/index.tsx
@@ -2,15 +2,37 @@
 /**
  * External dependencies
  */
+import interpolateComponents from '@automattic/interpolate-components';
 import { __ } from '@wordpress/i18n';
 import React from 'react';
-import interpolateComponents from '@automattic/interpolate-components';
 
 /**
  * Internal dependencies
  */
 import { HoverTooltip } from 'components/tooltip';
-import { getDocumentationUrlForDisabledPaymentMethod } from './utils';
+import PAYMENT_METHOD_IDS from 'wcpay/payment-methods/constants';
+
+export const DocumentationUrlForDisabledPaymentMethod = {
+	DEFAULT:
+		'https://woocommerce.com/document/woopayments/payment-methods/additional-payment-methods/#method-cant-be-enabled',
+	BNPLS:
+		'https://woocommerce.com/document/woopayments/payment-methods/buy-now-pay-later/#contact-support',
+};
+
+export const getDocumentationUrlForDisabledPaymentMethod = (
+	paymentMethodId: string
+): string => {
+	let link;
+	switch ( paymentMethodId ) {
+		case PAYMENT_METHOD_IDS.AFTERPAY_CLEARPAY:
+		case PAYMENT_METHOD_IDS.AFFIRM:
+			link = DocumentationUrlForDisabledPaymentMethod.BNPLS;
+			break;
+		default:
+			link = DocumentationUrlForDisabledPaymentMethod.DEFAULT;
+	}
+	return link;
+};
 
 const PaymentMethodDisabledTooltip = ( {
 	id,
--- client/components/payment-method-disabled-tooltip/test/index.test.tsx
+++ client/components/payment-method-disabled-tooltip/test/index.test.tsx
@@ -2,73 +2,57 @@
 /**
  * External dependencies
  */
-import { render, screen, fireEvent } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import React from 'react';
 
 /**
  * Internal dependencies
  */
-import PaymentMethodDisabledTooltip from '../index';
-import { getDocumentationUrlForDisabledPaymentMethod } from '../utils';
-
-// Mock the utils function
-jest.mock( '../utils', () => ( {
-	getDocumentationUrlForDisabledPaymentMethod: jest.fn(),
-} ) );
+import PAYMENT_METHOD_IDS from 'wcpay/payment-methods/constants';
+import PaymentMethodDisabledTooltip, {
+	DocumentationUrlForDisabledPaymentMethod,
+	getDocumentationUrlForDisabledPaymentMethod,
+} from '../index';
 
 describe( 'PaymentMethodDisabledTooltip', () => {
-	const mockMethodId = 'testId';
-	const mockUrl = 'http://test.url';
+	test.each( [
+		[
+			PAYMENT_METHOD_IDS.AFTERPAY_CLEARPAY,
+			DocumentationUrlForDisabledPaymentMethod.BNPLS,
+		],
+		[ 'default-method', DocumentationUrlForDisabledPaymentMethod.DEFAULT ],
+	] )(
+		'renders tooltip with correct learn more link for %s',
+		( tooltipId, expectedUrl ) => {
+			render(
+				<PaymentMethodDisabledTooltip id={ tooltipId }>
+					Test children
+				</PaymentMethodDisabledTooltip>
+			);
 
-	const mockGetDocumentationUrlForDisabledPaymentMethod = getDocumentationUrlForDisabledPaymentMethod as jest.MockedFunction<
-		typeof getDocumentationUrlForDisabledPaymentMethod
-	>;
+			const element = screen.getByText( 'Test children' );
+			expect( element ).toBeInTheDocument();
 
-	beforeAll( () => {
-		mockGetDocumentationUrlForDisabledPaymentMethod.mockImplementation(
-			() => mockUrl
-		);
-	} );
+			fireEvent.mouseOver( element );
 
-	afterEach( () => {
-		jest.clearAllMocks();
-	} );
+			const tooltip = screen.getByRole( 'tooltip' );
+			expect( tooltip ).toBeInTheDocument();
+			expect( tooltip ).toHaveTextContent(
+				'We need more information from you to enable this method. Learn more.'
+			);
 
-	test( 'renders with correct text and link', () => {
-		render(
-			<PaymentMethodDisabledTooltip id={ mockMethodId }>
-				Test children
-			</PaymentMethodDisabledTooltip>
-		);
-
-		const element = screen.getByText( 'Test children' );
-		expect( element ).toBeInTheDocument();
-
-		fireEvent.mouseOver( element );
-
-		const tooltip = screen.getByRole( 'tooltip' );
-		expect( tooltip ).toBeInTheDocument();
-		expect( tooltip ).toHaveTextContent(
-			'We need more information from you to enable this method. Learn more.'
-		);
-
-		const learnMoreLink = screen.getByRole( 'link', {
-			name: 'Learn more.',
-		} );
-		expect( learnMoreLink ).toHaveAttribute( 'href', mockUrl );
-		expect( learnMoreLink ).toHaveAttribute( 'target', '_blank' );
-		expect( learnMoreLink ).toHaveAttribute( 'rel', 'noreferrer' );
-	} );
-
-	test( 'calls getDocumentationUrlForDisabledPaymentMethod with correct id', () => {
-		render(
-			<PaymentMethodDisabledTooltip id={ mockMethodId }>
-				Test children
-			</PaymentMethodDisabledTooltip>
-		);
-
-		expect(
-			mockGetDocumentationUrlForDisabledPaymentMethod
-		).toHaveBeenCalledWith( mockMethodId );
-	} );
+			const learnMoreLink = screen.getByRole( 'link', {
+				name: 'Learn more.',
+			} );
+			expect( learnMoreLink ).toHaveAttribute(
+				'href',
+				getDocumentationUrlForDisabledPaymentMethod( tooltipId )
+			);
+			expect( learnMoreLink.getAttribute( 'href' ) ).toEqual(
+				expectedUrl
+			);
+			expect( learnMoreLink ).toHaveAttribute( 'target', '_blank' );
+			expect( learnMoreLink ).toHaveAttribute( 'rel', 'noreferrer' );
+		}
+	);
 } );
