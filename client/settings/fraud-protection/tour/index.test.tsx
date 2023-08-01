a41e738fe6a4a873231ecaa93373a44f9b05d4ee
Allie Mims <allie.mims@automattic.com>
2023-08-01 16:05:21 -0400

    Add query param check Update unit tests Add unit test Update snapshot file

--- client/settings/fraud-protection/tour/index.test.tsx
+++ client/settings/fraud-protection/tour/index.test.tsx
@@ -46,6 +46,15 @@ describe( 'FraudProtectionTour', () => {
 	} );
 
 	it( 'should render the tour component correctly', () => {
+		Object.defineProperty( window, 'location', {
+			value: {
+				configurable: true,
+				enumerable: true,
+				search:
+					'?page=wc-settings&tab=checkout&anchor=%23fp-settings&section=woocommerce_payments/',
+			},
+		} );
+
 		const { baseElement } = render( <FraudProtectionTour /> );
 
 		expect( baseElement ).toMatchSnapshot();
@@ -57,6 +66,22 @@ describe( 'FraudProtectionTour', () => {
 				isWelcomeTourDismissed: true,
 			},
 		};
+
+		const { baseElement } = render( <FraudProtectionTour /> );
+
+		expect( baseElement ).toMatchSnapshot();
+	} );
+
+	it( 'should not render the tour component if settings page accessed directly', () => {
+		Reflect.deleteProperty( window, 'location' );
+		Object.defineProperty( window, 'location', {
+			configurable: true,
+			enumerable: true,
+			value: {
+				search:
+					'?page=wc-settings&tab=checkout&section=woocommerce_payments/',
+			},
+		} );
 
 		const { baseElement } = render( <FraudProtectionTour /> );
 
