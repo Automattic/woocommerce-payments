c830bf7d6eff7f4ef7f3d945ee2d44ea8359a2f5
Radoslav Georgiev <rageorgiev@gmail.com>
2023-08-02 18:50:58 +0300

    Adding blank proxies, and a provider.

---
+++ src/Internal/Proxies/HooksProxy.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Class HooksProxy
+ *
+ * @package WooCommerce\Payments
+ */
+
+namespace WCPay\Internal\Proxy;
+
+/**
+ * Actions Proxy
+ *
+ * Used for accessing WP hooks, incl. actions and filters.
+ */
+class HooksProxy {
+}
---
+++ src/Internal/Proxies/LegacyProxy.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Class LegacyProxy
+ *
+ * @package WooCommerce\Payments
+ */
+
+namespace WCPay\Internal\Proxy;
+
+/**
+ * Legacy Proxy
+ *
+ * Used for accessing legacy code, incl. functions, static methods, and globals.
+ * Classes are handled through WCPay\Internal\DependencyManagement\DelegateContainer\LegacyContainer.
+ */
+class LegacyProxy {
+
+}
