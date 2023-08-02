8ca3d531beee057ad12f5e1a86e16f3e3ff387d7
Dat Hoang <htdat@users.noreply.github.com>
2023-08-02 11:56:17 +0700

    Add WCPAY_NEW_PROCESS

--- src/Internal/Service/PaymentProcessingService.php
+++ src/Internal/Service/PaymentProcessingService.php
@@ -7,9 +7,21 @@
 
 namespace WCPay\Internal\Service;
 
+use Exception;
+
 /**
  *  Payment Processing Service.
  */
 class PaymentProcessingService {
 
+	/**
+	 * Process payment.
+	 *
+	 * @param  int $order_id Order ID provided by WooCommerce core.
+	 *
+	 * @throws Exception
+	 */
+	public function process_payment( int $order_id ) {
+		throw new Exception( 'Re-engineering payment process is in-progress. Sit tight, and wait more!' );
+	}
 }
