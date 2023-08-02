0c2247cc0ed350d1730209e2e419d1a3d9ab6bef
Radoslav Georgiev <rageorgiev@gmail.com>
2023-08-02 18:53:21 +0300

    Adding empty tests

---
+++ tests/unit/src/Internal/Proxy/HooksProxyTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Class HooksProxyTest
+ *
+ * @package WooCommerce\Payments
+ */
+
+namespace WCPay\Tests\Internal\Proxy;
+
+use WCPAY_UnitTestCase;
+use WCPay\Internal\Proxy\HooksProxy;
+
+/**
+ * Hooks proxy unit tests.
+ */
+class HooksProxyTest extends WCPAY_UnitTestCase {
+
+}
---
+++ tests/unit/src/Internal/Proxy/LegacyProxyTest.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Class LegacyProxyTest
+ *
+ * @package WooCommerce\Payments
+ */
+
+namespace WCPay\Tests\Internal\Proxy;
+
+use WCPAY_UnitTestCase;
+use WCPay\Internal\Proxy\LegacyProxy;
+
+/**
+ * Legacy proxy unit tests.
+ */
+class LegacyProxyTest extends WCPAY_UnitTestCase {
+
+}
