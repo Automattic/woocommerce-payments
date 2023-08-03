1d35e1fdf608d1f3a8fd53de9c98ca4940e95422
Radoslav Georgiev <rageorgiev@gmail.com>
2023-08-03 17:03:25 +0300

    Fixing all the broken tests

--- src/Internal/DependencyManagement/DelegateContainer/LegacyContainer.php
+++ src/Internal/DependencyManagement/DelegateContainer/LegacyContainer.php
@@ -17,7 +17,7 @@ use WCPay\Core\Mode;
  * This class is a proxy between `src` and `includes`, allwing
  * `includes` classes to be used as dependencies.
  */
-final class LegacyContainer implements ContainerInterface {
+class LegacyContainer implements ContainerInterface {
 	/**
 	 * Finds an entry of the container by its identifier and returns it.
 	 *
