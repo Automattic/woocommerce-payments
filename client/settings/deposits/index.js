5347ae6eee845e2ba116023e3516e0b1b6a8b2b8
elazzabi <ahmed.el.azzabi@automattic.com>
2023-08-03 10:11:31 +0100

    Add a check before adding "Daily" option

--- client/settings/deposits/index.js
+++ client/settings/deposits/index.js
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import React, { useContext } from 'react';
+import { select } from '@wordpress/data';
 import { __ } from '@wordpress/i18n';
 import {
 	Card,
@@ -10,6 +11,7 @@ import {
 	Notice,
 } from '@wordpress/components';
 import HelpOutlineIcon from 'gridicons/dist/help-outline';
+import { STORE_NAME } from 'wcpay/data/constants';
 
 /**
  * Internal dependencies
@@ -62,6 +64,8 @@ const CustomizeDepositSchedule = () => {
 		setDepositScheduleMonthlyAnchor,
 	] = useDepositScheduleMonthlyAnchor();
 
+	const settings = select( STORE_NAME ).getSettings();
+
 	const handleIntervalChange = ( newInterval ) => {
 		switch ( newInterval ) {
 			case 'weekly':
@@ -80,6 +84,27 @@ const CustomizeDepositSchedule = () => {
 		setDepositScheduleInterval( newInterval );
 	};
 
+	let depositIntervalsOptions = [
+		{
+			value: 'weekly',
+			label: __( 'Weekly', 'woocommerce-payments' ),
+		},
+		{
+			value: 'monthly',
+			label: __( 'Monthly', 'woocommerce-payments' ),
+		},
+	];
+
+	if ( settings.deposit_delay_days <= 1 ) {
+		depositIntervalsOptions = [
+			{
+				value: 'daily',
+				label: __( 'Daily', 'woocommerce-payments' ),
+			},
+			...depositIntervalsOptions,
+		];
+	}
+
 	return (
 		<>
 			<div className="schedule-controls">
@@ -87,20 +112,7 @@ const CustomizeDepositSchedule = () => {
 					label={ __( 'Frequency', 'woocommerce-payments' ) }
 					value={ depositScheduleInterval }
 					onChange={ handleIntervalChange }
-					options={ [
-						{
-							value: 'daily',
-							label: __( 'Daily', 'woocommerce-payments' ),
-						},
-						{
-							value: 'weekly',
-							label: __( 'Weekly', 'woocommerce-payments' ),
-						},
-						{
-							value: 'monthly',
-							label: __( 'Monthly', 'woocommerce-payments' ),
-						},
-					] }
+					options={ depositIntervalsOptions }
 				/>
 				{ depositScheduleInterval === 'monthly' && (
 					<SelectControl
