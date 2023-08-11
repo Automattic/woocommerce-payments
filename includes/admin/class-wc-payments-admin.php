2293e87da7f5a3260b3940498326ef78dbd03625
bruce aldridge <bruce.aldridge@gmail.com>
2023-08-11 13:34:39 +1200

    Update includes/admin/class-wc-payments-admin.php
    Co-authored-by: Eric Jinks <3147296+Jinksi@users.noreply.github.com>

--- includes/admin/class-wc-payments-admin.php
+++ includes/admin/class-wc-payments-admin.php
@@ -695,7 +695,7 @@ class WC_Payments_Admin {
 		if ( in_array( $screen->id, [ 'shop_order', 'woocommerce_page_wc-orders' ], true ) ) {
 			$order = wc_get_order();
 
-			if ( WC_Payment_Gateway_WCPay::GATEWAY_ID === $order->get_payment_method() ) {
+			if ( $order && WC_Payment_Gateway_WCPay::GATEWAY_ID === $order->get_payment_method() ) {
 				$refund_amount = $order->get_remaining_refund_amount();
 				wp_localize_script(
 					'WCPAY_ADMIN_ORDER_ACTIONS',
