21ea0474088efad81be3447e45abeff65649ac08
Shendy Kurnia <shendy.kurnia@a8c.com>
2023-08-14 10:10:34 +0700

    Show empty bordered section for dispute details.

--- client/payment-details/dispute-details/index.tsx
+++ client/payment-details/dispute-details/index.tsx
@@ -8,6 +8,7 @@ import React from 'react';
  * Internal dependencies
  */
 import { Dispute } from '../../types/disputes';
+import { Card, CardBody } from '@wordpress/components';
 import './style.scss';
 
 interface DisputeDetailsProps {
@@ -15,7 +16,16 @@ interface DisputeDetailsProps {
 }
 
 const DisputeDetails: React.FC< DisputeDetailsProps > = ( { dispute } ) => {
-	return <></>;
+	return (
+		<div>
+			<Card className="transaction-details-dispute-details-wrapper">
+				<CardBody>
+					<div></div>
+				</CardBody>
+			</Card>
+			<br />
+		</div>
+	);
 };
 
 export default DisputeDetails;
