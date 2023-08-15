a69c6d7453cb85f88c607e4cf3fcc8d46007cfa5
Bruce Aldridge <bruce.aldridge@gmail.com>
2023-08-15 16:20:43 +1200

    Add dispute notice to transactions page

---
+++ client/payment-details/dispute-details/dispute-notice.tsx
@@ -0,0 +1,67 @@
+/** @format **/
+
+/**
+ * External dependencies
+ */
+import React from 'react';
+import { __, sprintf } from '@wordpress/i18n';
+import { createInterpolateElement } from '@wordpress/element';
+import NoticeOutlineIcon from 'gridicons/dist/notice-outline';
+/**
+ * Internal dependencies
+ */
+import './style.scss';
+import BannerNotice from 'components/banner-notice';
+import { reasons } from 'wcpay/disputes/strings';
+import { Dispute } from 'wcpay/types/disputes';
+
+interface DisputeNoticeProps {
+	dispute: Dispute;
+	urgent: boolean;
+}
+
+const DisputeNotice: React.FC< DisputeNoticeProps > = ( {
+	dispute,
+	urgent,
+} ) => {
+	const clientClaim =
+		reasons[ dispute.reason ]?.claim ??
+		__(
+			'The cardholder claims this is an unrecognized charge.',
+			'woocommerce-payments'
+		);
+
+	return (
+		<BannerNotice
+			status={ urgent ? 'error' : 'warning' }
+			icon={ <NoticeOutlineIcon /> }
+			className="dispute-notice"
+			isDismissible={ false }
+		>
+			{ createInterpolateElement(
+				sprintf(
+					/* translators: %s is the clients claim for the dispute, eg "The cardholder claims this is an unrecognized charge." */
+					__(
+						// eslint-disable-next-line max-len
+						'<strong>%s</strong> Challenge the dispute if you believe the claim is invalid, or accept to forfeit the funds and pay the dispute fee. Non-response will result in an automatic loss. <a>Learn more about responding to disputes</a>',
+						'woocommerce-payments'
+					),
+					clientClaim
+				),
+				{
+					a: (
+						// eslint-disable-next-line jsx-a11y/anchor-has-content
+						<a
+							target="_blank"
+							rel="noopener noreferrer"
+							href="https://woocommerce.com/document/woopayments/fraud-and-disputes/managing-disputes/#section-3"
+						/>
+					),
+					strong: <strong />,
+				}
+			) }
+		</BannerNotice>
+	);
+};
+
+export default DisputeNotice;
