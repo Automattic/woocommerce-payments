27889f661e147abf2cfa76f405f767b8cd707729
Ismael Martín Alabarce <info@ismaeld.com>
2023-08-16 14:35:11 +0200

    Display onboarding MCC field validation error (#7010)

--- client/components/form/fields.tsx
+++ client/components/form/fields.tsx
@@ -9,26 +9,40 @@ import classNames from 'classnames';
  * Internal dependencies
  */
 import CustomSelectControl, {
-	ControlProps,
-	Item,
-} from 'components/custom-select-control';
+	ControlProps as SelectControlProps,
+	Item as SelectItem,
+} from '../custom-select-control';
 import PhoneNumberControl, {
 	PhoneNumberControlProps,
 } from '../phone-number-control';
+import GroupedSelectControl, {
+	GroupedSelectControlProps,
+	ListItem as GroupedSelectItem,
+} from '../grouped-select-control';
 import './style.scss';
 
 interface CommonProps {
 	error?: string;
 }
 
+type Item = SelectItem | GroupedSelectItem;
 export type TextFieldProps = TextControl.Props & CommonProps;
-export type SelectFieldProps< ItemType > = ControlProps< ItemType > &
+export type SelectFieldProps< ItemType > = SelectControlProps< ItemType > &
 	CommonProps;
 export type PhoneNumberFieldProps = PhoneNumberControlProps & CommonProps;
+export type GroupedSelectFieldProps< ItemType > = GroupedSelectControlProps<
+	ItemType
+> &
+	CommonProps;
 
 type FieldProps< ItemType > = {
-	component: 'text' | 'select' | 'phone';
-} & ( TextFieldProps | SelectFieldProps< ItemType > | PhoneNumberFieldProps );
+	component: 'text' | 'select' | 'phone' | 'grouped-select';
+} & (
+	| TextFieldProps
+	| SelectFieldProps< ItemType >
+	| PhoneNumberFieldProps
+	| GroupedSelectFieldProps< ItemType >
+ );
 
 const Field = < ItemType extends Item >( {
 	component,
@@ -46,12 +60,16 @@ const Field = < ItemType extends Item >( {
 			field = <TextControl { ...props } />;
 			break;
 		case 'select':
-			props = rest as SelectFieldProps< ItemType >;
+			props = rest as SelectFieldProps< SelectItem >;
 			field = <CustomSelectControl { ...props } />;
 			break;
 		case 'phone':
 			props = rest as PhoneNumberFieldProps;
 			field = <PhoneNumberControl { ...props } />;
+			break;
+		case 'grouped-select':
+			props = rest as GroupedSelectFieldProps< GroupedSelectItem >;
+			field = <GroupedSelectControl { ...props } />;
 			break;
 	}
 
@@ -69,12 +87,16 @@ export const TextField: React.FC< TextFieldProps > = ( props ) => (
 	<Field component={ 'text' } { ...props } />
 );
 
-export const SelectField = < ItemType extends Item >(
+export const SelectField = < ItemType extends SelectItem >(
 	props: SelectFieldProps< ItemType >
 ): JSX.Element => <Field component={ 'select' } { ...props } />;
 
 export const PhoneNumberField: React.FC< PhoneNumberControlProps > = (
 	props
 ) => <Field component={ 'phone' } { ...props } />;
+
+export const GroupedSelectField = < ItemType extends GroupedSelectItem >(
+	props: GroupedSelectControlProps< ItemType >
+): JSX.Element => <Field component={ 'grouped-select' } { ...props } />;
 
 export default Field;
