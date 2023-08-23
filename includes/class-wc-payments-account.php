b91ef8e6aeede7696c19e8312149e13a63b67aeb
David Quiñones <daquinons@users.noreply.github.com>
2023-08-23 16:35:22 +0900

    Add default value to detailsSubmitted

--- includes/class-wc-payments-account.php
+++ includes/class-wc-payments-account.php
@@ -242,7 +242,7 @@ class WC_Payments_Account {
 			'status'                => $account['status'],
 			'created'               => $account['created'] ?? '',
 			'paymentsEnabled'       => $account['payments_enabled'],
-			'detailsSubmitted'      => $account['details_submitted'],
+			'detailsSubmitted'      => $account['details_submitted'] ?? true,
 			'deposits'              => $account['deposits'] ?? [],
 			'depositsStatus'        => $account['deposits']['status'] ?? $account['deposits_status'] ?? '',
 			'currentDeadline'       => $account['current_deadline'] ?? false,
