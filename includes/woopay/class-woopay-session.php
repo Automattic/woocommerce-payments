bf1676a5bf539a869bc3773ca43ddb0e5047d12e
Brian Borman <brian.borman@automattic.com>
2023-08-23 16:09:19 -0400

    Session compatible fix for WooPay multi-currency

--- includes/woopay/class-woopay-session.php
+++ includes/woopay/class-woopay-session.php
@@ -53,7 +53,7 @@ class WooPay_Session {
 	 */
 	public static function init() {
 		add_filter( 'determine_current_user', [ __CLASS__, 'determine_current_user_for_woopay' ], 20 );
-		add_filter( 'rest_request_before_callbacks', [ __CLASS__, 'add_woopay_store_api_session_handler' ], 10, 3 );
+		add_filter( 'woocommerce_session_handler', [ __CLASS__, 'add_woopay_store_api_session_handler' ], 20 );
 		add_action( 'woocommerce_order_payment_status_changed', [ __CLASS__, 'remove_order_customer_id_on_requests_with_verified_email' ] );
 		add_action( 'woopay_restore_order_customer_id', [ __CLASS__, 'restore_order_customer_id_from_requests_with_verified_email' ] );
 
@@ -64,31 +64,24 @@ class WooPay_Session {
 	 * This filter is used to add a custom session handler before processing Store API request callbacks.
 	 * This is only necessary because the Store API SessionHandler currently doesn't provide an `init_session_cookie` method.
 	 *
-	 * @param mixed           $response The response object.
-	 * @param mixed           $handler The handler used for the response.
-	 * @param WP_REST_Request $request The request used to generate the response.
+	 * @param string $default_session_handler The default session handler class name.
 	 *
-	 * @return mixed
+	 * @return string The session handler class name.
 	 */
-	public static function add_woopay_store_api_session_handler( $response, $handler, WP_REST_Request $request ) {
-		$cart_token = $request->get_header( 'Cart-Token' );
+	public static function add_woopay_store_api_session_handler( $default_session_handler ) {
+		$cart_token = wc_clean( wp_unslash( $_SERVER['HTTP_CART_TOKEN'] ?? null ) );
 
 		if (
 			$cart_token &&
+			self::is_request_from_woopay() &&
 			self::is_store_api_request() &&
 			class_exists( JsonWebToken::class ) &&
 			JsonWebToken::validate( $cart_token, '@' . wp_salt() )
 		) {
-			add_filter(
-				'woocommerce_session_handler',
-				function ( $session_handler ) {
-					return SessionHandler::class;
-				},
-				20
-			);
+			return SessionHandler::class;
 		}
 
-		return $response;
+		return $default_session_handler;
 	}
 
 	/**
@@ -493,10 +486,6 @@ class WooPay_Session {
 	 * @return bool True if request is a Store API request, false otherwise.
 	 */
 	private static function is_store_api_request(): bool {
-		if ( ! defined( 'REST_REQUEST' ) || ! REST_REQUEST ) {
-			return false;
-		}
-
 		$url_parts    = wp_parse_url( esc_url_raw( $_SERVER['REQUEST_URI'] ?? '' ) ); // phpcs:ignore WordPress.Security.ValidatedSanitizedInput.MissingUnslash
 		$request_path = rtrim( $url_parts['path'], '/' );
 		$rest_route   = str_replace( trailingslashit( rest_get_url_prefix() ), '', $request_path );
