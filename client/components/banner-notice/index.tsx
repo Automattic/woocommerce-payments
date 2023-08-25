1ebae11e69bddeb01ad1b507431e3ebe82ce41e3
Ismael Martín Alabarce <info@ismaeld.com>
2023-08-25 12:31:58 +0200

    Update BannerNotice to use icons baed on status
    in addition to custom icons.

--- client/components/banner-notice/index.tsx
+++ client/components/banner-notice/index.tsx
@@ -4,38 +4,30 @@
 import * as React from 'react';
 import { Flex, FlexItem, Icon, Notice, Button } from '@wordpress/components';
 import classNames from 'classnames';
+import CheckmarkIcon from 'gridicons/dist/checkmark';
+import NoticeOutlineIcon from 'gridicons/dist/notice-outline';
+import InfoOutlineIcon from 'gridicons/dist/info-outline';
 
 /**
  * Internal dependencies.
  */
 import './styles.scss';
 
-/**
- * Props for the BannerNotice component.
- *
- * @typedef {Object} BannerNoticeProps
- * @property {Icon.IconType<unknown>} icon The icon to display.
- */
 interface BannerNoticeProps extends Notice.Props {
-	icon?: Icon.IconType< unknown >;
+	/**
+	 * Whether to display the default icon based on status prop or the icon to display.
+	 * Supported values are: boolean, JSX.Element and `undefined`.
+	 *
+	 * @default undefined
+	 */
+	icon?: boolean | JSX.Element;
 }
 
 /**
  * Renders a banner notice.
- *
- * @param {BannerNoticeProps} props                    Banner notice props.
- * @param {Icon.IconType<unknown>} props.icon          The icon to display. Supports all icons from @wordpress/icons.
- * @param {Notice.Props} props.noticeProps             The props for the Notice component.
- * @param {string} props.noticeProps.status            The status of the notice.
- * @param {boolean} props.noticeProps.isDismissible    Whether the notice is dismissible.
- * @param {string} props.noticeProps.className         The class name for the notice.
- * @param {React.ReactNode} props.noticeProps.children The children of the notice.
- * @param {Notice.Action[]} props.noticeProps.actions  The actions for the notice.
- *
- * @return {JSX.Element} Rendered banner notice.
  */
 function BannerNotice( props: BannerNoticeProps ): JSX.Element {
-	const { icon, ...noticeProps } = props;
+	const { icon, actions, children, ...noticeProps } = props;
 
 	// Add the default class name to the notice.
 	noticeProps.className = classNames(
@@ -44,62 +36,70 @@ function BannerNotice( props: BannerNoticeProps ): JSX.Element {
 		noticeProps.className
 	);
 
-	// Convert the notice actions to buttons or link elements.
-	let actions = null;
-	if ( noticeProps.actions ) {
-		const actionClass = 'wcpay-banner-notice__action';
-		actions = noticeProps.actions.map( ( action, index ) => {
-			// Actions that contain a URL will be rendered as a link.
-			// This matches WP Notice component behavior.
-			if ( 'url' in action ) {
-				return (
-					<a
-						key={ index }
-						className={ actionClass }
-						href={ action.url }
-					>
-						{ action.label }
-					</a>
-				);
-			}
-
-			return (
-				<Button
-					key={ index }
-					className={ actionClass }
-					onClick={ action.onClick }
-				>
-					{ action.label }
-				</Button>
-			);
-		} );
-
-		// We'll render the actions ourselves so we need to remove them from the props sent to the notice component.
-		delete noticeProps.actions;
+	// Use default icon based on status if icon === true.
+	let iconToDisplay = icon;
+	if ( iconToDisplay === true ) {
+		switch ( noticeProps.status ) {
+			case 'success':
+				iconToDisplay = <CheckmarkIcon />;
+				break;
+			case 'error':
+			case 'warning':
+				iconToDisplay = <NoticeOutlineIcon />;
+				break;
+			case 'info':
+			default:
+				iconToDisplay = <InfoOutlineIcon />;
+				break;
+		}
 	}
+
+	// Convert the notice actions to buttons or link elements.
+	const actionClass = 'wcpay-banner-notice__action';
+	const mappedActions = actions?.map( ( action, index ) => {
+		// Actions that contain a URL will be rendered as a link.
+		// This matches WP Notice component behavior.
+		if ( 'url' in action ) {
+			return (
+				<a key={ index } className={ actionClass } href={ action.url }>
+					{ action.label }
+				</a>
+			);
+		}
+
+		return (
+			<Button
+				key={ index }
+				className={ actionClass }
+				onClick={ action.onClick }
+			>
+				{ action.label }
+			</Button>
+		);
+	} );
 
 	return (
 		<Notice { ...noticeProps }>
 			<Flex align="center" justify="flex-start">
-				{ icon && (
+				{ iconToDisplay && (
 					<FlexItem
 						className={ `wcpay-banner-notice__icon wcpay-banner-${ noticeProps.status }-notice__icon` }
 					>
-						<Icon icon={ icon } size={ 24 } />
+						<Icon icon={ iconToDisplay } size={ 24 } />
 					</FlexItem>
 				) }
 				<FlexItem
 					className={ `wcpay-banner-notice__content wcpay-banner-${ noticeProps.status }-notice__content` }
 				>
-					{ noticeProps.children }
-					{ actions && (
+					{ children }
+					{ mappedActions && (
 						<Flex
 							className="wcpay-banner-notice__content__actions"
 							align="baseline"
 							justify="flex-start"
 							gap={ 4 }
 						>
-							{ actions }
+							{ mappedActions }
 						</Flex>
 					) }
 				</FlexItem>
