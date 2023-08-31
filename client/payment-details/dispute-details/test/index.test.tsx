3545ae9698b3022ec29cfef4e35d19f1049523d3
Eric Jinks <3147296+Jinksi@users.noreply.github.com>
2023-08-31 16:49:54 +1000

    Add test for dispute with staged evidence

--- client/payment-details/dispute-details/test/index.test.tsx
+++ client/payment-details/dispute-details/test/index.test.tsx
@@ -9,6 +9,7 @@ import React from 'react';
  * Internal dependencies
  */
 import type { Dispute } from 'wcpay/types/disputes';
+import type { Charge } from 'wcpay/types/charges';
 import { useDisputeAccept } from 'wcpay/data';
 import DisputeDetails from '..';
 
@@ -52,68 +53,83 @@ global.wcpaySettings = {
 	},
 };
 
-const charge = {
-	id: 'ch_38jdHA39KKA',
-	/* Stripe data comes in seconds, instead of the default Date milliseconds */
-	created: Date.parse( 'Sep 19, 2019, 5:24 pm' ) / 1000,
-	amount: 2000,
-	amount_refunded: 0,
-	application_fee_amount: 70,
-	disputed: false,
-	dispute: {
-		id: 'dp_1',
-		amount: 6800,
-		charge: 'ch_38jdHA39KKA',
-		order: null,
-		balance_transactions: [
-			{
-				amount: -2000,
-				currency: 'usd',
-				fee: 1500,
-				reporting_category: 'dispute',
-			},
-		],
-		created: 1693453017,
-		currency: 'usd',
-		evidence: {
-			billing_address: '123 test address',
-			customer_email_address: 'test@email.com',
-			customer_name: 'Test customer',
-			shipping_address: '123 test address',
-		},
-		evidence_details: {
-			due_by: 1694303999,
-			has_evidence: false,
-			past_due: false,
-			submission_count: 0,
-		},
-		// issuer_evidence: null,
-		metadata: [],
-		payment_intent: 'pi_1',
-		reason: 'fraudulent',
-		status: 'needs_response',
-	} as Dispute,
-	currency: 'usd',
-	type: 'charge',
-	status: 'succeeded',
-	paid: true,
-	captured: true,
-	balance_transaction: {
+interface ChargeWithDisputeRequired extends Charge {
+	dispute: Dispute;
+}
+
+const getBaseCharge = (): ChargeWithDisputeRequired =>
+	( {
+		id: 'ch_38jdHA39KKA',
+		/* Stripe data comes in seconds, instead of the default Date milliseconds */
+		created: Date.parse( 'Sep 19, 2019, 5:24 pm' ) / 1000,
 		amount: 2000,
+		amount_refunded: 0,
+		application_fee_amount: 70,
+		disputed: true,
+		dispute: {
+			id: 'dp_1',
+			amount: 6800,
+			charge: 'ch_38jdHA39KKA',
+			order: null,
+			balance_transactions: [
+				{
+					amount: -2000,
+					currency: 'usd',
+					fee: 1500,
+					reporting_category: 'dispute',
+				},
+			],
+			created: 1693453017,
+			currency: 'usd',
+			evidence: {
+				billing_address: '123 test address',
+				customer_email_address: 'test@email.com',
+				customer_name: 'Test customer',
+				shipping_address: '123 test address',
+			},
+			evidence_details: {
+				due_by: 1694303999,
+				has_evidence: false,
+				past_due: false,
+				submission_count: 0,
+			},
+			// issuer_evidence: null,
+			metadata: [],
+			payment_intent: 'pi_1',
+			reason: 'fraudulent',
+			status: 'needs_response',
+		} as Dispute,
 		currency: 'usd',
-		fee: 70,
-	},
-	refunds: {
-		data: [],
-	},
-	payment_method_details: {
-		card: {
-			brand: 'visa',
-			last4: '4242',
+		type: 'charge',
+		status: 'succeeded',
+		paid: true,
+		captured: true,
+		balance_transaction: {
+			amount: 2000,
+			currency: 'usd',
+			fee: 70,
 		},
-		type: 'card',
-	},
-};
+		refunds: {
+			data: [],
+		},
+		order: {
+			number: 45981,
+			url: 'https://somerandomorderurl.com/?edit_order=45981',
+		},
+		billing_details: {
+			name: 'Customer name',
+		},
+		payment_method_details: {
+			card: {
+				brand: 'visa',
+				last4: '4242',
+			},
+			type: 'card',
+		},
+		outcome: {
+			risk_level: 'normal',
+		},
+	} as any );
 
 // mock the useDisputeAccept hook
 jest.mock( 'wcpay/data', () => ( {
@@ -147,6 +163,7 @@ describe( 'DisputeDetails', () => {
 	} );
 
 	test( 'correctly renders dispute details', () => {
+		const charge = getBaseCharge();
 		render( <DisputeDetails dispute={ charge.dispute } /> );
 
 		screen.getByText(
@@ -161,7 +178,28 @@ describe( 'DisputeDetails', () => {
 		} );
 	} );
 
+	test( 'correctly renders dispute details for a dispute with staged evidence', () => {
+		const charge = getBaseCharge();
+		charge.dispute.evidence_details = {
+			has_evidence: true,
+			due_by: 1694303999,
+			past_due: false,
+			submission_count: 0,
+		};
+
+		render( <DisputeDetails dispute={ charge.dispute } /> );
+
+		screen.getByText(
+			/The cardholder claims this is an unauthorized transaction/,
+			{ ignore: '.a11y-speak-region' }
+		);
+		screen.getByRole( 'button', {
+			name: /Continue with challenge/,
+		} );
+	} );
+
 	test( 'correctly renders the accept dispute modal and accepts', () => {
+		const charge = getBaseCharge();
 		render( <DisputeDetails dispute={ charge.dispute } /> );
 
 		const openModalButton = screen.getByRole( 'button', {
@@ -186,6 +224,7 @@ describe( 'DisputeDetails', () => {
 	} );
 
 	test( 'correctly navigates to the challenge screen', () => {
+		const charge = getBaseCharge();
 		render( <DisputeDetails dispute={ charge.dispute } /> );
 
 		const challengeButton = screen.getByRole( 'button', {
