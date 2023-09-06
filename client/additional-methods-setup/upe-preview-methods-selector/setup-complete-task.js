c682a08aad2bdc2365aacc807b20546e14be2b74
Daniel Mallory <daniel.mallory@automattic.com>
2023-09-06 12:12:48 +0100

    Adding code to update an option when the APMs setup is complete.

--- client/additional-methods-setup/upe-preview-methods-selector/setup-complete-task.js
+++ client/additional-methods-setup/upe-preview-methods-selector/setup-complete-task.js
@@ -82,6 +82,11 @@ const SetupComplete = () => {
 			return;
 		}
 
+		updateOptions( {
+			// eslint-disable-next-line camelcase
+			wcpay_apm_setup_complete: 'yes',
+		} );
+
 		window.wcpaySettings.additionalMethodsSetup.isUpeEnabled = true;
 	}, [ isActive, updateOptions ] );
 
