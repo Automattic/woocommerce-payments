9b5706dc11b9cc6b9beb048500bac5432b8f05fe
James Allan <james.allan@automattic.com>
2023-09-07 11:34:51 +1000

    Update public encryption setting js test

--- client/settings/advanced-settings/test/index.test.js
+++ client/settings/advanced-settings/test/index.test.js
@@ -10,25 +10,8 @@ import userEvent from '@testing-library/user-event';
  * Internal dependencies
  */
 import AdvancedSettings from '..';
-import { useSettings } from 'wcpay/data';
-
-jest.mock( 'wcpay/data', () => ( {
-	useSettings: jest.fn(),
-} ) );
 
 describe( 'AdvancedSettings', () => {
-	beforeEach( () => {
-		useSettings.mockReturnValue( {
-			saveSettings: () => Promise.resolve( true ),
-			isSaving: false,
-			isLoading: false,
-		} );
-	} );
-
-	afterEach( () => {
-		jest.clearAllMocks();
-	} );
-
 	it( 'toggles the advanced settings section', () => {
 		global.wcpaySettings = {
 			isClientEncryptionEligible: true,
@@ -53,14 +36,13 @@ describe( 'AdvancedSettings', () => {
 		};
 		render( <AdvancedSettings /> );
 
-		expect( screen.queryByText( 'Debug mode' ) ).not.toBeInTheDocument();
-
-		userEvent.click( screen.getByText( 'Advanced settings' ) );
-
 		expect(
 			screen.queryByText( 'Enable Public Key Encryption' )
 		).not.toBeInTheDocument();
 		expect( screen.queryByText( 'Debug mode' ) ).toBeInTheDocument();
-		expect( screen.getByText( 'Debug mode' ) ).toHaveFocus();
+
+		userEvent.click( screen.getByText( 'Advanced settings' ) );
+
+		expect( screen.queryByText( 'Debug mode' ) ).not.toBeInTheDocument();
 	} );
 } );
