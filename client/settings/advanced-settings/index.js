921a15b1b61d9c4ce44a1eefa2842b0f9df90a96
James Allan <james.allan@automattic.com>
2023-09-08 10:23:46 +1000

    Make the advanced settings a standalone card

--- client/settings/advanced-settings/index.js
+++ client/settings/advanced-settings/index.js
@@ -2,61 +2,37 @@
  * External dependencies
  */
 import React from 'react';
-import { Icon, chevronDown, chevronUp } from '@wordpress/icons';
-import { __ } from '@wordpress/i18n';
-import { Card, Button } from '@wordpress/components';
+import { Card } from '@wordpress/components';
 
 /**
  * Internal dependencies
  */
-import SettingsSection from '../settings-section';
 import DebugMode from './debug-mode';
 import MultiCurrencyToggle from './multi-currency-toggle';
 import WCPaySubscriptionsToggle from './wcpay-subscriptions-toggle';
-import useToggle from './use-toggle';
 import './style.scss';
 import CardBody from '../card-body';
-import ErrorBoundary from '../../components/error-boundary';
 import ClientSecretEncryptionToggle from './client-secret-encryption-toggle';
 import StripeBillingSection from './stripe-billing-section';
-import LoadableSettingsSection from '../loadable-settings-section';
 
 const AdvancedSettings = () => {
-	const [ isSectionExpanded, toggleIsSectionExpanded ] = useToggle( true );
-
 	return (
 		<>
-			<SettingsSection>
-				<Button onClick={ toggleIsSectionExpanded } isTertiary>
-					{ __( 'Advanced settings', 'wordpress-components' ) }
-					<Icon
-						icon={ isSectionExpanded ? chevronUp : chevronDown }
-					/>
-				</Button>
-			</SettingsSection>
-			{ isSectionExpanded && (
-				<SettingsSection>
-					<LoadableSettingsSection numLines={ 15 }>
-						<ErrorBoundary>
-							<Card>
-								<CardBody>
-									<MultiCurrencyToggle />
-									{ wcpaySettings.isClientEncryptionEligible && (
-										<ClientSecretEncryptionToggle />
-									) }
-									{ wcpaySettings.isSubscriptionsActive &&
-									wcpaySettings.isStripeBillingEligible ? (
-										<StripeBillingSection />
-									) : (
-										<WCPaySubscriptionsToggle />
-									) }
-									<DebugMode />
-								</CardBody>
-							</Card>
-						</ErrorBoundary>
-					</LoadableSettingsSection>
-				</SettingsSection>
-			) }
+			<Card>
+				<CardBody>
+					<MultiCurrencyToggle />
+					{ wcpaySettings.isClientEncryptionEligible && (
+						<ClientSecretEncryptionToggle />
+					) }
+					{ wcpaySettings.isSubscriptionsActive &&
+					wcpaySettings.isStripeBillingEligible ? (
+						<StripeBillingSection />
+					) : (
+						<WCPaySubscriptionsToggle />
+					) }
+					<DebugMode />
+				</CardBody>
+			</Card>
 		</>
 	);
 };
