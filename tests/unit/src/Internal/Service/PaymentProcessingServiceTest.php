de946eb78dd7a2edc366b9a6e4989fe93d43741e
Radoslav Georgiev <rageorgiev@gmail.com>
2023-09-12 16:53:51 +0200

    Fixing existing tests

--- tests/unit/src/Internal/Service/PaymentProcessingServiceTest.php
+++ tests/unit/src/Internal/Service/PaymentProcessingServiceTest.php
@@ -7,6 +7,9 @@
 
 namespace WCPay\Tests\Internal\Service;
 
+use PHPUnit\Framework\MockObject\MockObject;
+use WCPay\Internal\Payment\StateFactory;
+use WCPay\Internal\Proxy\LegacyProxy;
 use WCPAY_UnitTestCase;
 use WCPay\Internal\Service\PaymentProcessingService;
 
@@ -22,12 +25,32 @@ class PaymentProcessingServiceTest extends WCPAY_UnitTestCase {
 	private $sut;
 
 	/**
+	 * StateFactory mock.
+	 *
+	 * @var StateFactory|MockObject
+	 */
+	private $state_factory_mock;
+
+	/**
+	 * LegacyProxy mock.
+	 *
+	 * @var LegacyProxy|MockObject
+	 */
+	private $legacy_proxy_mock;
+
+	/**
 	 * Set up the test.
 	 */
 	protected function setUp(): void {
 		parent::setUp();
 
-		$this->sut = new PaymentProcessingService();
+		$this->state_factory_mock = $this->createMock( StateFactory::class );
+		$this->legacy_proxy_mock  = $this->createMock( LegacyProxy::class );
+
+		$this->sut = new PaymentProcessingService(
+			$this->state_factory_mock,
+			$this->legacy_proxy_mock
+		);
 	}
 
 	/**
@@ -36,7 +59,7 @@ class PaymentProcessingServiceTest extends WCPAY_UnitTestCase {
 	public function test_class_is_loaded() {
 		$this->assertTrue( class_exists( PaymentProcessingService::class ) );
 
-		$instance = new PaymentProcessingService();
+		$instance = new PaymentProcessingService( $this->state_factory_mock, $this->legacy_proxy_mock );
 		$this->assertInstanceOf( PaymentProcessingService::class, $instance );
 	}
 
