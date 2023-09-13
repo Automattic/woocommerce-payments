f2a9286815f269149555267dfbccd1fdfd63e77d
frosso <francesco.rosso@automattic.com>
2023-09-13 13:47:16 +0200

    fix: express checkouts links consistency

--- client/settings/express-checkout/link-item.tsx
+++ client/settings/express-checkout/link-item.tsx
@@ -3,7 +3,7 @@
  */
 import React from 'react';
 import { __ } from '@wordpress/i18n';
-import { CheckboxControl, VisuallyHidden } from '@wordpress/components';
+import { Button, CheckboxControl, VisuallyHidden } from '@wordpress/components';
 import interpolateComponents from '@automattic/interpolate-components';
 
 /**
@@ -19,6 +19,8 @@ import { HoverTooltip } from 'components/tooltip';
 import LinkIcon from 'assets/images/payment-methods/link.svg?asset';
 import NoticeOutlineIcon from 'gridicons/dist/notice-outline';
 import { EnabledMethodIdsHook } from './interfaces';
+import { getAdminUrl } from 'wcpay/utils';
+import { ProtectionLevel } from 'wcpay/settings/fraud-protection/advanced-settings/constants';
 
 const LinkExpressCheckoutItem = (): React.ReactElement => {
 	const availablePaymentMethodIds = useGetAvailablePaymentMethodIds() as Array<
@@ -154,26 +156,18 @@ const LinkExpressCheckoutItem = (): React.ReactElement => {
 								</div>
 							</div>
 							<div className="express-checkout__link">
-								{
-									/* eslint-disable jsx-a11y/anchor-has-content */
-									interpolateComponents( {
-										mixedString: __(
-											'{{linkDocs}}Read more{{/linkDocs}}',
-											'woocommerce-payments'
-										),
-										components: {
-											linkDocs: (
-												<a
-													target="_blank"
-													rel="noreferrer"
-													/* eslint-disable-next-line max-len */
-													href="https://woocommerce.com/document/woopayments/payment-methods/link-by-stripe/"
-												/>
-											),
-										},
-									} )
-									/* eslint-enable jsx-a11y/anchor-has-content */
-								}
+								<Button
+									target="_blank"
+									rel="noreferrer"
+									/* eslint-disable-next-line max-len */
+									href="https://woocommerce.com/document/woopayments/payment-methods/link-by-stripe/"
+									isSecondary
+								>
+									{ __(
+										'Read more',
+										'woocommerce-payments'
+									) }
+								</Button>
 							</div>
 						</div>
 					</div>
