0597040584da4f1970e9df1cf96aa7285f0777d2
hsingyuc <hsingyuc.7@gmail.com>
2023-09-26 16:26:39 -0500

    Remove the express button on the pay-for-order page

--- includes/class-wc-payments-express-checkout-button-display-handler.php
+++ includes/class-wc-payments-express-checkout-button-display-handler.php
@@ -58,11 +58,6 @@ class WC_Payments_Express_Checkout_Button_Display_Handler {
 			add_action( 'woocommerce_proceed_to_checkout', [ $this, 'display_express_checkout_buttons' ], 21 );
 			add_action( 'woocommerce_checkout_before_customer_details', [ $this, 'display_express_checkout_buttons' ], 1 );
 		}
-
-		if ( class_exists( '\Automattic\WooCommerce\Blocks\Package' ) && version_compare( \Automattic\WooCommerce\Blocks\Package::get_version(), '10.8.0', '>=' ) ) {
-			add_action( 'wp_enqueue_scripts', [ $this, 'add_pay_for_order_params_to_js_config' ], 5 );
-			add_action( 'woocommerce_pay_order_before_payment', [ $this, 'display_express_checkout_buttons' ], 1 );
-		}
 	}
 
 	/**
