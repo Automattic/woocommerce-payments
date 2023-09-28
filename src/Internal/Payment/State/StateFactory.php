56933778b2c3bf6cfb97fad2e4e5204e4a74fd07
Radoslav Georgiev <rageorgiev@gmail.com>
2023-09-28 23:58:53 +0300

    Restructuring exceptions in the StateFactory

--- src/Internal/Payment/State/StateFactory.php
+++ src/Internal/Payment/State/StateFactory.php
@@ -41,39 +41,28 @@ class StateFactory {
 	 * @param string         $state_class Name of the state class.
 	 * @param PaymentContext $context     Context for the new state.
 	 * @return PaymentState               The generated payment state instance.
-	 * @throws StateTransitionException   In case the class was not found or invalid.
+	 * @throws StateTransitionException   When the class name is not a state.
+	 * @throws ContainerException         In case the class was not found or could not be constructed.
 	 */
 	public function create_state( string $state_class, PaymentContext $context ): PaymentState {
-		/**
-		 * If resolved, this will always be a payment state.
-		 *
-		 * @var PaymentState|null
-		 */
-		$state = null;
-
-		if ( is_subclass_of( $state_class, PaymentState::class ) ) {
-			try {
-				// Load the state from the container.
-				$state = $this->container->get( $state_class );
-			} catch ( ContainerException $e ) {
-				// A proper exception would be thrown if the state is null.
-				$state = null;
-			}
-		}
-
-		if ( is_null( $state ) ) {
+		if ( ! is_subclass_of( $state_class, PaymentState::class ) ) {
 			throw new StateTransitionException(
 				sprintf(
 					// Translators: %1$s is the PHP class for a new payment state, %1$s is the state base class.
-					__( 'The class %1$s either does not exist, or is not a subclass of %2$s', 'woocommerce-payments' ),
+					__( 'The class %1$s is not a subclass of %2$s', 'woocommerce-payments' ),
 					$state_class,
 					PaymentState::class
 				)
 			);
 		}
 
-		$state->set_context( $context );
+		try {
+			$state = $this->container->get( $state_class );
+		} catch ( ContainerException $e ) {
+			throw new StateTransitionException( $e->getMessage(), $e->getCode(), $e );
+		}
 
+		$state->set_context( $context );
 		return $state;
 	}
 }
