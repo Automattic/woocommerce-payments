d3cc93f0f133319fd8a0d479ddde958703b2963e
Kārlis Janisels <karlisjanisels@Karliss-MacBook-Pro.local>
2023-10-03 11:22:01 +0300

    Add payment method details in transaction detail page for Klarna

---
+++ client/payment-details/payment-method/klarna/index.js
@@ -0,0 +1,146 @@
+/** @format **/
+
+/**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+/**
+ * Internal dependencies.
+ */
+import PaymentDetailsPaymentMethodDetail from '../detail';
+
+/**
+ * Extracts and formats payment method details from a charge.
+ *
+ * @param {Object} charge The charge object.
+ * @return {Object}       A flat hash of all necessary values.
+ */
+const formatPaymentMethodDetails = ( charge ) => {
+	const { billing_details: billingDetails, payment_method: id } = charge;
+
+	const {
+		country: countryCode,
+		verified_name: verifiedName,
+		payment_method_category: paymentMethodCategory,
+		preferred_locale: preferredLocale,
+	} = charge.payment_method_details.klarna;
+
+	const { name, email, formatted_address: formattedAddress } = billingDetails;
+	// Use the full country name.
+	const country = wcSettings.countries[ countryCode ];
+
+	return {
+		verifiedName,
+		id,
+		name,
+		email,
+		country,
+		formattedAddress,
+		paymentMethodCategory,
+		preferredLocale,
+	};
+};
+
+/**
+ * Placeholders to display while loading.
+ */
+const paymentMethodPlaceholders = {
+	verifiedName: 'verified name placeholder',
+	fingerprint: 'fingerprint placeholder',
+	id: 'id placeholder',
+	name: 'name placeholder',
+	email: 'email placeholder',
+	formattedAddress: 'address placeholder',
+	country: 'country placeholder',
+};
+
+const KlarnaDetails = ( { charge = {}, isLoading } ) => {
+	const details = charge.payment_method_details
+		? formatPaymentMethodDetails( charge )
+		: paymentMethodPlaceholders;
+
+	const {
+		verifiedName,
+		id,
+		name,
+		email,
+		country,
+		formattedAddress,
+		paymentMethodCategory,
+		preferredLocale,
+	} = details;
+
+	// Shorthand for more readable code.
+	const Detail = PaymentDetailsPaymentMethodDetail;
+
+	return (
+		<div className="payment-method-details">
+			<div className="payment-method-details__column">
+				<Detail
+					isLoading={ isLoading }
+					label={ __( 'ID', 'woocommerce-payments' ) }
+				>
+					{ id }
+				</Detail>
+
+				<Detail
+					isLoading={ isLoading }
+					label={ __( 'Category', 'woocommerce-payments' ) }
+				>
+					{ paymentMethodCategory }
+				</Detail>
+
+				<Detail
+					isLoading={ isLoading }
+					label={ __( 'Preferred Locale', 'woocommerce-payments' ) }
+				>
+					{ preferredLocale }
+				</Detail>
+			</div>
+
+			<div className="payment-method-details__column">
+				<Detail
+					isLoading={ isLoading }
+					label={ __( 'Verified name', 'woocommerce-payments' ) }
+				>
+					{ verifiedName }
+				</Detail>
+
+				<Detail
+					isLoading={ isLoading }
+					label={ __( 'Owner', 'woocommerce-payments' ) }
+				>
+					{ name }
+				</Detail>
+
+				<Detail
+					isLoading={ isLoading }
+					label={ __( 'Owner email', 'woocommerce-payments' ) }
+				>
+					{ email }
+				</Detail>
+
+				<Detail
+					isLoading={ isLoading }
+					label={ __( 'Address', 'woocommerce-payments' ) }
+				>
+					<span
+						dangerouslySetInnerHTML={ {
+							__html: formattedAddress,
+						} }
+					/>
+				</Detail>
+
+				<Detail
+					isLoading={ isLoading }
+					label={ __( 'Origin', 'woocommerce-payments' ) }
+				>
+					{ country }
+				</Detail>
+			</div>
+		</div>
+	);
+};
+
+export default KlarnaDetails;
