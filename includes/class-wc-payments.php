20669715a0089ed032fb9b6a3ed4dd1f55f64762
Zvonimir Maglica <zvonimirmaglica4@gmail.com>
2023-10-04 17:01:51 +0200

    API endpoint to fetch customer's saved payment methods. (#7344)

--- includes/class-wc-payments.php
+++ includes/class-wc-payments.php
@@ -1032,6 +1032,10 @@ class WC_Payments {
 		$onboarding_controller = new WC_REST_Payments_Onboarding_Controller( self::$api_client, self::$onboarding_service );
 		$onboarding_controller->register_routes();
 
+		include_once WCPAY_ABSPATH . 'includes/admin/class-wc-rest-payments-customer-controller.php';
+		$customer_controller = new WC_REST_Payments_Customer_Controller( self::$api_client, self::$customer_service );
+		$customer_controller->register_routes();
+
 		if ( WC_Payments_Features::is_upe_settings_preview_enabled() ) {
 			include_once WCPAY_ABSPATH . 'includes/admin/class-wc-rest-upe-flag-toggle-controller.php';
 			$upe_flag_toggle_controller = new WC_REST_UPE_Flag_Toggle_Controller( self::get_gateway() );
