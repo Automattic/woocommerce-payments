cd4dfff2c6a7181646911b90a9f1d7f3f8a0f995
César Costa <costa.cesar.a@hotmail.com>
2023-10-04 18:05:23 -0300

    Prevent WooPay button from re-rendering when navigating away
    The reason this occurred, was because an `addToCart()` HTTP request was made, which triggered the `updated_cart_totals` event to fire. This event then caused a new WooPay button to be rendered right before the browser redirected to the WooPay checkout page.

--- client/checkout/woopay/express-button/woopay-express-checkout-payment-method.js
+++ client/checkout/woopay/express-button/woopay-express-checkout-payment-method.js
@@ -1,3 +1,5 @@
+/* global jQuery */
+
 /**
  * Internal dependencies
  */
@@ -6,27 +8,15 @@ import { WoopayExpressCheckoutButton } from './woopay-express-checkout-button';
 import { getConfig } from '../../../utils/checkout';
 import WCPayAPI from '../../api';
 import request from '../../utils/request';
+import ReactDOM from 'react-dom';
 
-// Create an API object, which will be used throughout the checkout.
-const api = new WCPayAPI(
-	{
-		publishableKey: getConfig( 'publishableKey' ),
-		accountId: getConfig( 'accountId' ),
-		forceNetworkSavedCards: getConfig( 'forceNetworkSavedCards' ),
-		locale: getConfig( 'locale' ),
-	},
-	request
-);
+const Container = () => {
+	return <span id="woopay-express-checkout-container" />;
+};
 
 const wooPayExpressCheckoutPaymentMethod = () => ( {
 	name: PAYMENT_METHOD_NAME_WOOPAY_EXPRESS_CHECKOUT,
-	content: (
-		<WoopayExpressCheckoutButton
-			buttonSettings={ getConfig( 'woopayButton' ) }
-			api={ api }
-			emailSelector="#email"
-		/>
-	),
+	content: <Container />,
 	edit: (
 		<WoopayExpressCheckoutButton
 			buttonSettings={ getConfig( 'woopayButton' ) }
@@ -40,5 +30,76 @@ const wooPayExpressCheckoutPaymentMethod = () => ( {
 		features: getConfig( 'features' ),
 	},
 } );
+
+const oldWoopayContainers = [];
+
+const renderWooPayExpressCheckoutButton = ( listenForCartChanges = {} ) => {
+	// Create an API object, which will be used throughout the checkout.
+	const api = new WCPayAPI(
+		{
+			publishableKey: getConfig( 'publishableKey' ),
+			accountId: getConfig( 'accountId' ),
+			forceNetworkSavedCards: getConfig( 'forceNetworkSavedCards' ),
+			locale: getConfig( 'locale' ),
+		},
+		request
+	);
+
+	const woopayContainer = document.getElementById(
+		'woopay-express-checkout-container'
+	);
+
+	if ( woopayContainer ) {
+		while ( oldWoopayContainers.length > 0 ) {
+			// Ensure previous buttons are unmounted and cleaned up.
+			const oldWoopayContainer = oldWoopayContainers.pop();
+			ReactDOM.unmountComponentAtNode( oldWoopayContainer );
+		}
+
+		oldWoopayContainers.push( woopayContainer );
+
+		ReactDOM.render(
+			<WoopayExpressCheckoutButton
+				listenForCartChanges={ listenForCartChanges }
+				buttonSettings={ getConfig( 'woopayButton' ) }
+				api={ api }
+				isProductPage={
+					!! woopayContainer.getAttribute( 'data-product_page' )
+				}
+				emailSelector="#billing_email"
+			/>,
+			woopayContainer
+		);
+	}
+};
+
+let listenForCartChanges = null;
+const renderWooPayExpressCheckoutButtonWithCallbacks = () => {
+	renderWooPayExpressCheckoutButton( listenForCartChanges );
+};
+
+jQuery( ( $ ) => {
+	listenForCartChanges = {
+		start: () => {
+			$( document.body ).on(
+				'updated_cart_totals',
+				renderWooPayExpressCheckoutButtonWithCallbacks
+			);
+		},
+		stop: () => {
+			$( document.body ).off(
+				'updated_cart_totals',
+				renderWooPayExpressCheckoutButtonWithCallbacks
+			);
+		},
+	};
+
+	listenForCartChanges.start();
+} );
+
+window.addEventListener(
+	'load',
+	renderWooPayExpressCheckoutButtonWithCallbacks
+);
 
 export default wooPayExpressCheckoutPaymentMethod;
