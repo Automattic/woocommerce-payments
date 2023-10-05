0bcc08b38bb98d046ea669a726ecba4ddaa2dcb7
César Costa <costa.cesar.a@hotmail.com>
2023-10-05 12:14:06 -0300

    Change approach to use the new ReactDOM API and remove jQuery

--- client/checkout/woopay/express-button/woopay-express-checkout-payment-method.js
+++ client/checkout/woopay/express-button/woopay-express-checkout-payment-method.js
@@ -1,22 +1,50 @@
-/* global jQuery */
+/**
+ * External dependencies
+ */
+import { useCallback } from 'react';
+import ReactDOM from 'react-dom';
 
 /**
  * Internal dependencies
  */
 import { PAYMENT_METHOD_NAME_WOOPAY_EXPRESS_CHECKOUT } from '../../constants';
 import { WoopayExpressCheckoutButton } from './woopay-express-checkout-button';
-import { getConfig } from '../../../utils/checkout';
+import { getConfig } from 'wcpay/utils/checkout';
 import WCPayAPI from '../../api';
 import request from '../../utils/request';
-import ReactDOM from 'react-dom';
 
-const Container = () => {
-	return <span id="woopay-express-checkout-container" />;
+// Create an API object, which will be used throughout the checkout.
+const api = new WCPayAPI(
+	{
+		publishableKey: getConfig( 'publishableKey' ),
+		accountId: getConfig( 'accountId' ),
+		forceNetworkSavedCards: getConfig( 'forceNetworkSavedCards' ),
+		locale: getConfig( 'locale' ),
+	},
+	request
+);
+
+const WooPayExpressCheckoutButtonContainer = () => {
+	const onRefChange = useCallback( ( node ) => {
+		if ( node ) {
+			const root = ReactDOM.createRoot( node );
+
+			root.render(
+				<WoopayExpressCheckoutButton
+					buttonSettings={ getConfig( 'woopayButton' ) }
+					api={ api }
+					emailSelector="#email"
+				/>
+			);
+		}
+	}, [] );
+
+	return <span ref={ onRefChange } />;
 };
 
-const wooPayExpressCheckoutPaymentMethod = () => ( {
+const WooPayExpressCheckoutPaymentMethod = () => ( {
 	name: PAYMENT_METHOD_NAME_WOOPAY_EXPRESS_CHECKOUT,
-	content: <Container />,
+	content: <WooPayExpressCheckoutButtonContainer />,
 	edit: (
 		<WoopayExpressCheckoutButton
 			buttonSettings={ getConfig( 'woopayButton' ) }
@@ -31,72 +59,4 @@ const wooPayExpressCheckoutPaymentMethod = () => ( {
 	},
 } );
 
-const oldWoopayContainers = [];
-
-const renderWooPayExpressCheckoutButton = ( listenForCartChanges = {} ) => {
-	// Create an API object, which will be used throughout the checkout.
-	const api = new WCPayAPI(
-		{
-			publishableKey: getConfig( 'publishableKey' ),
-			accountId: getConfig( 'accountId' ),
-			forceNetworkSavedCards: getConfig( 'forceNetworkSavedCards' ),
-			locale: getConfig( 'locale' ),
-		},
-		request
-	);
-
-	const woopayContainer = document.getElementById(
-		'woopay-express-checkout-container'
-	);
-
-	if ( woopayContainer ) {
-		while ( oldWoopayContainers.length > 0 ) {
-			// Ensure previous buttons are unmounted and cleaned up.
-			const oldWoopayContainer = oldWoopayContainers.pop();
-			ReactDOM.unmountComponentAtNode( oldWoopayContainer );
-		}
-
-		oldWoopayContainers.push( woopayContainer );
-
-		ReactDOM.render(
-			<WoopayExpressCheckoutButton
-				listenForCartChanges={ listenForCartChanges }
-				buttonSettings={ getConfig( 'woopayButton' ) }
-				api={ api }
-				emailSelector="#email"
-			/>,
-			woopayContainer
-		);
-	}
-};
-
-let listenForCartChanges = null;
-const renderWooPayExpressCheckoutButtonWithCallbacks = () => {
-	renderWooPayExpressCheckoutButton( listenForCartChanges );
-};
-
-jQuery( ( $ ) => {
-	listenForCartChanges = {
-		start: () => {
-			$( document.body ).on(
-				'updated_cart_totals',
-				renderWooPayExpressCheckoutButtonWithCallbacks
-			);
-		},
-		stop: () => {
-			$( document.body ).off(
-				'updated_cart_totals',
-				renderWooPayExpressCheckoutButtonWithCallbacks
-			);
-		},
-	};
-
-	listenForCartChanges.start();
-} );
-
-window.addEventListener(
-	'load',
-	renderWooPayExpressCheckoutButtonWithCallbacks
-);
-
-export default wooPayExpressCheckoutPaymentMethod;
+export default WooPayExpressCheckoutPaymentMethod;
