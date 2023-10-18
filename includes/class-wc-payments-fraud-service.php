a74b8c45a023da1c41edd06d13b86d5245c17b3c
Vlad Olaru <vlad@pixelgrade.com>
2023-10-18 14:43:43 +0300

    refact: Fetch and store fraud services config

--- includes/class-wc-payments-fraud-service.php
+++ includes/class-wc-payments-fraud-service.php
@@ -7,6 +7,8 @@
 
 defined( 'ABSPATH' ) || exit;
 
+use WCPay\Core\Server\Request\Get_Fraud_Services;
+use WCPay\Database_Cache;
 use WCPay\Exceptions\API_Exception;
 use WCPay\Logger;
 
@@ -43,23 +45,33 @@ class WC_Payments_Fraud_Service {
 	private $session_service;
 
 	/**
+	 * Cache util for managing the database cached data.
+	 *
+	 * @var Database_Cache
+	 */
+	private $database_cache;
+
+	/**
 	 * Constructor for WC_Payments_Fraud_Service.
 	 *
 	 * @param WC_Payments_API_Client       $payments_api_client      - WooCommerce Payments API client.
 	 * @param WC_Payments_Customer_Service $customer_service         - Customer class instance.
 	 * @param WC_Payments_Account          $account                  - Account class instance.
 	 * @param WC_Payments_Session_Service  $session_service          - Session Service class instance.
+	 * @param Database_Cache               $database_cache           - Database cache instance.
 	 */
 	public function __construct(
 		WC_Payments_API_Client $payments_api_client,
 		WC_Payments_Customer_Service $customer_service,
 		WC_Payments_Account $account,
-		WC_Payments_Session_Service $session_service
+		WC_Payments_Session_Service $session_service,
+		Database_Cache $database_cache
 	) {
 		$this->payments_api_client = $payments_api_client;
 		$this->customer_service    = $customer_service;
 		$this->account             = $account;
 		$this->session_service     = $session_service;
+		$this->database_cache      = $database_cache;
 	}
 
 	/**
@@ -68,9 +80,115 @@ class WC_Payments_Fraud_Service {
 	 * @return void
 	 */
 	public function init_hooks() {
-		add_filter( 'wcpay_prepare_fraud_config', [ $this, 'prepare_fraud_config' ], 10, 2 );
 		add_action( 'init', [ $this, 'link_session_if_user_just_logged_in' ] );
 		add_action( 'admin_print_footer_scripts', [ $this, 'add_sift_js_tracker' ] );
+	}
+
+	/**
+	 * Gets the various anti-fraud services that must be included on every WCPay-related page.
+	 *
+	 * @return array Assoc array. Each key is the slug of a fraud service that must be incorporated to every page.
+	 *               The value is a service-specific config for it.
+	 */
+	public function get_fraud_services_config(): array {
+		$raw_config = null;
+
+		// First, try to get the config from the account data (preserve backwards-compatibility).
+		$account = $this->account->get_cached_account_data();
+		if ( ! empty( $account ) && isset( $account['fraud_services'] ) ) {
+			$raw_config = $account['fraud_services'];
+		}
+
+		// If the fraud services config is not available in the account data, try to get it from the server.
+		// If we've been given an empty array, we respect that; so no empty checks.
+		if ( is_null( $raw_config ) ) {
+			$raw_config = $this->get_cached_fraud_services();
+			// Set to null if we've failed to fetch.
+			if ( false === $raw_config ) {
+				$raw_config = null;
+			}
+		}
+
+		if ( is_null( $raw_config ) ) {
+			// This was the default before adding new anti-fraud providers, preserve backwards-compatibility.
+			$raw_config = [ 'stripe' => [] ];
+		}
+
+		$services_config = [];
+		foreach ( $raw_config as $service_id => $config ) {
+			if ( ! is_array( $config ) ) {
+				$config = [];
+			}
+
+			// Apply our internal logic before allowing others to have a say through filters.
+			$config = $this->prepare_fraud_config( $config, $service_id );
+
+			$services_config[ $service_id ] = apply_filters( 'wcpay_prepare_fraud_config', $config, $service_id );
+		}
+
+		return $services_config;
+	}
+
+	/**
+	 * Gets and caches the public fraud services config.
+	 *
+	 * @param bool $force_refresh Forces data to be fetched from the server, rather than using the cache.
+	 *
+	 * @return array|bool Fraud services config or false if failed to retrieve fraud services config.
+	 */
+	public function get_cached_fraud_services( bool $force_refresh = false ) {
+		$refreshed = false;
+
+		$fraud_services = $this->database_cache->get_or_add(
+			Database_Cache::FRAUD_SERVICES_KEY,
+			function () {
+				try {
+					$request        = Get_Fraud_Services::create();
+					$response       = $request->send();
+					$fraud_services = $response->to_array();
+
+				} catch ( API_Exception $e ) {
+					// Return false to signal retrieval error.
+					return false;
+				}
+
+				if ( ! $this->is_valid_cached_fraud_services( $fraud_services ) ) {
+					return false;
+				}
+
+				return $fraud_services;
+			},
+			[ $this, 'is_valid_cached_fraud_services' ],
+			$force_refresh,
+			$refreshed
+		);
+
+		if ( is_null( $fraud_services ) ) {
+			return false;
+		}
+
+		return $fraud_services;
+	}
+
+	/**
+	 * Checks if the cached fraud services config can be used in the current plugin state.
+	 *
+	 * @param bool|string|array $fraud_services_config The cached config.
+	 *
+	 * @return bool True if the cached fraud services config is valid.
+	 */
+	public function is_valid_cached_fraud_services( $fraud_services_config ): bool {
+		// null/false means no config has been cached.
+		if ( null === $fraud_services_config || false === $fraud_services_config ) {
+			return false;
+		}
+
+		// Non-array values are not expected.
+		if ( ! is_array( $fraud_services_config ) ) {
+			return false;
+		}
+
+		return true;
 	}
 
 	/**
@@ -81,11 +199,12 @@ class WC_Payments_Fraud_Service {
 	 *
 	 * @return array|NULL Array with all the required data to initialize the anti-fraud script, or NULL if the service shouldn't be used.
 	 */
-	public function prepare_fraud_config( $config, $service_id ) {
+	private function prepare_fraud_config( array $config, string $service_id ): ?array {
 		switch ( $service_id ) {
 			case 'sift':
 				return $this->prepare_sift_config( $config );
 		}
+
 		return $config;
 	}
 
@@ -94,42 +213,63 @@ class WC_Payments_Fraud_Service {
 	 *
 	 * @param array $config Associative array with the SIFT-related configuration returned from the server.
 	 *
-	 * @return array|NULL Assoc array, ready for the client to consume, or NULL if the client shouldn't enqueue this script.
+	 * @return array|null Assoc array, ready for the client to consume, or NULL if the client shouldn't enqueue this script.
 	 */
-	private function prepare_sift_config( $config ) {
+	private function prepare_sift_config( array $config ): ?array {
+		$test_mode = false;
+		try {
+			$test_mode = WC_Payments::mode()->is_test();
+		} catch ( Exception $e ) {
+			Logger::log( sprintf( 'WooPayments JS settings: Could not determine if WCPay should be in test mode! Message: %s', $e->getMessage() ), 'warning' );
+		}
+
 		// The server returns both production and sandbox beacon keys. Use the sandbox one if test mode is enabled.
-		if ( WC_Payments::mode()->is_test() ) {
+		if ( $test_mode && isset( $config['sandbox_beacon_key'] ) ) {
 			$config['beacon_key'] = $config['sandbox_beacon_key'];
 		}
 		unset( $config['sandbox_beacon_key'] );
 
-		$wpcom_blog_id = $this->payments_api_client->get_blog_id();
-		if ( ! $wpcom_blog_id ) {
-			// Don't enqueue the SIFT script if Jetpack hasn't been connected yet.
-			return null;
-		}
-
-		$config['user_id'] = '';
-
-		if ( is_user_logged_in() ) {
-			if ( is_admin() ) {
-				$config['user_id'] = $this->account->get_stripe_account_id();
-			} else {
-				$customer_id = $this->customer_service->get_customer_id_by_user_id( get_current_user_id() );
-				if ( isset( $customer_id ) ) {
-					$config['user_id'] = $customer_id;
-				}
-			}
-		}
-
+		$config['user_id']    = $this->get_sift_user_id();
 		$config['session_id'] = $this->session_service->get_sift_session_id();
 
 		return $config;
 	}
 
 	/**
-	 * Called after the WooCommerce session has been initialized. Check if the current user has just logged in,
+	 * Get the Sift user ID to use for the current request.
+	 *
+	 * @return string The Sift user ID to use for the current request. Empty string if we couldn't determine one.
+	 */
+	private function get_sift_user_id(): string {
+		$user_id = '';
+
+		if ( ! is_user_logged_in() ) {
+			return $user_id;
+		}
+
+		if ( is_admin() ) {
+			// In the WP admin we deal with merchant accounts, not customers.
+			$user_id = $this->account->get_stripe_account_id() ?? '';
+		} else {
+			$customer_id = $this->customer_service->get_customer_id_by_user_id( get_current_user_id() );
+			if ( isset( $customer_id ) ) {
+				$user_id = $customer_id;
+			}
+		}
+
+		return $user_id;
+	}
+
+	/**
+	 * Check if the current user has just logged in,
 	 * and sends that information to the server to link the current browser session with the user.
+	 *
+	 * Called after the WooCommerce session has been initialized.
+	 *
+	 * @return void
+	 *
+	 * @throws Exception In case the main gateway class has not been initialized yet.
+	 *                   This means that the method is called before the `init` hook.
 	 */
 	public function link_session_if_user_just_logged_in() {
 		$wpcom_blog_id = $this->payments_api_client->get_blog_id();
@@ -142,7 +282,7 @@ class WC_Payments_Fraud_Service {
 			return;
 		}
 
-		$fraud_config = $this->account->get_fraud_services_config();
+		$fraud_config = $this->get_fraud_services_config();
 		if ( ! isset( $fraud_config['sift'] ) ) {
 			// If Sift isn't enabled, we don't need to link the session.
 			return;
@@ -166,6 +306,9 @@ class WC_Payments_Fraud_Service {
 	 * Adds the Sift JS page tracker if needed. See the comments for the detailed logic.
 	 *
 	 * @return  void
+	 *
+	 * @throws Exception In case the main gateway class has not been initialized yet.
+	 *                    This means that the method is called before the `init` hook.
 	 */
 	public function add_sift_js_tracker() {
 		if ( ! isset( $_GET['wcpay-connection-success'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification
@@ -173,7 +316,7 @@ class WC_Payments_Fraud_Service {
 			return;
 		}
 
-		$fraud_config = $this->account->get_fraud_services_config();
+		$fraud_config = $this->get_fraud_services_config();
 		if ( ! isset( $fraud_config['sift'] ) ) {
 			// Abort if Sift is not enabled for this account.
 			return;
