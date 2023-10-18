6a5ede6e8a86ef9dbcac7978353d4619600fcec1
Jessy P <jessy.pappachan@automattic.com>
2023-10-18 09:00:30 +0530

    Renamed object under test for consistency

--- tests/unit/src/Internal/LoggerTest.php
+++ tests/unit/src/Internal/LoggerTest.php
@@ -25,7 +25,7 @@ class LoggerTest extends WCPAY_UnitTestCase {
 	 *
 	 * @var Logger|MockObject
 	 */
-	private $logger;
+	private $sut;
 
 	/**
 	 * Holds the underlying WC_Logger
@@ -57,7 +57,7 @@ class LoggerTest extends WCPAY_UnitTestCase {
 		$this->mock_wc_logger = $this->createMock( WC_Logger::class );
 		$this->mode           = $this->createMock( Mode::class );
 		$this->mock_gateway   = $this->createMock( WC_Payment_Gateway_WCPay::class );
-		$this->logger         = $this->getMockBuilder( Logger::class )
+		$this->sut         = $this->getMockBuilder( Logger::class )
 			->setConstructorArgs(
 				[
 					$this->mock_wc_logger,
@@ -65,7 +65,7 @@ class LoggerTest extends WCPAY_UnitTestCase {
 					$this->mock_gateway,
 				]
 			)
-			->setMethods( [ 'can_log' ] )
+			->onlyMethods( [ 'can_log' ] )
 			->getMock();
 	}
 
@@ -73,146 +73,146 @@ class LoggerTest extends WCPAY_UnitTestCase {
 	 * Test log method of the Logger class.
 	 */
 	public function test_log_success() {
-		$this->logger->expects( $this->once() )
+		$this->sut->expects( $this->once() )
 			->method( 'can_log' )
 			->willReturn( true );
 		$this->mock_wc_logger
 			->expects( $this->once() )
 			->method( 'log' )
 			->with( WC_Log_Levels::INFO, 'Test Log 1' );
-		$this->logger->log( 'Test Log 1' );
+		$this->sut->log( 'Test Log 1' );
 	}
 
 	/**
 	 * Test log method of the Logger class when can_log returns false.
 	 */
 	public function test_log_failure() {
-		$this->logger->expects( $this->once() )
+		$this->sut->expects( $this->once() )
 			->method( 'can_log' )
 			->willReturn( false );
 		$this->mock_wc_logger
 			->expects( $this->never() )
 			->method( 'log' );
-		$this->logger->log( 'Test Log 1' );
+		$this->sut->log( 'Test Log 1' );
 	}
 
 	/**
 	 * Test emergency method of the Logger class.
 	 */
 	public function test_log_level_emergency() {
-		$this->logger->expects( $this->once() )
+		$this->sut->expects( $this->once() )
 			->method( 'can_log' )
 			->willReturn( true );
 		$this->mock_wc_logger
 			->expects( $this->once() )
 			->method( 'log' )
 			->with( WC_Log_Levels::EMERGENCY, 'Test Log Emergency' );
-		$this->logger->emergency( 'Test Log Emergency' );
+		$this->sut->emergency( 'Test Log Emergency' );
 	}
 
 	/**
 	 * Test alert method of the Logger class.
 	 */
 	public function test_log_level_alert() {
-		$this->logger->expects( $this->once() )
+		$this->sut->expects( $this->once() )
 			->method( 'can_log' )
 			->willReturn( true );
 		$this->mock_wc_logger
 			->expects( $this->once() )
 			->method( 'log' )
 			->with( WC_Log_Levels::ALERT, 'Test Log Alert' );
-		$this->logger->alert( 'Test Log Alert' );
+		$this->sut->alert( 'Test Log Alert' );
 	}
 
 	/**
 	 * Test critical method of the Logger class.
 	 */
 	public function test_log_level_critical() {
-		$this->logger->expects( $this->once() )
+		$this->sut->expects( $this->once() )
 			->method( 'can_log' )
 			->willReturn( true );
 		$this->mock_wc_logger
 			->expects( $this->once() )
 			->method( 'log' )
 			->with( WC_Log_Levels::CRITICAL, 'Test Log Critical' );
-		$this->logger->critical( 'Test Log Critical' );
+		$this->sut->critical( 'Test Log Critical' );
 	}
 
 	/**
 	 * Test error method of the Logger class.
 	 */
 	public function test_log_level_error() {
-		$this->logger->expects( $this->once() )
+		$this->sut->expects( $this->once() )
 			->method( 'can_log' )
 			->willReturn( true );
 		$this->mock_wc_logger
 			->expects( $this->once() )
 			->method( 'log' )
 			->with( WC_Log_Levels::ERROR, 'Test Log Error' );
-		$this->logger->error( 'Test Log Error' );
+		$this->sut->error( 'Test Log Error' );
 	}
 
 	/**
 	 * Test warning method of the Logger class.
 	 */
 	public function test_log_level_warning() {
-		$this->logger->expects( $this->once() )
+		$this->sut->expects( $this->once() )
 			->method( 'can_log' )
 			->willReturn( true );
 		$this->mock_wc_logger
 			->expects( $this->once() )
 			->method( 'log' )
 			->with( WC_Log_Levels::WARNING, 'Test Log Warning' );
-		$this->logger->warning( 'Test Log Warning' );
+		$this->sut->warning( 'Test Log Warning' );
 	}
 
 	/**
 	 * Test notice method of the Logger class.
 	 */
 	public function test_log_level_notice() {
-		$this->logger->expects( $this->once() )
+		$this->sut->expects( $this->once() )
 			->method( 'can_log' )
 			->willReturn( true );
 		$this->mock_wc_logger
 			->expects( $this->once() )
 			->method( 'log' )
 			->with( WC_Log_Levels::NOTICE, 'Test Log Notice' );
-		$this->logger->notice( 'Test Log Notice' );
+		$this->sut->notice( 'Test Log Notice' );
 	}
 
 	/**
 	 * Test info method of the Logger class.
 	 */
 	public function test_log_level_info() {
-		$this->logger->expects( $this->once() )
+		$this->sut->expects( $this->once() )
 			->method( 'can_log' )
 			->willReturn( true );
 		$this->mock_wc_logger
 			->expects( $this->once() )
 			->method( 'log' )
 			->with( WC_Log_Levels::INFO, 'Test Log Warning' );
-		$this->logger->info( 'Test Log Warning' );
+		$this->sut->info( 'Test Log Warning' );
 	}
 
 	/**
 	 * Test debug method of the Logger class.
 	 */
 	public function test_log_level_debug() {
-		$this->logger->expects( $this->once() )
+		$this->sut->expects( $this->once() )
 			->method( 'can_log' )
 			->willReturn( true );
 		$this->mock_wc_logger
 			->expects( $this->once() )
 			->method( 'log' )
 			->with( WC_Log_Levels::DEBUG, 'Test Log Debug' );
-		$this->logger->debug( 'Test Log Debug' );
+		$this->sut->debug( 'Test Log Debug' );
 	}
 
 	/**
 	 * Test that the can_log method of the Logger class returns true when the mode is dev.
 	 */
 	public function test_can_log_dev_mode() {
-		$this->logger = new Logger(
+		$this->sut = new Logger(
 			$this->mock_wc_logger,
 			$this->mode,
 			$this->mock_gateway
@@ -220,14 +220,14 @@ class LoggerTest extends WCPAY_UnitTestCase {
 		$this->mode->expects( $this->once() )
 			->method( 'is_dev' )
 			->willReturn( true );
-		$this->assertTrue( $this->logger->can_log() );
+		$this->assertTrue( $this->sut->can_log() );
 	}
 
 	/**
 	 * Test that the can_log method of the Logger class returns false when the mode throws an exception.
 	 */
 	public function test_can_log_exception() {
-		$this->logger = new Logger(
+		$this->sut = new Logger(
 			$this->mock_wc_logger,
 			$this->mode,
 			$this->mock_gateway
@@ -235,14 +235,14 @@ class LoggerTest extends WCPAY_UnitTestCase {
 		$this->mode->expects( $this->once() )
 			->method( 'is_dev' )
 			->willThrowException( new Exception() );
-		$this->assertFalse( $this->logger->can_log() );
+		$this->assertFalse( $this->sut->can_log() );
 	}
 
 	/**
 	 * Test that the can_log method of the Logger class returns false when the gateway option is not set.
 	 */
 	public function test_can_log_no_option() {
-		$this->logger = new Logger(
+		$this->sut = new Logger(
 			$this->mock_wc_logger,
 			$this->mode,
 			$this->mock_gateway
@@ -254,14 +254,14 @@ class LoggerTest extends WCPAY_UnitTestCase {
 			->method( 'get_option' )
 			->with( 'enable_logging' )
 			->willReturn( null );
-		$this->assertFalse( $this->logger->can_log() );
+		$this->assertFalse( $this->sut->can_log() );
 	}
 
 	/**
 	 * Test that the can_log method of the Logger class returns false when the gateway option is set to 'no'.
 	 */
 	public function test_can_log_disabled() {
-		$this->logger = new Logger(
+		$this->sut = new Logger(
 			$this->mock_wc_logger,
 			$this->mode,
 			$this->mock_gateway
@@ -273,14 +273,14 @@ class LoggerTest extends WCPAY_UnitTestCase {
 			->method( 'get_option' )
 			->with( 'enable_logging' )
 			->willReturn( 'no' );
-		$this->assertFalse( $this->logger->can_log() );
+		$this->assertFalse( $this->sut->can_log() );
 	}
 
 	/**
 	 * Test that the can_log method of the Logger class returns true when the gateway option is set to 'yes'.
 	 */
 	public function test_can_log_enabled() {
-		$this->logger = new Logger(
+		$this->sut = new Logger(
 			$this->mock_wc_logger,
 			$this->mode,
 			$this->mock_gateway
@@ -292,7 +292,7 @@ class LoggerTest extends WCPAY_UnitTestCase {
 			->method( 'get_option' )
 			->with( 'enable_logging' )
 			->willReturn( 'yes' );
-		$this->assertTrue( $this->logger->can_log() );
+		$this->assertTrue( $this->sut->can_log() );
 	}
 
 }
