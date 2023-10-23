adb8aeaa42110eb728893ef8982c929123c1afae
Radoslav Georgiev <rageorgiev@gmail.com>
2023-10-23 12:41:08 +0300

    Workflow for src tests coverage

--- bin/phpunit.sh
+++ bin/phpunit.sh
@@ -6,6 +6,7 @@ fi
 
 CURRENT_PHP_MAJOR_VERSION=$(php -r 'echo PHP_MAJOR_VERSION;')
 CURRENT_PHP_MINOR_VERSION=$(php -r 'echo PHP_MINOR_VERSION;')
+CONFIG_FILE=${1-phpunit.xml.dist}
 
 # The PHPUnit version inside composer.json is not compatible with PHP versions bellow 7.3
 # Update this constant if you wish to bump supported PHP major version
@@ -13,13 +14,13 @@ SUPPORTED_PHP_MAJOR_VERSION_FOR_PHPUNIT_INSTALLED_VIA_COMPOSER_JSON=7
 SUPPORTED_PHP_MINOR_VERSION_FOR_PHPUNIT_INSTALLED_VIA_COMPOSER_JSON=3
 
 if [ $CURRENT_PHP_MAJOR_VERSION -gt $SUPPORTED_PHP_MAJOR_VERSION_FOR_PHPUNIT_INSTALLED_VIA_COMPOSER_JSON ]; then
-	./vendor/bin/phpunit -c phpunit.xml.dist "$@";
+	./vendor/bin/phpunit -c $CONFIG_FILE "$@";
 else
 	if [ $CURRENT_PHP_MINOR_VERSION -ge $SUPPORTED_PHP_MINOR_VERSION_FOR_PHPUNIT_INSTALLED_VIA_COMPOSER_JSON ]; then
-		./vendor/bin/phpunit -c phpunit.xml.dist "$@";
+		./vendor/bin/phpunit -c $CONFIG_FILE "$@";
 	else
     	chmod +x ./bin/phpunit6
-    	./bin/phpunit6 -c phpunit.xml.dist "$@";
+    	./bin/phpunit6 -c $CONFIG_FILE "$@";
     fi
 fi
 
---
+++ bin/run-ci-tests-check-src-coverage.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# set strict mode for bash
+set -euo pipefail
+IFS=$'\n\t'
+
+# set environment variables
+WCPAY_DIR="$GITHUB_WORKSPACE"
+
+composer self-update && composer install --no-progress
+sudo systemctl start mysql.service
+bash bin/install-wp-tests.sh woocommerce_test root root localhost $WP_VERSION $WC_VERSION false
+echo 'Running the tests...'
+bash bin/phpunit.sh phpunit-src.xml.dist --coverage-clover /tmp/clover.xml
+vendor/bin/coverage-check /tmp/clover.xml 100
+
