f9a24e724402496466945f3a2a65dcf4006791bd
Eric Jinks <3147296+Jinksi@users.noreply.github.com>
2023-11-06 15:31:57 +1000

    Remove `estimated` status from getNextDeposit util

--- client/components/deposits-overview/utils.ts
+++ client/components/deposits-overview/utils.ts
@@ -29,7 +29,7 @@ export const getNextDeposit = (
 		return {
 			id: undefined,
 			date: 0,
-			status: 'estimated',
+			status: 'pending',
 			amount: formatCurrency( 0, overview?.currency ),
 		};
 	}
@@ -39,7 +39,7 @@ export const getNextDeposit = (
 	return {
 		id: nextScheduled.id,
 		date: nextScheduled.date ?? 0,
-		status: nextScheduled.status ?? 'estimated',
+		status: nextScheduled.status ?? '-',
 		amount: formatCurrency( nextScheduled.amount ?? 0, currency ),
 	};
 };
