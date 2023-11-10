ba680515870d5fe5d53b34c6f2d2e80198d5e39e
Dat Hoang <htdat@users.noreply.github.com>
2023-11-10 15:05:02 +0700

    Update MinimumAmountServiceTest

--- tests/unit/src/Internal/Service/MinimumAmountServiceTest.php
+++ tests/unit/src/Internal/Service/MinimumAmountServiceTest.php
@@ -8,7 +8,7 @@
 namespace WCPay\Tests\Internal\Service;
 
 use PHPUnit\Framework\MockObject\MockObject;
-use WC_Payments_Utils;
+use WCPay\Exceptions\Amount_Too_Small_Exception;
 use WCPay\Internal\Proxy\LegacyProxy;
 use WCPay\Internal\Service\MinimumAmountService;
 use WCPAY_UnitTestCase;
@@ -39,15 +39,46 @@ class MinimumAmountServiceTest extends WCPAY_UnitTestCase {
 		$this->sut               = new MinimumAmountService( $this->mock_legacy_proxy );
 	}
 
-	public function test_set_cache() {
+	public function test_store_amount_from_exception() {
+		$exception = new Amount_Too_Small_Exception( 'Amount too small', 100, 'EUR', 400 );
+
+		$this->mock_legacy_proxy->expects( $this->once() )
+			->method( 'call_function' )
+			->with( 'set_transient', 'wcpay_minimum_amount_eur', 100, DAY_IN_SECONDS );
+
+		$this->sut->store_amount_from_exception( $exception );
+	}
+
+	public function test_verify_amount_returns_void() {
+		$this->mock_legacy_proxy->expects( $this->once() )
+			->method( 'call_function' )
+			->with( 'get_transient', 'wcpay_minimum_amount_eur' )
+			->willReturn( 100 );
+
+		$this->sut->verify_amount( 'EUR', 150 );
+	}
+
+	public function test_verify_amount_throw_exception() {
+		$this->mock_legacy_proxy->expects( $this->once() )
+			->method( 'call_function' )
+			->with( 'get_transient', 'wcpay_minimum_amount_eur' )
+			->willReturn( 100 );
+
+		$this->expectException( Amount_Too_Small_Exception::class );
+		$this->expectExceptionMessage( 'Order amount too small' );
+
+		$this->sut->verify_amount( 'EUR', 50 );
+	}
+
+	public function test_set_cached_amount() {
 		$this->mock_legacy_proxy->expects( $this->once() )
 			->method( 'call_function' )
 			->with( 'set_transient', 'wcpay_minimum_amount_usd', 100, DAY_IN_SECONDS );
 
-		$this->sut->set_cache( 'USD', 100 );
+		\PHPUnit_Utils::call_method( $this->sut, 'set_cached_amount', [ 'USD', 100 ] );
 	}
 
-	public function provider_get_cache() {
+	public function provider_get_cached_amount() {
 		return [
 			'Transient not set'       => [ false, 0 ],
 			'Transient invalid value' => [ null, 0 ],
@@ -56,33 +87,14 @@ class MinimumAmountServiceTest extends WCPAY_UnitTestCase {
 	}
 
 	/**
-	 * @dataProvider provider_get_cache
+	 * @dataProvider provider_get_cached_amount
 	 */
-	public function test_get_cache( $transient_value, $expected ) {
+	public function test_get_cached_amount( $transient_value, $expected ) {
 		$this->mock_legacy_proxy->expects( $this->once() )
 			->method( 'call_function' )
 			->with( 'get_transient', 'wcpay_minimum_amount_eur' )
 			->willReturn( $transient_value );
 
-		$this->assertSame( $expected, $this->sut->get_cache( 'EUR' ) );
-	}
-
-	public function test_get_error_message_for_shoppers() {
-
-		$this->mock_legacy_proxy->expects( $this->exactly( 2 ) )
-			->method( 'call_static' )
-			->withConsecutive(
-				[ WC_Payments_Utils::class, 'interpret_stripe_amount', 50, 'EUR' ],
-				[ WC_Payments_Utils::class, 'format_currency', 0.5, 'EUR' ],
-			)
-			->willReturnOnConsecutiveCalls(
-				0.5,
-				'0.5 €'
-			);
-
-		$this->assertSame(
-			'The selected payment method requires a total amount of at least 0.5 €.',
-			$this->sut->get_error_message_for_shoppers( 'EUR', 50 )
-		);
+		\PHPUnit_Utils::call_method( $this->sut, 'get_cached_amount', [ 'EUR' ] );
 	}
 }
