b48da511417be31f27559d7a63cf7bee5d6e920a
Taha Paksu <3295+tpaksu@users.noreply.github.com>
2023-11-13 16:40:56 +0300

    Add onPaymentSetup for saved cards

--- client/checkout/blocks/saved-token-handler.js
+++ client/checkout/blocks/saved-token-handler.js
@@ -1,15 +1,40 @@
 /**
  * Internal dependencies
  */
+import { useEffect } from 'react';
 import { usePaymentCompleteHandler } from './hooks';
+import { select } from '@wordpress/data';
+
+const { getPaymentMethodData } = select( 'wc/store/payment' );
 
 export const SavedTokenHandler = ( {
 	api,
 	stripe,
 	elements,
-	eventRegistration: { onCheckoutSuccess },
+	eventRegistration: { onPaymentSetup, onCheckoutSuccess },
 	emitResponse,
 } ) => {
+	useEffect( () => {
+		onPaymentSetup( async () => {
+			const paymentMethodData = getPaymentMethodData();
+
+			const fraudPreventionToken = document
+				.querySelector( '#wcpay-fraud-prevention-token' )
+				?.getAttribute( 'value' );
+
+			return {
+				type: 'success',
+				meta: {
+					paymentMethodData: {
+						...paymentMethodData,
+						'wcpay-fraud-prevention-token':
+							fraudPreventionToken ?? '',
+					},
+				},
+			};
+		} );
+	}, [ onPaymentSetup ] );
+
 	// Once the server has completed payment processing, confirm the intent of necessary.
 	usePaymentCompleteHandler(
 		api,
