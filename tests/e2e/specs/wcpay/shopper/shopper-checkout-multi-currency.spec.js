3c4e05f1dd189a5cbb67b89ac1099b697c211ef3
Rafael Zaleski <rafaelzaleski@users.noreply.github.com>
2023-11-22 12:01:44 -0300

    Add skeleton for test cases

---
+++ tests/e2e/specs/wcpay/shopper/shopper-checkout-multi-currency.spec.js
@@ -0,0 +1,19 @@
+/**
+ * External dependencies
+ */
+
+/**
+ * Internal dependencies
+ */
+
+describe( 'Shopper Multi-Currency', () => {
+	beforeAll( async () => {} );
+
+	it( 'should be able to checkout with different currency than the store currency', async () => {} );
+
+	it( 'should show correct currency in order received page', async () => {} );
+
+	it( 'should show the correct currency in the order page', async () => {} );
+
+	it( 'should show the correct currency in a previous order with a different currency', async () => {} );
+} );
