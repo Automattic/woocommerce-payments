25401218f2201d4de9988109fa4c41ee16831965
Rafael Zaleski <rafaelzaleski@users.noreply.github.com>
2023-11-23 18:15:18 -0300

    Add checkout test

--- tests/e2e/specs/wcpay/shopper/shopper-checkout-multi-currency.spec.js
+++ tests/e2e/specs/wcpay/shopper/shopper-checkout-multi-currency.spec.js
@@ -1,15 +1,47 @@
 /**
  * External dependencies
  */
+import config from 'config';
+
+const { shopper } = require( '@woocommerce/e2e-utils' );
 
 /**
  * Internal dependencies
  */
 
-describe( 'Shopper Multi-Currency', () => {
-	beforeAll( async () => {} );
+import { fillCardDetails, setupProductCheckout } from '../../../utils/payments';
+import { shopperWCP } from '../../../utils';
 
-	it( 'should be able to checkout with different currency than the store currency', async () => {} );
+describe( 'Shopper Multi-Currency checkout', () => {
+	beforeAll( async () => {
+		await shopper.login();
+	} );
+
+	afterAll( async () => {
+		// Clear the cart at the end so it's ready for another test
+		await shopperWCP.emptyCart();
+	} );
+
+	it( 'should be able to checkout with different currency than the store currency', async () => {
+		// Check that the order was placed successfully
+		// Check that the order has the correct currency
+		await setupProductCheckout(
+			config.get( 'addresses.customer.billing' ),
+			[ [ config.get( 'products.simple.name' ), 1 ] ],
+			'EUR'
+		);
+		const card = config.get( 'cards.basic' );
+		await fillCardDetails( page, card );
+		await shopper.placeOrder();
+		await expect( page ).toMatch( 'Order received' );
+		// should show correct currency in order received page
+		expect(
+			await page.$eval(
+				'.woocommerce-order-overview__total',
+				( el ) => el.textContent
+			)
+		).toMatch( /EUR/ );
+	} );
 
 	it( 'should show correct currency in order received page', async () => {} );
 
