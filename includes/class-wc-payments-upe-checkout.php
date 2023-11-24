2da2193f8a1266a3d865bd49b351624b570870fd
Timur Karimov <timurkarimov@timurs-macbook-pro.home>
2023-11-24 13:40:23 +0100

    remove branching to make back-end provide only dUPE related fields to the front-end

--- includes/class-wc-payments-upe-checkout.php
+++ includes/class-wc-payments-upe-checkout.php
@@ -158,16 +158,9 @@ class WC_Payments_UPE_Checkout extends WC_Payments_Checkout {
 		$payment_fields['upeAppearance']            = get_transient( UPE_Payment_Gateway::UPE_APPEARANCE_TRANSIENT );
 		$payment_fields['wcBlocksUPEAppearance']    = get_transient( UPE_Payment_Gateway::WC_BLOCKS_UPE_APPEARANCE_TRANSIENT );
 		$payment_fields['cartContainsSubscription'] = $this->gateway->is_subscription_item_in_cart();
-
-		if ( WC_Payments_Features::is_upe_deferred_intent_enabled() ) {
-			$payment_fields['currency']  = get_woocommerce_currency();
-			$cart_total                  = ( WC()->cart ? WC()->cart->get_total( '' ) : 0 );
-			$payment_fields['cartTotal'] = WC_Payments_Utils::prepare_amount( $cart_total, get_woocommerce_currency() );
-		} elseif ( WC_Payments_Features::is_upe_legacy_enabled() ) {
-			$payment_fields['checkoutTitle']        = $this->gateway->get_checkout_title();
-			$payment_fields['upePaymentIntentData'] = $this->gateway->get_payment_intent_data_from_session();
-			$payment_fields['upeSetupIntentData']   = $this->gateway->get_setup_intent_data_from_session();
-		}
+		$payment_fields['currency']                 = get_woocommerce_currency();
+		$cart_total                                 = ( WC()->cart ? WC()->cart->get_total( '' ) : 0 );
+		$payment_fields['cartTotal']                = WC_Payments_Utils::prepare_amount( $cart_total, get_woocommerce_currency() );
 
 		$enabled_billing_fields = [];
 		foreach ( WC()->checkout()->get_checkout_fields( 'billing' ) as $billing_field => $billing_field_options ) {
