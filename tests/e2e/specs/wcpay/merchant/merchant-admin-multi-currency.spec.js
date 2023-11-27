848e41549fbd95a50b092697fffc9d6c1e8cf28a
César Costa <costa.cesar.a@hotmail.com>
2023-11-27 19:48:02 -0300

    Add test to assert the currency switcher can be added to the sidebar

--- tests/e2e/specs/wcpay/merchant/merchant-admin-multi-currency.spec.js
+++ tests/e2e/specs/wcpay/merchant/merchant-admin-multi-currency.spec.js
@@ -6,7 +6,9 @@ const { merchant } = require( '@woocommerce/e2e-utils' );
 /**
  * Internal dependencies
  */
+import config from 'config';
 import { merchantWCP, takeScreenshot } from '../../../utils';
+import { uiLoaded } from '../../../utils/helpers';
 
 describe( 'Admin Multi-Currency', () => {
 	beforeAll( async () => {
@@ -19,5 +21,21 @@ describe( 'Admin Multi-Currency', () => {
 			text: 'Enabled currencies',
 		} );
 		await takeScreenshot( 'merchant-admin-multi-currency' );
+	} );
+
+	it( 'should be possible to add the currency switcher to the sidebar', async () => {
+		await page.goto( `${ config.get( 'url' ) }wp-admin/widgets.php`, {
+			waitUntil: 'networkidle0',
+		} );
+		await uiLoaded();
+		await page.click( 'button[aria-label="Add block"]' );
+
+		const searchInput = await page.waitForSelector(
+			'input.components-search-control__input'
+		);
+		searchInput.type( 'switcher', { delay: 20 } );
+		await expect( page ).toMatchElement( 'button[role="option"]', {
+			text: 'Currency Switcher Widget',
+		} );
 	} );
 } );
