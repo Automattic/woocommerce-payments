4b8af1bf3dd25cf0c50d53bde5ff3ce672cdaead
César Costa <costa.cesar.a@hotmail.com>
2023-12-01 12:24:11 -0300

    Add test to assert currency switcher doesn't affect account order prices

--- tests/e2e/specs/wcpay/shopper/shopper-multi-currency-widget.spec.js
+++ tests/e2e/specs/wcpay/shopper/shopper-multi-currency-widget.spec.js
@@ -7,10 +7,17 @@ const { merchant, shopper } = require( '@woocommerce/e2e-utils' );
  * Internal dependencies
  */
 import config from 'config';
-import { merchantWCP } from '../../../utils';
+import { merchantWCP, shopperWCP } from '../../../utils';
+import { fillCardDetails, setupProductCheckout } from '../../../utils/payments';
 
 const pagesTable = [
-	[ 'home page', null, 'shop' ],
+	[
+		'home page',
+		async () => {
+			await shopper.goToShop();
+		},
+		'shop',
+	],
 	[
 		'product page',
 		async () => {
@@ -21,9 +28,7 @@ const pagesTable = [
 	[
 		'cart',
 		async () => {
-			await shopper.addToCartFromShopPage(
-				config.get( 'products.simple.name' )
-			);
+			await shopperWCP.addToCartBySlug( 'beanie' );
 			await shopper.goToCart();
 		},
 		'cart',
@@ -31,27 +36,26 @@ const pagesTable = [
 	[
 		'checkout',
 		async () => {
-			await shopper.addToCartFromShopPage(
-				config.get( 'products.simple.name' )
+			await setupProductCheckout(
+				config.get( 'addresses.customer.billing' )
 			);
-			await shopper.goToCheckout();
 		},
 		'checkout',
 	],
 ];
 
 describe( 'Shopper Multi-Currency widget', () => {
-	it( 'should display currency switcher if multi-currency is enabled', async () => {
+	it( 'should display currency switcher widget if multi-currency is enabled', async () => {
 		await merchant.login();
 		await merchantWCP.addMulticurrencyWidget();
 		await shopper.goToShop();
 		await page.waitForSelector( '.widget select[name=currency]', {
 			visible: true,
+			timeout: 5000,
 		} );
 	} );
 
 	it( 'should not display currency switcher widget if multi-currency is disabled', async () => {
-		await merchant.login();
 		await merchantWCP.openWCPSettings();
 		await merchantWCP.deactivateMulticurrency();
 		await shopper.goToShop();
@@ -63,16 +67,14 @@ describe( 'Shopper Multi-Currency widget', () => {
 
 		// Activate it again for the other tests.
 		await merchantWCP.activateMulticurrency();
+		await merchant.logout();
 	} );
 
 	describe.each( pagesTable )(
 		'Should allow shopper to switch currency',
 		( pageName, setupTest, url ) => {
 			it( `at the ${ pageName }`, async () => {
-				await shopper.goToShop();
-				if ( typeof setupTest === 'function' ) {
-					await setupTest();
-				}
+				await setupTest();
 				await page.waitForSelector( '.widget select[name=currency]', {
 					visible: true,
 				} );
@@ -88,4 +90,35 @@ describe( 'Shopper Multi-Currency widget', () => {
 			} );
 		}
 	);
+
+	it( 'should not affect prices when currency switching on My account > Orders', async () => {
+		await shopper.login();
+		await page.select( '.widget select[name=currency]', 'USD' );
+		await setupProductCheckout(
+			config.get( 'addresses.customer.billing' )
+		);
+		await fillCardDetails( page, config.get( 'cards.basic' ) );
+		await shopper.placeOrder();
+		await expect( page ).toMatch( 'Order received' );
+
+		const orderId = await page.evaluate(
+			() => document.querySelector( 'li.order strong' ).innerText
+		);
+		const orderTotal = Number(
+			await page.evaluate( () =>
+				document
+					.querySelector( 'li.total strong' )
+					.innerText.replace( /[^\d.]/g, '' )
+			)
+		);
+
+		await shopperWCP.goToOrders();
+		await page.select( '.widget select[name=currency]', 'EUR' );
+		await page.waitForSelector(
+			'.widget select[name=currency] option[value=EUR][selected]'
+		);
+		await expect( page ).toMatch( `#${ orderId }` );
+		await expect( page ).toMatch( `${ orderTotal.toFixed( 2 ) } USD` );
+		await shopper.logout();
+	} );
 } );
