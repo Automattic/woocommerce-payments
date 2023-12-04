48c51ce7cac6021e85c5ead9a31759b7fb1c2834
Timur Karimov <timurkarimov@timurs-macbook-pro.home>
2023-12-04 14:05:34 +0100

    remove unused upe flag toggle controller

--- includes/class-wc-payments.php
+++ includes/class-wc-payments.php
@@ -1042,10 +1042,6 @@ class WC_Payments {
 		$customer_controller->register_routes();
 
 		if ( WC_Payments_Features::is_upe_settings_preview_enabled() ) {
-			include_once WCPAY_ABSPATH . 'includes/admin/class-wc-rest-upe-flag-toggle-controller.php';
-			$upe_flag_toggle_controller = new WC_REST_UPE_Flag_Toggle_Controller( self::get_gateway() );
-			$upe_flag_toggle_controller->register_routes();
-
 			include_once WCPAY_ABSPATH . 'includes/admin/class-wc-rest-payments-survey-controller.php';
 			$survey_controller = new WC_REST_Payments_Survey_Controller( self::get_wc_payments_http() );
 			$survey_controller->register_routes();
