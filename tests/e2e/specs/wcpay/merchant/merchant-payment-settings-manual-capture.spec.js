b473c54fab7315ada218f520fd9f761aaff872fd
Timur Karimov <timurkarimov@timurs-macbook-pro.home>
2023-12-08 11:30:21 +0100

    improve manual capture e2e tests

--- tests/e2e/specs/wcpay/merchant/merchant-payment-settings-manual-capture.spec.js
+++ tests/e2e/specs/wcpay/merchant/merchant-payment-settings-manual-capture.spec.js
@@ -12,15 +12,19 @@ const confirmationModalClass = '.wcpay-confirmation-modal';
 describe( 'As a merchant, I should be prompted a confirmation modal when I try to activate the manual capture', () => {
 	beforeAll( async () => {
 		await merchant.login();
+		await merchantWCP.openWCPSettings();
+		await merchantWCP.skipFraudProtectionTour();
 	} );
 
 	afterAll( async () => {
 		await merchant.logout();
 	} );
 
-	it( 'should show the confirmation dialog when enabling the manual capture while UPE is activated', async () => {
-		await merchantWCP.openWCPSettings();
-		await merchantWCP.skipFraudProtectionTour();
+	afterEach( async () => {
+		await merchantWCP.unsetCheckboxByTestId( checkboxCaptureLaterOption );
+	} );
+
+	it( 'should show the confirmation dialog whe n enabling the manual capture while UPE is activated', async () => {
 		await merchantWCP.setCheckboxByTestId( checkboxCaptureLaterOption );
 		const confirmationModal = await expect( page ).toMatchElement(
 			confirmationModalClass
@@ -31,8 +35,6 @@ describe( 'As a merchant, I should be prompted a confirmation modal when I try t
 	} );
 
 	it( 'should not show the confirmation dialog when disabling the manual capture while UPE is activated', async () => {
-		await merchantWCP.openWCPSettings();
-		await merchantWCP.skipFraudProtectionTour();
 		await merchantWCP.setCheckboxByTestId( checkboxCaptureLaterOption );
 		const confirmationModal = await expect( page ).toMatchElement(
 			confirmationModalClass
@@ -46,8 +48,6 @@ describe( 'As a merchant, I should be prompted a confirmation modal when I try t
 	} );
 
 	it( 'should show the UPE methods disabled when manual capture is enabled', async () => {
-		await merchantWCP.openWCPSettings();
-		await merchantWCP.skipFraudProtectionTour();
 		await merchantWCP.setCheckboxByTestId( checkboxCaptureLaterOption );
 		const confirmationModal = await expect( page ).toMatchElement(
 			confirmationModalClass
