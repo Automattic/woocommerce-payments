6aadf5d97cee9b0209ba8d074fc24bd3ccc20a52
Eric Jinks <3147296+Jinksi@users.noreply.github.com>
2023-12-12 14:05:57 +1000

    Show schedule notice on deposits list screen

--- client/deposits/index.tsx
+++ client/deposits/index.tsx
@@ -10,12 +10,37 @@ import React from 'react';
  */
 import Page from 'components/page';
 import { TestModeNotice } from 'components/test-mode-notice';
+import BannerNotice from 'components/banner-notice';
+import DepositSchedule from 'components/deposits-overview/deposit-schedule';
+import { useAllDepositsOverviews } from 'data';
 import DepositsList from './list';
+
+const NextDepositNotice: React.FC = () => {
+	const {
+		overviews: { account },
+	} = useAllDepositsOverviews();
+
+	const depositSettings = wcpaySettings.accountStatus?.deposits;
+	const isDepositsUnrestricted =
+		wcpaySettings.accountStatus.deposits?.restrictions ===
+		'deposits_unrestricted';
+
+	if ( ! depositSettings || ! isDepositsUnrestricted ) {
+		return null;
+	}
+
+	return (
+		<BannerNotice status="info" isDismissible={ false }>
+			<DepositSchedule depositsSchedule={ account.deposits_schedule } />
+		</BannerNotice>
+	);
+};
 
 const DepositsPage: React.FC = () => {
 	return (
 		<Page>
 			<TestModeNotice currentPage="deposits" />
+			<NextDepositNotice />
 			<DepositsList />
 		</Page>
 	);
