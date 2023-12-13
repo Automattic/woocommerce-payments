a81f3689e871dd915f2dae8c3ac19bc10c7b34b1
Eric Jinks <3147296+Jinksi@users.noreply.github.com>
2023-12-13 15:49:13 +1000

    Transactions List - indicate when a transaction is expected to be included in a future deposit (#7878)

--- client/transactions/list/index.tsx
+++ client/transactions/list/index.tsx
@@ -329,12 +329,6 @@ export const TransactionsList = (
 			txn.customer_email
 		);
 
-		const deposit = (
-			<Deposit
-				depositId={ txn.deposit_id ?? '' }
-				dateAvailable={ txn.available_on }
-			/>
-		);
 		const currency = txn.currency.toUpperCase();
 
 		const dataType = txn.metadata ? txn.metadata.charge_type : txn.type;
@@ -372,13 +366,20 @@ export const TransactionsList = (
 			};
 		};
 
-		const depositStatus = txn.deposit_status
-			? displayDepositStatus[ txn.deposit_status ]
-			: '';
-
 		const isFinancingType =
 			-1 !==
 			[ 'financing_payout', 'financing_paydown' ].indexOf( txn.type );
+
+		const deposit = ! isFinancingType && (
+			<Deposit
+				depositId={ txn.deposit_id }
+				dateAvailable={ txn.available_on }
+			/>
+		);
+
+		const depositStatus = txn.deposit_status
+			? displayDepositStatus[ txn.deposit_status ]
+			: '';
 
 		// Map transaction into table row.
 		const data = {
