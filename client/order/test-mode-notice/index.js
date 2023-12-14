2e246c6e188712e287cd8ea22774fa837ac78d21
Dat Hoang <htdat@users.noreply.github.com>
2023-12-14 13:39:09 +0700

    Update to use interpolateComponents

--- client/order/test-mode-notice/index.js
+++ client/order/test-mode-notice/index.js
@@ -2,7 +2,7 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { createInterpolateElement } from '@wordpress/element';
+import interpolateComponents from '@automattic/interpolate-components';
 
 /**
  * Internal dependencies
@@ -12,25 +12,26 @@ import InlineNotice from 'wcpay/components/inline-notice';
 const TestModeNotice = () => {
 	return (
 		<InlineNotice icon={ true } status="warning" isDismissible={ false }>
-			{ __(
-				'WooPayments was in test mode when this order was placed.',
-				'woocommerce-payments'
-			) }
-
-			{ ' ' +
-				createInterpolateElement(
-					__(
-						'<a>Learn more about test mode</a>',
-						'woocommerce-payments'
+			{ interpolateComponents( {
+				mixedString: __(
+					'WooPayments was in test mode when this order was placed. {{learnMoreLink/}}',
+					'woocommerce-payments'
+				),
+				components: {
+					learnMoreLink: (
+						<a
+							target="_blank"
+							href="https://woo.com/document/woopayments/testing-and-troubleshooting/testing/"
+							rel="noopener noreferrer"
+						>
+							{ __(
+								'Learn more about test mode',
+								'woocommerce-payments'
+							) }
+						</a>
 					),
-					{
-						a: (
-							// createInterpolateElement is incompatible with this eslint rule as the <a> is decoupled from content.
-							// eslint-disable-next-line jsx-a11y/anchor-has-content
-							<a href="https://woo.com/document/woopayments/testing-and-troubleshooting/testing/" />
-						),
-					}
-				) }
+				},
+			} ) }
 		</InlineNotice>
 	);
 };
