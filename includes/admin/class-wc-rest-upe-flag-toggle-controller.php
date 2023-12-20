ae00c2b695ec969e9ac8180b3aca8e2b02340e1a
Timur Karimov <timurkarimov@timurs-macbook-pro.home>
2023-12-20 20:12:58 +0100

    fix linting

--- includes/admin/class-wc-rest-upe-flag-toggle-controller.php
+++ includes/admin/class-wc-rest-upe-flag-toggle-controller.php
@@ -9,5 +9,8 @@
  * REST controller for UPE feature flag. Needs to stay in the codebase to avoid error on plugin update for versions 6.9.2 or earlier.
  */
 class WC_REST_UPE_Flag_Toggle_Controller extends WP_REST_Controller {
-    public function register_routes() {}
+	/**
+	 * Register routes.
+	 */
+	public function register_routes() {}
 }
