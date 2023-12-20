526ebb9f96efbc4dd1b6ccae23d66985f5d208e9
Timur Karimov <timur.karimow.95@gmail.com>
2023-12-20 20:08:59 +0100

    Update includes/admin/class-wc-rest-upe-flag-toggle-controller.php
    Co-authored-by: Brett Shumaker <brettshumaker@gmail.com>

--- includes/admin/class-wc-rest-upe-flag-toggle-controller.php
+++ includes/admin/class-wc-rest-upe-flag-toggle-controller.php
@@ -9,4 +9,5 @@
  * REST controller for UPE feature flag. Needs to stay in the codebase to avoid error on plugin update for versions 6.9.2 or earlier.
  */
 class WC_REST_UPE_Flag_Toggle_Controller extends WP_REST_Controller {
+    public function register_routes() {}
 }
