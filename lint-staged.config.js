6fe49c965ce98e0f1414f3bf3bcc975ab786db9e
Vasily Belolapotkov <belolapotkov.v@gmail.com>
2023-12-28 10:48:51 +0300

    Disable pre-commit php linting to push code style fixes by chunks. Will revert this commit in the end.

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	'*.{js,jsx,ts,tsx}': [ 'npm run format:provided', 'eslint' ],
 	'*.{ts,tsx}': [ () => 'tsc --noEmit' ],
 	'*.{scss,css}': [ 'npm run format:provided', 'npm run lint:css' ],
-	'*.php':
-		'./vendor/bin/phpcs --standard=phpcs.xml.dist --basepath=. --colors',
+	// '*.php':
+	// './vendor/bin/phpcs --standard=phpcs.xml.dist --basepath=. --colors',
 	'composer.json': 'composer validate --strict --no-check-all',
 };
