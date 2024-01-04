a9b2903abb4bee8d0be356c95e57eb078759b136
Guilherme Pressutto <gpressutto5@gmail.com>
2024-01-04 16:56:43 -0300

    Fixed deposit tests class

--- tests/unit/helpers/class-wc-helper-deposit-product-manager.php
+++ tests/unit/helpers/class-wc-helper-deposit-product-manager.php
@@ -11,10 +11,25 @@
  * This helper class should ONLY be used for unit tests!.
  */
 class WC_Deposits_Product_Manager {
-	public static function get_deposit_type( WC_Product_Simple $product ) {
+	/**
+	 * @param WC_Product_Simple|int $product
+	 * @return mixed
+	 */
+	public static function get_deposit_type( $product ) {
+		if ( is_int( $product ) ) {
+			$product = wc_get_product( $product );
+		}
 		return $product->get_meta( '_wc_deposit_type' );
 	}
-	public static function deposits_enabled( WC_Product_Simple $product ) {
+
+	/**
+	 * @param WC_Product_Simple|int $product
+	 * @return bool
+	 */
+	public static function deposits_enabled( $product ) {
+		if ( is_int( $product ) ) {
+			$product = wc_get_product( $product );
+		}
 		return true === $product->get_meta( '_wc_deposits_enabled' );
 	}
 }
