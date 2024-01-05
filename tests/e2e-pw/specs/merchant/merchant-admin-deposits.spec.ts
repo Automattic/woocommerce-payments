b5a8d522b34ebfa6e47be96444c6266a0720f96a
Ismael Martín Alabarce <info@ismaeld.com>
2024-01-05 11:11:19 +0100

    Restore deposits visual regression snapshot test
    Take a partial screenshot focusing only on the WCPay page element to avoid flakiness caused by general notices and banners.

--- tests/e2e-pw/specs/merchant/merchant-admin-deposits.spec.ts
+++ tests/e2e-pw/specs/merchant/merchant-admin-deposits.spec.ts
@@ -11,10 +11,20 @@ test.describe( 'Merchant deposits', () => {
 		await page.goto(
 			'/wp-admin/admin.php?page=wc-admin&path=/payments/deposits'
 		);
+
+		// Wait for the deposits table to load.
+		await page
+			.locator( '.woocommerce-table__table.is-loading' )
+			.waitFor( { state: 'hidden' } );
+
 		expect(
 			page.getByRole( 'heading', {
 				name: 'Deposit history',
 			} )
 		).toBeVisible();
+
+		await expect(
+			page.locator( '.woocommerce-payments-page' ).last()
+		).toHaveScreenshot();
 	} );
 } );
