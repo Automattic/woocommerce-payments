df4d9310f8c840ce2e0f19a50c8f66af75b1f089
Eric Jinks <3147296+Jinksi@users.noreply.github.com>
2024-01-05 13:25:05 +1000

    Add e2e test for viewing dispute details via the order notice

---
+++ tests/e2e/specs/wcpay/merchant/merchant-disputes-view-details-via-order-notice.spec.js
@@ -0,0 +1,57 @@
+/**
+ * External dependencies
+ */
+import config from 'config';
+
+/**
+ * Internal dependencies
+ */
+import { fillCardDetails, setupProductCheckout } from '../../../utils/payments';
+
+const { merchant, shopper } = require( '@woocommerce/e2e-utils' );
+
+describe( 'Disputes > View dispute details via disputed order notice', () => {
+	beforeAll( async () => {
+		await page.goto( config.get( 'url' ), { waitUntil: 'networkidle0' } );
+
+		// Place an order to dispute later
+		await setupProductCheckout(
+			config.get( 'addresses.customer.billing' )
+		);
+		const card = config.get( 'cards.disputed-fraudulent' );
+		await fillCardDetails( page, card );
+		await shopper.placeOrder();
+		await expect( page ).toMatch( 'Order received' );
+
+		// Get the order ID
+		const orderIdField = await page.$(
+			'.woocommerce-order-overview__order.order > strong'
+		);
+		const orderId = await orderIdField.evaluate( ( el ) => el.innerText );
+
+		await merchant.login();
+		await merchant.goToOrder( orderId );
+	} );
+
+	afterAll( async () => {
+		await merchant.logout();
+	} );
+
+	it( 'should click the disputed order notice and view the dispute details', async () => {
+		// TODO: if WC < 7.9, skip this test.
+
+		// Click the order dispute notice.
+		await expect( page ).toClick( '[type="button"]', {
+			text: 'Respond now',
+		} );
+
+		await page.waitForNavigation( {
+			waitUntil: 'networkidle0',
+		} );
+
+		// Verify we see the dispute details on the transaction details page.
+		await expect( page ).toMatchElement( '.dispute-notice', {
+			text: 'The cardholder claims this is an unauthorized transaction',
+		} );
+	} );
+} );
