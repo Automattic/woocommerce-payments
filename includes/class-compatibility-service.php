7963cc6d34bf2f38189b015032acd2e891034ed3
Jesse Pearson <jesse.pearson@automattic.com>
2024-01-08 12:40:35 -0400

    Add compatibility data when onboarding

--- includes/class-compatibility-service.php
+++ includes/class-compatibility-service.php
@@ -39,6 +39,19 @@ class Compatibility_Service {
 	 */
 	public function init_hooks() {
 		add_action( 'woocommerce_payments_account_refreshed', [ $this, 'update_compatibility_data' ] );
+		add_filter( 'wc_payments_get_onboarding_data_args', [ $this, 'add_compatibility_onboarding_data' ] );
+	}
+
+	/**
+	 * Gets the compatibility data.
+	 *
+	 * @return array
+	 */
+	public function get_compatibility_data(): array {
+		return [
+			'woopayments_version' => WCPAY_VERSION_NUMBER,
+			'woocommerce_version' => WC_VERSION,
+		];
 	}
 
 	/**
@@ -48,14 +61,21 @@ class Compatibility_Service {
 	 */
 	public function update_compatibility_data() {
 		try {
-			$this->payments_api_client->update_compatibility_data(
-				[
-					'woopayments_version' => WCPAY_VERSION_NUMBER,
-					'woocommerce_version' => WC_VERSION,
-				]
-			);
+			$this->payments_api_client->update_compatibility_data( $this->get_compatibility_data() );
 		} catch ( API_Exception $e ) { // phpcs:ignore Generic.CodeAnalysis.EmptyStatement.DetectedCatch
 			// The exception is already logged if logging is on, nothing else needed.
 		}
+	}
+
+	/**
+	 * Adds the compatibility data to the onboarding args.
+	 *
+	 * @param array $args The args being sent when onboarding.
+	 *
+	 * @return array
+	 */
+	public function add_compatibility_onboarding_data( $args ): array {
+		$args['compatibility_data'] = $this->get_compatibility_data();
+		return $args;
 	}
 }
