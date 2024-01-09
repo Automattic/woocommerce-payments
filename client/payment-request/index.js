8ba894bfd2c7faed0326f374337d8f8aa15d2fb5
Guilherme Pressutto <gpressutto5@gmail.com>
2024-01-09 16:31:05 -0300

    JS style fix

--- client/payment-request/index.js
+++ client/payment-request/index.js
@@ -440,10 +440,14 @@ jQuery( ( $ ) => {
 
 			// WooCommerce Deposits support.
 			// Trigger the "woocommerce_variation_has_changed" event when the deposit option is changed.
-			$( 'input[name=wc_deposit_option],input[name=wc_deposit_payment_plan]' ).on( 'change', () => {
-				$( 'form' ).has('input[name=wc_deposit_option],input[name=wc_deposit_payment_plan]').trigger(
-					'woocommerce_variation_has_changed'
-				);
+			$(
+				'input[name=wc_deposit_option],input[name=wc_deposit_payment_plan]'
+			).on( 'change', () => {
+				$( 'form' )
+					.has(
+						'input[name=wc_deposit_option],input[name=wc_deposit_payment_plan]'
+					)
+					.trigger( 'woocommerce_variation_has_changed' );
 			} );
 
 			$( document.body ).on( 'woocommerce_variation_has_changed', () => {
