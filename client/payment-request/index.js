63afd86f54e4e06f61a7208973bea6bb6e570b15
Guilherme Pressutto <gpressutto5@gmail.com>
2024-01-09 15:45:02 -0300

    Apply suggestions from code review
    Co-authored-by: Peter Wilson <519727+peterwilsoncc@users.noreply.github.com>

--- client/payment-request/index.js
+++ client/payment-request/index.js
@@ -440,8 +440,8 @@ jQuery( ( $ ) => {
 
 			// WooCommerce Deposits support.
 			// Trigger the "woocommerce_variation_has_changed" event when the deposit option is changed.
-			$( 'input[name=wc_deposit_option]' ).on( 'change', () => {
-				$( 'form:has(input[name=wc_deposit_option])' ).trigger(
+			$( 'input[name=wc_deposit_option],input[name=wc_deposit_payment_plan]' ).on( 'change', () => {
+				$( 'form' ).has('input[name=wc_deposit_option],input[name=wc_deposit_payment_plan]').trigger(
 					'woocommerce_variation_has_changed'
 				);
 			} );
