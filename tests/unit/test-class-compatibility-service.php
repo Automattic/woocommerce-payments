6194fd57823b2187fd39e52951e48f014fd4789d
Allie Mims <allie.mims@automattic.com>
2024-01-10 14:46:26 -0500

    Fix unit test

--- tests/unit/test-class-compatibility-service.php
+++ tests/unit/test-class-compatibility-service.php
@@ -59,12 +59,15 @@ class Compatibility_Service_Test extends WCPAY_UnitTestCase {
 			'woocommerce_version' => WC_VERSION,
 			'blog_theme'          => $stylesheet,
 			'post_types_count'    => [
-				'post'       => 0,
-				'page'       => 0,
+				'post'       => 1,
+				'page'       => 6,
 				'attachment' => 0,
-				'product'    => 0,
+				'product'    => 12,
 			],
 		];
+
+		// Arrange: Insert test posts.
+		$post_ids = $this->insert_test_posts( $expected['post_types_count'] );
 
 		// Arrange/Assert: Set the expectations for update_compatibility_data.
 		$this->mock_api_client
@@ -74,5 +77,45 @@ class Compatibility_Service_Test extends WCPAY_UnitTestCase {
 
 		// Act: Call the method we're testing.
 		$this->compatibility_service->update_compatibility_data();
+
+		// Clean up: Delete the test posts.
+		$this->delete_test_posts( $post_ids );
+	}
+
+	/**
+	 * Insert test posts for use during a unit test.
+	 *
+	 * @param  array $post_types  Assoc array of post types as keys and the number of posts to create for each.
+	 *
+	 * @return array Array of post IDs that were created.
+	 */
+	private function insert_test_posts( array $post_types ): array {
+		$post_ids = [];
+		foreach ( $post_types as $post_type => $count ) {
+			$title_content = 'This is a ' . $post_type . ' test post';
+			for ( $i = 0; $i < $count; $i++ ) {
+				$post_ids[] = (int) wp_insert_post(
+					[
+						'post_title'   => $title_content,
+						'post_content' => $title_content,
+						'post_type'    => $post_type,
+						'post_status'  => 'publish',
+					]
+				);
+			}
+		}
+
+		return $post_ids;
+	}
+
+	/**
+	 * Delete test posts that were created during a unit test.
+	 *
+	 * @param array $post_ids Array of post IDs to delete.
+	 */
+	private function delete_test_posts( array $post_ids ) {
+		foreach ( $post_ids as $post_id ) {
+			wp_delete_post( (int) $post_id, true );
+		}
 	}
 }
