a25a55555a8f2b5230d26d30d2a0fae3f921e2a3
Eric Jinks <3147296+Jinksi@users.noreply.github.com>
2024-01-12 15:16:58 +1000

    Replace state-dependant deposit visual test with limited UI test

--- tests/e2e-pw/specs/merchant/merchant-admin-deposits.spec.ts
+++ tests/e2e-pw/specs/merchant/merchant-admin-deposits.spec.ts
@@ -22,9 +22,35 @@ test.describe( 'Merchant deposits', () => {
 				name: 'Deposit history',
 			} )
 		).toBeVisible();
+	} );
+
+	test( 'Select deposits list advanced filters', async ( { page } ) => {
+		await page.goto(
+			'/wp-admin/admin.php?page=wc-admin&path=/payments/deposits'
+		);
+
+		// Wait for the deposits table to load.
+		await page
+			.locator( '.woocommerce-table__table.is-loading' )
+			.waitFor( { state: 'hidden' } );
+
+		// Open the advanced filters.
+		await page.getByRole( 'button', { name: 'All deposits' } ).click();
+		await page.getByRole( 'button', { name: 'Advanced filters' } ).click();
+
+		// Select a filter
+		await page.getByRole( 'button', { name: 'Add a Filter' } ).click();
+		await page.getByRole( 'button', { name: 'Status' } ).click();
+
+		// Select a filter option
+		await page
+			.getByLabel( 'Select a deposit status', {
+				exact: true,
+			} )
+			.selectOption( 'Pending' );
 
 		await expect(
-			page.locator( '.woocommerce-payments-page' ).last()
+			page.locator( '.woocommerce-filters' ).last()
 		).toHaveScreenshot();
 	} );
 } );
