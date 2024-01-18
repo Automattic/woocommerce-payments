f6c889d198249f12f01e6d356e2f1d0ad1c65942
Miguel Gasca <miguel.gasca@automattic.com>
2024-01-18 09:35:40 +0100

    WIP: found blocker

--- tests/e2e/specs/wcpay/merchant/merchant-admin-transactions.spec.js
+++ tests/e2e/specs/wcpay/merchant/merchant-admin-transactions.spec.js
@@ -1,7 +1,8 @@
 /**
  * External dependencies
  */
-const { merchant } = require( '@woocommerce/e2e-utils' );
+import config from 'config';
+const { merchant, shopper } = require( '@woocommerce/e2e-utils' );
 
 /**
  * Internal dependencies
@@ -12,14 +13,25 @@ import {
 	takeScreenshot,
 	describeif,
 } from '../../../utils';
+import { fillCardDetails, setupProductCheckout } from '../../../utils/payments';
+
+const card = config.get( 'cards.basic' );
+const product1 = config.get( 'products.simple.name' );
 
 describe( 'Admin transactions', () => {
 	beforeAll( async () => {
+		await setupProductCheckout(
+			config.get( 'addresses.customer.billing' ),
+			[ [ product1, 1 ] ]
+		);
+		await fillCardDetails( page, card );
+		await shopper.placeOrder();
 		await merchant.login();
-	} );
+	}, 200000 );
 
 	it( 'page should load without any errors', async () => {
 		await merchantWCP.openTransactions();
+		await jestPuppeteer.debug();
 		await expect( page ).toMatchElement( 'h2', { text: 'Transactions' } );
 		await takeScreenshot( 'merchant-admin-transactions' );
 	} );
