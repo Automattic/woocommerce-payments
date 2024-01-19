605ee355982851f74df76f6d88972d26fc3a2b82
Eric Jinks <3147296+Jinksi@users.noreply.github.com>
2024-01-19 14:50:30 +1000

    Remove redundant comments from playwright config

--- tests/e2e-pw/playwright.config.ts
+++ tests/e2e-pw/playwright.config.ts
@@ -4,12 +4,6 @@
  */
 import { defineConfig, devices } from '@playwright/test';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
 const { BASE_URL } = process.env;
 
 /**
@@ -35,15 +29,12 @@ export default defineConfig( {
 		trace: 'retain-on-failure',
 		video: 'on-first-retry',
 		viewport: { width: 1280, height: 720 },
-		// storageState: 'tests/e2e-pw/storage/state.json',
 	},
 	expect: {
 		toHaveScreenshot: { maxDiffPixelRatio: 0.025 },
 	},
 	snapshotPathTemplate:
 		'{testDir}/__snapshots__/{testFilePath}/{arg}_{projectName}{ext}',
-	// globalSetup: require.resolve( './global-setup' ),
-	// globalTeardown: require.resolve( './global-teardown' ),
 
 	/* Configure projects for major browsers */
 	projects: [
@@ -55,34 +46,5 @@ export default defineConfig( {
 
 		// Setup project
 		{ name: 'setup', testMatch: /.*\.setup\.ts/ },
-		// {
-		// 	name: 'firefox',
-		// 	use: { ...devices[ 'Desktop Firefox' ] },
-		// },
-
-		// {
-		// 	name: 'webkit',
-		// 	use: { ...devices[ 'Desktop Safari' ] },
-		// },
-
-		/* Test against mobile viewports. */
-		// {
-		//   name: 'Mobile Chrome',
-		//   use: { ...devices['Pixel 5'] },
-		// },
-		// {
-		//   name: 'Mobile Safari',
-		//   use: { ...devices['iPhone 12'] },
-		// },
-
-		/* Test against branded browsers. */
-		// {
-		//   name: 'Microsoft Edge',
-		//   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-		// },
-		// {
-		//   name: 'Google Chrome',
-		//   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-		// },
 	],
 } );
