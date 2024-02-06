c3892f9a7cfaabd3fff88ec6f219e94bcf9aa1b6
Eric Jinks <3147296+Jinksi@users.noreply.github.com>
2024-02-07 09:41:20 +1000

    Disable local parallelism

--- tests/e2e-pw/playwright.config.ts
+++ tests/e2e-pw/playwright.config.ts
@@ -17,8 +17,8 @@ export default defineConfig( {
 	forbidOnly: !! process.env.CI,
 	/* Retry on CI only */
 	retries: process.env.CI ? 2 : 0,
-	/* Opt out of parallel tests on CI. */
-	workers: process.env.CI ? 1 : undefined,
+	/* Opt out of parallel tests. */
+	workers: 1,
 	/* Reporters to use. See https://playwright.dev/docs/test-reporters */
 	reporter: process.env.CI
 		? [
