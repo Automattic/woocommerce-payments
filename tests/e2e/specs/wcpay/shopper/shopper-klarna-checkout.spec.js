c63101c8cb00ef966efe2c3d3b8310dc31b70c79
frosso <francesco.rosso@automattic.com>
2024-02-08 13:50:31 +0100

    chore: add Klarna e2e tests

---
+++ tests/e2e/specs/wcpay/shopper/shopper-klarna-checkout.spec.js
@@ -0,0 +1,105 @@
+/**
+ * External dependencies
+ */
+const { shopper, merchant } = require( '@woocommerce/e2e-utils' );
+import config from 'config';
+import { uiUnblocked } from '@woocommerce/e2e-utils/build/page-utils';
+/**
+ * Internal dependencies
+ */
+import { merchantWCP, shopperWCP } from '../../../utils/flows';
+import { setupProductCheckout } from '../../../utils/payments';
+
+const UPE_METHOD_CHECKBOXES = [
+	"//label[contains(text(), 'Klarna')]/preceding-sibling::span/input[@type='checkbox']",
+];
+
+const checkoutPaymentMethodSelector = `//*[@id='payment']/ul/li/label[contains(text(), 'Klarna')]`;
+
+describe( 'Klarna checkout', () => {
+	beforeAll( async () => {
+		await merchant.login();
+		await merchantWCP.enablePaymentMethod( UPE_METHOD_CHECKBOXES );
+		await merchant.logout();
+	} );
+
+	beforeEach( async () => {
+		await shopper.login();
+		await shopperWCP.changeAccountCurrencyTo( 'USD' );
+	} );
+
+	afterEach( async () => {
+		await shopperWCP.emptyCart();
+	} );
+
+	afterAll( async () => {
+		await shopperWCP.logout();
+		await merchant.login();
+		await merchantWCP.disablePaymentMethod( UPE_METHOD_CHECKBOXES );
+		await merchant.logout();
+	} );
+
+	it( 'should not show Klarna at checkout if the order amount is less than the minimum threshold', async () => {
+		await setupProductCheckout(
+			config.get( 'addresses.customer.billing' ),
+			[ [ 'Beanie', 1 ] ]
+		);
+
+		await uiUnblocked();
+
+		expect( await page.$x( checkoutPaymentMethodSelector ) ).toBeNull();
+	} );
+
+	it( 'should show the product messaging on the product page', async () => {
+		await shopper.goToProductPageBySlug( 'belt' );
+
+		// waiting for the "product messaging" component to be rendered, so we can click on it.
+		await page.waitForSelector(
+			'button[aria-label="Open Learn More Modal"]'
+		);
+		const [ productMessaging ] = await page.$(
+			'button[aria-label="Open Learn More Modal"]'
+		);
+		await productMessaging.click();
+
+		await expect( page ).toMatchElement( '[data-testid="ModalHeader"]', {
+			text: 'Buy Now. Pay Later.',
+		} );
+		await expect( page ).toMatchElement(
+			'[data-testid="ModalDescription"]',
+			{
+				text:
+					'Select Klarna as your payment method at checkout to pay in installments.',
+			}
+		);
+	} );
+
+	it( `should successfully place an order with Klarna`, async () => {
+		await setupProductCheckout(
+			config.get( 'addresses.customer.billing' ),
+			[ [ 'Beanie', 3 ] ]
+		);
+
+		await uiUnblocked();
+		// Select BNPL provider as payment method.
+		await page.waitForXPath( checkoutPaymentMethodSelector );
+		const [ paymentMethodLabel ] = await page.$x(
+			checkoutPaymentMethodSelector
+		);
+		await paymentMethodLabel.click();
+		await shopper.placeOrder();
+
+		// Authorize payment with Stripe.
+		// This XPath selector matches the Authorize Payment button, that is either a button or an anchor.
+		const xPathAuthorizePaymentButton = `//*[self::button or self::a][contains(text(), 'Authorize Test Payment')]`;
+		await page.waitForXPath( xPathAuthorizePaymentButton );
+		const [ stripeButton ] = await page.$x( xPathAuthorizePaymentButton );
+		await stripeButton.click();
+
+		// Wait for the order confirmation page to load.
+		await page.waitForNavigation( {
+			waitUntil: 'networkidle0',
+		} );
+		await expect( page ).toMatch( 'Order received' );
+	} );
+} );
