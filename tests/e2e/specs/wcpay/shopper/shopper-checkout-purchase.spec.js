c0c4933bb7a3a757c96f717d225df38b50474545
Taha Paksu <3295+tpaksu@users.noreply.github.com>
2024-02-09 20:28:54 +0300

    Convert to describe.each to keep code clean

--- tests/e2e/specs/wcpay/shopper/shopper-checkout-purchase.spec.js
+++ tests/e2e/specs/wcpay/shopper/shopper-checkout-purchase.spec.js
@@ -17,73 +17,49 @@ import {
 
 import { shopperWCP, merchantWCP } from '../../../utils';
 
-describe( 'Successful purchase', () => {
-	beforeEach( async () => {
-		await setupProductCheckout(
-			config.get( 'addresses.customer.billing' )
-		);
-	} );
-
-	afterAll( async () => {
-		// Clear the cart at the end so it's ready for another test
-		await shopperWCP.emptyCart();
-	} );
-
-	it( 'using a basic card', async () => {
-		const card = config.get( 'cards.basic' );
-		await fillCardDetails( page, card );
-		await shopper.placeOrder();
-		await expect( page ).toMatch( 'Order received' );
-	} );
-
-	it( 'using a 3DS card', async () => {
-		const card = config.get( 'cards.3ds' );
-		await fillCardDetails( page, card );
-		await expect( page ).toClick( '#place_order' );
-		await confirmCardAuthentication( page );
-		await page.waitForNavigation( {
-			waitUntil: 'networkidle0',
+describe.each( [ [ true ], [ false ] ] )(
+	'Successful purchase, CT enabled: %s',
+	( cardTestingPreventionEnabled ) => {
+		beforeAll( async () => {
+			if ( cardTestingPreventionEnabled ) {
+				await merchant.login();
+				await merchantWCP.enableCardTestingProtection();
+				await merchant.logout();
+			}
 		} );
-		await expect( page ).toMatch( 'Order received' );
-	} );
-} );
 
-describe( 'Successful purchase with card testing protections enabled', () => {
-	beforeAll( async () => {
-		await merchant.login();
-		await merchantWCP.enableCardTestingProtection();
-		await merchant.logout();
-	} );
-
-	beforeEach( async () => {
-		await setupProductCheckout(
-			config.get( 'addresses.customer.billing' )
-		);
-	} );
-
-	afterAll( async () => {
-		// Clear the cart at the end so it's ready for another test
-		await shopperWCP.emptyCart();
-		await merchant.login();
-		await merchantWCP.disableCardTestingProtection();
-		await merchant.logout();
-	} );
-
-	it( 'using a basic card', async () => {
-		const card = config.get( 'cards.basic' );
-		await fillCardDetails( page, card );
-		await shopper.placeOrder();
-		await expect( page ).toMatch( 'Order received' );
-	} );
-
-	it( 'using a 3DS card', async () => {
-		const card = config.get( 'cards.3ds' );
-		await fillCardDetails( page, card );
-		await expect( page ).toClick( '#place_order' );
-		await confirmCardAuthentication( page );
-		await page.waitForNavigation( {
-			waitUntil: 'networkidle0',
+		beforeEach( async () => {
+			await setupProductCheckout(
+				config.get( 'addresses.customer.billing' )
+			);
 		} );
-		await expect( page ).toMatch( 'Order received' );
-	} );
-} );
+
+		afterAll( async () => {
+			// Clear the cart at the end so it's ready for another test
+			await shopperWCP.emptyCart();
+			if ( cardTestingPreventionEnabled ) {
+				await merchant.login();
+				await merchantWCP.disableCardTestingProtection();
+				await merchant.logout();
+			}
+		} );
+
+		it( 'using a basic card', async () => {
+			const card = config.get( 'cards.basic' );
+			await fillCardDetails( page, card );
+			await shopper.placeOrder();
+			await expect( page ).toMatch( 'Order received' );
+		} );
+
+		it( 'using a 3DS card', async () => {
+			const card = config.get( 'cards.3ds' );
+			await fillCardDetails( page, card );
+			await expect( page ).toClick( '#place_order' );
+			await confirmCardAuthentication( page );
+			await page.waitForNavigation( {
+				waitUntil: 'networkidle0',
+			} );
+			await expect( page ).toMatch( 'Order received' );
+		} );
+	}
+);
