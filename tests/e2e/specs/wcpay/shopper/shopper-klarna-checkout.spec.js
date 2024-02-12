f107aea569d41066b43d58e9843ea2d890d452df
frosso <francesco.rosso@automattic.com>
2024-02-12 20:34:50 +0100

    WIP

--- tests/e2e/specs/wcpay/shopper/shopper-klarna-checkout.spec.js
+++ tests/e2e/specs/wcpay/shopper/shopper-klarna-checkout.spec.js
@@ -39,18 +39,7 @@ describe( 'Klarna checkout', () => {
 		await merchant.logout();
 	} );
 
-	it( 'should not show Klarna at checkout if the order amount is less than the minimum threshold', async () => {
-		await setupProductCheckout(
-			config.get( 'addresses.customer.billing' ),
-			[ [ 'Beanie', 1 ] ]
-		);
-
-		await uiUnblocked();
-
-		expect( await page.$x( checkoutPaymentMethodSelector ) ).toBeNull();
-	} );
-
-	it.only( 'should show the product messaging on the product page', async () => {
+	it( 'should show the product messaging on the product page', async () => {
 		await shopperWCP.goToProductPageBySlug( 'belt' );
 
 		// waiting for the "product messaging" component to be rendered, so we can click on it.
@@ -59,29 +48,30 @@ describe( 'Klarna checkout', () => {
 		);
 		const paymentMethodMessageIframe = await paymentMethodMessageFrameHandle.contentFrame();
 		const productMessaging = await paymentMethodMessageIframe.waitForSelector(
-			'button[aria-label="Open Learn More Modal"]',
-			{ timeout: 30000 }
+			'button[aria-label="Open Learn More Modal"]'
 		);
-		console.log( '###', paymentMethodMessageIframe, productMessaging );
-		// const productMessaging = await paymentMethodMessageIframe.$(
-		// 	'button[aria-label="Open Learn More Modal"]'
-		// );
+		// scrolling vertically to get the "product messaging" into view, since it seems that otherwise clicking on it fails.
+		await page.evaluate( () => {
+			window.scrollBy( 0, 100 );
+		} );
 		await productMessaging.click();
 
+		// we need to wait for the iframe to be added by Stripe JS after clicking on the element.
+		await page.waitFor( 1000 );
+
 		const paymentMethodMessageModalIframeHandle = await page.waitForSelector(
-			'iframe[src*="js.stripe.com/v3/elements-inner-payment-method-messaging-modal"]',
-			{ timeout: 30000 }
+			'iframe[src*="js.stripe.com/v3/elements-inner-payment-method-messaging-modal"]'
 		);
 		const paymentMethodMessageModalIframe = await paymentMethodMessageModalIframeHandle.contentFrame();
 
 		await expect( paymentMethodMessageModalIframe ).toMatchElement(
-			'[data-testid="ModalHeader"]',
+			'[data-testid="ModalHeader"] > p',
 			{
 				text: 'Buy Now. Pay Later.',
 			}
 		);
 		await expect( paymentMethodMessageModalIframe ).toMatchElement(
-			'[data-testid="ModalDescription"]',
+			'[data-testid="ModalDescription"] > p',
 			{
 				text:
 					'Select Klarna as your payment method at checkout to pay in installments.',
@@ -90,6 +80,12 @@ describe( 'Klarna checkout', () => {
 	} );
 
 	it( `should successfully place an order with Klarna`, async () => {
+		const getNewKlarnaIframe = async () => {
+			const klarnaFrameHandle = await page.waitForSelector(
+				'#klarna-apf-iframe'
+			);
+			return await klarnaFrameHandle.contentFrame();
+		};
 		await setupProductCheckout(
 			config.get( 'addresses.customer.billing' ),
 			[ [ 'Beanie', 3 ] ]
@@ -105,20 +101,18 @@ describe( 'Klarna checkout', () => {
 		await shopper.placeOrder();
 
 		// waiting for the redirect & the Klarna iframe to load within the Stripe test page
-		const klarnaFrameHandle = await page.waitForSelector(
-			'#klarna-apf-iframe'
-		);
-		const klarnaIframe = await klarnaFrameHandle.contentFrame();
 		(
-			await klarnaIframe.waitForSelector(
+			await ( await getNewKlarnaIframe() ).waitForSelector(
 				'[data-testid="select-payment-category"]'
 			)
 		 ).click();
 		(
-			await klarnaIframe.waitForSelector( '[data-testid="pick-plan"]' )
+			await ( await getNewKlarnaIframe() ).waitForSelector(
+				'[data-testid="pick-plan"]'
+			)
 		 ).click();
 		(
-			await klarnaIframe.waitForSelector(
+			await ( await getNewKlarnaIframe() ).waitForSelector(
 				'[data-testid="confirm-and-pay"]'
 			)
 		 ).click();
