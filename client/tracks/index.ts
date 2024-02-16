5f6562a41ec598cbc4391d378f166edd0ea519f2
Ismael Martín Alabarce <info@ismaeld.com>
2024-02-16 12:04:23 +0100

    Filter out undefined event properties

--- client/tracks/index.ts
+++ client/tracks/index.ts
@@ -41,6 +41,13 @@ export const recordEvent = (
 			woo_country_code: wcpaySettings.connect.country,
 			hosting_provider: wcpaySettings.trackingInfo?.hosting_provider,
 		} );
+
+		// Filter our undefined properties.
+		for ( const key in eventProperties ) {
+			if ( eventProperties[ key ] === undefined ) {
+				delete eventProperties[ key ];
+			}
+		}
 	}
 	// Wc-admin track script is enqueued after ours, wrap in domReady
 	// to make sure we're not too early.
