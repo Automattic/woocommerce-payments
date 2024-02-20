8c01b10f63daca9cef8bb7dbadb4b25db8420435
elazzabi <ahmed.el.azzabi@automattic.com>
2024-02-20 12:08:39 +0100

    Add unit tests

---
+++ client/components/payment-activity/test/index.tsx
@@ -0,0 +1,44 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import { render } from '@testing-library/react';
+
+/**
+ * Internal dependencies
+ */
+import PaymentActivity from '..';
+
+declare const global: {
+	wcpaySettings: {
+		transactions_data: {
+			tpv: number;
+		};
+	};
+};
+
+describe( 'PaymentActivity component', () => {
+	test( 'Component renders an empty state', () => {
+		global.wcpaySettings = {
+			transactions_data: {
+				tpv: 0,
+			},
+		};
+
+		const { container, getByText } = render( <PaymentActivity /> );
+		// Making sure the empty state is rendered
+		getByText( 'No payments… yet!' );
+	} );
+
+	test( 'Component renders non-empty state', () => {
+		global.wcpaySettings = {
+			transactions_data: {
+				tpv: 1000,
+			},
+		};
+
+		const { container, getByText } = render( <PaymentActivity /> );
+		// Should be updated with correct checks
+		getByText( 'You have some money' );
+	} );
+} );
