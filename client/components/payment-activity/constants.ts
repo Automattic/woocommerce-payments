c27ab15924d1a1f36d09e9273e478d8d4723a3c4
elazzabi <ahmed.el.azzabi@automattic.com>
2024-02-22 10:10:31 +0100

    Add consts for time ranges

---
+++ client/components/payment-activity/constants.ts
@@ -0,0 +1,25 @@
+export enum TIME_RANGES {
+	TODAY_VALUE = 'today',
+	TODAY_LABEL = 'Today',
+
+	SEVEN_DAYS_VALUE = '7-days',
+	SEVEN_DAYS_LABEL = 'Last 7 days',
+
+	FOUR_WEEKS_VALUE = '4-weeks',
+	FOUR_WEEKS_LABEL = 'Last 4 weeks',
+
+	THREE_MONTHS_VALUE = '3-months',
+	THREE_MONTHS_LABEL = 'Last 3 months',
+
+	TWELVE_MONTHS_VALUE = '12-months',
+	TWELVE_MONTHS_LABEL = 'Last 12 months',
+
+	MONTH_TO_DATE_VALUE = 'month-to-date',
+	MONTH_TO_DATE_LABEL = 'Month to date',
+
+	QUARTER_TO_DATE_VALUE = 'quarter-to-date',
+	QUARTER_TO_DATE_LABEL = 'Quarter to date',
+
+	YEAR_TO_DATE_VALUE = 'year-to-date',
+	YEAR_TO_DATE_LABEL = 'Year to date',
+}
