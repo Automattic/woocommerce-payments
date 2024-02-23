43ae88ff1e91d4250617de18431527298a3ee265
Hector Lovo <>
2024-02-23 14:11:36 -0500

    Only apply WooPay Direct Checkout on the cart page for now

--- includes/class-wc-payments-woopay-direct-checkout.php
+++ includes/class-wc-payments-woopay-direct-checkout.php
@@ -29,8 +29,22 @@ class WC_Payments_WooPay_Direct_Checkout {
 	 * @return void
 	 */
 	public function scripts() {
+		// Only enqueue the script on the cart page, for now.
+		if ( ! $this->is_cart_page() ) {
+			return;
+		}
+
 		WC_Payments::register_script_with_dependencies( 'WCPAY_WOOPAY_DIRECT_CHECKOUT', 'dist/woopay-direct-checkout' );
 
 		wp_enqueue_script( 'WCPAY_WOOPAY_DIRECT_CHECKOUT' );
+	}
+
+	/**
+	 * Check if the current page is the cart page.
+	 *
+	 * @return bool True if the current page is the cart page, false otherwise.
+	 */
+	public function is_cart_page(): bool {
+		return is_cart() || has_block( 'woocommerce/cart' );
 	}
 }
