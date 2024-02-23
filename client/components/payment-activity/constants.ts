781a8b4c3da031757882c20e78f1bd8d5ea14e16
elazzabi <ahmed.el.azzabi@automattic.com>
2024-02-23 11:40:22 +0100

    change from enum to const to be able to use i18n

--- client/components/payment-activity/constants.ts
+++ client/components/payment-activity/constants.ts
@@ -1,25 +1,33 @@
-export enum TIME_RANGES {
-	TODAY_VALUE = 'today',
-	TODAY_LABEL = 'Today',
+/**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
 
-	SEVEN_DAYS_VALUE = '7-days',
-	SEVEN_DAYS_LABEL = 'Last 7 days',
+export const ranges = {
+	TODAY_VALUE: 'today',
+	TODAY_LABEL: __( 'Today', 'woocommerce-payments' ),
 
-	FOUR_WEEKS_VALUE = '4-weeks',
-	FOUR_WEEKS_LABEL = 'Last 4 weeks',
+	SEVEN_DAYS_VALUE: '7-days',
+	SEVEN_DAYS_LABEL: __( 'Last 7 days', 'woocommerce-payments' ),
 
-	THREE_MONTHS_VALUE = '3-months',
-	THREE_MONTHS_LABEL = 'Last 3 months',
+	FOUR_WEEKS_VALUE: '4-weeks',
+	FOUR_WEEKS_LABEL: __( 'Last 4 weeks', 'woocommerce-payments' ),
 
-	TWELVE_MONTHS_VALUE = '12-months',
-	TWELVE_MONTHS_LABEL = 'Last 12 months',
+	THREE_MONTHS_VALUE: '3-months',
+	THREE_MONTHS_LABEL: __( 'Last 3 months', 'woocommerce-payments' ),
 
-	MONTH_TO_DATE_VALUE = 'month-to-date',
-	MONTH_TO_DATE_LABEL = 'Month to date',
+	TWELVE_MONTHS_VALUE: '12-months',
+	TWELVE_MONTHS_LABEL: __( 'Last 12 months', 'woocommerce-payments' ),
 
-	QUARTER_TO_DATE_VALUE = 'quarter-to-date',
-	QUARTER_TO_DATE_LABEL = 'Quarter to date',
+	MONTH_TO_DATE_VALUE: 'month-to-date',
+	MONTH_TO_DATE_LABEL: __( 'Month to date', 'woocommerce-payments' ),
 
-	YEAR_TO_DATE_VALUE = 'year-to-date',
-	YEAR_TO_DATE_LABEL = 'Year to date',
-}
+	QUARTER_TO_DATE_VALUE: 'quarter-to-date',
+	QUARTER_TO_DATE_LABEL: __( 'Quarter to date', 'woocommerce-payments' ),
+
+	YEAR_TO_DATE_VALUE: 'year-to-date',
+	YEAR_TO_DATE_LABEL: __( 'Year to date', 'woocommerce-payments' ),
+
+	CUSTOM_VALUE: 'custom',
+	CUSTOM_LABEL: __( 'Custom', 'woocommerce-payments' ),
+};
