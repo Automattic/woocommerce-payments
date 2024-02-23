204ce365d7769372c831d6c9f4e7824b10bfea3b
Shendy <73803630+shendy-a8c@users.noreply.github.com>
2024-02-24 05:05:47 +0700

    Have TotalBalanceTooltip as its own component.

--- client/components/account-balances/balance-tooltip.tsx
+++ client/components/account-balances/balance-tooltip.tsx
@@ -3,16 +3,24 @@
  */
 import React from 'react';
 import HelpOutlineIcon from 'gridicons/dist/help-outline';
+import interpolateComponents from '@automattic/interpolate-components';
+import { __ } from '@wordpress/i18n';
 
 /**
  * Internal dependencies
  */
 import { ClickTooltip } from 'components/tooltip';
+import { documentationUrls, fundLabelStrings } from './strings';
+import InlineNotice from 'components/inline-notice';
 
 type BalanceTooltipProps = {
 	label: string;
 	content: React.ReactNode;
 	maxWidth?: string | undefined;
+};
+
+type TotalBalanceTooltipProps = {
+	balance: number;
 };
 
 const BalanceTooltip: React.FC< BalanceTooltipProps > = ( {
@@ -27,6 +35,51 @@ const BalanceTooltip: React.FC< BalanceTooltipProps > = ( {
 			buttonIcon={ <HelpOutlineIcon /> }
 			buttonLabel={ label }
 			maxWidth={ maxWidth }
+		/>
+	);
+};
+
+export const TotalBalanceTooltip: React.FC< TotalBalanceTooltipProps > = ( {
+	balance,
+} ) => {
+	return (
+		<BalanceTooltip
+			label={ `${ fundLabelStrings.total } tooltip` }
+			maxWidth={ balance < 0 ? undefined : '335px' }
+			content={
+				<>
+					<>
+						{ interpolateComponents( {
+							mixedString: __(
+								'{{bold}}Total balance{{/bold}} combines both pending funds (transactions under processing) and available funds (ready for deposit). {{learnMoreLink}}Learn more{{/learnMoreLink}}',
+								'woocommerce-payments'
+							),
+							components: {
+								bold: <b />,
+								learnMoreLink: (
+									// eslint-disable-next-line jsx-a11y/anchor-has-content
+									<a
+										rel="external noopener noreferrer"
+										target="_blank"
+										href={
+											documentationUrls.depositSchedule
+										}
+									/>
+								),
+							},
+						} ) }
+					</>
+					<InlineNotice
+						className="wcpay-account-balances__balances-total-balance-tooltip"
+						isDismissible={ false }
+					>
+						{ __(
+							'Total balance = Available funds + Pending funds',
+							'woocommerce-payments'
+						) }
+					</InlineNotice>
+				</>
+			}
 		/>
 	);
 };
