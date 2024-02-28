660c07717e3bf01e29784b4bf04f8f02a360f0f8
Eric Jinks <3147296+Jinksi@users.noreply.github.com>
2024-02-28 16:33:27 +1000

    Use docker-compose file for configuring Playwright docker

--- tests/e2e-pw/test-e2e-pw-ui.sh
+++ tests/e2e-pw/test-e2e-pw-ui.sh
@@ -4,11 +4,5 @@ echo "🎭 Running Playwright e2e tests in interactive UI mode.";
 echo "";
 echo "Open http://localhost:8077 in your browser to see the UI.";
 
-# When updating the Playwright version in the image tage below, make sure to update the npm `@playwright/test` package.json version as well.
-
-docker run -v "$PWD:/woopayments" -w /woopayments \
-	-p 8077:8077 \
-	-e BASE_URL=http://host.docker.internal:8084 \
-	-it --rm --ipc=host \
-	mcr.microsoft.com/playwright:v1.39.0-jammy \
+docker compose -f ./tests/e2e-pw/docker-compose.yml run --rm -it --service-ports playwright \
 	npx playwright test --config=tests/e2e-pw/playwright.config.ts --ui --ui-host=0.0.0.0 --ui-port=8077 "$@"
--- tests/e2e-pw/test-e2e-pw.sh
+++ tests/e2e-pw/test-e2e-pw.sh
@@ -2,10 +2,5 @@
 
 echo "🎭 Running Playwright e2e tests in default headless mode.";
 
-# When updating the Playwright version in the image tage below, make sure to update the npm `@playwright/test` package.json version as well.
-
-docker run -v "$PWD:/woopayments" -w /woopayments \
-	-e BASE_URL=http://host.docker.internal:8084 \
-	-it --rm --ipc=host \
-	mcr.microsoft.com/playwright:v1.39.0-jammy \
+docker compose -f ./tests/e2e-pw/docker-compose.yml run --rm -it --service-ports playwright \
 	npx playwright test --config=tests/e2e-pw/playwright.config.ts "$@"
