b843658579c8f4210a228df7a8be1ae9ee9ac282
Vasily Belolapotkov <belolapotkov.v@gmail.com>
2024-03-15 17:19:38 +0300

    Fix password input locator to avoid locating show password button

--- tests/e2e-pw/utils/helpers.ts
+++ tests/e2e-pw/utils/helpers.ts
@@ -23,7 +23,7 @@ export const wpAdminLogin = async (
 ): void => {
 	await page.goto( `/wp-admin` );
 	await page.getByLabel( 'Username or Email Address' ).fill( user.username );
-	await page.getByLabel( 'Password' ).fill( user.password );
+	await page.getByLabel( 'Password', { exact: true } ).fill( user.password ); // Need exact match to avoid resolving "Show password" button.
 	await page.getByRole( 'button', { name: 'Log In' } ).click();
 };
 
