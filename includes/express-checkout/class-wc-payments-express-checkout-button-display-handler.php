4222d2d48c026bfc28366b8927d8d579bb558d43
Timur Karimov <timurkarimov@timurs-macbook-pro.home>
2024-03-26 12:17:06 +0100

    remove isset

--- includes/express-checkout/class-wc-payments-express-checkout-button-display-handler.php
+++ includes/express-checkout/class-wc-payments-express-checkout-button-display-handler.php
@@ -173,8 +173,8 @@ class WC_Payments_Express_Checkout_Button_Display_Handler {
 					$user_email = isset( $_POST['email'] ) ? sanitize_email( wp_unslash( filter_input( INPUT_POST, 'email' ) ) ) : $session_email;
 
 					$js_config['order_id']      = $order->get_id();
-					$js_config['pay_for_order'] = sanitize_text_field( wp_unslash( isset( $_GET['pay_for_order'] ) ) );
-					$js_config['key']           = sanitize_text_field( wp_unslash( isset( $_GET['key'] ) ) );
+					$js_config['pay_for_order'] = sanitize_text_field( wp_unslash( $_GET['pay_for_order'] ) );
+					$js_config['key']           = sanitize_text_field( wp_unslash( $_GET['key'] ) );
 					$js_config['billing_email'] = current_user_can( 'read_private_shop_orders' ) ||
 						( get_current_user_id() !== 0 && $order->get_customer_id() === get_current_user_id() )
 						? $order->get_billing_email()
