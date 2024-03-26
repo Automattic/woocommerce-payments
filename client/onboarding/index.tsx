8dbba9cdb9fe1948ef86770b311b6b1dce59078b
mordeth <cvetan.cvetanov@automattic.com>
2024-03-26 15:32:32 +0200

    Onboarding cancellation should redirect to payments/connect

--- client/onboarding/index.tsx
+++ client/onboarding/index.tsx
@@ -15,16 +15,15 @@ import BusinessDetails from './steps/business-details';
 import StoreDetails from './steps/store-details';
 import LoadingStep from './steps/loading';
 import { trackStarted } from './tracking';
+import { getAdminUrl } from 'wcpay/utils';
 import './style.scss';
 
 const OnboardingStepper = () => {
 	const handleExit = () => {
-		if (
-			window.history.length > 1 &&
-			document.referrer.includes( wcSettings.adminUrl )
-		)
-			return window.history.back();
-		window.location.href = wcSettings.adminUrl;
+		window.location.href = getAdminUrl( {
+			page: 'wc-admin',
+			path: '/payments/connect',
+		} );
 	};
 
 	const handleStepChange = () => window.scroll( 0, 0 );
