1054b1303dbc00c1811598ec41142f5e064fae1c
Nagesh Pai <nagesh.pai@automattic.com>
2024-04-02 17:00:39 +0530

    Add test for empty component

--- client/components/payment-activity/test/index.tsx
+++ client/components/payment-activity/test/index.tsx
@@ -9,10 +9,25 @@ import { render } from '@testing-library/react';
  */
 import PaymentActivity from '..';
 
+declare const global: {
+	wcpaySettings: {
+		all_time_tpv: number;
+	};
+};
+
 describe( 'PaymentActivity component', () => {
 	it( 'should render', () => {
 		const { container } = render( <PaymentActivity /> );
 
+		expect( container ).toMatchSnapshot();
+	} );
+
+	it( 'should render an empty state', () => {
+		global.wcpaySettings.all_time_tpv = 0;
+
+		const { container, getByText } = render( <PaymentActivity /> );
+
+		getByText( 'No payments...yet!' );
 		expect( container ).toMatchSnapshot();
 	} );
 } );
