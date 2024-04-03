ff37f4db1a3d0c0301953c99c30d008ec683aa2f
Nagesh Pai <nagesh.pai@automattic.com>
2024-04-03 15:17:37 +0530

    Add string constants

---
+++ client/components/payment-activity/strings.ts
@@ -0,0 +1,11 @@
+/**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+export const paymentsDataHighlightsStrings = {
+	charges: __( 'Charges', 'woocommerce-payments' ),
+	refunds: __( 'Refunds', 'woocommerce-payments' ),
+	disputes: __( 'Disputes', 'woocommerce-payments' ),
+	fees: __( 'Fees', 'woocommerce-payments' ),
+};
