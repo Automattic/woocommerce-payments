5f8a733e42acb0e107071e6df7c5e704da9ce24a
César Costa <costa.cesar.a@hotmail.com>
2024-04-03 15:24:59 -0300

    Reset cookies to initial state in the tests

--- client/checkout/woopay/direct-checkout/test/woopay-direct-checkout.test.js
+++ client/checkout/woopay/direct-checkout/test/woopay-direct-checkout.test.js
@@ -4,6 +4,15 @@
 import WooPayDirectCheckout from '../woopay-direct-checkout';
 
 describe( 'WooPay Direct Checkout', () => {
+	const originalDocumentCookie = window.document.cookie;
+
+	afterEach( () => {
+		Object.defineProperty( window.document, 'cookie', {
+			writable: true,
+			value: originalDocumentCookie,
+		} );
+	} );
+
 	test( 'should skip WooPay returns true if cookie is set', () => {
 		Object.defineProperty( window.document, 'cookie', {
 			writable: true,
