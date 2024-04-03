67482b6e35528369c8d9be780955c50c2b752a3c
Nagesh Pai <nagesh.pai@automattic.com>
2024-04-03 15:35:27 +0530

    Add individual PaymentsDataHighlight block to parent

--- client/components/payment-activity/payments-data-highlights.tsx
+++ client/components/payment-activity/payments-data-highlights.tsx
@@ -7,12 +7,37 @@ import * as React from 'react';
  * Internal dependencies.
  */
 import './style.scss';
+import PaymentsDataHighlightBlock from './payments-data-highlight-block';
+import { paymentsDataHighlightsStrings } from './strings';
 
 const PaymentsDataHighlights: React.FC = () => {
 	return (
 		<>
 			<div className="payments-data-highlights">
-				{ /* Sub components go here */ }
+				<PaymentsDataHighlightBlock
+					id="payments-data-highlights__charges"
+					title={ paymentsDataHighlightsStrings.charges }
+					currencyCode="USD"
+					amount={ 123456 }
+				/>
+				<PaymentsDataHighlightBlock
+					id="payments-data-highlights__refunds"
+					title={ paymentsDataHighlightsStrings.refunds }
+					currencyCode="USD"
+					amount={ 123456 }
+				/>
+				<PaymentsDataHighlightBlock
+					id="payments-data-highlights__disputes"
+					title={ paymentsDataHighlightsStrings.disputes }
+					currencyCode="USD"
+					amount={ 123456 }
+				/>
+				<PaymentsDataHighlightBlock
+					id="payments-data-highlights__fees"
+					title={ paymentsDataHighlightsStrings.fees }
+					currencyCode="USD"
+					amount={ 123456 }
+				/>
 			</div>
 		</>
 	);
