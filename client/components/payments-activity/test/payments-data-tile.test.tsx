221c0a7a11f4fa1505c7bb348e57d19ce98edd83
Nagesh Pai <nagesh.pai@automattic.com>
2024-04-09 11:41:47 +0530

    Add test for payments data tile

---
+++ client/components/payments-activity/test/payments-data-tile.test.tsx
@@ -0,0 +1,87 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+
+/**
+ * Internal dependencies
+ */
+import PaymentsDataTile from '../payments-data-tile';
+
+declare const global: {
+	wcpaySettings: {
+		accountDefaultCurrency: string;
+		zeroDecimalCurrencies: string[];
+		currencyData: Record< string, any >;
+		connect: {
+			country: string;
+		};
+	};
+};
+
+describe( 'PaymentsDataTile', () => {
+	global.wcpaySettings = {
+		accountDefaultCurrency: 'USD',
+		zeroDecimalCurrencies: [],
+		connect: {
+			country: 'US',
+		},
+		currencyData: {
+			US: {
+				code: 'USD',
+				symbol: '$',
+				symbolPosition: 'left',
+				thousandSeparator: ',',
+				decimalSeparator: '.',
+				precision: 2,
+			},
+		},
+	};
+
+	test( 'renders label correctly', () => {
+		const label = 'Total Payments';
+		render(
+			<PaymentsDataTile
+				id="total-payments"
+				currencyCode="USD"
+				label={ label }
+			/>
+		);
+		const labelElement = screen.getByText( label );
+		expect( labelElement ).toBeInTheDocument();
+	} );
+
+	test( 'renders amount correctly', () => {
+		const amount = 10000;
+		const currencyCode = 'USD';
+		render(
+			<PaymentsDataTile
+				id="charges-test-tile"
+				label="Charges"
+				amount={ amount }
+				currencyCode={ currencyCode }
+			/>
+		);
+		const amountElement = screen.getByText( '$100.00' );
+		expect( amountElement ).toBeInTheDocument();
+	} );
+
+	test( 'renders report link correctly', () => {
+		const reportLink = 'https://example.com/report';
+		render(
+			<PaymentsDataTile
+				id="charges-test-tile"
+				label="Charges"
+				amount={ 10000 }
+				currencyCode="USD"
+				reportLink={ reportLink }
+			/>
+		);
+		const reportLinkElement = screen.getByRole( 'link', {
+			name: 'View report',
+		} );
+		expect( reportLinkElement ).toBeInTheDocument();
+		expect( reportLinkElement ).toHaveAttribute( 'href', reportLink );
+	} );
+} );
