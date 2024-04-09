b3d9a7fc84e30a66306d7707adc03c067581e887
Timur Karimov <timurkarimov@timurs-macbook-pro.home>
2024-04-09 10:47:15 +0200

    make sure all types (CC + APMs + PRBs work)

--- includes/admin/class-wc-rest-payments-settings-controller.php
+++ includes/admin/class-wc-rest-payments-settings-controller.php
@@ -453,56 +453,63 @@ class WC_REST_Payments_Settings_Controller extends WC_Payments_REST_Controller {
 	private function find_duplicates() {
 		$keywords = [
 			// Credit card.
-			'credit_card'          => CC_Payment_Method::PAYMENT_METHOD_STRIPE_ID,
-			'creditcard'           => CC_Payment_Method::PAYMENT_METHOD_STRIPE_ID,
-			'cc'                   => CC_Payment_Method::PAYMENT_METHOD_STRIPE_ID,
-			'card'                 => CC_Payment_Method::PAYMENT_METHOD_STRIPE_ID,
-			'woocommerce_payments' => CC_Payment_Method::PAYMENT_METHOD_STRIPE_ID,
-			'stripe'               => CC_Payment_Method::PAYMENT_METHOD_STRIPE_ID,
+			'credit_card' => CC_Payment_Method::PAYMENT_METHOD_STRIPE_ID,
+			'creditcard'  => CC_Payment_Method::PAYMENT_METHOD_STRIPE_ID,
+			'cc'          => CC_Payment_Method::PAYMENT_METHOD_STRIPE_ID,
+			'card'        => CC_Payment_Method::PAYMENT_METHOD_STRIPE_ID,
 
 			// Google Pay/Apple Pay.
-			'apple_pay'            => 'apple_pay_google_pay',
-			'applepay'             => 'apple_pay_google_pay',
-			'google_pay'           => 'apple_pay_google_pay',
-			'googlepay'            => 'apple_pay_google_pay',
+			'apple_pay'   => 'apple_pay_google_pay',
+			'applepay'    => 'apple_pay_google_pay',
+			'google_pay'  => 'apple_pay_google_pay',
+			'googlepay'   => 'apple_pay_google_pay',
 
 			// APMs including BNPLs.
-			'bancontact'           => Bancontact_Payment_Method::PAYMENT_METHOD_STRIPE_ID,
-			'sepa'                 => Sepa_Payment_Method::PAYMENT_METHOD_STRIPE_ID,
-			'giropay'              => Giropay_Payment_Method::PAYMENT_METHOD_STRIPE_ID,
-			'sofort'               => Sofort_Payment_Method::PAYMENT_METHOD_STRIPE_ID,
-			'p24'                  => P24_Payment_Method::PAYMENT_METHOD_STRIPE_ID,
-			'przelewy24'           => P24_Payment_Method::PAYMENT_METHOD_STRIPE_ID,
-			'ideal'                => Ideal_Payment_Method::PAYMENT_METHOD_STRIPE_ID,
-			'becs'                 => Becs_Payment_Method::PAYMENT_METHOD_STRIPE_ID,
-			'eps'                  => Eps_Payment_Method::PAYMENT_METHOD_STRIPE_ID,
-			'affirm'               => Affirm_Payment_Method::PAYMENT_METHOD_STRIPE_ID,
-			'afterpay'             => Afterpay_Payment_Method::PAYMENT_METHOD_STRIPE_ID,
-			'clearpay'             => Afterpay_Payment_Method::PAYMENT_METHOD_STRIPE_ID,
-			'klarna'               => Klarna_Payment_Method::PAYMENT_METHOD_STRIPE_ID,
+			'bancontact'  => Bancontact_Payment_Method::PAYMENT_METHOD_STRIPE_ID,
+			'sepa'        => Sepa_Payment_Method::PAYMENT_METHOD_STRIPE_ID,
+			'giropay'     => Giropay_Payment_Method::PAYMENT_METHOD_STRIPE_ID,
+			'sofort'      => Sofort_Payment_Method::PAYMENT_METHOD_STRIPE_ID,
+			'p24'         => P24_Payment_Method::PAYMENT_METHOD_STRIPE_ID,
+			'przelewy24'  => P24_Payment_Method::PAYMENT_METHOD_STRIPE_ID,
+			'ideal'       => Ideal_Payment_Method::PAYMENT_METHOD_STRIPE_ID,
+			'becs'        => Becs_Payment_Method::PAYMENT_METHOD_STRIPE_ID,
+			'eps'         => Eps_Payment_Method::PAYMENT_METHOD_STRIPE_ID,
+			'affirm'      => Affirm_Payment_Method::PAYMENT_METHOD_STRIPE_ID,
+			'afterpay'    => Afterpay_Payment_Method::PAYMENT_METHOD_STRIPE_ID,
+			'clearpay'    => Afterpay_Payment_Method::PAYMENT_METHOD_STRIPE_ID,
+			'klarna'      => Klarna_Payment_Method::PAYMENT_METHOD_STRIPE_ID,
 		];
 
 		$gateways_qualified_by_duplicates_detector = [];
 		$gateways                                  = WC()->payment_gateways()->payment_gateways;
 
 		foreach ( $gateways as $gateway ) {
-			foreach ( $keywords as $keyword => $stripe_id ) {
-				// Card + APMs (incl. BNPLs).
-				if ( 'yes' === $gateway->enabled && strpos( $gateway->id, $keyword ) !== false ) {
-					$gateways_qualified_by_duplicates_detector[ $stripe_id ][] = $gateway->id;
-
-					// WooPayments PRB.
-					if ( 'woocommerce_payments' === $gateway->id && 'yes' === $gateway->get_option( 'payment_request' ) ) {
-						$gateways_qualified_by_duplicates_detector['apple_pay_google_pay'][] = $gateway->id;
+			if ( 'yes' === $gateway->enabled ) {
+				foreach ( $keywords as $keyword => $stripe_id ) {
+					// Card + APMs (incl. BNPLs).
+					if ( strpos( $gateway->id, $keyword ) !== false ) {
+						$gateways_qualified_by_duplicates_detector[ $stripe_id ][] = $gateway->id;
+						break;
 					}
 
-					// Stripe PRB.
-					if ( 'stripe' === $gateway->id && 'yes' === $gateway->get_option( 'payment_request' ) ) {
-						$gateways_qualified_by_duplicates_detector['apple_pay_google_pay'][] = $gateway->id;
+					if ( 'woocommerce_payments' === $gateway->id ) {
+						$gateways_qualified_by_duplicates_detector[ CC_Payment_Method::PAYMENT_METHOD_STRIPE_ID ][] = $gateway->id;
+
+						// WooPayments PRBs.
+						if ( 'yes' === $gateway->get_option( 'payment_request' ) ) {
+							$gateways_qualified_by_duplicates_detector['apple_pay_google_pay'][] = $gateway->id;
+						}
+						break;
 					}
 
-					break;
+					if ( 'stripe' === $gateway->id ) {
+						$gateways_qualified_by_duplicates_detector[ CC_Payment_Method::PAYMENT_METHOD_STRIPE_ID ][] = $gateway->id;
+						break;
+					}
 				}
+			}
+			if ( 'stripe' === $gateway->id && 'yes' === $gateway->get_option( 'payment_request' ) ) {
+				$gateways_qualified_by_duplicates_detector['apple_pay_google_pay'][] = $gateway->id;
 			}
 		}
 
