33499d00f5287b9fc67ee7f005035b832341ae2a
Naman Malhotra <naman03malhotra@gmail.com>
2024-04-10 14:28:41 +0530

    TSify and integrated data

--- client/components/payments-activity/payments-activity-data.tsx
+++ client/components/payments-activity/payments-activity-data.tsx
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import * as React from 'react';
+import moment from 'moment';
 import { __ } from '@wordpress/i18n';
 import HelpOutlineIcon from 'gridicons/dist/help-outline';
 
@@ -10,17 +11,53 @@ import HelpOutlineIcon from 'gridicons/dist/help-outline';
  */
 import PaymentsDataTile from './payments-data-tile';
 import { ClickTooltip } from '../tooltip';
+import { usePaymentActivityData } from 'wcpay/data';
+
 import { getAdminUrl } from 'wcpay/utils';
 import './style.scss';
 
+interface DateRange {
+	date_start: string;
+	date_end: string;
+}
+
+interface PaymentsActivityData {
+	paymentActivityData: {
+		total_payments_volume: number;
+		charges: number;
+		fees: number;
+		disputes: number;
+		refunds: number;
+	};
+	isLoading: boolean;
+}
+
+const getDateRange = (): DateRange => {
+	return {
+		date_start: moment()
+			.subtract( 7, 'd' )
+			.format( 'YYYY-MM-DD\\THH:mm:ss' ),
+		date_end: moment().format( 'YYYY-MM-DD\\THH:mm:ss' ),
+	};
+};
+
 const PaymentsActivityData: React.FC = () => {
+	const {
+		paymentActivityData: {
+			total_payments_volume: totalPaymentsVolume,
+			charges,
+			fees,
+			disputes,
+			refunds,
+		},
+	} = usePaymentActivityData( getDateRange() ) as PaymentsActivityData;
+
 	return (
 		<div className="wcpay-payments-activity-data">
 			<PaymentsDataTile
 				id="wcpay-payments-activity-data__total-payments-volume"
 				label={ __( 'Total payments volume', 'woocommerce-payments' ) }
 				currencyCode="EUR"
-				amount={ 156373 }
 				tooltip={
 					<ClickTooltip
 						className="total-payments-volume__tooltip"
@@ -35,6 +72,7 @@ const PaymentsActivityData: React.FC = () => {
 						) }
 					/>
 				}
+				amount={ totalPaymentsVolume }
 				reportLink={ getAdminUrl( {
 					page: 'wc-admin',
 					path: '/payments/transactions',
@@ -45,7 +83,6 @@ const PaymentsActivityData: React.FC = () => {
 					id="wcpay-payments-data-highlights__charges"
 					label={ __( 'Charges', 'woocommerce-payments' ) }
 					currencyCode="EUR"
-					amount={ 314300 }
 					tooltip={
 						<ClickTooltip
 							className="payments-data-highlights__charges__tooltip"
@@ -57,6 +94,7 @@ const PaymentsActivityData: React.FC = () => {
 							content={ __( 'test charge content' ) }
 						/>
 					}
+					amount={ charges }
 					reportLink={ getAdminUrl( {
 						page: 'wc-admin',
 						path: '/payments/transactions',
@@ -68,7 +106,7 @@ const PaymentsActivityData: React.FC = () => {
 					id="wcpay-payments-data-highlights__refunds"
 					label={ __( 'Refunds', 'woocommerce-payments' ) }
 					currencyCode="EUR"
-					amount={ 153200 }
+					amount={ refunds }
 					reportLink={ getAdminUrl( {
 						page: 'wc-admin',
 						path: '/payments/transactions',
@@ -80,7 +118,7 @@ const PaymentsActivityData: React.FC = () => {
 					id="wcpay-payments-data-highlights__disputes"
 					label={ __( 'Disputes', 'woocommerce-payments' ) }
 					currencyCode="EUR"
-					amount={ 4727 }
+					amount={ disputes }
 					reportLink={ getAdminUrl( {
 						page: 'wc-admin',
 						path: '/payments/disputes',
@@ -91,7 +129,6 @@ const PaymentsActivityData: React.FC = () => {
 					id="wcpay-payments-data-highlights__fees"
 					label={ __( 'Fees', 'woocommerce-payments' ) }
 					currencyCode="EUR"
-					amount={ 9429 }
 					tooltip={
 						<ClickTooltip
 							className="payments-data-highlights__fees__tooltip"
@@ -106,6 +143,7 @@ const PaymentsActivityData: React.FC = () => {
 							) }
 						/>
 					}
+					amount={ fees }
 				/>
 			</div>
 		</div>
