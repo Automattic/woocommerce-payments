410c92368b052deea18f4be3eb3f4d9d1ae38d28
Naman Malhotra <naman03malhotra@gmail.com>
2024-04-15 15:26:35 +0530

    review changes, snapshot update

--- client/components/payments-activity/payments-activity-data.tsx
+++ client/components/payments-activity/payments-activity-data.tsx
@@ -40,13 +40,14 @@ const PaymentsActivityData: React.FC = () => {
 	const fees = paymentActivityData?.fees ?? 0;
 	const disputes = paymentActivityData?.disputes ?? 0;
 	const refunds = paymentActivityData?.refunds ?? 0;
+	const { storeCurrency } = wcpaySettings;
 
 	return (
 		<div className="wcpay-payments-activity-data">
 			<PaymentsDataTile
 				id="wcpay-payments-activity-data__total-payments-volume"
 				label={ __( 'Total payments volume', 'woocommerce-payments' ) }
-				currencyCode="EUR"
+				currencyCode={ storeCurrency }
 				tooltip={
 					<ClickTooltip
 						className="total-payments-volume__tooltip"
@@ -72,7 +73,7 @@ const PaymentsActivityData: React.FC = () => {
 				<PaymentsDataTile
 					id="wcpay-payments-data-highlights__charges"
 					label={ __( 'Charges', 'woocommerce-payments' ) }
-					currencyCode="EUR"
+					currencyCode={ storeCurrency }
 					tooltip={
 						<ClickTooltip
 							className="payments-data-highlights__charges__tooltip"
@@ -96,7 +97,7 @@ const PaymentsActivityData: React.FC = () => {
 				<PaymentsDataTile
 					id="wcpay-payments-data-highlights__refunds"
 					label={ __( 'Refunds', 'woocommerce-payments' ) }
-					currencyCode="EUR"
+					currencyCode={ storeCurrency }
 					amount={ refunds }
 					reportLink={ getAdminUrl( {
 						page: 'wc-admin',
@@ -109,7 +110,7 @@ const PaymentsActivityData: React.FC = () => {
 				<PaymentsDataTile
 					id="wcpay-payments-data-highlights__disputes"
 					label={ __( 'Disputes', 'woocommerce-payments' ) }
-					currencyCode="EUR"
+					currencyCode={ storeCurrency }
 					amount={ disputes }
 					reportLink={ getAdminUrl( {
 						page: 'wc-admin',
@@ -121,7 +122,7 @@ const PaymentsActivityData: React.FC = () => {
 				<PaymentsDataTile
 					id="wcpay-payments-data-highlights__fees"
 					label={ __( 'Fees', 'woocommerce-payments' ) }
-					currencyCode="EUR"
+					currencyCode={ storeCurrency }
 					tooltip={
 						<ClickTooltip
 							className="payments-data-highlights__fees__tooltip"
