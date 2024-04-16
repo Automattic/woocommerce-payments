e5bd64321a34e3283c08a0ee5f726ebbdcd8e41f
Naman Malhotra <naman03malhotra@gmail.com>
2024-04-16 16:18:16 +0530

    review changes

--- client/components/payments-activity/payments-activity-data.tsx
+++ client/components/payments-activity/payments-activity-data.tsx
@@ -13,7 +13,7 @@ import PaymentsDataTile from './payments-data-tile';
 import { ClickTooltip } from '../tooltip';
 import { usePaymentActivityData } from 'wcpay/data';
 import { getAdminUrl } from 'wcpay/utils';
-import { DateRange, PaymentsActivityData } from './types';
+import type { DateRange } from './types';
 
 import './style.scss';
 
