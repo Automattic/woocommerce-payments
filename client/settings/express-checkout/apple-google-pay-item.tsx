64260da154e06bebe1bf8eb728a91cecf1521dc6
Timur Karimov <timurkarimov@timurs-macbook-pro.home>
2024-04-18 12:30:04 +0200

    remove unused import

--- client/settings/express-checkout/apple-google-pay-item.tsx
+++ client/settings/express-checkout/apple-google-pay-item.tsx
@@ -4,7 +4,7 @@
 import { __ } from '@wordpress/i18n';
 import { Button, CheckboxControl } from '@wordpress/components';
 import interpolateComponents from '@automattic/interpolate-components';
-import React, { useContext, useState } from 'react';
+import React, { useContext } from 'react';
 
 /**
  * Internal dependencies
