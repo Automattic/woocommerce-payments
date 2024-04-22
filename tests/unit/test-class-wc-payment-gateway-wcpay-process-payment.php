37e4d9f7d4229113b4538b941a9f3377026574d0
Guilherme Pressutto <gpressutto5@gmail.com>
2024-04-22 13:50:38 -0300

    Patch: Fix subscription renewals exceptions by reverting billing address related PRs (#8689)
    Co-authored-by: Timur Karimov <timur.karimow.95@gmail.com>
    Co-authored-by: Timur Karimov <timurkarimov@timurs-macbook-pro.home>
    Co-authored-by: Daniel Mallory <mal@Daniels-MacBook-Pro.local>
    Co-authored-by: Daniel Mallory <daniel.mallory@automattic.com>

--- tests/unit/test-class-wc-payment-gateway-wcpay-process-payment.php
+++ tests/unit/test-class-wc-payment-gateway-wcpay-process-payment.php
@@ -1209,8 +1209,17 @@ class WC_Payment_Gateway_WCPay_Process_Payment_Test extends WCPAY_UnitTestCase {
 			->willReturn( $intent );
 
 		$this->mock_action_scheduler_service
-			->expects( $this->never() )
-			->method( 'schedule_job' );
+			->expects( $this->once() )
+			->method( 'schedule_job' )
+			->with(
+				$this->anything(),
+				WC_Payment_Gateway_WCPay::UPDATE_SAVED_PAYMENT_METHOD,
+				[
+					'payment_method' => 'pm_mock',
+					'order_id'       => $order_id,
+					'is_test_mode'   => false,
+				]
+			);
 
 		$this->mock_wcpay_gateway->process_payment( $order_id );
 	}
