7205001cf1872655e1af7315871d2496e278f152
Brett Shumaker <brettshumaker@gmail.com>
2024-04-29 11:24:59 -0400

    Remove extraneous str_replace

--- includes/class-wc-payments-order-success-page.php
+++ includes/class-wc-payments-order-success-page.php
@@ -140,7 +140,7 @@ class WC_Payments_Order_Success_Page {
 
 		ob_start();
 		?>
-		<div class="wc-payment-gateway-method-logo-wrapper wc-payment-bnpl-logo <?php echo str_replace( 'woocommerce_payments_', '', $payment_method->get_id() ); ?>">
+		<div class="wc-payment-gateway-method-logo-wrapper wc-payment-bnpl-logo <?php echo $payment_method->get_id(); ?>">
 			<img alt="<?php echo $payment_method->get_title(); ?>" src="<?php echo esc_url_raw( $method_logo_url ); ?>">
 		</div>
 		<?php
