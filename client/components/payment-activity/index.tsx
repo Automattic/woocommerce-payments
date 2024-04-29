1cdf4f9f26159b6a3d04dfe24bd587b371759864
Shendy <73803630+shendy-a8c@users.noreply.github.com>
2024-04-29 19:09:00 +0700

    Add preset date ranges dropdown filter on payment activity widget.

--- client/components/payment-activity/index.tsx
+++ client/components/payment-activity/index.tsx
@@ -2,7 +2,13 @@
  * External dependencies
  */
 import * as React from 'react';
-import { Card, CardBody, CardHeader } from '@wordpress/components';
+import { useState } from 'react';
+import {
+	Card,
+	CardBody,
+	CardHeader,
+	SelectControl,
+} from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import interpolateComponents from '@automattic/interpolate-components';
 
@@ -22,12 +28,67 @@ const PaymentActivity: React.FC = () => {
 	const isOverviewSurveySubmitted =
 		wcpaySettings.isOverviewSurveySubmitted ?? false;
 
+	const [ presetDateRangeState, setPresetDateRangeState ] = useState(
+		'last_7_days'
+	);
+	const presetDateRanges = [
+		{
+			value: 'today',
+			label: 'Today',
+		},
+		{
+			value: 'last_7_days',
+			label: 'Last 7 days',
+		},
+		{
+			value: 'last_4_weeks',
+			label: 'Last 4 weeks',
+		},
+		{
+			value: 'last_3_months',
+			label: 'Last 3 months',
+		},
+		{
+			value: 'last_12_months',
+			label: 'Last 12 months',
+		},
+		{
+			value: 'month_to_date',
+			label: 'Month to date',
+		},
+		{
+			value: 'quarter_to_date',
+			label: 'Quarter to date',
+		},
+		{
+			value: 'year_to_date',
+			label: 'Year to date',
+		},
+		{
+			value: 'all_time',
+			label: 'All time',
+		},
+	];
 	return (
 		<Card>
 			<CardHeader>
 				{ __( 'Your payment activity', 'woocommerce-payments' ) }
 
-				{ hasAtLeastOnePayment && <>{ /* Filters go here */ }</> }
+				{ hasAtLeastOnePayment && (
+					<>
+						{
+							<SelectControl
+								value={ presetDateRangeState }
+								onChange={ ( newPresetDateRange ) =>
+									setPresetDateRangeState(
+										newPresetDateRange
+									)
+								}
+								options={ presetDateRanges }
+							/>
+						}
+					</>
+				) }
 			</CardHeader>
 			<CardBody className="wcpay-payment-activity__card__body">
 				{ hasAtLeastOnePayment ? (
