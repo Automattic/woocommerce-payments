7bc82f9e68be114d5cc2c5ad6502a7a2a4f5bb9c
frosso <francesco.rosso@automattic.com>
2024-05-06 16:29:40 +0200

    JSDocs

--- client/tokenized-payment-request/transformers/wc-to-stripe.js
+++ client/tokenized-payment-request/transformers/wc-to-stripe.js
@@ -3,8 +3,14 @@
  */
 import { __ } from '@wordpress/i18n';
 
+/**
+ * Transforms the data from the Store API Cart response to `displayItems` for the Stripe PRB.
+ * See https://docs.stripe.com/js/appendix/payment_item_object for the data structure
+ *
+ * @param {Object} cartData Store API Cart response object.
+ * @return {{pending: boolean, label: string, amount: integer}} `displayItems` for Stripe.
+ */
 export const transformCartDataForDisplayItems = ( cartData ) => {
-	// see https://docs.stripe.com/js/appendix/payment_item_object for the data structure
 	const displayItems = cartData.items.map( ( item ) => ( {
 		amount: parseInt( item.prices.price, 10 ),
 		// TODO: should we also add variation attributes?
@@ -33,6 +39,12 @@ export const transformCartDataForDisplayItems = ( cartData ) => {
 	return displayItems;
 };
 
+/**
+ * Transforms the data from the Store API Cart response to `shippingOptions` for the Stripe PRB.
+ *
+ * @param {Object} cartData Store API Cart response object.
+ * @return {{id: string, label: string, amount: integer, detail: string}} `shippingOptions` for Stripe.
+ */
 export const transformCartDataForShippingOptions = ( cartData ) =>
 	cartData.shipping_rates[ 0 ].shipping_rates.map( ( rate ) => ( {
 		id: rate.rate_id,
