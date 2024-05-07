fcbcfaf668b351b8871c0ff6af5f940ab0c2e52d
Eric Jinks <3147296+Jinksi@users.noreply.github.com>
2024-05-07 15:15:58 +1000

    Separate welcome message from account balance card

--- client/overview/index.js
+++ client/overview/index.js
@@ -235,40 +235,35 @@ const OverviewPage = () => {
 			{ showConnectionSuccess && <ConnectionSuccessNotice /> }
 			{ ! accountRejected && ! accountUnderReview && (
 				<ErrorBoundary>
-					<>
-						{ showTaskList ? (
-							<>
-								<Card>
-									<Welcome />
-									<ErrorBoundary>
-										<TaskList
-											tasks={ tasks }
-											overviewTasksVisibility={
-												overviewTasksVisibility
-											}
-										/>
-									</ErrorBoundary>
-								</Card>
-								<Card>
-									<AccountBalances />
-								</Card>
-							</>
-						) : (
-							<Card>
-								<Welcome />
-								<AccountBalances />
-							</Card>
-						) }
-						{
-							/* Show Payment Activity widget only when feature flag is set. To be removed before go live */
-							isPaymentOverviewWidgetEnabled && (
-								<ErrorBoundary>
-									<PaymentActivity />
-								</ErrorBoundary>
-							)
-						}
-						<DepositsOverview />
-					</>
+					<Welcome />
+
+					{ showTaskList && (
+						<Card>
+							<ErrorBoundary>
+								<TaskList
+									tasks={ tasks }
+									overviewTasksVisibility={
+										overviewTasksVisibility
+									}
+								/>
+							</ErrorBoundary>
+						</Card>
+					) }
+
+					<Card>
+						<AccountBalances />
+					</Card>
+
+					{
+						/* Show Payment Activity widget only when feature flag is set. To be removed before go live */
+						isPaymentOverviewWidgetEnabled && (
+							<ErrorBoundary>
+								<PaymentActivity />
+							</ErrorBoundary>
+						)
+					}
+
+					<DepositsOverview />
 				</ErrorBoundary>
 			) }
 			<ErrorBoundary>
