f45d41d6f0fbf62cb342a87a04aa1c68735cd13c
César Costa <costa.cesar.a@hotmail.com>
2024-05-28 18:32:58 -0300

    Decrypt email if it's passed as an array

--- includes/woopay/class-woopay-session.php
+++ includes/woopay/class-woopay-session.php
@@ -385,6 +385,10 @@ class WooPay_Session {
 	 */
 	private static function get_user_email( $user ) {
 		if ( ! empty( $_POST['email'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification
+			if ( is_array( $_POST['email'] ) ) { // phpcs:ignore WordPress.Security.NonceVerification
+				 // phpcs:ignore WordPress.Security.NonceVerification, WordPress.Security.ValidatedSanitizedInput.InputNotSanitized, WordPress.Security.ValidatedSanitizedInput.MissingUnslash
+				return sanitize_email( wp_unslash( WooPay_Utilities::decrypt_signed_data( $_POST['email'] ) ) ) ?? '';
+			}
 			return sanitize_email( wp_unslash( $_POST['email'] ) ); // phpcs:ignore WordPress.Security.NonceVerification
 		}
 
