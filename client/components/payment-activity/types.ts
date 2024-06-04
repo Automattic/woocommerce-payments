6f2fdbc6646f820931e12d9f578146f7e00e85b3
Nagesh Pai <nagesh.pai@automattic.com>
2024-06-04 20:04:56 +0530

    Remove unwanted external types file for now

--- client/components/payment-activity/types.ts
+++
@@ -1,7 +0,0 @@
-/* eslint-disable wpcalypso/import-docblock */
-import { Moment } from 'moment';
-
-export interface DateRangeProps {
-	start: Moment | undefined; // Start date
-	end: Moment | undefined; // End date
-}
