a6eb277c656fd909b425ac18acdafe0d1b3234a8
Nagesh Pai <nagesh.pai@automattic.com>
2024-06-05 17:46:55 +0530

    Initial changes copied from PR - 8730

---
+++ client/components/payment-activity/date-range.tsx
@@ -0,0 +1,25 @@
+/**
+ * External dependencies
+ */
+import * as React from 'react';
+
+/**
+ * Internal dependencies
+ */
+
+import './style.scss';
+
+interface Props {
+	start: string;
+	end: string;
+}
+
+const DateRange: React.FC< Props > = ( { start, end } ) => {
+	return (
+		<>
+			{ start } - { end }
+		</>
+	);
+};
+
+export default DateRange;
--- client/components/payment-activity/index.tsx
+++ client/components/payment-activity/index.tsx
@@ -2,7 +2,14 @@
  * External dependencies
  */
 import * as React from 'react';
-import { Card, CardBody, CardHeader } from '@wordpress/components';
+import { useState } from 'react';
+import {
+	Card,
+	CardBody,
+	CardHeader,
+	Flex,
+	SelectControl,
+} from '@wordpress/components';
 import { __ } from '@wordpress/i18n';
 import interpolateComponents from '@automattic/interpolate-components';
 import moment from 'moment';
@@ -11,26 +18,21 @@ import moment from 'moment';
  * Internal dependencies
  */
 
+import DateRange from './date-range';
 import EmptyStateAsset from 'assets/images/payment-activity-empty-state.svg?asset';
 import PaymentActivityDataComponent from './payment-activity-data';
 import Survey from './survey';
 import { WcPayOverviewSurveyContextProvider } from './survey/context';
 import { usePaymentActivityData } from 'wcpay/data';
-import type { DateRange } from './types';
 import './style.scss';
 
-/**
- * This will be replaces in the future with a dynamic date range picker.
- */
-const getDateRange = (): DateRange => {
-	return {
-		// Subtract 7 days from the current date.
-		date_start: moment()
-			.subtract( 7, 'd' )
-			.format( 'YYYY-MM-DD\\THH:mm:ss' ),
-		date_end: moment().format( 'YYYY-MM-DD\\THH:mm:ss' ),
-	};
-};
+interface DateRangeProps {
+	start: moment.Moment | undefined;
+	end: moment.Moment | undefined;
+}
+
+const { lifetimeTPV } = wcpaySettings;
+const hasAtLeastOnePayment = lifetimeTPV > 0;
 
 const PaymentActivityEmptyState: React.FC = () => (
 	<Card>
@@ -65,8 +67,158 @@ const PaymentActivity: React.FC = () => {
 	const isOverviewSurveySubmitted =
 		wcpaySettings.isOverviewSurveySubmitted ?? false;
 
+	const yesterdayEndOfDay = moment()
+		.clone()
+		.subtract( 1, 'd' )
+		.set( { hour: 23, minute: 59, second: 59, millisecond: 0 } );
+
+	const todayEndOfDay = moment()
+		.clone()
+		.set( { hour: 23, minute: 59, second: 59, millisecond: 0 } );
+
+	const [ dateRangeState, setDateRangeState ] = useState( {
+		start: moment().clone().subtract( 7, 'd' ),
+		end: yesterdayEndOfDay,
+	} as DateRangeProps );
+
+	const [ dateRangePresetState, setDateRangePresetState ] = useState(
+		'last_7_days'
+	);
+
+	const dateRangePresets = [
+		{
+			value: 'today',
+			label: 'Today',
+		},
+		{
+			value: 'last_7_days',
+			label: 'Last 7 days',
+		},
+		{
+			value: 'last_4_weeks',
+			label: 'Last 4 weeks',
+		},
+		{
+			value: 'last_3_months',
+			label: 'Last 3 months',
+		},
+		{
+			value: 'last_12_months',
+			label: 'Last 12 months',
+		},
+		{
+			value: 'month_to_date',
+			label: 'Month to date',
+		},
+		{
+			value: 'quarter_to_date',
+			label: 'Quarter to date',
+		},
+		{
+			value: 'year_to_date',
+			label: 'Year to date',
+		},
+		{
+			value: 'all_time',
+			label: 'All time',
+		},
+	];
+
+	const dateRangePresetOnChangeHandler = ( newDateRangePreset: string ) => {
+		let start, end;
+		const now = moment();
+		setDateRangePresetState( newDateRangePreset );
+
+		switch ( newDateRangePreset ) {
+			case 'today': {
+				start = now
+					.clone()
+					.set( { hour: 0, minute: 0, second: 0, millisecond: 0 } );
+				end = yesterdayEndOfDay;
+				break;
+			}
+			case 'last_7_days': {
+				start = now
+					.clone()
+					.subtract( 7, 'd' )
+					.set( { hour: 0, minute: 0, second: 0, millisecond: 0 } );
+				end = yesterdayEndOfDay;
+				break;
+			}
+			case 'last_4_weeks': {
+				start = now
+					.clone()
+					.subtract( 4, 'w' )
+					.set( { hour: 0, minute: 0, second: 0, millisecond: 0 } );
+				end = yesterdayEndOfDay;
+				break;
+			}
+			case 'last_3_months': {
+				start = now
+					.clone()
+					.subtract( 3, 'm' )
+					.set( { hour: 0, minute: 0, second: 0, millisecond: 0 } );
+				end = yesterdayEndOfDay;
+				break;
+			}
+			case 'last_12_months': {
+				start = now
+					.clone()
+					.subtract( 12, 'm' )
+					.set( { hour: 0, minute: 0, second: 0, millisecond: 0 } );
+				end = yesterdayEndOfDay;
+				break;
+			}
+			case 'month_to_date': {
+				start = now.clone().set( {
+					date: 1,
+					hour: 0,
+					minute: 0,
+					second: 0,
+					millisecond: 0,
+				} );
+				end = todayEndOfDay;
+				break;
+			}
+			case 'quarter_to_date': {
+				start = now.clone().set( {
+					month: Math.floor( now.month() / 3 ) * 3,
+					date: 1,
+					hour: 0,
+					minute: 0,
+					second: 0,
+					millisecond: 0,
+				} );
+				end = todayEndOfDay;
+				break;
+			}
+			case 'year_to_date': {
+				start = now.clone().set( {
+					month: 0,
+					date: 1,
+					hour: 0,
+					minute: 0,
+					second: 0,
+					millisecond: 0,
+				} );
+				end = todayEndOfDay;
+				break;
+			}
+			case 'all_time':
+				// TODO
+				break;
+		}
+
+		setDateRangeState( { start, end } );
+	};
+
 	const { paymentActivityData, isLoading } = usePaymentActivityData( {
-		...getDateRange(),
+		date_start: dateRangeState.start
+			? dateRangeState.start.format( 'YYYY-MM-DDTHH:mm:ss' )
+			: '',
+		date_end: dateRangeState.end
+			? dateRangeState.end.format( 'YYYY-MM-DDTHH:mm:ss' )
+			: '',
 		timezone: moment( new Date() ).format( 'Z' ),
 	} );
 
@@ -83,7 +235,34 @@ const PaymentActivity: React.FC = () => {
 		<Card>
 			<CardHeader>
 				{ __( 'Your payment activity', 'woocommerce-payments' ) }
-				{ /* Filters go here */ }
+
+				{ hasAtLeastOnePayment && (
+					<>
+						<Flex className="wcpay-payment-activity-filters">
+							<SelectControl
+								value={ dateRangePresetState }
+								onChange={ dateRangePresetOnChangeHandler }
+								options={ dateRangePresets }
+							/>
+							<DateRange
+								start={
+									dateRangeState.start
+										? dateRangeState.start.format(
+												'MMMM D'
+										  )
+										: ''
+								}
+								end={
+									dateRangeState.end
+										? dateRangeState.end.format(
+												'MMMM D, YYYY'
+										  )
+										: ''
+								}
+							/>
+						</Flex>
+					</>
+				) }
 			</CardHeader>
 			<CardBody className="wcpay-payment-activity__card__body">
 				<PaymentActivityDataComponent
@@ -102,9 +281,6 @@ const PaymentActivity: React.FC = () => {
 };
 
 const PaymentActivityWrapper: React.FC = () => {
-	const { lifetimeTPV } = wcpaySettings;
-	const hasAtLeastOnePayment = lifetimeTPV > 0;
-
 	if ( ! hasAtLeastOnePayment ) {
 		return <PaymentActivityEmptyState />;
 	}
