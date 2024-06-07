eee9e11729cd63f77a2b5e166af104369cd48700
Timur Karimov <timur.karimov@automattic.com>
2024-06-07 15:29:04 +0200

    improve naming of defined TS type interface II

--- client/globals.d.ts
+++ client/globals.d.ts
@@ -2,6 +2,7 @@
  * Internal dependencies
  */
 import type { MccsDisplayTreeItem, Country } from 'onboarding/types';
+import { PaymentMethodToPluginsMap } from './components/duplicate-notice';
 
 declare global {
 	const wcpaySettings: {
@@ -90,7 +91,7 @@ declare global {
 			isEligibilityModalDismissed: boolean;
 		};
 		enabledPaymentMethods: string[];
-		dismissedDuplicateNotices: { [ key: string ]: string[] };
+		dismissedDuplicateNotices: PaymentMethodToPluginsMap;
 		accountDefaultCurrency: string;
 		isFRTReviewFeatureActive: boolean;
 		frtDiscoverBannerSettings: string;
