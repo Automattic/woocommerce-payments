983890425077bda2a354751e2352c8190882826c
Nagesh Pai <4162931+nagpai@users.noreply.github.com>
2024-06-12 13:46:03 +0530

    Reporting: Add preset date selector to the Payment activity widget (#8927)
    Co-authored-by: Nagesh Pai <nagesh.pai@automattic.com>
    Co-authored-by: Jessy <jessy.pappachan@automattic.com>
    Co-authored-by: Eric Jinks <3147296+Jinksi@users.noreply.github.com>

--- client/data/payment-activity/actions.ts
+++ client/data/payment-activity/actions.ts
@@ -4,13 +4,19 @@
  * Internal Dependencies
  */
 import TYPES from './action-types';
-import { PaymentActivityData, PaymentActivityAction } from './types';
+import type {
+	PaymentActivityData,
+	PaymentActivityAction,
+	PaymentActivityQuery,
+} from './types';
 
 export function updatePaymentActivity(
-	data: PaymentActivityData
+	data: PaymentActivityData,
+	query: PaymentActivityQuery
 ): PaymentActivityAction {
 	return {
 		type: TYPES.SET_PAYMENT_ACTIVITY_DATA,
+		query,
 		data,
 	};
 }
