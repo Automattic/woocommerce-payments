195edbe8811ba689771b404b7d5c863719a66d69
deepakpathania <deepakpathania789@gmail.com>
2024-06-28 16:02:32 +0530

    Revert "Update the regex matcher for routes"
    This reverts commit 5481ff0b950ff694a504074220291f8f1b7e52a4.

--- includes/admin/class-wc-rest-payments-payment-intents-controller.php
+++ includes/admin/class-wc-rest-payments-payment-intents-controller.php
@@ -32,7 +32,7 @@ class WC_REST_Payments_Payment_Intents_Controller extends WC_Payments_REST_Contr
 	public function register_routes() {
 		register_rest_route(
 			$this->namespace,
-			'/' . $this->rest_base . '/(?P<payment_intent_id>[A-Za-z0-9_-]+)',
+			'/' . $this->rest_base . '/(?P<payment_intent_id>\w+)',
 			[
 				'methods'             => WP_REST_Server::READABLE,
 				'callback'            => [ $this, 'get_payment_intent' ],
