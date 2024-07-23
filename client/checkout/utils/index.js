385a47c123639a126450dbc6355f946f5be606e3
Hsing-yu Flowers <hsingyuc.7@gmail.com>
2024-07-23 18:00:16 -0400

    Pass product, blocks cart, classic cart and checkout on get WooPay session (#9138)

--- client/checkout/utils/index.js
+++ client/checkout/utils/index.js
@@ -13,3 +13,25 @@ export const normalizeCurrencyToMinorUnit = (
 ) => {
 	return parseInt( amount * Math.pow( 10, accuracy - minorUnit ), 10 );
 };
+
+export const getAppearanceType = () => {
+	if ( document.querySelector( '.wp-block-woocommerce-checkout' ) ) {
+		return 'blocks_checkout';
+	}
+
+	if ( document.querySelector( '.woocommerce-billing-fields' ) ) {
+		return 'shortcode_checkout';
+	}
+
+	if ( document.querySelector( '.wp-block-woocommerce-cart' ) ) {
+		return 'bnpl_cart_block';
+	}
+
+	if ( document.querySelector( '.woocommerce-cart-form' ) ) {
+		return 'bnpl_classic_cart';
+	}
+
+	if ( document.querySelector( '.wp-block-woocommerce-product-details' ) ) {
+		return 'bnpl_product_page';
+	}
+};
