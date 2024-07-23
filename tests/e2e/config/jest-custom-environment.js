035d46217162949ceefaf078af6f4ea84d0c9c2f
Daniel Guerra <daniel.guerra@automattic.com>
2024-07-23 08:56:50 -0600

    Remove unnecessary wait in custom test environment

--- tests/e2e/config/jest-custom-environment.js
+++ tests/e2e/config/jest-custom-environment.js
@@ -2,15 +2,6 @@ const { TestEnvironment } = require( 'jest-environment-puppeteer' );
 require( 'jest-circus' );
 
 class JestCustomEnvironment extends TestEnvironment {
-	async teardown() {
-		if ( this.global.page ) {
-			// Wait a few seconds before tearing down the page so we
-			// have time to take screenshots and handle other events
-			await this.global.page.waitForTimeout( 1000 );
-		}
-		await super.teardown();
-	}
-
 	async handleTestEvent( event, state ) {
 		if ( event.name === 'test_fn_failure' && this.global.page ) {
 			const testDescription = state.currentlyRunningTest.name.replace(
