3f4077a56eb9abf29d1f4543266a1aa185a51f0e
César Costa <cesar.costa@automattic.com>
2024-07-25 15:54:24 -0300

    Update comment

--- tests/e2e-pw/utils/merchant.ts
+++ tests/e2e-pw/utils/merchant.ts
@@ -63,7 +63,7 @@ export const addMulticurrencyWidget = async ( page: Page ) => {
 		).toBeEnabled();
 		await page.getByRole( 'button', { name: 'Update' } ).click();
 		// For some reason the alert "Widgets saved." doesn't show up in the testing.
-		// So we refresh the page to make sure it's really there.
+		// So we reload the page to make sure the widget has been added.
 		await page.waitForResponse( '**/wp-json/wp/v2/sidebars**' );
 		await page.reload( { waitUntil: 'load' } );
 		// Wait for all widgets to load. This is important to prevent flakiness.
