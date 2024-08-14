ef9be3d70a6fa1651de0a1919f6096efd47ab754
Vlad Olaru <vlad@pixelgrade.com>
2024-08-14 13:10:25 +0300

    Use the include WP CLI as opposed to a dedicated container

--- bin/docker-setup.sh
+++ bin/docker-setup.sh
@@ -14,34 +14,32 @@ redirect_output() {
     fi
 }
 
-# --user 33:33 forces the wordpress:cli container to use a user with the same ID as the main wordpress container. See:
-# https://hub.docker.com/_/wordpress#running-as-an-arbitrary-user
-# https://github.com/docker-library/wordpress/discussions/909#discussioncomment-9941634
-cli()
+wp_cli()
 {
 	INTERACTIVE=''
 	if [ -t 1 ] ; then
 		INTERACTIVE='-it'
 	fi
-	redirect_output docker run $INTERACTIVE --env-file default.env --rm --user 33:33 --volumes-from $WP_CONTAINER --network container:$WP_CONTAINER wordpress:cli "$@"
+
+	redirect_output docker exec $INTERACTIVE --env-file default.env --user www-data $WP_CONTAINER wp "$@"
 }
 
 set +e
 # Wait for containers to be started up before the setup.
 # The db being accessible means that the db container started and the WP has been downloaded and the plugin linked
-cli wp db check --path=/var/www/html --quiet > /dev/null
+wp_cli db check --path=/var/www/html --quiet > /dev/null
 while [[ $? -ne 0 ]]; do
 	echo "Waiting until the service is ready..."
 	sleep 5
-	cli wp db check --path=/var/www/html --quiet > /dev/null
+	wp_cli db check --path=/var/www/html --quiet > /dev/null
 done
 
 # If the plugin is already active then return early
-cli wp plugin is-active woocommerce-payments > /dev/null
+wp_cli plugin is-active woocommerce-payments > /dev/null
 if [[ $? -eq 0 ]]; then
 	set -e
 	echo
-	echo "WCPay is installed and active"
+	echo "WooPayments is installed and active"
 	echo "SUCCESS! You should now be able to access http://${SITE_URL}/wp-admin/"
 	echo "You can login by using the username and password both as 'admin'"
 	exit 0
@@ -53,11 +51,8 @@ echo
 echo "Setting up environment..."
 echo
 
-echo "Pulling the WordPress CLI docker image..."
-docker pull wordpress:cli > /dev/null
-
 echo "Setting up WordPress..."
-cli wp core install \
+wp_cli core install \
 	--path=/var/www/html \
 	--url=$SITE_URL \
 	--title=${SITE_TITLE-"WooCommerce Payments Dev"} \
@@ -67,64 +62,64 @@ cli wp core install \
 	--skip-email
 
 echo "Updating WordPress to the latest version..."
-cli wp core update --quiet
+wp_cli core update --quiet
 
 echo "Updating the WordPress database..."
-cli wp core update-db --quiet
+wp_cli core update-db --quiet
 
 echo "Configuring WordPress to work with ngrok (in order to allow creating a Jetpack-WPCOM connection)";
-cli config set DOCKER_HOST "\$_SERVER['HTTP_X_ORIGINAL_HOST'] ?? \$_SERVER['HTTP_HOST'] ?? 'localhost'" --raw
-cli config set DOCKER_REQUEST_URL "( ! empty( \$_SERVER['HTTPS'] ) ? 'https://' : 'http://' ) . DOCKER_HOST" --raw
-cli config set WP_SITEURL DOCKER_REQUEST_URL --raw
-cli config set WP_HOME DOCKER_REQUEST_URL --raw
+wp_cli config set DOCKER_HOST "\$_SERVER['HTTP_X_ORIGINAL_HOST'] ?? \$_SERVER['HTTP_HOST'] ?? 'localhost'" --raw
+wp_cli config set DOCKER_REQUEST_URL "( ! empty( \$_SERVER['HTTPS'] ) ? 'https://' : 'http://' ) . DOCKER_HOST" --raw
+wp_cli config set WP_SITEURL DOCKER_REQUEST_URL --raw
+wp_cli config set WP_HOME DOCKER_REQUEST_URL --raw
 
 echo "Enabling WordPress debug flags"
-cli config set WP_DEBUG true --raw
-cli config set WP_DEBUG_DISPLAY true --raw
-cli config set WP_DEBUG_LOG true --raw
-cli config set SCRIPT_DEBUG true --raw
+wp_cli config set WP_DEBUG true --raw
+wp_cli config set WP_DEBUG_DISPLAY true --raw
+wp_cli config set WP_DEBUG_LOG true --raw
+wp_cli config set SCRIPT_DEBUG true --raw
 
 echo "Enabling WordPress development environment (enforces Stripe testing mode)";
-cli config set WP_ENVIRONMENT_TYPE development
+wp_cli config set WP_ENVIRONMENT_TYPE development
 
 echo "Updating permalink structure"
-cli wp rewrite structure '/%postname%/'
+wp_cli rewrite structure '/%postname%/'
 
 echo "Installing and activating WooCommerce..."
-cli wp plugin install woocommerce --activate
+wp_cli plugin install woocommerce --activate
 
 echo "Installing and activating Storefront theme..."
-cli wp theme install storefront --activate
+wp_cli theme install storefront --activate
 
 echo "Adding basic WooCommerce settings..."
-cli wp option set woocommerce_store_address "60 29th Street"
-cli wp option set woocommerce_store_address_2 "#343"
-cli wp option set woocommerce_store_city "San Francisco"
-cli wp option set woocommerce_default_country "US:CA"
-cli wp option set woocommerce_store_postcode "94110"
-cli wp option set woocommerce_currency "USD"
-cli wp option set woocommerce_product_type "both"
-cli wp option set woocommerce_allow_tracking "no"
+wp_cli option set woocommerce_store_address "60 29th Street"
+wp_cli option set woocommerce_store_address_2 "#343"
+wp_cli option set woocommerce_store_city "San Francisco"
+wp_cli option set woocommerce_default_country "US:CA"
+wp_cli option set woocommerce_store_postcode "94110"
+wp_cli option set woocommerce_currency "USD"
+wp_cli option set woocommerce_product_type "both"
+wp_cli option set woocommerce_allow_tracking "no"
 
 echo "Importing WooCommerce shop pages..."
-cli wp wc --user=admin tool run install_pages
+wp_cli wc --user=admin tool run install_pages
 
 echo "Installing and activating the WordPress Importer plugin..."
-cli wp plugin install wordpress-importer --activate
+wp_cli plugin install wordpress-importer --activate
 
 echo "Importing some sample data..."
-cli wp import wp-content/plugins/woocommerce/sample-data/sample_products.xml --authors=skip
+wp_cli import wp-content/plugins/woocommerce/sample-data/sample_products.xml --authors=skip
 
-echo "Activating the WooCommerce Payments plugin..."
-cli wp plugin activate woocommerce-payments
+echo "Activating the WooPayments plugin..."
+wp_cli plugin activate woocommerce-payments
 
-echo "Setting up WooCommerce Payments..."
-if [[ "0" == "$(cli wp option list --search=woocommerce_woocommerce_payments_settings --format=count)" ]]; then
-	echo "Creating WooCommerce Payments settings"
-	cli wp option add woocommerce_woocommerce_payments_settings --format=json '{"enabled":"yes"}'
+echo "Setting up WooPayments..."
+if [[ "0" == "$(wp_cli option list --search=woocommerce_woocommerce_payments_settings --format=count)" ]]; then
+	echo "Creating WooPayments settings"
+	wp_cli option add woocommerce_woocommerce_payments_settings --format=json '{"enabled":"yes"}'
 else
-	echo "Updating WooCommerce Payments settings"
-	cli wp option update woocommerce_woocommerce_payments_settings --format=json '{"enabled":"yes"}'
+	echo "Updating WooPayments settings"
+	wp_cli option update woocommerce_woocommerce_payments_settings --format=json '{"enabled":"yes"}'
 fi
 
 echo "Installing and activating Disable WordPress Updates..."
@@ -134,10 +129,10 @@ echo "Installing dev tools plugin..."
 set +e
 git clone git@github.com:Automattic/woocommerce-payments-dev-tools.git docker/wordpress/wp-content/plugins/woocommerce-payments-dev-tools
 if [[ $? -eq 0 ]]; then
-	cli wp plugin activate woocommerce-payments-dev-tools
+	wp_cli plugin activate woocommerce-payments-dev-tools
 else
 	echo
-	echo "WARN: Could not access the dev tools repository. Skipping the install."
+	echo "WARN: Could not clone the dev tools repository. Skipping the install."
 fi;
 set -e
 
