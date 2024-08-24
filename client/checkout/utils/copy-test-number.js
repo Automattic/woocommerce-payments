8f059b799d689f60a0d917fa169b63f0eb12e181
Samir Merchant <samir.merchant@automattic.com>
2024-08-23 22:27:06 -0400

    disables clipboard button when navigator.clipboard is undefined

--- client/checkout/utils/copy-test-number.js
+++ client/checkout/utils/copy-test-number.js
@@ -6,31 +6,37 @@ import { __ } from '@wordpress/i18n';
 document.addEventListener(
 	'click',
 	function ( event ) {
+		if ( ! navigator.clipboard ) {
+			return;
+		}
+
 		const copyNumberButton = event.target?.closest(
 			'.js-woopayments-copy-test-number'
 		);
-		if ( copyNumberButton ) {
-			event.preventDefault();
-			const number = copyNumberButton.parentElement.querySelector(
-				'.js-woopayments-test-number'
-			).innerText;
-			navigator.clipboard.writeText( number );
-
-			window.wp?.data
-				?.dispatch( 'core/notices' )
-				?.createInfoNotice(
-					__(
-						'Test number copied to your clipboard!',
-						'woocommerce-payments'
-					),
-					{
-						// the unique `id` prevents the JS from creating multiple notices with the same text before they're dismissed.
-						id: 'woopayments/test-number-copied',
-						type: 'snackbar',
-						context: 'wc/checkout/payments',
-					}
-				);
+		if ( ! copyNumberButton ) {
+			return;
 		}
+
+		event.preventDefault();
+		const number = copyNumberButton.parentElement.querySelector(
+			'.js-woopayments-test-number'
+		).innerText;
+		navigator.clipboard.writeText( number );
+
+		window.wp?.data
+			?.dispatch( 'core/notices' )
+			?.createInfoNotice(
+				__(
+					'Test number copied to your clipboard!',
+					'woocommerce-payments'
+				),
+				{
+					// the unique `id` prevents the JS from creating multiple notices with the same text before they're dismissed.
+					id: 'woopayments/test-number-copied',
+					type: 'snackbar',
+					context: 'wc/checkout/payments',
+				}
+			);
 	},
 	false
 );
