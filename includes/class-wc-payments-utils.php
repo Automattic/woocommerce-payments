28ddf490676cbea24eaee755a6004007cb5ec5fe
César Costa <cesar.costa@automattic.com>
2024-08-28 15:33:26 -0300

    Fallback minimum amount helper to Stripe provided amounts

--- includes/class-wc-payments-utils.php
+++ includes/class-wc-payments-utils.php
@@ -703,6 +703,62 @@ class WC_Payments_Utils {
 	}
 
 	/**
+	 * Retrieves Stripe minimum order value authorized per currency.
+	 * The values are based on Stripe's recommendations.
+	 * See https://docs.stripe.com/currencies#minimum-and-maximum-charge-amounts.
+	 *
+	 * @param string $currency The currency.
+	 *
+	 * @return int The minimum amount.
+	 */
+	public static function get_stripe_minimum_amount( $currency ) {
+		switch ( $currency ) {
+			case 'AED':
+			case 'MYR':
+			case 'PLN':
+			case 'RON':
+				$minimum_amount = 200;
+				break;
+			case 'BGN':
+				$minimum_amount = 100;
+				break;
+			case 'CZK':
+				$minimum_amount = 1500;
+				break;
+			case 'DKK':
+				$minimum_amount = 250;
+				break;
+			case 'GBP':
+				$minimum_amount = 30;
+				break;
+			case 'HKD':
+				$minimum_amount = 400;
+				break;
+			case 'HUF':
+				$minimum_amount = 17500;
+				break;
+			case 'JPY':
+				$minimum_amount = 5000;
+				break;
+			case 'MXN':
+			case 'THB':
+				$minimum_amount = 1000;
+				break;
+			case 'NOK':
+			case 'SEK':
+				$minimum_amount = 300;
+				break;
+			default:
+				$minimum_amount = 50;
+				break;
+		}
+
+		self::cache_minimum_amount( $currency, $minimum_amount );
+
+		return $minimum_amount;
+	}
+
+	/**
 	 * Saves the minimum amount required for transactions in a given currency.
 	 *
 	 * @param string $currency The currency.
@@ -713,15 +769,15 @@ class WC_Payments_Utils {
 	}
 
 	/**
-	 * Checks if there is a minimum amount required for transactions in a given currency.
+	 * Retrieves the minimum amount required for transactions in a given currency.
 	 *
 	 * @param string $currency The currency to check for.
 	 *
-	 * @return int|null Either the minimum amount, or `null` if not available.
+	 * @return int The minimum amount.
 	 */
 	public static function get_cached_minimum_amount( $currency ) {
 		$cached = get_transient( 'wcpay_minimum_amount_' . strtolower( $currency ) );
-		return (int) $cached ? (int) $cached : null;
+		return (int) $cached ? (int) $cached : self::get_stripe_minimum_amount( $currency );
 	}
 
 	/**
