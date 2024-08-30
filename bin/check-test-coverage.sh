082c8a425af8698bad7282cc2c463c95ccd079c5
Achyuth Ajoy <achyuthajoy@gmail.com>
2024-08-30 17:42:18 +0530

    Change xdebug.start_with_request value to trigger

--- bin/check-test-coverage.sh
+++ bin/check-test-coverage.sh
@@ -18,7 +18,7 @@ docker compose exec -u www-data wordpress \
 echo "Checking coverage..."
 
 docker-compose exec -u www-data wordpress \
-	php -d xdebug.start_with_request=yes \
+	php -d xdebug.start_with_request=trigger \
         /var/www/html/wp-content/plugins/woocommerce-payments/vendor/bin/phpunit \
         --configuration "/var/www/html/wp-content/plugins/woocommerce-payments/$CONFIGURATION_FILE" \
         --coverage-html /var/www/html/php-test-coverage \
