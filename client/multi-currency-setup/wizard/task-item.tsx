acf17bd00fb035e0c9e71b6889d0066e3701914a
Eric Jinks <3147296+Jinksi@users.noreply.github.com>
2024-10-02 11:19:00 +1000

    Migrate WizardTaskItem and CollapsibleBody components to TS for improve code safety and clarity (#9510)

--- client/multi-currency-setup/wizard/task-item.tsx
+++ client/multi-currency-setup/wizard/task-item.tsx
@@ -8,10 +8,24 @@ import { Icon, check } from '@wordpress/icons';
 /**
  * Internal dependencies
  */
-import WizardTaskContext from './task/context';
+import WizardTaskContext from '../../additional-methods-setup/wizard/task/context';
 import './task-item.scss';
 
-const WizardTaskItem = ( { children, title, index, className = '' } ) => {
+interface WizardTaskItemProps {
+	children: React.ReactNode;
+	title: string;
+	index: number;
+	visibleDescription: string;
+	className?: string;
+}
+
+const WizardTaskItem: React.FC< WizardTaskItemProps > = ( {
+	children,
+	title,
+	index,
+	visibleDescription,
+	className,
+} ) => {
 	const { isCompleted, isActive } = useContext( WizardTaskContext );
 
 	return (
@@ -26,7 +40,7 @@ const WizardTaskItem = ( { children, title, index, className = '' } ) => {
 				className="wcpay-wizard-task__headline"
 				// tabindex with value `-1` is necessary to programmatically set the focus
 				// on an element that is not interactive.
-				tabIndex="-1"
+				tabIndex={ -1 }
 			>
 				<div className="wcpay-wizard-task__icon-wrapper">
 					<div className="wcpay-wizard-task__icon-text">
@@ -39,6 +53,16 @@ const WizardTaskItem = ( { children, title, index, className = '' } ) => {
 				</div>
 				<span className="wcpay-wizard-task__title">{ title }</span>
 			</div>
+			{ visibleDescription && ! isActive && (
+				<span
+					className={ classNames(
+						'wcpay-wizard-task__visible-description-element',
+						'is-muted-color'
+					) }
+				>
+					{ visibleDescription }
+				</span>
+			) }
 			<div className="wcpay-wizard-task__body">{ children }</div>
 		</li>
 	);
