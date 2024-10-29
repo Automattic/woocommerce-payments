58af60b53f09af04d47511680eb9662db11e5a5b
Miguel Gasca <miguel.gasca@automattic.com>
2024-10-29 12:08:37 +0100

    Tweak comments

--- client/utils/date-time.ts
+++ client/utils/date-time.ts
@@ -8,14 +8,14 @@ type DateTimeFormat = string | null;
 interface FormatDateTimeOptions {
 	includeTime?: boolean; // Whether to include time in the formatted string (defaults to true)
 	useGmt?: boolean; // Whether to display the time in GMT/UTC (defaults to false)
-	separator?: string; // Separator between date and time (defaults to '/')
+	separator?: string; // Separator between date and time (defaults to ' / ')
 	customFormat?: DateTimeFormat; // Custom format to use instead of WordPress settings
 }
 
 /**
  * Formats a date and time string according to WordPress settings or a custom format.
  *
- * @param { string } dateTime - The date and time string or date from the database in UTC (e.g., '2024-10-23 15:28:26').
+ * @param { string | Date } dateTime - The date and time string (e.g., '2024-10-23 15:28:26') or a JS Date object.
  * @param { FormatDateTimeOptions } options - Additional options to control time inclusion and whether to use GMT/UTC.
  * @return { string } - The formatted date and time string.
  */
