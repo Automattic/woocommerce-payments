cccd63e468e90d2dee538f2d8f99523fe3b89104
Ricardo Metring <ricardometring@gmail.com>
2024-11-06 17:07:52 -0300

    Fix comment

--- includes/multi-currency/MultiCurrency.php
+++ includes/multi-currency/MultiCurrency.php
@@ -364,7 +364,7 @@ class MultiCurrency {
 			return $settings_pages;
 		}
 
-		// We shouldn't initiate MCCY settings if the plugin was just upgraded due to autoload caveats: https://github.com/Automattic/woocommerce-payments/issues/9676.
+		// Due to autoloader limitations, we shouldn't initiate MCCY settings if the plugin was just upgraded: https://github.com/Automattic/woocommerce-payments/issues/9676.
 		if ( did_action( 'upgrader_process_complete' ) ) {
 			return $settings_pages;
 		}
