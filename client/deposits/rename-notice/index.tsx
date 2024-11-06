edf839c34cf586680978f496ba5347d790a9b90a
Jessy <jessy.pappachan@automattic.com>
2024-11-06 11:06:57 +0530

    Draft payouts rename notice, with placeholder text and image.

---
+++ client/deposits/rename-notice/index.tsx
@@ -0,0 +1,86 @@
+/**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+import React, { useState, useEffect } from 'react';
+import { TourKit } from '@woocommerce/components';
+import { useDispatch } from '@wordpress/data';
+import { createInterpolateElement } from '@wordpress/element';
+
+/**
+ * Internal dependencies
+ */
+import './style.scss';
+
+export const PayoutsRenameNotice = () => {
+	const isPayoutsRenameNoticeDismissed =
+		wcpaySettings.isPayoutsRenameNoticeDismissed;
+	const { updateOptions } = useDispatch( 'wc/admin/options' );
+	const [ showTour, setShowTour ] = useState( true );
+
+	const onClose = () => {
+		updateOptions( {
+			wcpay_payouts_rename_notice_dismissed: true,
+		} );
+		setShowTour( false );
+		wcpaySettings.isPayoutsRenameNoticeDismissed = true;
+	};
+
+	useEffect( () => {
+		setShowTour( ! isPayoutsRenameNoticeDismissed );
+	}, [ isPayoutsRenameNoticeDismissed ] );
+
+	if ( ! showTour ) return null;
+
+	return (
+		<TourKit
+			config={ {
+				placement: 'bottom',
+				options: {
+					effects: {
+						overlay: false,
+						autoScroll: {
+							behavior: 'smooth',
+						},
+					},
+					classNames:
+						'wc-admin-payments-overview-payouts-rename-tour',
+				},
+				steps: [
+					{
+						referenceElements: {
+							desktop:
+								'#toplevel_page_wc-admin-path--payments-overview ul.wp-submenu li a[href*="payouts"]',
+						},
+						meta: {
+							name: 'deposits-now-payouts',
+							heading: __(
+								'Deposits are now Payouts!',
+								'woocommerce-payments'
+							),
+							descriptions: {
+								desktop: createInterpolateElement(
+									__(
+										"Heads up! We've given Deposits a new industry approved alias: Payouts! Don't worry, it's still the same reliable function, just with a little more style. <link>Learn More.</link>",
+										'woocommerce'
+									),
+									{
+										link: (
+											// eslint-disable-next-line jsx-a11y/anchor-has-content
+											<a
+												href="https://woocommerce.com/document/woopayments/payouts/deposits-and-payouts/"
+												target="_blank"
+												rel="noreferrer"
+											/>
+										),
+									}
+								),
+							},
+						},
+					},
+				],
+				closeHandler: onClose,
+			} }
+		></TourKit>
+	);
+};
