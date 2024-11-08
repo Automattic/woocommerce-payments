64d32ab4e26838cf38be651bec595adb2a037a1a
Timur Karimov <timur.karimov@automattic.com>
2024-11-08 11:31:48 +0100

    tests and fix interface

--- includes/class-wc-payments.php
+++ includes/class-wc-payments.php
@@ -464,6 +464,7 @@ class WC_Payments {
 		include_once __DIR__ . '/exceptions/class-rate-limiter-enabled-exception.php';
 		include_once __DIR__ . '/exceptions/class-invalid-address-exception.php';
 		include_once __DIR__ . '/constants/class-base-constant.php';
+		include_once __DIR__ . '/constants/class-country-test-cards.php';
 		include_once __DIR__ . '/constants/class-country-code.php';
 		include_once __DIR__ . '/constants/class-currency-code.php';
 		include_once __DIR__ . '/constants/class-fraud-meta-box-type.php';
