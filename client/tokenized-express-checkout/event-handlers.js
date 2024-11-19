596fb37c69ab6faeb99a27c11d2f07f3f9ffe245
frosso <francesco.rosso@automattic.com>
2024-11-19 18:23:53 +0100

    it was an ez fix

--- client/tokenized-express-checkout/event-handlers.js
+++ client/tokenized-express-checkout/event-handlers.js
@@ -50,7 +50,6 @@ export const shippingAddressChangeHandler = async ( api, event, elements ) => {
 		}
 
 		elements.update( {
-			// TODO ~FR: fix price transformation for Japanese Yen.
 			amount: transformPrice(
 				parseInt( cartData.totals.total_price, 10 ) -
 					parseInt( cartData.totals.total_refund || 0, 10 ),
