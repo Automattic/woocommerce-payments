8378c25227de6a416dfcbb7fc460059503a027b4
Timur Karimov <timur@Timurs-MacBook-Pro.local>
2024-11-28 15:44:26 +0100

    adjust the naming

--- bin/jurassic-tube-setup.sh
+++ bin/jurassic-tube-setup.sh
@@ -4,19 +4,19 @@
 set -e
 
 # Define Jurassic Tube directory using bin directory
-TUBE_DIR="${PWD}/bin/jurassictube"
+JT_DIR="${PWD}/bin/jurassictube"
 
 echo "Setting up Standalone Jurassic Tube..."
 
 # Create Jurassic Tube directory if it doesn't exist
-if [ ! -d "$TUBE_DIR" ]; then
-    echo "Creating Jurassic Tube directory at $TUBE_DIR..."
-    mkdir -p "$TUBE_DIR"
+if [ ! -d "$JT_DIR" ]; then
+    echo "Creating Jurassic Tube directory at $JT_DIR..."
+    mkdir -p "$JT_DIR"
 fi
 
 # Download and run the installer
 echo "Downloading the standalone installer..."
-curl "https://jurassic.tube/installer-standalone.sh" -o "$TUBE_DIR/installer.sh" && chmod +x "$TUBE_DIR/installer.sh" && "$TUBE_DIR/installer.sh"
+curl "https://jurassic.tube/installer-standalone.sh" -o "$JT_DIR/installer.sh" && chmod +x "$JT_DIR/installer.sh" && "$JT_DIR/installer.sh"
 
 echo
 read -p "Go to https://jurassic.tube/ in a browser, paste your public key which was printed above into the box, and click 'Add Public Key'. Press enter to continue"
@@ -28,13 +28,13 @@ echo
 read -p "Please enter your Automattic/WordPress.com username: " username
 echo 
 
-if [ ! -f "${TUBE_DIR}/config.env" ]; then
-    touch "${TUBE_DIR}/config.env"
+if [ ! -f "${JT_DIR}/config.env" ]; then
+    touch "${JT_DIR}/config.env"
 fi
 
-echo "username=${username}" >> ${TUBE_DIR}/config.env
-echo "subdomain=${subdomain}" >> ${TUBE_DIR}/config.env
-echo "port=8082" >> ${TUBE_DIR}/config.env
+echo "username=${username}" >> ${JT_DIR}/config.env
+echo "subdomain=${subdomain}" >> ${JT_DIR}/config.env
+echo "port=8082" >> ${JT_DIR}/config.env
 
 echo "Setup complete!"
 echo "Use the command: npm run tube:start from the root directory of your WooPayments project to start running Jurassic Tube."
