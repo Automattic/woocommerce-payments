07537861a54a098ec1a61a0c02dd12f1b8c4ba1f
Timur Karimov <timur@Timurs-MacBook-Pro.local>
2024-11-29 13:28:08 +0100

    try to minimize the changes

--- bin/jurassic-tube-setup.sh
+++ bin/jurassic-tube-setup.sh
@@ -6,22 +6,29 @@ set -e
 # Define Jurassic Tube directory using bin directory
 JT_DIR="${PWD}/bin/jurassictube"
 
-echo "Setting up Standalone Jurassic Tube..."
+echo "Checking if ${JT_DIR} directory exists..."
 
-# Create Jurassic Tube directory if it doesn't exist
-if [ ! -d "$JT_DIR" ]; then
-    echo "Creating Jurassic Tube directory at $JT_DIR..."
-    mkdir -p "$JT_DIR"
+if [ -d "${JT_DIR}" ]; then
+    echo "${JT_DIR} already exists."
+else
+    echo "Creating ${JT_DIR} directory..."
+    mkdir -p "${JT_DIR}"
 fi
 
-# Download the installer only if it's not already present
-if [ ! -f "$JT_DIR/installer.sh" ]; then
+echo "Downloading the latest version of the installer script..."
+echo 
+
+# Download the installer (if it's not already present):
+if [ ! -f "${JT_DIR}/installer.sh" ]; then
     echo "Downloading the standalone installer..."
-    curl "https://jurassic.tube/installer-standalone.sh" -o "$JT_DIR/installer.sh" && chmod +x "$JT_DIR/installer.sh"
+    curl "https://jurassic.tube/installer-standalone.sh" -o "${JT_DIR}/installer.sh" && chmod +x "${JT_DIR}/installer.sh"
 fi
 
-# Run the installer
-"$JT_DIR/installer.sh"
+echo "Running the installation script..."
+echo 
+
+# Run the installer script
+"${JT_DIR}/installer.sh"
 
 echo
 read -p "Go to https://jurassic.tube/ in a browser, paste your public key which was printed above into the box, and click 'Add Public Key'. Press enter to continue"
@@ -29,6 +36,9 @@ echo
 
 read -p "Go to https://jurassic.tube/ in a browser, add a subdomain using the desired name for your subdomain, and click 'Add Subdomain'. The subdomain name is what you will use to access WC Payments in a browser. When this is done, type the subdomain name here and press enter. Please just type in the subdomain, not the full URL: " subdomain
 echo 
+
+# npm run wp option update home https://${subdomain}.jurassic.tube/
+# npm run wp option update siteurl https://${subdomain}.jurassic.tube/
 
 read -p "Please enter your Automattic/WordPress.com username: " username
 echo 
@@ -39,9 +49,9 @@ else
     > "${JT_DIR}/config.env"
 fi
 
-echo "username=${username}" >> ${JT_DIR}/config.env
-echo "subdomain=${subdomain}" >> ${JT_DIR}/config.env
+echo "username=${username}" >> "${JT_DIR}/config.env"
+echo "subdomain=${subdomain}" >> "${JT_DIR}/config.env"
 
 echo "Setup complete!"
-echo "Use the command: npm run tube:start from the root directory of your WooPayments project to start running Jurassic Tube."
+echo "Use the command: npm run tube:start from the root directory of your WC Payments project to start running Jurassic Tube."
 echo 
