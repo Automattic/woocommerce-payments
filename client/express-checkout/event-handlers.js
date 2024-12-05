21e38d7833478ba5804908deb4ba069602e26b53
Rafael Zaleski <rafaelzaleski@users.noreply.github.com>
2024-12-05 16:16:54 -0300

    Simplify logic to refresh page.

--- client/express-checkout/event-handlers.js
+++ client/express-checkout/event-handlers.js
@@ -176,30 +176,12 @@ export const onCompletePaymentHandler = () => {
 };
 
 export const onCancelHandler = () => {
-	if ( wasShippingInfoUpdated ) {
-		const context = getExpressCheckoutData( 'button_context' );
-		const isBlocks = getExpressCheckoutData( 'has_block' );
-
-		switch ( context ) {
-			case 'cart':
-				if ( isBlocks ) {
-					alert( 'Cart block context' );
-				} else {
-					alert( 'Cart shortcode context' );
-				}
-				location.reload( true );
-				break;
-			case 'checkout':
-				if ( isBlocks ) {
-					alert( 'Checkout block context' );
-				} else {
-					alert( 'Checkout shortcode context' );
-				}
-				location.reload( true );
-				break;
-			default:
-				alert( 'Default context' );
-		}
+	const context = getExpressCheckoutData( 'button_context' );
+	if (
+		wasShippingInfoUpdated &&
+		[ 'cart', 'checkout' ].includes( context )
+	) {
+		location.reload( true );
 	}
 
 	wasShippingInfoUpdated = false;
