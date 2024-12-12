84ced5a497de0fa593c4aa39a3a5f893d3566c2b
Rafael Zaleski <rafaelzaleski@users.noreply.github.com>
2024-12-12 17:14:13 -0300

    Add shortcodeCheckout to utils index

--- client/express-checkout/utils/index.ts
+++ client/express-checkout/utils/index.ts
@@ -2,6 +2,7 @@
  * Internal dependencies
  */
 export * from './normalize';
+export * from './shortcodeCheckout';
 import { getDefaultBorderRadius } from 'wcpay/utils/express-checkout';
 
 interface MyWindow extends Window {
