b05ebb965bea20a6133c6bdf7c11165c64b7812d
Nagesh Pai <nagesh.pai@automattic.com>
2025-01-02 13:10:00 +0530

    Tweak language param

--- client/transactions/list/index.tsx
+++ client/transactions/list/index.tsx
@@ -587,7 +587,7 @@ export const TransactionsList = (
 		const { page, path, ...params } = getQuery();
 		const userEmail = wcpaySettings.currentUserEmail;
 
-		const locale = wcSettings.locale.userLocale;
+		const locale = wcSettings.locale.userLocale.split( '_' )[ 0 ];
 		const {
 			date_after: dateAfter,
 			date_before: dateBefore,
