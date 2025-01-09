84468185be871f0e4f97a3cba602e08cfc4c52a9
Jesse Pearson <jesse.pearson@automattic.com>
2025-01-09 15:05:44 -0400

    E2E Playwright Migration: convert Admin Disputes spec

---
+++ tests/e2e-pw/specs/merchant/merchant-admin-disputes.spec.ts
@@ -0,0 +1,34 @@
+/**
+ * External dependencies
+ */
+import { test, expect } from '@playwright/test';
+
+/**
+ * Internal dependencies
+ */
+import { useMerchant } from '../../utils/helpers';
+
+test.describe( 'Merchant disputes', () => {
+	// Use the merchant user for this test suite.
+	useMerchant();
+
+	test( 'Load the disputes list page', async ( { page } ) => {
+		await page.goto(
+			'/wp-admin/admin.php?page=wc-admin&path=/payments/disputes'
+		);
+
+		// Wait for the disputes table to load.
+		await page
+			.locator( '.woocommerce-table__table.is-loading' )
+			.waitFor( { state: 'hidden' } );
+
+		// .nth( 1 ) defines the second instance of the Disputes heading, which is in the table.
+		expect(
+			page
+				.getByRole( 'heading', {
+					name: 'Disputes',
+				} )
+				.nth( 1 )
+		).toBeVisible();
+	} );
+} );
