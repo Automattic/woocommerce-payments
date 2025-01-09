8f0a3b73a7310128b65c6415a4a5ce4b6a6edbb5
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2025-01-09 11:08:36 -0300

    Update getIsCardTestingProtectionEnabled to use a more end-user approach

--- tests/e2e-pw/utils/devtools.ts
+++ tests/e2e-pw/utils/devtools.ts
@@ -3,30 +3,23 @@
  */
 import { Page, expect } from '@playwright/test';
 
-const goToDevToolsSettings = async ( page: Page ) => {
-	await page.goto( 'wp-admin/admin.php?page=wcpaydev', {
+const goToDevToolsSettings = ( page: Page ) =>
+	page.goto( 'wp-admin/admin.php?page=wcpaydev', {
 		waitUntil: 'load',
 	} );
-};
 
 const saveDevToolsSettings = async ( page: Page ) => {
 	await page.getByRole( 'button', { name: 'Save Changes' } ).click();
 	expect( page.getByText( /Settings saved/ ) ).toBeVisible();
 };
 
-const getIsCardTestingProtectionEnabled = async ( page: Page ) => {
-	return (
-		( await page
-			.locator( '#wcpaydev_force_card_testing_protection_on:checked' )
-			.count() ) === 1
-	);
-};
+const getIsCardTestingProtectionEnabled = ( page: Page ) =>
+	page.getByLabel( 'Card testing mitigations enabled' ).isChecked();
 
-const toggleCardTestingProtection = async ( page: Page ) => {
-	await page
+const toggleCardTestingProtection = ( page: Page ) =>
+	page
 		.locator( 'label[for="wcpaydev_force_card_testing_protection_on"]' )
 		.click();
-};
 
 export const enableCardTestingProtection = async ( page: Page ) => {
 	await goToDevToolsSettings( page );
