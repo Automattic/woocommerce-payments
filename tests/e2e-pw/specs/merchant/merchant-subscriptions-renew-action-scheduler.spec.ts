dc8263668a49254cddc0f6cfccfd74f3e026bcdc
Allie Mims <allie.mims@automattic.com>
2025-01-13 15:48:33 -0500

    WIP spec

---
+++ tests/e2e-pw/specs/merchant/merchant-subscriptions-renew-action-scheduler.spec.ts
@@ -0,0 +1,40 @@
+/**
+ * External dependencies
+ */
+import { test, expect } from '@playwright/test';
+
+/**
+ * Internal dependencies
+ */
+import { useMerchant, describeif } from '../../utils/helpers';
+import {
+	runSubscriptionsTests,
+	runActionSchedulerTests,
+} from '../../utils/flows';
+import { addCartProduct, placeOrder, setupCheckout } from '../../utils/shopper';
+import { config } from '../../config/default';
+
+// Run the tests if the two 'skip' environment variables are not set.
+describeif( runSubscriptionsTests && runActionSchedulerTests )(
+	'Subscriptions > Renew a subscription via Action Scheduler',
+	() => {
+		useMerchant();
+
+		test.beforeEach( async ( { page } ) => {
+			await addCartProduct( page, 88 ); // Subscription no signup fee product
+			await setupCheckout(
+				page,
+				config.addresses[ 'subscriptions-customer' ].billing
+			);
+			await placeOrder( page );
+		} );
+
+		test( 'should renew a subscription with action scheduler', async ( {
+			page,
+		} ) => {
+			// WIP: This test is not yet implemented.
+			// To keep the linter happy for now:
+			expect( page.url() ).toContain( '/checkout/' );
+		} );
+	}
+);
