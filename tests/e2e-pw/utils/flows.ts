322bd7d8b08b4854812cb261ce56f7a859e86d19
Allie Mims <allie.mims@automattic.com>
2025-01-15 11:49:10 -0500

    Remove flows

--- tests/e2e-pw/utils/flows.ts
+++
@@ -1,5 +0,0 @@
-export const runSubscriptionsTests =
-	process.env.SKIP_WC_SUBSCRIPTIONS_TESTS !== '1';
-
-export const runActionSchedulerTests =
-	process.env.SKIP_WC_ACTION_SCHEDULER_TESTS !== '1';
