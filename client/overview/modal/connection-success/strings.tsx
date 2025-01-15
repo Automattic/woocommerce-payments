6ae5b01612f9a88a344098c47e5df742efe8da88
mordeth <cvetan.cvetanov@automattic.com>
2025-01-15 13:56:21 +0200

    Implement connection success modal

---
+++ client/overview/modal/connection-success/strings.tsx
@@ -0,0 +1,16 @@
+/* eslint-disable max-len */
+/**
+ * External dependencies
+ */
+import { __ } from '@wordpress/i18n';
+
+export default {
+	button: __( 'Dismiss', 'woocommerce-payments' ),
+
+	heading: __( "You're ready to accept payments!", 'woocommerce-payments' ),
+
+	description: __(
+		'Great news — your WooPayments account has been activated. You can now start accepting payments on your store.',
+		'woocommerce-payments'
+	),
+};
