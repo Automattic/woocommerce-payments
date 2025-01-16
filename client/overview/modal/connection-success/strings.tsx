2bd07c15cdf9243d6ce8f6316f93f9684b469966
mordeth <cvetan.cvetanov@automattic.com>
2025-01-16 19:50:24 +0200

    Update connection success modal string

--- client/overview/modal/connection-success/strings.tsx
+++ client/overview/modal/connection-success/strings.tsx
@@ -2,15 +2,18 @@
 /**
  * External dependencies
  */
-import { __ } from '@wordpress/i18n';
+import { __, sprintf } from '@wordpress/i18n';
 
 export default {
 	button: __( 'Dismiss', 'woocommerce-payments' ),
 
 	heading: __( "You're ready to accept payments!", 'woocommerce-payments' ),
 
-	description: __(
-		'Great news — your WooPayments account has been activated. You can now start accepting payments on your store.',
-		'woocommerce-payments'
+	description: sprintf(
+		__(
+			'Great news — your %s account has been activated. You can now start accepting payments on your store.',
+			'woocommerce-payments'
+		),
+		'WooPayments'
 	),
 };
