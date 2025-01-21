a72e2a131451438707d0e1c2b936b388f6d6623a
Jesse Pearson <jesse.pearson@automattic.com>
2025-01-21 14:40:13 -0400

    Revert saveWooPaymentsSettings changes due to they break other places the function is used

--- tests/e2e-pw/utils/merchant.ts
+++ tests/e2e-pw/utils/merchant.ts
@@ -57,9 +57,9 @@ export const saveWooPaymentsSettings = async ( page: Page ) => {
 	await ensureSupportPhoneIsFilled( page );
 
 	await page.getByRole( 'button', { name: 'Save changes' } ).click();
-	await expect( page.locator( '.components-snackbar__content' ) ).toHaveText(
-		'Settings saved.'
-	);
+	await expect( page.getByLabel( 'Dismiss this notice' ) ).toBeVisible( {
+		timeout: 10000,
+	} );
 };
 
 export const isMulticurrencyEnabled = async ( page: Page ) => {
