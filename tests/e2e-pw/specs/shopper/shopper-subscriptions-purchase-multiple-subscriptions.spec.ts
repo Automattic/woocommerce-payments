e8a5a5f4883acd94582f588ce42c5c56eba07808
Taha Paksu <3295+tpaksu@users.noreply.github.com>
2025-01-21 15:45:53 +0300

    Add restoreCurrencies to enable explicit formatting

--- tests/e2e-pw/specs/shopper/shopper-subscriptions-purchase-multiple-subscriptions.spec.ts
+++ tests/e2e-pw/specs/shopper/shopper-subscriptions-purchase-multiple-subscriptions.spec.ts
@@ -24,6 +24,7 @@ import {
 import {
 	activateMulticurrency,
 	deactivateMulticurrency,
+	restoreCurrencies,
 } from '../../utils/merchant';
 
 const products = {
@@ -47,6 +48,7 @@ describeif( shouldRunSubscriptionsTests )(
 			wasMulticurrencyEnabled = await activateMulticurrency(
 				merchantPage
 			);
+			await restoreCurrencies( merchantPage );
 		} );
 
 		test.afterAll( async () => {
