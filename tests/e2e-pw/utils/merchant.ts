d224ffffa6e0b29f37cdb40136958cdb6fb54da6
César Costa <cesar.costa@automattic.com>
2025-01-22 13:19:48 -0300

    Add Blocks support to addMulticurrencyWidget helper

--- tests/e2e-pw/utils/merchant.ts
+++ tests/e2e-pw/utils/merchant.ts
@@ -90,26 +90,33 @@ export const deactivateMulticurrency = async ( page: Page ) => {
 	await saveWooPaymentsSettings( page );
 };
 
-export const addMulticurrencyWidget = async ( page: Page ) => {
+export const addMulticurrencyWidget = async (
+	page: Page,
+	blocksVersion = false
+) => {
 	await navigation.goToWidgets( page );
 	// Wait for all widgets to load. This is important to prevent flakiness.
-	await page.locator( '.components-spinner' ).first().waitFor();
 	await expect( page.locator( '.components-spinner' ) ).toHaveCount( 0 );
 
 	if ( await page.getByRole( 'button', { name: 'Close' } ).isVisible() ) {
 		await page.getByRole( 'button', { name: 'Close' } ).click();
 	}
 
-	const isWidgetAdded = await page
-		.locator( 'iframe[srcdoc*=currency]' )
-		.first()
-		.isVisible();
+	// At this point, widgets might still be loading individually.
+	await expect( page.locator( '.components-spinner' ) ).toHaveCount( 0 );
+
+	const widgetName = blocksVersion
+		? 'Currency Switcher Block'
+		: 'Currency Switcher Widget';
+	const isWidgetAdded = blocksVersion
+		? await page.locator( `[data-title="${ widgetName }"]` ).isVisible()
+		: await page.getByRole( 'heading', { name: widgetName } ).isVisible();
 
 	if ( ! isWidgetAdded ) {
 		await page.getByRole( 'button', { name: 'Add block' } ).click();
 		await page
 			.locator( 'input[placeholder="Search"]' )
-			.pressSequentially( 'switcher', { delay: 20 } );
+			.pressSequentially( widgetName, { delay: 20 } );
 		await expect(
 			page.locator( 'button.components-button[role="option"]' ).first()
 		).toBeVisible( { timeout: 5000 } );
