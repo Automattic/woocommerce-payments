66b596c09cb7743013368629dbd730bc73453275
Taha Paksu <3295+tpaksu@users.noreply.github.com>
2025-01-23 17:50:30 +0300

    Wait for loading animation to finish

--- tests/e2e-pw/utils/shopper.ts
+++ tests/e2e-pw/utils/shopper.ts
@@ -168,8 +168,9 @@ export const confirmCardAuthentication = async (
 		name: authorize ? 'Complete' : 'Fail',
 	} );
 
-	// Not ideal, but we need to wait for the loading animation to finish before we can click the button.
-	await page.waitForTimeout( 1000 );
+	await expect(
+		stripeFrame.locator( '.LightboxModalLoadingIndicator' )
+	).not.toBeVisible();
 
 	await button.click();
 };
