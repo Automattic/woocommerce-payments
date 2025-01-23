d76d03415b5bb89ef6a337e33f785e1022e2edac
Taha Paksu <3295+tpaksu@users.noreply.github.com>
2025-01-24 00:28:08 +0300

    Remove single test running

--- tests/e2e-pw/playwright.config.ts
+++ tests/e2e-pw/playwright.config.ts
@@ -19,7 +19,7 @@ export default defineConfig( {
 	/* Run tests in files in parallel */
 	fullyParallel: false,
 	/* Fail the build on CI if you accidentally left test.only in the source code. */
-	// forbidOnly: !! process.env.CI,
+	forbidOnly: !! process.env.CI,
 	/* Retry on CI only */
 	retries: process.env.CI ? 2 : 0,
 	/* Opt out of parallel tests. */
