9dfce49463bb76886e949e7453ed99184953b8c3
Taha Paksu <3295+tpaksu@users.noreply.github.com>
2025-01-24 14:28:37 +0300

    Replace command

--- tests/e2e-pw/test-e2e-pw-changed.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-echo "🎭 Running Playwright e2e tests in default headless mode, skipping @todo.";
-
-docker compose -f ./tests/e2e-pw/docker-compose.yml run --rm -it --service-ports playwright \
-	npx playwright test --only-changed --pass-with-no-tests --config=tests/e2e-pw/playwright.config.ts --grep-invert @todo "$@"
