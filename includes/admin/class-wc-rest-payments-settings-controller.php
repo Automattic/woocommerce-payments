d99cace1aff1032b653989f2eeb6a8a03a995e63
César Costa <cesar.costa@automattic.com>
2025-01-28 16:05:56 -0300

    Revert if statement on settings REST controller
    In order to avoid masking or silencing this and other failures, we're reverting the if statement added previously. Since no issues were reported so far, most likely this won't make a big difference.
    
    Plus, there are other calls to  in the same file without extra checks, we should be on the safe side with this removal.

--- includes/admin/class-wc-rest-payments-settings-controller.php
+++ includes/admin/class-wc-rest-payments-settings-controller.php
@@ -945,15 +945,13 @@ class WC_REST_Payments_Settings_Controller extends WC_Payments_REST_Controller {
 			return;
 		}
 
-		$wcpay_form_fields = $this->wcpay_gateway->get_form_fields();
+		$woopay_enabled_locations = $request->get_param( 'woopay_enabled_locations' );
+		$wcpay_form_fields        = $this->wcpay_gateway->get_form_fields();
+		$all_locations            = $wcpay_form_fields['payment_request_button_locations']['options'];
 
-		if ( isset( $wcpay_form_fields['payment_request_button_locations']['options'] ) ) {
-			$woopay_enabled_locations = $request->get_param( 'woopay_enabled_locations' );
-			$all_locations            = $wcpay_form_fields['payment_request_button_locations']['options'];
+		WC_Payments::woopay_tracker()->woopay_locations_updated( $all_locations, $woopay_enabled_locations );
 
-			$this->wcpay_gateway->update_option( 'platform_checkout_button_locations', $woopay_enabled_locations );
-			WC_Payments::woopay_tracker()->woopay_locations_updated( $all_locations, $woopay_enabled_locations );
-		}
+		$this->wcpay_gateway->update_option( 'platform_checkout_button_locations', $woopay_enabled_locations );
 	}
 
 	/**
