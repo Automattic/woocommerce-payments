aa2eb6b2fc32390cfc60d233da693458b1ad70be
Allie Mims <allie.mims@automattic.com>
2025-01-31 14:51:43 -0500

    Update Playwright config

--- tests/e2e-pw/playwright.config.ts
+++ tests/e2e-pw/playwright.config.ts
@@ -84,8 +84,27 @@ export default defineConfig( {
 			dependencies: [ 'setup' ],
 		},
 		{
-			name: 'chromium',
+			name: 'wcpay - merchant',
 			use: { ...devices[ 'Desktop Chrome' ] },
+			testMatch: getTestMatch( 'wcpay', 'merchant' ),
+			dependencies: [ 'setup' ],
+		},
+		{
+			name: 'wcpay - shopper',
+			use: { ...devices[ 'Desktop Chrome' ] },
+			testMatch: getTestMatch( 'wcpay', 'shopper' ),
+			dependencies: [ 'setup' ],
+		},
+		{
+			name: 'subscriptions - merchant',
+			use: { ...devices[ 'Desktop Chrome' ] },
+			testMatch: getTestMatch( 'subscriptions', 'merchant' ),
+			dependencies: [ 'setup' ],
+		},
+		{
+			name: 'subscriptions - shopper',
+			use: { ...devices[ 'Desktop Chrome' ] },
+			testMatch: getTestMatch( 'subscriptions', 'shopper' ),
 			dependencies: [ 'setup' ],
 		},
 		// Setup project
