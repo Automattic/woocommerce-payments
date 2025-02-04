e5ced30870b999f1a584ab580746c91bf3da09e5
Shendy <73803630+shendy-a8c@users.noreply.github.com>
2025-02-05 02:47:36 +0700

    Rest end-point to get the disputes export URL.

--- includes/wc-payment-api/class-wc-payments-api-client.php
+++ includes/wc-payment-api/class-wc-payments-api-client.php
@@ -456,7 +456,7 @@ class WC_Payments_API_Client implements MultiCurrencyApiClientInterface {
 	}
 
 	/**
-	 * Get the export URL for a given export ID, if available.
+	 * Get the transactions export URL for a given export ID, if available.
 	 *
 	 * @param string $export_id The export ID.
 	 *
@@ -465,6 +465,18 @@ class WC_Payments_API_Client implements MultiCurrencyApiClientInterface {
 	 */
 	public function get_transactions_export_url( string $export_id ): array {
 		return $this->request( [], self::TRANSACTIONS_API . "/download/{$export_id}", self::GET );
+	}
+
+	/**
+	 * Get the disputes export URL for a given export ID, if available.
+	 *
+	 * @param string $export_id The export ID.
+	 *
+	 * @return array The export URL response.
+	 * @throws API_Exception - Exception thrown on request failure.
+	 */
+	public function get_disputes_export_url( string $export_id ): array {
+		return $this->request( [], self::DISPUTES_API . "/download/{$export_id}", self::GET );
 	}
 
 	/**
