9c1007f0c878998327d4f17654c0060fc7551175
Eduardo Umpierre <eduardoumpierre@hotmail.com>
2025-02-11 10:12:51 -0300

    Update E2E scripts

--- tests/e2e/test-e2e-ui.sh
+++ tests/e2e/test-e2e-ui.sh
@@ -4,5 +4,5 @@ echo "🎭 Running Playwright e2e tests in interactive UI mode.";
 echo "";
 echo "Open http://localhost:8077 in your browser to see the UI.";
 
-docker compose -f ./tests/e2e-pw/docker-compose.yml run --rm -it --service-ports playwright \
-	npx playwright test --config=tests/e2e-pw/playwright.config.ts --ui --ui-host=0.0.0.0 --ui-port=8077 "$@"
+docker compose -f ./tests/e2e/docker-compose.yml run --rm -it --service-ports playwright \
+	npx playwright test --config=tests/e2e/playwright.config.ts --ui --ui-host=0.0.0.0 --ui-port=8077 "$@"
--- tests/e2e/test-e2e.sh
+++ tests/e2e/test-e2e.sh
@@ -2,5 +2,5 @@
 
 echo "🎭 Running Playwright e2e tests in default headless mode, skipping @todo.";
 
-docker compose -f ./tests/e2e-pw/docker-compose.yml run --rm -it --service-ports playwright \
-	npx playwright test --config=tests/e2e-pw/playwright.config.ts --grep-invert @todo "$@"
+docker compose -f ./tests/e2e/docker-compose.yml run --rm -it --service-ports playwright \
+	npx playwright test --config=tests/e2e/playwright.config.ts --grep-invert @todo "$@"
