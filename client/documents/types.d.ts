30df83e5e5c467d4f8a336ddcde4fea52533252f
Eric Jinks <3147296+Jinksi@users.noreply.github.com>
2025-02-19 12:30:27 +1000

    Inline VAT API response interfaces

--- client/documents/types.d.ts
+++ client/documents/types.d.ts
@@ -1,23 +1,3 @@
-export interface VatValidationResult {
-	address: string | null;
-	country_code: string;
-	name: string | null;
-	valid: boolean;
-	vat_number: string;
-}
-
-export interface VatSaveDetails {
-	vat_number?: string;
-	name: string;
-	address: string;
-}
-
-export interface VatSaveDetailsResult {
-	vat_number: string | null;
-	name: string;
-	address: string;
-}
-
 export type VatFormOnCompleted = (
 	vatNumber: string | null,
 	companyName: string,
