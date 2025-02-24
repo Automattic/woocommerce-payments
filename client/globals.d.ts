191927a1de75c318f54fa5d8ea678b39c63b491a
frosso <francesco.rosso@automattic.com>
2025-02-24 14:07:29 +0100

    another missing ff

--- client/globals.d.ts
+++ client/globals.d.ts
@@ -14,6 +14,7 @@ declare global {
 		featureFlags: {
 			customSearch: boolean;
 			woopay: boolean;
+			documents: boolean;
 			woopayExpressCheckout: boolean;
 			isAuthAndCaptureEnabled: boolean;
 			paymentTimeline: boolean;
