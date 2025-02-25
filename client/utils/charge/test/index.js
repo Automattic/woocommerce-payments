45c328239dcf3b2bfd4857146e236efa1a4803cb
Dat Hoang <htdat@users.noreply.github.com>
2025-02-25 14:45:12 +0700

    Fix tests + update snapshots

--- client/utils/charge/test/index.js
+++ client/utils/charge/test/index.js
@@ -499,14 +499,14 @@ describe( 'Charge utilities / get channel string', () => {
 			expect( result ).toBe( 'In-Person' );
 		} );
 
-		test( 'should return "Online" for online channel', () => {
+		test( 'should return "Online Store" for online channel', () => {
 			const result = utils.getTransactionChannel( 'online' );
-			expect( result ).toBe( 'Online' );
+			expect( result ).toBe( 'Online Store' );
 		} );
 
-		test( 'should return "Online" for null channel', () => {
+		test( 'should return "Online Store" for null channel', () => {
 			const result = utils.getTransactionChannel( null );
-			expect( result ).toBe( 'Online' );
+			expect( result ).toBe( 'Online Store' );
 		} );
 	} );
 
@@ -530,11 +530,11 @@ describe( 'Charge utilities / get channel string', () => {
 			expect( result ).toBe( 'In-Person' );
 		} );
 
-		test( 'should return "Online" for online type', () => {
+		test( 'should return "Online Store" for online type', () => {
 			const result = utils.getChargeChannel( 'online', {
 				ipp_channel: 'mobile_pos',
 			} );
-			expect( result ).toBe( 'Online' );
+			expect( result ).toBe( 'Online Store' );
 		} );
 	} );
 } );
