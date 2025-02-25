8ab3e7fd4055ffa4b2bf0989a6749e69dca09d4c
frosso <francesco.rosso@automattic.com>
2025-02-25 12:45:05 +0100

    more

--- client/utils/test-utils.js
+++ client/utils/test-utils.js
@@ -5,12 +5,6 @@
  */
 import moment from 'moment';
 
-export function getUnformattedAmount( formattedAmount ) {
-	let amount = formattedAmount.replace( /[^0-9,.' ]/g, '' ).trim();
-	amount = amount.replace( ',', '.' ); // Euro fix
-	return amount;
-}
-
 export function getUserTimeZone() {
 	return moment( new Date() ).format( 'Z' );
 }
