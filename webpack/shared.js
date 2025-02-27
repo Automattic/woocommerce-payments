58c00557a25afa02c10db486b09875fe2696e249
mordeth <cvetan.cvetanov@automattic.com>
2025-02-27 18:31:35 +0200

    Remove block of @wordpress/components

--- webpack/shared.js
+++ webpack/shared.js
@@ -148,16 +148,12 @@ module.exports = {
 			injectPolyfill: true,
 			requestToExternal( request ) {
 				switch ( request ) {
-					case '@wordpress/components':
-						return null;
 					case 'wp-mediaelement':
 						return [ 'wp', 'mediaelement' ];
 				}
 			},
 			requestToHandle( request ) {
 				switch ( request ) {
-					case '@wordpress/components':
-						return null;
 					case 'wp-mediaelement':
 						return 'wp-mediaelement';
 				}
