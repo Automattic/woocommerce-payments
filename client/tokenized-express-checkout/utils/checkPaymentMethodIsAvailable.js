8daf95d3a7e4fd1ac7b64121b5853b87b9e3ffeb
frosso <francesco.rosso@automattic.com>
2025-02-27 09:49:18 +0100

    chore: port ECE changes into tokenized ECE

--- client/tokenized-express-checkout/utils/checkPaymentMethodIsAvailable.js
+++ client/tokenized-express-checkout/utils/checkPaymentMethodIsAvailable.js
@@ -14,71 +14,75 @@ import WCPayAPI from 'wcpay/checkout/api';
 import { getUPEConfig } from 'wcpay/utils/checkout';
 
 export const checkPaymentMethodIsAvailable = memoize(
-	( paymentMethod, cart, resolve ) => {
-		// Create the DIV container on the fly
-		const containerEl = document.createElement( 'div' );
+	( paymentMethod, cart ) => {
+		return new Promise( ( resolve ) => {
+			// Create the DIV container on the fly
+			const containerEl = document.createElement( 'div' );
 
-		// Ensure the element is hidden and doesn’t interfere with the page layout.
-		containerEl.style.display = 'none';
+			// Ensure the element is hidden and doesn’t interfere with the page layout.
+			containerEl.style.display = 'none';
 
-		document.querySelector( 'body' ).appendChild( containerEl );
+			document.querySelector( 'body' ).appendChild( containerEl );
 
-		const root = ReactDOM.createRoot( containerEl );
+			const root = ReactDOM.createRoot( containerEl );
 
-		const api = new WCPayAPI(
-			{
-				publishableKey: getUPEConfig( 'publishableKey' ),
-				accountId: getUPEConfig( 'accountId' ),
-				forceNetworkSavedCards: getUPEConfig(
-					'forceNetworkSavedCards'
-				),
-				locale: getUPEConfig( 'locale' ),
-				isStripeLinkEnabled: isLinkEnabled(
-					getUPEConfig( 'paymentMethodsConfig' )
-				),
-			},
-			request
-		);
+			const api = new WCPayAPI(
+				{
+					publishableKey: getUPEConfig( 'publishableKey' ),
+					accountId: getUPEConfig( 'accountId' ),
+					forceNetworkSavedCards: getUPEConfig(
+						'forceNetworkSavedCards'
+					),
+					locale: getUPEConfig( 'locale' ),
+					isStripeLinkEnabled: isLinkEnabled(
+						getUPEConfig( 'paymentMethodsConfig' )
+					),
+				},
+				request
+			);
 
-		root.render(
-			<Elements
-				stripe={ api.loadStripeForExpressCheckout() }
-				options={ {
-					mode: 'payment',
-					paymentMethodCreation: 'manual',
-					amount: Number( cart.cartTotals.total_price ),
-					currency: cart.cartTotals.currency_code.toLowerCase(),
-				} }
-			>
-				<ExpressCheckoutElement
-					onLoadError={ () => resolve( false ) }
+			root.render(
+				<Elements
+					stripe={ api.loadStripeForExpressCheckout() }
 					options={ {
-						paymentMethods: {
-							amazonPay: 'never',
-							applePay:
-								paymentMethod === 'applePay'
-									? 'always'
-									: 'never',
-							googlePay:
-								paymentMethod === 'googlePay'
-									? 'always'
-									: 'never',
-							link: 'never',
-							paypal: 'never',
-						},
+						mode: 'payment',
+						paymentMethodCreation: 'manual',
+						amount: Number( cart.cartTotals.total_price ),
+						currency: cart.cartTotals.currency_code.toLowerCase(),
 					} }
-					onReady={ ( event ) => {
-						let canMakePayment = false;
-						if ( event.availablePaymentMethods ) {
-							canMakePayment =
-								event.availablePaymentMethods[ paymentMethod ];
-						}
-						resolve( canMakePayment );
-						root.unmount();
-						containerEl.remove();
-					} }
-				/>
-			</Elements>
-		);
+				>
+					<ExpressCheckoutElement
+						onLoadError={ () => resolve( false ) }
+						options={ {
+							paymentMethods: {
+								amazonPay: 'never',
+								applePay:
+									paymentMethod === 'applePay'
+										? 'always'
+										: 'never',
+								googlePay:
+									paymentMethod === 'googlePay'
+										? 'always'
+										: 'never',
+								link: 'never',
+								paypal: 'never',
+							},
+						} }
+						onReady={ ( event ) => {
+							let canMakePayment = false;
+							if ( event.availablePaymentMethods ) {
+								canMakePayment =
+									event.availablePaymentMethods[
+										paymentMethod
+									];
+							}
+							resolve( canMakePayment );
+							root.unmount();
+							containerEl.remove();
+						} }
+					/>
+				</Elements>
+			);
+		} );
 	}
 );
