06c3226c3bfe25f27248ee20cf42fa7ce42c8729
Daniel Mallory <daniel.mallory@automattic.com>
2025-03-06 09:41:12 +0000

    Remove unused component.

--- client/onboarding/steps/business-details.tsx
+++ client/onboarding/steps/business-details.tsx
@@ -16,7 +16,6 @@ import {
 	getMccsFlatList,
 } from 'onboarding/utils';
 import { BusinessType } from 'onboarding/types';
-import InlineNotice from 'components/inline-notice';
 import strings from 'onboarding/strings';
 
 /**
