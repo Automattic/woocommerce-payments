9ff21ad64aba6f5aa61837630d3f85543eeb95be
BurakParsAydin <hi@burakaydin.net>
2025-03-10 23:41:49 +0300

    fix(tests): suppress unexpected JSON output

--- tests/unit/test-class-wc-payments-account.php
+++ tests/unit/test-class-wc-payments-account.php
@@ -428,7 +428,9 @@ class WC_Payments_Account_Test extends WCPAY_UnitTestCase {
 		);
 
 		// Act.
+		ob_start();
 		$wcpay_account->maybe_handle_onboarding();
+		ob_end_clean();
 
 		remove_all_filters( 'wp_doing_ajax' );
 		remove_all_filters( 'wp_die_ajax_handler' );
