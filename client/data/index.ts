6c40226db0111d3c7c4ea0d29c79b77c0f166b74
Valery Sukhomlinov <683297+dmvrtx@users.noreply.github.com>
2025-03-13 20:18:31 +0100

    Revert "Transaction breakdown component for the Payment Details page (#10283)"
    This reverts commit f00b02c230cb6be2bbb7c11a5f5a7dabe6ee9b3f.

--- client/data/index.ts
+++ client/data/index.ts
@@ -25,14 +25,3 @@ export * from './payment-intents/hooks';
 export * from './authorizations/hooks';
 export * from './files/hooks';
 export * from './payment-activity/hooks';
-
-import { TimelineItem } from './timeline/types';
-import { ApiError } from '../types/errors';
-
-export declare function useTimeline(
-	transactionId: string
-): {
-	timeline: Array< TimelineItem >;
-	timelineError: ApiError | undefined;
-	isLoading: boolean;
-};
