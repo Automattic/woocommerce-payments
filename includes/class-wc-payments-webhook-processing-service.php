d006e3845b2bc0a8cf6a24bb816f2215d1de3794
Dat Hoang <htdat@users.noreply.github.com>
2025-03-20 17:48:25 +0700

    Fix for webhook

--- includes/class-wc-payments-webhook-processing-service.php
+++ includes/class-wc-payments-webhook-processing-service.php
@@ -879,7 +879,7 @@ class WC_Payments_Webhook_Processing_Service {
 
 		$wc_refund = $this->order_service->create_refund_for_order( $order, $refunded_amount, $refund_reason, ( ! $is_partial_refund ? $order->get_items() : [] ) );
 		// Process the refund in the order service.
-		$this->order_service->add_note_and_metadata_for_refund( $order, $wc_refund, $refund_id, $refund_balance_transaction_id );
+		$this->order_service->add_note_and_metadata_for_refund( $order, $wc_refund, $refund_id, $refund_balance_transaction_id, $refund['status'] );
 	}
 
 	/**
