b433d42fe0c13c23b303ad3ce9a589ccf3ac6ba1
Miguel Gasca <miguel.gasca@automattic.com>
2025-03-25 19:02:50 +0100

    Fix Multi-currency doesn't convert range type cost adjustments from Woo Bookings

--- includes/multi-currency/Compatibility/WooCommerceBookings.php
+++ includes/multi-currency/Compatibility/WooCommerceBookings.php
@@ -53,6 +53,8 @@ class WooCommerceBookings extends BaseCompatibility {
 				add_filter( 'woocommerce_product_get_resource_base_costs', [ $this, 'get_resource_prices' ], 50, 1 );
 				add_filter( 'woocommerce_product_get_resource_block_costs', [ $this, 'get_resource_prices' ], 50, 1 );
 				add_filter( MultiCurrency::FILTER_PREFIX . 'should_convert_product_price', [ $this, 'should_convert_product_price' ], 50, 2 );
+				add_filter( 'woocommerce_bookings_process_cost_rules_cost', [ $this, 'get_price' ], 50, 1 );
+				add_filter( 'woocommerce_bookings_process_cost_rules_base_cost', [ $this, 'get_price' ], 50, 1 );
 				add_action( 'wp_ajax_wc_bookings_calculate_costs', [ $this, 'add_wc_price_args_filter_for_ajax' ], 9 );
 				add_action( 'wp_ajax_nopriv_wc_bookings_calculate_costs', [ $this, 'add_wc_price_args_filter_for_ajax' ], 9 );
 			}
