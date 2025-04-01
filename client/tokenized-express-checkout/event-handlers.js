8aa445f49196457c9103f43c4192a1261ff93b35
frosso <francesco.rosso@automattic.com>
2025-04-01 12:18:46 +0200

    more tests

--- client/tokenized-express-checkout/event-handlers.js
+++ client/tokenized-express-checkout/event-handlers.js
@@ -37,7 +37,6 @@ export const shippingAddressChangeHandler = async ( event, elements ) => {
 	lastSelectedAddress = event.address;
 
 	try {
-		console.log( '### entering' );
 		// Please note that the `event.address` might not contain all the fields.
 		// Some fields might not be present (like `line_1` or `line_2`) due to semi-anonymized data.
 		const cartData = await cartApi.updateCustomer( {
@@ -47,14 +46,11 @@ export const shippingAddressChangeHandler = async ( event, elements ) => {
 			),
 		} );
 
-		console.log( cartData );
-
 		const shippingRates = transformCartDataForShippingRates( cartData );
 
 		// when no shipping options are returned, the API still returns a 200 status code.
 		// We need to ensure that shipping options are present - otherwise the ECE dialog won't update correctly.
 		if ( shippingRates.length === 0 ) {
-			console.log( '### no rates' );
 			event.reject();
 
 			return;
@@ -73,7 +69,6 @@ export const shippingAddressChangeHandler = async ( event, elements ) => {
 			lineItems: transformCartDataForDisplayItems( cartData ),
 		} );
 	} catch ( error ) {
-		console.log( '### err', error );
 		event.reject();
 	}
 };
