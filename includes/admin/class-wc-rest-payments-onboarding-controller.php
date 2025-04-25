21ca10f25ea8d014e9e82cc20e7c07aa9c4c0b43
oaratovskyi <oleksandr.aratovskyi@automattic.com>
2025-04-25 15:32:06 +0300

    More cleanup

--- includes/admin/class-wc-rest-payments-onboarding-controller.php
+++ includes/admin/class-wc-rest-payments-onboarding-controller.php
@@ -164,62 +164,6 @@ class WC_REST_Payments_Onboarding_Controller extends WC_Payments_REST_Controller
 
 		register_rest_route(
 			$this->namespace,
-			'/' . $this->rest_base . '/router/po_eligible',
-			[
-				'methods'             => WP_REST_Server::CREATABLE,
-				'args'                => [
-					'business'        => [
-						'required'    => true,
-						'description' => 'The context about the merchant\'s business (self-assessment data).',
-						'type'        => 'object',
-						'properties'  => [
-							'country' => [
-								'type'        => 'string',
-								'description' => 'The country code where the company is legally registered.',
-								'required'    => true,
-							],
-							'type'    => [
-								'type'        => 'string',
-								'description' => 'The company incorporation type.',
-								'required'    => true,
-							],
-							'mcc'     => [
-								'type'        => 'string',
-								'description' => 'The merchant category code. This can either be a true MCC or an MCCs tree item id from the onboarding form.',
-								'required'    => true,
-							],
-						],
-					],
-					'store'           => [
-						'required'    => true,
-						'description' => 'The context about the merchant\'s store (self-assessment data).',
-						'type'        => 'object',
-						'properties'  => [
-							'annual_revenue'    => [
-								'type'        => 'string',
-								'description' => 'The estimated annual revenue bucket id.',
-								'required'    => true,
-							],
-							'go_live_timeframe' => [
-								'type'        => 'string',
-								'description' => 'The timeframe bucket for the estimated first live transaction.',
-								'required'    => true,
-							],
-						],
-					],
-					'woo_store_stats' => [
-						'required'    => false,
-						'description' => 'Context about the merchant\'s current WooCommerce store.',
-						'type'        => 'object',
-					],
-				],
-				'callback'            => [ $this, 'get_progressive_onboarding_eligible' ],
-				'permission_callback' => [ $this, 'check_permission' ],
-			]
-		);
-
-		register_rest_route(
-			$this->namespace,
 			'/' . $this->rest_base . '/test_drive_account/init',
 			[
 				'methods'             => WP_REST_Server::CREATABLE,
@@ -350,24 +294,6 @@ class WC_REST_Payments_Onboarding_Controller extends WC_Payments_REST_Controller
 	public function get_business_types( WP_REST_Request $request ) {
 		$business_types = $this->onboarding_service->get_cached_business_types();
 		return rest_ensure_response( [ 'data' => $business_types ] );
-	}
-
-	/**
-	 * Get progressive onboarding eligibility via API.
-	 *
-	 * @param WP_REST_Request $request Request object.
-	 *
-	 * @return WP_REST_Response|WP_Error
-	 */
-	public function get_progressive_onboarding_eligible( WP_REST_Request $request ) {
-		return $this->forward_request(
-			'get_onboarding_po_eligible',
-			[
-				'business_info'   => $request->get_param( 'business' ),
-				'store_info'      => $request->get_param( 'store' ),
-				'woo_store_stats' => $request->get_param( 'woo_store_stats' ) ?? [],
-			]
-		);
 	}
 
 	/**
