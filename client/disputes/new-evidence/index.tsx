d0920c43d090dddf4c12c622b9582edb1d26025d
Rafael Meneses <meneses.tio@gmail.com>
2025-06-04 16:48:22 -0300

    change event record

--- client/disputes/new-evidence/index.tsx
+++ client/disputes/new-evidence/index.tsx
@@ -346,12 +346,6 @@ ${ merchantName }`;
 					: 'wcpay_dispute_save_evidence_clicked'
 			);
 
-			recordEvent(
-				submit
-					? 'wcpay_dispute_submit_evidence_success'
-					: 'wcpay_dispute_save_evidence_success'
-			);
-
 			createSuccessNotice(
 				submit
 					? __( 'Evidence submitted!', 'woocommerce-payments' )
@@ -412,6 +406,13 @@ ${ merchantName }`;
 					submit,
 				},
 			} );
+
+			recordEvent(
+				submit
+					? 'wcpay_dispute_submit_evidence_success'
+					: 'wcpay_dispute_save_evidence_success'
+			);
+
 			setRedirectAfterSave( true );
 		} catch ( err ) {
 			recordEvent(
