3d2a425be38fda122cd5e71c7a3a9a49a4669f55
Rafael Meneses <meneses.tio@gmail.com>
2025-06-09 16:36:59 -0300

    use css class instead

--- client/disputes/new-evidence/file-upload-control.tsx
+++ client/disputes/new-evidence/file-upload-control.tsx
@@ -37,7 +37,12 @@ const FileUploadControl: React.FC< FileUploadControlProps > = ( {
 } ) => {
 	return (
 		<div className="wcpay-dispute-evidence-file-upload-control">
-			{ isDone && fileName ? <Icon icon={ check } size={ 36 } /> : null }
+			{ isDone && fileName ? (
+				<Icon
+					className="wcpay-dispute-evidence-file-upload-control__check"
+					icon={ check }
+				/>
+			) : null }
 			<label className="wcpay-dispute-evidence-file-upload-control__label">
 				{ label }
 			</label>
