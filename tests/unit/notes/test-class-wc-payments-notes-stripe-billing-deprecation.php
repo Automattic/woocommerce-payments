b47d22052ee339e69537c30bfb796e1018a28420
Rafael Meneses <meneses.tio@gmail.com>
2025-06-13 16:08:38 -0300

    add test

---
+++ tests/unit/notes/test-class-wc-payments-notes-stripe-billing-deprecation.php
@@ -0,0 +1,81 @@
+<?php
+/**
+ * Class WC_Payments_Notes_Stripe_Billing_Deprecation_Test
+ *
+ * @package WooCommerce\Payments\Tests
+ */
+
+/**
+ * Class WC_Payments_Notes_Stripe_Billing_Deprecation tests.
+ */
+class WC_Payments_Notes_Stripe_Billing_Deprecation_Test extends WCPAY_UnitTestCase {
+	public function set_up() {
+		parent::set_up();
+		require_once WCPAY_ABSPATH . 'includes/notes/class-wc-payments-notes-stripe-billing-deprecation.php';
+	}
+
+	/**
+	 * Tests for WC_Payments_Notes_Stripe_Billing_Deprecation::can_be_added()
+	 */
+	public function test_can_be_added() {
+		// Mock WC_Payments_Features::is_stripe_billing_enabled() to return true.
+		$this->set_feature_flag_option( WC_Payments_Features::WCPAY_SUBSCRIPTIONS_FLAG_NAME, '1' );
+		$this->set_feature_flag_option( WC_Payments_Features::STRIPE_BILLING_FLAG_NAME, '1' );
+
+		// Mock class_exists('WC_Subscriptions') to return false.
+		$this->mock_class_exists( 'WC_Subscriptions', false );
+
+		$this->assertTrue( WC_Payments_Notes_Stripe_Billing_Deprecation::can_be_added() );
+
+		// Test when WooCommerce Subscriptions is active.
+		$this->mock_class_exists( 'WC_Subscriptions', true );
+		$this->assertFalse( WC_Payments_Notes_Stripe_Billing_Deprecation::can_be_added() );
+
+		// Test when Stripe Billing is not enabled.
+		$this->set_feature_flag_option( WC_Payments_Features::STRIPE_BILLING_FLAG_NAME, '0' );
+		$this->mock_class_exists( 'WC_Subscriptions', false );
+		$this->assertFalse( WC_Payments_Notes_Stripe_Billing_Deprecation::can_be_added() );
+	}
+
+	/**
+	 * Tests for WC_Payments_Notes_Stripe_Billing_Deprecation::get_note()
+	 */
+	public function test_get_note() {
+		$title   = 'Important information regarding subscriptions in WooPayments';
+		$content = 'From version 9.7 of WooPayments (scheduled for 23 July, 2025), you\'ll no longer be able to offer new product subscriptions using the built-in subscriptions functionality. To avoid disruption, please install WooCommerce Subscriptions for free.';
+
+		$note = WC_Payments_Notes_Stripe_Billing_Deprecation::get_note();
+
+		$this->assertInstanceOf( 'Automattic\WooCommerce\Admin\Notes\Note', $note );
+		$this->assertEquals( $title, $note->get_title() );
+		$this->assertEquals( $content, $note->get_content() );
+		$this->assertEquals( 'info', $note->get_type() );
+		$this->assertEquals( 'wc-payments-notes-stripe-billing-deprecation', $note->get_name() );
+		$this->assertEquals( 'woocommerce-payments', $note->get_source() );
+
+		$actions = $note->get_actions();
+		$this->assertCount( 1, $actions );
+		$this->assertEquals( 'Install WooCommerce Subscriptions', $actions[0]->label );
+		$this->assertEquals( 'https://woocommerce.com/products/woocommerce-subscriptions/', $actions[0]->query );
+	}
+
+	/**
+	 * Mock class_exists() for a specific class.
+	 *
+	 * @param string $class_name The class name to mock.
+	 * @param bool   $exists     Whether the class exists.
+	 */
+	private function mock_class_exists( $class_name, $exists ) {
+		global $wp_test_helpers;
+		$wp_test_helpers->mock_function( 'class_exists', $exists );
+	}
+
+	private function set_feature_flag_option( string $option, string $value ) {
+		add_filter(
+			'pre_option_' . $option,
+			function () use ( $value ) {
+				return $value;
+			}
+		);
+	}
+}
