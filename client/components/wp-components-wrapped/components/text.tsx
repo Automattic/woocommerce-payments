bd99ccfaf3e62ed73bbcc9933e12d8e1df5d7e41
Rafael Meneses <meneses.tio@gmail.com>
2025-07-15 15:58:57 -0300

    add new component

---
+++ client/components/wp-components-wrapped/components/text.tsx
@@ -0,0 +1,13 @@
+/**
+ * External dependencies
+ */
+// @ts-expect-error: Suppressing Module '"@wordpress/components"' has no exported member '__experimentalText'.
+// eslint-disable-next-line @wordpress/no-unsafe-wp-apis, no-restricted-syntax
+import { __experimentalText as BundledText } from '@wordpress/components';
+
+/**
+ * Internal dependencies
+ */
+import { makeWrappedComponent } from '../make-wrapped-component';
+
+export const Text = makeWrappedComponent( BundledText, 'Text' );
