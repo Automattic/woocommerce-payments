7660202ecf2d40d7f9ea95e20ebe5a40e31bc711
frosso <francesco.rosso@automattic.com>
2025-07-16 14:46:49 +0200

    WIP

---
+++ client/settings/fraud-protection/advanced-settings/__tests__/rule-card.test.js
@@ -0,0 +1,21 @@
+/**
+ * External dependencies
+ */
+import React from 'react';
+import { render } from '@testing-library/react';
+import FraudProtectionRuleCard from '../rule-card';
+/**
+ * Internal dependencies
+ */
+describe( 'Fraud protection rule card tests', () => {
+	test( 'renders correctly', () => {
+		const container = render(
+			<FraudProtectionRuleCard title="test title" id="test-id">
+				test content
+			</FraudProtectionRuleCard>
+		);
+		expect( container ).toMatchSnapshot();
+		expect( container.queryByText( 'test title' ) ).toBeInTheDocument();
+		expect( container.queryByText( 'test content' ) ).toBeInTheDocument();
+	} );
+} );
