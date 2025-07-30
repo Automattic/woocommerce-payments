d05748a0708801fd889d60533bd2f84d19ceb22e
mordeth <cvetan.cvetanov@automattic.com>
2025-07-30 11:46:19 +0300

    Show Activate Payments notice only if test mode onboarding

--- includes/admin/class-wc-payments-admin-settings.php
+++ includes/admin/class-wc-payments-admin-settings.php
@@ -51,7 +51,7 @@ class WC_Payments_Admin_Settings {
 	 * Add notice explaining test mode when it's enabled.
 	 */
 	public function display_test_mode_notice() {
-		if ( WC_Payments::mode()->is_test() ) {
+		if ( WC_Payments::mode()->is_test_mode_onboarding() ) {
 			?>
 			<div id="wcpay-test-mode-notice" class="notice notice-warning">
 				<p>
