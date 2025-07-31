9dd23661b5ffd4119092fcf79cc5057930f2f233
frosso <francesco.rosso@automattic.com>
2025-07-31 19:29:42 +0200

    WIP

--- client/settings/payment-methods-list/__tests__/payment-method.test.js
+++ client/settings/payment-methods-list/__tests__/payment-method.test.js
@@ -4,7 +4,7 @@
  * External dependencies
  */
 import React from 'react';
-import { render, screen, waitFor, act } from '@testing-library/react';
+import { render, screen, waitFor } from '@testing-library/react';
 import user from '@testing-library/user-event';
 
 /**
