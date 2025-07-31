0851214324ad9b169388697c7dab8254e21987e0
frosso <francesco.rosso@automattic.com>
2025-07-31 16:31:49 +0200

    WIP

--- client/components/form/fields.tsx
+++ client/components/form/fields.tsx
@@ -40,26 +40,30 @@ export type GroupedSelectFieldProps< ItemType > = GroupedSelectControlProps<
  */
 const makeField = (
 	Control: React.ElementType,
-	props: CommonProps & Record< string, any >,
+	props: CommonProps & Record< any, any >,
 	ref?: React.Ref< any >
-): React.ReactElement => {
+) => {
 	const { error, ...rest } = props;
-	if ( ! error ) return React.createElement( Control, { ...rest, ref } );
+	if ( ! error ) return <Control { ...rest } ref={ ref } />;
 	return (
 		<>
-			{ React.createElement( Control, {
-				...rest,
-				ref,
-				className: clsx( rest.className, 'has-error' ),
-			} ) }
-			<div className="components-form-field__error">{ error }</div>
+			<Control
+				{ ...rest }
+				ref={ ref }
+				className={ clsx( rest.className, 'has-error' ) }
+			/>
+			{ <div className="components-form-field__error">{ error }</div> }
 		</>
 	);
 };
 
 export const TextField = forwardRef< HTMLInputElement, TextFieldProps >(
 	( props, ref ) => {
-		return makeField( TextControl, { ...props }, ref );
+		return makeField(
+			TextControl,
+			{ ...props, __nextHasNoMarginBottom: true },
+			ref
+		);
 	}
 );
 
