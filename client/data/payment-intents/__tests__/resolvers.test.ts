76c87cdc7c4cb4670c9bc3f1be8b578389a05437
frosso <francesco.rosso@automattic.com>
2025-08-01 11:19:25 +0200

    WIP

---
+++ client/data/payment-intents/__tests__/resolvers.test.ts
@@ -0,0 +1,66 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+import { apiFetch } from '@wordpress/data-controls';
+import { controls } from '@wordpress/data';
+
+/**
+ * Internal dependencies
+ */
+import { updatePaymentIntent, updateErrorForPaymentIntent } from '../actions';
+import { getPaymentIntent } from '../resolvers';
+import { PaymentIntent } from 'wcpay/types/payment-intents';
+import { paymentIntentId, paymentIntentMock } from './hooks.test';
+
+const errorResponse = { code: 'error' };
+
+const paymentIntentResponse: { data: PaymentIntent } = {
+	data: paymentIntentMock,
+};
+
+describe( 'getPaymentIntent resolver', () => {
+	let generator: Generator< unknown >;
+
+	beforeEach( () => {
+		generator = getPaymentIntent( paymentIntentId );
+		expect( generator.next().value ).toEqual(
+			apiFetch( {
+				path: `/wc/v3/payments/payment_intents/${ paymentIntentId }`,
+			} )
+		);
+	} );
+
+	afterEach( () => {
+		expect( generator.next().done ).toStrictEqual( true );
+	} );
+
+	describe( 'on success', () => {
+		test( 'should update state with payment intent data', () => {
+			expect(
+				generator.next( paymentIntentResponse.data ).value
+			).toEqual(
+				updatePaymentIntent(
+					paymentIntentResponse.data.id,
+					paymentIntentResponse.data
+				)
+			);
+		} );
+	} );
+
+	describe( 'on error', () => {
+		test( 'should update state with error', () => {
+			expect( generator.throw( errorResponse ).value ).toEqual(
+				controls.dispatch(
+					'core/notices',
+					'createErrorNotice',
+					expect.any( String )
+				)
+			);
+			expect( generator.next().value ).toEqual(
+				updateErrorForPaymentIntent( paymentIntentId, errorResponse )
+			);
+		} );
+	} );
+} );
