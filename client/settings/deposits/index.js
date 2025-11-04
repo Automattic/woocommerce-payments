6fa4ba8976c62de45ffd4aaa5c83989f77bc478b
Dat Hoang <htdat@users.noreply.github.com>
2025-11-04 15:57:18 +0700

    Update texts for New Account Waiting Period

--- client/settings/deposits/index.js
+++ client/settings/deposits/index.js
@@ -191,7 +191,7 @@ const DepositsSchedule = () => {
 					mixedString: __(
 						'Payout scheduling becomes available after the standard 7-day waiting period for new accounts is complete.' +
 							' ' +
-							'{{whyLink}}Learn more{{/whyLink}}',
+							'{{learnMoreLink}}Learn more{{/learnMoreLink}}',
 						'woocommerce-payments'
 					),
 					components: {
