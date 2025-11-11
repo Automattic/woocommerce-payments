3a61d0c150f1d51d36aaddc2a820094f59e02fe0
Ahmed <ahmed.el.azzabi@automattic.com>
2025-11-11 18:07:35 +0100

    Fix failing test

--- tests/unit/test-class-wc-payments-order-service.php
+++ tests/unit/test-class-wc-payments-order-service.php
@@ -1395,12 +1395,16 @@ class WC_Payments_Order_Service_Test extends WCPAY_UnitTestCase {
 	}
 
 	public function test_attach_transaction_fee_to_order_uncaptured_charge() {
-		$order  = WC_Helper_Order::create_order();
+		$mock_order = $this->createMock( 'WC_Order' );
+		$mock_order
+			->expects( $this->never() )
+			->method( 'update_meta_data' );
+
 		$charge = new WC_Payments_API_Charge( 'ch_mock', 1500, new DateTime(), null, null, null, null, 113, [], [], 'usd' );
 		$charge->set_captured( false );
-		$this->order_service->attach_transaction_fee_to_order( $order, $charge );
+
 		// Fee should not be set for uncaptured charges.
-		$this->assertEquals( '', $order->get_meta( '_wcpay_transaction_fee', true ) );
+		$this->order_service->attach_transaction_fee_to_order( $mock_order, $charge );
 	}
 
 	public function test_add_note_and_metadata_for_created_refund_successful_fully_refunded(): void {
