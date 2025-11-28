e903c9927ba7828719fb7f55ca14127d22cfe639
Daniel Mallory <daniel.mallory@automattic.com>
2025-11-28 16:25:45 +0000

    Adding tests for the new util functions and promotional-badge component.

---
+++ client/components/promotional-badge/__tests__/index.test.tsx
@@ -0,0 +1,120 @@
+/** @format */
+
+/**
+ * External dependencies
+ */
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+
+/**
+ * Internal dependencies
+ */
+import PromotionalBadge from '../';
+
+describe( 'PromotionalBadge', () => {
+	test( 'renders the badge with message', () => {
+		render(
+			<PromotionalBadge
+				message="50% off fees"
+				tooltip="You are getting 50% off on processing fees."
+			/>
+		);
+
+		expect( screen.getByText( '50% off fees' ) ).toBeInTheDocument();
+	} );
+
+	test( 'renders with success type by default', () => {
+		const { container } = render(
+			<PromotionalBadge
+				message="25% off fees"
+				tooltip="Discount details"
+			/>
+		);
+
+		const badge = container.querySelector( '.wcpay-promotional-badge' );
+		expect( badge ).toHaveClass( 'chip-success' );
+	} );
+
+	test( 'renders with custom chip type', () => {
+		const { container } = render(
+			<PromotionalBadge
+				message="Limited offer"
+				tooltip="Discount details"
+				type="warning"
+			/>
+		);
+
+		const badge = container.querySelector( '.wcpay-promotional-badge' );
+		expect( badge ).toHaveClass( 'chip-warning' );
+	} );
+
+	test( 'renders the info icon button', () => {
+		render(
+			<PromotionalBadge
+				message="50% off fees"
+				tooltip="Tooltip content"
+				tooltipLabel="Discount details"
+			/>
+		);
+
+		const tooltipButton = screen.getByRole( 'button', {
+			name: /discount details/i,
+		} );
+		expect( tooltipButton ).toBeInTheDocument();
+	} );
+
+	test( 'shows tooltip content when clicking the info icon', () => {
+		render(
+			<PromotionalBadge
+				message="50% off fees"
+				tooltip="You are getting 50% off on processing fees."
+				tooltipLabel="Discount details"
+			/>
+		);
+
+		const tooltipButton = screen.getByRole( 'button', {
+			name: /discount details/i,
+		} );
+		fireEvent.click( tooltipButton );
+
+		expect(
+			screen.getByText( 'You are getting 50% off on processing fees.' )
+		).toBeInTheDocument();
+	} );
+
+	test( 'uses default tooltip label when not provided', () => {
+		render(
+			<PromotionalBadge
+				message="50% off fees"
+				tooltip="Tooltip content"
+			/>
+		);
+
+		const tooltipButton = screen.getByRole( 'button', {
+			name: /more information/i,
+		} );
+		expect( tooltipButton ).toBeInTheDocument();
+	} );
+
+	test( 'applies chip base class', () => {
+		const { container } = render(
+			<PromotionalBadge message="Test badge" tooltip="Test tooltip" />
+		);
+
+		const badge = container.querySelector( '.wcpay-promotional-badge' );
+		expect( badge ).toHaveClass( 'chip' );
+	} );
+
+	test( 'renders correctly with all props', () => {
+		const { container } = render(
+			<PromotionalBadge
+				message="30% off fees through Dec 31, 2026"
+				tooltip="You are getting 30% off on processing fees for the first $1,000 of total payment volume or through Dec 31, 2026."
+				type="primary"
+				tooltipLabel="View promotion details"
+			/>
+		);
+
+		expect( container ).toMatchSnapshot();
+	} );
+} );
