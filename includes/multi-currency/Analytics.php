80ef8d2dfec60ee12e5efb5c608d745c957203fb
Ahmed <ahmed.el.azzabi@automattic.com>
2025-12-24 13:29:52 +0100

    Add script translation support for WooCommerce Payments in Analytics class

--- includes/multi-currency/Analytics.php
+++ includes/multi-currency/Analytics.php
@@ -163,6 +163,7 @@ class Analytics {
 		$this->register_admin_scripts();
 
 		wp_enqueue_script( self::SCRIPT_NAME );
+		wp_set_script_translations( self::SCRIPT_NAME, 'woocommerce-payments' );
 	}
 
 	/**
