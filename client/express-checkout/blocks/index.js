ffc0c121f728db6fef0610886ec07e3e722e51a5
frosso <francesco.rosso@automattic.com>
2026-02-06 15:59:39 +0100

    separate chunk to reduce bundle size

--- client/express-checkout/blocks/index.js
+++ client/express-checkout/blocks/index.js
@@ -2,6 +2,7 @@
  * External dependencies
  */
 import { __ } from '@wordpress/i18n';
+import { lazy, Suspense } from 'react';
 
 /**
  * Internal dependencies
@@ -9,11 +10,44 @@ import { __ } from '@wordpress/i18n';
 import { PAYMENT_METHOD_NAME_EXPRESS_CHECKOUT_ELEMENT } from 'wcpay/checkout/constants';
 import { getConfig } from 'wcpay/utils/checkout';
 import ExpressCheckoutContainer from './components/express-checkout-container';
-import ApplePayPreview from './components/apple-pay-preview';
-import GooglePayPreview from './components/google-pay-preview';
-import AmazonPayPreview from './components/amazon-pay-preview';
 import { checkPaymentMethodIsAvailable } from '../utils/checkPaymentMethodIsAvailable';
 import '../compatibility/wc-order-attribution';
+
+const LazyApplePayPreview = lazy( () =>
+	import(
+		/* webpackChunkName: "express-checkout-previews" */ './components/apple-pay-preview'
+	)
+);
+const LazyGooglePayPreview = lazy( () =>
+	import(
+		/* webpackChunkName: "express-checkout-previews" */ './components/google-pay-preview'
+	)
+);
+const LazyAmazonPayPreview = lazy( () =>
+	import(
+		/* webpackChunkName: "express-checkout-previews" */ './components/amazon-pay-preview'
+	)
+);
+
+const PreviewFallback = () => <div style={ { minHeight: '40px' } } />;
+
+const ApplePayPreview = ( props ) => (
+	<Suspense fallback={ <PreviewFallback /> }>
+		<LazyApplePayPreview { ...props } />
+	</Suspense>
+);
+
+const GooglePayPreview = ( props ) => (
+	<Suspense fallback={ <PreviewFallback /> }>
+		<LazyGooglePayPreview { ...props } />
+	</Suspense>
+);
+
+const AmazonPayPreview = ( props ) => (
+	<Suspense fallback={ <PreviewFallback /> }>
+		<LazyAmazonPayPreview { ...props } />
+	</Suspense>
+);
 
 export const expressCheckoutElementApplePay = ( api ) => ( {
 	paymentMethodId: PAYMENT_METHOD_NAME_EXPRESS_CHECKOUT_ELEMENT,
