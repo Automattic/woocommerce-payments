f4ed4c4bf5055cd696847b77532a328113bda088
Radoslav Georgiev <rageorgiev@gmail.com>
2026-02-24 20:18:26 +0200

    Use variables for CSS, remove unused line.

--- client/review-prompt/index.tsx
+++ client/review-prompt/index.tsx
@@ -3,7 +3,6 @@
  */
 import React, { useCallback, useState } from 'react';
 import { __ } from '@wordpress/i18n';
-import { Icon, external } from '@wordpress/icons';
 
 /**
  * Internal dependencies
