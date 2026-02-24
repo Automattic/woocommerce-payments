d78b05745f4a9265954c9ca7dbb2ab9be17b847f
frosso <francesco.rosso@automattic.com>
2026-02-24 12:33:58 +0100

    remove mocks

--- client/express-checkout/utils/__tests__/button-appearance.test.js
+++ client/express-checkout/utils/__tests__/button-appearance.test.js
@@ -3,10 +3,6 @@
  */
 import { getExpressCheckoutButtonAppearance } from '..';
 
-jest.mock( 'wcpay/utils/express-checkout', () => ( {
-	getDefaultBorderRadius: jest.fn( () => 4 ),
-} ) );
-
 describe( 'getExpressCheckoutButtonAppearance', () => {
 	afterEach( () => {
 		delete window.wcpayExpressCheckoutParams;
