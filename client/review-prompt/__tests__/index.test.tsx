b257efb1c2d9beac6c1ec7b832220f9d42cf1d85
Radoslav Georgiev <rageorgiev@gmail.com>
2026-02-25 13:04:46 +0200

    Fix JS tests

--- client/review-prompt/__tests__/index.test.tsx
+++ client/review-prompt/__tests__/index.test.tsx
@@ -157,8 +157,7 @@ describe( 'ReviewPrompt', () => {
 		await waitFor( () => {
 			expect( mockWindowOpen ).toHaveBeenCalledWith(
 				'https://wordpress.org/support/plugin/woocommerce-payments/reviews/#new-post',
-				'_blank',
-				'noopener,noreferrer'
+				'_blank'
 			);
 		} );
 	} );
@@ -174,8 +173,7 @@ describe( 'ReviewPrompt', () => {
 		await waitFor( () => {
 			expect( mockWindowOpen ).toHaveBeenCalledWith(
 				'https://woocommerce.com/products/woocommerce-payments/#reviews',
-				'_blank',
-				'noopener,noreferrer'
+				'_blank'
 			);
 		} );
 	} );
@@ -347,8 +345,7 @@ describe( 'ReviewPrompt', () => {
 			// Should have tried to open in new window
 			expect( mockWindowOpen ).toHaveBeenCalledWith(
 				'https://wordpress.org/support/plugin/woocommerce-payments/reviews/#new-post',
-				'_blank',
-				'noopener,noreferrer'
+				'_blank'
 			);
 
 			// Should fall back to navigating current window
