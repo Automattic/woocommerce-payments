3a800f33179770e6434e65fd8fd5fe1bd9a216ce
frosso <francesco.rosso@automattic.com>
2026-03-10 22:57:13 +0100

    fix: unit tests after new WC version

--- tests/unit/test-class-wc-payment-gateway-wcpay.php
+++ tests/unit/test-class-wc-payment-gateway-wcpay.php
@@ -4467,6 +4467,7 @@ class WC_Payment_Gateway_WCPay_Test extends WCPAY_UnitTestCase {
 		}
 
 		update_option( WC_Payments_Features::WCPAY_DYNAMIC_CHECKOUT_PLACE_ORDER_BUTTON_FLAG_NAME, '1' );
+		\WC_Payments::get_gateway()->update_option( 'express_checkout_in_payment_methods', 'yes' );
 
 		$amazon_pay_payment_method = new UPE_Payment_Method(
 			$this->mock_token_service,
@@ -4492,6 +4493,7 @@ class WC_Payment_Gateway_WCPay_Test extends WCPAY_UnitTestCase {
 		$this->assertTrue( $gateway->has_custom_place_order_button );
 
 		delete_option( WC_Payments_Features::WCPAY_DYNAMIC_CHECKOUT_PLACE_ORDER_BUTTON_FLAG_NAME );
+		\WC_Payments::get_gateway()->update_option( 'express_checkout_in_payment_methods', 'no' );
 	}
 
 	public function test_has_custom_place_order_button_not_set_for_express_checkout_when_feature_disabled() {
