e857c75336ab69bd8ff198ba9f56a8f98308c903
Dat Hoang <htdat@users.noreply.github.com>
2026-06-27 15:01:32 +0700

    fix: changelog formatter PCRE JIT stack exhaustion on large changelog files

--- bin/class-wcpay-changelog-formatter.php
+++ bin/class-wcpay-changelog-formatter.php
@@ -76,8 +76,12 @@ class WCPay_Changelog_Formatter extends Parser implements FormatterPlugin {
 		}
 
 		// Entries make up the rest of the document.
+		// (*NO_JIT) prevents JIT stack exhaustion on large changelogs; interpreter limits are far more generous.
 		$entries = [];
-		preg_match_all( '/^=\s+([^\n=]+)\s+=(((?!^=).)+)/ms', $changelog, $version_sections );
+		$matched = preg_match_all( '/(*NO_JIT)^=\s+([^\n=]+)\s+=(((?!^=).)+)/ms', $changelog, $version_sections );
+		if ( false === $matched ) {
+			throw new \RuntimeException( 'Failed to parse changelog: PCRE error ' . preg_last_error_msg() );
+		}
 
 		foreach ( $version_sections[0] as $section ) {
 			$heading_pattern = '/^= +(\[?[^] ]+\]?) - (.+?) =/';
