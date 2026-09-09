4c7b13731c69837cf41409aa05bf5d1f7d0bb14f
frosso <francesco.rosso@automattic.com>
2026-09-09 14:57:04 +0200

    simplify comments

--- tasks/release.js
+++ tasks/release.js
@@ -32,11 +32,11 @@ const filesToCopy = [
 
 // run npm dist
 rm( '-rf', 'dist' );
-// fatal: false to name the step that broke instead of throwing. A command
-// killed by a signal still reports code 0, so check the output too.
 const clientBuild = exec( 'SOURCEMAP=hidden pnpm run build:client', {
+	// using fatal: false so that it doesn't throw
 	fatal: false,
 } );
+// checking both the code and the output, just to be safe
 if ( clientBuild.code !== 0 || ! fs.existsSync( 'dist' ) ) {
 	console.error(
 		chalk.red( 'The client build failed; nothing was packaged.' )
